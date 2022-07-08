5ddd9cc8243436a3faf7631e3a85fb5c58e90acd
Lorenz <ESs95s3P5z8Pheb>
2022-07-08 13:17:15 +0200

    moved all settings into the feature logic

--- src/main/java/at/lorenz/mod/config/Features.java
+++ src/main/java/at/lorenz/mod/config/Features.java
@@ -1,4 +1,4 @@
-package at.lorenz.mod;
+package at.lorenz.mod.config;
 
 import com.google.gson.annotations.Expose;
 import com.thatgravyboat.skyblockhud.LorenzMod;
@@ -77,30 +77,79 @@ public class Features extends Config {
     public Dungeon dungeon = new Dungeon();
 
     @Expose
-    @Category(name = "Inventory", desc = "Changing the behavior around the inventory.")
-    public Inventory inventory = new Inventory();
+    @Category(name = "Item", desc = "Changing the behavior around items and the inventory.")
+    public Inventory item = new Inventory();
+
+    @Expose
+    @Category(name = "Bazaar", desc = "Bazaar settings.")
+    public Bazaar bazaar = new Bazaar();
 
     public static class Chat {
-
         @Expose
         @ConfigOption(name = "Main Chat Filter", desc = "Hides all the annoying chat messages.")
         @ConfigEditorBoolean
-        public boolean filter = false;
+        public boolean mainFilter = true;
     }
 
     public static class Dungeon {
-
         @Expose
         @ConfigOption(name = "Hide Dungeon Messages", desc = "Hides annoyung dungeon messages.")
         @ConfigEditorBoolean
         public boolean hideAnnoyingMessages = false;
+
+        @Expose
+        @ConfigOption(name = "Highlight Clicked Blocks", desc = "Highlight blocks in dungeon when clicked: Lever, Chest, Wither Essence")
+        @ConfigEditorBoolean
+        public boolean highlightClickedBlocks = false;
     }
 
     public static class Inventory {
-
         @Expose
-        @ConfigOption(name = "Hide Not Clickable Items", desc = "Hide items that are not clickable in " + "the current inventory: ah, bz, accessory bag, etc")
+        @ConfigOption(name = "Not Clickable Items", desc = "Hide items that are not clickable in " +
+                "the current inventory: ah, bz, accessory bag, etc")
         @ConfigEditorBoolean
         public boolean hideNotClickableItems = false;
+
+        @Expose
+        @ConfigOption(name = "Master Star Number", desc = "Shows the Tier of the Master Star.")
+        @ConfigEditorBoolean
+        public boolean displayMasterStarNumber = false;
+
+        @Expose
+        @ConfigOption(name = "Master Skull Number", desc = "Shows the tier of the Master Skull accessory.")
+        @ConfigEditorBoolean
+        public boolean displayMasterSkullNumber = false;
+
+        @Expose
+        @ConfigOption(name = "Dungeon Head Floor", desc = "Shows the correct floor for golden and diamond heads.")
+        @ConfigEditorBoolean
+        public boolean displayDungeonHeadFloor = false;
+
+        @Expose
+        @ConfigOption(name = "New Year Cake Number", desc = "Shows the Number of the Year of New Year Cakes.")
+        @ConfigEditorBoolean
+        public boolean displayNewYearCakeNumber = false;
+
+        @Expose
+        @ConfigOption(name = "Pet Level", desc = "Shows the level of the pet when not maxed.")
+        @ConfigEditorBoolean
+        public boolean displayPetLevel = false;
+
+        @Expose
+        @ConfigOption(name = "Sack Name", desc = "Shows an abbreviation of the Sack name.")
+        @ConfigEditorBoolean
+        public boolean displaySackName = false;
+
+        @Expose
+        @ConfigOption(name = "Minion Tier", desc = "Shows the Minion Tier over Items.")
+        @ConfigEditorBoolean
+        public boolean displayMinionTier = false;
+    }
+
+    public static class Bazaar {
+        @Expose
+        @ConfigOption(name = "Order Helper", desc = "Show visual hints when items are ready to pickup or outbid.")
+        @ConfigEditorBoolean
+        public boolean orderHelper = false;
     }
 }
