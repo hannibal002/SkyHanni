5a609c48b9d0ebb89c19627cd6dbbff21fc8324a
Lorenz <ESs95s3P5z8Pheb>
2022-07-08 23:34:20 +0200

    add hide exp bottles support

---
+++ src/main/java/at/lorenz/mod/mixins/MixinRenderManager.java
@@ -0,0 +1,18 @@
+package at.lorenz.mod.mixins;
+
+import at.lorenz.mod.mixinhooks.RenderManagerHookKt;
+import net.minecraft.client.renderer.culling.ICamera;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RenderManager.class)
+public class MixinRenderManager {
+    @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
+    private void shouldRender(Entity entityIn, ICamera camera, double camX, double camY, double camZ, CallbackInfoReturnable<Boolean> cir) {
+        RenderManagerHookKt.shouldRender(entityIn, camera, camX, camY, camZ, cir);
+    }
+}
