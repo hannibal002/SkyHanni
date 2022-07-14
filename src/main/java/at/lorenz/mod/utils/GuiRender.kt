ba2470c8bbd708224e00bd6460983e583f3f294f
Lorenz <ESs95s3P5z8Pheb>
2022-07-14 10:05:23 +0200

    default text render is white now

--- src/main/java/at/lorenz/mod/utils/GuiRender.kt
+++ src/main/java/at/lorenz/mod/utils/GuiRender.kt
@@ -1,5 +1,6 @@
 package at.lorenz.mod.utils
 
+import at.lorenz.mod.utils.LorenzUtils.removeColorCodes
 import com.thatgravyboat.skyblockhud_2.core.config.Position
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.ScaledResolution
@@ -8,13 +9,14 @@ import net.minecraft.client.renderer.GlStateManager
 object GuiRender {
 
     fun Position.renderString(string: String) {
+        val textToRender = "§f$string"
 
         GlStateManager.pushMatrix()
         val resolution = ScaledResolution(Minecraft.getMinecraft())
 
         val renderer = Minecraft.getMinecraft().renderManager.fontRenderer
 
-        val offsetX = (200 - renderer.getStringWidth(string)) / 2
+        val offsetX = (200 - renderer.getStringWidth(textToRender.removeColorCodes())) / 2
 
         val x = getAbsX(resolution, 200) + offsetX
         val y = getAbsY(resolution, 16)
@@ -22,7 +24,7 @@ object GuiRender {
 
 
         GlStateManager.translate(x + 1.0, y + 1.0, 0.0)
-        renderer.drawStringWithShadow(string, 0f, 0f, 0)
+        renderer.drawStringWithShadow(textToRender, 0f, 0f, 0)
 
 
         GlStateManager.popMatrix()
