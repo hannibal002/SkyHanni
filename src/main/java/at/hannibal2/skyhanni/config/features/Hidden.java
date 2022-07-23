977991e8cfe61b843c922e380be72a0c72e886b5
Lorenz <ESs95s3P5z8Pheb>
2022-07-23 10:19:23 +0200

    moving api key and current pet data into hidden config entry

---
+++ src/main/java/at/hannibal2/skyhanni/config/features/Hidden.java
@@ -0,0 +1,12 @@
+package at.hannibal2.skyhanni.config.features;
+
+import com.google.gson.annotations.Expose;
+
+public class Hidden {
+
+    @Expose
+    public String apiKey = "";
+
+    @Expose
+    public String currentPet = "";
+}
