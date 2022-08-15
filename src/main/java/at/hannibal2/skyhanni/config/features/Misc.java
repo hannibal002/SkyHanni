00ddcea975b06bc5c6dd185698401a75ef3c39b0
Lorenz <lo.scherf@gmail.com>
2022-08-15 21:45:23 +0200

    add sven slayer

--- src/main/java/at/hannibal2/skyhanni/config/features/Misc.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/Misc.java
@@ -48,7 +48,7 @@ public class Misc {
                     "\u00a7bEnder Dragon (not finished)",
                     "\u00a7bRevenant Horror",
                     "\u00a7bTarantula Broodfather",
-                    "\u00a7bWolf Slayer (not implemented)",
+                    "\u00a7bSven Packmaster",
                     "\u00a7bVoidgloom Seraph",
                     "\u00a7bInferno Demonlord (only tier 1 yet)",
                     "\u00a7bHeadless Horseman (bugged)",
@@ -63,7 +63,7 @@ public class Misc {
     )
     @ConfigAccordionId(id = 1)
     //TODO only show currently working and tested features
-    public List<Integer> damageIndicatorBossesToShow = new ArrayList<>(Arrays.asList(0, 1, 2, 5, 6, 8, 9));
+    public List<Integer> damageIndicatorBossesToShow = new ArrayList<>(Arrays.asList(0, 1, 2, 5, 6, 7, 8, 9));
 
 
     @Expose
