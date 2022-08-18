2f78bd32d9bebd216581d61dcdc53bd6adcea13a
Lorenz <lo.scherf@gmail.com>
2022-08-18 10:04:18 +0200

    adding dungeon dead and other island spectator chat format

--- src/main/java/at/hannibal2/skyhanni/events/PlayerSendChatEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/PlayerSendChatEvent.kt
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.events
 
 import at.hannibal2.skyhanni.features.chat.PlayerMessageChannel
 
-
 class PlayerSendChatEvent(
     val channel: PlayerMessageChannel,
     val playerName: String,
--- src/main/java/at/hannibal2/skyhanni/features/chat/PlayerMessageChannel.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/PlayerMessageChannel.kt
@@ -1,10 +1,12 @@
 package at.hannibal2.skyhanni.features.chat
 
-enum class PlayerMessageChannel(val prefix: String) {
+enum class PlayerMessageChannel(val prefix: String, val originalPrefix: String) {
 
-    ALL("§fA>"),
-    ALL_ADVERTISEMENT("§8A>"),
-    PARTY("§9P>"),
-    GUILD("§2G>"),
-    COOP("§bCC>"),
+    ALL("§fA>", ""),
+    ALL_GUESTING("§aA>", "§a[✌] "),
+    ALL_DUNGEON_DEAD("§7A>", "§7[GHOST] "),
+    PARTY("§9P>", "§9Party §8> "),
+    GUILD("§2G>", "§2Guild > "),
+    COOP("§bCC>", "§bCo-op > "),
+
 }
