900bc9058a90130ac7bb0f88c6b30ef1bf390504
Lorenz <lo.scherf@gmail.com>
2022-08-22 04:27:26 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/test/LorenzTest.kt
+++ src/main/java/at/hannibal2/skyhanni/test/LorenzTest.kt
@@ -2,9 +2,9 @@ package at.hannibal2.skyhanni.test
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.PacketEvent
-import at.hannibal2.skyhanni.utils.GuiRender.renderString
 import at.hannibal2.skyhanni.utils.LorenzDebug
 import at.hannibal2.skyhanni.utils.LorenzLogger
+import at.hannibal2.skyhanni.utils.RenderUtils.renderString
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.network.play.server.S0EPacketSpawnObject
 import net.minecraft.network.play.server.S0FPacketSpawnMob
