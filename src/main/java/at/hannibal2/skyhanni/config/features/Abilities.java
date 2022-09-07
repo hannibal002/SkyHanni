a356555e2cf6c9a88e9039340e8a294a3ed5677a
Lorenz <lo.scherf@gmail.com>
2022-09-07 13:46:31 +0200

    fixed bazaar api checking too often

--- src/main/java/at/hannibal2/skyhanni/config/features/Abilities.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/Abilities.java
@@ -52,7 +52,22 @@ public class Abilities {
     public boolean summoningMobColored = false;
 
     @Expose
-    @ConfigOption(name = "Fire Veil Particles", desc = "Hiding the flame particles when using the Fire Veil Wand ability.")
-    @ConfigEditorBoolean
-    public boolean fireVeilWandHider = false;
+    @ConfigOption(name = "Fire Veil", desc = "")
+    @ConfigEditorAccordion(id = 1)
+    public boolean fireVeilWand = false;
+
+    @Expose
+    @ConfigOption(name = "Fire Veil Design", desc = "Changes the flame particles of the Fire Veil Wand ability")
+    @ConfigEditorDropdown(values = {"Particles", "Line", "Off"})
+    @ConfigAccordionId(id = 1)
+    public int fireVeilWandDisplay = 0;
+
+    @Expose
+    @ConfigOption(
+            name = "Line Color",
+            desc = "Changes the color of the Fire Veil Wand line"
+    )
+    @ConfigEditorColour
+    @ConfigAccordionId(id = 1)
+    public String fireVeilWandDisplayColor = "0:245:255:85:85";
 }
