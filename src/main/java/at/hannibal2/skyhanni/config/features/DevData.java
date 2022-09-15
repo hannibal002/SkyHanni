39a5252747839f6b5207177cab974704d388562a
Lorenz <lo.scherf@gmail.com>
2022-09-15 15:34:30 +0200

    added more mc console filters

--- src/main/java/at/hannibal2/skyhanni/config/features/DevData.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/DevData.java
@@ -34,15 +34,21 @@ public class DevData {
     public boolean minecraftConsole = false;
 
     @Expose
-    @ConfigOption(name = "Unfiltered Debug", desc = "Print the debug information for unfiltered console messages")
+    @ConfigOption(name = "Unfiltered Debug", desc = "Print the debug information for unfiltered console messages.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 1)
     public boolean printUnfilteredDebugs = false;
 
     @Expose
+    @ConfigOption(name = "Unfiltered Debug File", desc = "Print the debug information into log files instead of into the console for unfiltered console messages.")
+    @ConfigEditorBoolean
+    @ConfigAccordionId(id = 1)
+    public boolean logUnfilteredFile = false;
+
+    @Expose
     @ConfigOption(
             name = "Outside SkyBlock",
-            desc = "Print the debug information for unfiltered console messages outside SkyBlock too"
+            desc = "Print the debug information for unfiltered console messages outside SkyBlock too."
     )
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 1)
@@ -51,7 +57,7 @@ public class DevData {
     @Expose
     @ConfigOption(
             name = "Log Filtered",
-            desc = "Log the filtered messages into the console"
+            desc = "Log the filtered messages into the console."
     )
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 1)
@@ -64,7 +70,7 @@ public class DevData {
     public boolean consoleFilters = false;
 
     @Expose
-    @ConfigOption(name = "Filter Chat", desc = "Filter chat messages")
+    @ConfigOption(name = "Filter Chat", desc = "Filter chat messages.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 2)
     public boolean filterChat = false;
@@ -76,33 +82,45 @@ public class DevData {
     public boolean filterGrowBuffer = false;
 
     @Expose
-    @ConfigOption(name = "Filter Sound Error", desc = "Filter 'Unable to play unknown soundEvent'")
+    @ConfigOption(name = "Filter Sound Error", desc = "Filter 'Unable to play unknown soundEvent'.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 2)
     public boolean filterUnknownSound = false;
 
     @Expose
     @ConfigOption(name = "Filter Scoreboard Errors", desc = "Filter error messages with Scoreboard: removeTeam, createTeam, " +
-            "removeObjective and 'scoreboard team already exists'")
+            "removeObjective and 'scoreboard team already exists'.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 2)
     public boolean filterScoreboardErrors = false;
 
     @Expose
-    @ConfigOption(name = "Filter Particle", desc = "Filter message 'Could not spawn particle effect VILLAGER_HAPPY'")
+    @ConfigOption(name = "Filter Particle", desc = "Filter message 'Could not spawn particle effect VILLAGER_HAPPY'.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 2)
     public boolean filterParticleVillagerHappy = false;
 
     @Expose
-    @ConfigOption(name = "Filter OptiFine", desc = "Filter OptiFine messages CustomItems and ConnectedTextures during loading")
+    @ConfigOption(name = "Filter OptiFine", desc = "Filter OptiFine messages CustomItems and ConnectedTextures during loading.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 2)
     public boolean filterOptiFine = false;
 
     @Expose
-    @ConfigOption(name = "Filter AsmHelper Transformer", desc = "Filter messages when AsmHelper is Transforming a class during loading")
+    @ConfigOption(name = "Filter AsmHelper Transformer", desc = "Filter messages when AsmHelper is Transforming a class during loading.")
     @ConfigEditorBoolean
     @ConfigAccordionId(id = 2)
     public boolean filterAmsHelperTransformer = false;
+
+    @Expose
+    @ConfigOption(name = "Filter Applying AsmWriter", desc = "Filter messages when AsmHelper is applying AsmWriter ModifyWriter.")
+    @ConfigEditorBoolean
+    @ConfigAccordionId(id = 2)
+    public boolean filterAsmHelperApplying = false;
+
+    @Expose
+    @ConfigOption(name = "Filter Biome ID Bounds", desc = "Filter message 'Biome ID is out of bounds'.")
+    @ConfigEditorBoolean
+    @ConfigAccordionId(id = 2)
+    public boolean filterBiomeIdBounds = false;
 }
