adaf98c3b893689c56ffb873063c4200209bf763
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2022-09-17 09:51:12 +0200

    comment out lorenz test stuff by default

--- src/main/java/at/hannibal2/skyhanni/test/LorenzTest.kt
+++ src/main/java/at/hannibal2/skyhanni/test/LorenzTest.kt
@@ -147,165 +147,164 @@ class LorenzTest {
         val volume = event.volume
 
         //background music
-        if (soundName == "note.harp") {
-//                if (distance < 2) {
-
-
-            //Wilderness
-            val list = mutableListOf<Float>()
-//                list.add(0.4920635)
-//                list.add(0.74603176)
-//                list.add(0.8888889)
-//                list.add(1.1746032)
-//                list.add(1.7777778)
-//                list.add(0.5873016)
-//                list.add(1f)
-//                list.add(1.4920635)
-//                list.add(0.4920635)
-//                list.add(1.8730159)
-//                list.add(0.82539684)
-//                list.add(1.1111112)
-//                list.add(1.6666666)
-//                list.add(0.5555556)
-//                list.add(0.6984127)
-//                list.add(0.93650794)
-//                list.add(1.4126984)
-//                list.add(1.3333334)
-//                list.add(1.5873016)
-
-            if (pitch in list) {
-                if (Minecraft.getMinecraft().thePlayer.isSneaking) {
-                    event.isCanceled = true
-                }
-                return
-            }
-        }
+//        if (soundName == "note.harp") {
+////                if (distance < 2) {
+//
+//
+//            //Wilderness
+//            val list = mutableListOf<Float>()
+////                list.add(0.4920635)
+////                list.add(0.74603176)
+////                list.add(0.8888889)
+////                list.add(1.1746032)
+////                list.add(1.7777778)
+////                list.add(0.5873016)
+////                list.add(1f)
+////                list.add(1.4920635)
+////                list.add(0.4920635)
+////                list.add(1.8730159)
+////                list.add(0.82539684)
+////                list.add(1.1111112)
+////                list.add(1.6666666)
+////                list.add(0.5555556)
+////                list.add(0.6984127)
+////                list.add(0.93650794)
+////                list.add(1.4126984)
+////                list.add(1.3333334)
+////                list.add(1.5873016)
+//
+//            if (pitch in list) {
+//                if (Minecraft.getMinecraft().thePlayer.isSneaking) {
+//                    event.isCanceled = true
+//                }
+//                return
+//            }
+//        }
 
 
         //diana ancestral spade
-        if (soundName == "note.harp") {
-            val list = mutableListOf<Float>()
-            list.add(0.52380955f)
-            list.add(0.5555556f)
-            list.add(0.6031746f)
-            list.add(0.63492066f)
-            list.add(0.6825397f)
-            list.add(0.71428573f)
-            list.add(0.7619048f)
-            list.add(0.7936508f)
-            list.add(0.84126985f)
-            list.add(0.8888889f)
-            list.add(0.9206349f)
-            list.add(0.96825397f)
-            list.add(1.476191f)
-            list.add(1.476191f)
-            list.add(0.50793654f)
-            list.add(0.6507937f)
-            list.add(0.6984127f)
-            list.add(0.74603176f)
-            list.add(0.93650794f)
-            list.add(0.984127f)
-            list.add(1.968254f)
-            list.add(0.4920635f)
-            list.add(1.1587307f)
-            list.add(1.1587301f)
-            list.add(1.2857143f)
-            list.add(1.4126984f)
-            list.add(1.6825397f)
-            list.add(1.8095238f)
-            list.add(1.9365079f)
-            list.add(1.4920635f)
-            list.add(1.5396825f)
-            list.add(0.8730159f)
-            list.add(1.2539682f)
-            list.add(1.4285715f)
-            list.add(1.6190476f)
-            list.add(1.4920635f)
-            list.add(0.9047619f)
-            list.add(1.1111112f)
-            list.add(1.3174603f)
-            list.add(1.5238096f)
-            list.add(1.7301587f)
-
-            list.add(0.5873016f)
-            list.add(0.61904764f)
-            list.add(0.6666667f)
-            list.add(0.73015875f)
-            list.add(0.7777778f)
-            list.add(0.8095238f)
-            list.add(0.8095238f)
-            list.add(0.82539684f)
-
-            list.add(0.5714286f)
-            list.add(0.85714287f)
-            list.add(1.3174603f)
-            list.add(1.9523809f)
-            list.add(1.1428572f)
-            list.add(1.2063493f)
-            list.add(1.2698413f)
-            list.add(1.6349206f)
-            list.add(1.2380953f)
-            list.add(1.7936507f)
-            list.add(1.9841269f)
-            list.add(1.1746032f)
-            list.add(1.3492063f)
-            list.add(1.6984127f)
-            list.add(1.8571428f)
-
-            if (pitch in list) {
-                return
-            }
-        }
+//        if (soundName == "note.harp") {
+//            val list = mutableListOf<Float>()
+//            list.add(0.52380955f)
+//            list.add(0.5555556f)
+//            list.add(0.6031746f)
+//            list.add(0.63492066f)
+//            list.add(0.6825397f)
+//            list.add(0.71428573f)
+//            list.add(0.7619048f)
+//            list.add(0.7936508f)
+//            list.add(0.84126985f)
+//            list.add(0.8888889f)
+//            list.add(0.9206349f)
+//            list.add(0.96825397f)
+//            list.add(1.476191f)
+//            list.add(1.476191f)
+//            list.add(0.50793654f)
+//            list.add(0.6507937f)
+//            list.add(0.6984127f)
+//            list.add(0.74603176f)
+//            list.add(0.93650794f)
+//            list.add(0.984127f)
+//            list.add(1.968254f)
+//            list.add(0.4920635f)
+//            list.add(1.1587307f)
+//            list.add(1.1587301f)
+//            list.add(1.2857143f)
+//            list.add(1.4126984f)
+//            list.add(1.6825397f)
+//            list.add(1.8095238f)
+//            list.add(1.9365079f)
+//            list.add(1.4920635f)
+//            list.add(1.5396825f)
+//            list.add(0.8730159f)
+//            list.add(1.2539682f)
+//            list.add(1.4285715f)
+//            list.add(1.6190476f)
+//            list.add(1.4920635f)
+//            list.add(0.9047619f)
+//            list.add(1.1111112f)
+//            list.add(1.3174603f)
+//            list.add(1.5238096f)
+//            list.add(1.7301587f)
+//
+//            list.add(0.5873016f)
+//            list.add(0.61904764f)
+//            list.add(0.6666667f)
+//            list.add(0.73015875f)
+//            list.add(0.7777778f)
+//            list.add(0.8095238f)
+//            list.add(0.8095238f)
+//            list.add(0.82539684f)
+//
+//            list.add(0.5714286f)
+//            list.add(0.85714287f)
+//            list.add(1.3174603f)
+//            list.add(1.9523809f)
+//            list.add(1.1428572f)
+//            list.add(1.2063493f)
+//            list.add(1.2698413f)
+//            list.add(1.6349206f)
+//            list.add(1.2380953f)
+//            list.add(1.7936507f)
+//            list.add(1.9841269f)
+//            list.add(1.1746032f)
+//            list.add(1.3492063f)
+//            list.add(1.6984127f)
+//            list.add(1.8571428f)
+//
+//            if (pitch in list) {
+//                return
+//            }
+//        }
 
         //use ancestral spade
-        if (soundName == "mob.zombie.infect") {
-            if (pitch == 1.968254f) {
-                if (volume == 0.3f) {
-                    LorenzUtils.chat("used ancestral spade!")
-                    return
-                }
-            }
-        }
+//        if (soundName == "mob.zombie.infect") {
+//            if (pitch == 1.968254f) {
+//                if (volume == 0.3f) {
+//                    LorenzUtils.chat("used ancestral spade!")
+//                    return
+//                }
+//            }
+//        }
 
         //wither shield activated
-        if (soundName == "mob.zombie.remedy") {
-            if (pitch == 0.6984127f) {
-                if (volume == 1f) {
-                    return
-                }
-            }
-        }
+//        if (soundName == "mob.zombie.remedy") {
+//            if (pitch == 0.6984127f) {
+//                if (volume == 1f) {
+//                    return
+//                }
+//            }
+//        }
 
         //wither shield cooldown over
-        if (soundName == "random.levelup") {
-            if (pitch == 3f) {
-                if (volume == 1f) {
-                    return
-                }
-            }
-        }
+//        if (soundName == "random.levelup") {
+//            if (pitch == 3f) {
+//                if (volume == 1f) {
+//                    return
+//                }
+//            }
+//        }
 
         //teleport (hyp or aote)
-        if (soundName == "mob.endermen.portal") {
-            if (pitch == 1f && volume == 1f) {
-                return
-            }
-        }
+//        if (soundName == "mob.endermen.portal") {
+//            if (pitch == 1f && volume == 1f) {
+//                return
+//            }
+//        }
 
         //hyp wither impact
-        if (soundName == "random.explode") {
-            if (pitch == 1f && volume == 1f) {
-                return
-            }
-        }
+//        if (soundName == "random.explode") {
+//            if (pitch == 1f && volume == 1f) {
+//                return
+//            }
+//        }
 
         //pick coins up
-        if (soundName == "random.orb") {
-            if (pitch == 1.4920635f && volume == 1f) {
-                return
-            }
-        }
-
+//        if (soundName == "random.orb") {
+//            if (pitch == 1.4920635f && volume == 1f) {
+//                return
+//            }
+//        }
 
 
         if (soundName == "game.player.hurt") return
