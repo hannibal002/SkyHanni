55d444d2e9105a15fceb99b553c3754f2e247716
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2022-09-24 19:47:28 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/sign/SkyBlockEventHandler.java
+++
@@ -1,8 +0,0 @@
-package at.hannibal2.skyhanni.sign;
-
-import at.hannibal2.skyhanni.utils.LorenzUtils;
-
-public class SkyBlockEventHandler {
-
-    public static boolean isSkyBlock = LorenzUtils.INSTANCE.getInSkyblock();
-}
