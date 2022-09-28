7b65b874ece9c4379293f208ba1173ace7c4a3ac
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2022-09-28 16:15:58 +0200

    used kotlin for the mixin classes, deleted some hooks

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinRendererLivingEntity.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinRendererLivingEntity.kt
@@ -1,35 +1,44 @@
-package at.hannibal2.skyhanni.mixins.transformers.renderer;
+package at.hannibal2.skyhanni.mixins.transformers.renderer
 
-import at.hannibal2.skyhanni.events.RenderMobColoredEvent;
-import at.hannibal2.skyhanni.events.ResetEntityHurtEvent;
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.entity.RendererLivingEntity;
-import net.minecraft.entity.EntityLivingBase;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import at.hannibal2.skyhanni.events.RenderMobColoredEvent
+import at.hannibal2.skyhanni.events.ResetEntityHurtEvent
+import net.minecraft.client.renderer.entity.Render
+import net.minecraft.client.renderer.entity.RenderManager
+import net.minecraft.client.renderer.entity.RendererLivingEntity
+import net.minecraft.entity.EntityLivingBase
+import org.objectweb.asm.Opcodes
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.Redirect
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 
-@Mixin(RendererLivingEntity.class)
-public abstract class MixinRendererLivingEntity<T extends EntityLivingBase> extends Render<T> {
-    protected MixinRendererLivingEntity(RenderManager renderManager) {
-        super(renderManager);
+@Mixin(RendererLivingEntity::class)
+abstract class MixinRendererLivingEntity<T : EntityLivingBase?> protected constructor(renderManager: RenderManager?) :
+    Render<T>(renderManager) {
+    @Inject(method = ["getColorMultiplier"], at = [At("HEAD")], cancellable = true)
+    private fun setColorMultiplier(
+        entity: T,
+        lightBrightness: Float,
+        partialTickTime: Float,
+        cir: CallbackInfoReturnable<Int>
+    ) {
+        val event = RenderMobColoredEvent(entity as EntityLivingBase, 0)
+        event.postAndCatch()
+        cir.returnValue = event.color
     }
 
-    @Inject(method = "getColorMultiplier", at = @At("HEAD"), cancellable = true)
-    private void setColorMultiplier(T entity, float lightBrightness, float partialTickTime, CallbackInfoReturnable<Integer> cir) {
-        RenderMobColoredEvent event = new RenderMobColoredEvent(entity, 0);
-        event.postAndCatch();
-        cir.setReturnValue(event.getColor());
-    }
-
-    @Redirect(method = "setBrightness", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/EntityLivingBase;hurtTime:I", opcode = Opcodes.GETFIELD))
-    private int changeHurtTime(EntityLivingBase entity) {
-        ResetEntityHurtEvent event = new ResetEntityHurtEvent(entity, false);
-        event.postAndCatch();
-        return event.getShouldReset() ? 0 : entity.hurtTime;
+    @Redirect(
+        method = ["setBrightness"],
+        at = At(
+            value = "FIELD",
+            target = "Lnet/minecraft/entity/EntityLivingBase;hurtTime:I",
+            opcode = Opcodes.GETFIELD
+        )
+    )
+    private fun changeHurtTime(entity: EntityLivingBase): Int {
+        val event = ResetEntityHurtEvent(entity, false)
+        event.postAndCatch()
+        return if (event.shouldReset) 0 else entity.hurtTime
     }
 }
