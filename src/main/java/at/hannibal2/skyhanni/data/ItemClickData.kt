52c2633a9468475cd021fd94a81ce0624f59fff7
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-01-28 14:39:42 +0100

    Created EntityClickEvent.

--- src/main/java/at/hannibal2/skyhanni/data/ItemClickData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/ItemClickData.kt
@@ -1,6 +1,8 @@
 package at.hannibal2.skyhanni.data
 
+import at.hannibal2.skyhanni.events.EntityClickEvent
 import at.hannibal2.skyhanni.events.ItemClickInHandEvent
+import at.hannibal2.skyhanni.utils.LorenzUtils
 import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.InputEvent
@@ -13,12 +15,30 @@ class ItemClickData {
         if (!Mouse.getEventButtonState()) return
 
         val clickType = when (Mouse.getEventButton()) {
-            0 -> ItemClickInHandEvent.ClickType.LEFT_CLICK
-            1 -> ItemClickInHandEvent.ClickType.RIGHT_CLICK
+            0 -> ClickType.LEFT_CLICK
+            1 -> ClickType.RIGHT_CLICK
             else -> return
         }
 
         val itemStack = Minecraft.getMinecraft().thePlayer.heldItem
         ItemClickInHandEvent(clickType, itemStack).postAndCatch()
+    }
+
+    @SubscribeEvent
+    fun onEntityClick(event: InputEvent.MouseInputEvent) {
+        if (!LorenzUtils.inSkyBlock) return
+
+        val minecraft = Minecraft.getMinecraft()
+        val clickedEntity = minecraft.pointedEntity
+        if (minecraft.thePlayer == null) return
+        if (clickedEntity == null) return
+
+        val clickType = when (Mouse.getEventButton()) {
+            0 -> ClickType.LEFT_CLICK
+            1 -> ClickType.RIGHT_CLICK
+            else -> return
+        }
+
+        EntityClickEvent(clickType, clickedEntity).postAndCatch()
     }
 }
