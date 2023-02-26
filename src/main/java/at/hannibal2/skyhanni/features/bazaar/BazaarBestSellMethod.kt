c7078f95e792ed191028333989c178444551757b
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-02-26 17:47:10 +0100

    Code cleanup.

--- src/main/java/at/hannibal2/skyhanni/features/bazaar/BazaarBestSellMethod.kt
+++ src/main/java/at/hannibal2/skyhanni/features/bazaar/BazaarBestSellMethod.kt
@@ -19,7 +19,7 @@ class BazaarBestSellMethod {
     }
 
     @SubscribeEvent
-    fun onBackgroundDrawn(event: InventoryCloseEvent) {
+    fun onInventoryClose(event: InventoryCloseEvent) {
         display = ""
     }
 
