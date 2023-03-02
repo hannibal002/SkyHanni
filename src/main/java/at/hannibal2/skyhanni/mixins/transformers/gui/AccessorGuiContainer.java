e1c45045e8ae9ec0afd2c14515a13f9332094018
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-03-02 22:54:24 +0100

    Removed old, unused methods.

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/gui/AccessorGuiContainer.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/gui/AccessorGuiContainer.java
@@ -1,33 +1,16 @@
 package at.hannibal2.skyhanni.mixins.transformers.gui;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(GuiContainer.class)
 public interface AccessorGuiContainer {
-
-    @Invoker("getSlotAtPosition")
-    Slot doGetSlotAtPosition(int x, int y);
-
-    @Invoker("drawSlot")
-    void doDrawSlot(Slot slot);
-
-    @Invoker("isMouseOverSlot")
-    boolean doIsMouseOverSlot(Slot slot, int x, int y);
 
     @Accessor("guiLeft")
     int getGuiLeft();
 
     @Accessor("guiTop")
     int getGuiTop();
-
-    @Accessor("xSize")
-    int getXSize();
-
-    @Accessor("ySize")
-    int getYSize();
 
 }
--- src/main/java/at/hannibal2/skyhanni/sign/IEditSign.java
+++
@@ -1,7 +0,0 @@
-package at.hannibal2.skyhanni.sign;
-
-public interface IEditSign {
-    TextInputUtil getTextInputUtil();
-
-    SignSelectionList getSignSelectionList();
-}
