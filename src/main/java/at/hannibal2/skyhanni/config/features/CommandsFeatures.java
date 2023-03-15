3d147fd473601c386fbdcf6a36baa1999b8b0885
NetheriteMiner <88792142+NetheriteMiner@users.noreply.github.com>
2023-03-15 12:15:11 -0400

    Added link to Hypixel wiki, period at the end

--- src/main/java/at/hannibal2/skyhanni/config/features/CommandsFeatures.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/CommandsFeatures.java
@@ -7,7 +7,7 @@ import com.google.gson.annotations.Expose;
 public class CommandsFeatures {
 
     @Expose
-    @ConfigOption(name = "Fandom Wiki", desc = "Using §ehypixel-skyblock.fandom.com §7instead of Hypixel wiki")
+    @ConfigOption(name = "Fandom Wiki", desc = "Using §ehypixel-skyblock.fandom.com §7instead of Hypixel wiki (§ewiki.hypixel.net§7).")
     @ConfigEditorBoolean
     public boolean useFandomWiki = false;
 }
