a1e8604ae3974e076d902b52581a041424816933
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-03-16 12:00:59 +0100

    Added additional debug message

--- src/main/java/at/hannibal2/skyhanni/features/bazaar/BazaarOrderHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/bazaar/BazaarOrderHelper.kt
@@ -49,6 +49,7 @@ class BazaarOrderHelper {
             val bazaarName = BazaarApi.getCleanBazaarName(rawName)
             val data = BazaarApi.getBazaarDataForName(bazaarName)
             if (data == null) {
+                LorenzUtils.debug("Bazaar data is null!")
                 println("Bazaar data is null for '$rawName'/'$bazaarName'")
                 continue
             }
