4e62243f86832438c6f605dde65dc1d149985a84
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-04-10 20:54:07 +0200

    Hide own player name or hypixel watchdog player names from visitor detection

--- src/main/java/at/hannibal2/skyhanni/features/garden/GardenVisitorFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/GardenVisitorFeatures.kt
@@ -342,6 +342,13 @@ class GardenVisitorFeatures {
                 if (!name.contains("§")) {
                     name = "§f$name"
                 }
+
+                // Hide hypixel watchdog entries
+                if (name.contains("§c") && !name.contains("Spaceman")) continue
+
+                //hide own player name
+                if (name.contains(LorenzUtils.getPlayerUuid())) continue
+
                 visitorsInTab.add(name)
             }
         }
