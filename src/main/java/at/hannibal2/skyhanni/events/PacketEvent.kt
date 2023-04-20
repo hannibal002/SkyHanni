f3b199cf8abd93e6b8b761af8e7b28bed5fe7592
Roman / Linnea Gräf <nea@nea.moe>
2023-04-20 21:59:19 +0200

    Mark packet event as async and fix threading issue in garden visitor … (#51)

--- src/main/java/at/hannibal2/skyhanni/events/PacketEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/PacketEvent.kt
@@ -4,19 +4,25 @@ import net.minecraft.network.Packet
 import net.minecraftforge.fml.common.eventhandler.Cancelable
 
 @Cancelable
-open class PacketEvent(val packet: Packet<*>) : LorenzEvent() {
-    var direction: Direction? = null
+/**
+ * Note: This event is async and may not be executed on the main minecraft thread.
+ */
+abstract class PacketEvent : LorenzEvent() {
+    abstract val direction: Direction
+    abstract val packet: Packet<*>
 
-    class ReceiveEvent(packet: Packet<*>) : PacketEvent(packet) {
-        init {
-            direction = Direction.INBOUND
-        }
+    /**
+     * Note: This event is async and may not be executed on the main minecraft thread.
+     */
+    data class ReceiveEvent(override val packet: Packet<*>) : PacketEvent() {
+        override val direction = Direction.INBOUND
     }
 
-    class SendEvent(packet: Packet<*>) : PacketEvent(packet) {
-        init {
-            direction = Direction.OUTBOUND
-        }
+    /**
+     * Note: This event is async and may not be executed on the main minecraft thread.
+     */
+    data class SendEvent(override val packet: Packet<*>) : PacketEvent() {
+        override val direction = Direction.OUTBOUND
     }
 
     enum class Direction {
