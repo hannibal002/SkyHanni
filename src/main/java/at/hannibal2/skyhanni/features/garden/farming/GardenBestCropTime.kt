a2d4d0ea7144d3ca0c01988e69b24d3ce491d12c
thesefer <20844000+thesefer@users.noreply.github.com>
2023-04-26 15:45:25 +0200

    Update src/main/java/at/hannibal2/skyhanni/features/garden/farming/GardenBestCropTime.kt
    Co-authored-by: Roman / Linnea Gräf <roman.graef@gmail.com>

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/GardenBestCropTime.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/GardenBestCropTime.kt
@@ -62,7 +62,7 @@ class GardenBestCropTime {
             val duration = TimeUtils.formatDuration(millis, maxUnits = 2)
             val isCurrent = crop == currentCrop
             number++
-            if (number > config.cropMilestoneShowOnlyBest && (!config.cropMilestoneShowCurrent && isCurrent || !isCurrent)) continue
+            if (number > config.cropMilestoneShowOnlyBest && (!config.cropMilestoneShowCurrent || !isCurrent)) continue
 
             val list = mutableListOf<Any>()
             if (!config.cropMilestoneBestCompact) {
