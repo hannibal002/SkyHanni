361ac65d7c3559884af9c400560218fed10b09f6
Roman / Linnea Gräf <nea@nea.moe>
2023-04-27 12:41:20 +0200

    The mixinification of custom keybinds (#62)

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/AccessorKeyBinding.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/AccessorKeyBinding.java
@@ -2,11 +2,18 @@ package at.hannibal2.skyhanni.mixins.transformers;
 
 import net.minecraft.client.settings.KeyBinding;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(KeyBinding.class)
 public interface AccessorKeyBinding {
 
+    @Accessor("pressTime")
+    int getPressTime_skyhanni();
+
+    @Accessor("pressTime")
+    void setPressTime_skyhanni(int pressTime);
+
     @Invoker("unpressKey")
-     void skyhanni_unpressKey();
+    void skyhanni_unpressKey();
 }
