56012e2ed8ab72805799ebbfe0f57c93a67d9ceb
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-05-10 12:32:04 +0200

    Fixed bug that chat peek hotkey works while inside skyhanni config

--- src/main/java/at/hannibal2/skyhanni/features/misc/ChatPeek.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/ChatPeek.kt
@@ -2,6 +2,7 @@ package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.utils.NEUItems
+import io.github.moulberry.moulconfig.gui.GuiScreenElementWrapper
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.inventory.GuiEditSign
 import org.lwjgl.input.Keyboard
@@ -15,6 +16,7 @@ object ChatPeek {
         if (Minecraft.getMinecraft().thePlayer == null) return false
         if (key <= Keyboard.KEY_NONE) return false
         if (Minecraft.getMinecraft().currentScreen is GuiEditSign) return false
+        if (Minecraft.getMinecraft().currentScreen is GuiScreenElementWrapper) return false
 
         if (NEUItems.neuHasFocus()) return false
 
