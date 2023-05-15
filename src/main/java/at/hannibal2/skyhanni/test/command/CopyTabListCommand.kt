235c0ea5596d0f566db453a5fea38d8f23112227
CalMWolfs <cwolfson58@gmail.com>
2023-05-15 19:35:38 +1000

    CopyTablist additons

--- src/main/java/at/hannibal2/skyhanni/test/command/CopyTabListCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/test/command/CopyTabListCommand.kt
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.OSUtils
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.TabListData
+import net.minecraft.client.Minecraft
 
 object CopyTabListCommand {
     fun command(args: Array<String>) {
@@ -14,7 +15,9 @@ object CopyTabListCommand {
                 val tabListLine = if (noColor) line.removeColor() else line
                 if (tabListLine != "") resultList.add("'$tabListLine'")
             }
-            val string = resultList.joinToString("\n")
+            val tabHeader = Minecraft.getMinecraft().ingameGUI.tabList.header.formattedText
+            val tabFooter = Minecraft.getMinecraft().ingameGUI.tabList.footer.formattedText
+            val string = "Header:\n\n" + tabHeader.toString() + "\n\nBody:\n\n" + resultList.joinToString("\n") + "\nFooter:\n\n" + tabFooter.toString()
             OSUtils.copyToClipboard(string)
             LorenzUtils.chat("§e[SkyHanni] tablist copied into the clipboard!")
         }
