b499cb1398838b691095b05393e3a53ee103a576
CalMWolfs <cwolfson58@gmail.com>
2023-05-18 21:08:35 +1000

    Saving before merge

--- src/main/java/at/hannibal2/skyhanni/features/garden/fortuneguide/CaptureFarmingGear.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/fortuneguide/CaptureFarmingGear.kt
@@ -1,19 +1,17 @@
 package at.hannibal2.skyhanni.features.garden.fortuneguide
 
+import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.GardenToolChangeEvent
 import at.hannibal2.skyhanni.features.garden.CropType
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
+import at.hannibal2.skyhanni.utils.OSUtils
 import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class CaptureFarmingGear {
-
-
-
     @SubscribeEvent
     fun onGardenToolChange(event: GardenToolChangeEvent) {
-        //update tools here
         // it runs when you switch to a non tool as well so be careful
         val resultList = mutableListOf<String>()
 
@@ -22,14 +20,35 @@ class CaptureFarmingGear {
         resultList.add(itemStack.displayName.toString())
         resultList.add(itemID)
 
+        // TODO work with compact tool tips for farming gear
+        CropPageMath.texttt.clear()
         for (line in itemStack.getLore()) {
-            resultList.add("'$line'")
+            CropPageMath.texttt.add(line)
         }
+
+        val string = resultList.joinToString("\n")
+        OSUtils.copyToClipboard(string)
+
         val currentCrop = CropType.values().firstOrNull { itemID.startsWith(it.toolName) }
-            ?:
-            // idk if this elvis operator will work
-            //test to see if its a universal tool, eg advanced, basic or rookie
-            return
+
+        if (currentCrop == null) {
+
+        } else {
+            val a = currentCrop.ordinal
+
+        }
+
+
+        println(currentCrop)
+
+//            ?:
+//            // idk if this elvis operator will work
+//            //test to see if its a universal tool, eg advanced, basic or rookie
+//            return
+
+
+
+
 
 
 
