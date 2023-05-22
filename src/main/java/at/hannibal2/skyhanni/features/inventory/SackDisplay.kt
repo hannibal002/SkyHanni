313d243c1357405262357d2e907e8e2b72bbfdcd
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-05-22 21:40:59 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/features/inventory/SackDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/SackDisplay.kt
@@ -36,30 +36,30 @@ class SackDisplay {
     private val gemstoneItem = mutableMapOf<String, Gemstone>()
     private val sackPattern = "^(.* Sack|Enchanted .* Sack)$".toPattern()
     private val gemstoneMap = mapOf(
-            "Jade Gemstones" to "ROUGH_JADE_GEM",
-            "Amber Gemstones" to "ROUGH_AMBER_GEM",
-            "Topaz Gemstones" to "ROUGH_TOPAZ_GEM",
-            "Sapphire Gemstones" to "ROUGH_SAPPHIRE_GEM",
-            "Amethyst Gemstones" to "ROUGH_AMETHYST_GEM",
-            "Jasper Gemstones" to "ROUGH_JASPER_GEM",
-            "Ruby Gemstones" to "ROUGH_RUBY_GEM",
-            "Opal Gemstones" to "ROUGH_OPAL_GEM"
+        "Jade Gemstones" to "ROUGH_JADE_GEM",
+        "Amber Gemstones" to "ROUGH_AMBER_GEM",
+        "Topaz Gemstones" to "ROUGH_TOPAZ_GEM",
+        "Sapphire Gemstones" to "ROUGH_SAPPHIRE_GEM",
+        "Amethyst Gemstones" to "ROUGH_AMETHYST_GEM",
+        "Jasper Gemstones" to "ROUGH_JASPER_GEM",
+        "Ruby Gemstones" to "ROUGH_RUBY_GEM",
+        "Opal Gemstones" to "ROUGH_OPAL_GEM"
     )
 
     private val numPattern =
-            "(?:(?:§[0-9a-f](?<level>I{1,3})§7:)?|(?:§7Stored:)?) (?<color>§[0-9a-f])(?<stored>\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?)§7/(?<total>\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?)".toPattern()
+        "(?:(?:§[0-9a-f](?<level>I{1,3})§7:)?|(?:§7Stored:)?) (?<color>§[0-9a-f])(?<stored>\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?)§7/(?<total>\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?)".toPattern()
     private val gemstonePattern =
-            " (?:§[0-9a-f])(?<gemrarity>[A-z]*): §[0-9a-f](?<stored>\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?) §[0-9a-f]\\((?:\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?)\\)".toPattern()
+        " (?:§[0-9a-f])(?<gemrarity>[A-z]*): §[0-9a-f](?<stored>\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?) §[0-9a-f]\\((?:\\d+(?:\\.\\d+)?(?:,\\d+)?[kKmM]?)\\)".toPattern()
 
 
     @SubscribeEvent
     fun onBackgroundDraw(event: GuiRenderEvent.ChestBackgroundRenderEvent) {
         if (inInventory) {
             config.position.renderStringsAndItems(
-                    display,
-                    extraSpace = config.extraSpace,
-                    itemScale = 1.3,
-                    posLabel = "Sacks Items"
+                display,
+                extraSpace = config.extraSpace,
+                itemScale = 1.3,
+                posLabel = "Sacks Items"
             )
         }
     }
@@ -68,11 +68,9 @@ class SackDisplay {
         display = drawDisplay()
     }
 
-
     private fun update() {
         updateDisplay()
     }
-
 
     private fun drawDisplay(): List<List<Any>> {
         val newDisplay = mutableListOf<List<Any>>()
@@ -87,7 +85,7 @@ class SackDisplay {
                 else -> sackItem.toList().sortedByDescending { it.second.stored.formatNumber() }.toMap().toMutableMap()
             }
 
-            sortedPairs.toList().forEach() {
+            sortedPairs.toList().forEach {
                 if (it.second.stored == "0" && !config.showEmpty) {
                     sortedPairs.remove(it.first)
                 }
@@ -120,58 +118,40 @@ class SackDisplay {
                 if (colorCode == "§a") // §a = Full, §e = Not full, §7 = Empty
                     list.add(" §c§l(Full!)")
 
-                val format: String = when (config.priceFormat) {
-                    0 -> NumberUtil.format(price)
-                    1 -> price.addSeparators()
-                    else -> ""
-                }
-                if (config.showPrice && format.isNotEmpty() && format != "0")
-                    list.add(" §7(§6$format§7)")
+                if (config.showPrice && price != 0)
+                    list.add(" §7(§6${format(price)}§7)")
 
                 totalPrice += price
                 rendered++
                 newDisplay.add(list)
             }
-            val finalPrice: String = when (config.priceFormat) {
-                0 -> NumberUtil.format(totalPrice)
-                1 -> totalPrice.addSeparators()
-                else -> ""
-            }
 
-            val sortList = mutableListOf<Any>()
-
-            val storedDesc = Renderable.optionalLink("§7[§bDESC§7] ", {
-                config.sortingType = 0
-                update()
-            }) { !NEUItems.neuHasFocus() }
-
-            val storedAsc = Renderable.optionalLink("§7[§bASC§7] ", {
-                config.sortingType = 1
-                update()
-            }) { !NEUItems.neuHasFocus() }
-
-            sortList.add("§eSort by: §6Stored ")
-            sortList.add(storedDesc)
-            sortList.add(storedAsc)
-
-            if (config.showPrice) {
-                val priceDesc = Renderable.optionalLink("§7[§bDESC§7] ", {
-                    config.sortingType = 2
+            newDisplay.add(buildList {
+                add("§eSort by: §6Stored ")
+                add(Renderable.optionalLink("§7[§bDESC§7] ", {
+                    config.sortingType = 0
                     update()
-                }) { !NEUItems.neuHasFocus() }
-
-                val priceAsc = Renderable.optionalLink("§7[§bASC§7]", {
-                    config.sortingType = 3
+                }) { !NEUItems.neuHasFocus() })
+                add(Renderable.optionalLink("§7[§bASC§7] ", {
+                    config.sortingType = 1
                     update()
-                }) { !NEUItems.neuHasFocus() }
-                sortList.add("§6Price ")
-                sortList.add(priceDesc)
-                sortList.add(priceAsc)
-            }
-            newDisplay.add(sortList)
+                }) { !NEUItems.neuHasFocus() })
 
-            if (config.showPrice && finalPrice.isNotEmpty())
-                newDisplay.addAsSingletonList("§eTotal price: §6$finalPrice")
+                if (config.showPrice) {
+                    add("§6Price ")
+                    add(Renderable.optionalLink("§7[§bDESC§7] ", {
+                        config.sortingType = 2
+                        update()
+                    }) { !NEUItems.neuHasFocus() })
+                    add(Renderable.optionalLink("§7[§bASC§7]", {
+                        config.sortingType = 3
+                        update()
+                    }) { !NEUItems.neuHasFocus() })
+                }
+            })
+
+            if (config.showPrice)
+                newDisplay.addAsSingletonList("§eTotal price: §6${format(totalPrice)}")
         }
 
         if (runeItem.isNotEmpty()) {
@@ -189,40 +169,30 @@ class SackDisplay {
 
         if (gemstoneItem.isNotEmpty()) {
             newDisplay.addAsSingletonList("§7Gemstones:")
-            for (gemstone in gemstoneItem) {
-                val list = mutableListOf<Any>()
-                val (name, gem) = gemstone
+            for ((name, gem) in gemstoneItem) {
                 val (internalName, rough, flawed, fine, flawless, roughprice, flawedprice, fineprice, flawlessprice) = gem
-
-                list.add(" §7- ")
-                list.add(NEUItems.getItemStack(internalName))
-                list.add(Renderable.optionalLink("$name: ", {
-                    if (!NEUItems.neuHasFocus() && !LorenzUtils.noTradeMode) {
-                        LorenzUtils.sendCommandToServer("bz ${name.removeColor().dropLast(1)}")
-                    }
-                }) { !NEUItems.neuHasFocus() })
-                list.add(" ($rough-§a$flawed-§9$fine-§5$flawless)")
-                val price = (roughprice + flawedprice + fineprice + flawlessprice)
-                val finalPrice: String = when (config.priceFormat) {
-                    0 -> NumberUtil.format(price)
-                    1 -> price.addSeparators()
-                    else -> ""
-                }
-                if (config.showPrice && finalPrice.isNotEmpty() && finalPrice != "0")
-                    list.add(" §7(§6$finalPrice§7)")
-                newDisplay.add(list)
-                totalPrice += price
+                newDisplay.add(buildList {
+                    add(" §7- ")
+                    add(NEUItems.getItemStack(internalName))
+                    add(Renderable.optionalLink("$name: ", {
+                        if (!NEUItems.neuHasFocus() && !LorenzUtils.noTradeMode) {
+                            LorenzUtils.sendCommandToServer("bz ${name.removeColor().dropLast(1)}")
+                        }
+                    }) { !NEUItems.neuHasFocus() })
+                    add(" ($rough-§a$flawed-§9$fine-§5$flawless)")
+                    val price = (roughprice + flawedprice + fineprice + flawlessprice)
+                    totalPrice += price
+                    if (config.showPrice && price != 0)
+                        add(" §7(§6${format(price)}§7)")
+                })
             }
-            val finalPrice: String = when (config.priceFormat) {
-                0 -> NumberUtil.format(totalPrice)
-                1 -> totalPrice.addSeparators()
-                else -> ""
-            }
-            if (config.showPrice && finalPrice.isNotEmpty())
-                newDisplay.addAsSingletonList("§eTotal price: §6$finalPrice")
+            if (config.showPrice)
+                newDisplay.addAsSingletonList("§eTotal price: §6${format(totalPrice)}")
         }
         return newDisplay
     }
+
+    private fun format(price: Int) = if (config.priceFormat == 0) NumberUtil.format(price) else price.addSeparators()
 
     @SubscribeEvent
     fun onInventoryClose(event: InventoryCloseEvent) {
@@ -258,7 +228,8 @@ class SackDisplay {
                         val stored = group("stored")
                         gem.internalName = gemstoneMap[name.removeColor()].toString()
                         if (gemstoneMap.containsKey(name.removeColor())) {
-                            val internalName = "${rarity.uppercase()}_${name.uppercase().split(" ")[0].removeColor()}_GEM"
+                            val internalName =
+                                "${rarity.uppercase()}_${name.uppercase().split(" ")[0].removeColor()}_GEM"
 
                             when (rarity) {
                                 "Rough" -> {
@@ -339,51 +310,45 @@ class SackDisplay {
 
 
     data class Gemstone(
-            var internalName: String = "",
-            var rough: String = "0",
-            var flawed: String = "0",
-            var fine: String = "0",
-            var flawless: String = "0",
-            var roughPrice: Int = 0,
-            var flawedPrice: Int = 0,
-            var finePrice: Int = 0,
-            var flawlessPrice: Int = 0
+        var internalName: String = "",
+        var rough: String = "0",
+        var flawed: String = "0",
+        var fine: String = "0",
+        var flawless: String = "0",
+        var roughPrice: Int = 0,
+        var flawedPrice: Int = 0,
+        var finePrice: Int = 0,
+        var flawlessPrice: Int = 0,
     )
 
     data class Rune(
-            var stack: ItemStack? = null,
-            var lvl1: String = "0",
-            var lvl2: String = "0",
-            var lvl3: String = "0"
+        var stack: ItemStack? = null,
+        var lvl1: String = "0",
+        var lvl2: String = "0",
+        var lvl3: String = "0",
     )
 
     data class Item(
-            var internalName: String = "",
-            var colorCode: String = "",
-            var stored: String = "0",
-            var total: String = "0",
-            var price: Int = 0
+        var internalName: String = "",
+        var colorCode: String = "",
+        var stored: String = "0",
+        var total: String = "0",
+        var price: Int = 0,
     )
 
     private fun isEnabled() = LorenzUtils.inSkyBlock && config.enabled
     private fun isRuneDisplayEnabled() = config.showRunes
 
-    private fun calculatePrice(internalName: String, stored: String): Int {
-        return when (config.priceFrom) {
-            0 -> {
-                (NEUItems.getPrice(internalName) * stored.formatNumber()).toInt()
-            }
+    private fun calculatePrice(internalName: String, stored: String) = when (config.priceFrom) {
+        0 -> (NEUItems.getPrice(internalName) * stored.formatNumber()).toInt()
 
-            1 -> {
-                try {
-                    val bazaarData = BazaarApi.getBazaarDataByInternalName(internalName)
-                    (((bazaarData?.npcPrice?.toInt() ?: 0) * stored.formatNumber())).toInt()
-                } catch (e: Exception) {
-                    0
-                }
-            }
-
-            else -> 0
+        1 -> try {
+            val npcPrice = BazaarApi.getBazaarDataByInternalName(internalName)?.npcPrice ?: 0.0
+            (npcPrice * stored.formatNumber()).toInt()
+        } catch (e: Exception) {
+            0
         }
+
+        else -> 0
     }
 }
