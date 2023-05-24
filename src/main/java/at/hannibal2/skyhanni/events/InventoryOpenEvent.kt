1824a3d9172e6f6e350b2d2d98c8faf5519566e5
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-05-25 01:40:35 +0200

    random name change

--- src/main/java/at/hannibal2/skyhanni/events/InventoryOpenEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/InventoryOpenEvent.kt
@@ -3,14 +3,14 @@ package at.hannibal2.skyhanni.events
 import at.hannibal2.skyhanni.data.OtherInventoryData
 import net.minecraft.item.ItemStack
 
-open class BaseInventoryOpenEvent(inventory: OtherInventoryData.Inventory): LorenzEvent() {
+open class InventoryEvent(inventory: OtherInventoryData.Inventory): LorenzEvent() {
     val inventoryId: Int by lazy { inventory.windowId }
     val inventoryName: String by lazy {inventory.title }
     val inventorySize: Int by lazy {inventory.slotCount }
     val inventoryItems: Map<Int, ItemStack>  by lazy {inventory.items }
 }
 
-class InventoryOpenEvent(inventory: OtherInventoryData.Inventory): BaseInventoryOpenEvent(inventory)
+class InventoryOpenEvent(inventory: OtherInventoryData.Inventory): InventoryEvent(inventory)
 
 // Firing with items that are added later
-class LateInventoryOpenEvent(inventory: OtherInventoryData.Inventory): BaseInventoryOpenEvent(inventory)
+class LateInventoryOpenEvent(inventory: OtherInventoryData.Inventory): InventoryEvent(inventory)
