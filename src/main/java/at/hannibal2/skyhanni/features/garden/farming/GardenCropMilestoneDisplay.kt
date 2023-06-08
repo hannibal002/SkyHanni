296e8a52359b623db414a86eaa55c6330533fc4a
superhize <superhize@gmail.com>
2023-06-09 01:03:35 +0200

    fix

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/GardenCropMilestoneDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/GardenCropMilestoneDisplay.kt
@@ -56,7 +56,6 @@ object GardenCropMilestoneDisplay {
 
     @SubscribeEvent
     fun onRenderOverlay(event: GuiRenderEvent.GameOverlayRenderEvent) {
-       // println(config.cropMilestoneText)
         if (!isEnabled()) return
         if (GardenAPI.hideExtraGuis()) return
 
