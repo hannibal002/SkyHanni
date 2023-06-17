b09c4e4a38db21c1a83501b41d77c48d3b08d59b
CalMWolfs <cwolfson58@gmail.com>
2023-06-18 09:28:48 +1000

    small additions and fixes

--- src/main/java/at/hannibal2/skyhanni/features/garden/fortuneguide/FarmingReforges.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/fortuneguide/FarmingReforges.kt
@@ -9,13 +9,16 @@ enum class FarmingReforges(
     val epic: Int,
     val legendary: Int,
     val mythic: Int
-) {
+) { // if reforge item is an empty string it means it will never be called, just for upgrading and recomb stats
     BLESSED("Blessed", "BLESSED_FRUIT", 5, 7, 9, 13, 16, 20),
     BOUNTIFUL("Bountiful", "GOLDEN_BALL", 1, 2, 3, 5, 7, 10),
     BLOOMING("Blooming", "FLOWERING_BOUQUET", 1, 2, 3, 4, 5, 6),
     ROOTED("Rooted", "BURROWING_SPORES", 4, 6, 8, 10, 12, 14),
     BUSTLING("Bustling", "SKYMART_BROCHURE", 1, 2, 4, 6, 8, 10),
-    MOSSY("Mossy", "OVERGROWN_GRASS", 5, 10, 15, 20, 25, 30)
+    MOSSY("Mossy", "OVERGROWN_GRASS", 5, 10, 15, 20, 25, 30),
+    ROBUST("Robust", "", 2, 3, 4, 6, 8, 10),
+    EARTHLY("Earthly", "LARGE_WALNUT", 1, 4, 6, 8, 10, 12),
+    GREEN_THUMB("Green Thumb", "", 1, 2, 3, 4, 5, 6)
 }
 
 operator fun FarmingReforges.get(index: Int, current: Double = 0.0): Double? {
