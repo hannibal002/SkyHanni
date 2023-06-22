ffc4aca7cd50eab822a8607b2b3dcae27be1563c
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-06-22 14:27:19 +0200

    merged

--- src/main/java/at/hannibal2/skyhanni/features/rift/RiftAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/RiftAPI.kt
@@ -4,5 +4,5 @@ import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.utils.LorenzUtils
 
 object RiftAPI {
-    fun inRift() = LorenzUtils.inSkyBlock && LorenzUtils.skyBlockIsland == IslandType.RIFT
+    fun inRift() = LorenzUtils.inIsland(IslandType.THE_RIFT)
 }
