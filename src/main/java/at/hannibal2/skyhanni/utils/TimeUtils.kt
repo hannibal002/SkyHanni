4726b8a54a6f326291de09d8dbfd17db3f2f890d
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-06-24 20:06:57 +0200

    fixed Assertion failed error

--- src/main/java/at/hannibal2/skyhanni/utils/TimeUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/TimeUtils.kt
@@ -19,7 +19,6 @@ object TimeUtils {
         duration.inWholeMilliseconds - 999, biggestUnit, showMilliSeconds, longName, maxUnits
     )
 
-    @Deprecated("Use kotlin.time.Duration instead!")
     fun formatDuration(
         millis: Long,
         biggestUnit: TimeUnit = TimeUnit.YEAR,
