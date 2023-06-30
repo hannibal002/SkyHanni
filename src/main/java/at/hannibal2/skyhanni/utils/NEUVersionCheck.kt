20ba67da25de8f2849d374266f6e826a0e0c6158
CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>
2023-07-01 09:37:52 +1000

    Merge pull request #281
    * neu thing
    
    * forgot things

--- src/main/java/at/hannibal2/skyhanni/utils/NEUVersionCheck.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/NEUVersionCheck.kt
@@ -20,9 +20,10 @@ object NEUVersionCheck {
             neuWarning(
                 "NotEnoughUpdates is missing!\n" +
                         "SkyHanni requires the latest version of NotEnoughUpdates to work.\n" +
-                        "You currently need NEU version 2.1.1-Alpha-16.1 or later.\n" +
+                        "You currently need NEU version 2.1.1-Alpha-17.1 or later.\n" +
                         "NEU 2.1 is NOT the latest version.\n" +
-                        "It is ONLY in the alphas channel in the NEU discord\n" +
+                        "It is ONLY in the #neu-alphas channel in the NEU discord\n" +
+                        "Or in the #neu-updates channel in the SkyHanni discord\n" +
                         "Use these links to download the latest version:"
             )
             return
@@ -39,9 +40,10 @@ object NEUVersionCheck {
         neuWarning(
             "NotEnoughUpdates is outdated!\n" +
                     "SkyHanni requires the latest version of NotEnoughUpdates to work.\n" +
-                    "You currently need NEU version 2.1.1-Alpha-16.1 or later.\n" +
+                    "You currently need NEU version 2.1.1-Alpha-17.1 or later.\n" +
                     "NEU 2.1 is NOT the latest version.\n" +
-                    "It is ONLY in the alphas channel in the NEU discord\n" +
+                    "It is ONLY in the #neu-alphas channel in the NEU discord\n" +
+                    "Or in the #neu-updates channel in the SkyHanni discord\n" +
                     "Use these links to download the latest version:"
         )
     }
