d12f0d3d6935b516066bd750322b61230b194910
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2023-07-05 17:30:23 +0200

    Merge pull request #267
    * added random code cleanups
    
    * Merge branch 'beta' into random_code_cleanup
    
    * ...

--- src/main/java/at/hannibal2/skyhanni/events/ItemClickEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/ItemClickEvent.kt
@@ -1,8 +1,5 @@
 package at.hannibal2.skyhanni.events
 
-import at.hannibal2.skyhanni.data.ClickType
-import at.hannibal2.skyhanni.utils.BlockUtils.getBlockStateAt
-import at.hannibal2.skyhanni.utils.LorenzVec
 import net.minecraft.item.ItemStack
 
 class ItemClickEvent(val itemInHand: ItemStack?) : LorenzEvent()
