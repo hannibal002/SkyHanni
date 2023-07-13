3258d39e846305a7eaf56a627fd686848bc7fed3
ReyMaratov <musterjohn6@gmail.com>
2023-07-13 22:09:39 +0200

    Ive updated the RNG counter init to have the new drops also in it, which got added in the latest dicer buff, to hopefully make the RNG counter for dicer work again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
         maven("https://maven.minecraftforge.net/")
         maven("https://repo.spongepowered.org/maven/")
         maven("https://repo.sk1er.club/repository/maven-releases/")
+        maven("https://maven.notenoughupdates.org/releases")
     }
     resolutionStrategy {
         eachPlugin {
