f4195e90248dbb57901b4384c5749bff4c71a0f6
superhize <superhize@gmail.com>
2023-07-14 23:41:37 +0200

    add npc motes sell value for current opened chest

--- src/main/java/at/hannibal2/skyhanni/features/rift/everywhere/RiftAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/everywhere/RiftAPI.kt
@@ -17,6 +17,22 @@ object RiftAPI {
 
     fun ItemStack.motesNpcPrice() = motesPrice[getInternalName()]
 
+    fun ItemStack.motesNpcPriceBurgers(): Double {
+        val base = motesNpcPrice() ?: 0.0
+        return if (config.motes.burgerStacks == 0) {
+            base
+        } else {
+            val burgerStacks = config.motes.burgerStacks
+            val motesPerItem = base + (burgerStacks * 5) * base / 100
+            if (stackSize > 1) {
+                motesPerItem * stackSize
+            } else {
+                motesPerItem
+            }
+        }
+    }
+
+
     fun inLivingCave() = LorenzUtils.skyBlockArea == "Living Cave"
     fun inLivingStillness() = LorenzUtils.skyBlockArea == "Living Stillness"
 }
