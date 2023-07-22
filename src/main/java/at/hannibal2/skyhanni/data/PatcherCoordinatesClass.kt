2e19488bb98495183b06780810833d2394424f2f
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-07-22 22:23:12 +0200

    using "for iterator" instead of index, using "removeIf" and removed logging for "remove check", no longer checking for player movement, rather checking 2 times a second

--- src/main/java/at/hannibal2/skyhanni/data/PatcherCoordinatesClass.kt
+++ src/main/java/at/hannibal2/skyhanni/data/PatcherCoordinatesClass.kt
@@ -2,4 +2,4 @@ package at.hannibal2.skyhanni.data
 
 import at.hannibal2.skyhanni.utils.LorenzVec
 
-data class PatcherBeacon(val loc: LorenzVec, val name: String, val time: Long)
+data class PatcherBeacon(val location: LorenzVec, val name: String, val time: Long)
--- src/main/java/at/hannibal2/skyhanni/features/misc/PatcherSendCoordinatesHighlight.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/PatcherSendCoordinatesHighlight.kt
@@ -3,8 +3,12 @@ package at.hannibal2.skyhanni.features.misc
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.PatcherBeacon
 import at.hannibal2.skyhanni.events.LorenzChatEvent
+import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.test.GriffinUtils.drawWaypointFilled
-import at.hannibal2.skyhanni.utils.*
+import at.hannibal2.skyhanni.utils.LocationUtils
+import at.hannibal2.skyhanni.utils.LorenzColor
+import at.hannibal2.skyhanni.utils.LorenzLogger
+import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.RenderUtils.drawColor
 import at.hannibal2.skyhanni.utils.RenderUtils.drawString
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
@@ -13,7 +17,6 @@ import net.minecraftforge.client.event.RenderWorldLastEvent
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import net.minecraftforge.fml.common.gameevent.TickEvent
 
 class PatcherSendCoordinatesHighlight {
 
@@ -41,36 +44,24 @@ class PatcherSendCoordinatesHighlight {
     fun onWorldRender(event: RenderWorldLastEvent) {
         if (!SkyHanniMod.feature.misc.patcherSendCoordWaypoint) return
 
-        for (i in 0 until patcherBeacon.size) {
-            val location = patcherBeacon[i].loc
+        for (beacon in patcherBeacon) {
+            val location = beacon.location
             event.drawColor(location, LorenzColor.DARK_GREEN, alpha = 1f)
             event.drawWaypointFilled(location, LorenzColor.GREEN.toColor(), true, true)
-            event.drawString(location.add(0.5, 0.5, 0.5), patcherBeacon[i].name, true, LorenzColor.DARK_BLUE.toColor())
+            event.drawString(location.add(0.5, 0.5, 0.5), beacon.name, true, LorenzColor.DARK_BLUE.toColor())
         }
     }
 
     @SubscribeEvent
-    fun onEnterWaypoint(event: TickEvent.PlayerTickEvent) {
-        val player = event.player
-        if (patcherBeacon.size > 0) {
-            if (player.motionX > 0 || player.motionZ > 0) {
-                val location = player.getLorenzVec()
-                for (i in 0 until patcherBeacon.size) {
-                    logger.log(patcherBeacon[i].time.toString())
-                    if (System.currentTimeMillis() / 1000 > patcherBeacon[i].time + 5) {
-                        if (location.distanceIgnoreY(patcherBeacon[i].loc) < 5) {
-                            patcherBeacon.removeAt(i)
-                            logger.log("removed patcher beacon!")
-                            break;
-                        }
-                    }
-                }
-            }
-            if (System.currentTimeMillis() / 1000 > patcherBeacon[0].time + 60) {
-                patcherBeacon.removeAt(0)
-                logger.log("removed patcher beacon after time!")
-            }
-        }
+    fun onEnterWaypoint(event: LorenzTickEvent) {
+        if (!event.isMod(10)) return
+
+        val location = LocationUtils.playerLocation()
+        // removed patcher beacon!
+        patcherBeacon.removeIf { System.currentTimeMillis() / 1000 > it.time + 5 && location.distanceIgnoreY(it.location) < 5 }
+
+        // removed patcher beacon after time!
+        patcherBeacon.removeIf { System.currentTimeMillis() / 1000 > it.time + 60 }
     }
 
     @SubscribeEvent
