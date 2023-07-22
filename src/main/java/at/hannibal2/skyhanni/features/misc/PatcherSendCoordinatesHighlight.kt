4b19ac75aec483b0c8f1679647e9a41e260f4d54
dragon99z <dragon99@online.de>
2023-07-22 21:12:40 +0200

    +Added a PatcherBeacon class to store the location,name and creation time *chaned the Patcher Waypoint system to not remove the waypoint instantly on creaton for yourself and auto remove the beacons after 60 seconds

--- src/main/java/at/hannibal2/skyhanni/features/misc/PatcherSendCoordinatesHighlight.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/PatcherSendCoordinatesHighlight.kt
@@ -1,16 +1,14 @@
 package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.data.PatcherBeacon
 import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.test.GriffinUtils.drawWaypointFilled
-import at.hannibal2.skyhanni.utils.LorenzColor
-import at.hannibal2.skyhanni.utils.LorenzLogger
-import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.*
 import at.hannibal2.skyhanni.utils.RenderUtils.drawColor
 import at.hannibal2.skyhanni.utils.RenderUtils.drawString
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
-import at.hannibal2.skyhanni.utils.getLorenzVec
 import net.minecraftforge.client.event.RenderWorldLastEvent
 import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.EventPriority
@@ -19,14 +17,14 @@ import net.minecraftforge.fml.common.gameevent.TickEvent
 
 class PatcherSendCoordinatesHighlight {
 
-    private val patcherBeacon = mutableListOf<LorenzVec>()
-    private val patcherName = mutableListOf<String>()
+    private val patcherBeacon = mutableListOf<PatcherBeacon>()
     private val logger = LorenzLogger("misc/patchercoords")
     private val pattern = "(?<playerName>.*): x: (?<x>.*), y: (?<y>.*), z: (?<z>.*)".toPattern()
 
+
     @SubscribeEvent
     fun onPatcherCoordinates(event: LorenzChatEvent) {
-        if (!SkyHanniMod.feature.misc.patcherSendCoordHighlight) return
+        if (!SkyHanniMod.feature.misc.patcherSendCoordWaypoint) return
 
         val message = event.message.removeColor()
         pattern.matchMatcher(message) {
@@ -34,39 +32,43 @@ class PatcherSendCoordinatesHighlight {
             val x = group("x").toInt()
             val y = group("y").toInt()
             val z = group("z").toInt()
-
-            patcherBeacon.add(LorenzVec(x, y, z))
-            patcherName.add(playerName)
+            patcherBeacon.add(PatcherBeacon(LorenzVec(x, y, z),playerName,System.currentTimeMillis()/1000))
             logger.log("got patcher coords and username")
         }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
     fun onWorldRender(event: RenderWorldLastEvent) {
-        if (!SkyHanniMod.feature.misc.patcherSendCoordHighlight) return
+        if (!SkyHanniMod.feature.misc.patcherSendCoordWaypoint) return
 
         for (i in 0 until patcherBeacon.size) {
-            val location = patcherBeacon[i]
+            val location = patcherBeacon[i].loc
             event.drawColor(location, LorenzColor.DARK_GREEN, alpha = 1f)
             event.drawWaypointFilled(location, LorenzColor.GREEN.toColor(), true, true)
-            event.drawString(location.add(0.5, 0.5, 0.5), patcherName[i], true, LorenzColor.DARK_BLUE.toColor())
-            logger.log("added patcher beacon!")
+            event.drawString(location.add(0.5, 0.5, 0.5), patcherBeacon[i].name, true, LorenzColor.DARK_BLUE.toColor())
         }
     }
 
     @SubscribeEvent
     fun onEnterWaypoint(event: TickEvent.PlayerTickEvent) {
-        if (!SkyHanniMod.feature.misc.patcherSendCoordHighlight) return
-
         val player = event.player
-        if (player.motionX > 0 || player.motionZ > 0) {
-            val location = player.getLorenzVec()
-            for (i in 0 until patcherBeacon.size) {
-                if (location.distanceIgnoreY(patcherBeacon[i]) < 5) {
-                    patcherBeacon.removeAt(i)
-                    patcherName.removeAt(i)
-                    logger.log("removed patcher beacon!")
+        if(patcherBeacon.size > 0){
+            if (player.motionX > 0 || player.motionZ > 0) {
+                val location = player.getLorenzVec()
+                for (i in 0 until patcherBeacon.size) {
+                    logger.log(patcherBeacon[i].time.toString())
+                    if(System.currentTimeMillis()/1000 > patcherBeacon[i].time+5){
+                        if (location.distanceIgnoreY(patcherBeacon[i].loc) < 5) {
+                            patcherBeacon.removeAt(i)
+                            logger.log("removed patcher beacon!")
+                            break;
+                        }
+                    }
                 }
+            }
+            if(System.currentTimeMillis()/1000 > patcherBeacon[0].time+60){
+                patcherBeacon.removeAt(0)
+                logger.log("removed patcher beacon after time!")
             }
         }
     }
@@ -74,7 +76,6 @@ class PatcherSendCoordinatesHighlight {
     @SubscribeEvent
     fun onWorldChange(event: WorldEvent.Load) {
         patcherBeacon.clear()
-        patcherName.clear()
         logger.log("Reset everything (world change)")
     }
 }
