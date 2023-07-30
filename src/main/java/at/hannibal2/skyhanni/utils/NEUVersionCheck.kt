7d27d93609cd2b56b55d9bfdaeee5a9ee8413d76
CalMWolfs <cwolfson58@gmail.com>
2023-07-30 12:14:12 +1000

    one more empty line

--- src/main/java/at/hannibal2/skyhanni/utils/NEUVersionCheck.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/NEUVersionCheck.kt
@@ -39,7 +39,7 @@ object NEUVersionCheck {
         }
         neuWarning(
             "NotEnoughUpdates is outdated!\n" +
-                    "You currently need NEU version 2.1.1-Alpha-19 or later.\n" +
+                    "You currently need NEU version 2.1.1-Alpha-19 or later.\n\n" +
                     "NEU 2.1 is NOT the latest version.\n\n" +
                     "NEU 2.1.1 is NOT on the NEU GitHub.\n\n" +
                     "It is ONLY in the #neu-alphas channel in the NEU discord\n" +
