bc5396947eb02c9dfad5cc50b02a39d193bac8ec
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-08-02 17:17:27 +0200

    allowing /warp is command only inb skyblock

--- src/main/java/at/hannibal2/skyhanni/features/commands/WarpIsCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/WarpIsCommand.kt
@@ -10,6 +10,7 @@ class WarpIsCommand {
 
     @SubscribeEvent
     fun onSendPacket(event: PacketEvent.SendEvent) {
+        if (!LorenzUtils.inSkyBlock) return
         if (!SkyHanniMod.feature.commands.replaceWarpIs) return
 
         val packet = event.packet
