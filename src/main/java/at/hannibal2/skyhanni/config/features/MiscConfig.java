c08d07dd0b9763f3b4c845ecdbb171309f565179
superhize <superhize@gmail.com>
2023-08-26 02:01:29 +0200

    better default position

--- src/main/java/at/hannibal2/skyhanni/config/features/MiscConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/MiscConfig.java
@@ -811,7 +811,7 @@ public class MiscConfig {
         public Property<List<Integer>> textFormat = Property.of(new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)));
 
         @Expose
-        public Position position = new Position(100, 100, false, true);
+        public Position position = new Position(-274, 0, false, true);
 
     }
 
