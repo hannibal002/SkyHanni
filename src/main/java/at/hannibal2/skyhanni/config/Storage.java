576ed8a68a254584683f9edd56a8b18b26037b30
CalMWolfs <cwolfson58@gmail.com>
2023-08-29 08:55:39 +1000

    fix variable name

--- src/main/java/at/hannibal2/skyhanni/config/Storage.java
+++ src/main/java/at/hannibal2/skyhanni/config/Storage.java
@@ -229,7 +229,7 @@ public class Storage {
             public long lastComposterEmptyWarningTime = 0;
 
             @Expose
-            public FarmingWeightConfig faramingWeight = new FarmingWeightConfig();
+            public FarmingWeightConfig farmingWeight = new FarmingWeightConfig();
 
             public static class FarmingWeightConfig {
 
