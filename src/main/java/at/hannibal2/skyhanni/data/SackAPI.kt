03d4df759424df53e4ca69d46088ccec82df1392
CalMWolfs <cwolfson58@gmail.com>
2023-08-30 07:38:59 +1000

    fix conflicts fully

--- src/main/java/at/hannibal2/skyhanni/data/SackAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/data/SackAPI.kt
@@ -5,7 +5,6 @@ import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
 import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.events.SackChangeEvent
-import at.hannibal2.skyhanni.features.bazaar.BazaarApi.Companion.getBazaarData
 import at.hannibal2.skyhanni.features.fishing.trophy.TrophyFishManager
 import at.hannibal2.skyhanni.features.fishing.trophy.TrophyRarity
 import at.hannibal2.skyhanni.features.inventory.SackDisplay
@@ -15,6 +14,7 @@ import at.hannibal2.skyhanni.utils.ItemUtils.name
 import at.hannibal2.skyhanni.utils.LorenzUtils.editCopy
 import at.hannibal2.skyhanni.utils.NEUInternalName
 import at.hannibal2.skyhanni.utils.NEUInternalName.Companion.asInternalName
+import at.hannibal2.skyhanni.utils.NEUItems.getNpcPriceOrNull
 import at.hannibal2.skyhanni.utils.NEUItems.getPrice
 import at.hannibal2.skyhanni.utils.NumberUtil.formatNumber
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
@@ -87,7 +87,7 @@ object SackAPI {
         0 -> (getPrice(true) * stored.formatNumber()).toInt().let { if (it < 0) 0 else it }
 
         1 -> try {
-            val npcPrice = getBazaarData()?.npcPrice ?: 0.0
+            val npcPrice = getNpcPriceOrNull() ?: 0.0
             (npcPrice * stored.formatNumber()).toInt()
         } catch (e: Exception) {
             0
