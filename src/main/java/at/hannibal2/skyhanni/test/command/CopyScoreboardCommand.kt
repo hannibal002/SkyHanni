368523b99992687fbef3acb29f915c0b6ada5fd9
CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>
2023-09-23 20:42:52 +1000

    guh (#495)
    internal sidebar changes #495

--- src/main/java/at/hannibal2/skyhanni/test/command/CopyScoreboardCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/test/command/CopyScoreboardCommand.kt
@@ -7,19 +7,19 @@ import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 
 object CopyScoreboardCommand {
     fun command(args: Array<String>) {
-        try {
-            val resultList = mutableListOf<String>()
-            val noColor = args.size == 1 && args[0] == "true"
-            for (line in ScoreboardData.sidebarLinesFormatted) {
-                val scoreboardLine = if (noColor) line.removeColor() else line
-                resultList.add("'$scoreboardLine'")
-            }
-            val string = resultList.joinToString("\n")
-            OSUtils.copyToClipboard(string)
-            LorenzUtils.chat("§e[SkyHanni] scoreboard copied into the clipboard!")
+        val resultList = mutableListOf<String>()
+        val noColor = args.size == 1 && args[0] == "true"
+        resultList.add("Header:")
+        resultList.add(if (noColor) ScoreboardData.objectiveLine.removeColor() else ScoreboardData.objectiveLine)
+        resultList.add("")
+
+        for (line in ScoreboardData.sidebarLinesFormatted) {
+            val scoreboardLine = if (noColor) line.removeColor() else line
+            resultList.add("'$scoreboardLine'")
         }
-        catch (_: Throwable) {
-            LorenzUtils.chat("§c[SkyHanni] Nothing in scoreboard")
-        }
+
+        val string = resultList.joinToString("\n")
+        OSUtils.copyToClipboard(string)
+        LorenzUtils.chat("§e[BedWar] scoreboard copied into your clipboard!")
     }
 }
