6f12c23a0a3053d79f5f3a6bc6e345a9910b8b5b
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-09-26 11:26:47 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonRankTabListColor.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonRankTabListColor.kt
@@ -10,8 +10,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class DungeonRankTabListColor {
     private val config get() = SkyHanniMod.feature.dungeon.tabList
-
-    val pattern = "§r(?<playerName>.*) §r§f\\(§r§d(?<className>.*) (?<classLevel>.*)§r§f\\)§r".toPattern()
+    private val pattern = "§r(?<playerName>.*) §r§f\\(§r§d(?<className>.*) (?<classLevel>.*)§r§f\\)§r".toPattern()
 
     @SubscribeEvent
     fun onTabListText(event: TabListLineRenderEvent) {
@@ -19,22 +18,15 @@ class DungeonRankTabListColor {
 
         pattern.matchMatcher(event.text) {
             val playerName = group("playerName")
-//                println("playerName: $playerName")
             val split = playerName.split(" ")
             val sbLevel = split[0]
             val cleanName = split[1].cleanPlayerName()
-//                println("cleanName: $cleanName")
 
             val className = group("className")
-//                println("className: $className")
+            val level = group("classLevel").romanToDecimal()
+            val color = getColor(level)
 
-            val classLevel = group("classLevel")
-//                println("classLevel: $classLevel")
-            val lvl = classLevel.romanToDecimal()
-//                println("lvl: $lvl")
-            val lvlColor = getColor(lvl)
-
-            event.text = "$sbLevel §b$cleanName §7(§e$className $lvlColor$lvl§7)"
+            event.text = "$sbLevel §b$cleanName §7(§e$className $color$level§7)"
         }
     }
 
