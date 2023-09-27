20ad34d307ef3126e6c48ad7b2d5c77eb7ca3f10
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-09-27 00:05:38 -0400

    class import optimize

--- src/main/java/at/hannibal2/skyhanni/features/inventory/MenuItemDisplayOverlaySBLeveling.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/MenuItemDisplayOverlaySBLeveling.kt
@@ -9,7 +9,6 @@ import at.hannibal2.skyhanni.utils.ItemUtils.cleanName
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.ItemUtils.name
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
-import at.hannibal2.skyhanni.utils.StringUtils.matchRegex
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
