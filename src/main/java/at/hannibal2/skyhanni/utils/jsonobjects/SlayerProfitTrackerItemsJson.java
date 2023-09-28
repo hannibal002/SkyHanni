2ea43c913a9d4d28e7d12918ac62c5ffde0ac311
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-09-28 11:24:39 +0200

    Ignoring non-slayer drops in slayer profit tracker.

---
+++ src/main/java/at/hannibal2/skyhanni/utils/jsonobjects/SlayerProfitTrackerItemsJson.java
@@ -0,0 +1,13 @@
+package at.hannibal2.skyhanni.utils.jsonobjects;
+
+import at.hannibal2.skyhanni.utils.NEUInternalName;
+import com.google.gson.annotations.Expose;
+
+import java.util.List;
+import java.util.Map;
+
+public class SlayerProfitTrackerItemsJson {
+
+    @Expose
+    public Map<String, List<NEUInternalName>> slayers;
+}
