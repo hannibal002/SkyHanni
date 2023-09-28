5baba39ff026fe777362ffece92b1a56395df396
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-09-28 09:28:10 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/features/misc/items/EstimatedItemValue.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/items/EstimatedItemValue.kt
@@ -288,7 +288,6 @@ object EstimatedItemValue {
 
     private fun addReforgeStone(stack: ItemStack, list: MutableList<String>): Double {
         var rawReforgeName = stack.getReforgeName() ?: return 0.0
-        if (rawReforgeName.equals("pitchin")) rawReforgeName = "pitchin_koi" // exception
 
         for ((rawInternalName, values) in Constants.REFORGESTONES.entrySet()) {
             val stoneJson = values.asJsonObject
