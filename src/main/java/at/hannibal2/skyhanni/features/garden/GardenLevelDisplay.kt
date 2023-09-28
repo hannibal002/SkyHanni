bd4a39abea7afaa490b4e86d8f5e1221fd3d24ff
Walker Selby <git@walkerselby.com>
2023-09-28 13:25:19 +0100

    Merge if statement with nested one

--- src/main/java/at/hannibal2/skyhanni/features/garden/GardenLevelDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/GardenLevelDisplay.kt
@@ -43,15 +43,13 @@ class GardenLevelDisplay {
         val oldLevel = GardenAPI.getGardenLevel()
         GardenAPI.gardenExp = gardenExp + moreExp
         val newLevel = GardenAPI.getGardenLevel()
-        if (newLevel == oldLevel + 1) {
-            if (newLevel > 15) {
-                LorenzUtils.runDelayed(50.milliseconds) {
-                    LorenzUtils.clickableChat(
-                        " \n§b§lGARDEN LEVEL UP §8$oldLevel ➜ §b$newLevel\n" +
-                                " §8+§aRespect from Elite Farmers and SkyHanni members :)\n ",
-                        "/gardenlevels"
-                    )
-                }
+        if (newLevel == oldLevel + 1 && newLevel > 15) {
+            LorenzUtils.runDelayed(50.milliseconds) {
+                LorenzUtils.clickableChat(
+                    " \n§b§lGARDEN LEVEL UP §8$oldLevel ➜ §b$newLevel\n" +
+                            " §8+§aRespect from Elite Farmers and SkyHanni members :)\n ",
+                    "/gardenlevels"
+                )
             }
         }
         update()
