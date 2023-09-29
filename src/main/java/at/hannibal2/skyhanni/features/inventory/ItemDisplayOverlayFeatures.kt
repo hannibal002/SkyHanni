ad2fdfbddab6da0e989d7ace8bc0817eb67f9f7c
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-09-29 11:43:54 -0400

    apparently there's this one item in the composter it uses the internalname "_SACKS" suffix however i doubt hypixel is going to name a skill "Insert" anytime soon

--- src/main/java/at/hannibal2/skyhanni/features/inventory/ItemDisplayOverlayFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/ItemDisplayOverlayFeatures.kt
@@ -91,6 +91,7 @@ class ItemDisplayOverlayFeatures {
 
         if (SkyHanniMod.feature.inventory.displaySackName) {
             if (ItemUtils.isSack(item)) {
+                if (itemName.contains("Insert")) return ""
                 val sackName = grabSackName(itemName)
                 return (if (itemName.contains("Enchanted")) "§5" else "") + sackName.substring(0, 2)
             }
