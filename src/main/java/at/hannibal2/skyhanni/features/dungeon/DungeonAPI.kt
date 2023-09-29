40b6b86889477ce3eb73e91903ab8c3047bf3b07
Walker Selby <git@walkerselby.com>
2023-09-29 13:16:01 +0100

    Format to match line length

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonAPI.kt
@@ -55,9 +55,10 @@ class DungeonAPI {
             if (!inDungeon()) return
             val message = rawMessage.removeColor()
             val bossName = message.substringAfter("[BOSS] ").substringBefore(":").trim()
-            if (bossName != "The Watcher" && dungeonFloor != null && checkBossName(dungeonFloor!!, bossName) && !inBossRoom) {
-                DungeonBossRoomEnterEvent().postAndCatch()
-                inBossRoom = true
+            if (bossName != "The Watcher" && dungeonFloor != null && checkBossName(dungeonFloor!!, bossName) &&
+                !inBossRoom) {
+                    DungeonBossRoomEnterEvent().postAndCatch()
+                    inBossRoom = true
             }
         }
 
