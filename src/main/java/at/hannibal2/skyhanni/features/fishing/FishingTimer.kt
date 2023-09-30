e816afb882a97d7a6d1283f92abd4c70ee3b1fbb
Walker Selby <git@walkerselby.com>
2023-09-30 22:23:11 +0100

    Update FishingConfig Refactor classes improperly named Replace deprecated @ConfigEditorAccordion with @Accordion Remove deprecated @ConfigAccordionId Update references to config Add ConfigFixes

--- src/main/java/at/hannibal2/skyhanni/features/fishing/FishingTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/FishingTimer.kt
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.features.fishing
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
@@ -29,7 +30,7 @@ class FishingTimer {
     @SubscribeEvent
     fun onTick(event: LorenzTickEvent) {
         if (!LorenzUtils.inSkyBlock) return
-        if (!config.barnTimer) return
+        if (!config.barnTimer.enabled) return
 
         if (event.repeatSeconds(3)) {
             rightLocation = isRightLocation()
@@ -39,14 +40,14 @@ class FishingTimer {
 
         if (event.isMod(5)) checkMobs()
         if (event.isMod(7)) tryPlaySound()
-        if (OSUtils.isKeyHeld(config.manualResetTimer)) startTime = System.currentTimeMillis()
+        if (OSUtils.isKeyHeld(config.barnTimer.manualResetTimer)) startTime = System.currentTimeMillis()
     }
 
     private fun tryPlaySound() {
         if (currentCount == 0) return
 
         val duration = System.currentTimeMillis() - startTime
-        val barnTimerAlertTime = config.barnTimerAlertTime * 1_000
+        val barnTimerAlertTime = config.barnTimer.alertTime * 1_000
         if (duration > barnTimerAlertTime && duration < barnTimerAlertTime + 3_000) {
             SoundUtils.playBeepSound()
         }
@@ -64,7 +65,7 @@ class FishingTimer {
             startTime = 0
         }
 
-        if (inHollows && newCount >= 60 && config.wormLimitAlert) {
+        if (inHollows && newCount >= 60 && config.barnTimer.wormLimitAlert) {
             SoundUtils.playBeepSound()
         }
     }
@@ -82,9 +83,9 @@ class FishingTimer {
     private fun isRightLocation(): Boolean {
         inHollows = false
 
-        if (config.barnTimerForStranded && LorenzUtils.isStrandedProfile) return true
+        if (config.barnTimer.forStranded && LorenzUtils.isStrandedProfile) return true
 
-        if (config.barnTimerCrystalHollows && IslandType.CRYSTAL_HOLLOWS.isInIsland()) {
+        if (config.barnTimer.crystalHollows && IslandType.CRYSTAL_HOLLOWS.isInIsland()) {
             inHollows = true
             return true
         }
@@ -99,17 +100,27 @@ class FishingTimer {
     @SubscribeEvent
     fun onRenderOverlay(event: GuiRenderEvent.GameOverlayRenderEvent) {
         if (!LorenzUtils.inSkyBlock) return
-        if (!config.barnTimer) return
+        if (!config.barnTimer.enabled) return
         if (!rightLocation) return
         if (currentCount == 0) return
 
         val duration = System.currentTimeMillis() - startTime
-        val barnTimerAlertTime = config.barnTimerAlertTime * 1_000
+        val barnTimerAlertTime = config.barnTimer.alertTime * 1_000
         val color = if (duration > barnTimerAlertTime) "§c" else "§e"
         val timeFormat = TimeUtils.formatDuration(duration, biggestUnit = TimeUnit.MINUTE)
         val name = if (currentCount == 1) "sea creature" else "sea creatures"
         val text = "$color$timeFormat §8(§e$currentCount §b$name§8)"
 
         config.barnTimerPos.renderString(text, posLabel = "BarnTimer")
+    }
+
+    @SubscribeEvent
+    fun onConfigFix(event: ConfigUpdaterMigrator.ConfigFixEvent){
+        event.move(3, "fishing.barnTimer", "fishing.barnTimer.enabled")
+        event.move(3, "fishing.barnTimerAlertTime", "fishing.barnTimer.alertTime")
+        event.move(3, "fishing.barnTimerCrystalHollows", "fishing.barnTimer.crystalHollows")
+        event.move(3, "fishing.barnTimerForStranded", "fishing.barnTimer.forStranded")
+        event.move(3, "fishing.wormLimitAlert", "fishing.barnTimer.wormLimitAlert")
+        event.move(3, "fishing.manualResetTimer", "fishing.barnTimer.manualResetTimer")
     }
 }
