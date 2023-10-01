847619f15751b7ad038e5ae5cdf039058e4dc2cc
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-09-30 20:10:09 -0400

    jasper ruby prevent false positive

--- src/main/java/at/hannibal2/skyhanni/features/inventory/MenuItemDisplayOverlayMining.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/MenuItemDisplayOverlayMining.kt
@@ -89,9 +89,9 @@ class MenuItemDisplayOverlayMining {
             var crystalsNotFound = 0
             val totalCrystals = 5 //change "5" to whatever new value Hypixel does if this value ever changes
             for (line in lore) {
-                if (line.contains(" §e✖ Not Placed")) crystalsNotPlaced++
+                if (line.contains("Your Other Crystals") || line.contains("Jasper") || line.contains("Ruby")) break
+                else if (line.contains(" §e✖ Not Placed")) crystalsNotPlaced++
                 else if (line.contains(" §c✖ Not Found")) crystalsNotFound++
-                else if (line.contains("Your Other Crystals") || line.contains("Jasper") || line.contains("Ruby")) break
             }
             var crystalsPlaced = totalCrystals - crystalsNotPlaced - crystalsNotFound
             return "§a${crystalsPlaced}§r§e${crystalsNotPlaced}§r§c${crystalsNotFound}"
