0e7331716ce97128695bdeaeccf629ec799d361b
Walker Selby <git@walkerselby.com>
2023-10-02 13:53:06 +0100

    Update GardenConfig (MoneyPerHour) Replace deprecated @ConfigEditorAccordion with @Accordion Remove deprecated @ConfigAccordionId Update references to config Add ConfigFixes Fix formatting Refactor class names to match Config format

--- src/main/java/at/hannibal2/skyhanni/config/features/GardenConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/GardenConfig.java
@@ -796,133 +796,120 @@ public class GardenConfig {
 
     @Expose
     @ConfigOption(name = "Money per Hour", desc = "")
-    @ConfigEditorAccordion(id = 13)
-    public boolean moneyPerHour = false;
+    @Accordion
+    public MoneyPerHourConfig moneyPerHour = new MoneyPerHourConfig();
+    public static class MoneyPerHourConfig{
+        @Expose
+        @ConfigOption(name = "Show Money per Hour",
+                desc = "Displays the money per hour YOU get with YOUR crop/minute value when selling the item to bazaar. " +
+                        "Supports Bountiful, Mushroom Cow Perk, Armor Crops and Dicer Drops. Their toggles are below.")
+        @ConfigEditorBoolean
+        @FeatureToggle
+        public boolean display = true;
 
-    @Expose
-    @ConfigOption(name = "Show Money per Hour",
-            desc = "Displays the money per hour YOU get with YOUR crop/minute value when selling the item to bazaar. " +
-                    "Supports Bountiful, Mushroom Cow Perk, Armor Crops and Dicer Drops. Their toggles are below.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    @FeatureToggle
-    public boolean moneyPerHourDisplay = true;
+        // TODO moulconfig runnable support
+        @Expose
+        @ConfigOption(name = "Only Show Top", desc = "Only show the best # items.")
+        @ConfigEditorSlider(
+                minValue = 1,
+                maxValue = 25,
+                minStep = 1
+        )
+        public int showOnlyBest = 5;
 
-    // TODO moulconfig runnable support
-    @Expose
-    @ConfigOption(name = "Only Show Top", desc = "Only show the best # items.")
-    @ConfigEditorSlider(
-            minValue = 1,
-            maxValue = 25,
-            minStep = 1
-    )
-    @ConfigAccordionId(id = 13)
-    public int moneyPerHourShowOnlyBest = 5;
+        @Expose
+        @ConfigOption(name = "Extend Top List", desc = "Add current crop to the list if its lower ranked than the set limit by extending the list.")
+        @ConfigEditorBoolean
+        public boolean showCurrent = true;
 
-    @Expose
-    @ConfigOption(name = "Extend Top List", desc = "Add current crop to the list if its lower ranked than the set limit by extending the list.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourShowCurrent = true;
+        // TODO moulconfig runnable support
+        @Expose
+        @ConfigOption(
+                name = "Always On",
+                desc = "Always show the money/hour Display while on the garden.")
+        @ConfigEditorBoolean
+        public boolean alwaysOn = false;
 
-    // TODO moulconfig runnable support
-    @Expose
-    @ConfigOption(
-            name = "Always On",
-            desc = "Always show the money/hour Display while on the garden.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourAlwaysOn = false;
+        @Expose
+        @ConfigOption(
+                name = "Compact Mode",
+                desc = "Hide the item name and the position number.")
+        @ConfigEditorBoolean
+        public boolean compact = false;
 
-    @Expose
-    @ConfigOption(
-            name = "Compact Mode",
-            desc = "Hide the item name and the position number.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourCompact = false;
+        @Expose
+        @ConfigOption(
+                name = "Compact Price",
+                desc = "Show the price more compact.")
+        @ConfigEditorBoolean
+        public boolean compactPrice = false;
 
-    @Expose
-    @ConfigOption(
-            name = "Compact Price",
-            desc = "Show the price more compact.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourCompactPrice = false;
+        @Expose
+        @ConfigOption(
+                name = "Use Custom",
+                desc = "Use the custom format below instead of classic ➜ §eSell Offer §7and other profiles ➜ §eNPC Price.")
+        @ConfigEditorBoolean
+        public boolean useCustomFormat = false;
 
-    @Expose
-    @ConfigOption(
-            name = "Use Custom",
-            desc = "Use the custom format below instead of classic ➜ §eSell Offer §7and other profiles ➜ §eNPC Price.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourUseCustomFormat = false;
+        @Expose
+        @ConfigOption(
+                name = "Custom Format",
+                desc = "Set what prices to show")
+        @ConfigEditorDraggableList(
+                exampleText = {
+                        "§eSell Offer",
+                        "§eInstant Sell",
+                        "§eNPC Price"
+                },
+                requireNonEmpty = true
+        )
+        public List<Integer> customFormat = new ArrayList<>(Arrays.asList(0, 1, 2));
 
-    @Expose
-    @ConfigOption(
-            name = "Custom Format",
-            desc = "Set what prices to show")
-    @ConfigEditorDraggableList(
-            exampleText = {
-                    "§eSell Offer",
-                    "§eInstant Sell",
-                    "§eNPC Price"
-            },
-            requireNonEmpty = true
-    )
-    @ConfigAccordionId(id = 13)
-    public List<Integer> moneyPerHourCustomFormat = new ArrayList<>(Arrays.asList(0, 1, 2));
+        @Expose
+        @ConfigOption(
+                name = "Merge Seeds",
+                desc = "Merge the seeds price with the wheat price.")
+        @ConfigEditorBoolean
+        public boolean mergeSeeds = true;
 
-    @Expose
-    @ConfigOption(
-            name = "Merge Seeds",
-            desc = "Merge the seeds price with the wheat price.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourMergeSeeds = true;
+        @Expose
+        @ConfigOption(
+                name = "Include Bountiful",
+                desc = "Includes the coins from Bountiful in the calculation.")
+        @ConfigEditorBoolean
+        public boolean bountiful = true;
 
-    @Expose
-    @ConfigOption(
-            name = "Include Bountiful",
-            desc = "Includes the coins from Bountiful in the calculation.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourBountiful = true;
+        @Expose
+        @ConfigOption(
+                name = "Include Mooshroom Cow",
+                desc = "Includes the coins you get from selling the mushrooms from your Mooshroom Cow pet.")
+        @ConfigEditorBoolean
+        public boolean mooshroom = true;
 
-    @Expose
-    @ConfigOption(
-            name = "Include Mooshroom Cow",
-            desc = "Includes the coins you get from selling the mushrooms from your Mooshroom Cow pet.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourMooshroom = true;
+        @Expose
+        @ConfigOption(
+                name = "Include Armor Drops",
+                desc = "Includes the average coins/hr from your armor.")
+        @ConfigEditorBoolean
+        public boolean armor = true;
 
-    @Expose
-    @ConfigOption(
-            name = "Include Armor Drops",
-            desc = "Includes the average coins/hr from your armor.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourArmor = true;
+        @Expose
+        @ConfigOption(
+                name = "Include Dicer Drops",
+                desc = "Includes the average coins/hr from your melon or pumpkin dicer.")
+        @ConfigEditorBoolean
+        public boolean dicer = true;
 
-    @Expose
-    @ConfigOption(
-            name = "Include Dicer Drops",
-            desc = "Includes the average coins/hr from your melon or pumpkin dicer.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourDicer = true;
+        @Expose
+        @ConfigOption(
+                name = "Hide Title",
+                desc = "Hides the first line of 'Money Per Hour' entirely.")
+        @ConfigEditorBoolean
+        public boolean hideTitle = false;
 
-    @Expose
-    @ConfigOption(
-            name = "Hide Title",
-            desc = "Hides the first line of 'Money Per Hour' entirely.")
-    @ConfigEditorBoolean
-    @ConfigAccordionId(id = 13)
-    public boolean moneyPerHourHideTitle = false;
-
-    @Expose
-    public Position moneyPerHourPos = new Position(-330, 170, false, true);
+        @Expose
+        public Position pos = new Position(-330, 170, false, true);
+    }
 
     @Expose
     @ConfigOption(name = "Next Jacob's Contest", desc = "")
