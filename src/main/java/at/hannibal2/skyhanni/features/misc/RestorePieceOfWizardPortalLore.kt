60d8d8ba5c555d5538e569cd0f230dbef3372ba0
Walker Selby <git@walkerselby.com>
2023-10-10 00:04:56 +0100

    Update usages Remove old import

--- src/main/java/at/hannibal2/skyhanni/features/misc/RestorePieceOfWizardPortalLore.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/RestorePieceOfWizardPortalLore.kt
@@ -2,7 +2,7 @@ package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.LorenzToolTipEvent
-import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName_old
+import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getRecipientName
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -14,7 +14,7 @@ class RestorePieceOfWizardPortalLore {
     fun onTooltip(event: LorenzToolTipEvent) {
         if (!config.restorePieceOfWizardPortalLore) return
         val stack = event.itemStack
-        if (stack.getInternalName_old() != "WIZARD_PORTAL_MEMENTO") return
+        if (!stack.getInternalName().equals("WIZARD_PORTAL_MEMENTO")) return
         val recipient = stack.getRecipientName() ?: return
         if (!event.toolTip[5].contains(recipient)) {
             event.toolTip.add(5, "§7Earned by: $recipient")
