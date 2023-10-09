8d197b7f52d3c0d136814959ad64d698258ad8f4
Walker Selby <git@walkerselby.com>
2023-10-10 00:10:16 +0100

    Update usage Remove old import

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/WildStrawberryDyeNotification.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/WildStrawberryDyeNotification.kt
@@ -6,7 +6,7 @@ import at.hannibal2.skyhanni.events.GuiContainerEvent
 import at.hannibal2.skyhanni.events.OwnInventoryItemUpdateEvent
 import at.hannibal2.skyhanni.features.garden.GardenAPI
 import at.hannibal2.skyhanni.utils.ItemBlink
-import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName_old
+import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.ItemUtils.name
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NEUItems
@@ -34,8 +34,8 @@ class WildStrawberryDyeNotification {
             val diff = System.currentTimeMillis() - lastCloseTime
             if (diff < 1_000) return@execute
 
-            val internalName = event.itemStack.getInternalName_old()
-            if (internalName == "DYE_WILD_STRAWBERRY") {
+            val internalName = event.itemStack.getInternalName()
+            if (internalName.equals("DYE_WILD_STRAWBERRY")) {
                 val name = event.itemStack.name!!
                 TitleUtils.sendTitle(name, 5.seconds)
                 LorenzUtils.chat("§e[SkyHanni] You found a $name§e!")
