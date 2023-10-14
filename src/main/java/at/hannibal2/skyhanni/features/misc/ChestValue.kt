6f1fe665fc78757cc74ca7461c1700eaddec9f52
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-10-14 09:31:51 -0400

    bring back former storagegui code but in trycatch (#568)
    old check for neu storage enabled #568

--- src/main/java/at/hannibal2/skyhanni/features/misc/ChestValue.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/ChestValue.kt
@@ -1,13 +1,16 @@
 package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.config.ConfigManager
 import at.hannibal2.skyhanni.data.IslandType
+import at.hannibal2.skyhanni.data.OtherMod
 import at.hannibal2.skyhanni.events.GuiContainerEvent
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryOpenEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.features.misc.items.EstimatedItemValue
+import at.hannibal2.skyhanni.utils.APIUtil
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalNameOrNull
 import at.hannibal2.skyhanni.utils.LorenzUtils
@@ -31,6 +34,7 @@ import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.awt.Color
+import java.io.File
 import kotlin.time.Duration.Companion.seconds
 
 class ChestValue {
@@ -246,10 +250,15 @@ class ChestValue {
 
     private val isNeuStorageEnabled = RecalculatingValue(1.seconds) {
         try {
-            NotEnoughUpdates.INSTANCE.config.storageGUI.enableStorageGUI3
-        } catch (e: Exception) {
-            false
-        }
+            val configPath = OtherMod.NEU.configPath
+            if (File(configPath).exists()) {
+                val json = ConfigManager.gson.fromJson(
+                    APIUtil.readFile(File(configPath)),
+                    com.google.gson.JsonObject::class.java
+                )
+                json["storageGUI"].asJsonObject["enableStorageGUI3"].asBoolean
+            } else false
+        } catch (e: Exception) { false }
     }
 
     private fun String.reduceStringLength(targetLength: Int, char: Char): String {
