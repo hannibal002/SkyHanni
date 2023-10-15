66d99adb67e52b0eca5ba93cd051263137952bd3
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-10-15 15:02:21 +0200

    typo

--- src/main/java/at/hannibal2/skyhanni/features/nether/QuestItemHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/QuestItemHelper.kt
@@ -30,7 +30,7 @@ class QuestItemHelper {
                 questItem = group("name")
                 questAmount = group("amount").toInt()
                 LorenzUtils.clickableChat(
-                    "§e[SkyHanni] Click here go grab x$questAmount $questItem from sacks!",
+                    "§e[SkyHanni] Click here to grab x$questAmount $questItem from sacks!",
                     "gfs $questItem $questAmount"
                 )
                 lastSentMessage = SimpleTimeMark.now()
