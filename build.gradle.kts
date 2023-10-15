cf770f27aac90200aefe630f60c05a57dae5dc75
CalMWolfs <cwolfson58@gmail.com>
2023-10-16 06:54:36 +1100

    undo

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
 
     modRuntimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.1.0")
 
-    modCompileOnly("com.github.hannibal002:notenoughupdates:4957f0b:all") {
+    modImplementation("com.github.hannibal002:notenoughupdates:4957f0b:all") {
         exclude(module = "unspecified")
         isTransitive = false
     }
