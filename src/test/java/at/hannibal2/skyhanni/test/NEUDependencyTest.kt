9fc9cfd3c8b77308ca5345a7696132e9790569d6
Helfull <admin@helfull.de>
2023-10-17 14:51:02 +0200

    feat(Testing): add mocking check for the initial problem

--- src/test/java/at/hannibal2/skyhanni/test/NEUDependencyTest.kt
+++ src/test/java/at/hannibal2/skyhanni/test/NEUDependencyTest.kt
@@ -2,9 +2,15 @@ package at.hannibal2.skyhanni.test
 
 import at.hannibal2.skyhanni.test.NEUDependent.test
 import io.github.moulberry.notenoughupdates.util.SkyBlockTime
+import io.mockk.every
+import io.mockk.mockkObject
 import org.junit.jupiter.api.Test
 
 object NEUDependent {
+    fun mockMe(): Boolean {
+        return false
+    }
+
     fun SkyBlockTime.test(): Boolean {
         return true
     }
@@ -13,6 +19,9 @@ object NEUDependent {
 class NEUDependencyTest {
     @Test
     fun `test that we can have extensions of NEU on object classes`() {
+        mockkObject(NEUDependent)
+        every { NEUDependent.mockMe() } returns true
+        assert(NEUDependent.mockMe())
         assert(SkyBlockTime(0).test())
     }
 }
