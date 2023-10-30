15ebf95a3b70139dc32076360c8e377c30b6b88c
Walker Selby <git@walkerselby.com>
2023-10-30 18:41:03 +0000

    Add Comment about order

--- src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/MobFinder.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/MobFinder.kt
@@ -81,6 +81,12 @@ class MobFinder {
             RiftAPI.inRift() -> tryAddRift(entity)
             else -> {
                 when (entity) {
+                    /*
+                     * Note that the order does matter here.
+                     * For example, if you put EntityZombie before EntityPigZombie,
+                     * EntityPigZombie will never be reached because EntityPigZombie extends EntityZombie.
+                     * Please take this into consideration if you are to modify this.
+                    */
                     is EntityOtherPlayerMP -> tryAddEntityOtherPlayerMP(entity)
                     is EntityIronGolem -> tryAddEntityIronGolem(entity)
                     is EntityPigZombie -> tryAddEntityPigZombie(entity)
