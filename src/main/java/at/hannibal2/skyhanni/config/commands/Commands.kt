e622b712f4fb851d3a8c2632456d7c3c27510ab6
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-11-03 19:04:20 +0100

    reverted unnecessary changes

--- src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
@@ -37,7 +37,13 @@ import at.hannibal2.skyhanni.test.PacketTest
 import at.hannibal2.skyhanni.test.SkyHanniConfigSearchResetCommand
 import at.hannibal2.skyhanni.test.SkyHanniDebugsAndTests
 import at.hannibal2.skyhanni.test.TestBingo
-import at.hannibal2.skyhanni.test.command.*
+import at.hannibal2.skyhanni.test.command.CopyItemCommand
+import at.hannibal2.skyhanni.test.command.CopyNearbyEntitiesCommand
+import at.hannibal2.skyhanni.test.command.CopyNearbyParticlesCommand
+import at.hannibal2.skyhanni.test.command.CopyScoreboardCommand
+import at.hannibal2.skyhanni.test.command.CopyTabListCommand
+import at.hannibal2.skyhanni.test.command.ErrorManager
+import at.hannibal2.skyhanni.test.command.TestChatCommand
 import at.hannibal2.skyhanni.utils.APIUtil
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.SoundUtils
