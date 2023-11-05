1fe1dd74eeac193487df7926db468e6036c07c11
VixidDev <52578495+VixidDev@users.noreply.github.com>
2023-11-05 21:33:21 +0000

    Add option to ignore chat in Everything Chroma

--- src/main/java/at/hannibal2/skyhanni/config/features/ChromaConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/ChromaConfig.java
@@ -52,11 +52,17 @@ public class ChromaConfig {
     @ConfigEditorBoolean
     public boolean allChroma = false;
 
+    @Expose
+    @ConfigOption(name = "Ignore Chat", desc = "Prevents Everything Chroma from applying to the chat if you unironically use that feature...")
+    @ConfigEditorBoolean
+    public boolean ignoreChat = false;
+
     private void resetChromaSettings() {
         SkyHanniMod.getFeature().chroma.chromaSize = 30f;
         SkyHanniMod.getFeature().chroma.chromaSpeed = 6f;
         SkyHanniMod.getFeature().chroma.chromaSaturation = 0.75f;
         SkyHanniMod.getFeature().chroma.allChroma = false;
         SkyHanniMod.getFeature().chroma.chromaDirection = 0;
+        SkyHanniMod.getFeature().chroma.ignoreChat = false;
     }
 }
--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/gui/MixinGuiNewChat.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/gui/MixinGuiNewChat.java
@@ -1,10 +1,12 @@
 package at.hannibal2.skyhanni.mixins.transformers.gui;
 
 import at.hannibal2.skyhanni.features.chat.ChatPeek;
+import at.hannibal2.skyhanni.mixins.hooks.FontRendererHook;
 import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GuiNewChat.class)
@@ -13,5 +15,15 @@ public class MixinGuiNewChat {
     @Inject(method = "getChatOpen", at = @At("HEAD"), cancellable = true)
     public void onIsOpen(CallbackInfoReturnable<Boolean> cir) {
         if (ChatPeek.peek()) cir.setReturnValue(true);
+    }
+
+    @Inject(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;enableBlend()V", shift = At.Shift.AFTER))
+    private void setTextRenderIsFromChat(int updateCounter, CallbackInfo ci) {
+        FontRendererHook.INSTANCE.setCameFromChat(true);
+    }
+
+    @Inject(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;disableAlpha()V", shift = At.Shift.BEFORE))
+    private void setTextRenderIsntFromChat(int updateCounter, CallbackInfo ci) {
+        FontRendererHook.INSTANCE.setCameFromChat(false);
     }
 }
