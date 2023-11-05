84fa795ab4e7ff87fe0c01a7ab0c03dfdb6f6c4a
VixidDev <52578495+VixidDev@users.noreply.github.com>
2023-11-05 15:39:47 +0000

    Fix formatting

--- src/main/java/at/hannibal2/skyhanni/features/misc/items/enchants/Enchants.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/items/enchants/Enchants.kt
@@ -9,35 +9,35 @@ import kotlin.collections.HashMap
 private const val COLOR_CODES = "0123456789abcdefZ"
 
 class Enchants {
-        var NORMAL: HashMap<String, Enchant.Normal> = hashMapOf()
-        var ULTIMATE: HashMap<String, Enchant.Ultimate> = hashMapOf()
-        var STACKING: HashMap<String, Enchant.Stacking> = hashMapOf()
+    var NORMAL: HashMap<String, Enchant.Normal> = hashMapOf()
+    var ULTIMATE: HashMap<String, Enchant.Ultimate> = hashMapOf()
+    var STACKING: HashMap<String, Enchant.Stacking> = hashMapOf()
 
-        fun getFromLore(passedLoreName: String) : Enchant {
-            val loreName = passedLoreName.lowercase()
-            var enchant: Enchant? = NORMAL[loreName]
-            if (enchant == null) enchant = ULTIMATE[loreName]
-            if (enchant == null) enchant = STACKING[loreName]
-            if (enchant == null) enchant = Enchant.Dummy(passedLoreName)
-            return enchant
-        }
+    fun getFromLore(passedLoreName: String) : Enchant {
+        val loreName = passedLoreName.lowercase()
+        var enchant: Enchant? = NORMAL[loreName]
+        if (enchant == null) enchant = ULTIMATE[loreName]
+        if (enchant == null) enchant = STACKING[loreName]
+        if (enchant == null) enchant = Enchant.Dummy(passedLoreName)
+        return enchant
+    }
 
-        fun containsEnchantment(enchants: Map<String, Int>, line: String) : Boolean {
-            val matcher = ENCHANTMENT_PATTERN.matcher(line)
-            while (matcher.find()) {
-                val enchant = this.getFromLore(matcher.group("enchant"))
-                if (enchants.isNotEmpty()) {
-                    if (enchants.containsKey(enchant.nbtName)) return true
-                } else {
-                    if (NORMAL.containsKey(enchant.loreName.lowercase())) return true
-                    if (ULTIMATE.containsKey(enchant.loreName.lowercase())) return true
-                    if (STACKING.containsKey(enchant.loreName.lowercase())) return true
-                }
+    fun containsEnchantment(enchants: Map<String, Int>, line: String) : Boolean {
+        val matcher = ENCHANTMENT_PATTERN.matcher(line)
+        while (matcher.find()) {
+            val enchant = this.getFromLore(matcher.group("enchant"))
+            if (enchants.isNotEmpty()) {
+                if (enchants.containsKey(enchant.nbtName)) return true
+            } else {
+                if (NORMAL.containsKey(enchant.loreName.lowercase())) return true
+                if (ULTIMATE.containsKey(enchant.loreName.lowercase())) return true
+                if (STACKING.containsKey(enchant.loreName.lowercase())) return true
             }
-            return false
         }
+        return false
+    }
 
-        fun hasEnchantData() = NORMAL.isNotEmpty() && ULTIMATE.isNotEmpty() && STACKING.isNotEmpty()
+    fun hasEnchantData() = NORMAL.isNotEmpty() && ULTIMATE.isNotEmpty() && STACKING.isNotEmpty()
 }
 
 open class Enchant : Comparable<Enchant> {
