ffe420b0eaa42232658037002548f8c39966fd3f
Walker Selby <git@walkerselby.com>
2023-11-15 16:05:23 +0000

    Refactor MinionsConfig subclasses into separate classes for readability Accordions/Subcategories

--- src/main/java/at/hannibal2/skyhanni/config/features/minion/MinionsConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/minion/MinionsConfig.java
@@ -27,58 +27,10 @@ public class MinionsConfig {
     @Accordion
     public LastClickedMinionConfig lastClickedMinion = new LastClickedMinionConfig();
 
-    public static class LastClickedMinionConfig {
-        @Expose
-        @ConfigOption(name = "Last Minion Display", desc = "Marks the location of the last clicked minion, even through walls.")
-        @ConfigEditorBoolean
-        @FeatureToggle
-        public boolean display = false;
-
-        @Expose
-        @ConfigOption(
-                name = "Last Minion Color",
-                desc = "The color in which the last minion should be displayed."
-        )
-        @ConfigEditorColour
-        public String color = "0:245:85:255:85";
-
-        @Expose
-        @ConfigOption(
-                name = "Last Minion Time",
-                desc = "Time in seconds how long the last minion should be displayed."
-        )
-        @ConfigEditorSlider(
-                minValue = 3,
-                maxValue = 120,
-                minStep = 1
-        )
-        public int time = 20;
-    }
-
     @Expose
     @ConfigOption(name = "Emptied Time", desc = "")
     @Accordion
     public EmptiedTimeConfig emptiedTime = new EmptiedTimeConfig();
-
-    public static class EmptiedTimeConfig {
-        @Expose
-        @ConfigOption(name = "Emptied Time Display", desc = "Show the time when the hopper in the minion was last emptied.")
-        @ConfigEditorBoolean
-        @FeatureToggle
-        public boolean display = false;
-
-        @Expose
-        @ConfigOption(
-                name = "Distance",
-                desc = "Maximum distance to display minion data."
-        )
-        @ConfigEditorSlider(
-                minValue = 3,
-                maxValue = 30,
-                minStep = 1
-        )
-        public int distance = 10;
-    }
 
     @Expose
     @ConfigOption(name = "Hopper Profit Display", desc = "Use the hopper's held coins and the last empty time to calculate the coins per day.")
