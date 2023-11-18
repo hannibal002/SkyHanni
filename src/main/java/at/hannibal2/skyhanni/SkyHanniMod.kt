72c3403788a2a046d6147ae07578d9c12ab696e2
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-11-18 01:44:48 +0100

    0.21.1 Beta 2.1

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -331,7 +331,7 @@ import org.apache.logging.log4j.Logger
     clientSideOnly = true,
     useMetadata = true,
     guiFactory = "at.hannibal2.skyhanni.config.ConfigGuiForgeInterop",
-    version = "0.21.1.Beta.2",
+    version = "0.21.1.Beta.2.1",
 )
 class SkyHanniMod {
     @Mod.EventHandler
--- src/main/java/at/hannibal2/skyhanni/features/misc/massconfiguration/DefaultConfigFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/massconfiguration/DefaultConfigFeatures.kt
@@ -38,7 +38,7 @@ object DefaultConfigFeatures {
                 "shdefaultoptions"
             )
         } else if (updated) {
-            val lastVersion = knownToggles.keys.last()
+            val lastVersion = knownToggles.keys.last { it != SkyHanniMod.version }
             val command = "/shdefaultoptions $lastVersion ${SkyHanniMod.version}"
             LorenzUtils.clickableChat(
                 "§e[SkyHanni] Looks like you updated SkyHanni. " +
