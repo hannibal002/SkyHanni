0d97e894458049e14f2b8ddb34a3f2840788036d
Walker Selby <git@walkerselby.com>
2023-11-22 01:11:54 +0000

    Move declaration inside when
    Signed-off-by: Walker Selby <git@walkerselby.com>

--- src/main/java/at/hannibal2/skyhanni/test/SkyHanniConfigSearchResetCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/test/SkyHanniConfigSearchResetCommand.kt
@@ -244,8 +244,7 @@ object SkyHanniConfigSearchResetCommand {
         if (this is Runnable) return "Runnable"
 
         // we don't use javaClass.simpleName since we want to catch edge cases
-        val name = javaClass.name
-        return when (name) {
+        return when (val name = javaClass.name) {
             "at.hannibal2.skyhanni.config.core.config.Position" -> "Position"
             "java.lang.Boolean" -> "Boolean"
             "java.lang.Integer" -> "Int"
