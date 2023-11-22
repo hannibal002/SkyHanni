387e792db9106019ce2ef1bebfa76aec9a71c390
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-11-22 21:09:36 +0100

    added neu item drops as well

--- src/main/java/at/hannibal2/skyhanni/features/fishing/FishingProfitTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/FishingProfitTracker.kt
@@ -11,7 +11,7 @@ import at.hannibal2.skyhanni.events.SackChangeEvent
 import at.hannibal2.skyhanni.events.entity.ItemAddInInventoryEvent
 import at.hannibal2.skyhanni.features.bazaar.BazaarApi.Companion.getBazaarData
 import at.hannibal2.skyhanni.test.PriceSource
-import at.hannibal2.skyhanni.utils.ItemUtils.getItemName
+import at.hannibal2.skyhanni.utils.ItemUtils.nameWithEnchantment
 import at.hannibal2.skyhanni.utils.KeyboardManager
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils.addAsSingletonList
@@ -19,6 +19,7 @@ import at.hannibal2.skyhanni.utils.LorenzUtils.addSelector
 import at.hannibal2.skyhanni.utils.LorenzUtils.sortedDesc
 import at.hannibal2.skyhanni.utils.NEUInternalName
 import at.hannibal2.skyhanni.utils.NEUInternalName.Companion.asInternalName
+import at.hannibal2.skyhanni.utils.NEUItems.getItemStack
 import at.hannibal2.skyhanni.utils.NEUItems.getNpcPriceOrNull
 import at.hannibal2.skyhanni.utils.NEUItems.getPriceOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil
@@ -32,6 +33,7 @@ import at.hannibal2.skyhanni.utils.renderables.Renderable
 import at.hannibal2.skyhanni.utils.tracker.SkyHanniTracker
 import at.hannibal2.skyhanni.utils.tracker.TrackerData
 import com.google.gson.annotations.Expose
+import io.github.moulberry.notenoughupdates.NotEnoughUpdates
 import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
@@ -71,11 +73,11 @@ object FishingProfitTracker {
             var hidden = false
 
             override fun toString() = "FishingItem{" +
-                "internalName='" + internalName + '\'' +
-                ", timesDropped=" + timesCaught +
-                ", totalAmount=" + totalAmount +
-                ", hidden=" + hidden +
-                '}'
+                    "internalName='" + internalName + '\'' +
+                    ", timesDropped=" + timesCaught +
+                    ", totalAmount=" + totalAmount +
+                    ", hidden=" + hidden +
+                    '}'
 
             var lastTimeUpdated = SimpleTimeMark.farPast()
         }
@@ -102,8 +104,9 @@ object FishingProfitTracker {
                 itemProfit.timesCaught
             } else amount
 
-            val cleanName = if (internalName == SKYBLOCK_COIN) "§6Coins" else internalName.getItemName()
-            var name = cleanName
+            val cleanName =
+                if (internalName == SKYBLOCK_COIN) "§6Coins" else internalName.getItemStack().nameWithEnchantment
+            var name = cleanName ?: error("no name for $internalName")
             val priceFormat = NumberUtil.format(price)
             val hidden = itemProfit.hidden
 
@@ -252,22 +255,69 @@ object FishingProfitTracker {
         addItem(internalName, amount)
     }
 
-    private var itemCategories = mutableMapOf<String, List<NEUInternalName>>()
+    private var itemCategories = mapOf<String, List<NEUInternalName>>()
+
+    private var shItemCategories = mapOf<String, List<NEUInternalName>>()
+    private var neuItemCategories = mapOf<String, List<NEUInternalName>>()
 
     @SubscribeEvent
     fun onRepoReload(event: RepositoryReloadEvent) {
-        itemCategories = event.getConstant<FishingProfitItemsJson>("FishingProfitItems").categories
+        shItemCategories = event.getConstant<FishingProfitItemsJson>("FishingProfitItems").categories
+        updateItemCategories()
     }
 
-    private fun isAllowedItem(internalName: NEUInternalName): Boolean {
-        for ((name, items) in itemCategories) {
-            if (internalName in items) {
-                return true
+    private fun updateItemCategories() {
+        itemCategories = shItemCategories + neuItemCategories
+    }
+
+    @SubscribeEvent
+    fun onNeuRepoReload(event: io.github.moulberry.notenoughupdates.events.RepositoryReloadEvent) {
+        val totalDrops = mutableListOf<String>()
+        val dropCategories = mutableMapOf<String, MutableList<NEUInternalName>>()
+        for ((seaCreature, data) in NotEnoughUpdates.INSTANCE.manager.itemInformation.filter { it.key.endsWith("_SC") }) {
+            val asJsonObject = data.getAsJsonArray("recipes")[0].asJsonObject
+            val drops = asJsonObject.getAsJsonArray("drops")
+                .map { it.asJsonObject.get("id").asString }.map { it.split(":").first() }
+            val asJsonArray = asJsonObject.get("extra")
+            val extra = asJsonArray?.let {
+                asJsonArray.asJsonArray.toList()
+                    .map { it.toString() }
+                    .filter { !it.contains("Fishing Skill") && !it.contains("Requirements:") && !it.contains("Fished from water") }
+                    .joinToString(" + ")
+            } ?: "null"
+            val category = if (extra.contains("Fishing Festival")) {
+                "Fishing Festival"
+            } else if (extra.contains("Spooky Festival")) {
+                "Spooky Festival"
+            } else if (extra.contains("Jerry Pond")) {
+                "Jerry Pond"
+            } else if (extra.contains("Oasis")) {
+                "Oasis"
+            } else if (extra.contains("Magma Fields") || extra.contains("Precursor Remnants") ||
+                extra.contains("Goblin Holdout")
+            ) {
+                "Crystal Hollows"
+            } else if (extra.contains("Crimson Isle Lava")) {
+                "Crimson Isle Lava"
+            } else {
+                if (extra.isNotEmpty()) {
+                    println("unknown extra: $extra = $seaCreature ($drops)")
+                }
+                "Water"
+            } + " Sea Creatures"
+            for (drop in drops) {
+                if (drop !in totalDrops) {
+                    totalDrops.add(drop)
+                    dropCategories.getOrPut(category) { mutableListOf() }.add(drop.asInternalName())
+                    println("$drop = $category")
+                }
             }
         }
-
-        return false
+        neuItemCategories = dropCategories
+        updateItemCategories()
     }
+
+    private fun isAllowedItem(internalName: NEUInternalName) = itemCategories.any { internalName in it.value }
 
     private fun getPrice(internalName: NEUInternalName) = when (config.priceFrom) {
         0 -> internalName.getBazaarData()?.sellPrice ?: internalName.getPriceOrNull() ?: 0.0
