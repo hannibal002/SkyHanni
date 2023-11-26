2d45461ddd85f657fd8a188b9ef778d6bf94102e
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-11-26 11:10:01 +0100

    Added SprayType

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestType.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestType.kt
@@ -2,15 +2,15 @@ package at.hannibal2.skyhanni.features.garden.pests
 
 import at.hannibal2.skyhanni.features.combat.damageindicator.BossType
 
-enum class PestType(val displayName: String, val damageIndicatorBoss: BossType) {
-    BEETLE("Beetle", BossType.GARDEN_PEST_BEETLE),
-    CRICKET("Cricket", BossType.GARDEN_PEST_CRICKET),
-    EARTHWORM("Earthworm", BossType.GARDEN_PEST_EARTHWORM),
-    FLY("Fly", BossType.GARDEN_PEST_FLY),
-    LOCTUS("Loctus", BossType.GARDEN_PEST_LOCUST),
-    MITE("Mite", BossType.GARDEN_PEST_MITE),
-    MOSQUITO("Mosquito", BossType.GARDEN_PEST_MOSQUITO),
-    MOTH("Moth", BossType.GARDEN_PEST_MOTH),
-    RAT("Rat", BossType.GARDEN_PEST_RAT),
-    SLUG("Slug", BossType.GARDEN_PEST_SLUG),
+enum class PestType(val displayName: String, val damageIndicatorBoss: BossType, val spray: SprayType) {
+    BEETLE("Beetle", BossType.GARDEN_PEST_BEETLE, SprayType.DUNG),
+    CRICKET("Cricket", BossType.GARDEN_PEST_CRICKET, SprayType.HONEY_JAR),
+    EARTHWORM("Earthworm", BossType.GARDEN_PEST_EARTHWORM, SprayType.COMPOST),
+    FLY("Fly", BossType.GARDEN_PEST_FLY, SprayType.DUNG),
+    LOCTUS("Loctus", BossType.GARDEN_PEST_LOCUST, SprayType.PLANT_MATTER),
+    MITE("Mite", BossType.GARDEN_PEST_MITE, SprayType.TASTY_CHEESE),
+    MOSQUITO("Mosquito", BossType.GARDEN_PEST_MOSQUITO, SprayType.COMPOST),
+    MOTH("Moth", BossType.GARDEN_PEST_MOTH, SprayType.HONEY_JAR),
+    RAT("Rat", BossType.GARDEN_PEST_RAT, SprayType.TASTY_CHEESE),
+    SLUG("Slug", BossType.GARDEN_PEST_SLUG, SprayType.PLANT_MATTER),
 }
---
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/SprayType.kt
@@ -0,0 +1,10 @@
+package at.hannibal2.skyhanni.features.garden.pests
+
+enum class SprayType(val displayName: String) {
+    PLANT_MATTER("Plant Matter"),
+    DUNG("Dung"),
+    COMPOST("Compost"),
+    HONEY_JAR("Honey Jar"),
+    TASTY_CHEESE("Tasty Cheese"),
+    ;
+}
