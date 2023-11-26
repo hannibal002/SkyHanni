725f1f84dc791c3b31d3917bfe61c69a0b586448
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-11-26 16:59:42 +0100

    Showing a better plot name in scoreboard. Updates faster and doesnt hide when pests are spawned.

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiIngameHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiIngameHook.kt
@@ -1,6 +1,10 @@
 package at.hannibal2.skyhanni.mixins.hooks
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.features.garden.GardenAPI
+import at.hannibal2.skyhanni.features.garden.GardenPlotAPI
+import at.hannibal2.skyhanni.features.garden.GardenPlotAPI.isBarn
+import at.hannibal2.skyhanni.features.garden.GardenPlotAPI.name
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import net.minecraft.client.gui.FontRenderer
 
@@ -21,6 +25,24 @@ fun drawString(
         piggyPattern.matchMatcher(text) {
             val coins = group("coins")
             return instance.drawString("Purse: $coins", x, y, color)
+        }
+    }
+
+    if (SkyHanniMod.feature.garden.plotNameInScoreboard && GardenAPI.inGarden()) {
+        if (text.contains("⏣")) {
+            val plot = GardenPlotAPI.getCurrentPlot()
+            val pests = if (text.contains("ൠ")) {
+                text.last().digitToInt()
+            } else 0
+            var name = plot?.let {
+                if (it.isBarn()) {
+                    "§aGarden: The Barn"
+                } else "§aPlot: §b" + it.name
+            } ?: "§aGarden §cOutside"
+            if (pests > 0) {
+                name = "$name §7(§4${pests}ൠ§7)"
+            }
+            return instance.drawString(" §7⏣ $name", x, y, color)
         }
     }
 
