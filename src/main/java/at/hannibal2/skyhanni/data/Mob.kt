57409415ce244a0442d98adec9992d406a71a7c9
Thunderblade73 <gaidermarkus@gmail.com>
2023-11-26 12:26:58 +0100

    Added KDoc for Mob Class

--- src/main/java/at/hannibal2/skyhanni/data/Mob.kt
+++ src/main/java/at/hannibal2/skyhanni/data/Mob.kt
@@ -1,5 +1,7 @@
 package at.hannibal2.skyhanni.data
 
+import at.hannibal2.skyhanni.data.Mob.Type
+import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.utils.EntityUtils.canBeSeen
 import at.hannibal2.skyhanni.utils.EntityUtils.cleanName
 import at.hannibal2.skyhanni.utils.EntityUtils.isCorrupted
@@ -13,6 +15,41 @@ import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.item.EntityArmorStand
 import net.minecraft.util.AxisAlignedBB
 
+/**
+ * Represents a Mob in Hypixel Skyblock.
+ *
+ * @property baseEntity The main entity representing the Mob.
+ *
+ * Avoid caching, as it may change without notice.
+ * @property mobType The type of the Mob.
+ * @property armorStand The armor stand entity associated with the Mob, if it has one.
+ * @property name The name of the Mob.
+ * @property extraEntities Additional entities associated with the Mob.
+ *
+ * Avoid caching, as they may change without notice.
+ * @property owner Valid for: [Type.Summon], [Type.Slayer]
+ *
+ * The owner of the Mob.
+ * @property hasStar Valid for: [Type.Dungeon]
+ *
+ * Indicates whether the Mob has a star.
+ * @property attribute Valid for: [Type.Dungeon]
+ *
+ * The attribute of the Mob..
+ * @property levelOrTier Valid for: [Type.Basic], [Type.Slayer]
+ *
+ * The level or tier of the Mob.
+ * @property hologram1 Valid for: [Type.Basic], [Type.Slayer]
+ *
+ * Gives back the first additional armor stand.
+ *
+ *   (should be called in the [MobEvent.Spawn] since it is a lazy)
+ * @property hologram2 Valid for: [Type.Basic], [Type.Slayer]
+ *
+ * Gives back the second additional armor stand.
+ *
+ *   (should be called in the [MobEvent.Spawn] since it is a lazy)
+ */
 class Mob(
     var baseEntity: EntityLivingBase,
     val mobType: Type,
@@ -44,6 +81,8 @@ class Mob(
     override fun hashCode(): Int {
         return baseEntity.hashCode()
     }
+
+    override fun toString(): String = "$name - ${baseEntity.entityId}"
 
     val isCorrupted get() = baseEntity.isCorrupted() // Can change
     val isRunic = baseEntity.isRunic() // Does not Change
