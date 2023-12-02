970aa5faa21b54c75963c87fe8c04cbe986dd15a
Walker Selby <git@walkerselby.com>
2023-12-02 18:40:10 +0000

    Add NEU internal name .matches function
    Signed-off-by: Walker Selby <git@walkerselby.com>

--- src/main/java/at/hannibal2/skyhanni/utils/NEUInternalName.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/NEUInternalName.kt
@@ -1,5 +1,8 @@
 package at.hannibal2.skyhanni.utils
 
+import at.hannibal2.skyhanni.utils.StringUtils.matches
+import java.util.regex.Pattern
+
 class NEUInternalName private constructor(private val internalName: String) {
 
     companion object {
@@ -40,3 +43,5 @@ class NEUInternalName private constructor(private val internalName: String) {
     fun replace(oldValue: String, newValue: String) =
         internalName.replace(oldValue.uppercase(), newValue.uppercase()).asInternalName()
 }
+
+fun Pattern.matches(internalName: NEUInternalName): Boolean = this.matches(internalName.asString())
