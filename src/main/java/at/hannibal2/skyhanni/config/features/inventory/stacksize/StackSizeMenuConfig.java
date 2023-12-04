ef9e21d117dbe2fe4a3cf92d1211d22b8f14bfe0
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-04 12:02:43 -0500

    fight the pain away my bed is in ruins

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
@@ -10,6 +10,16 @@ import java.util.List;
 
 public class StackSizeMenuConfig {
 
+    @Expose
+    @ConfigOption(
+        name = "§aBingo Card§7",
+        desc = "Showing various pieces of information as a stack size for these menu items.\nSome values may be truncated percentages or §a✔§r§7s.\n§c§lWARNING§r§c: If you have any respect for your FPS, please §l§odon't§r§c enable §l§oeverything§r§c at once."
+    )
+    @ConfigEditorDraggableList
+    public List<Bingo> bingo = new ArrayList<>(Arrays.asList(
+        Bingo.SECRET_BINGO_DISCOVERY
+    ));
+
     public enum Bingo {
         SECRET_BINGO_DISCOVERY("§bSecret Bingo Goal Discovery (#, caps at 10k)"),
         SECRET_BINGO_HINT_COUNTDOWN(""),
