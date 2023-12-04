c4aa54d273acb213b02d6da031094ffe8998973d
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-04 14:02:08 -0500

    e

--- src/main/java/at/hannibal2/skyhanni/utils/CachedItemData.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/CachedItemData.kt
@@ -21,10 +21,10 @@ data class CachedItemData(
 
     // null = not loaded
     var itemRarity: LorenzRarity? = null,
-    
-    /** -1 = not loaded */
+
+    // -1 = not loaded
     var auctionNumber: Int? = -1,
 
-    /** -1 = not loaded */
+    // -1 = not loaded
     var editionNumber: Int? = -1,
 )
