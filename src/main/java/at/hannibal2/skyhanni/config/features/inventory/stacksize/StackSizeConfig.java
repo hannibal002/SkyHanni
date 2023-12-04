d4bca32cbcabf46844c7e76e4c0d9672e419763e
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-04 14:23:09 -0500

    old man yells at cloud, somehow fixes all problems

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeConfig.java
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.config.features.inventory.stacksize;
 
 import at.hannibal2.skyhanni.config.HasLegacyId;
 import com.google.gson.annotations.Expose;
-import io.github.moulberry.moulconfig.annotations.Accordion;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorDraggableList;
 import io.github.moulberry.moulconfig.annotations.ConfigOption;
 
