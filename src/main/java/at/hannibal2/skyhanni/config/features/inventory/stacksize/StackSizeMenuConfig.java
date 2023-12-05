f343296016ad143a6ad95576c2626a29f1cc52c0
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-05 18:14:08 -0500

    [CHERRY PICK ABIPHONE] silly things
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
@@ -31,7 +31,8 @@ public class StackSizeMenuConfig {
         SELECTED_RINGTONE("§bSelected Ringtone"), // (Abbv)
         TIC_TAC_TOE("§bTic Tac Toe Stats"), // (§aW§eT§cL§b)
         SNAKE("§bSnake Highest Score"), // (#)
-        NAVIGATION("§bSorting/Filtering Abbreviations");
+        NAVIGATION("§bSorting/Filtering Abbreviations"),
+        ;
 
         final String str;
 
