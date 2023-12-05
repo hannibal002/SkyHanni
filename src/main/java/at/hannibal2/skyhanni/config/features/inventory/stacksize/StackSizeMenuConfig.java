6a732e358ac6c8e76a1e5f319f3a8ef637e204c9
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-05 18:38:51 -0500

    [NEW CHERRY PICK ABIPHONE] silly things
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
@@ -36,7 +36,6 @@ public class StackSizeMenuConfig {
 
         final String str;
         Abiphone(String str) { this.str = str; }
-        @Override
-        public String toString() { return str; }
+        @Override public String toString() { return str; }
     }
 }
