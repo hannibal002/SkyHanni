e2b37e82a3712eae6f522eb7b28555049c8643ed
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-05 18:41:01 -0500

    [NEW CHERRY PICK GENERAL] silly things v2
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/stacksize/StackSizeMenuConfig.java
@@ -42,7 +42,6 @@ public class StackSizeMenuConfig {
 
         final String str;
         PlayerGeneral(String str) { this.str = str; }
-        @Override
-        public String toString() {return str; }
+        @Override public String toString() {return str; }
     }
 }
