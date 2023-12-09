7442b2ad183f43b42c2df161ee0b8c2f9ec1377d
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-08 19:13:42 -0500

    GITHUB WAKE THE FUCK UP
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
@@ -1,10 +1,16 @@
 package at.hannibal2.skyhanni.features.inventory.itemdisplayoverlay
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.RenderItemTipEvent
 import net.minecraft.item.ItemStack
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 abstract class AbstractStackSize {
     val configItemStackSize get() = SkyHanniMod.feature.inventory
+    @SubscribeEvent
+    open fun onRenderItemTip(event: RenderItemTipEvent) {
+        event.stackTip = getStackTip(event.stack)
+    }
     abstract fun getStackTip(item: ItemStack): String
 }
 
