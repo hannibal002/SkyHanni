2969c92f9775fd4fd0f59a7d0b999654578220e9
alexia <me@alexia.lol>
2023-12-24 23:15:30 +0100

    Make vacuum bag show 40+ instead of 40 when capped (#830)
    Make vacuum bag show 40+ instead of 40 when capped #830

--- src/main/java/at/hannibal2/skyhanni/features/inventory/ItemDisplayOverlayFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/ItemDisplayOverlayFeatures.kt
@@ -207,7 +207,7 @@ object ItemDisplayOverlayFeatures {
                 gardenVacuumPatterm.matchMatcher(line) {
                     val pests = group("amount").formatNumber()
                     return if (config.vacuumBagCap) {
-                        if (pests > 39) "§640" else "$pests"
+                        if (pests > 39) "§640+" else "$pests"
                     } else {
                         when {
                             pests < 40 -> "$pests"
