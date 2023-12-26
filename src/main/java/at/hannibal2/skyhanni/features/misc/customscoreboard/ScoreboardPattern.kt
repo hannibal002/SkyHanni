a79217a081678b4a2bbc69185b1195779e53e0e2
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2023-12-26 10:32:29 +0100

    adde empty line

--- src/main/java/at/hannibal2/skyhanni/features/misc/customscoreboard/ScoreboardPattern.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/customscoreboard/ScoreboardPattern.kt
@@ -4,5 +4,6 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 object ScoreboardPattern {
     val group = RepoPattern.group("features.misc.customscoreboard")
+
     val lobbycodePattern by group.pattern ("lobbycode", "§(\\d{3}/\\d{2}/\\d{2}) §(?<code>.*)$")
 }
