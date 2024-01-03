5a122431b7dcdfd1536ca19253cc7669a10e52ca
calmwolfs <cwolfson58@gmail.com>
2024-01-03 17:07:13 -0500

    migrate SlayerQuestWarning

--- src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerQuestWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerQuestWarning.kt
@@ -12,13 +12,19 @@ import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.getLorenzVec
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.entity.EntityLivingBase
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
 
 class SlayerQuestWarning {
     private val config get() = SkyHanniMod.feature.slayer
-    private val talkToMaddoxPattern = " {3}§r§5§l» §r§7Talk to Maddox to claim your (.+) Slayer XP!".toPattern()
+
+    private  val talkToMaddoxPattern by RepoPattern.pattern(
+        "slayer.questwarning.talkto",
+        " {3}§r§5§l» §r§7Talk to Maddox to claim your .+ Slayer XP!"
+    )
+
     private var needSlayerQuest = false
     private var lastWarning = 0L
     private var currentReason = ""
