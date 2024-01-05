f84b3f6563720174ef6d8275376d958a29f68cbe
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-05 12:08:24 -0500

    [cherrypick everywhere except IDOF]

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
@@ -2,8 +2,10 @@ package at.hannibal2.skyhanni.features.inventory.itemdisplayoverlay
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.RenderItemTipEvent
+import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.util.regex.Pattern
 
 abstract class AbstractStackSize {
     val configItemStackSize get() = SkyHanniMod.feature.inventory
@@ -16,4 +18,11 @@ abstract class AbstractStackSize {
 
 abstract class AbstractMenuStackSize : AbstractStackSize()  {
     val configMenuStackSize get() = configItemStackSize.stackSize.menu
+    
+    fun Pattern.returnPercentFromLoreLineAsStackSize(line: String): String {
+        this.matchMatcher(line) {
+            return group("percent").replace("100", "§a✔")
+        }
+        return ""
+    }
 }
