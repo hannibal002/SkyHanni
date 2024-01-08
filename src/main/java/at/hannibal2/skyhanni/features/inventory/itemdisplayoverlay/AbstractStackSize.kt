241dfb9c0e726d7adb7422a72414ff686bddbc46
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-08 16:05:02 -0500

    [chrypck @e] intellij yelling @me to specify types pt1
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/AbstractStackSize.kt
@@ -1,19 +1,21 @@
 package at.hannibal2.skyhanni.features.inventory.itemdisplayoverlay
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.config.features.inventory.InventoryConfig
 import at.hannibal2.skyhanni.events.RenderItemTipEvent
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPatternGroup
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 abstract class AbstractStackSize {
-    val configItemStackSize get() = SkyHanniMod.feature.inventory
+    val configItemStackSize: InventoryConfig get() = SkyHanniMod.feature.inventory
     @SubscribeEvent
     open fun onRenderItemTip(event: RenderItemTipEvent) {
         event.stackTip = getStackTip(event.stack)
     }
     abstract fun getStackTip(item: ItemStack): String
-    val itemStackSizeGroup = RepoPattern.group("itemstacksize")
+    val itemStackSizeGroup: RepoPatternGroup = RepoPattern.group("itemstacksize")
     val greenCheckmark: String = "§a✔"
     val bigRedCross: String = "§c§l✖"
 }
