946f88fd589472975fc398ece68157a163237e1d
Thunderblade73 <gaidermarkus@gmail.com>
2024-01-09 11:43:19 +0100

    using the other arrow json format

---
+++ src/main/java/at/hannibal2/skyhanni/data/ArrowType.kt
@@ -0,0 +1,3 @@
+package at.hannibal2.skyhanni.data
+
+class ArrowType(val arrow: String, val internalName: String)
--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/ArrowTypeJson.java
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/ArrowTypeJson.java
@@ -1,10 +1,14 @@
 package at.hannibal2.skyhanni.data.jsonobjects.repo;
 
 import com.google.gson.annotations.Expose;
-
-import java.util.List;
+import java.util.Map;
 
 public class ArrowTypeJson {
     @Expose
-    public List<ArrowType> arrows;
+    public Map<String, ArrowAttributes> arrows;
+
+    public static class ArrowAttributes {
+        @Expose
+        public String internalName;
+    }
 }
