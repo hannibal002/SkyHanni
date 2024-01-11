844e6272ebba1989dbabe971910978570d7a5c58
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-01-11 19:26:06 +0100

    Feature: ShiftClickNPCSell (#907)
    Option to makes normal clicks to shift clicks in equipment inventory. #907

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/InventoryConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/InventoryConfig.java
@@ -9,7 +9,6 @@ import io.github.moulberry.moulconfig.annotations.Category;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorDraggableList;
 import io.github.moulberry.moulconfig.annotations.ConfigOption;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -184,9 +183,15 @@ public class InventoryConfig {
     public boolean copyUnderbidPrice = false;
 
     @Expose
-    @ConfigOption(name = "Shift Click Equipment", desc = "Makes normal clicks to shift clicks in equipment inventory")
+    @ConfigOption(name = "Shift Click Equipment", desc = "Makes normal clicks to shift clicks in equipment inventory.")
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean shiftClickForEquipment = false;
+
+    @Expose
+    @ConfigOption(name = "Shift Click NPC sell", desc = "Makes normal clicks to shift clicks in npc inventory for selling.")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    public boolean shiftClickNPCSell = false;
 
 }
