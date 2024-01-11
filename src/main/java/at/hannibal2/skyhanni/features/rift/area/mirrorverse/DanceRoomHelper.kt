3c9e80059601b08c624cd8d14f8c319b97a3bd0b
calmwolfs <cwolfson58@gmail.com>
2024-01-10 23:41:06 -1000

    use correct coroutine thing

--- src/main/java/at/hannibal2/skyhanni/features/rift/area/mirrorverse/DanceRoomHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/mirrorverse/DanceRoomHelper.kt
@@ -1,6 +1,8 @@
 package at.hannibal2.skyhanni.features.rift.area.mirrorverse
 
+import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
+import at.hannibal2.skyhanni.data.jsonobjects.repo.DanceRoomInstructionsJson
 import at.hannibal2.skyhanni.events.CheckRenderEntityEvent
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
@@ -12,7 +14,6 @@ import at.hannibal2.skyhanni.features.rift.RiftAPI
 import at.hannibal2.skyhanni.utils.LocationUtils.isPlayerInside
 import at.hannibal2.skyhanni.utils.RenderUtils.renderStrings
 import at.hannibal2.skyhanni.utils.StringUtils.firstLetterUppercase
-import at.hannibal2.skyhanni.data.jsonobjects.repo.DanceRoomInstructionsJson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -142,7 +143,7 @@ object DanceRoomHelper {
         val countdownString = "%01d:%03d".format(seconds, milliseconds)
         countdown = countdownString
 
-        CoroutineScope(Dispatchers.Default).launch {
+        SkyHanniMod.coroutineScope.launch {
             delay(1)
             var updatedSeconds = seconds
             var updatedMilliseconds = milliseconds - 1
