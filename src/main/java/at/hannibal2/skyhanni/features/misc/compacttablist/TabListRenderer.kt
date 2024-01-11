0bb2a47bce773ca32d3a367b51dea33490544060
Linnea Gräf <nea@nea.moe>
2024-01-11 17:48:56 +0100

    Add fire hide tab list (#865)
    Add tab list fire sale advertisement hider. #865

--- src/main/java/at/hannibal2/skyhanni/features/misc/compacttablist/TabListRenderer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/compacttablist/TabListRenderer.kt
@@ -1,8 +1,11 @@
 package at.hannibal2.skyhanni.features.misc.compacttablist
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.SkipTabListLineEvent
 import at.hannibal2.skyhanni.mixins.transformers.AccessorGuiPlayerTabOverlay
 import at.hannibal2.skyhanni.utils.LorenzUtils
+import at.hannibal2.skyhanni.utils.StringUtils.matches
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Gui
 import net.minecraft.client.gui.ScaledResolution
@@ -91,18 +94,31 @@ object TabListRenderer {
         }
 
         var middleX = x
+        var lastTitle = null as TabLine?
+        var lastSubTitle = null as TabLine?
         for (column in columns) {
             var middleY = if (config.hideAdverts) headerY else headerY + padding + 2
+
+            val filteredColumn = column.lines.filterTo(mutableListOf()) { tabLine ->
+                if (tabLine.type == TabStringType.TITLE) {
+                    lastSubTitle = null
+                    lastTitle = tabLine
+                }
+                if (tabLine.type == TabStringType.SUB_TITLE) {
+                    lastSubTitle = tabLine
+                }
+                !SkipTabListLineEvent(tabLine, lastSubTitle, lastTitle).postAndCatch()
+            }.let(::RenderColumn)
 
             Gui.drawRect(
                 middleX - padding + 1,
                 middleY - padding + 1,
-                middleX + column.getMaxWidth() + padding - 2,
-                middleY + column.size() * lineHeight + padding - 2,
+                middleX + filteredColumn.getMaxWidth() + padding - 2,
+                middleY + filteredColumn.size() * lineHeight + padding - 2,
                 0x20AAAAAA
             )
 
-            for (tabLine in column.lines) {
+            for (tabLine in filteredColumn.lines) {
                 val savedX = middleX
 
                 val hideIcons = config.advancedPlayerList.hidePlayerIcons && !AdvancedPlayerList.ignoreCustomTabList()
@@ -125,7 +141,7 @@ object TabListRenderer {
                 if (tabLine.type == TabStringType.TITLE) {
                     minecraft.fontRendererObj.drawStringWithShadow(
                         text,
-                        middleX + column.getMaxWidth() / 2f - tabLine.getWidth() / 2f,
+                        middleX + filteredColumn.getMaxWidth() / 2f - tabLine.getWidth() / 2f,
                         middleY.toFloat(),
                         0xFFFFFF
                     )
@@ -140,7 +156,7 @@ object TabListRenderer {
                 middleY += lineHeight
                 middleX = savedX
             }
-            middleX += column.getMaxWidth() + columnSpacing
+            middleX += filteredColumn.getMaxWidth() + columnSpacing
         }
 
         if (footer.isNotEmpty()) {
@@ -156,4 +172,14 @@ object TabListRenderer {
             }
         }
     }
+
+    val fireSaleRegex by RepoPattern.pattern("tablist.firesaletitle", "^§b§lFire Sales: §r§f\\([0-9]+\\)$")
+
+    @SubscribeEvent
+    fun hideFireFromTheTabListBecauseWhoWantsThose(event: SkipTabListLineEvent) {
+        if (config.hideFiresales && event.lastSubTitle != null && fireSaleRegex.matches(event.lastSubTitle.text)) {
+            event.cancel()
+        }
+    }
+
 }
