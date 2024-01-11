6449271f9b0392f93d8434ab0908385e0e2411bb
calmwolfs <cwolfson58@gmail.com>
2024-01-10 14:04:58 -1000

    migrate QuestItemHelper

--- src/main/java/at/hannibal2/skyhanni/features/nether/QuestItemHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/QuestItemHelper.kt
@@ -10,13 +10,18 @@ import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.hours
 
 class QuestItemHelper {
     private val config get() = SkyHanniMod.feature.crimsonIsle
 
-    private val itemCollectionPattern = ". (?<name>[\\w ]+) x(?<amount>\\d+)".toPattern()
+    private val itemCollectionPattern by RepoPattern.pattern(
+        "nether.itemhelper.itemcollection",
+        ". (?<name>[\\w ]+) x(?<amount>\\d+)"
+    )
+
     private var lastSentMessage = SimpleTimeMark.farPast()
 
     @SubscribeEvent
