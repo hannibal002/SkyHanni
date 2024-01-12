060be41affc046a09271133f2f5504af1232475c
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-12 09:02:03 -0500

    `@`combat: @CalMWolfs style regex
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/menu/MenuItemDisplayOverlayCombat.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/menu/MenuItemDisplayOverlayCombat.kt
@@ -13,16 +13,42 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 class MenuItemDisplayOverlayCombat : AbstractMenuStackSize() {
     private val combatSubgroup = itemStackSizeGroup.group("combat")
     
-    // private val genericPercentPattern = ".* (§.)?(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%".toPattern()
-    private val bestiaryChestNamePattern by combatSubgroup.pattern(("bestiary.chestname"), ("Bestiary.*"))
-    private val bestiaryMilestoneItemNamePattern by combatSubgroup.pattern(("bestiarymilestone.itemname"), ("Bestiary Milestone (?<milestone>[\\w]+)"))
-    private val familiesCompletedOverallProgressPercentLoreLinePattern by combatSubgroup.pattern(("familiescompletedoverallprogresspercent.loreline"), (".*(Families Completed|Overall Progress):.* (§.)?(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%.*"))
-    private val slayerLevelLoreLinePattern by combatSubgroup.pattern(("slayerlevel.loreline"), ("(§.)*(?<mobType>[\\w]+) Slayer: (§.)*LVL (?<level>[\\w]+)"))
-    private val slayerLevelOtherLoreLinePattern by combatSubgroup.pattern(("slayerlevelother.loreline"), ("(§.)*Current LVL: (§.)*(?<level>[\\w]+)"))
-    private val combatWisdomBuffLoreLinePattern by combatSubgroup.pattern(("combatwisdombuff.loreline"), ("(§.)*Total buff: (§.)*\\+(?<combatWise>[\\w]+). Combat Wisdom"))
-    private val rngMeterProgressPercentLoreLinePattern by combatSubgroup.pattern(("rngmeterprogresspercent.loreline"), (".*(§.)+Progress:.* (§.)?(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%.*"))
-    private val unlockedSlayerRecipesLoreLinePattern by combatSubgroup.pattern(("unlockedslayerrecipes.loreline"), (".*(§.)*Unlocked: (§.)*(?<recipes>[\\w]+) recipes.*"))
-    private val overallProgressToggleLoreLinePattern by combatSubgroup.pattern(("overallprogresstoggle.loreline"), ("(§.)*Overall Progress: (§.)*(?<status>[\\w]+)"))
+    private val bestiaryChestNamePattern by combatSubgroup.pattern(
+        "bestiary.chestname",
+        "Bestiary.*"
+    )
+    private val bestiaryMilestoneItemNamePattern by combatSubgroup.pattern(
+        "bestiarymilestone.itemname",
+        "Bestiary Milestone (?<milestone>[\\w]+)"
+    )
+    private val familiesCompletedOverallProgressPercentLoreLinePattern by combatSubgroup.pattern(
+        "familiescompletedoverallprogresspercent.loreline",
+        ".*(Families Completed|Overall Progress):.* (§.)?(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%.*"
+    )
+    private val slayerLevelLoreLinePattern by combatSubgroup.pattern(
+        "slayerlevel.loreline",
+        "(§.)*(?<mobType>[\\w]+) Slayer: (§.)*LVL (?<level>[\\w]+)"
+    )
+    private val slayerLevelOtherLoreLinePattern by combatSubgroup.pattern(
+        "slayerlevelother.loreline",
+        "(§.)*Current LVL: (§.)*(?<level>[\\w]+)"
+    )
+    private val combatWisdomBuffLoreLinePattern by combatSubgroup.pattern(
+        "combatwisdombuff.loreline",
+        "(§.)*Total buff: (§.)*\\+(?<combatWise>[\\w]+). Combat Wisdom"
+    )
+    private val rngMeterProgressPercentLoreLinePattern by combatSubgroup.pattern(
+        "rngmeterprogresspercent.loreline",
+        ".*(§.)+Progress:.* (§.)?(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%.*"
+    )
+    private val unlockedSlayerRecipesLoreLinePattern by combatSubgroup.pattern(
+        "unlockedslayerrecipes.loreline",
+        ".*(§.)*Unlocked: (§.)*(?<recipes>[\\w]+) recipes.*"
+    )
+    private val overallProgressToggleLoreLinePattern by combatSubgroup.pattern(
+        "overallprogresstoggle.loreline",
+        "(§.)*Overall Progress: (§.)*(?<status>[\\w]+)"
+    )
 
     @SubscribeEvent
     override fun onRenderItemTip(event: RenderItemTipEvent) {
