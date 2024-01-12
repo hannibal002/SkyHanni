2c588eb45e5c83ccb1278feaaee7630896f466db
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-12 13:44:46 -0500

    upstream
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

---
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/ShiftClickNPCSell.kt
@@ -0,0 +1,54 @@
+package at.hannibal2.skyhanni.features.inventory
+
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.GuiContainerEvent
+import at.hannibal2.skyhanni.events.InventoryCloseEvent
+import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
+import at.hannibal2.skyhanni.utils.ItemUtils.getLore
+import at.hannibal2.skyhanni.utils.LorenzUtils
+import at.hannibal2.skyhanni.utils.StringUtils.matches
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import net.minecraft.client.Minecraft
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+object ShiftClickNPCSell {
+
+    private val config get() = SkyHanniMod.feature.inventory.shiftClickNPCSell
+
+    private val sellSlot = 49
+    private val lastLoreLineOfSellPattern by RepoPattern.pattern(
+        "inventory.npc.sell.lore",
+        "§7them to this Shop!|§eClick to buyback!"
+    )
+
+    var inInventory = false
+        private set
+
+    fun isEnabled() = LorenzUtils.inSkyBlock && config
+
+    @SubscribeEvent
+    fun onOpen(event: InventoryFullyOpenedEvent) {
+        if (!LorenzUtils.inSkyBlock) return
+        inInventory = lastLoreLineOfSellPattern.matches(event.inventoryItems[sellSlot]?.getLore()?.lastOrNull() ?: "")
+    }
+
+    @SubscribeEvent
+    fun onClose(event: InventoryCloseEvent) {
+        inInventory = false
+    }
+
+    @SubscribeEvent
+    fun onSlotClick(event: GuiContainerEvent.SlotClickEvent) {
+        if (!isEnabled()) return
+        if (!inInventory) return
+
+        val slot = event.slot ?: return
+
+        if (slot.slotNumber == slot.slotIndex) return
+
+        event.isCanceled = true
+        Minecraft.getMinecraft().playerController.windowClick(
+            event.container.windowId, event.slot.slotNumber, event.clickedButton, 1, Minecraft.getMinecraft().thePlayer
+        )
+    }
+}
