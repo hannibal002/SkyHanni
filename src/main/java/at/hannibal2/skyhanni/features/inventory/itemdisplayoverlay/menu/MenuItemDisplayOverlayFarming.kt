53c706bd33354a4bb8ff0aad8998c9967554a45e
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-12 09:06:20 -0500

    `@`farming: @CalMWolfs style regex
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/menu/MenuItemDisplayOverlayFarming.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/itemdisplayoverlay/menu/MenuItemDisplayOverlayFarming.kt
@@ -13,13 +13,30 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 class MenuItemDisplayOverlayFarming : AbstractMenuStackSize() {
     private val farmingSubgroup = itemStackSizeGroup.group("farming")
     
-    // private val genericPercentPattern = ".* (§.)?(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%".toPattern()
-    private val totallingCountResourceLoreLinePattern by farmingSubgroup.pattern(("totallingcountresource.loreline"), (".*(§.)Totalling ((§.)+)(?<resourceCount>[\\w]+) (?<resourceType>[ \\w]+)(§.)\\..*"))
-    private val jacobFarmingContestMedalInventoryLoreLinePattern by farmingSubgroup.pattern(("jacobfarmingcontestmedalinventory.loreline"), ("(?<colorCode>§.)(?<bold>§l)+(?<medal>[\\w]+) (§.)*(m|M)edals: (§.)*(?<count>[\\w]+)"))
-    private val nextVisitorCountdownLoreLinePattern by farmingSubgroup.pattern(("nextvisitorcountdown.loreline"), ("(§.)*Next Visitor: (§.)*(?<time>[\\w]+)(m|s).*"))
-    private val insertResourceFromLocationItemNamePattern by farmingSubgroup.pattern(("insertresourcefromlocation.itemname"), ("Insert (?<resource>[\\w]+) from (?<inventorySacks>[\\w]+)"))
-    private val visitorLogbookNPCRarityLoreLinePattern by farmingSubgroup.pattern(("visitorlogbooknpcrarity.loreline"), ("§.§(L|l)(UNCOMMON|RARE|LEGENDARY|SPECIAL|MYTHIC)"))
-    private val visitorMilestonePercentProgressLoreLinePattern by farmingSubgroup.pattern(("visitormilestonepercentprogress.loreline"), ("(§.)*Progress to Tier (?<tier>[\\w]+):.* (§.)*(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%"))
+    private val totallingCountResourceLoreLinePattern by farmingSubgroup.pattern(
+        "totallingcountresource.loreline",
+        ".*(§.)Totalling ((§.)+)(?<resourceCount>[\\w]+) (?<resourceType>[ \\w]+)(§.)\\..*"
+    )
+    private val jacobFarmingContestMedalInventoryLoreLinePattern by farmingSubgroup.pattern(
+        "jacobfarmingcontestmedalinventory.loreline",
+        "(?<colorCode>§.)(?<bold>§l)+(?<medal>[\\w]+) (§.)*(m|M)edals: (§.)*(?<count>[\\w]+)"
+    )
+    private val nextVisitorCountdownLoreLinePattern by farmingSubgroup.pattern(
+        "nextvisitorcountdown.loreline",
+        "(§.)*Next Visitor: (§.)*(?<time>[\\w]+)(m|s).*"
+    )
+    private val insertResourceFromLocationItemNamePattern by farmingSubgroup.pattern(
+        "insertresourcefromlocation.itemname",
+        "Insert (?<resource>[\\w]+) from (?<inventorySacks>[\\w]+)"
+    )
+    private val visitorLogbookNPCRarityLoreLinePattern by farmingSubgroup.pattern(
+        "visitorlogbooknpcrarity.loreline",
+        "§.§(L|l)(UNCOMMON|RARE|LEGENDARY|SPECIAL|MYTHIC)"
+    )
+    private val visitorMilestonePercentProgressLoreLinePattern by farmingSubgroup.pattern(
+        "visitormilestonepercentprogress.loreline",
+        "(§.)*Progress to Tier (?<tier>[\\w]+):.* (§.)*(?<percent>[0-9]+)(\\.[0-9]*)?(§.)?%"
+    )
 
     @SubscribeEvent
     override fun onRenderItemTip(event: RenderItemTipEvent) {
@@ -31,10 +48,13 @@ class MenuItemDisplayOverlayFarming : AbstractMenuStackSize() {
         val itemName = item.cleanName()
         val stackSizeConfig = configMenuStackSize.farming
         val chestName = InventoryUtils.openInventoryName()
+        val lore = item.getLore()
+        
+        if (lore.isEmpty()) return ""
 
         if (stackSizeConfig.contains(StackSizeMenuConfig.Farming.JACOBS_MEDALS) && ((chestName == "Jacob's Farming Contests") && itemName == ("Claim your rewards!"))) {
             var result = ""
-            for (line in item.getLore()) {
+            for (line in lore) {
                 jacobFarmingContestMedalInventoryLoreLinePattern.matchMatcher(line) {
                     result = "$result${group("colorCode")}${group("count")}"
                 }
@@ -43,7 +63,7 @@ class MenuItemDisplayOverlayFarming : AbstractMenuStackSize() {
         }
 
         if (stackSizeConfig.contains(StackSizeMenuConfig.Farming.VISITORS_LOGBOOK_COUNTDOWN) && ((chestName == "Visitor's Logbook") && itemName == ("Logbook"))) {
-            for (line in item.getLore()) {
+            for (line in lore) {
                 nextVisitorCountdownLoreLinePattern.matchMatcher(line) {
                     return group("time")
                 }
@@ -51,18 +71,14 @@ class MenuItemDisplayOverlayFarming : AbstractMenuStackSize() {
         }
 
         if (stackSizeConfig.contains(StackSizeMenuConfig.Farming.VISITOR_MILESTONES) && (chestName == "Visitor Milestones")) {
-            val lore = item.getLore()
-            if (lore.isNotEmpty()) {
-                for (line in lore) {
-                    visitorMilestonePercentProgressLoreLinePattern.matchMatcher(line) {
-                        return group("percent").convertPercentToGreenCheckmark()
-                    }
+            for (line in lore) {
+                visitorMilestonePercentProgressLoreLinePattern.matchMatcher(line) {
+                    return group("percent").convertPercentToGreenCheckmark()
                 }
             }
         }
 
         if (stackSizeConfig.contains(StackSizeMenuConfig.Farming.VISITOR_NPC_RARITIES) && (chestName == "Visitor's Logbook")) {
-            val lore = item.getLore()
             for (line in lore) {
                 visitorLogbookNPCRarityLoreLinePattern.matchMatcher(line) {
                     return line.take(5)
@@ -71,13 +87,8 @@ class MenuItemDisplayOverlayFarming : AbstractMenuStackSize() {
         }
 
         if (stackSizeConfig.contains(StackSizeMenuConfig.Farming.COMPOSTER_INSERT_ABBV)) {
-            // (chestName.contains("Composter"))
-            // (itemName.contains("Insert ") && itemName.contains(" from "))
             if (chestName == "Composter") {
                 insertResourceFromLocationItemNamePattern.matchMatcher(itemName) {
-                    val lore = item.getLore()
-                    // §7Totalling §2§240k Fuel§7.
-                    // Totalling 40k Fuel.
                     for (line in lore) {
                         totallingCountResourceLoreLinePattern.matchMatcher(line) {
                             return group("resourceCount")
