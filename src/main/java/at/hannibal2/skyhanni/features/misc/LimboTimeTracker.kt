eea55a2bf448b354b1c1a26726177cb90d8aa3d6
martimcavaco <martim.cavaco@tutanota.com>
2024-01-12 01:00:41 +0000

    optimize imports

--- src/main/java/at/hannibal2/skyhanni/features/misc/LimboTimeTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/LimboTimeTracker.kt
@@ -15,8 +15,6 @@ import at.hannibal2.skyhanni.utils.LorenzUtils.round
 import at.hannibal2.skyhanni.utils.RenderUtils.renderString
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.TimeUtils.format
-import net.minecraft.client.Minecraft
-import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration
