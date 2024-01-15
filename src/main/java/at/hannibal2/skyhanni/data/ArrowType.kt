9f72eef58074e3418e113839f1960ff04eb49d0c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-01-15 22:17:18 +0100

    Update src/main/java/at/hannibal2/skyhanni/data/ArrowType.kt
    Co-authored-by: Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/data/ArrowType.kt
+++ src/main/java/at/hannibal2/skyhanni/data/ArrowType.kt
@@ -1,7 +1,5 @@
 package at.hannibal2.skyhanni.data
 
 class ArrowType(val arrow: String, val internalName: String) {
-    override fun toString(): String {
-        return internalName
-    }
+    override fun toString() = internalName
 }
