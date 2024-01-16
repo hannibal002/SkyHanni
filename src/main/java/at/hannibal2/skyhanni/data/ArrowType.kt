daf06a3a3112fe57f6e704e7a799da3335d448ec
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-01-16 18:56:44 +0100

    Overrode .hashCode() and .equals() in ArrowType

--- src/main/java/at/hannibal2/skyhanni/data/ArrowType.kt
+++ src/main/java/at/hannibal2/skyhanni/data/ArrowType.kt
@@ -1,5 +1,23 @@
 package at.hannibal2.skyhanni.data
 
-class ArrowType(val arrow: String, val internalName: String) {
+data class ArrowType(val arrow: String, val internalName: String) {
     override fun toString() = internalName
+
+    override fun hashCode(): Int {
+        var result = arrow.hashCode()
+        result = 31 * result + internalName.hashCode()
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ArrowType
+
+        if (arrow != other.arrow) return false
+        if (internalName != other.internalName) return false
+
+        return true
+    }
 }
