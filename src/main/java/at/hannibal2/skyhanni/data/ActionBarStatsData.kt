78ad3ae8aa3d7dbe65cd1f8b18cf52a758088b32
Thunderblade73 <gaidermarkus@gmail.com>
2024-01-17 21:08:55 +0100

    implemented nea suggestion

--- src/main/java/at/hannibal2/skyhanni/data/ActionBarStatsData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/ActionBarStatsData.kt
@@ -8,10 +8,11 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import org.intellij.lang.annotations.Language
 
-enum class ActionBarStatsData(@Language("RegExp") rawPattern: String) { HEALTH(
-    // language=RegExp
-    "§[c6](?<health>[\\d,]+)/[\\d,]+❤.*"
-),
+enum class ActionBarStatsData(@Language("RegExp") rawPattern: String) {
+    HEALTH(
+        // language=RegExp
+        "§[c6](?<health>[\\d,]+)/[\\d,]+❤.*"
+    ),
     DEFENSE(
         // language=RegExp
         ".*§a(?<defense>[\\d,]+)§a❈.*"
@@ -45,12 +46,12 @@ enum class ActionBarStatsData(@Language("RegExp") rawPattern: String) { HEALTH(
         fun onActionBar(event: LorenzActionBarEvent) {
             if (!LorenzUtils.inSkyBlock) return
 
-            entries.mapNotNull { value ->
-                value.pattern.matchMatcher(event.message) {
+            entries.mapNotNull { data ->
+                data.pattern.matchMatcher(event.message) {
                     val newValue = group(1)
-                    if (value.value != newValue) {
-                        value.value = newValue
-                        ActionBarValueUpdate(value)
+                    if (data.value != newValue) {
+                        data.value = newValue
+                        ActionBarValueUpdate(data)
                     } else null
                 }
             }.forEach { it.postAndCatch() }
