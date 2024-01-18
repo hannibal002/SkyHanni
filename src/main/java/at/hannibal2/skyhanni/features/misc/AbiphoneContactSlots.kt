099bf6fe14b2e0d1e84bc4d48ce9c1fe9cacb150
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-01-17 21:48:48 -0500

    attempt to cleanup per @CalMWolfs comment
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/misc/AbiphoneContactSlots.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/AbiphoneContactSlots.kt
@@ -8,46 +8,36 @@ import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.matches
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.client.Minecraft
-import net.minecraft.client.gui.inventory.GuiEditSign
+import net.minecraft.client.gui.inventory.GuiInventory
 import net.minecraftforge.event.entity.player.ItemTooltipEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class AbiphoneContactSlots {
-    private val allPossibleUpgrades: List<Int> = listOf<Int>(36, 2, 2) //36: Contacts Trio, 2: GG Abicase, 2: Abiphone relays. see https://antifandom.com/hypixel-skyblock/wiki/Abiphones#Contact_Slots
-    private val couldNotCalculateNotice: String = " §8Could not calculate contact slots. [SkyHanni]"
+    private val ALL_UPGRADES_TOTAL: Int = 40
     private val abiphoneContactSlotsGroup = RepoPattern.group("abiphone.contact.slots")
-    private val isAbiphoneInternalNamePattern by abiphoneContactSlotsGroup.pattern(("isabiphone.internalname"), ("ABIPHONE_.*")) //is it even worth it (assuming, well, *possible at all*) to increase contact slots on the abingohpone?
+    private val isAbiphoneInternalNamePattern by abiphoneContactSlotsGroup.pattern(("isabiphone.internalname"), ("ABIPHONE_.*"))
     private val maximumContactSlotsLoreLinePattern by abiphoneContactSlotsGroup.pattern(("maximumcontactslots.loreline"), (" ?§7Maximum Contacts: (?:§.)*(?<first>\\d+)(?: |§.)*(?:[(\\[]\\+?(?<second>\\d+)[)\\]])?(?: |§.)*(?:[(\\[]\\+?(?<third>\\d+)[)\\]])?(?: |§.)*(?:[(\\[]\\+?(?<fourth>\\d+)[)\\]])?(?: |§.)*(?:[(\\[]\\+?(?<fifth>\\d+)[)\\]])?")) // adapted from nea's https://regex101.com/r/aCVGHN/1 -ery
 
     @SubscribeEvent
     fun onTooltip(event: ItemTooltipEvent) {
-        if (!LorenzUtils.inSkyBlock) return
-        if (!SkyHanniMod.feature.misc.abiphoneContactsProgress) return
-        val currScreen = Minecraft.getMinecraft().currentScreen ?: return
-        if (currScreen is GuiEditSign) return
+        if (!(LorenzUtils.inSkyBlock && SkyHanniMod.feature.misc.abiphoneContactsProgress)) return
+        if (Minecraft.getMinecraft().currentScreen !is GuiInventory) return
         val itemStack = event.itemStack
         if (!(isAbiphoneInternalNamePattern.matches(itemStack.getInternalName().asString()))) return
         val itemLore = itemStack.getLore()
         if (itemLore.none { maximumContactSlotsLoreLinePattern.matches(it) }) return
-        // "mc.currentScreen !is AccessorGuiEditSign" is to prevent such calculations happening with NEU custom auction house search
-        // §7Maximum Contacts: §b7 §c(+15) §5(+1)
-        // §7Maximum Contacts: §b7 §c(+15) §5(+1) §f(+1)
-        // 7 15 1 1
-        var total = 0
         val trueIndex = itemLore.indexOfFirst { maximumContactSlotsLoreLinePattern.matches(it) }
-        if (trueIndex == -1) return
-        val index = trueIndex + 2
         maximumContactSlotsLoreLinePattern.matchMatcher(itemLore[trueIndex]) {
-            val contactsBeforeSplit = "${group("first") ?: 0} ${group("second") ?: 0} ${group("third") ?: 0} ${group("fourth") ?: 0}" // ${group("fifth") ?: 0}"
-            val contacts = contactsBeforeSplit.split(" ")
-            for (eachInt in contacts) if (eachInt.toIntOrNull() != null) total += eachInt.toInt()
+            val upgrades = listOf<String>(group("first") ?: "0", group("second") ?: "0", group("third") ?: "0", group("fourth") ?: "0", group("fifth") ?: "0")
+            var total = 0
+            upgrades.forEach { total += it.toIntOrNull() ?: 0 }
             if (total > 0) {
                 event.toolTip.add(
-                    index,
-                    " §7Contacts Progress: §b$total§7/§b${(allPossibleUpgrades.sum() + contacts.first().toInt())}"
+                    trueIndex + 2,
+                    " §7Contacts Progress: §b$total§7/§b${(ALL_UPGRADES_TOTAL + upgrades.first().toInt())}"
                 )
-            } else { //this is in case hypixel changes formatting but still keeps spaces somehow
-                event.toolTip.add(index, couldNotCalculateNotice)
+            } else {
+                event.toolTip.add(trueIndex + 2, " §8Could not calculate contact slots. [SkyHanni]")
             }
         }
     }
