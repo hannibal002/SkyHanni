10b3b32b6df02951c59be9944066df4dac7ccafb
CarsCupcake <09carshi@gmail.com>
2024-01-18 10:39:15 +0100

    Rename

--- src/main/java/at/hannibal2/skyhanni/features/inventory/QuiverNotification.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/QuiverNotification.kt
@@ -11,12 +11,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
 
 object QuiverNotification {
-    private val pattern by RepoPattern.pattern("inventory.quiver.chat.low", "You only have (?<arrowsLeft>.*) arrows left in your Quiver!")
+    private val quiverChatPattern by RepoPattern.pattern("inventory.quiver.chat.low", "You only have (?<arrowsLeft>.*) arrows left in your Quiver!")
     @SubscribeEvent
     fun onChatMessage(event: LorenzChatEvent) {
         if (!SkyHanniMod.configManager.features.inventory.quiverAlert) return
         val message = event.message.removeColor()
-        pattern.matchMatcher(message) {
+        quiverChatPattern.matchMatcher(message) {
             TitleManager.sendTitle("§c${group("arrowsLeft")} arrows left!", 3.seconds, 3.6, 7.0)
             SoundUtils.repeatSound(100, 30, SoundUtils.plingSound)
         }
