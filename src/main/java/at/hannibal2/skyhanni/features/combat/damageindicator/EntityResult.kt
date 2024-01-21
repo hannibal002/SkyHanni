3bc18d545a395eb5a7d045d5163b51b2bc9a3278
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-01-21 20:16:25 +0100

    Fixed all Damage Indicator boss timers in Dungeons being off by a second.

--- src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/EntityResult.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/EntityResult.kt
@@ -1,7 +1,7 @@
 package at.hannibal2.skyhanni.features.combat.damageindicator
 
 class EntityResult(
-    val delayedStart: Long = -1L,
+    val delayedStart: Long? = null,
     val ignoreBlocks: Boolean = false,
     val finalDungeonBoss: Boolean = false,
     val bossType: BossType = BossType.GENERIC_DUNGEON_BOSS,
