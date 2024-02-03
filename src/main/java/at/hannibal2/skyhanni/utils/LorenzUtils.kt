a040318b8accb0bff210eda13d7e0188a6da1f28
Thunderblade73 <gaidermarkus@gmail.com>
2024-02-03 23:07:28 +0100

    ChatUtils added

--- src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
@@ -6,10 +6,10 @@ import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.MayorElection
 import at.hannibal2.skyhanni.data.TitleManager
 import at.hannibal2.skyhanni.events.GuiContainerEvent
-import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
 import at.hannibal2.skyhanni.mixins.transformers.AccessorGuiEditSign
 import at.hannibal2.skyhanni.test.TestBingo
+import at.hannibal2.skyhanni.utils.ChatUtils.lastButtonClicked
 import at.hannibal2.skyhanni.utils.LanguageUtils.sortedDesc
 import at.hannibal2.skyhanni.utils.NEUItems.getItemStackOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
@@ -23,22 +23,16 @@ import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.inventory.GuiEditSign
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.SharedMonsterAttributes
-import net.minecraft.event.ClickEvent
-import net.minecraft.event.HoverEvent
 import net.minecraft.launchwrapper.Launch
 import net.minecraft.util.ChatComponentText
 import net.minecraftforge.fml.common.FMLCommonHandler
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.awt.Color
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
-import java.util.LinkedList
-import java.util.Queue
 import java.util.Timer
 import java.util.TimerTask
 import java.util.regex.Matcher
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 
 object LorenzUtils {
@@ -73,98 +67,6 @@ object LorenzUtils {
     val isIronmanProfile get() = inSkyBlock && HypixelData.ironman
 
     val lastWorldSwitch get() = HypixelData.joinedWorld
-
-    // TODO log based on chat category (error, warning, debug, user error, normal)
-    private val log = LorenzLogger("chat/mod_sent")
-    var lastButtonClicked = 0L
-
-    private const val DEBUG_PREFIX = "[SkyHanni Debug] §7"
-    private const val USER_ERROR_PREFIX = "§c[SkyHanni] "
-    private val ERROR_PREFIX by lazy { "§c[SkyHanni-${SkyHanniMod.version}] " }
-    private const val CHAT_PREFIX = "[SkyHanni] "
-
-    /**
-     * Sends a debug message to the chat and the console.
-     * This is only sent if the debug feature is enabled.
-     *
-     * @param message The message to be sent
-     *
-     * @see DEBUG_PREFIX
-     */
-    fun debug(message: String) {
-        if (SkyHanniMod.feature.dev.debug.enabled && internalChat(DEBUG_PREFIX + message)) {
-            consoleLog("[Debug] $message")
-        }
-    }
-
-    /**
-     * Sends a message to the user that they did something incorrectly.
-     * We should tell them what to do instead as well.
-     *
-     * @param message The message to be sent
-     *
-     * @see USER_ERROR_PREFIX
-     */
-    fun userError(message: String) {
-        internalChat(USER_ERROR_PREFIX + message)
-    }
-
-    /**
-     * Sends a message to the user that an error occurred caused by something in the code.
-     * This should be used for errors that are not caused by the user.
-     *
-     * Why deprecate this? Even if this message is descriptive for the user and the developer,
-     * we don't want inconsitencies in errors, and we would need to search
-     * for the code line where this error gets printed any way.
-     * so it's better to use the stack trace still.
-     *
-     * @param message The message to be sent
-     * @param prefix Whether to prefix the message with the error prefix, default true
-     *
-     * @see ERROR_PREFIX
-     */
-    @Deprecated(
-        "Do not send the user a non clickable non stacktrace containing error message.",
-        ReplaceWith("ErrorManager")
-    )
-    fun error(message: String) {
-        println("error: '$message'")
-        internalChat(ERROR_PREFIX + message)
-    }
-
-    /**
-     * Sends a message to the user
-     * @param message The message to be sent
-     * @param prefix Whether to prefix the message with the chat prefix, default true
-     * @param prefixColor Color that the prefix should be, default yellow (§e)
-     *
-     * @see CHAT_PREFIX
-     */
-    fun chat(message: String, prefix: Boolean = true, prefixColor: String = "§e") {
-        if (prefix) {
-            internalChat(prefixColor + CHAT_PREFIX + message)
-        } else {
-            internalChat(message)
-        }
-    }
-
-    private fun internalChat(message: String): Boolean {
-        log.log(message)
-        val minecraft = Minecraft.getMinecraft()
-        if (minecraft == null) {
-            consoleLog(message.removeColor())
-            return false
-        }
-
-        val thePlayer = minecraft.thePlayer
-        if (thePlayer == null) {
-            consoleLog(message.removeColor())
-            return false
-        }
-
-        thePlayer.addChatMessage(ChatComponentText(message))
-        return true
-    }
 
     fun SimpleDateFormat.formatCurrentTime(): String = this.format(System.currentTimeMillis())
 
@@ -287,78 +189,6 @@ object LorenzUtils {
         val index = gui.editLine
         val text = lines[index].unformattedText + addedText
         lines[index] = ChatComponentText(text.capAtMinecraftLength(91))
-    }
-
-    /**
-     * Sends a message to the user that they can click and run a command
-     * @param message The message to be sent
-     * @param command The command to be executed when the message is clicked
-     * @param prefix Whether to prefix the message with the chat prefix, default true
-     * @param prefixColor Color that the prefix should be, default yellow (§e)
-     *
-     * @see CHAT_PREFIX
-     */
-    fun clickableChat(message: String, command: String, prefix: Boolean = true, prefixColor: String = "§e") {
-        val msgPrefix = if (prefix) prefixColor + CHAT_PREFIX else ""
-        val text = ChatComponentText(msgPrefix + message)
-        val fullCommand = "/" + command.removePrefix("/")
-        text.chatStyle.chatClickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, fullCommand)
-        text.chatStyle.chatHoverEvent =
-            HoverEvent(HoverEvent.Action.SHOW_TEXT, ChatComponentText("§eExecute $fullCommand"))
-        Minecraft.getMinecraft().thePlayer.addChatMessage(text)
-    }
-
-    /**
-     * Sends a message to the user that they can click and run a command
-     * @param message The message to be sent
-     * @param hover The message to be shown when the message is hovered
-     * @param command The command to be executed when the message is clicked
-     * @param prefix Whether to prefix the message with the chat prefix, default true
-     * @param prefixColor Color that the prefix should be, default yellow (§e)
-     *
-     * @see CHAT_PREFIX
-     */
-    fun hoverableChat(
-        message: String,
-        hover: List<String>,
-        command: String? = null,
-        prefix: Boolean = true,
-        prefixColor: String = "§e",
-    ) {
-        val msgPrefix = if (prefix) prefixColor + CHAT_PREFIX else ""
-        val text = ChatComponentText(msgPrefix + message)
-        text.chatStyle.chatHoverEvent =
-            HoverEvent(HoverEvent.Action.SHOW_TEXT, ChatComponentText(hover.joinToString("\n")))
-
-        command?.let {
-            text.chatStyle.chatClickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/${it.removePrefix("/")}")
-        }
-
-        Minecraft.getMinecraft().thePlayer.addChatMessage(text)
-    }
-
-    private var lastMessageSent = SimpleTimeMark.farPast()
-    private val sendQueue: Queue<String> = LinkedList()
-
-    @SubscribeEvent
-    fun sendQueuedChatMessages(event: LorenzTickEvent) {
-        val player = Minecraft.getMinecraft().thePlayer
-        if (player == null) {
-            sendQueue.clear()
-            return
-        }
-        if (lastMessageSent.passedSince() > 300.milliseconds) {
-            player.sendChatMessage(sendQueue.poll() ?: return)
-            lastMessageSent = SimpleTimeMark.now()
-        }
-    }
-
-    fun sendCommandToServer(command: String) {
-        sendMessageToServer("/$command")
-    }
-
-    fun sendMessageToServer(message: String) {
-        sendQueue.add(message)
     }
 
     fun colorCodeToRarity(colorCode: Char): String {
