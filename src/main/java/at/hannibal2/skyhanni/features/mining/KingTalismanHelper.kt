0881cf54badc7330591102914a33d344c156564d
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-02-05 11:00:44 -0500

    ok what the fuck,
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/mining/KingTalismanHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/KingTalismanHelper.kt
@@ -134,10 +134,10 @@ class KingTalismanHelper {
                 val current = king == currentKing
 
                 val missingTimeFormat = if (current) {
-                    val time = Duration.format()
+                    val time = //TimeUtils.formatDuration(timeUntil - 1000 * 60 * 20 * (kingCircles.size - 1))
                     "§7(§b$time remaining§7)"
                 } else {
-                    val time = Duration.format()
+                    val time = //TimeUtils.formatDuration(timeUntil, maxUnits = 2)
                     "§7(§bin $time§7)"
                 }
 
