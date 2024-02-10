8256b6fc0c7046585ec8975f0bb113a4ee1c632a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-02-10 21:36:29 +0100

    .

--- src/main/java/at/hannibal2/skyhanni/config/features/gui/customscoreboard/PartyConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/gui/customscoreboard/PartyConfig.java
@@ -1,6 +1,5 @@
 package at.hannibal2.skyhanni.config.features.gui.customscoreboard;
 
-import at.hannibal2.skyhanni.config.FeatureToggle;
 import com.google.gson.annotations.Expose;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.moulberry.moulconfig.annotations.ConfigEditorSlider;
@@ -9,7 +8,7 @@ import io.github.moulberry.moulconfig.observer.Property;
 
 public class PartyConfig {
     @Expose
-    @ConfigOption(name = "Max Party List", desc = "Max number of party members to show in the party list. (You are not included)")
+    @ConfigOption(name = "Max Party List", desc = "Max number of party members to show in the party list (You are not included).")
     @ConfigEditorSlider(
         minValue = 0,
         maxValue = 25, // why do I even set it so high
@@ -18,7 +17,7 @@ public class PartyConfig {
     public Property<Integer> maxPartyList = Property.of(4);
 
     @Expose
-    @ConfigOption(name = "Show Party everywhere", desc = "Show the party list everywhere.\nIf disabled, it will only show in Dungeon hub, Crimson Isle & Kuudra")
+    @ConfigOption(name = "Show Party everywhere", desc = "Show the party list everywhere.\nIf disabled, it will only show in Dungeon hub, Crimson Isle & Kuudra.")
     @ConfigEditorBoolean
     public boolean showPartyEverywhere = false;
 }
