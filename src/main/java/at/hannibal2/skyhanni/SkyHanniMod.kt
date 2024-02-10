1af3c89a8daf1eac3921c8b26a49e379f0d18f08
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-02-10 02:31:59 +0100

    Version 0.23 Beta 18

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -384,7 +384,7 @@ import org.apache.logging.log4j.Logger
     clientSideOnly = true,
     useMetadata = true,
     guiFactory = "at.hannibal2.skyhanni.config.ConfigGuiForgeInterop",
-    version = "0.23.Beta.17",
+    version = "0.23.Beta.18",
 )
 class SkyHanniMod {
 
