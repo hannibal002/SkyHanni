0e95b82bb62d62c21acfe8a904db6e75e211199d
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2024-02-16 11:39:53 +0100

    Cal's suggestion
    Co-authored-by: CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/data/PetAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/data/PetAPI.kt
@@ -5,22 +5,21 @@ import at.hannibal2.skyhanni.utils.StringUtils.matches
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 object PetAPI {
-    private val group = RepoPattern.group("misc.pet")
-    private val petMenuPattern by group.pattern(
+    private val patternGroup = RepoPattern.group("misc.pet")
+    private val petMenuPattern by patternGroup.pattern(
         "menu.title",
         "Pets(?: \\(\\d+/\\d+\\) )?"
     )
-
-    private val petItemName by group.pattern(
+    private val petItemName by patternGroup.pattern(
         "item.name",
         "§.\\[Lvl (?<level>\\d)] (?<name>.*)"
     )
-
-    private val neuRepoPetItemName by group.pattern(
+    private val neuRepoPetItemName by patternGroup.pattern(
         "item.name.neu.format",
         "(§f§f)?§7\\[Lvl 1➡(100|200)] (?<name>.*)"
     )
 
+``` To better match the format of patterns in the rest of the mod
     fun isPetMenu(inventoryTitle: String): Boolean = petMenuPattern.matches(inventoryTitle)
 
     // Contains color code + name and for older SkyHanni users maybe also the pet level
