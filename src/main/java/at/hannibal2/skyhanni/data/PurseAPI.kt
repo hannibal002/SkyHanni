641e965951d645c39d2d3877fc684e89d0271c80
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-02-17 00:22:11 +0100

    Fixed Purse Pattern (#1017)
    * Updated pattern
    
    * Update PurseAPI.kt

--- src/main/java/at/hannibal2/skyhanni/data/PurseAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/data/PurseAPI.kt
@@ -15,7 +15,7 @@ object PurseAPI {
     private val patternGroup = RepoPattern.group("data.purse")
     private val coinsPattern by patternGroup.pattern(
         "coins",
-        "(Piggy|Purse): §6(?<coins>[\\d,]+).*"
+        "(§.)*(Piggy|Purse): §6(?<coins>[\\d,]+(\\.\\d)?)( ?(§.)*\\([+-](?<earned>[\\w,.]+)\\)?|.*)?$"
     )
     val piggyPattern by patternGroup.pattern(
         "piggy",
