dcca651280a3720bf9031d4509ef05071a004c57
Thunderblade73 <gaidermarkus@gmail.com>
2024-02-19 20:52:11 +0100

    in hand

--- src/main/java/at/hannibal2/skyhanni/config/features/combat/EnderNodeConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/combat/EnderNodeConfig.java
@@ -44,7 +44,7 @@ public class EnderNodeConfig {
     @Expose
     @ConfigOption(
         name = "Only While Holding Tool",
-        desc = "Only shows the tracker if holding a pickaxe, drill or gauntlet."
+        desc = "Only shows the tracker if holding a pickaxe, drill or gauntlet in hand."
     )
     @ConfigEditorBoolean
     public boolean onlyPickaxe = false;
