16d6a47c85be560517505499df1fff6df9582c18
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2024-02-25 08:15:04 +0100

    Fixed internal name resolving. #1046

--- src/main/java/at/hannibal2/skyhanni/utils/NEUInternalName.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/NEUInternalName.kt
@@ -13,11 +13,14 @@ class NEUInternalName private constructor(private val internalName: String) {
         val SKYBLOCK_COIN = "SKYBLOCK_COIN".asInternalName()
 
         fun String.asInternalName(): NEUInternalName {
-            val internalName = uppercase()
+            val internalName = uppercase().replace(" ", "_")
             return map.getOrPut(internalName) { NEUInternalName(internalName) }
         }
 
-        fun fromItemName(itemName: String) = NEUItems.getInternalNameFromItemName(itemName)
+        fun fromItemNameOrNull(itemName: String): NEUInternalName? = ItemNameResolver.getInternalNameOrNull(itemName)
+
+        fun fromItemName(itemName: String): NEUInternalName =
+            fromItemNameOrNull(itemName) ?: throw Error("NEUInternalName is null for item name '$itemName'")
     }
 
     fun asString() = internalName
