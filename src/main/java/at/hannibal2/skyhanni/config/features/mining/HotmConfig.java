d954a740bcf55fcd12bca7e237f584527dc0656f
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-02-25 18:16:48 +0100

    .

--- src/main/java/at/hannibal2/skyhanni/config/features/mining/HotmConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/HotmConfig.java
@@ -8,19 +8,19 @@ import io.github.moulberry.moulconfig.annotations.ConfigOption;
 public class HotmConfig {
 
     @Expose
-    @ConfigOption(name = "Enabled Highlight", desc = "Highlights enabled perks in the hotm tree green and disabled red. Not unlocked perks are highlighted gray")
+    @ConfigOption(name = "Enabled Highlight", desc = "Highlights enabled perks in the hotm tree green and disabled red. Not unlocked perks are highlighted gray.")
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean highlightEnabledPerks = true;
 
     @Expose
-    @ConfigOption(name = "Level Stack", desc = "Shows the level of a perk as item stacks")
+    @ConfigOption(name = "Level Stack", desc = "Shows the level of a perk as item stacks.")
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean levelStackSize = true;
 
     @Expose
-    @ConfigOption(name = "Token Stack", desc = "Shows unused tokens on the heart")
+    @ConfigOption(name = "Token Stack", desc = "Shows unused tokens on the heart.")
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean tokenStackSize = true;
