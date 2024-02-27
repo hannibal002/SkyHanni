347c0714c906a8391c31e45e740d05a9e31cac85
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2024-02-27 19:37:29 +0100

    Fixed skill timer. (#1070)

--- src/main/java/at/hannibal2/skyhanni/features/skillprogress/SkillProgress.kt
+++ src/main/java/at/hannibal2/skyhanni/features/skillprogress/SkillProgress.kt
@@ -359,7 +359,6 @@ object SkillProgress {
         add(Renderable.clickAndHover("§7Session: §e$session ${if (xpInfo.sessionTimerActive) "" else "§c(PAUSED)"}",
             listOf("§eClick to reset!")) {
             xpInfo.sessionTimerActive = false
-            activeSkill.timer = null
             xpInfo.timeActive = 0L
             chat("Timer for §b${activeSkill.displayName} §ehas been reset!")
         })
