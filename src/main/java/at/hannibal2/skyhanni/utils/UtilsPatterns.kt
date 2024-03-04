269a89bcfec9e3cfbd14f0a6814c6e365df3f308
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-03-04 12:40:37 +0100

    fixing SkyHanni features not working on alpha

--- src/main/java/at/hannibal2/skyhanni/utils/UtilsPatterns.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/UtilsPatterns.kt
@@ -80,6 +80,6 @@ object UtilsPatterns {
 
     val tabListProfilePattern by patternGroup.pattern(
         "tablist.profile",
-        "§.§lProfile: §r§a(?<profile>.*)"
+        "(?:§l§r)?§.§lProfile: §r§a(?<profile>.*)"
     )
 }
