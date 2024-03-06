67c3ed59126d658956d522e25e004c1c963c5006
ILike2WatchMemes <ilike2watchmemes@gmail.com>
2024-03-06 16:09:53 +0100

    Update MovementSpeedDisplay.kt

--- src/main/java/at/hannibal2/skyhanni/features/misc/MovementSpeedDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/MovementSpeedDisplay.kt
@@ -1,42 +1,48 @@
 package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.config.enums.OutsideSbFeature
 import at.hannibal2.skyhanni.events.GuiRenderEvent
-import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils.round
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.RenderUtils.renderString
 import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import kotlin.concurrent.fixedRateTimer
 
 class MovementSpeedDisplay {
 
     private val config get() = SkyHanniMod.feature.misc
+    private var display = ""
 
-    @SubscribeEvent
-    fun onTick(event: LorenzTickEvent) {
+    init {
+        fixedRateTimer(name = "skyhanni-movement-speed-display", period = 250, initialDelay = 1_000) {
+            checkSpeed()
+        }
+    }
+
+    private fun checkSpeed() {
         if (!isEnabled()) return
 
-        currentBPS = with(Minecraft.getMinecraft().thePlayer) {
+        val distance = with(Minecraft.getMinecraft().thePlayer) {
             val oldPos = LorenzVec(prevPosX, prevPosY, prevPosZ)
             val newPos = LorenzVec(posX, posY, posZ)
 
             // Distance from previous tick, multiplied by TPS
-            (oldPos.distance(newPos) * 20).round(2)
+            oldPos.distance(newPos) * 20
         }
+        display = "Movement Speed: ${distance.round(2)}"
     }
 
     @SubscribeEvent
     fun onRenderOverlay(event: GuiRenderEvent.GuiOverlayRenderEvent) {
         if (!isEnabled()) return
 
-        config.playerMovementSpeedPos.renderString("Movement Speed: $currentBPS", posLabel = "Movement Speed")
+        config.playerMovementSpeedPos.renderString(display, posLabel = "Movement Speed")
     }
 
-    fun isEnabled() = LorenzUtils.inSkyBlock && config.playerMovementSpeed
-
-    companion object {
-        var currentBPS: Double = 0.0
-    }
+    fun isEnabled() = LorenzUtils.onHypixel &&
+        (LorenzUtils.inSkyBlock || OutsideSbFeature.MOVEMENT_SPEED.isSelected()) &&
+        config.playerMovementSpeed
 }
