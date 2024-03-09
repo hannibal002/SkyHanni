75ad698ea80f854abea0bc5306ebc20873d580c8
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-03-09 12:20:51 +0100

    cleanup MarkedPlayerManager

--- src/main/java/at/hannibal2/skyhanni/features/misc/MarkedPlayerManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/MarkedPlayerManager.kt
@@ -6,8 +6,8 @@ import at.hannibal2.skyhanni.events.ConfigLoadEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.LorenzWorldChangeEvent
 import at.hannibal2.skyhanni.events.RenderMobColoredEvent
-import at.hannibal2.skyhanni.events.ResetEntityHurtEvent
 import at.hannibal2.skyhanni.events.withAlpha
+import at.hannibal2.skyhanni.mixins.hooks.RenderLivingEntityHelper
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.EntityUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils
@@ -16,9 +16,9 @@ import net.minecraft.client.entity.EntityOtherPlayerMP
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class MarkedPlayerManager {
-    private val config get() = SkyHanniMod.feature.markedPlayers
-
     companion object {
+
+        private val config get() = SkyHanniMod.feature.markedPlayers
 
         val playerNamesToMark = mutableListOf<String>()
         private val markedPlayers = mutableMapOf<String, EntityOtherPlayerMP>()
@@ -44,6 +44,7 @@ class MarkedPlayerManager {
                 ChatUtils.chat("§aMarked §eplayer §b$displayName§e!")
             } else {
                 playerNamesToMark.remove(name)
+                markedPlayers[name]?.let { RenderLivingEntityHelper.removeNoHurtTime(it) }
                 markedPlayers.remove(name)
                 ChatUtils.chat("§cUnmarked §eplayer §b$displayName§e!")
             }
@@ -56,11 +57,15 @@ class MarkedPlayerManager {
                 val name = entity.name.lowercase()
                 if (name in playerNamesToMark) {
                     markedPlayers[name] = entity
+                    RenderLivingEntityHelper.setNoHurtTime(entity, ::isEnabled)
                 }
             }
         }
 
         fun isMarkedPlayer(player: String): Boolean = player.lowercase() in playerNamesToMark
+
+        private fun isEnabled() = (LorenzUtils.inSkyBlock || OutsideSbFeature.MARKED_PLAYERS.isSelected())
+            && config.highlightInWorld
     }
 
     @SubscribeEvent
@@ -97,22 +102,10 @@ class MarkedPlayerManager {
     }
 
     @SubscribeEvent
-    fun onResetEntityHurtTime(event: ResetEntityHurtEvent) {
-        if (!isEnabled()) return
-
-        val entity = event.entity
-        if (entity in markedPlayers.values) {
-            event.shouldReset = true
-        }
-    }
-
-    private fun isEnabled() = (LorenzUtils.inSkyBlock || OutsideSbFeature.MARKED_PLAYERS.isSelected())
-        && config.highlightInWorld
-
-    @SubscribeEvent
     fun onWorldChange(event: LorenzWorldChangeEvent) {
         if (Minecraft.getMinecraft().thePlayer == null) return
 
+        markedPlayers.forEach { RenderLivingEntityHelper.removeNoHurtTime(it.value) }
         markedPlayers.clear()
         if (config.markOwnName.get()) {
             val name = LorenzUtils.getPlayerName()
