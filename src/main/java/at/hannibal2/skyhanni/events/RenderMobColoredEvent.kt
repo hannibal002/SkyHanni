e20f875630703871cc1c2e95d3474365f9504ffe
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-03-09 12:55:39 +0100

    added deprecation notice

--- src/main/java/at/hannibal2/skyhanni/events/RenderMobColoredEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/RenderMobColoredEvent.kt
@@ -3,4 +3,5 @@ package at.hannibal2.skyhanni.events
 import net.minecraft.entity.EntityLivingBase
 
 // TODO remove
+@Deprecated("use RenderLivingHelper .setEntityColor && .removeEntityColor")
 class RenderMobColoredEvent(val entity: EntityLivingBase, var color: Int) : LorenzEvent()
