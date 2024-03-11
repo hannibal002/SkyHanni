519803466655a132e7750f1b338a2438d96b65a6
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-03-11 18:05:35 +0100

    fix merge conflicts

--- src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
@@ -10,7 +10,6 @@ import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
 import at.hannibal2.skyhanni.mixins.transformers.AccessorGuiEditSign
 import at.hannibal2.skyhanni.test.TestBingo
 import at.hannibal2.skyhanni.utils.ChatUtils.lastButtonClicked
-import at.hannibal2.skyhanni.utils.CollectionUtils.sortedDesc
 import at.hannibal2.skyhanni.utils.ItemUtils.getItemCategoryOrNull
 import at.hannibal2.skyhanni.utils.NEUItems.getItemStackOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
@@ -158,11 +157,11 @@ object LorenzUtils {
     fun getPlayerName(): String = Minecraft.getMinecraft().thePlayer.name
 
     fun MutableList<List<Any>>.fillTable(data: List<DisplayTableEntry>) {
-        val keys = data.associateWith { it.sort }.sortedDesc().keys
+        val sorted = data.sortedByDescending { it.sort }
         val renderer = Minecraft.getMinecraft().fontRendererObj
-        val longest = keys.map { it.left }.maxOfOrNull { renderer.getStringWidth(it.removeColor()) } ?: 0
+        val longest = sorted.maxOfOrNull { renderer.getStringWidth(it.left.removeColor()) } ?: 0
 
-        for (entry in data) {
+        for (entry in sorted) {
             var displayName = entry.left
             while (renderer.getStringWidth(displayName.removeColor()) < longest) {
                 displayName += " "
@@ -368,6 +367,8 @@ object LorenzUtils {
     @Deprecated("moved", ReplaceWith("ChatUtils.sendMessageToServer(message)"))
     fun sendMessageToServer(message: String) = ChatUtils.sendMessageToServer(message)
 
+    fun inAdvancedMiningIsland() = IslandType.DWARVEN_MINES.isInIsland() || IslandType.CRYSTAL_HOLLOWS.isInIsland()
+
     fun inMiningIsland() = IslandType.GOLD_MINES.isInIsland() || IslandType.DEEP_CAVERNS.isInIsland()
-        || IslandType.DWARVEN_MINES.isInIsland() || IslandType.CRYSTAL_HOLLOWS.isInIsland()
+        || inAdvancedMiningIsland()
 }
