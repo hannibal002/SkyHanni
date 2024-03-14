3e43c375e136615098c8064591402dcaa71e99ea
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-03-14 16:55:25 +0100

    Removed scoreboard replacement line

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/ScoreboardElements.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/ScoreboardElements.kt
@@ -18,7 +18,6 @@ import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.Comp
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.Companion.informationFilteringConfig
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.formatNum
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getGroupFromPattern
-import at.hannibal2.skyhanni.mixins.hooks.tryToReplaceScoreboardLine
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.CollectionUtils.nextAfter
 import at.hannibal2.skyhanni.utils.LorenzUtils.inAdvancedMiningIsland
@@ -389,15 +388,11 @@ private fun getIslandDisplayPair() =
 
 private fun getLocationDisplayPair() = buildList {
     add(
-        (tryToReplaceScoreboardLine(
-            getGroupFromPattern(
-                ScoreboardData.sidebarLinesFormatted,
-                ScoreboardPattern.locationPattern,
-                "location"
-            )
-        )?.trim()
-            ?: "<hidden>") to HorizontalAlignment.LEFT
-    )
+        getGroupFromPattern(
+            ScoreboardData.sidebarLinesFormatted,
+            ScoreboardPattern.locationPattern,
+            "location"
+        ).trim() to HorizontalAlignment.LEFT)
 
     val plotLine = ScoreboardData.sidebarLinesFormatted.firstOrNull { ScoreboardPattern.plotPattern.matches(it) }
     if (plotLine != null) add(plotLine to HorizontalAlignment.LEFT)
