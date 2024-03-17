98d2b9e306228ff3c05ac2af26f382c07eba5571
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-03-17 12:31:00 +0100

    formatting

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/neu/NeuRNGScore.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/neu/NeuRNGScore.kt
@@ -4,8 +4,6 @@ import at.hannibal2.skyhanni.utils.NEUInternalName
 import com.google.gson.annotations.Expose
 
 data class NeuRNGScore(
-    @Expose
-    val catacombs: Map<String, Map<NEUInternalName, Long>>,
-    @Expose
-    val slayer: Map<String, Map<NEUInternalName, Long>>
+    @Expose val catacombs: Map<String, Map<NEUInternalName, Long>>,
+    @Expose val slayer: Map<String, Map<NEUInternalName, Long>>
 )
