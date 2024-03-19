053b48dc8e56e752d0bf2327d09b795d94b7684e
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-03-19 21:09:59 +0100

    fix slayer cost from bank counting plus instead of minus profit

--- src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerProfitTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerProfitTracker.kt
@@ -112,7 +112,7 @@ object SlayerProfitTracker {
     fun onChat(event: LorenzChatEvent) {
         if (!isEnabled()) return
         autoSlayerBankPattern.matchMatcher(event.message) {
-            addSlayerCosts(group("coins").formatDouble())
+            addSlayerCosts(-group("coins").formatDouble())
         }
     }
 
@@ -180,7 +180,7 @@ object SlayerProfitTracker {
                     listOf("§7You paid §c$mobKillCoinsFormat §7in total", "§7for starting the slayer quests.")
                 )
             )
-            profit += slayerSpawnCost
+            profit -= slayerSpawnCost
         }
 
         val slayerCompletedCount = itemLog.slayerCompletedCount
