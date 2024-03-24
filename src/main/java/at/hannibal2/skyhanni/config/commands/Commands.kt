431150610168dba16ccbd1293a1974b865d98260
martimavocado <martim.cavaco@tutanota.com>
2024-03-24 01:39:05 +0000

    add keywords to shlimbostats' description

--- src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
@@ -289,7 +289,7 @@ object Commands {
         )
         registerCommand(
             "shlimbostats",
-            "Prints your Limbo Stats!"
+            "Prints your Limbo Stats.\n §7This includes your Personal Best, Playtime, and §aSkyHanni User Luck§7!"
         ) { LimboTimeTracker.printStats() }
         registerCommand(
             "shlimbo",
