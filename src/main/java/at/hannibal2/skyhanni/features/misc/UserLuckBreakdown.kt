1e3e6fcf82258bc582250dc55b39611ad6c73b0a
martimavocado <martim.cavaco@tutanota.com>
2024-03-27 13:26:11 +0000

    add custom breakdown menu

--- src/main/java/at/hannibal2/skyhanni/features/misc/UserLuckBreakdown.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/UserLuckBreakdown.kt
@@ -13,6 +13,7 @@ import at.hannibal2.skyhanni.utils.NEUItems.getItemStack
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import io.github.moulberry.notenoughupdates.events.ReplaceItemEvent
+import io.github.moulberry.notenoughupdates.events.SlotClickEvent
 import io.github.moulberry.notenoughupdates.util.Utils
 import net.minecraft.client.player.inventory.ContainerLocalMenu
 import net.minecraft.item.ItemStack
@@ -26,59 +27,102 @@ class UserLuckBreakdown {
 
     private val storage get() = ProfileStorageData.playerSpecific
 
-    private lateinit var luckItem: ItemStack
-    private val itemID = "ENDER_PEARL".asInternalName()
-    private val itemName = "§a✴ SkyHanni User Luck"
+    private lateinit var mainLuckItem: ItemStack
+    private val mainLuckID = "ENDER_PEARL".asInternalName().getItemStack().item
+    private val mainLuckName = "§a✴ SkyHanni User Luck"
 
-    private var isAdvanced = true
-    private val isAdvancedPattern = RepoPattern.pattern(
-        "misc.statsbreakdown.advanced",
+    private lateinit var fillerItem: ItemStack
+    private var fillerID = "STAINED_GLASS_PANE".asInternalName().getItemStack().item
+    private val fillerName = " "
+
+    private lateinit var limboItem: ItemStack
+    private var limboID = "ENDER_PEARL".asInternalName().getItemStack().item
+    private val limboName = "§a✴ Limbo Personal Best"
+
+    private var showAllStats = true
+    private val showAllStatsPattern = RepoPattern.pattern(
+        "misc.statsbreakdown.showallstats",
         "§7Show all stats: §.(?<toggle>.*)"
     )
 
     private val luckTooltipString = "§5§o §a✴ SkyHanni User Luck §f"
+    private var inCustomBreakdown = false
 
-    private val validItemSlots = (10..43).filter { it !in listOf(17, 18, 26, 27, 35, 36) }
+    private val validItemSlots = (10..53).filter { it !in listOf(17, 18, 26, 27, 35, 36) && it !in 44..53 }
+    private val invalidItemSlots = (0..53).filter { it !in validItemSlots }
 
     @SubscribeEvent
     fun replaceItem(event: ReplaceItemEvent) {
         if (event.inventory !is ContainerLocalMenu) return
         if (!inMiscStats) return
-        if (event.slotNumber != replaceSlot) return
 
-        if (event.slotNumber == replaceSlot) {
+        if (event.slotNumber == replaceSlot && !inCustomBreakdown) {
             val limboUserLuck = storage?.limbo?.userLuck ?: 0.0f
-            if (limboUserLuck == 0.0f && !isAdvanced) return
+            if (limboUserLuck == 0.0f && !showAllStats) return
             if (itemCreateCoolDown.passedSince() > 3.seconds) {
                 itemCreateCoolDown = SimpleTimeMark.now()
-                val luckString = tryTruncateFloat(limboUserLuck.round(1))
-                luckItem = Utils.createItemStack(
-                    itemID.getItemStack().item,
-                    "$itemName §f$luckString",
-                    *createItemLore(limboUserLuck)
-                )
+                createItems()
             }
-            event.replaceWith(luckItem)
+            event.replaceWith(mainLuckItem)
+            return
+        }
+        if (inCustomBreakdown) {
+            if (itemCreateCoolDown.passedSince() > 3.seconds) {
+                itemCreateCoolDown = SimpleTimeMark.now()
+                createItems()
+            }
+            when (event.slotNumber) {
+                48 -> return
+                49 -> return
+                10 -> {
+                    event.replaceWith(limboItem)
+                    return
+                }
+                in validItemSlots -> {
+                    event.replaceWith(null)
+                    return
+                }
+                in invalidItemSlots -> {
+                    if (event.original.item == limboID) return
+                    event.replaceWith(fillerItem)
+                    return
+                }
+            }
         }
     }
 
-    private fun createItemLore(luckInput: Float): Array<String> {
-        val luckString = tryTruncateFloat(luckInput.round(2))
-        return if (luckInput == 0.0f) { arrayOf(
-                "§7SkyHanni User Luck is the best stat.",
-                "",
-                "§7Flat: §a+$luckString✴",
-                "",
-                "§8You have none of this stat!",
-                "§cDON'T §eclick to view!"
-            )
-        } else { arrayOf(
-                "§7SkyHanni User Luck is the best stat.",
-                "",
-                "§7Flat: §a+$luckString✴",
-                "",
-                "§cDON'T §eclick to view!"
-            )
+    private fun createItemLore(type: String, luckInput: Float): Array<String> {
+        when (type) {
+            "mainMenu" -> {
+                val luckString = tryTruncateFloat(luckInput.round(2))
+                return if (luckInput == 0.0f) { arrayOf(
+                    "§7SkyHanni User Luck is the best stat.",
+                    "",
+                    "§7Flat: §a+$luckString✴",
+                    "",
+                    "§8You have none of this stat!",
+                    "§eClick to view!"
+                )
+                } else { arrayOf(
+                    "§7SkyHanni User Luck is the best stat.",
+                    "",
+                    "§7Flat: §a+$luckString✴",
+                    "",
+                    "§eClick to view!"
+                )
+                }
+            }
+            "limbo" -> {
+                val luckString = tryTruncateFloat(luckInput.round(2))
+                return arrayOf(
+                    "§8Action",
+                    "",
+                    "§7Value: §a+$luckString✴",
+                    "",
+                    "§8Gain more by going to Limbo,",
+                    "§8and obtaining a higher §6Personal Best§8."
+                )
+            } else -> return arrayOf("")
         }
     }
 
@@ -94,9 +138,9 @@ class UserLuckBreakdown {
         replaceSlot = findValidSlot(event.inventoryItems)
         val isAdvancedLore = event.inventoryItems[50]?.getLore() ?: listOf("")
         isAdvancedLore.forEach {
-            val matcher = isAdvancedPattern.value.matcher(it)
+            val matcher = showAllStatsPattern.value.matcher(it)
             if (matcher.find()) {
-                isAdvanced = when (matcher.group("toggle")) {
+                showAllStats = when (matcher.group("toggle")) {
                     "Yes" -> true
                     "Nope" -> false
                     else -> false
@@ -109,11 +153,12 @@ class UserLuckBreakdown {
     @SubscribeEvent
     fun closeInventory(event: InventoryCloseEvent) {
         inMiscStats = false
+        inCustomBreakdown = false
     }
 
     private fun findValidSlot(input: Map<Int, ItemStack>): Int {
         for (slot in input.keys) {
-            if (slot !in validItemSlots) continue
+            if (slot !in validItemSlots && slot < 44) continue
             val itemStack = input[slot]
             if (itemStack?.name == " ") {
                 return slot
@@ -129,7 +174,7 @@ class UserLuckBreakdown {
             "Your Equipment and Stats" -> {
                 if (event.slot.slotIndex != 25) return
                 val limboUserLuck = storage?.limbo?.userLuck ?: 0.0f
-                if (limboUserLuck == 0.0f && !isAdvanced) return
+                if (limboUserLuck == 0.0f && !showAllStats) return
 
                 val lastIndex = event.toolTip.indexOfLast { it == "§5§o" }
                 if (lastIndex == -1) return
@@ -139,9 +184,12 @@ class UserLuckBreakdown {
             }
             "Your Stats Breakdown" -> {
                 if (!inMiscStats) return
+                if (inCustomBreakdown && event.slot.slotIndex == 48) {
+                    event.toolTip[1] = "§7To Your Stats Breakdown"
+                }
                 if (event.slot.slotIndex != 4) return
                 val limboUserLuck = storage?.limbo?.userLuck ?: 0.0f
-                if (limboUserLuck == 0.0f && !isAdvanced) return
+                if (limboUserLuck == 0.0f && !showAllStats) return
 
                 val luckString = tryTruncateFloat(limboUserLuck.round(1))
                 event.toolTip.add("§5§o §a✴ SkyHanni User Luck §f$luckString")
@@ -165,5 +213,46 @@ class UserLuckBreakdown {
         val string = input.toString()
         return if (string.endsWith(".0")) return string.dropLast(2)
         else string
+    }
+
+    @SubscribeEvent
+    fun onStackClick(event: SlotClickEvent) {
+        if (!inMiscStats) return
+        val limboUserLuck = storage?.limbo?.userLuck ?: 0.0f
+        if (limboUserLuck == 0.0f && !showAllStats) return
+
+        if (inCustomBreakdown && event.slotId != 49) event.cancel()
+        when (event.slotId) {
+            replaceSlot -> {
+                if (inCustomBreakdown) return
+                event.cancel()
+                inCustomBreakdown = true
+            }
+            48 -> {
+                if (!inCustomBreakdown) return
+                inCustomBreakdown = false
+            }
+            else -> return
+        }
+    }
+
+    private fun createItems() {
+        val limboUserLuck = storage?.limbo?.userLuck ?: 0.0f
+        fillerItem = Utils.createItemStack(
+            fillerID,
+            fillerName,
+            15,
+        )
+        val luckString = tryTruncateFloat(limboUserLuck.round(1))
+        mainLuckItem = Utils.createItemStack(
+            mainLuckID,
+            "$mainLuckName §f$luckString",
+            *createItemLore("mainMenu", limboUserLuck)
+        )
+        limboItem = Utils.createItemStack(
+            limboID,
+            limboName,
+            *createItemLore("limbo", limboUserLuck)
+        )
     }
 }
