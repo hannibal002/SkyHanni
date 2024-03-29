d54e1bb0625c8d786a687707371f31e4ac62c9d0
Linnea Gräf <nea@nea.moe>
2024-03-29 23:10:54 +0100

    Add persian SkyHanni support (#1310)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
@@ -28,6 +28,8 @@ import net.minecraft.util.ChatComponentText
 import net.minecraftforge.fml.common.FMLCommonHandler
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Month
 import java.util.Timer
 import java.util.TimerTask
 import java.util.regex.Matcher
@@ -66,6 +68,11 @@ object LorenzUtils {
     val isIronmanProfile get() = inSkyBlock && HypixelData.ironman
 
     val lastWorldSwitch get() = HypixelData.joinedWorld
+
+    val isAprilFoolsDay: Boolean
+        get() = SkyHanniMod.feature.dev.debug.alwaysFunnyTime || LocalDate.now().let {
+            it.month == Month.APRIL && it.dayOfMonth == 1
+        }
 
     fun SimpleDateFormat.formatCurrentTime(): String = this.format(System.currentTimeMillis())
 
