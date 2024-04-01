737eac07340dbe2246c15418e8d6825ce5c2c29e
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-01 21:17:32 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/features/event/diana/HighlightInquisitors.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/diana/HighlightInquisitors.kt
@@ -2,8 +2,8 @@ package at.hannibal2.skyhanni.features.event.diana
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.mixins.hooks.RenderLivingEntityHelper
+import at.hannibal2.skyhanni.utils.ColorUtils.toChromaColorInt
 import at.hannibal2.skyhanni.utils.LorenzUtils
-import at.hannibal2.skyhanni.utils.SpecialColour
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -20,7 +20,7 @@ class HighlightInquisitors {
         val entity = event.entity
 
         if (entity is EntityPlayer && entity.name == "Minos Inquisitor") {
-            val color = SpecialColour.specialToChromaRGB(config.color)
+            val color = config.color.toChromaColorInt()
             RenderLivingEntityHelper.setEntityColorWithNoHurtTime(entity, color) { config.highlightInquisitors }
         }
     }
