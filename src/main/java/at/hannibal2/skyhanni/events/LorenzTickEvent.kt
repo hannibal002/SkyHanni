bd87aa9e103db6bcb7414a60306655e674ceebfc
Cal <cwolfson58@gmail.com>
2024-04-03 18:00:59 +1100

    offset

--- src/main/java/at/hannibal2/skyhanni/events/LorenzTickEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/LorenzTickEvent.kt
@@ -2,7 +2,7 @@ package at.hannibal2.skyhanni.events
 
 class LorenzTickEvent(private val tick: Int) : LorenzEvent() {
 
-    fun isMod(i: Int, seed: Int = 0) = (tick + seed) % i == 0
+    fun isMod(i: Int, offset: Int = 0) = (tick + offset) % i == 0
 
-    fun repeatSeconds(i: Int, seed: Int = 0) = isMod(i * 20, seed)
+    fun repeatSeconds(i: Int, offset: Int = 0) = isMod(i * 20, offset)
 }
