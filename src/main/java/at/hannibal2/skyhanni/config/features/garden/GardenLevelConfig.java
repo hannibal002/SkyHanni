74ecfea0b3e2865e47d7687707bbaee25d6cc434
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-08 18:47:59 +0200

    Add @ConfigLink to many garden guis

--- src/main/java/at/hannibal2/skyhanni/config/features/garden/GardenLevelConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/garden/GardenLevelConfig.java
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.config.FeatureToggle;
 import at.hannibal2.skyhanni.config.core.config.Position;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class GardenLevelConfig {
@@ -14,5 +15,6 @@ public class GardenLevelConfig {
     public boolean display = false;
 
     @Expose
+    @ConfigLink(owner = GardenLevelConfig.class, field = "display")
     public Position pos = new Position(390, 40, false, true);
 }
