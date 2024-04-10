61807bea93e441fd9996f36b7d9f1f9e839918a9
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-04-10 11:28:11 +0200

    test?

--- src/test/java/at/hannibal2/skyhanni/test/RepoPatternTest.kt
+++ src/test/java/at/hannibal2/skyhanni/test/RepoPatternTest.kt
@@ -48,4 +48,46 @@ object RepoPatternTest {
 
         RepoPatternManager.inTestDuplicateUsage = true
     }
+
+    @Test
+    fun testRemoteLoadList() {
+        val simpleLocalePattern1 = "I'm a test value"
+        val simpleLocalePattern2 = "I'm a test value 2"
+
+        val listPatterns = RepoPattern.list("testonly.list.a", simpleLocalePattern1, simpleLocalePattern2)
+        val list by listPatterns
+
+        val remoteValue = "I'm remote."
+
+        val isLocalWorking = list[0].pattern() == simpleLocalePattern1 && list[1].pattern() == simpleLocalePattern2
+
+        assert(isLocalWorking)
+
+        RepoPatternManager.loadPatternsFromDump(
+            RepoPatternDump(
+                regexes = mapOf(
+                    listPatterns.key to remoteValue
+                )
+            )
+        )
+
+        val isRemoteWorking = list.size != 0 && list.all { it.pattern() == remoteValue }
+
+        assert(isRemoteWorking)
+
+    }
+
+    /* @Test
+    fun testExclusivityList() {
+        RepoPatternManager.inTestDuplicateUsage = false
+
+        assertThrows<RuntimeException> {
+            val pattern1 by RepoPattern.pattern("testonly.b", "")
+            val pattern2 by RepoPattern.pattern("testonly.b", "")
+            pattern1
+            pattern2
+        }
+
+        RepoPatternManager.inTestDuplicateUsage = true
+    } */
 }
