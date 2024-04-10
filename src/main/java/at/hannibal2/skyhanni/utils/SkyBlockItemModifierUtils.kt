8e4a4a156b659fcbb26dfec8ff619777771d0c59
Fix3dll <serhanduzce@gmail.com>
2024-04-10 10:19:44 +0300

    Add: New Gemstones to ModifierUtils (#1416)

--- src/main/java/at/hannibal2/skyhanni/utils/SkyBlockItemModifierUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SkyBlockItemModifierUtils.kt
@@ -288,6 +288,10 @@ object SkyBlockItemModifierUtils {
         JASPER("Jasper"),
         RUBY("Ruby"),
         OPAL("Opal"),
+        ONYX("Onyx"),
+        AQUAMARINE("Aquamarine"),
+        CITRINE("Citrine"),
+        PERIDOT("Peridot"),
         ;
 
         companion object {
@@ -305,8 +309,11 @@ object SkyBlockItemModifierUtils {
         JASPER('d'),
         RUBY('c'),
         OPAL('f'),
+        ONYX('8'),
+        AQUAMARINE('3'),
+        CITRINE('4'),
+        PERIDOT('2'),
         COMBAT('4'),
-        OFFENSIVE('9'),
         DEFENSIVE('a'),
         MINING('5'),
         UNIVERSAL('f')
