c52e49c3be13630c872bcb4d2beac13959fbe557
CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>
2024-04-12 15:24:15 +1000

    Feature: Fossil Excavator (#1427)

--- src/main/java/at/hannibal2/skyhanni/config/features/mining/MiningConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/MiningConfig.java
@@ -34,6 +34,11 @@ public class MiningConfig {
     public AreaWallsConfig crystalHollowsAreaWalls = new AreaWallsConfig();
 
     @Expose
+    @ConfigOption(name = "Fossil Excavator", desc = "")
+    @Accordion
+    public FossilExcavatorConfig fossilExcavator = new FossilExcavatorConfig();
+
+    @Expose
     @ConfigOption(name = "Highlight Commission Mobs", desc = "Highlight Mobs that are part of active commissions.")
     @ConfigEditorBoolean
     @FeatureToggle
