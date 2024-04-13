f64a5a336cfb488a5c97190f22dee00d949f42a4
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-13 22:27:21 +0200

    Add Profit Per Excavation

---
+++ src/main/java/at/hannibal2/skyhanni/features/mining/fossilexcavator/ProfitPerExcavation.kt
@@ -0,0 +1,45 @@
+package at.hannibal2.skyhanni.features.mining.fossilexcavator
+
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.mining.FossilExcavationEvent
+import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.CollectionUtils.sortedDesc
+import at.hannibal2.skyhanni.utils.ItemUtils.itemName
+import at.hannibal2.skyhanni.utils.NEUInternalName
+import at.hannibal2.skyhanni.utils.NEUInternalName.Companion.asInternalName
+import at.hannibal2.skyhanni.utils.NEUItems.getPrice
+import at.hannibal2.skyhanni.utils.NumberUtil
+import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+class ProfitPerExcavation {
+    private val config get() = SkyHanniMod.feature.mining.fossilExcavator
+
+    @SubscribeEvent
+    fun onFossilExcavation(event: FossilExcavationEvent) {
+        if (!config.profitPerExcavation) return
+        val loot = event.loot
+
+        val scrapItem = "SUSPICIOUS_SCRAP".asInternalName()
+
+        var totalProfit = 0.0
+        val map = mutableMapOf<String, Double>()
+        loot.forEach { (name, amount) ->
+            NEUInternalName.fromItemNameOrNull(name)?.let {
+                val profit = amount * it.getPrice()
+                val text = "Found $name §8${amount.addSeparators()}x §7(§6${NumberUtil.format(profit)}§7)"
+                map[text] = profit
+                totalProfit += profit
+            }
+        }
+
+        val scrapPrice = scrapItem.getPrice()
+        map["${scrapItem.itemName}: §c-${NumberUtil.format(scrapPrice)}"] = -scrapPrice
+        totalProfit -= scrapPrice
+
+        val hover = map.sortedDesc().keys.toList()
+        val profitPrefix = if (totalProfit < 0) "§c" else "§6"
+        val totalMessage = "Total profit this run: $profitPrefix${NumberUtil.format(totalProfit)}"
+        ChatUtils.hoverableChat(totalMessage, hover)
+    }
+}
