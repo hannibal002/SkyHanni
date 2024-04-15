34e9f01594e498ed16ebe55f51ba990948635456
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-04-15 20:14:49 +0200

    Backend: MultiClickable (#1407)

--- src/main/java/at/hannibal2/skyhanni/utils/renderables/Renderable.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/renderables/Renderable.kt
@@ -83,7 +83,8 @@ interface Renderable {
             bypassChecks: Boolean = false,
             highlightsOnHoverSlots: List<Int> = emptyList(),
             condition: () -> Boolean = { true },
-        ): Renderable = link(string(text), onClick, bypassChecks, highlightsOnHoverSlots = highlightsOnHoverSlots, condition)
+        ): Renderable =
+            link(string(text), onClick, bypassChecks, highlightsOnHoverSlots = highlightsOnHoverSlots, condition)
 
         fun link(
             renderable: Renderable,
@@ -119,6 +120,20 @@ interface Renderable {
             )
         }
 
+        fun multiClickAndHover(
+            text: Any,
+            tips: List<Any>,
+            bypassChecks: Boolean = false,
+            click: Map<Int, () -> Unit>,
+            onHover: () -> Unit = {},
+        ): Renderable {
+            return multiClickable(
+                hoverTips(text, tips, bypassChecks = bypassChecks, onHover = onHover),
+                click,
+                bypassChecks = bypassChecks
+            )
+        }
+
         fun clickable(
             render: Renderable,
             onClick: () -> Unit,
@@ -136,6 +151,28 @@ interface Renderable {
                     shouldAllowLink(true, bypassChecks) && (button - 100).isKeyClicked()
                 ) {
                     onClick()
+                }
+                render.render(posX, posY)
+            }
+        }
+
+        fun multiClickable(
+            render: Renderable,
+            click: Map<Int, () -> Unit>,
+            bypassChecks: Boolean = false,
+            condition: () -> Boolean = { true },
+        ) = object : Renderable {
+            override val width = render.width
+            override val height = render.height
+            override val horizontalAlign = render.horizontalAlign
+            override val verticalAlign = render.verticalAlign
+
+            override fun render(posX: Int, posY: Int) {
+                if (isHovered(posX, posY) && condition() &&
+                    shouldAllowLink(true, bypassChecks)
+                ) for ((button, onClick) in click) {
+                    if ((button - 100).isKeyClicked())
+                        onClick()
                 }
                 render.render(posX, posY)
             }
@@ -259,13 +296,13 @@ interface Renderable {
 
             override fun render(posX: Int, posY: Int) {
                 val pair = Pair(posX, posY)
-                    isHovered = if (isHovered(posX, posY) && condition() && shouldAllowLink(true, bypassChecks)) {
-                        hovered.render(posX, posY)
-                        HighlightOnHoverSlot.currentSlots[pair] = highlightsOnHoverSlots
-                        true
-                    } else {
-                        unhovered.render(posX, posY)
-                        HighlightOnHoverSlot.currentSlots.remove(pair)
+                isHovered = if (isHovered(posX, posY) && condition() && shouldAllowLink(true, bypassChecks)) {
+                    hovered.render(posX, posY)
+                    HighlightOnHoverSlot.currentSlots[pair] = highlightsOnHoverSlots
+                    true
+                } else {
+                    unhovered.render(posX, posY)
+                    HighlightOnHoverSlot.currentSlots.remove(pair)
                     false
                 }
             }
