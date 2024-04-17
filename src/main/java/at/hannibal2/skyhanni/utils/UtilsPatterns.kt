2f72956dce75c93c82f66a36060d104c721f20f9
Cal <cwolfson58@gmail.com>
2024-04-17 20:38:55 +1000

    Fix: Akin profile id

--- src/main/java/at/hannibal2/skyhanni/utils/UtilsPatterns.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/UtilsPatterns.kt
@@ -86,6 +86,6 @@ object UtilsPatterns {
 
     val tabListProfilePattern by patternGroup.pattern(
         "tablist.profile",
-        "(?:§.)+Profile: §r§a(?<profile>\\S+).*"
+        "(?:§.)+Profile: §r§a(?<profile>[\\w\\s]+[^ §]).*"
     )
 }
