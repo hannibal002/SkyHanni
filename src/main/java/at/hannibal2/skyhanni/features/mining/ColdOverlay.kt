59bd34b5dfad9b11ebf56ae60301572601461a55
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-04-17 09:29:15 +0200

    make overlay work between world changes

--- src/main/java/at/hannibal2/skyhanni/features/mining/ColdOverlay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/ColdOverlay.kt
@@ -1,11 +1,12 @@
 package at.hannibal2.skyhanni.features.mining
 
 import at.hannibal2.skyhanni.SkyHanniMod
-import at.hannibal2.skyhanni.data.MiningAPI
 import at.hannibal2.skyhanni.data.MiningAPI.inColdIsland
 import at.hannibal2.skyhanni.events.ColdUpdateEvent
 import at.hannibal2.skyhanni.events.GuiRenderEvent
+import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.NumberUtil
+import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import io.github.moulberry.notenoughupdates.util.Utils
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.ScaledResolution
@@ -13,12 +14,14 @@ import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import org.lwjgl.opengl.GL11
+import kotlin.time.Duration.Companion.seconds
 
 class ColdOverlay {
 
     private val config get() = SkyHanniMod.feature.mining.coldOverlay
     var cold = 0
     var lastCold = 0
+    private var lastColdUpdate = SimpleTimeMark.farPast()
 
     @SubscribeEvent
     fun onRenderOverlay(event: GuiRenderEvent.GuiOverlayRenderEvent) {
@@ -55,15 +58,20 @@ class ColdOverlay {
     }
 
     private fun getColdAlpha(): Float {
-        val coldInterp = NumberUtil.interpolate(cold.toFloat(), lastCold.toFloat(), MiningAPI.lastColdUpdate.toMillis())
+        val coldInterp = NumberUtil.interpolate(cold.toFloat(), lastCold.toFloat(), lastColdUpdate.toMillis())
         val coldPercentage = (coldInterp - config.coldThreshold) / (100 - config.coldThreshold)
         return coldPercentage.coerceAtLeast(0f) * (config.maxAlpha / 100)
     }
 
     @SubscribeEvent
     fun onColdUpdate(event: ColdUpdateEvent) {
-        lastCold = cold
-        cold = event.cold
+        val duration = if (event.cold == 0) 2.seconds else 0.seconds
+        DelayedRun.runDelayed(duration) {
+            lastCold = cold
+            cold = event.cold
+            lastColdUpdate = SimpleTimeMark.now()
+
+        }
     }
 
     private fun isEnabled() = inColdIsland() && config.enabled
