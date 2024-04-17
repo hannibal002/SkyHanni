99fe36785eed6be5b7637f83a858d6f7414e1fbf
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-17 22:10:56 +0200

    fix merge conflicts

--- src/main/java/at/hannibal2/skyhanni/config/features/enchantparsing/EnchantParsingConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/enchantparsing/EnchantParsingConfig.java
@@ -3,10 +3,10 @@ package at.hannibal2.skyhanni.config.features.enchantparsing;
 import at.hannibal2.skyhanni.config.FeatureToggle;
 import at.hannibal2.skyhanni.utils.LorenzColor;
 import com.google.gson.annotations.Expose;
-import io.github.moulberry.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.moulberry.moulconfig.annotations.ConfigEditorDropdown;
-import io.github.moulberry.moulconfig.annotations.ConfigOption;
-import io.github.moulberry.moulconfig.observer.Property;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
+import io.github.notenoughupdates.moulconfig.observer.Property;
 
 public class EnchantParsingConfig {
 
