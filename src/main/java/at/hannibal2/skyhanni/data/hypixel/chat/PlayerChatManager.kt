ca07044d39a0a8996df0ab09df0207a3ac17dc91
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-18 09:31:37 +0200

    add regex test to global

--- src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/PlayerChatManager.kt
+++ src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/PlayerChatManager.kt
@@ -11,6 +11,16 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 class PlayerChatManager {
 
     private val patternGroup = RepoPattern.group("data.chat.player")
+
+    /**
+     * REGEX-TEST: §8[§r§6428§r§8] §r§b[MVP§5+§b] Alea1337§f: t
+     * REGEX-TEST: §8[§r§e102§r§8] §r§7☠ §r§b[MVP§d+§b] cobyjoey§f§r§f: first person to type "halo0011 is my favorite player on the game I love halo0011!!!"
+     * REGEX-TEST: §8[§r§5396§r§8] §r§7☢ §r§b[MVP§c+§b] hannibal2§f: hello
+     * REGEX-TEST: §8[§r§e97§r§8] §r§7☃ §r§7Tambaloo§7§r§7: i did capital i
+     * REGEX-TEST: §8[§r§f76§r§8] §r§7❂ §r§a[VIP] Asymmetrically§f§r§f: i need to put on my necron
+     * REGEX-TEST: §8[§r§c446§r§8] §r§b§l⚛ §r§6[MVP§1++§6] XueRuu§f§r§f: TROPHY FISH! You caught a Lavahorse DIAMOND.
+     * REGEX-TEST: §b[MVP§c+§b] hannibal2§f: test
+     */
     private val globalPattern by patternGroup.pattern(
         "global",
         "(?:§8\\[§r(?<levelColor>§.)(?<level>\\d+)§r§8] §r)?(?<author>§.+)(?:§f|§7§r§7): (?<message>.*)"
