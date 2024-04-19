f3b6288fed75eacede95627282cb9bfec73f0c9e
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2024-04-19 12:55:32 +0200

    Fix: paleontologist in fossil excavator (#1490)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/mining/fossilexcavator/FossilExcavatorAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/fossilexcavator/FossilExcavatorAPI.kt
@@ -101,10 +101,20 @@ object FossilExcavatorAPI {
             inLoot = false
             return
         }
-
-        val pair = itemPattern.matchMatcher(message) {
+        var pair = itemPattern.matchMatcher(message) {
+            /**
+             * TODO fix the bug that readItemAmount produces two different outputs:
+             * §r§fEnchanted Book -> §fEnchanted
+             * §fEnchanted Book §r§8x -> §fEnchanted Book
+             *
+             * also maybe this is no bug, as enchanted book is no real item?
+             */
             ItemUtils.readItemAmount(group("item"))
         } ?: return
+        // Workaround: If it is a enchanted book, we assume it is a paleontologist I book
+        if (pair.first.let { it ==  "§fEnchanted" || it == "§fEnchanted Book"}) {
+            pair = "Paleontologist I" to pair.second
+        }
         loot.add(pair)
     }
 }
