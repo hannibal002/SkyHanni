c405d912c6cc27daf140fbcfdfcb46b8c3ac4e17
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-04-21 19:14:41 +0200

    cancel more events

--- src/main/java/at/hannibal2/skyhanni/data/GuiData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/GuiData.kt
@@ -2,6 +2,7 @@ package at.hannibal2.skyhanni.data
 
 import at.hannibal2.skyhanni.events.GuiContainerEvent
 import at.hannibal2.skyhanni.events.NEURenderEvent
+import net.minecraftforge.client.event.GuiScreenEvent
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -9,13 +10,23 @@ object GuiData {
 
     var preDrawEventCanceled = false
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent(priority = EventPriority.HIGH)
     fun onNeuRenderEvent(event: NEURenderEvent) {
         if (preDrawEventCanceled) event.cancel()
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent(priority = EventPriority.HIGH)
     fun onClick(event: GuiContainerEvent.SlotClickEvent) {
         if (preDrawEventCanceled) event.cancel()
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    fun onGuiClick(event: GuiScreenEvent.MouseInputEvent.Pre) {
+        if (preDrawEventCanceled) event.isCanceled = true
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    fun onGuiClick(event: GuiScreenEvent.KeyboardInputEvent.Pre) {
+        if (preDrawEventCanceled) event.isCanceled = true
     }
 }
