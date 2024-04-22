e74c59f81f635faf4522fa9e647a83af2d71df84
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-04-23 01:44:40 +0200

    fix name

--- src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/WardrobeOverlay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/WardrobeOverlay.kt
@@ -19,6 +19,7 @@ import net.minecraft.client.entity.AbstractClientPlayer
 import net.minecraft.client.gui.inventory.GuiInventory.drawEntityOnScreen
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.resources.DefaultPlayerSkin
+import net.minecraft.util.IChatComponent
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.awt.Color
@@ -58,6 +59,36 @@ class WardrobeOverlay {
 
             override fun getName(): String {
                 return ""
+            }
+
+            override fun hasCustomName(): Boolean {
+                return false
+            }
+
+            override fun getDisplayNameString(): String? {
+                return null
+            }
+
+            override fun getAlwaysRenderNameTag(): Boolean {
+                return false
+            }
+
+            override fun getAlwaysRenderNameTagForRender(): Boolean {
+                return false
+            }
+
+            override fun getDisplayName(): IChatComponent {
+                return IChatComponent.Serializer.jsonToComponent("{\"text\":\"\"}")
+            }
+
+            override fun getCustomNameTag(): String? {
+                return null
+            }
+
+            override fun setCustomNameTag(name: String?) {}
+
+            override fun setAlwaysRenderNameTag(alwaysRenderNameTag: Boolean) {
+                super.setAlwaysRenderNameTag(false)
             }
         }
 
