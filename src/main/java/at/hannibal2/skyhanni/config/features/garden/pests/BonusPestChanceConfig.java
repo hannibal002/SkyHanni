1aea2e91d000ade45a743611266e0ea8ee1a4f11
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-23 11:32:22 +0200

    added ConfigLink support

--- src/main/java/at/hannibal2/skyhanni/config/features/garden/pests/BonusPestChanceConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/garden/pests/BonusPestChanceConfig.java
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.config.FeatureToggle;
 import at.hannibal2.skyhanni.config.core.config.Position;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class BonusPestChanceConfig {
@@ -17,5 +18,6 @@ public class BonusPestChanceConfig {
     public boolean display = false;
 
     @Expose
+    @ConfigLink(owner = BonusPestChanceConfig.class, field = "display")
     public Position pos = new Position(5, -115, false, true);
 }
