e444bdea0d1714a291b20ae6b542a0f770d51a21
Cal <cwolfson58@gmail.com>
2024-04-23 19:19:26 +1000

    start funny

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/RendererLivingEntityHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/RendererLivingEntityHook.kt
@@ -1,10 +1,29 @@
 package at.hannibal2.skyhanni.mixins.hooks
 
 import at.hannibal2.skyhanni.utils.EntityOutlineRenderer
+import at.hannibal2.skyhanni.utils.LorenzUtils
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.EnumPlayerModelParts
 
 class RendererLivingEntityHook {
+
+    // todo use repo list
+    private val coolPeople = setOf(
+        "Dinnerbone",
+        "Biscut",
+        "Pinpointed",
+        "Berded",
+        "Potat_owo",
+        "Pnda__",
+        "Throwpo",
+        "StopUsingSBE",
+        "catgirlseraid",
+        "ThatGravyBoat",
+        "CalMWolfs",
+        "Wolfie586"
+    )
 
     fun setOutlineColor(red: Float, green: Float, blue: Float, alpha: Float, entity: EntityLivingBase) {
         val color = EntityOutlineRenderer.getCustomOutlineColor(entity)
@@ -17,5 +36,20 @@ class RendererLivingEntityHook {
         } else {
             GlStateManager.color(red, green, blue, alpha)
         }
+    }
+
+    fun isCoolPerson(name: String?): Boolean {
+        if (!LorenzUtils.inSkyBlock) return false
+        return coolPeople.contains(name)
+    }
+
+    fun isWearing(entityPlayer: EntityPlayer, p_175148_1_: EnumPlayerModelParts?): Boolean {
+        if (!LorenzUtils.inSkyBlock) return entityPlayer.isWearing(p_175148_1_)
+        return isCoolPerson(entityPlayer.name) || entityPlayer.isWearing(p_175148_1_)
+    }
+
+    fun getRotation(entity: Any): Int {
+        if (entity !is EntityPlayer) return 0
+        return (entity.ticksExisted % 90) * 4
     }
 }
---
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinContributorRendererEntityLiving.java
@@ -0,0 +1,33 @@
+package at.hannibal2.skyhanni.mixins.transformers.renderer;
+
+import at.hannibal2.skyhanni.mixins.hooks.RendererLivingEntityHook;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EnumPlayerModelParts;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(RendererLivingEntity.class)
+public class MixinContributorRendererEntityLiving<T extends EntityLivingBase> {
+
+    @Unique
+    private final RendererLivingEntityHook skyHanni$hook = new RendererLivingEntityHook();
+
+    @Redirect(method = "rotateCorpse", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z", ordinal = 0))
+    private boolean rotateCorpse(String displayName, Object v2, T bat, float p_77043_2_, float p_77043_3_, float partialTicks) {
+        if (skyHanni$hook.isCoolPerson(displayName)) {
+            GlStateManager.scale(1.1f, 1.1f, 1.1f);
+            GlStateManager.rotate(skyHanni$hook.getRotation(bat), 0f, 1f, 0f);
+        }
+        return skyHanni$hook.isCoolPerson(displayName);
+    }
+
+    @Redirect(method = "rotateCorpse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/EntityPlayer;isWearing(Lnet/minecraft/entity/player/EnumPlayerModelParts;)Z"))
+    private boolean rotateCorpse(EntityPlayer bat, EnumPlayerModelParts p_175148_1_) {
+        return skyHanni$hook.isWearing(bat, EnumPlayerModelParts.CAPE);
+    }
+}
