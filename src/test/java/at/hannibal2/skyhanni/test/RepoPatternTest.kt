796dd5758dda0f39ab50d0708bfc32abc758fb0a
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-04-24 20:03:41 +0200

    test work

--- src/test/java/at/hannibal2/skyhanni/test/RepoPatternTest.kt
+++ src/test/java/at/hannibal2/skyhanni/test/RepoPatternTest.kt
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPatternDump
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPatternManager
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 object RepoPatternTest {
@@ -57,7 +58,10 @@ object RepoPatternTest {
         val listPatterns = RepoPattern.list("testonly.list.a", simpleLocalePattern1, simpleLocalePattern2)
         val list by listPatterns
 
-        val remoteValue = "I'm remote."
+        val remoteValue1 = "I'm remote."
+        val remoteValue2 = "I'm remote 2."
+        val remoteValue3 = "I'm remote 3."
+        val remoteValue4 = "I'm remote 4."
 
         val isLocalWorking = list[0].pattern() == simpleLocalePattern1 && list[1].pattern() == simpleLocalePattern2
 
@@ -66,28 +70,83 @@ object RepoPatternTest {
         RepoPatternManager.loadPatternsFromDump(
             RepoPatternDump(
                 regexes = mapOf(
-                    listPatterns.key to remoteValue
+                    listPatterns.key + ".1" to remoteValue1,
+                    listPatterns.key + ".2" to remoteValue2
                 )
             )
         )
 
-        val isRemoteWorking = list.size != 0 && list.all { it.pattern() == remoteValue }
+        val isRemoteWorking = list[0].pattern() == remoteValue1 && list[1].pattern() == remoteValue2
 
         assert(isRemoteWorking)
 
+        RepoPatternManager.loadPatternsFromDump(
+            RepoPatternDump(
+                regexes = mapOf(
+                    listPatterns.key + ".1" to remoteValue3,
+                )
+            )
+        )
+
+        val isRemoteSingleWorking = list[0].pattern() == remoteValue3
+
+        assert(isRemoteSingleWorking)
+        assertThrows<IndexOutOfBoundsException> { list[1] }
+
+        RepoPatternManager.loadPatternsFromDump(
+            RepoPatternDump(
+                regexes = mapOf(
+                    listPatterns.key to remoteValue4,
+                )
+            )
+        )
+
+        val isRemoteListToSingleWorking = list.isEmpty()
+
+        assert(isRemoteListToSingleWorking)
     }
 
-    /* @Test
+    @Test
     fun testExclusivityList() {
         RepoPatternManager.inTestDuplicateUsage = false
 
         assertThrows<RuntimeException> {
-            val pattern1 by RepoPattern.pattern("testonly.b", "")
-            val pattern2 by RepoPattern.pattern("testonly.b", "")
+            val pattern1 by RepoPattern.list("testonly.c", "")
+            val pattern2 by RepoPattern.list("testonly.c", "")
             pattern1
             pattern2
         }
 
+        assertThrows<RuntimeException> {
+            val pattern1 by RepoPattern.list("testonly.d", "")
+            val pattern2 by RepoPattern.pattern("testonly.d", "")
+            pattern1
+            pattern2
+        }
+
+        assertThrows<RuntimeException> {
+            val pattern1 by RepoPattern.list("testonly.e", "")
+            val pattern2 by RepoPattern.pattern("testonly.e.1", "")
+            pattern1
+            pattern2
+        }
+
+        assertThrows<RuntimeException> {
+            val pattern1 by RepoPattern.list("testonly.f", "")
+            val pattern2 by RepoPattern.pattern("testonly.f.a", "")
+            pattern1
+            pattern2
+        }
+
+        assertDoesNotThrow {
+            val pattern1 by RepoPattern.list("testonly.g", "")
+            val pattern2 by RepoPattern.list("testonly.h", "")
+            val pattern3 by RepoPattern.pattern("testonly.i", "")
+            pattern1
+            pattern2
+            pattern3
+        }
+
         RepoPatternManager.inTestDuplicateUsage = true
-    } */
+    }
 }
