71168010881707d336736cb37c88f6eb777c0ffc
martimavocado <martim.cavaco@tutanota.com>
2024-04-28 19:51:24 +0100

    this also works

--- src/main/java/at/hannibal2/skyhanni/features/event/chocolatefactory/ChocolateFactoryBarnManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/chocolatefactory/ChocolateFactoryBarnManager.kt
@@ -69,21 +69,13 @@ object ChocolateFactoryBarnManager {
             return
         }
 
-        if (profileStorage.currentRabbits == profileStorage.maxRabbits) {
-            ChatUtils.clickableChat(
-                "§cYour barn is full! §7(${barnStatus()}). §cUpgrade it so they don't get crushed",
-                onClick = {
-                    HypixelCommands.chocolateFactory()
-                }
-            )
-        } else {
-            ChatUtils.clickableChat(
-                "§cYour barn is almost full! §7(${barnStatus()}). §cUpgrade it so they don't get crushed",
-                onClick = {
-                    HypixelCommands.chocolateFactory()
-                }
-            )
-        }
+        ChatUtils.clickableChat(
+            message = if (profileStorage.currentRabbits == profileStorage.maxRabbits) { "§cYour barn is full! §7(${barnStatus()}). §cUpgrade it so they don't get crushed" }
+            else { "§cYour barn is almost full! §7(${barnStatus()}). §cUpgrade it so they don't get crushed"},
+            onClick = {
+                HypixelCommands.chocolateFactory()
+            }
+        )
         SoundUtils.playBeepSound()
         lastBarnFullWarning = SimpleTimeMark.now()
     }
