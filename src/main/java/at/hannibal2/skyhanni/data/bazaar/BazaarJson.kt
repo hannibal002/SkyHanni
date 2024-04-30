886dd053e155077dea78dbc1faecbe7d2a061813
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-30 13:13:27 +0200

    compact

--- src/main/java/at/hannibal2/skyhanni/data/bazaar/BazaarJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/bazaar/BazaarJson.kt
@@ -4,57 +4,33 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 class BazaarApiResponse(
-    @Expose
-    val success: Boolean,
-    @Expose
-    val cause: String,
-    @Expose
-    val lastUpdated: Long,
-    @Expose
-    val products: Map<String, BazaarProduct>,
+    @Expose val success: Boolean,
+    @Expose val cause: String,
+    @Expose val lastUpdated: Long,
+    @Expose val products: Map<String, BazaarProduct>,
 )
 
 data class BazaarProduct(
-    @Expose
-    @SerializedName("product_id")
-    val productId: String,
-    @Expose
-    @SerializedName("quick_status")
-    val quickStatus: BazaarQuickStatus,
-    @Expose
-    @SerializedName("sell_summary")
-    val sellSummary: List<BazaarSummary>,
-    @Expose
-    @SerializedName("buy_summary")
-    val buySummary: List<BazaarSummary>,
+    @Expose @SerializedName("product_id") val productId: String,
+    @Expose @SerializedName("quick_status") val quickStatus: BazaarQuickStatus,
+    @Expose @SerializedName("sell_summary") val sellSummary: List<BazaarSummary>,
+    @Expose @SerializedName("buy_summary") val buySummary: List<BazaarSummary>,
 )
 
 class BazaarQuickStatus(
-    @Expose
-    val productId: String,
-    @Expose
-    val sellPrice: Double,
-    @Expose
-    val sellVolume: Long,
-    @Expose
-    val sellMovingWeek: Long,
-    @Expose
-    val sellOrders: Long,
-    @Expose
-    val buyPrice: Double,
-    @Expose
-    val buyVolume: Long,
-    @Expose
-    val buyMovingWeek: Long,
-    @Expose
-    val buyOrders: Long,
+    @Expose val productId: String,
+    @Expose val sellPrice: Double,
+    @Expose val sellVolume: Long,
+    @Expose val sellMovingWeek: Long,
+    @Expose val sellOrders: Long,
+    @Expose val buyPrice: Double,
+    @Expose val buyVolume: Long,
+    @Expose val buyMovingWeek: Long,
+    @Expose val buyOrders: Long,
 )
 
 data class BazaarSummary(
-    @Expose
-    val amount: Long,
-    @Expose
-    val pricePerUnit: Double,
-    @Expose
-    val orders: Long,
+    @Expose val amount: Long,
+    @Expose val pricePerUnit: Double,
+    @Expose val orders: Long,
 )
