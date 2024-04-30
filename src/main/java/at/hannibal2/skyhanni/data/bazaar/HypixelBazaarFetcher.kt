decd18011c06234f74f7cdaf720f57baa36ed5d4
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-04-30 11:38:20 +0200

    integrating new bazaar data into SkyHanni

--- src/main/java/at/hannibal2/skyhanni/data/bazaar/HypixelBazaarFetcher.kt
+++ src/main/java/at/hannibal2/skyhanni/data/bazaar/HypixelBazaarFetcher.kt
@@ -3,12 +3,15 @@ package at.hannibal2.skyhanni.data.bazaar
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.config.ConfigManager
 import at.hannibal2.skyhanni.events.LorenzTickEvent
+import at.hannibal2.skyhanni.features.inventory.bazaar.BazaarData
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.APIUtil
 import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.ItemUtils.itemName
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NEUInternalName
 import at.hannibal2.skyhanni.utils.NEUItems
+import at.hannibal2.skyhanni.utils.NEUItems.getItemStackOrNull
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.fromJson
 import com.google.gson.annotations.Expose
@@ -24,7 +27,7 @@ object HypixelBazaarFetcher {
     private val url = "https://api.hypixel.net/v2/skyblock/bazaar"
     private val hiddenFailedAttempts = 3
 
-    private var latestProductInformation = mapOf<NEUInternalName, BazaarProduct>()
+    var latestProductInformation = mapOf<NEUInternalName, BazaarData>()
     private var nextFetchTime = SimpleTimeMark.farPast()
     private var failedAttepmts = 0
     private var nextFetchIsManual = false
@@ -45,8 +48,7 @@ object HypixelBazaarFetcher {
             val jsonResponse = withContext(Dispatchers.IO) { APIUtil.getJSONResponse(url) }.asJsonObject
             val response = ConfigManager.gson.fromJson<BazaarApiResponse>(jsonResponse)
             if (response.success) {
-                latestProductInformation = response.products
-                    .mapKeys { NEUItems.transHypixelNameToInternalName(it.key) }
+                latestProductInformation = process(response.products)
                 failedAttepmts = 0
             } else {
                 onError(fetchType, Exception("success=false, cause=${response.cause}"))
@@ -55,6 +57,19 @@ object HypixelBazaarFetcher {
             onError(fetchType, e)
         }
     }
+
+    private fun process(products: Map<String, BazaarProduct>) = products.mapNotNull { (key, product) ->
+        val internalName = NEUItems.transHypixelNameToInternalName(key)
+        val sellOfferPrice = product.buySummary.minOfOrNull { it.pricePerUnit } ?: 0.0
+        val insantBuyPrice = product.sellSummary.maxOfOrNull { it.pricePerUnit } ?: 0.0
+        if (internalName.getItemStackOrNull() == null) {
+            // Items that exist in Hypixel's Bazaar API, but not in NEU repo (not visible in in the ingame bazaar)
+            if (LorenzUtils.getPlayerUuid() == "8a9f184148e948edb14f76a124e6c9df" || LorenzUtils.debug)
+                println("jani moment: $internalName/$key")
+            return@mapNotNull null
+        }
+        internalName to BazaarData(internalName.itemName, sellOfferPrice, insantBuyPrice, product)
+    }.toMap()
 
     private fun onError(fetchType: String, e: Exception) {
         val userMessage = "Failed fetching bazaar price data from hypixel"
