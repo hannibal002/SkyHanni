e656e850ecb6caa9e86edf6304f1a9eb378387d4
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-05-03 14:14:47 +0200

    make gui work + stuff
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/mining/MineshaftPityDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/MineshaftPityDisplay.kt
@@ -7,12 +7,11 @@ import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.events.mining.CustomBlockMineEvent
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.CollectionUtils.addOrPut
+import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.OSUtils
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderables
 import at.hannibal2.skyhanni.utils.StringUtils.matches
 import at.hannibal2.skyhanni.utils.renderables.Renderable
-import net.minecraft.init.Blocks
-import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object MineshaftPityDisplay {
@@ -38,7 +37,7 @@ object MineshaftPityDisplay {
         if (!isEnabled()) return
         if (MiningNotifications.mineshaftSpawn.matches(event.message)) {
             val resultList = mutableListOf<String>()
-            resultList.add("Mineshaft Pity Counter: ${calculateCounter()}/$MAX_COUNTER")
+            resultList.add("Mineshaft Pity Counter: ${MAX_COUNTER - calculateCounter()}/$MAX_COUNTER")
             resultList.add("Blocks mined:")
             minedBlocks.forEach {
                 resultList.add("    ${it.key.oreName}: ${it.value}")
@@ -51,9 +50,9 @@ object MineshaftPityDisplay {
     }
 
     private fun calculateCounter(): Int {
-        if (minedBlocks.isEmpty()) return 0
-        var counter = 0
-        minedBlocks.forEach { counter += it.key.shaftMultiplier * it.value }
+        if (minedBlocks.isEmpty()) return MAX_COUNTER
+        var counter = MAX_COUNTER
+        minedBlocks.forEach { counter -= it.key.shaftMultiplier * it.value }
         return counter
     }
 
@@ -64,29 +63,42 @@ object MineshaftPityDisplay {
     private fun drawDisplay() = buildList {
         val counter = calculateCounter()
 
-        add(
-            Renderable.horizontalContainer(
-                listOf(
-                    Renderable.itemStack(ItemStack(Blocks.packed_ice)),
-                    Renderable.string("§3Pity Counter: §e$counter§6/§e$MAX_COUNTER")
-                ),
-                spacing = 2
-            )
-        )
+        add(Renderable.string("§3Pity Counter: §e${MAX_COUNTER - counter}§6/§e$MAX_COUNTER"))
+        add(Renderable.string("§3Chance: §e1§6/§e$counter §7(§b${((1.0 / counter) * 100).addSeparators()}%§7)"))
 
+        add(Renderable.string("§3Needed to pity:"))
         val multiplierList = listOf(2, 4, 8)
 
         val list = mutableListOf<Renderable>()
         multiplierList.forEach { multiplier ->
             val iconList = mutableListOf<Renderable>()
             OreType.entries.filter { it.shaftMultiplier == multiplier }.forEach {
-                iconList.add(Renderable.itemStack(it.item))
+                iconList.add(Renderable.itemStack(it.item, xSpacing = 0))
             }
-            val icons = Renderable.horizontalContainer(iconList, -5)
-            val blocksNeeded = MAX_COUNTER - counter
-            list.add(Renderable.horizontalContainer(listOf(icons, Renderable.string("$blocksNeeded")), 2))
+            val icons = Renderable.horizontalContainer(iconList)
+            val blocksNeeded = counter / multiplier
+            list.add(
+                Renderable.horizontalContainer(
+                    listOf(
+                        Renderable.placeholder(10, icons.height),
+                        icons,
+                        Renderable.string("§b$blocksNeeded")
+                    ),
+                    2
+                )
+            )
         }
         add(Renderable.verticalContainer(list))
+
+        /*OreType.entries.filter { it.shaftMultiplier != 0 }.forEach {
+            add(Renderable.horizontalContainer(
+                listOf(
+                    Renderable.itemStack(it.item),
+                    Renderable.string(it.oreName)
+                ),
+                spacing = 2
+            ))
+        }*/
     }
 
     @SubscribeEvent
--- src/main/java/at/hannibal2/skyhanni/features/mining/OreType.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/OreType.kt
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.features.mining
 
 import net.minecraft.block.BlockSand
 import net.minecraft.block.BlockStone
-import net.minecraft.block.BlockStoneSlabNew
 import net.minecraft.init.Blocks
 import net.minecraft.item.EnumDyeColor
 import net.minecraft.item.ItemStack
@@ -12,8 +11,8 @@ enum class OreType(
     val item: ItemStack,
     val shaftMultiplier: Int = 0,
 ) {
-    MITHRIL("Mithril", ItemStack(Blocks.packed_ice, EnumDyeColor.LIGHT_BLUE.dyeDamage), 2),
-    TITANIUM("Titanium", ItemStack(Blocks.stone, BlockStone.EnumType.DIORITE_SMOOTH.metadata), 8),
+    MITHRIL("Mithril", ItemStack(Blocks.wool, 1, EnumDyeColor.LIGHT_BLUE.metadata), 2),
+    TITANIUM("Titanium", ItemStack(Blocks.stone, 1, BlockStone.EnumType.DIORITE_SMOOTH.metadata), 8),
     COBBLESTONE("Cobblestone", ItemStack(Blocks.cobblestone)),
     COAL("Coal", ItemStack(Blocks.coal_block)),
     IRON("Iron", ItemStack(Blocks.iron_block)),
@@ -26,14 +25,14 @@ enum class OreType(
     QUARTZ("Nether Quartz", ItemStack(Blocks.quartz_block)),
     GLOWSTONE("Glowstone", ItemStack(Blocks.glowstone)),
     MYCELIUM("Mycelium", ItemStack(Blocks.mycelium)),
-    RED_SAND("Red Sand", ItemStack(Blocks.sand, BlockSand.EnumType.RED_SAND.metadata)),
+    RED_SAND("Red Sand", ItemStack(Blocks.sand, 1, BlockSand.EnumType.RED_SAND.metadata)),
     SULPHUR("Sulphur", ItemStack(Blocks.sponge)),
     GRAVEL("Gravel", ItemStack(Blocks.gravel)),
     END_STONE("End Stone", ItemStack(Blocks.end_stone)),
     OBSIDIAN("Obsidian", ItemStack(Blocks.obsidian)),
     HARD_STONE("Hard Stone", ItemStack(Blocks.stone)),
-    GEMSTONE("Gemstone", ItemStack(Blocks.stained_glass, EnumDyeColor.RED.dyeDamage), 4),
-    UMBER("Umber", ItemStack(Blocks.double_stone_slab2, BlockStoneSlabNew.EnumType.RED_SANDSTONE.metadata), 4),
+    GEMSTONE("Gemstone", ItemStack(Blocks.stained_glass, 1, EnumDyeColor.RED.metadata), 4),
+    UMBER("Umber", ItemStack(Blocks.red_sandstone), 4),
     TUNGSTEN("Tungsten", ItemStack(Blocks.clay), 4),
     GLACITE("Glacite", ItemStack(Blocks.packed_ice), 4),
     ;
