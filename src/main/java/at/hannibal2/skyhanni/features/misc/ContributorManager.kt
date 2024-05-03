18c1a9f8db00fdcff2bca2ea1a457558492f131d
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-05-03 23:02:07 +0200

    add contributor suffix to nametags
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/misc/ContributorManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/ContributorManager.kt
@@ -3,7 +3,13 @@ package at.hannibal2.skyhanni.features.misc
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.jsonobjects.repo.ContributorJsonEntry
 import at.hannibal2.skyhanni.data.jsonobjects.repo.ContributorsJson
+import at.hannibal2.skyhanni.events.IslandChangeEvent
+import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.events.RepositoryReloadEvent
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.ChatComponentText
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 object ContributorManager {
@@ -16,7 +22,21 @@ object ContributorManager {
         contributors = event.getConstant<ContributorsJson>("Contributors").contributors.mapKeys { it.key.lowercase() }
     }
 
-    fun getTabListSuffix(username: String): String? = getContributor(username)?.suffix
+    @SubscribeEvent
+    fun onMobSpawnPlayer(event: MobEvent.Spawn.Player) {
+        addSuffixToNametag(event.mob.baseEntity)
+    }
+
+    @SubscribeEvent
+    fun onIslandChange(event: IslandChangeEvent) {
+        addSuffixToNametag(Minecraft.getMinecraft().thePlayer)
+    }
+
+    private fun addSuffixToNametag(entity: EntityLivingBase) {
+        getSuffix(entity.name)?.let { (entity as? EntityPlayer)?.addSuffix(ChatComponentText(" $it")) }
+    }
+
+    fun getSuffix(username: String): String? = getContributor(username)?.suffix
 
     fun canSpin(username: String): Boolean = getContributor(username)?.spinny ?: false
 
