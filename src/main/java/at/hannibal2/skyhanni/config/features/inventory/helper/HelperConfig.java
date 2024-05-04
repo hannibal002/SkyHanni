52093654496763c10b06ab42bec0783a16333365
raven <raveeeennnn@hotmail.com>
2024-05-04 22:53:35 +0100

    Improved the description.

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/helper/HelperConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/helper/HelperConfig.java
@@ -36,7 +36,7 @@ public class HelperConfig {
         public boolean showNumbers = false;
 
         @Expose
-        @ConfigOption(name = "Hide Tooltip", desc = "Hides the item tooltips while in the Harp.")
+        @ConfigOption(name = "Hide Tooltip", desc = "Hides the item tooltips inside the Harp.")
         @ConfigEditorBoolean
         @FeatureToggle
         public boolean hideMelodyTooltip = false;
