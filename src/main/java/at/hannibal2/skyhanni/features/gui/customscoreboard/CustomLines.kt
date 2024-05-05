80b1de38f98f25509243ed035d0d81450fb9432a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-05-05 10:50:55 +0200

    added some more replacements, changed to triple
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/CustomLines.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/CustomLines.kt
@@ -1,15 +1,24 @@
 package at.hannibal2.skyhanni.features.gui.customscoreboard
 
+import at.hannibal2.skyhanni.data.HypixelData
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getBits
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getBitsAvailable
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getPurse
+import at.hannibal2.skyhanni.utils.LorenzUtils.round
+import net.minecraft.client.Minecraft
 
 object CustomLines {
 
-    private val replacements = mapOf(
-        "%purse%" to { getPurse() },
-        "%bits%" to { getBits() },
-        "%bitsAvailable%" to { getBitsAvailable() }
+    private val replacements = listOf(
+        Triple("%x%", { Minecraft.getMinecraft().thePlayer.posX.round(2) }, "X-Coordinate"),
+        Triple("%y%", { Minecraft.getMinecraft().thePlayer.posY.round(2) }, "Y-Coordinate"),
+        Triple("%z%", { Minecraft.getMinecraft().thePlayer.posZ.round(2) }, "Z-Coordinate"),
+        Triple("%dir%", { Minecraft.getMinecraft().thePlayer.rotationYaw.round(2) }, "Direction"),
+        Triple("%purse%", { getPurse() }, "Purse"),
+        Triple("%bits%", { getBits() }, "Bits"),
+        Triple("%bits_available%", { getBitsAvailable() }, "Bits Available"),
+        Triple("%island%", { HypixelData.skyBlockIsland.displayName }, "Island"),
+        Triple("%area%", { HypixelData.skyBlockArea }, "Area"),
     )
 
     internal fun String.handleCustomLine(): List<String> {
@@ -19,7 +28,7 @@ object CustomLines {
     private fun String.replaceWithReplacements(): List<String> {
         var modifiedString = this
         replacements.forEach { (placeholder, replacement) ->
-            modifiedString = modifiedString.replace(placeholder, replacement.invoke())
+            modifiedString = modifiedString.replace(placeholder, replacement.invoke().toString())
         }
         return modifiedString.split("\\n")
     }
