3f0d5a7ea972f13bbd535decb237e7883dca0153
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-05-06 20:43:06 +0200

    config link to gui
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/gui/customscoreboard/CustomLinesConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/gui/customscoreboard/CustomLinesConfig.java
@@ -1,10 +1,16 @@
 package at.hannibal2.skyhanni.config.features.gui.customscoreboard;
 
+import at.hannibal2.skyhanni.config.commands.Commands;
 import com.google.gson.annotations.Expose;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorButton;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class CustomLinesConfig {
+
+    @ConfigOption(name = "Open Custom Lines GUI", desc = "Open the GUI to edit the custom lines")
+    @ConfigEditorButton(buttonText = "Open")
+    public Runnable open = Commands::openCustomLines;
 
     @Expose
     @ConfigOption(name = "Custom Line 1", desc = "Custom line 1")
