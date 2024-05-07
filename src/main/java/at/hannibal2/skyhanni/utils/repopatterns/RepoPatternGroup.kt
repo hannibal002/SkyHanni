b8508785c88ee5d23c65601d55d0bad86155deba
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-05-07 21:05:07 +0200

    some renaming

--- src/main/java/at/hannibal2/skyhanni/utils/repopatterns/RepoPatternGroup.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/repopatterns/RepoPatternGroup.kt
@@ -7,7 +7,7 @@ import org.intellij.lang.annotations.Language
  */
 open class RepoPatternGroup internal constructor(
     val prefix: String,
-    protected var parent: RepoPatternKeyOwner? = null,
+    protected var parentGiver: RepoPatternKeyOwner? = null,
 ) {
 
     init {
@@ -18,27 +18,27 @@ open class RepoPatternGroup internal constructor(
      * Shortcut to [RepoPattern.pattern] prefixed with [prefix].
      */
     fun pattern(key: String, @Language("RegExp") fallback: String): RepoPattern {
-        return RepoPatternManager.of("$prefix.$key", fallback, parent)
+        return RepoPatternManager.of("$prefix.$key", fallback, parentGiver)
     }
 
     /**
      * Shortcut to [RepoPattern.list] prefixed with [prefix].
      */
     fun list(key: String, @Language("RegExp") vararg fallbacks: String): RepoPatternList {
-        return RepoPatternManager.ofList("$prefix.$key", fallbacks, parent)
+        return RepoPatternManager.ofList("$prefix.$key", fallbacks, parentGiver)
     }
 
     /**
      * Shortcut to [RepoPattern.group] prefixed with [prefix].
      */
     fun group(subgroup: String): RepoPatternGroup {
-        return RepoPatternGroup("$prefix.$subgroup", parent)
+        return RepoPatternGroup("$prefix.$subgroup", parentGiver)
     }
 
     /**
      * Shortcut to [RepoPattern.exclusiveGroup] prefixed with [prefix].
      */
     fun exclusiveGroup(subgroup: String): RepoPatternExclusiveGroupInfo {
-        return RepoPatternExclusiveGroupInfo("$prefix.$subgroup", parent)
+        return RepoPatternExclusiveGroupInfo("$prefix.$subgroup", parentGiver)
     }
 }
