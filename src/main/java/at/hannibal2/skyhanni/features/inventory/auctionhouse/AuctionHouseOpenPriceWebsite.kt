f70925479d8ca68c466605b5f39579ed1225fc4b
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-05-08 22:12:56 +0200

    added cofl ah search item

---
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/auctionhouse/AuctionHouseOpenPriceWebsite.kt
@@ -0,0 +1,89 @@
+package at.hannibal2.skyhanni.features.inventory.auctionhouse
+
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.GuiContainerEvent
+import at.hannibal2.skyhanni.events.InventoryCloseEvent
+import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
+import at.hannibal2.skyhanni.utils.LorenzUtils
+import at.hannibal2.skyhanni.utils.NEUInternalName.Companion.asInternalName
+import at.hannibal2.skyhanni.utils.NEUItems.getItemStack
+import at.hannibal2.skyhanni.utils.OSUtils
+import at.hannibal2.skyhanni.utils.SimpleTimeMark
+import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import io.github.moulberry.notenoughupdates.events.ReplaceItemEvent
+import io.github.moulberry.notenoughupdates.util.Utils
+import net.minecraft.entity.player.InventoryPlayer
+import net.minecraft.item.ItemStack
+import net.minecraftforge.fml.common.eventhandler.EventPriority
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import kotlin.time.Duration.Companion.seconds
+
+class AuctionHouseOpenPriceWebsite {
+
+    private val config get() = SkyHanniMod.feature.inventory.auctions
+    private var lastClick = SimpleTimeMark.farPast()
+
+    private val patternGroup = RepoPattern.group("inventory.auctionhouse")
+
+    /**
+     * REGEX-TEST: Auctions: "hyperion"
+     */
+    private val cookieDurationPattern by patternGroup.pattern(
+        "title.search",
+        "Auctions: \"(?<searchTerm>.*)\""
+    )
+
+    private var searchTerm = ""
+    private var displayItem: ItemStack? = null
+
+    @SubscribeEvent
+    fun onInventoryOpen(event: InventoryFullyOpenedEvent) {
+        if (!isEnabled()) return
+        cookieDurationPattern.matchMatcher(event.inventoryName) {
+            searchTerm = group("searchTerm")
+            displayItem = createDisplayItem()
+        }
+    }
+
+    private fun createDisplayItem() = Utils.createItemStack(
+        "PAPER".asInternalName().getItemStack().item,
+        "§bPrice History",
+        "§7Click here to open",
+        "§7the price history",
+        "§7of §e$searchTerm",
+        "§7on §csky.coflnet.com"
+    )
+
+    @SubscribeEvent
+    fun onInventoryClose(event: InventoryCloseEvent) {
+        displayItem = null
+    }
+
+    @SubscribeEvent
+    fun replaceItem(event: ReplaceItemEvent) {
+        if (!isEnabled()) return
+        if (event.inventory is InventoryPlayer) return
+
+        if (event.slotNumber == 8) {
+            displayItem?.let {
+                event.replaceWith(it)
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    fun onSlotClick(event: GuiContainerEvent.SlotClickEvent) {
+        if (!isEnabled()) return
+        displayItem ?: return
+        if (event.slotId != 8) return
+        event.isCanceled = true
+        if (lastClick.passedSince() > 0.3.seconds) {
+            val url = "https://sky.coflnet.com/api/mod/open/$searchTerm"
+            OSUtils.openBrowser(url)
+            lastClick = SimpleTimeMark.now()
+        }
+    }
+
+    fun isEnabled() = LorenzUtils.inSkyBlock && config.openPriceWebsite
+}
