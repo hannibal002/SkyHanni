d2cd6d2736f0a0bfc6526e8d93775a9d7d5aa7f5
raven <raveeeennnn@hotmail.com>
2024-05-09 12:43:50 +0100

    Changes with title

--- src/main/java/at/hannibal2/skyhanni/features/inventory/UltraRareBookAlert.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/UltraRareBookAlert.kt
@@ -1,25 +1,28 @@
 package at.hannibal2.skyhanni.features.inventory
 
 import at.hannibal2.skyhanni.SkyHanniMod
-import at.hannibal2.skyhanni.config.core.config.Position
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
 import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.ColorUtils.withAlpha
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.LorenzUtils
-import at.hannibal2.skyhanni.utils.RenderUtils.renderString
+import at.hannibal2.skyhanni.utils.RenderUtils
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SoundUtils.createSound
 import at.hannibal2.skyhanni.utils.SoundUtils.playSound
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.matches
+import at.hannibal2.skyhanni.utils.renderables.Renderable
+import at.hannibal2.skyhanni.utils.renderables.RenderableUtils.renderXYAligned
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.inventory.GuiContainer
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.awt.Color
 import kotlin.time.Duration.Companion.seconds
 
 object UltraRareBookAlert {
@@ -59,13 +62,16 @@ object UltraRareBookAlert {
         if (lastNotificationTime.passedSince() > 5.seconds) return
         val gui = Minecraft.getMinecraft().currentScreen as? GuiContainer ?: return
 
-        GlStateManager.translate(0f,0f,300f)
+        GlStateManager.translate(0f,0f,400f)
 
-        val pos = Position(gui.width/2, gui.height/2, 2f, true)
+        Renderable.drawInsideRoundedRect(
+            Renderable.string("§d§kXX§5 ULTRA-RARE BOOK! §d§kXX"),
+            Color(Color.LIGHT_GRAY.withAlpha(150), true),
+            horizontalAlign = RenderUtils.HorizontalAlignment.CENTER,
+            verticalAlign = RenderUtils.VerticalAlignment.CENTER,
+        ).renderXYAligned(0,0,gui.width,gui.height)
 
-        pos.renderString("§d§kXX§5 ULTRA-RARE BOOK! §d§kXX", posLabel = "ULTRA-RARE Book Notification")
-
-        GlStateManager.translate(0f,0f,-300f)
+        GlStateManager.translate(0f,0f,-400f)
     }
 
     @SubscribeEvent
