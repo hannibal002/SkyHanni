9966a6ab66b061b7c4972570b1dfe5f9bac97451
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-05-17 01:09:52 +0200

    a bit too late now, but version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "at.hannibal2.skyhanni"
-version = "0.25.Beta.26"
+version = "0.25.Beta.27"
 
 val gitHash by lazy {
     val baos = ByteArrayOutputStream()
--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -487,7 +487,7 @@ import org.apache.logging.log4j.Logger
     clientSideOnly = true,
     useMetadata = true,
     guiFactory = "at.hannibal2.skyhanni.config.ConfigGuiForgeInterop",
-    version = "0.25.Beta.26",
+    version = "0.25.Beta.27",
 )
 class SkyHanniMod {
 
