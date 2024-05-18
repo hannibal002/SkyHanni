dcaeb7a3c5dfeab24377f7556c793ea5c3d5c686
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-05-18 02:58:03 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/features/nether/VolcanoExplosivityDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/VolcanoExplosivityDisplay.kt
@@ -4,11 +4,9 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.TabListUpdateEvent
-import at.hannibal2.skyhanni.utils.CollectionUtils.nextAfter
 import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
 import at.hannibal2.skyhanni.utils.RenderUtils.renderString
-import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
-import at.hannibal2.skyhanni.utils.StringUtils.matches
+import at.hannibal2.skyhanni.utils.StringUtils.matchFirst
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -16,6 +14,10 @@ class VolcanoExplosivityDisplay {
 
     private val config get() = SkyHanniMod.feature.crimsonIsle
     private val patternGroup = RepoPattern.group("crimson.volcano")
+
+    /**
+     * REGEX-TEST:  Volcano: §r§8INACTIVE
+     */
     private val statusPattern by patternGroup.pattern(
         "tablistline",
         " *Volcano: (?<status>(?:§.)*\\S+)"
@@ -25,11 +27,8 @@ class VolcanoExplosivityDisplay {
     @SubscribeEvent
     fun onTabListUpdate(event: TabListUpdateEvent) {
         if (!isEnabled()) return
-        for (line in event.tabList) {
-            statusPattern.matchMatcher(line) {
-                display = "§bVolcano Explosivity§7: ${group("status")}"
-                break
-            }
+        event.tabList.matchFirst(statusPattern) {
+            display = "§bVolcano Explosivity§7: ${group("status")}"
         }
     }
 
