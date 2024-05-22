0a1323d64f12095a5e6f47ea48e0724efb69f17b
martimavocado <martim.cavaco@tutanota.com>
2024-05-22 20:41:53 +0100

    start dungeon phases

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -124,13 +124,12 @@ import at.hannibal2.skyhanni.features.dungeon.DungeonHideItems
 import at.hannibal2.skyhanni.features.dungeon.DungeonHighlightClickedBlocks
 import at.hannibal2.skyhanni.features.dungeon.DungeonLividFinder
 import at.hannibal2.skyhanni.features.dungeon.DungeonMilestonesDisplay
-import at.hannibal2.skyhanni.features.dungeon.DungeonPhaseHandler
+import at.hannibal2.skyhanni.features.dungeon.DungeonPhaseUtils
 import at.hannibal2.skyhanni.features.dungeon.DungeonRankTabListColor
 import at.hannibal2.skyhanni.features.dungeon.DungeonShadowAssassinNotification
 import at.hannibal2.skyhanni.features.dungeon.DungeonTeammateOutlines
 import at.hannibal2.skyhanni.features.dungeon.DungeonsRaceGuide
 import at.hannibal2.skyhanni.features.dungeon.HighlightDungeonDeathmite
-import at.hannibal2.skyhanni.features.dungeon.TerracottaPhase
 import at.hannibal2.skyhanni.features.event.UniqueGiftingOpportunitiesFeatures
 import at.hannibal2.skyhanni.features.event.diana.AllBurrowsList
 import at.hannibal2.skyhanni.features.event.diana.BurrowWarpHelper
@@ -886,7 +885,7 @@ class SkyHanniMod {
         loadModule(GlowingDroppedItems())
         loadModule(DungeonTeammateOutlines())
         loadModule(DungeonRankTabListColor())
-        loadModule(TerracottaPhase())
+        loadModule(DungeonPhaseUtils())
         loadModule(VolcanoExplosivityDisplay())
         loadModule(FixNEUHeavyPearls())
         loadModule(QuickCraftFeatures())
@@ -940,7 +939,6 @@ class SkyHanniMod {
         loadModule(ColdOverlay())
         loadModule(QuiverDisplay())
         loadModule(QuiverWarning())
-        loadModule(DungeonPhaseHandler())
 
         init()
 
--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonPhase.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonPhase.kt
@@ -1,8 +1,5 @@
 package at.hannibal2.skyhanni.features.dungeon
 
-import at.hannibal2.skyhanni.events.LorenzChatEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-
 enum class DungeonPhase {
     F6_TERRACOTTA,
     F6_GIANTS,
@@ -13,16 +10,27 @@ enum class DungeonPhase {
     F7_GOLDOR_2,
     F7_GOLDOR_3,
     F7_GOLDOR_4,
+    F7_GOLDOR_5,
     F7_NECRON,
     M7_WITHER_KING
 }
 
-class DungeonPhaseHandler {
-    @SubscribeEvent
-    fun onChat(event: LorenzChatEvent) {
-        if (!DungeonAPI.inDungeon()) return
-        when (DungeonAPI.dungeonFloor) {
-
-        }
-    }
-}
+// class DungeonPhaseHandler {
+//
+//     private val patternGroup = RepoPattern.group("dungeon.phases")
+//
+//     private val
+//
+//     @SubscribeEvent
+//     fun onChat(event: LorenzChatEvent) {
+//         if (!DungeonAPI.inDungeon()) return
+//         when (DungeonAPI.dungeonFloor) { //move to enum
+//             "F6", "M6" -> {
+//
+//             }
+//             "F7", "M7" -> {
+//
+//             }
+//         }
+//     }
+// }
