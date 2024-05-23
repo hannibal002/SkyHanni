140a10e1fb942143b66df6c5ce49a0b5db6c405e
Nealkitt <stellaarnott1@gmail.com>
2024-05-22 22:25:57 -0700

    Remove unused comments and stuff i forgot to clean up

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/floor7/TerminalWaypoints.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/floor7/TerminalWaypoints.kt
@@ -4,16 +4,15 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.DungeonStartEvent
 import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
+import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
 import at.hannibal2.skyhanni.test.GriffinUtils.drawWaypointFilled
 import at.hannibal2.skyhanni.utils.EntityUtils
 import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.RenderUtils.drawDynamicText
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
-import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import at.hannibal2.skyhanni.utils.toLorenzVec
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
 
 class TerminalWaypoints {
     private val config get() = SkyHanniMod.feature.dungeon
@@ -34,13 +33,9 @@ class TerminalWaypoints {
     fun onChat(event: LorenzChatEvent) {
         DungeonAPI.goldorTerminalPattern.matchMatcher(event.message){
             val playerName = group("playerName")
-//             println("matched $playerName")
             val playerEntity = EntityUtils.getAllEntities().filter { it is EntityPlayer }.firstOrNull{ it.name == playerName } ?: return
-//             println("found $playerEntity")
             val terminal = TerminalInfo.getClosestTerminal(playerEntity.position.toLorenzVec())
-//             println("$terminal is closest")
             terminal?.highlight = false
-//             println(terminal?.highlight)
         }
     }
     private fun isEnabled(): Boolean = DungeonAPI.inDungeon() && config.terminalWaypoints
