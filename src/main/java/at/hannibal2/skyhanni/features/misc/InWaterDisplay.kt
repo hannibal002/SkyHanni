1313139747f552062939afa651ca61ba69908c07
Nealkitt <stellaarnott1@gmail.com>
2024-05-25 12:39:25 -0700

    fix a huge oversight sorry i'm new

--- src/main/java/at/hannibal2/skyhanni/features/misc/InWaterDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/InWaterDisplay.kt
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.GuiRenderEvent
-import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.RenderUtils.renderStrings
 import net.minecraft.client.Minecraft
@@ -11,18 +10,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 class InWaterDisplay {
 
     private val config get() = SkyHanniMod.feature.misc.stranded
-    private var playerInWater = false
-
-    @SubscribeEvent
-    fun onTick(event: LorenzTickEvent) {
-        playerInWater = Minecraft.getMinecraft().thePlayer.isInWater
-    }
 
     @SubscribeEvent
     fun onRenderOverlay(event: GuiRenderEvent.GuiOverlayRenderEvent) {
         if (!isEnabled()) return
 
-        val text = "In Water: " + if (playerInWater) "§aTrue" else "§cFalse"
+        val text = "In Water: " + if (Minecraft.getMinecraft().thePlayer.isInWater) "§aTrue" else "§cFalse"
         config.inWaterPosition.renderStrings(listOf(text), posLabel = "In Water Display")
     }
 
