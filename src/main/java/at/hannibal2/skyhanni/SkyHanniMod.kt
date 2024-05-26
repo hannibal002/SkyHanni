c7d86491d8f5a040b48f121c4f2d6866b1ea8ecb
Cal <cwolfson58@gmail.com>
2024-05-26 21:59:25 +1000

    fix merge

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -491,7 +491,7 @@ import org.apache.logging.log4j.Logger
     clientSideOnly = true,
     useMetadata = true,
     guiFactory = "at.hannibal2.skyhanni.config.ConfigGuiForgeInterop",
-    version = "0.25",
+    version = "0.26.Beta.1",
 )
 class SkyHanniMod {
 
