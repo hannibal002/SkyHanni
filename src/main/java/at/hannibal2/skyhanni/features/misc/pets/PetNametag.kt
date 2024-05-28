e7257997cb8aeac75d9c3e0fcde1fe7c1f29f473
j10a1n15 <jan.leger@web.de>
2024-05-28 19:32:22 +0200

    thank you hypixel

--- src/main/java/at/hannibal2/skyhanni/features/misc/pets/PetNametag.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/pets/PetNametag.kt
@@ -3,9 +3,9 @@ package at.hannibal2.skyhanni.features.misc.pets
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.entity.EntityDisplayNameEvent
 import at.hannibal2.skyhanni.utils.LorenzUtils
-import at.hannibal2.skyhanni.utils.LorenzUtils.groupOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil.formatInt
-import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
+import at.hannibal2.skyhanni.utils.RegexUtils.groupOrNull
+import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.chat.Text.asComponent
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.entity.item.EntityArmorStand
@@ -16,12 +16,12 @@ class PetNametag {
     private val config get() = SkyHanniMod.feature.misc.pets.petNametag
 
     /**
-     * REGEX-TEST: §8[§7Lv99§8] §6Skirtwearer's Ammonite
-     * REGEX-TEST: §8[§7Lv100§8] §dSkirtwearer's Endermite§5 ✦
+     * REGEX-TEST: §8[§7Lv99§8] Ammonite
+     * REGEX-TEST: §8[§7Lv100§8] Endermite§5 ✦
      */
     private val pattern by RepoPattern.pattern(
         "feature.pet.nametag",
-        "(?<start>§8\\[§7Lv(?<lvl>\\d+)§8\\]) (?<rarity>§.)(?<player>[\\w_]+'s?) (?<pet>[\\w\\s]+)(?<skin>§. ✦)?"
+        "(?<start>§8\\[§7Lv(?<lvl>\\d+)§8\\]) (?<rarity>§.)(?<pet>[\\w\\s]+)(?<skin>§. ✦)?"
     )
 
     @SubscribeEvent
@@ -33,14 +33,12 @@ class PetNametag {
             val start = group("start")
             val lvl = group("lvl").formatInt()
             val rarity = group("rarity")
-            val player = group("player")
             val pet = group("pet")
             val skin = groupOrNull("skin") ?: ""
 
             var text = ""
             if (!config.hidePetLevel && (!config.hideMaxPetLevel || lvl == 100 || lvl == 200)) text += "$start "
             text += rarity
-            if (!config.hidePlayerName) text += "$player "
             text += pet + skin
 
             event.chatComponent = text.asComponent()
@@ -48,7 +46,7 @@ class PetNametag {
     }
 
     private fun isEnabled() = with(config) {
-        (hidePetLevel || hideMaxPetLevel || hidePlayerName) && LorenzUtils.inSkyBlock
+        (hidePetLevel || hideMaxPetLevel) && LorenzUtils.inSkyBlock
     }
 
 }
