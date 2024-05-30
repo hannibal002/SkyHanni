759ec708f697e20c3b2ed19d719b175a57d9531b
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-05-31 00:54:45 +0200

    Fixed rift slayer warning showing while hitting Oubliette Guard

--- src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerQuestWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerQuestWarning.kt
@@ -135,7 +135,9 @@ class SlayerQuestWarning {
                 )
             }
         }
-        return (getSlayerData().lastSlayerType == slayerType) && slayerType.clazz.isInstance(entity)
+        // workaround for rift mob that is unrelated to slayer
+        val isSlayer = slayerType.clazz.isInstance(entity) && entity.name != "Oubliette Guard"
+        return (getSlayerData().lastSlayerType == slayerType) && isSlayer
     }
 
     @SubscribeEvent
