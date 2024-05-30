8e92b30434097cd1147a52d0e5cabf6a4269fbe8
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-05-30 08:40:38 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/features/event/hoppity/HoppityUniqueEggLocations.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/hoppity/HoppityUniqueEggLocations.kt
@@ -1,8 +1,8 @@
 package at.hannibal2.skyhanni.features.event.hoppity
 
+import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.features.inventory.chocolatefactory.ChocolateFactoryAPI
 import at.hannibal2.skyhanni.test.command.ErrorManager
-import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.LocationUtils
 import at.hannibal2.skyhanni.utils.LocationUtils.distanceSqToPlayer
 import at.hannibal2.skyhanni.utils.LorenzUtils
@@ -10,30 +10,22 @@ import at.hannibal2.skyhanni.utils.LorenzVec
 
 object HoppityUniqueEggLocations {
 
-    private val islandEggLocations: List<LorenzVec>?
-        get() = HoppityEggLocator.getCurrentIslandEggLocations()
-
-    private val collectedEggLocations
+    private val collectedEggLocations: MutableMap<IslandType, MutableSet<LorenzVec>>?
         get() = ChocolateFactoryAPI.profileStorage?.collectedEggLocations
 
-    private fun getCurrentIslandCollectedEggs(): MutableSet<LorenzVec>? {
-        var currentSet = collectedEggLocations?.get(LorenzUtils.skyBlockIsland)
-        if (currentSet == null) {
-            currentSet = mutableSetOf<LorenzVec>()
-            collectedEggLocations?.set(LorenzUtils.skyBlockIsland, currentSet)
-        }
-        return currentSet
-    }
+    private fun getCurrentIslandCollectedEggs(): MutableSet<LorenzVec>? =
+        collectedEggLocations?.getOrPut(LorenzUtils.skyBlockIsland) { mutableSetOf<LorenzVec>() }
 
     fun saveNearestEgg() {
-        val location = islandEggLocations?.minByOrNull { it.distanceSqToPlayer() } ?: return
+        val location = HoppityEggLocator.getCurrentIslandEggLocations()
+            ?.minByOrNull { it.distanceSqToPlayer() } ?: return
         if (location.distanceSqToPlayer() > 100) {
-            ErrorManager.skyHanniError("Player far from any known egg location!",
+            ErrorManager.skyHanniError(
+                "Player far from any known egg location!",
                 "island" to LorenzUtils.skyBlockIsland,
                 "playerLocation" to LocationUtils.playerLocation(),
                 "closestKnownEgg" to location
             )
-            return
         }
 
         val collectedEggs = getCurrentIslandCollectedEggs() ?: return
