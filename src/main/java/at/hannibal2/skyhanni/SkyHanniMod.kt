4a6e2b16fc0171124c2a900751a34f71c5e470cd
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-06-01 13:13:37 +0200

    Feature: Beacon Power Display (#1901)
    Co-authored-by: CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -252,6 +252,7 @@ import at.hannibal2.skyhanni.features.garden.visitor.NPCVisitorFix
 import at.hannibal2.skyhanni.features.garden.visitor.VisitorAPI
 import at.hannibal2.skyhanni.features.garden.visitor.VisitorListener
 import at.hannibal2.skyhanni.features.garden.visitor.VisitorRewardWarning
+import at.hannibal2.skyhanni.features.gui.BeaconPower
 import at.hannibal2.skyhanni.features.gui.MovableHotBar
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard
 import at.hannibal2.skyhanni.features.gui.customscoreboard.ScoreboardPattern
@@ -936,6 +937,7 @@ class SkyHanniMod {
         loadModule(PestFinder)
         loadModule(PestParticleWaypoint())
         loadModule(StereoHarmonyDisplay())
+        loadModule(BeaconPower())
         loadModule(PestParticleLine())
         loadModule(SprayFeatures())
         loadModule(DojoRankDisplay())
