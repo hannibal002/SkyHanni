9c36d5cde75d0aca48e23cabeefbff9dbdcf8c7e
Cal <cwolfson58@gmail.com>
2024-06-03 15:55:15 +1000

    update config version

--- src/main/java/at/hannibal2/skyhanni/features/garden/fortuneguide/CaptureFarmingGear.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/fortuneguide/CaptureFarmingGear.kt
@@ -354,8 +354,8 @@ object CaptureFarmingGear {
     @SubscribeEvent
     fun onConfigUpdaterMigratorConfigFix(event: ConfigUpdaterMigrator.ConfigFixEvent) {
         //TODO check config version
-        event.move(46, "#profile.garden.fortune.carrotFortune", "#profile.garden.fortune.carrolyn.CARROT")
-        event.move(46, "#profile.garden.fortune.pumpkinFortune", "#profile.garden.fortune.carrolyn.PUMPKIN")
-        event.move(46, "#profile.garden.fortune.cocoaBeansFortune", "#profile.garden.fortune.carrolyn.COCOA_BEANS")
+        event.move(48, "#profile.garden.fortune.carrotFortune", "#profile.garden.fortune.carrolyn.CARROT")
+        event.move(48, "#profile.garden.fortune.pumpkinFortune", "#profile.garden.fortune.carrolyn.PUMPKIN")
+        event.move(48, "#profile.garden.fortune.cocoaBeansFortune", "#profile.garden.fortune.carrolyn.COCOA_BEANS")
     }
 }
