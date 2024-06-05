b6b05df7667860b6674504a9bf77a4239bdce90d
saga <sprepteis@gmail.com>
2024-06-05 11:10:44 -0300

    fixes2

--- src/main/java/at/hannibal2/skyhanni/features/inventory/FavoritePowerStone.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/FavoritePowerStone.kt
@@ -4,9 +4,10 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.MaxwellAPI
 import at.hannibal2.skyhanni.data.ProfileStorageData
 import at.hannibal2.skyhanni.events.GuiContainerEvent
+import at.hannibal2.skyhanni.events.InventoryCloseEvent
+import at.hannibal2.skyhanni.events.InventoryOpenEvent
 import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.name
 import at.hannibal2.skyhanni.utils.KeyboardManager
 import at.hannibal2.skyhanni.utils.LorenzColor
@@ -21,20 +22,19 @@ object FavoritePowerStone {
     private val config get() = SkyHanniMod.feature.inventory
     private val storage get() = ProfileStorageData.profileSpecific
 
-    private var highlightedSlots = mutableSetOf<Int>()
+    private var highlightedSlots = setOf<Int>()
+    private var inInventory = false
 
     @SubscribeEvent
     fun onBackgroundDrawn(event: GuiContainerEvent.BackgroundDrawnEvent) {
-        if (!isEnabled()) return
-        if (!MaxwellAPI.isThaumaturgyInventory(InventoryUtils.openInventoryName())) return
+        if (!isEnabled() || !inInventory) return
 
         highlightedSlots.forEach { event.gui.inventorySlots.inventorySlots[it] highlight LorenzColor.AQUA }
     }
 
     @SubscribeEvent
     fun onSlotClick(event: GuiContainerEvent.SlotClickEvent) {
-        if (!isEnabled() || !KeyboardManager.isShiftKeyDown()) return
-        if (!MaxwellAPI.isThaumaturgyInventory(InventoryUtils.openInventoryName())) return
+        if (!isEnabled() || !KeyboardManager.isShiftKeyDown() || !inInventory) return
 
         val displayName = event.item?.name?.removeColor()?.trim() ?: return
         val power = MaxwellAPI.getPowerByNameOrNull(displayName) ?: return
@@ -51,13 +51,25 @@ object FavoritePowerStone {
     }
 
     @SubscribeEvent
-    fun onInventoryUpdated(event: InventoryUpdatedEvent) {
-        if (!isEnabled()) return
-        if (!MaxwellAPI.isThaumaturgyInventory(event.inventoryName)) return
+    fun onInventoryOpen(event: InventoryOpenEvent) {
+        if (!isEnabled() || !MaxwellAPI.isThaumaturgyInventory(event.inventoryName)) return
 
+        inInventory = true
+    }
+
+    @SubscribeEvent
+    fun onInventoryUpdated(event: InventoryUpdatedEvent) {
+        if (!isEnabled() || !inInventory) return
+
+        highlightedSlots = setOf()
         event.inventoryItems.forEach { (slot, item) ->
             if (item.displayName in MaxwellAPI.favoritePowers) highlightedSlots += slot
         }
+    }
+
+    @SubscribeEvent
+    fun onInventoryClose(event: InventoryCloseEvent) {
+        inInventory = false
     }
 
     private fun isEnabled() = LorenzUtils.inSkyBlock && storage != null && config.favoritePowerStone
