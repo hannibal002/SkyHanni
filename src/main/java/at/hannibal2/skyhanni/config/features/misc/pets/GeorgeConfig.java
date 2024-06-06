02c4c1cbbe390642778a0dbd7d2bec812df2e7a6
Cal <cwolfson58@gmail.com>
2024-06-06 13:23:39 +1000

    fix build

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/pets/GeorgeConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/pets/GeorgeConfig.java
@@ -2,8 +2,8 @@ package at.hannibal2.skyhanni.config.features.misc.pets;
 
 import at.hannibal2.skyhanni.config.core.config.Position;
 import com.google.gson.annotations.Expose;
-import io.github.moulberry.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.moulberry.moulconfig.annotations.ConfigOption;
+import io.github.moulberry.notenoughupdates.core.config.annotations.ConfigEditorBoolean;
+import io.github.moulberry.notenoughupdates.core.config.annotations.ConfigOption;
 
 public class GeorgeConfig {
 
