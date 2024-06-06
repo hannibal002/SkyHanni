504ba6bf0d0ae4713edb28c3325077105c98a38d
Cal <cwolfson58@gmail.com>
2024-06-06 10:36:18 +1000

    fix merge

--- src/main/java/at/hannibal2/skyhanni/features/garden/BonusPestChanceDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/BonusPestChanceDisplay.kt
@@ -4,13 +4,16 @@ import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.ProfileJoinEvent
 import at.hannibal2.skyhanni.events.TabListUpdateEvent
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.CollectionUtils.addAsSingletonList
 import at.hannibal2.skyhanni.utils.RenderUtils.renderStringsAndItems
 import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
+@SkyHanniModule
 object BonusPestChanceDisplay {
+
     private val config get() = GardenAPI.config.bonusPestChanceDisplay
 
     private val patternGroup = RepoPattern.group("garden.pestbonuschancedisplay")
