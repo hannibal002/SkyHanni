7dff279c1cc6ef6f3a332c873496cb233ea22f6e
Cal <cwolfson58@gmail.com>
2024-06-06 14:06:17 +1000

    bring back fix

--- src/main/java/at/hannibal2/skyhanni/features/chat/WatchdogHider.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/WatchdogHider.kt
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.features.chat
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
 import at.hannibal2.skyhanni.data.ChatManager
 import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
@@ -50,6 +51,9 @@ object WatchdogHider {
             }
         }
     }
+
+    @SubscribeEvent
+    fun onConfigFix(event: ConfigUpdaterMigrator.ConfigFixEvent) {
+        event.move(3, "chat.watchDog", "chat.filterType.watchDog")
+    }
 }
-
-
