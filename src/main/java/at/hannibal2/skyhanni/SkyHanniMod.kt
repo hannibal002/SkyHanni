ed7f24ccc9553dbdeb32997475235f1da0292859
Cal <cwolfson58@gmail.com>
2024-06-06 13:30:23 +1000

    fix version number

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -76,7 +76,7 @@ import org.apache.logging.log4j.Logger
     clientSideOnly = true,
     useMetadata = true,
     guiFactory = "at.hannibal2.skyhanni.config.ConfigGuiForgeInterop",
-    version = "0.26.Beta.6",
+    version = "0.26.Beta.7",
 )
 class SkyHanniMod {
 
