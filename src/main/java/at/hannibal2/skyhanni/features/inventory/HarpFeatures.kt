c7e770ef8ae4db96d066c6906363252f5f5a8372
Cal <cwolfson58@gmail.com>
2024-06-08 15:15:41 +1000

    test wrong annotation

--- src/main/java/at/hannibal2/skyhanni/features/inventory/HarpFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/HarpFeatures.kt
@@ -1,7 +1,6 @@
 package at.hannibal2.skyhanni.features.inventory
 
 import at.hannibal2.skyhanni.SkyHanniMod
-import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
 import at.hannibal2.skyhanni.events.GuiContainerEvent
 import at.hannibal2.skyhanni.events.GuiKeyPressEvent
@@ -131,7 +130,7 @@ object HarpFeatures {
         unSetGUIScale()
     }
 
-    @HandleEvent
+    @SubscribeEvent
     fun onDisconnect(event: ClientDisconnectEvent) {
         if (!config.guiScale) return
         unSetGUIScale()
