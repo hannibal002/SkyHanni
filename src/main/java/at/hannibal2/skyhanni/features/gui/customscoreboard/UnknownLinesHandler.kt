a432e3e77275a26c856bb53ad2469b26c2e7ca3c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-06-12 16:18:27 +0200

    Fix: Unknown Lines not appearing instantly (#2063)

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/UnknownLinesHandler.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/UnknownLinesHandler.kt
@@ -174,7 +174,7 @@ object UnknownLinesHandler {
 
 
         /*
-         * handle broken scoreboard lines
+         * Handle broken scoreboard lines
          */
         confirmedUnknownLines.forEach { line ->
             if (!unconfirmedUnknownLines.contains(line)) {
