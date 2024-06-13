a08a3b99c932417faa242faa58cef5978d2df8c3
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-06-13 22:03:30 +0200

    fixed merge conflicts

--- src/main/java/at/hannibal2/skyhanni/test/SkyHanniDebugsAndTests.kt
+++ src/main/java/at/hannibal2/skyhanni/test/SkyHanniDebugsAndTests.kt
@@ -17,7 +17,6 @@ import at.hannibal2.skyhanni.features.garden.GardenNextJacobContest
 import at.hannibal2.skyhanni.features.garden.visitor.GardenVisitorColorNames
 import at.hannibal2.skyhanni.features.inventory.bazaar.BazaarApi.getBazaarData
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.test.GriffinUtils.drawWaypointFilled
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.CollectionUtils.editCopy
@@ -359,8 +358,8 @@ object SkyHanniDebugsAndTests {
             return
         }
 
-            val internalName = hand.getInternalNameOrNull()
-                ?: ErrorManager.skyHanniError("Internal name is null for item ${hand.name}")
+        val internalName = hand.getInternalNameOrNull()
+            ?: ErrorManager.skyHanniError("Internal name is null for item ${hand.name}")
 
         val rawInternalName = internalName.asString()
         OSUtils.copyToClipboard(rawInternalName)
