53d1f7774f3633084e050e4ec6da2eb3a048d5e3
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2024-06-13 11:04:43 +0000

    rough outline

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/ArrowStackWaypoints.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/ArrowStackWaypoints.kt
@@ -1,5 +1,10 @@
 package at.hannibal2.skyhanni.features.dungeon
 
+import at.hannibal2.skyhanni.utils.LorenzColor
+import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.events.LorenzChatEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
 /*
 [
    {
@@ -81,3 +86,48 @@ package at.hannibal2.skyhanni.features.dungeon
    }
 ]
  */
+
+// TODO: Add spawn locations
+enum class WitheredDragon(val spawnLocation: LorenzVec, val color: LorenzColor) {
+   ICE(LorenzVec(), LorenzColor.AQUA),
+   SOUL(LorenzVec(), LorenzColor.LIGHT_PURPLE),
+   FLAME(LorenzVec(), LorenzColor.GOLD),
+   APEX(LorenzVec(), LorenzColor.GREEN),
+   POWER(LorenzVec(), LorenzColor.DARK_RED)
+}
+
+data class ArrowStackLocation(val witheredDragonType: WitheredDragon, val location: LorenzVec)
+
+object ArrowStackWaypoints {
+
+   val locations = arrayOf(
+      ArrowStackLocation(WitheredDragon.ICE, LorenzVec(48,5,110)),
+      ArrowStackLocation(WitheredDragon.APEX, LorenzVec(25,6,119)),
+      ArrowStackLocation(WitheredDragon.FLAME, LorenzVec(53,4,90)),
+      ArrowStackLocation(WitheredDragon.POWER, LorenzVec(18,5,84)),
+      ArrowStackLocation(WitheredDragon.POWER, LorenzVec(10,6,83)),
+      ArrowStackLocation(WitheredDragon.SOUL, LorenzVec(31,5,97)),
+      ArrowStackLocation(WitheredDragon.SOUL, LorenzVec(81,5,99))
+   )
+
+   var closestLocation: ArrowStackLocation = null
+
+   // TODO: Detect class and the correct split to do
+   // TODO: Detect closest waypoint from the list is spawning and matches
+   // the current split
+   // TODO: Add regex that matches the spawn message
+   // TODO: Check correct floor etc.
+
+   @SubscribeEvent
+   fun onChat(event: LorenzChatEvent) {
+      // TODO: enable and disable feature
+      
+   }
+
+   @SubscribeEvent
+   fun onRenderWorld(event: LorenzRenderWorldEvent) {
+      // render waypoint and tracer to the correct location
+      // when standing in the correct location tracer to the spawn location of the dragon for lining up
+   }
+
+}
