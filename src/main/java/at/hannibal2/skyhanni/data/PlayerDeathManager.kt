2f93793252ed30cf4361bd03589c24c6cd0d22cc
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-06-18 23:30:24 +0200

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/data/PlayerDeathManager.kt
+++ src/main/java/at/hannibal2/skyhanni/data/PlayerDeathManager.kt
@@ -2,17 +2,19 @@ package at.hannibal2.skyhanni.data
 
 import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.events.LorenzEvent
-import at.hannibal2.skyhanni.utils.StringUtils.matchMatcher
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
+@SkyHanniModule
 object PlayerDeathManager {
 
     //§c ☠ §r§7§r§bZeroHazel§r§7 was killed by §r§8§lAshfang§r§7§r§7.
     private val deathMessagePattern by RepoPattern.pattern(
         "chat.player.death",
-        "§c ☠ §r§7§r§.(?<name>.+)§r§7 (?<reason>.+)"
+        "§c ☠ §r§7§r§.(?<name>.+)§r§7 (?<reason>.+)",
     )
 
     @SubscribeEvent
