d4da7597c48decf06bbe250959947512372f629e
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2024-06-19 10:29:22 +0200

    ChangelogJson to data class

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/other/ChangelogJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/other/ChangelogJson.kt
@@ -3,7 +3,7 @@ package at.hannibal2.skyhanni.data.jsonobjects.other
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class ChangelogJson(
+data class ChangelogJson(
     @Expose
     val body: String,
     @Expose
