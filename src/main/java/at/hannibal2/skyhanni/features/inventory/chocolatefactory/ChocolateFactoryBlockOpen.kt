507b7b5383ee8aa5a5ff07074c444b7e255badc1
martimavocado <martim.cavaco@tutanota.com>
2024-06-19 19:41:29 +0100

    hanni's suggestions

--- src/main/java/at/hannibal2/skyhanni/features/inventory/chocolatefactory/ChocolateFactoryBlockOpen.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/chocolatefactory/ChocolateFactoryBlockOpen.kt
@@ -1,14 +1,17 @@
 package at.hannibal2.skyhanni.features.inventory.chocolatefactory
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.config.ConfigGuiManager
 import at.hannibal2.skyhanni.data.PetAPI
 import at.hannibal2.skyhanni.events.MessageSendToServerEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
+import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object ChocolateFactoryBlockOpen {
@@ -25,14 +28,21 @@ object ChocolateFactoryBlockOpen {
         "/(?:cf|chocolatefactory)(?: .*)?",
     )
 
+    private var commandSentTimer = SimpleTimeMark.farPast()
+
     @SubscribeEvent
     fun onCommandSend(event: MessageSendToServerEvent) {
         if (!isEnabled()) return
         if (!commandPattern.matches(event.message)) return
+        if (commandSentTimer.passedSince() < 5.seconds) return
         if (PetAPI.currentPet?.startsWith("§dRabbit") == true) return
 
+        commandSentTimer = SimpleTimeMark.now()
         event.cancel()
-        ChatUtils.chat("Blocked opening the Chocolate Factory without a mythic §dRabbit §epet equipped.")
+        ChatUtils.clickableChat(
+            "Blocked opening the Chocolate Factory without a mythic §dRabbit §epet equipped. Click here to disable!",
+            { ConfigGuiManager.openConfigGui("mythic rabbit pet equipped") },
+        )
     }
 
     private fun isEnabled() = LorenzUtils.inSkyBlock && config.mythicRabbitRequirement
