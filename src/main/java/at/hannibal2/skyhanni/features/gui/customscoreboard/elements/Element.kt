42da904ada71b81d149cfb0793207cc90f804e04
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-06-22 00:39:13 +0200

    add todo
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/Element.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/Element.kt
@@ -4,4 +4,7 @@ abstract class Element {
     abstract fun getDisplay(): List<Any>
     abstract fun showWhen(): Boolean
     abstract val configLine: String
+
+    // TODO: Add Hover and Clickable Feedback to Lines
+    // Suggestion: https://discord.com/channels/997079228510117908/1226508204762992733
 }
