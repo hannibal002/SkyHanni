4153fb63447a8816e30a2078d81a1f405c124591
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-06-22 21:53:26 +0200

    example
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/Bank.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/Bank.kt
@@ -3,20 +3,17 @@ package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.displayConfig
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.informationFilteringConfig
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getBank
-import at.hannibal2.skyhanni.features.gui.customscoreboard.HIDDEN
 import at.hannibal2.skyhanni.features.rift.RiftAPI
 
 object Bank : ScoreboardElement() {
-    override fun getDisplay(): List<Any> {
+    override fun getDisplay(): String? {
         val bank = getBank()
 
-        return listOf(
-            when {
-                informationFilteringConfig.hideEmptyLines && (bank == "0" || bank == "0§7 / §60") -> HIDDEN
-                displayConfig.displayNumbersFirst -> "§6$bank Bank"
-                else -> "Bank: §6$bank"
-            },
-        )
+        return when {
+            informationFilteringConfig.hideEmptyLines && (bank == "0" || bank == "0§7 / §60") -> null
+            displayConfig.displayNumbersFirst -> "§6$bank Bank"
+            else -> "Bank: §6$bank"
+        }
     }
 
     override val configLine = "Bank: §6249M"
