a033aefcc0bd72d5272adc23d00073a367d03e3d
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-06-23 15:47:24 +0200

    added
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/gui/customscoreboard/EventsConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/gui/customscoreboard/EventsConfig.java
@@ -3,6 +3,7 @@ package at.hannibal2.skyhanni.config.features.gui.customscoreboard;
 import at.hannibal2.skyhanni.features.gui.customscoreboard.ScoreboardEvent;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorButton;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableList;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
@@ -18,6 +19,13 @@ public class EventsConfig {
     )
     @ConfigEditorDraggableList()
     public List<ScoreboardEvent> eventEntries = new ArrayList<>(ScoreboardEvent.defaultOption);
+
+    @ConfigOption(name = "Reset Events Priority", desc = "Reset the priority of all events.")
+    @ConfigEditorButton(buttonText = "Reset")
+    public Runnable reset = () -> {
+        eventEntries.clear();
+        eventEntries.addAll(ScoreboardEvent.defaultOption);
+    };
 
     @Expose
     @ConfigOption(name = "Show all active events", desc = "Show all active events in the scoreboard instead of the one with the highest priority.")
