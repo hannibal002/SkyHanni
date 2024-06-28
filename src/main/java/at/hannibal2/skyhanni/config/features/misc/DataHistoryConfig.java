a191e393f5129e0291adee024b4ebd7ec1987252
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-06-28 09:58:32 +0200

    typo

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/DataHistoryConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/DataHistoryConfig.java
@@ -7,7 +7,7 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class DataHistoryConfig {
     @Expose
-    @ConfigOption(name = "Purse History", desc = "Save your coins in the purse over time. See the history with §e/shpursehistory§7.")
+    @ConfigOption(name = "Purse History", desc = "Save your coins in the purse over time. View the history with §e/shpursehistory§7.")
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean purse = true;
