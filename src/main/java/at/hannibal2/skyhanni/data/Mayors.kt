f5c3c674c871c44673fd017a7600ee152e0b3a81
Nealkitt <stellaarnott1@gmail.com>
2024-06-29 08:31:53 -0700

    um like um yes um mayor um enum um yes

--- src/main/java/at/hannibal2/skyhanni/data/Mayors.kt
+++ src/main/java/at/hannibal2/skyhanni/data/Mayors.kt
@@ -12,25 +12,17 @@ enum class Mayor(
     vararg val perks: Perk,
 ) {
     AATROX("Aatrox", "§3", Perk.SLASHED_PRICING, Perk.SLAYER_XP_BUFF, Perk.PATHFINDER),
-    COLE("Cole", "§e", Perk.PROSPECTION, Perk.MINING_XP_BUFF, Perk.MINING_FIESTA),
-    DIANA("Diana", "§2", Perk.LUCKY, Perk.MYTHOLOGICAL_RITUAL, Perk.PET_XP_BUFF),
-    DIAZ("Diaz", "§6", Perk.BARRIER_STREET, Perk.SHOPPING_SPREE),
-    FINNEGAN("Finnegan", "§c", Perk.FARMING_SIMULATOR, Perk.PELT_POCALYPSE, Perk.GOATED),
-    FOXY(
-        "Foxy",
-        "§d",
-        Perk.SWEET_TOOTH,
-        Perk.BENEVOLENCE,
-        Perk.EXTRA_EVENT_MINING,
-        Perk.EXTRA_EVENT_FISHING,
-        Perk.EXTRA_EVENT_SPOOKY
-    ),
-    MARINA("Marina", "§b", Perk.FISHING_XP_BUFF, Perk.LUCK_OF_THE_SEA, Perk.FISHING_FESTIVAL),
+    COLE("Cole", "§e", Perk.PROSPECTION, Perk.MINING_XP_BUFF, Perk.MINING_FIESTA, Perk.MOLTEN_FORGE),
+    DIANA("Diana", "§2", Perk.LUCKY, Perk.MYTHOLOGICAL_RITUAL, Perk.PET_XP_BUFF, Perk.SHARING_IS_CARING),
+    DIAZ("Diaz", "§6", Perk.VOLUME_TRADING, Perk.SHOPPING_SPREE, Perk.STOCK_EXCHANGE, Perk.LONG_TERM_INVESTMENT),
+    FINNEGAN("Finnegan", "§c", Perk.FARMING_SIMULATOR, Perk.PELT_POCALYPSE, Perk.GOATED, Perk.BLOOMING_BUSINESS),
+    FOXY("Foxy", "§d", Perk.SWEET_BENEVOLENCE, Perk.A_TIME_FOR_GIVING, Perk.CHIVALROUS_CARNIVAL, Perk.EXTRA_EVENT_MINING, Perk.EXTRA_EVENT_FISHING, Perk.EXTRA_EVENT_SPOOKY),
+    MARINA("Marina", "§b", Perk.FISHING_XP_BUFF, Perk.LUCK_OF_THE_SEA, Perk.FISHING_FESTIVAL, Perk.DOUBLE_TROUBLE),
     PAUL("Paul", "§c", Perk.MARAUDER, Perk.EZPZ, Perk.BENEDICTION),
 
     SCORPIUS("Scorpius", "§d", Perk.BRIBE, Perk.DARKER_AUCTIONS),
     JERRY("Jerry", "§d", Perk.PERKPOCALYPSE, Perk.STATSPOCALYPSE, Perk.JERRYPOCALYPSE),
-    DERPY("Derpy", "§d", Perk.TURBO_MINIONS, Perk.AH_CLOSED, Perk.DOUBLE_MOBS_HP, Perk.MOAR_SKILLZ),
+    DERPY("Derpy", "§d", Perk.TURBO_MINIONS, Perk.AH_TAX, Perk.DOUBLE_MOBS_HP, Perk.MOAR_SKILLZ),
 
     UNKNOWN("Unknown", "§c"),
     DISABLED("§cDisabled", "§7"),
@@ -108,24 +100,30 @@ enum class Perk(val perkName: String) {
     PROSPECTION("Prospection"),
     MINING_XP_BUFF("Mining XP Buff"),
     MINING_FIESTA("Mining Fiesta"),
+    MOLTEN_FORGE("Molten Forge"),
 
     // Diana
     LUCKY("Lucky!"),
     MYTHOLOGICAL_RITUAL("Mythological Ritual"),
     PET_XP_BUFF("Pet XP Buff"),
+    SHARING_IS_CARING("Sharing is Caring"),
 
     // Diaz
-    BARRIER_STREET("Barrier Street"),
     SHOPPING_SPREE("Shopping Spree"),
+    VOLUME_TRADING("Volume Trading"),
+    STOCK_EXCHANGE("Stock Exchange"),
+    LONG_TERM_INVESTMENT("Long Term Investment"),
 
     // Finnegan
     FARMING_SIMULATOR("Farming Simulator"),
     PELT_POCALYPSE("Pelt-pocalypse"),
     GOATED("GOATed"),
+    BLOOMING_BUSINESS("Blooming Business"),
 
     // Foxy
-    SWEET_TOOTH("Sweet Tooth"),
-    BENEVOLENCE("Benevolence"),
+    SWEET_BENEVOLENCE("Sweet Benevolence"),
+    A_TIME_FOR_GIVING("A Time for Giving"),
+    CHIVALROUS_CARNIVAL("Chivalrous Carnival"),
     EXTRA_EVENT_MINING("Extra Event (Mining)"),
     EXTRA_EVENT_FISHING("Extra Event (Fishing)"),
     EXTRA_EVENT_SPOOKY("Extra Event (Spooky)"),
@@ -134,6 +132,7 @@ enum class Perk(val perkName: String) {
     FISHING_XP_BUFF("Fishing XP Buff"),
     LUCK_OF_THE_SEA("Luck of the Sea 2.0"),
     FISHING_FESTIVAL("Fishing Festival"),
+    DOUBLE_TROUBLE("Double Trouble"),
 
     // Paul
     MARAUDER("Marauder"),
@@ -151,7 +150,9 @@ enum class Perk(val perkName: String) {
 
     // Derpy
     TURBO_MINIONS("TURBO MINIONS!!!"),
-    AH_CLOSED("AH CLOSED!!!"),
+    AH_TAX("MOAR TAX!!!"),
+
+    // I don't know what the perk is actually gonna be named
     DOUBLE_MOBS_HP("DOUBLE MOBS HP!!!"),
     MOAR_SKILLZ("MOAR SKILLZ!!!"),
     ;
