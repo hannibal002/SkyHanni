801963e9b0dbea5c756af14e76772cf3a4b29e39
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-06-30 18:59:54 +0200

    add sizelimitedcache and modify sizelimitedset to use it
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/SizeLimitedSet.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SizeLimitedSet.kt
@@ -1,33 +1,33 @@
 package at.hannibal2.skyhanni.utils
 
-import com.google.common.cache.CacheBuilder
 import com.google.common.cache.RemovalCause
-import com.google.common.cache.RemovalNotification
 
 class SizeLimitedSet<T : Any>(
     maxSize: Long,
-    private val removalListener: (T?, RemovalCause) -> Unit = { _, _ -> },
+    private val removalListener: (T, RemovalCause) -> Unit = { _, _ -> },
 ) : Iterable<T> {
 
-    private val cache = CacheBuilder.newBuilder()
-        .maximumSize(maxSize)
-        .removalListener { notification: RemovalNotification<T, Unit> ->
-            removalListener(notification.key, notification.cause)
-        }.build<T, Unit>()
+    private val cache = SizeLimitedCache<T, Unit>(maxSize) { key, _, cause -> key?.let { removalListener(it, cause) } }
 
-    fun add(key: T) = cache.put(key, Unit)
+    fun add(element: T) {
+        cache[element] = Unit
+    }
 
     operator fun plusAssign(element: T) = add(element)
 
-    fun remove(key: T) = cache.invalidate(key)
+    fun addIfAbsent(element: T) {
+        if (!contains(element)) add(element)
+    }
+
+    fun remove(element: T) = cache.remove(element)
 
     operator fun minusAssign(element: T) = remove(element)
 
-    operator fun contains(key: T): Boolean = cache.getIfPresent(key) != null
+    operator fun contains(element: T): Boolean = cache.containsKey(element)
 
-    fun clear() = cache.invalidateAll()
+    fun clear() = cache.clear()
 
-    fun toSet(): Set<T> = cache.asMap().keys.let { keys ->
+    fun toSet(): Set<T> = cache.keys().let { keys ->
         if (keys.isEmpty()) emptySet() else keys.toSet()
     }
 
