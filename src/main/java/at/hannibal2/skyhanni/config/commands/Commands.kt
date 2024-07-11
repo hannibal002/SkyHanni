1a2a5c6a047449bbf1a36e29a26c6cc0ae9d8f1c
minhperry <minhpt2002@gmail.com>
2024-07-11 15:32:32 +0200

    feat: Colors and formatting help in chat.

--- src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
@@ -93,6 +93,7 @@ import at.hannibal2.skyhanni.utils.chat.ChatClickActionManager
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPatternGui
 import net.minecraft.command.ICommandSender
 import net.minecraft.util.BlockPos
+import net.minecraft.util.ChatComponentText
 import net.minecraftforge.client.ClientCommandHandler
 
 object Commands {
@@ -336,6 +337,10 @@ object Commands {
             "shtpinfested",
             "Teleports you to the nearest infested plot",
         ) { PestFinder.teleportNearestInfestedPlot() }
+        registerCommand(
+            "shcolors",
+           "Prints a list of all Minecraft color & formatting codes",
+        ) { printColorCodeList() }
     }
 
     private fun usersBugFix() {
@@ -599,6 +604,62 @@ object Commands {
             if (VisualWordGui.sbeConfigPath.exists()) VisualWordGui.drawImport = true
             SkyHanniMod.screenToOpen = VisualWordGui()
         }
+    }
+
+    @JvmStatic
+    private fun printColorCodeList() {
+        ChatUtils.chat(
+            ChatComponentText(
+                "§c=================== General Colors ===================\n" +
+                "§f&0 = §0Black              §f&1 = §1Dark Blue\n" +
+                "§f&2 = §2Dark Green      §f&3 = §3Dark Aqua\n" +
+                "§f&4 = §4Dark Red         §f&5 = §5Dark Purple\n" +
+                "§f&6 = §6Gold               §f&7 = §7Gray\n" +
+                "§f&8 = §8Dark Gray       §f&9 = §9Blue\n" +
+                "§f&a = §aGreen            §f&b = §bAqua\n" +
+                "§f&c = §cRed               §f&d = §dLight Purple\n" +
+                "§f&e = §eYellow            §f&f = §fWhite\n" +
+                "§f&z = §zChroma §r(needs to enable chroma setting)\n" +
+                "§c================= Formatting Codes ==================\n" +
+                "§f&k = Obfuscated (like this: §khellspawn§r)\n" +
+                "§f&l = §lBold           §r&m = §mStrikethrough \n" +
+                "§f&o = §oItalic            §r&n = §nUnderline\n" +
+                "§f&r = Reset\n"+
+                "§c==================================================="
+            )
+        )
+        ChatUtils.clickableChat(
+            "§eClick to view extra info about colors and formatting.",
+            onClick = { printColorCodesExtra() },
+            "§zClick to see more!",
+            prefix = false
+        )
+    }
+
+    @JvmStatic
+    private fun printColorCodesExtra() {
+        ChatUtils.chat("§c================= Formatting Extra ==================", false)
+        ChatUtils.clickableLinkChat(
+            "§#§6§a§e§e§4§8§/[Click here to view codes on minecraft.wiki]",
+            "https://minecraft.wiki/w/Formatting_codes#Color_codes",
+            "§fOpen minecraft.wiki",
+            false,
+            false
+        )
+        ChatUtils.chat(
+            "§eYou can also uses SkyHanni's system for any colors. " +
+            "This is different from chroma. " +
+            "Simply type &#&f&f&9&a&2&e&/ for color §#§f§f§9§a§2§e§/#ff9a2e§e " +
+            "(adds & before every characters including #, ends with '&/').",
+            false
+        )
+        ChatUtils.clickableLinkChat(
+            "§z[Click here to open color picker color-hex.com]",
+            url = "https://www.color-hex.com",
+            "§fOpen color-hex.com",
+            prefix = false
+        )
+        ChatUtils.chat("§c===================================================", false)
     }
 
     private fun clearFarmingItems() {
