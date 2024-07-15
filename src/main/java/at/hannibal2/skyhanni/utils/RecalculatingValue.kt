ab495eb069569f1e4a6f9abbd3dd19d89e76367c
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-07-15 18:15:03 +0200

    fix
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/RecalculatingValue.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/RecalculatingValue.kt
@@ -6,7 +6,7 @@ import kotlin.time.Duration
 
 class RecalculatingValue<T>(private val expireTime: Duration, private val calculation: () -> T) : ReadOnlyProperty<Any?, T> {
 
-    private var currentValue: T? = null
+    private var currentValue: Any? = UNINITIALIZED_VALUE
     private var lastAccessTime = SimpleTimeMark.farPast()
 
     @Deprecated("use \"by RecalculatingValue\" instead")
@@ -15,9 +15,14 @@ class RecalculatingValue<T>(private val expireTime: Duration, private val calcul
             currentValue = calculation()
             lastAccessTime = SimpleTimeMark.now()
         }
-        return currentValue!!
+        @Suppress("UNCHECKED_CAST")
+        return currentValue as T
     }
 
     @Suppress("DEPRECATION")
     override fun getValue(thisRef: Any?, property: KProperty<*>): T = getValue()
+
+    companion object {
+        private val UNINITIALIZED_VALUE = Any()
+    }
 }
