40e0182cd2c8e898c21649a2f3c2e7c57f869b18
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-07-15 18:21:30 -0400

    Fix V2 of the powder filter to work with new patterns

--- src/main/java/at/hannibal2/skyhanni/features/chat/PowderMiningChatFilter.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/PowderMiningChatFilter.kt
@@ -16,6 +16,7 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.groupOrNull
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
+import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 @SkyHanniModule
@@ -25,6 +26,8 @@ object PowderMiningChatFilter {
     private val gemstoneConfig get() = config.gemstoneFilterConfig
 
     val patternGroup = RepoPattern.group("filter.powdermining")
+
+    private var unclosedRewards = false;
 
     /**
      * REGEX-TEST: §aYou uncovered a treasure chest!
@@ -43,147 +46,192 @@ object PowderMiningChatFilter {
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§b+286 §r§aMithril Powder.
-     * REGEX-TEST: §aYou received §r§b+1,264 §r§aGemstone Powder.
+     * REGEX-TEST: §e§l▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+     * REGEX-TEST: §d§l▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+     */
+    private val chestWrapperPattern by patternGroup.pattern(
+        "powder.chestwrapper",
+        "^§[ed]§l▬{64}\$"
+    )
+
+    /**
+     * REGEX-TEST:   §r§6§lCHEST LOCKPICKED
+     */
+    private val lockPickedPattern by patternGroup.pattern(
+        "powder.picked",
+        ".*§r§6§lCHEST LOCKPICKED.*"
+    )
+
+    /**
+     * REGEX-TEST:   §r§5§lLOOT CHEST COLLECTED
+     */
+    private val lootChestCollectedPattern by patternGroup.pattern(
+        "lootchest.collected",
+        ".*§r§5§lLOOT CHEST COLLECTED.*"
+    )
+
+    /**
+     * REGEX-TEST:   §r§a§lREWARDS
+     */
+    private val rewardHeaderPattern by patternGroup.pattern(
+        "reward.header",
+        ".*§r§[af]§lREWARDS.*"
+    )
+
+    /**
+     * REGEX-TEST: §r§2Mithril Powder §r§8x153
+     * REGEX-TEST: §r§dGemstone Powder §r§8x537
      */
     private val powderRewardPattern by patternGroup.pattern(
         "reward.powder",
-        "§aYou received §r§b\\+(?<amount>[\\d|,]+) §r§a(?:Mithril|Gemstone) Powder.",
+        "§r§[d2](?:Gemstone|Mithril) Powder( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§b+4 Diamond Essence§r§a.
-     * REGEX-TEST: §aYou received §r§6+3 Gold Essence§r§a.
+     * REGEX-TEST: §r§dGold Essence
+     * REGEX-TEST: §r§dGold Essence §r§8x3
+     * REGEX-TEST: §r§dDiamond Essence §r§8x2
+     * REGEX-TEST: §r§dDiamond Essence
      */
     private val essenceRewardPattern by patternGroup.pattern(
         "reward.essence",
-        "§aYou received §r(?:§6|§b)\\+(?<amount>\\d+) (?:Gold|Diamond) Essence§r§a.",
+        "§r§d(?:Gold|Diamond) Essence( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f2 §r§9Ascension Rope§r§a.
+     * REGEX-TEST: §r§9Ascension Rope
      */
     private val ascensionRopeRewardPattern by patternGroup.pattern(
         "reward.ascensionrope",
-        "§aYou received §r§f\\d+ §r§9Ascension Rope§r§a.",
+        "§r§9Ascension Rope( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f3 §r§aWishing Compass§r§a.
+     * REGEX-TEST: §r§aWishing Compass
      */
     private val wishingCompassRewardPattern by patternGroup.pattern(
         "reward.wishingcompass",
-        "§aYou received §r§f\\d+ §r§aWishing Compass§r§a.",
+        "§r§aWishing Compass( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f2 §r§aOil Barrel§r§a.
+     * REGEX-TEST: §r§aOil Barrel
      */
     private val oilBarrelRewardPattern by patternGroup.pattern(
         "reward.oilbarrel",
-        "§aYou received §r§f\\d+ §r§aOil Barrel§r§a.",
+        "§r§aOil Barrel( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§fPrehistoric Egg§§a.
+     * REGEX-TEST: §r§fPrehistoric Egg
      */
     private val prehistoricEggPattern by patternGroup.pattern(
         "reward.prehistoricegg",
-        "§aYou received §r§f\\d+ §r§fPrehistoric Egg§r§a.",
+        "§r§fPrehistoric Egg( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§5Pickonimbus 2000§r§a.
+     * REGEX-TEST: §r§5Pickonimbus 2000
      */
     private val pickonimbusPattern by patternGroup.pattern(
         "reward.pickonimbus",
-        "§aYou received §r§f\\d+ §r§5Pickonimbus 2000§r§a.",
+        "§r§5Pickonimbus 2000( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§6Jungle Heart§r§a.
+     * REGEX-TEST: §r§6Jungle Heart
      */
     private val jungleHeartPattern by patternGroup.pattern(
         "reward.jungleheart",
-        "§aYou received §r§f\\d+ §r§6Jungle Heart§r§a.",
+        "§r§6Jungle Heart( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f10 §r§aSludge Juice§r§a.
+     * REGEX-TEST: §r§aSludge Juice
      */
     private val sludgeJuicePattern by patternGroup.pattern(
         "reward.sludgejuice",
-        "§aYou received §r§f\\d+ §r§aSludge Juice§r§a.",
+        "§r§aSludge Juice( §r§8x(?<amount>[\\d,]+))?",
     )
 
-    // Why is the Yoggie message missing an §r before the §a.?
-    // The world may never know.
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§bYoggie§a.
+     * REGEX-TEST: §r§aYoggie
      */
     private val yoggiePattern by patternGroup.pattern(
         "reward.yoggie",
-        "§aYou received §r§f\\d+ §r§bYoggie§a.",
+        "§r§aYoggie( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§9FTX 3070§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9Synthetic Heart§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9Control Switch§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9Robotron Reflector§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9Electron Transmitter§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9Superlite Motor§r§a.
+     * REGEX-TEST: §r§9FTX 3070
+     * REGEX-TEST: §r§9Synthetic Heart
+     * REGEX-TEST: §r§9Control Switch
+     * REGEX-TEST: §r§9Robotron Reflector
+     * REGEX-TEST: §r§9Electron Transmitter
+     * REGEX-TEST: §r§9Superlite Motor
      */
     private val robotPartsPattern by patternGroup.pattern(
         "reward.robotparts",
-        "§aYou received §r§f\\d+ §r§9(?:FTX 3070|Synthetic Heart|Control Switch|Robotron Reflector|Electron Transmitter|Superlite Motor)§r§a.",
+        "§r§9(?:FTX 3070|Synthetic Heart|Control Switch|Robotron Reflector|Electron Transmitter|Superlite Motor)( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§5Treasurite§r§a.
+     * REGEX-TEST: §r§5Treasurite
      */
     private val treasuritePattern by patternGroup.pattern(
         "reward.treasurite",
-        "§aYou received §r§f\\d+ §r§5Treasurite§r§a.",
+        "§r§5Treasurite( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f1 §r§9§r§cRed Goblin Egg§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9§r§3Blue Goblin Egg§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9Goblin Egg§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§a§r§aGreen Goblin Egg§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9§r§eYellow Goblin Egg§r§a.
+     * REGEX-TEST: §r§9§r§cRed Goblin Egg
+     * REGEX-TEST: §r§9§r§3Blue Goblin Egg
+     * REGEX-TEST: §r§9Goblin Egg
+     * REGEX-TEST: §r§9Goblin Egg §r§8x2
+     * REGEX-TEST: §r§a§r§aGreen Goblin Egg
+     * REGEX-TEST: §r§9§r§eYellow Goblin Egg
      */
     private val goblinEggPattern by patternGroup.pattern(
         "reward.goblineggs",
-        "§aYou received §r§f\\d+ (?:§.)*(?<color>[a-zA-Z]+)? ?Goblin Egg§r§a.",
+        "(?:§.)*(?<color>[a-zA-Z]+)? ?Goblin Egg( §r§8x(?<amount>[\\d,]+))?",
     )
 
     /**
-     * REGEX-TEST: §aYou received §r§f24 §r§f❈ Rough Amethyst Gemstone§r§a.
-     * REGEX-TEST: §aYou received §r§f4 §r§a❈ Flawed Amethyst Gemstone§r§a.
-     * REGEX-TEST: §aYou received §r§f1 §r§9⸕ Fine Amber Gemstone§r§a.
+     * REGEX-TEST: §r§f❈ Rough Amethyst Gemstone §r§8x24
+     * REGEX-TEST: §r§a❈ Flawed Amethyst Gemstone §r§8x4
+     * REGEX-TEST: §r§9⸕ Fine Amber Gemstone
      */
     private val gemstonePattern by patternGroup.pattern(
         "reward.gemstone",
-        "§aYou received §r§f(?<amount>[\\d,]+) §r§[fa9][❤❈☘⸕✎✧] (?<tier>Rough|Flawed|Fine) (?<gem>Ruby|Amethyst|Jade|Amber|Sapphire|Topaz) Gemstone§r§a.",
+        "§r§[fa9][❤❈☘⸕✎✧] (?<tier>Rough|Flawed|Fine) (?<gem>Ruby|Amethyst|Jade|Amber|Sapphire|Topaz) Gemstone( §r§8x(?<amount>[\\d,]+))?",
     )
 
     fun block(message: String): String {
 
-        uncoverChestPattern.matchMatcher(message) {
-            return "powder_mining_chest"
+        // Generic "you uncovered a chest" message
+        if (uncoverChestPattern.matches(message)) return "powder_mining_chest"
+        // Breaking power warning
+        if (breakingPowerPattern.matches(message) && gemstoneConfig.strongerToolMessages) return "stronger_tool"
+        // Closing or opening a reward 'loop' with the spam of ▬
+        if (chestWrapperPattern.matches(message)) {
+            unclosedRewards = !unclosedRewards
+            return "reward_wrapper"
         }
 
-        //Breaking power warning
-        breakingPowerPattern.matchMatcher(message) {
-            if (gemstoneConfig.strongerToolMessages) return "stronger_tool"
-        }
+        if (!unclosedRewards) return "no_match"
+        if (lockPickedPattern.matches(message)) return "powder_chest_lockpicked"
+        if (lootChestCollectedPattern.matches(message)) return "loot_chest_opened"
+        if (rewardHeaderPattern.matches((message))) return "powder_reward_header"
+
+        // All powder and loot chest rewards start with 4 spaces
+        // To simplify regex statements, this check is done outside
+        if (!message.startsWith("    ")) return "no_match"
+        val ssMessage = message.substring(4);
 
         //Powder
-        powderRewardPattern.matchMatcher(message) {
+        powderRewardPattern.matchMatcher(ssMessage) {
             if (config.powderFilterThreshold == 20000) return "powder_mining_powder"
-            val amountStr = groupOrNull("amount") ?: ""
+            val amountStr = groupOrNull("amount") ?: "1"
             if (amountStr.isNotEmpty() && config.powderFilterThreshold > 0) {
                 val amountParsed = amountStr.replace(",", "").toInt()
                 if (amountParsed < config.powderFilterThreshold) return "powder_mining_powder"
@@ -191,67 +239,36 @@ object PowderMiningChatFilter {
         }
 
         //Essence
-        essenceRewardPattern.matchMatcher(message) {
+        essenceRewardPattern.matchMatcher(ssMessage) {
             if (config.essenceFilterThreshold == 10) return "powder_mining_essence"
-            val amountStr = groupOrNull("amount") ?: ""
+            val amountStr = groupOrNull("amount") ?: "1"
             if (amountStr.isNotEmpty() && config.essenceFilterThreshold > 0) {
                 val amountParsed = amountStr.toInt()
                 if (amountParsed < config.essenceFilterThreshold) return "powder_mining_essence"
             }
         }
 
-        //Ascension Rope
-        ascensionRopeRewardPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(ASCENSION_ROPE)) return "powder_mining_ascension_rope"
-        }
-
-        //Wishing Compass
-        wishingCompassRewardPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(WISHING_COMPASS)) return "powder_mining_wishing_compass"
-        }
-
-        //Oil Barrel
-        oilBarrelRewardPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(OIL_BARREL)) return "powder_mining_oil_barrel"
-        }
-
-        //Prehistoric Egg
-        prehistoricEggPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(PREHISTORIC_EGG)) return "powder_mining_prehistoric_egg"
-        }
-
-        //Pickonimbus 2000
-        pickonimbusPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(PICKONIMBUS)) return "powder_mining_pickonimbus"
-        }
-
-        //Jungle Heart
-        jungleHeartPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(JUNGLE_HEART)) return "powder_mining_jungle_heart"
-        }
-
-        //Sludge Juice
-        sludgeJuicePattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(SLUDGE_JUICE)) return "powder_mining_sludge_juice"
-        }
-
-        //Yoggie
-        yoggiePattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(YOGGIE)) return "powder_mining_yoggie"
-        }
-
-        //Robot Parts
-        robotPartsPattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(ROBOT_PARTS)) return "powder_mining_robot_parts"
-        }
-
-        //Treasurite
-        treasuritePattern.matchMatcher(message) {
-            if (config.simplePowderMiningTypes.contains(TREASURITE)) return "powder_mining_treasurite"
+        //All the 'simple' boolean rewards
+        val rewardPatterns = mapOf(
+            ascensionRopeRewardPattern to ASCENSION_ROPE to "powder_mining_ascension_rope",
+            wishingCompassRewardPattern to WISHING_COMPASS to "powder_mining_wishing_compass",
+            oilBarrelRewardPattern to OIL_BARREL to "powder_mining_oil_barrel",
+            prehistoricEggPattern to PREHISTORIC_EGG to "powder_mining_prehistoric_egg",
+            pickonimbusPattern to PICKONIMBUS to "powder_mining_pickonimbus",
+            jungleHeartPattern to JUNGLE_HEART to "powder_mining_jungle_heart",
+            sludgeJuicePattern to SLUDGE_JUICE to "powder_mining_sludge_juice",
+            yoggiePattern to YOGGIE to "powder_mining_yoggie",
+            robotPartsPattern to ROBOT_PARTS to "powder_mining_robot_parts",
+            treasuritePattern to TREASURITE to "powder_mining_treasurite"
+        )
+        for ((patternToReward, returnReason) in rewardPatterns) {
+            if (patternToReward.first.matches(ssMessage) && config.simplePowderMiningTypes.contains(patternToReward.second)) {
+                return returnReason
+            }
         }
 
         //Goblin Eggs
-        goblinEggPattern.matchMatcher(message) {
+        goblinEggPattern.matchMatcher(ssMessage) {
             if (config.goblinEggs != PowderMiningFilterConfig.GoblinEggFilterEntry.SHOW_ALL) {
                 if (config.goblinEggs == PowderMiningFilterConfig.GoblinEggFilterEntry.HIDE_ALL) return "powder_mining_goblin_eggs"
 
@@ -283,7 +300,7 @@ object PowderMiningChatFilter {
         }
 
         //Gemstones
-        gemstonePattern.matchMatcher(message) {
+        gemstonePattern.matchMatcher(ssMessage) {
             val gemStr = groupOrNull("gem")?.lowercase() ?: ""
             val tierStr = groupOrNull("tier")?.lowercase() ?: ""
 
