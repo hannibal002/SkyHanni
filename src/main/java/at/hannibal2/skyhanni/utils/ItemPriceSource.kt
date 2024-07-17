f51a0d02689e011167ab275c9509ec0920699fe4
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-07-17 10:58:34 +0200

    Changed back the item source names to be short.

--- src/main/java/at/hannibal2/skyhanni/utils/ItemPriceSource.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/ItemPriceSource.kt
@@ -1,8 +1,8 @@
 package at.hannibal2.skyhanni.utils
 
 enum class ItemPriceSource(val displayName: String) {
-    BAZAAR_INSTANT_BUY("Instant Buy/Sell Offer"), // Sell Offer
-    BAZAAR_INSTANT_SELL("Instant Sell/Buy Order"), // Buy Order
+    BAZAAR_INSTANT_BUY("Instant Buy"), // Sell Offer
+    BAZAAR_INSTANT_SELL("Instant Sell"), // Buy Order
     NPC_SELL("NPC Sell"),
     ;
 
