8c7eacf0277a1b20d100f22db08d40b93abd6278
Nealkitt <stellaarnott1@gmail.com>
2024-07-19 17:06:38 -0700

    people deserve to be tortured

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/MiscConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/MiscConfig.java
@@ -134,6 +134,12 @@ public class MiscConfig {
     @FeatureToggle
     public boolean brewingStandOverlay = true;
 
+    @Expose
+    @ConfigOption(name = "Crash On Death", desc = "Crash every time you die in Skyblock")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    public boolean crashOnDeath = false;
+
     // TODO move into scoreboard accordion
     @Expose
     @ConfigOption(name = "Red Scoreboard Numbers", desc = "Hide the red scoreboard numbers on the right side of the screen.")
---
+++ src/main/java/at/hannibal2/skyhanni/features/misc/CrashOnDeath.kt
@@ -0,0 +1,34 @@
+package at.hannibal2.skyhanni.features.misc
+
+
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.LorenzChatEvent
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.LorenzUtils
+import at.hannibal2.skyhanni.utils.RegexUtils.matches
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import net.minecraft.client.Minecraft
+import net.minecraft.crash.CrashReport
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+
+@SkyHanniModule
+object CrashOnDeath {
+    private val config get() = SkyHanniMod.feature.misc
+
+    //
+    private val pattern by RepoPattern.pattern(
+        "ownplayer.death.chat",
+        "§c ☠ §r§7You (?<reason>.+)",
+    )
+
+    @SubscribeEvent
+    fun onChat(event: LorenzChatEvent) {
+        if (!isEnabled()) return
+
+        if (pattern.matches(event.message)) {
+            Minecraft.getMinecraft().crashed(CrashReport("Not Reading", Throwable("Don't toggle all the Options")))
+        }
+    }
+    private fun isEnabled() = LorenzUtils.inSkyBlock && config.crashOnDeath
+}
