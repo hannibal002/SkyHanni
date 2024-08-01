8a281cd07ea82ea9b880049803445bccfdfb165a
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-08-01 10:21:26 +0200

    Added /shdebugprice command.

---
+++ src/main/java/at/hannibal2/skyhanni/utils/ItemPriceUtils.kt
@@ -0,0 +1,60 @@
+package at.hannibal2.skyhanni.utils
+
+import at.hannibal2.skyhanni.features.inventory.bazaar.BazaarApi.getBazaarData
+import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
+import at.hannibal2.skyhanni.utils.NEUInternalName.Companion.asInternalName
+import at.hannibal2.skyhanni.utils.NEUItems.getItemStackOrNull
+import at.hannibal2.skyhanni.utils.NEUItems.getLowestBinOrNull
+import at.hannibal2.skyhanni.utils.NEUItems.getNpcPriceOrNull
+import at.hannibal2.skyhanni.utils.NEUItems.getPrice
+import at.hannibal2.skyhanni.utils.NEUItems.getRawCraftCostOrNull
+import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
+
+object ItemPriceUtils {
+
+    fun debugItemPrice(args: Array<String>) {
+        val internalName = getItemOrFromHand(args)
+        if (internalName == null) {
+            ChatUtils.userError("Hold an item in hand or do /shdebugprice <item name/id>")
+            return
+        }
+
+        println("")
+        println(" Debug Item Price for $internalName ")
+
+        val defaultPrice = internalName.getPrice()
+        println("defaultPrice: ${defaultPrice.addSeparators()}")
+        println(" #")
+        for (source in ItemPriceSource.values()) {
+            val price = internalName.getPrice(source)
+            println("${source.displayName} price: ${price.addSeparators()}")
+        }
+        println(" #")
+        println(" ")
+        println("getLowestBinOrNull: ${internalName.getLowestBinOrNull()?.addSeparators()}")
+
+        internalName.getBazaarData().let {
+            println("getBazaarData sellOfferPrice: ${it?.sellOfferPrice?.addSeparators()}")
+            println("getBazaarData instantBuyPrice: ${it?.instantBuyPrice?.addSeparators()}")
+        }
+        println("getNpcPriceOrNull: ${internalName.getNpcPriceOrNull()?.addSeparators()}")
+        println("getRawCraftCostOrNull: ${internalName.getRawCraftCostOrNull()?.addSeparators()}")
+        println(" ")
+    }
+
+    // TODO move either into inventory utils or new command utils
+    fun getItemOrFromHand(args: Array<String>): NEUInternalName? {
+        val name = args.joinToString(" ")
+        return if (name.isEmpty()) {
+            InventoryUtils.getItemInHand()?.getInternalName()
+        } else {
+            val internalName = name.asInternalName()
+            if (internalName.getItemStackOrNull() != null) {
+                internalName
+            } else {
+                NEUInternalName.fromItemNameOrNull(name)
+            }
+
+        }
+    }
+}
