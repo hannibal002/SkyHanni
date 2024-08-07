0759f24bd0a0307af7119138c24cda1e8152feb8
ItsEmpa <itsempa@users.noreply.github.com>
2024-08-07 19:35:46 +0200

    simplified a lot
    Signed-off-by: ItsEmpa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/rift/area/stillgorechateau/SplatterHearts.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/area/stillgorechateau/SplatterHearts.kt
@@ -8,23 +8,16 @@ import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.RenderUtils.drawFilledBoundingBox_nea
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
-import at.hannibal2.skyhanni.utils.TimeLimitedSet
 import net.minecraft.util.EnumParticleTypes
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object SplatterHearts {
     private var lastHearts = SimpleTimeMark.farPast()
-    private var lastSpawn = SimpleTimeMark.farPast()
 
-    private var spawning = false
-    private var currentIndex = 0
-
-    private data class SplatterHeart(var location: LorenzVec, var index: Int, val time: SimpleTimeMark = SimpleTimeMark.now())
-
-    private val hearts = TimeLimitedSet<SplatterHeart>(1500.milliseconds)
+    private var shownHearts = setOf<LorenzVec>()
+    private val currentHearts = mutableSetOf<LorenzVec>()
 
     @SubscribeEvent
     fun onParticle(event: ReceiveParticleEvent) {
@@ -32,48 +25,20 @@ object SplatterHearts {
         if (event.type != EnumParticleTypes.HEART) return
         if (event.count != 3 || event.speed != 0f) return
 
-        if (lastHearts.passedSince() > 2.seconds) {
-            println("spawning: true")
-            spawning = true
-            currentIndex = 0
-            lastSpawn = SimpleTimeMark.now()
+        if (lastHearts.passedSince() > 50.milliseconds) {
+            shownHearts = currentHearts
+            currentHearts.clear()
         }
-
-        if (spawning && lastSpawn.passedSince() > 100.milliseconds) {
-            spawning = false
-            println("spawning: false [${lastSpawn.passedSince()}]")
-        }
-        ++currentIndex
-
         lastHearts = SimpleTimeMark.now()
-        val pos = event.location
-
-        if (spawning && hearts.none { it.time.passedSince() > 2.seconds && it.location.distance(pos) < 0.5 }) {
-            hearts += SplatterHeart(pos, currentIndex)
-            println("added heart at ${pos.toCleanString()} ($currentIndex) [${lastSpawn.passedSince()}]")
-        } else {
-            val heart = hearts.apply { if (lastSpawn.passedSince() > 2.seconds) filter { it.location.distance(pos) < 0.2 } }
-                .minByOrNull { it.index }
-                ?: run {
-                    println("no heart found at ${pos.toCleanString()} [${lastSpawn.passedSince()}]")
-                    println("        ${hearts.joinToString { "([${it.location.toCleanString()}], ${it.index}, ${it.time.passedSince()})" }}")
-                    return
-                }
-            val prevIndex = heart.index
-            with(heart) {
-                location = pos
-                index = currentIndex
-            }
-            hearts.update(heart)
-            println("updated heart (${heart.location.toCleanString()} -> $pos) (${prevIndex} -> $currentIndex) [${lastSpawn.passedSince()}]")
-        }
+        currentHearts += event.location
     }
 
     @SubscribeEvent
     fun onRenderWorld(event: LorenzRenderWorldEvent) {
         if (!isEnabled()) return
-        hearts.forEach {
-            val pos = it.location.add(-0.5, 0.3, -0.5)
+        if (lastHearts.passedSince() > 300.milliseconds) return
+        shownHearts.forEach {
+            val pos = it.add(-0.5, 0.3, -0.5)
             val aabb = pos.axisAlignedTo(pos.add(1, 1, 1))
             event.drawFilledBoundingBox_nea(aabb, LorenzColor.RED.addOpacity(100))
         }
