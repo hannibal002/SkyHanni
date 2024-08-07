a15790ca65d13b0989b950b32150a14f9b468e55
ItsEmpa <itsempa@users.noreply.github.com>
2024-08-07 19:36:31 +0200

    remove update from timelimitedset (not needed anymore)
    Signed-off-by: ItsEmpa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/TimeLimitedSet.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/TimeLimitedSet.kt
@@ -27,10 +27,6 @@ class TimeLimitedSet<T : Any>(
 
     fun clear() = cache.clear()
 
-    fun update(element: T) {
-        add(element)
-    }
-
     fun toSet(): Set<T> = cache.keys().let { keys ->
         if (keys.isEmpty()) emptySet() else keys.toSet()
     }
