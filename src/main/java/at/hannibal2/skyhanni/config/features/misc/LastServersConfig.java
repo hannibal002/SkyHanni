98b962a3007a34e4ce5c723bd27d3eabc0c7d454
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-08-26 14:14:35 +0200

    formatting and default disable
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/LastServersConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/LastServersConfig.java
@@ -9,23 +9,13 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 public class LastServersConfig {
 
     @Expose
-    @ConfigOption(
-        name = "Enabled",
-        desc = "Receive notifications when you rejoin a server you have previously joined."
-    )
+    @ConfigOption(name = "Enabled", desc = "Receive notifications when you rejoin a server you have previously joined.")
     @ConfigEditorBoolean
     @FeatureToggle
-    public boolean enabled = true;
+    public boolean enabled = false;
 
     @Expose
-    @ConfigOption(
-        name = "Notification Time",
-        desc = "Get notified if you rejoin a server within the specified number of seconds."
-    )
-    @ConfigEditorSlider(
-        minValue = 5,
-        maxValue = 300,
-        minStep = 1
-    )
+    @ConfigOption(name = "Notification Time", desc = "Get notified if you rejoin a server within the specified number of seconds.")
+    @ConfigEditorSlider(minValue = 5, maxValue = 300, minStep = 1)
     public Integer warnTime = 60;
 }
