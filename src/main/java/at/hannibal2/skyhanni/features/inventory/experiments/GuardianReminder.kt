c1c3fcc963bbf79f75e33a9383431784f36bdf36
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2024-08-31 21:07:55 +0200

    Fix: Autopet Rule Detection (#2430)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/experiments/GuardianReminder.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/experiments/GuardianReminder.kt
@@ -37,9 +37,14 @@ object GuardianReminder {
         "mainmenu",
         "Experimentation Table",
     )
+
+    /**
+     * REGEX-TEST: §dGuardian
+     * REGEX-TEST: §9Guardian§e
+     */
     private val petNamePattern by patternGroup.pattern(
         "guardianpet",
-        "§[956d]Guardian",
+        "§[956d]Guardian.*",
     )
 
     @SubscribeEvent
