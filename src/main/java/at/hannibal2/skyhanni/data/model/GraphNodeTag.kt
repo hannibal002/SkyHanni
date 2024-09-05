77582eb87f9dd094995feee516ef193ca3e176e7
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-09-05 11:08:39 +0200

    added support for small_area

--- src/main/java/at/hannibal2/skyhanni/data/model/GraphNodeTag.kt
+++ src/main/java/at/hannibal2/skyhanni/data/model/GraphNodeTag.kt
@@ -1,39 +1,44 @@
 package at.hannibal2.skyhanni.data.model
 
-enum class GraphNodeTag(val internalName: String?, val displayName: String, val description: String) {
-    DEV("dev", "Dev", "Intentionally marked as dev."), // E.g. Spawn points, todos, etc
+import at.hannibal2.skyhanni.utils.LorenzColor
+
+enum class GraphNodeTag(val internalName: String?, val color: LorenzColor, val cleanName: String, val description: String) {
+    DEV("dev", LorenzColor.WHITE, "Dev", "Intentionally marked as dev."), // E.g. Spawn points, todos, etc
 
     // Everywhere
-    NPC("npc", "§eNPC", "A NPC entity."), // also take from neu repo
-    AREA("area", "§aArea", "A SkyBlock Area."),
-    POI("poi", "PoI", "Point of interest."),
-    LAUNCH_PAD("launch", "Launch Pad", "Slime blocks sending you to another server."),
+    NPC("npc", LorenzColor.YELLOW, "NPC", "A NPC entity."), // also take from neu repo
+    AREA("area", LorenzColor.DARK_GREEN, "Area", "A big SkyBlock area."),
+    SMALL_AREA("small_area", LorenzColor.GREEN, "Small Area", "A small SkyBlock area, e.g. a house."),
+    POI("poi", LorenzColor.WHITE, "PoI", "Point of interest."),
+    LAUNCH_PAD("launch", LorenzColor.WHITE, "Launch Pad", "Slime blocks sending you to another server."),
 
     // on multiple islands
-    ROMEO("romeo", "Romeo & Juliette Quest", "Blocks related to the Romeo and Juliette/Ring of Love quest line."),
-    RACE("race", "Race Start/Stop", "A race start or stop point."),
-    SLAYER("slayer", "Slayer", "A Slayer area"),
+    ROMEO("romeo", LorenzColor.WHITE, "Romeo & Juliette Quest", "Blocks related to the Romeo and Juliette/Ring of Love quest line."),
+    RACE("race", LorenzColor.WHITE, "Race Start/Stop", "A race start or stop point."),
+    SLAYER("slayer", LorenzColor.WHITE, "Slayer", "A Slayer area"),
     // hoppity
 
     // Hub
-    HUB_12_STARTER("starter_npc", "Starter NPC", "One of the 12 starter NPC's you need to talk to."),
+    HUB_12_STARTER("starter_npc", LorenzColor.WHITE, "Starter NPC", "One of the 12 starter NPC's you need to talk to."),
     // diana
 
     // Farming Islands: Pelts
-    FARMING_CROP("farming_crop", "Farming Crop", "A spot where you can break crops on farming islands."),
+    FARMING_CROP("farming_crop", LorenzColor.WHITE, "Farming Crop", "A spot where you can break crops on farming islands."),
 
     // Rift
-    RIFT_ENIGMA("rift_enigma", "§5Enigma Soul", "Enigma Souls in the rift."),
-    RIFT_EYE("rift_eye", "§4Eye", "An Eye in the rift to teleport to."),
+    RIFT_ENIGMA("rift_enigma", LorenzColor.DARK_PURPLE, "Enigma Soul", "Enigma Souls in the rift."),
+    RIFT_EYE("rift_eye", LorenzColor.DARK_RED, "Eye", "An Eye in the rift to teleport to."),
 
     // Spider's Den
-    SPIDER_RELIC("SPIDER_RELIC", "§5Relic", "An relic in the Spider's Den."),
+    SPIDER_RELIC("SPIDER_RELIC", LorenzColor.DARK_PURPLE, "Relic", "An relic in the Spider's Den."),
 
     // Dwarven Mines
-    MINES_EMISSARY("mines_emissary", "§6Emissary", "A Emissary from the king."),
+    MINES_EMISSARY("mines_emissary", LorenzColor.GOLD, "Emissary", "A Emissary from the king."),
     // commission areas
 
     ;
+
+    val displayName: String = color.getChatColor() + cleanName
 
     companion object {
         fun byId(internalName: String?): GraphNodeTag? = values().firstOrNull { it.internalName == internalName }
