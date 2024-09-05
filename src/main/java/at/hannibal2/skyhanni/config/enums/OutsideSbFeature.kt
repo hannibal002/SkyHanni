fcee8e5483b70c65ce5343830a411a9328accf07
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-09-05 21:43:02 +0200

    clarify scoreboard outside
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/enums/OutsideSbFeature.kt
+++ src/main/java/at/hannibal2/skyhanni/config/enums/OutsideSbFeature.kt
@@ -19,7 +19,7 @@ enum class OutsideSbFeature(private val displayName: String) {
     ARROW_TRAIL("Arrow Trail"),
     HIGHLIGHT_PARTY_MEMBERS("Highlight Party Members"),
     MOVEMENT_SPEED("Movement Speed"),
-    CUSTOM_SCOREBOARD("Custom Scoreboard"),
+    CUSTOM_SCOREBOARD("Custom Scoreboard (only on Hypixel)"),
     ;
 
     override fun toString() = displayName
