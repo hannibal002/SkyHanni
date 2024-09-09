710e61f1914af56c324b404f0434716be57d033f
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-09-09 17:56:04 +0200

    fix pressing the inquisitor share button showing an error

--- src/main/java/at/hannibal2/skyhanni/features/event/diana/InquisitorWaypointShare.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/diana/InquisitorWaypointShare.kt
@@ -11,7 +11,6 @@ import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.events.diana.InquisitorFoundEvent
 import at.hannibal2.skyhanni.events.minecraft.packet.PacketReceivedEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.CollectionUtils.editCopy
 import at.hannibal2.skyhanni.utils.EntityUtils
@@ -202,7 +201,8 @@ object InquisitorWaypointShare {
         lastShareTime = SimpleTimeMark.now()
 
         if (inquisitor == -1) {
-            ErrorManager.skyHanniError("No Inquisitor Found!")
+            ChatUtils.debug("Trying to send inquisitor via chat, vut no inquis is nearby.")
+            return
         }
 
         val inquisitor = EntityUtils.getEntityByID(inquisitor)
