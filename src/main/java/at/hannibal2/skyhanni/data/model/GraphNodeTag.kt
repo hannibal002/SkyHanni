5242a393d3b7aba3371b677e6fb05f8a889a838e
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-09-14 11:25:51 +0200

    added more tags

--- src/main/java/at/hannibal2/skyhanni/data/model/GraphNodeTag.kt
+++ src/main/java/at/hannibal2/skyhanni/data/model/GraphNodeTag.kt
@@ -2,7 +2,7 @@ package at.hannibal2.skyhanni.data.model
 
 import at.hannibal2.skyhanni.utils.LorenzColor
 
-enum class GraphNodeTag(val internalName: String?, val color: LorenzColor, val cleanName: String, val description: String) {
+enum class GraphNodeTag(val internalName: String, val color: LorenzColor, val cleanName: String, val description: String) {
     DEV("dev", LorenzColor.WHITE, "Dev", "Intentionally marked as dev."), // E.g. Spawn points, todos, etc
 
     // Everywhere
@@ -16,6 +16,7 @@ enum class GraphNodeTag(val internalName: String?, val color: LorenzColor, val c
     ROMEO("romeo", LorenzColor.WHITE, "Romeo & Juliette Quest", "Blocks related to the Romeo and Juliette/Ring of Love quest line."),
     RACE("race", LorenzColor.WHITE, "Race Start/Stop", "A race start or stop point."),
     SLAYER("slayer", LorenzColor.WHITE, "Slayer", "A Slayer area"),
+    HOPPITY("hoppity", LorenzColor.AQUA, "Hoppity Egg", "An egg location in Hoppity's Hunt."),
     // hoppity
 
     // Hub
@@ -23,24 +24,27 @@ enum class GraphNodeTag(val internalName: String?, val color: LorenzColor, val c
     // diana
 
     // Farming Islands: Pelts
-    FARMING_CROP("farming_crop", LorenzColor.WHITE, "Farming Crop", "A spot where you can break crops on farming islands."),
+//     FARMING_CROP("farming_crop", LorenzColor.WHITE, "Farming Crop", "A spot where you can break crops on farming islands."),
 
     // Rift
     RIFT_ENIGMA("rift_enigma", LorenzColor.DARK_PURPLE, "Enigma Soul", "Enigma Souls in the rift."),
-    RIFT_EYE("rift_eye", LorenzColor.DARK_RED, "Eye", "An Eye in the rift to teleport to."),
+    RIFT_EYE("rift_eye", LorenzColor.DARK_RED, "Rift Eye", "An Eye in the rift to teleport to."),
 
     // Spider's Den
-    SPIDER_RELIC("SPIDER_RELIC", LorenzColor.DARK_PURPLE, "Relic", "An relic in the Spider's Den."),
+    SPIDER_RELIC("SPIDER_RELIC", LorenzColor.DARK_PURPLE, "Spider's Relic", "An relic in the Spider's Den."),
 
     // Dwarven Mines
-    MINES_EMISSARY("mines_emissary", LorenzColor.GOLD, "Emissary", "A Emissary from the king."),
+    MINES_EMISSARY("mines_emissary", LorenzColor.GOLD, "Mines Emissary", "A Emissary from the king."),
     // commission areas
+
+    // Crimson Isles
+    CRIMSON_MINIBOSS("crimson_miniboss", LorenzColor.RED, "Crimson Miniboss", "Mini bosses in Crimson Isle."),
 
     ;
 
     val displayName: String = color.getChatColor() + cleanName
 
     companion object {
-        fun byId(internalName: String?): GraphNodeTag? = values().firstOrNull { it.internalName == internalName }
+        fun byId(internalName: String?): GraphNodeTag? = entries.firstOrNull { it.internalName == internalName }
     }
 }
