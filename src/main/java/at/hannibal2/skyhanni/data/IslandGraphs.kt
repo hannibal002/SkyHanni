4e978d1e412d1c85534dd670b89d50b6d0c5cc8b
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-09-14 20:17:53 +0200

    more tags

--- src/main/java/at/hannibal2/skyhanni/data/IslandGraphs.kt
+++ src/main/java/at/hannibal2/skyhanni/data/IslandGraphs.kt
@@ -40,6 +40,7 @@ import kotlin.math.abs
  * 	jump pads between servers
  * 	ring of love/romeo juliet quest
  * 	death location
+ * 	% of island discvovered (every node was most closest node at least once)
  * hub:
  * 	12 starter NPC's
  * 	diana
@@ -49,6 +50,8 @@ import kotlin.math.abs
  * 	enigma souls
  * 	eyes
  * 	big quests
+ * 	montezuma souls
+ * 	blood effigies
  * spider:
  * 	relicts + throw spot
  * dwarven mines:
@@ -56,7 +59,7 @@ import kotlin.math.abs
  * 	commssion areas
  * 	events: raffle, goblin slayer, donpieresso
  * deep
- * 	path to the bottom
+ * 	path to the bottom (Rhys NPC)
  * end
  * 	golem spawn
  * 	dragon death spot
@@ -71,6 +74,8 @@ import kotlin.math.abs
  * 	toggle distance to node by node path lengh, instead of eye of sight lenght
  * 	press test button again to enable "true test mode", with graph math and hiding other stuff
  * 	option to compare two graphs, and store multiple graphs in the edit mode in paralell
+ *
+ * 	mineshaft + corpse + ladder spot
  */
 
 @SkyHanniModule
