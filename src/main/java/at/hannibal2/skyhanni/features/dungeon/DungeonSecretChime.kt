2a2ab55b647f590be094be4c0497866346e38917
Ovi <ovi2561@gmail.com>
2024-09-16 00:22:35 +0600

    Chime for bat and item secrets.

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonSecretChime.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonSecretChime.kt
@@ -4,13 +4,27 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.ClickedBlockType
 import at.hannibal2.skyhanni.events.DungeonBlockClickEvent
+import at.hannibal2.skyhanni.events.ItemAddEvent
+import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.SoundUtils
 import at.hannibal2.skyhanni.utils.SoundUtils.playSound
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @SkyHanniModule
 object DungeonSecretChime {
     private val config get() = SkyHanniMod.feature.dungeon.secretChime
+    private val secretItems = listOf(
+        "DUNGEON_TRAP",
+        "TRAINING_WEIGHTS",
+        "SPIRIT_LEAP",
+        "INFLATABLE_JERRY",
+        "DUNGEON_CHEST_KEY",
+        "TREASURE_TALISMAN",
+        "DEFUSE_KIT",
+        // IDK THE NAME
+        "Splash Potion",
+    )
 
     @HandleEvent
     fun onDungeonClickedBlock(event: DungeonBlockClickEvent) {
@@ -24,6 +38,20 @@ object DungeonSecretChime {
             ClickedBlockType.LEVER,
             ClickedBlockType.WITHER_ESSENCE,
             -> playSound()
+        }
+    }
+
+    @SubscribeEvent
+    fun onBatKill(event: MobEvent.DeSpawn) {
+        if (isEnabled() && event.mob.name == "Dungeon Secret Bat") {
+            playSound()
+        }
+    }
+
+    @SubscribeEvent
+    fun onSecretItemPickup(event: ItemAddEvent) {
+        if (isEnabled() && event.internalName.asString() in secretItems) {
+            playSound()
         }
     }
 
