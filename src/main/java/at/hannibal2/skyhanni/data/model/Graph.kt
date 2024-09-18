aa59d0f915c25fde023ac6969e633c31639b176d
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-09-18 17:00:50 +0200

    made the code 1700 times faster
    Co-authored-by: nea <nea@nea.moe>

--- src/main/java/at/hannibal2/skyhanni/data/model/Graph.kt
+++ src/main/java/at/hannibal2/skyhanni/data/model/Graph.kt
@@ -10,6 +10,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.stream.JsonToken
 import java.util.PriorityQueue
 
+// TODO: This class should be disambiguated into a NodePath and a Graph class
 @JvmInline
 value class Graph(
     @Expose val nodes: List<GraphNode>,
@@ -167,18 +168,50 @@ class GraphNode(val id: Int, val position: LorenzVec, val name: String? = null, 
 
 fun Graph.findShortestPathAsGraph(start: GraphNode, end: GraphNode): Graph = this.findShortestPathAsGraphWithDistance(start, end).first
 
-fun Graph.findShortestPathAsGraphWithDistance(start: GraphNode, end: GraphNode): Pair<Graph, Double> {
+data class DijkstraTree(
+    val origin: GraphNode,
+    /**
+     * A map of distances between the [origin] and each node in a graph. This distance map is only accurate for nodes closer to the
+     * origin than the [lastVisitedNode]. In case there is no early bailout, this map will be accurate for all nodes in the graph.
+     */
+    val distances: Map<GraphNode, Double>,
+    /**
+     * A map of nodes to the neighbouring node that is the quickest path towards the origin (the neighbouring node that has the lowest value
+     * in [distances])
+     */
+    val towardsOrigin: Map<GraphNode, GraphNode>,
+    /**
+     * This is either the furthest away node in the graph, or the node that was bailed out on early because it fulfilled the search
+     * condition. In case the search condition matches nothing, this will *still* be the furthest away node, so an additional check might be
+     * necessary.
+     */
+    val lastVisitedNode: GraphNode,
+)
+
+/**
+ * Find a tree of distances to the [start] node using dijkstra's algorithm.
+ */
+fun Graph.findDijkstraDistances(
+    start: GraphNode,
+    /**
+     * Bail out early before collecting all the distances to all nodes in the graph. This will not collect valid distance data for *all*
+     * nodes for which bailout matches, but only the closest one.
+     */
+    bailout: (GraphNode) -> Boolean,
+): DijkstraTree {
     val distances = mutableMapOf<GraphNode, Double>()
     val previous = mutableMapOf<GraphNode, GraphNode>()
     val visited = mutableSetOf<GraphNode>()
     val queue = PriorityQueue<GraphNode>(compareBy { distances.getOrDefault(it, Double.MAX_VALUE) })
+    var lastVisitedNode: GraphNode = start
 
     distances[start] = 0.0
     queue.add(start)
 
     while (queue.isNotEmpty()) {
         val current = queue.poll()
-        if (current == end) break
+        lastVisitedNode = current
+        if (bailout(current)) break
 
         visited.add(current)
 
@@ -194,16 +227,34 @@ fun Graph.findShortestPathAsGraphWithDistance(start: GraphNode, end: GraphNode):
         }
     }
 
-    return Graph(
-        buildList {
-            var current = end
-            while (current != start) {
-                add(current)
-                current = previous[current] ?: return Graph(emptyList()) to 0.0
-            }
-            add(start)
-        }.reversed(),
-    ) to distances[end]!!
+    return DijkstraTree(
+        start,
+        distances,
+        previous,
+        lastVisitedNode,
+    )
+}
+
+fun Graph.findAllShortestDistances(start: GraphNode): DijkstraTree {
+    return findDijkstraDistances(start) { false }
+}
+
+fun DijkstraTree.findPathToDestination(end: GraphNode): Pair<Graph, Double> {
+    val distances = this
+    val reversePath = buildList {
+        var current = end
+        while (true) {
+            add(current)
+            if (current == distances.origin) break
+            current = distances.towardsOrigin[current] ?: return Graph(emptyList()) to 0.0
+        }
+    }
+    return Graph(reversePath.reversed()) to distances.distances[end]!!
+}
+
+fun Graph.findShortestPathAsGraphWithDistance(start: GraphNode, end: GraphNode): Pair<Graph, Double> {
+    val distances = findDijkstraDistances(start) { it == end }
+    return distances.findPathToDestination(end)
 }
 
 fun Graph.findShortestPath(start: GraphNode, end: GraphNode): List<LorenzVec> = this.findShortestPathAsGraph(start, end).toPositionsList()
--- src/main/java/at/hannibal2/skyhanni/utils/GraphUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/GraphUtils.kt
@@ -2,21 +2,39 @@ package at.hannibal2.skyhanni.utils
 
 import at.hannibal2.skyhanni.data.model.Graph
 import at.hannibal2.skyhanni.data.model.GraphNode
-import at.hannibal2.skyhanni.data.model.findShortestPathAsGraphWithDistance
+import at.hannibal2.skyhanni.data.model.findAllShortestDistances
+import at.hannibal2.skyhanni.data.model.findDijkstraDistances
+import at.hannibal2.skyhanni.data.model.findPathToDestination
 
 object GraphUtils {
+    /**
+     * Find the fastest path from [closestNode] to *any* node that matches [condition].
+     */
+    fun findFastestPath(graph: Graph,
+                        closestNode: GraphNode,
+                        condition: (GraphNode) -> Boolean): Pair<Graph, Double>? {
+        val distances = graph.findDijkstraDistances(closestNode, condition)
+        val entry = distances.lastVisitedNode.takeIf(condition)
+        return entry?.let {
+            distances.findPathToDestination(it)
+        }
+    }
 
+    /**
+     * Find the fastest path from [closestNode] to *all* nodes that matches [condition].
+     */
     fun findFastestPaths(
         graph: Graph,
-        closedNote: GraphNode,
+        closestNode: GraphNode,
         condition: (GraphNode) -> Boolean = { true },
     ): Pair<MutableMap<GraphNode, Graph>, MutableMap<GraphNode, Double>> {
         val paths = mutableMapOf<GraphNode, Graph>()
 
         val map = mutableMapOf<GraphNode, Double>()
+        val distances = graph.findAllShortestDistances(closestNode)
         for (graphNode in graph.nodes) {
             if (!condition(graphNode)) continue
-            val (path, distance) = graph.findShortestPathAsGraphWithDistance(closedNote, graphNode)
+            val (path, distance) = distances.findPathToDestination(graphNode)
             paths[graphNode] = path
             map[graphNode] = distance
         }
