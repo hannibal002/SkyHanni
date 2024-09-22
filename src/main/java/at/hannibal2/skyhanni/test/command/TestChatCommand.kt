c5446c339d8a4519efebb8a770705434b161a289
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-09-22 11:38:35 -0400

    Smh

--- src/main/java/at/hannibal2/skyhanni/test/command/TestChatCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/test/command/TestChatCommand.kt
@@ -23,7 +23,6 @@ object TestChatCommand {
             return
         }
 
-
         SkyHanniMod.launchCoroutine {
             val mutArgs = args.toMutableList()
             val multiLines = mutArgs.remove("-lines")
