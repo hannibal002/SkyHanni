45c1e80ce977f6ca2f355e6de282f2d1e16ae940
martimavocado <martim.cavaco@tutanota.com>
2024-09-22 23:08:34 +0100

    yeah

---
+++ src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/HighHeatSound.kt
@@ -0,0 +1,21 @@
+package at.hannibal2.skyhanni.features.mining.crystalhollows
+
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.data.MiningAPI
+import at.hannibal2.skyhanni.events.PlaySoundEvent
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+@SkyHanniModule
+object HighHeatSound {
+    private val config get() = SkyHanniMod.feature.mining
+
+    @SubscribeEvent
+    fun onSound(event: PlaySoundEvent) {
+        if (isEnabled()) return
+
+        if (event.soundName == "mob.wolf.panting" && event.pitch == 0.0f && event.volume == 1.0f) event.cancel()
+    }
+
+    private fun isEnabled() = config.muteHighHeat && MiningAPI.inCrystalHollows()
+}
