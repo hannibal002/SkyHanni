5535bcba03fd96c54394a82a500c92481aa4546a
Nealkitt <stellaarnott1@gmail.com>
2024-09-21 19:22:17 -0700

    This should work, Right?

--- src/main/java/at/hannibal2/skyhanni/features/combat/FlareDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/FlareDisplay.kt
@@ -100,7 +100,7 @@ object FlareDisplay {
                     }
                 }
             }
-            if (remainingTime > 5.seconds) continue
+            if (remainingTime > 5.seconds || remainingTime < 0.seconds) continue
             val message = "$name §eexpires in: §b${remainingTime.inWholeSeconds}s"
             when (config.alertType) {
                 FlareConfig.AlertType.CHAT -> {
@@ -194,7 +194,6 @@ object FlareDisplay {
         SOS("§5SOS Flare", "+125%"),
         ALERT("§9Alert Flare", "+50%"),
         WARNING("§aWarning Flare", null),
-        ;
     }
 
     private fun isEnabled() = LorenzUtils.inSkyBlock && config.enabled
