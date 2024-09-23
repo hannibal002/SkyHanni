ca272565b97b7bbdb4cf68070fa317d150ede08c
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2024-09-23 19:24:40 +0200

    feature works but might not be robust (untested)

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/SpringBootsHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/SpringBootsHelper.kt
@@ -1,35 +1,51 @@
 package at.hannibal2.skyhanni.features.dungeon
 
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.data.TitleManager
 import at.hannibal2.skyhanni.events.PlaySoundEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
-import at.hannibal2.skyhanni.utils.ItemUtils.getInternalNameOrNull
+import at.hannibal2.skyhanni.utils.SoundUtils
 import net.minecraft.client.Minecraft
-import net.minecraft.client.settings.KeyBinding
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object SpringBootsHelper {
 
-    private var index = 0
+    private val config get() = SkyHanniMod.feature.dungeon
+
+    /**
+     * Two [PlaySoundEvent] get created roughly every 100ms. This tracks the amount of times we heard the `note.pling` sound while wearing Spring Boots and sneaking.
+     */
+    private var soundStreak = 0
 
     @SubscribeEvent
     fun onSound(event: PlaySoundEvent) {
-        if (!(event.soundName == "note.pling" || event.soundName == "note.hat" || event.soundName == "fireworks.launch")) return
+        if (!isEnabled()) return
+        if (!(event.soundName == "note.pling" || event.soundName == "fireworks.launch")) return // event.soundName == "note.hat" ||
         if (InventoryUtils.getBoots()?.getInternalName()?.asString() != "SPRING_BOOTS") return
         if (event.soundName == "fireworks.launch") {
-            index = 0
-            println("reset index")
+            soundStreak = 0
             return
         }
         if (!Minecraft.getMinecraft().thePlayer.isSneaking) return
-        index += 1
-        if (index == 15) {
-            KeyBinding.setKeyBindState(Minecraft.getMinecraft().gameSettings.keyBindSneak.keyCode, false)
-            println("unsneaked")
+        soundStreak += 1
 
+        if (soundStreak == 5) { // 7-8 seems to be perfect but subtract a bit of reaction speed
+            TitleManager.sendTitle("§cSpring Boots ready!", 2.seconds, 3.6, 7.0f)
+            SoundUtils.playBeepSound()
         }
-        println("index = $index")
     }
+
+    private fun inDungeon(): Boolean {
+        if (!DungeonAPI.inDungeon()) return false
+        if (!DungeonAPI.inBossRoom) return false
+        if (!DungeonAPI.isOneOf("F7", "M7")) return false
+
+        return true
+    }
+
+    fun isEnabled() = inDungeon() && config.springBootsNotification
 }
