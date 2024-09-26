abb61d89b02a4d79e8bf570a61e9471ffd4d8a90
minhperry <minhpt2002@gmail.com>
2024-09-26 21:51:56 +0200

    feat: xp, coin and mode detection

--- src/main/java/at/hannibal2/skyhanni/features/inventory/FannCost.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/FannCost.kt
@@ -2,12 +2,16 @@ package at.hannibal2.skyhanni.features.inventory
 
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
 import at.hannibal2.skyhanni.events.LorenzToolTipEvent
+import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
+import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
-import io.github.moulberry.notenoughupdates.util.stripControlCodes
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 @SkyHanniModule
 object FannCost {
@@ -16,6 +20,7 @@ object FannCost {
     private val showCoins get() = config.coinsPerXP
     private val showBits get() = config.xpPerBit
     private var isDayTrainingMode = true
+
     private val patternGroup = RepoPattern.group("fann.inventory")
     private val trainingSlotInventoryPattern by patternGroup.pattern(
         "training",
@@ -26,8 +31,20 @@ object FannCost {
         "§aBegin Training",
     )
     private val expEarnedPattern by patternGroup.pattern(
-        "training.exp.earned",
-        """Will earn a total of (\d{1,3}(,\d{3})*(\.\d+)?|\d+(\.\d+)?) EXP\.?""",
+        "exp",
+        """Will earn a total of (\d{1,3}(?:,\d{3})*(?:\.\d+)?|\d+\.?\d*) EXP\.?""",
+    )
+    private val coinsPattern by patternGroup.pattern(
+        "coin",
+        """(\d{1,3}(?:,\d{3})*(?:\.\d+)?|\d+\.?\d*) Coins""",
+    )
+    private val desiredLevelPatter by patternGroup.pattern(
+        "slot24.name.level",
+        "Desired Level: (200|1?[0-9]?[0-9])",
+    )
+    private val userInputPattern by patternGroup.pattern(
+        "slot24.name.input",
+        "User Input",
     )
 
     @SubscribeEvent
@@ -35,37 +52,45 @@ object FannCost {
         if (!trainingSlotInventoryPattern.matches(InventoryUtils.openInventoryName())) return
         if (!anvilPattern.matches(event.itemStack.displayName)) return
         val tooltip = event.toolTip
-        for (line in tooltip) {
-            val linePlain = line.stripControlCodes()
+        ChatUtils.debug("exp earned: ${tooltip.getExpEarned()}")
+        ChatUtils.debug("coins: ${tooltip.getCoins()}")
+        ChatUtils.debug("isDayTrainingMode: $isDayTrainingMode")
+    }
 
+    @SubscribeEvent
+    fun onInventoryUpdate(event: InventoryUpdatedEvent) {
+        if (!trainingSlotInventoryPattern.matches(event.inventoryName.removeColor())) return
+        val slot24 = event.inventoryItems[24]
+        if (slot24 != null) {
+            val name = slot24.displayName.removeColor()
+            if (desiredLevelPatter.matches(name)) {
+                isDayTrainingMode = false
+            } else if (userInputPattern.matches(name)) {
+                isDayTrainingMode = true
+            }
         }
     }
 
-    private fun readExpEarned(lore: List<String>): Double {
+
+    private fun Pattern.read(lore: List<String>): Double? {
         for (line in lore) {
-            val linePlain = line.stripControlCodes()
-            if (expEarnedPattern.matches(linePlain)) {
-                return expEarnedPattern.matcher(linePlain).group(1).replace(",", "").toDouble()
+            val linePlain = line.removeColor()
+            val matcher = matcher(linePlain)
+            if (matcher.find()) {
+                val res = matcher.group(1).replace(",", "").toDouble()
+                return res
             }
         }
-        return 0.0
+        return null
     }
 
-    private fun readTrainingMode(lore: List<String>) {
-        var i = 0
-        val size = lore.size
-        while (i < size) {
-            val line = lore[i]
-            if (i + 1 < size) {
-                val nextLine = lore[i + 1]
-                isDayTrainingMode = line.plainContains("Duration:") && nextLine.plainContains("Reaches:")
-                return
-            }
-            i++
-        }
+    private fun List<String>.getCoins(): Double {
+        return coinsPattern.read(this) ?: 0.0
     }
 
-    private fun String.plainContains(other: String): Boolean {
-        return stripControlCodes().contains(other)
+    private fun List<String>.getExpEarned(): Double? {
+        return expEarnedPattern.read(this)
     }
+
+
 }
