36fc7c24764dba659608337f8bdee46df833d0d3
Ovi <ovi2561@gmail.com>
2024-09-27 20:32:55 +0600

    ohhhh

--- src/main/java/at/hannibal2/skyhanni/config/features/dungeon/SpiritLeapConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/dungeon/SpiritLeapConfig.java
@@ -9,7 +9,7 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class SpiritLeapConfig {
     public final String defaultColor = "0:200:0:0:0";
-    public final String deadPlayerColor = "0:200:120:0:0";
+    public final String deadColor = "0:200:120:0:0";
 
     @Expose
     @ConfigOption(name = "Enable Spirit Leap Overlay", desc = "Enable Spirit Leap Overlay inside Dungeons.")
@@ -56,7 +56,7 @@ public class SpiritLeapConfig {
     @ConfigOption(name = "Reset Colors", desc = "Restores the class highlighter colors to their default settings.")
     @ConfigEditorButton(buttonText = "Reset")
     public Runnable resetColors = () -> {
-        deadTeammateColor = deadPlayerColor;
+        deadTeammateColor = deadColor;
         archerClassColor = defaultColor;
         mageClassColor = defaultColor;
         berserkClassColor = defaultColor;
