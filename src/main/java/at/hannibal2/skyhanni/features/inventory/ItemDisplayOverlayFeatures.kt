ba6cd4d65ac92e32d08fde01c033fa6fc6f4e029
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2024-09-28 02:21:27 +0100

    Fixed Dungeons Diamond + Golden Heads + Master Skull stack sizes using item name instead of internalName (fixes neurename incompatability)

--- src/main/java/at/hannibal2/skyhanni/features/inventory/ItemDisplayOverlayFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/ItemDisplayOverlayFeatures.kt
@@ -70,6 +70,10 @@ object ItemDisplayOverlayFeatures {
         "masterskull",
         "(.*)Master Skull - Tier .",
     )
+    private val masterSkullIDPattern by patternGroup.pattern(
+        "masterskull.id",
+        "MASTER_SKULL_TIER_(?<tier>\\d)",
+    )
     private val gardenVacuumPatterm by patternGroup.pattern(
         "vacuum",
         "§7Vacuum Bag: §6(?<amount>\\d*) Pests?",
@@ -131,20 +135,20 @@ object ItemDisplayOverlayFeatures {
         }
 
         if (MASTER_SKULL_TIER.isSelected()) {
-            masterSkullPattern.matchMatcher(itemName) {
-                return itemName.substring(itemName.length - 1)
+            masterSkullIDPattern.matchMatcher(internalName.asString()) {
+                return group("tier")
             }
         }
 
-        if (DUNGEON_HEAD_FLOOR_NUMBER.isSelected() && (itemName.contains("Golden ") || itemName.contains("Diamond "))) {
+        if (DUNGEON_HEAD_FLOOR_NUMBER.isSelected() && (internalName.contains("GOLD_") || internalName.contains("DIAMOND_"))) {
             when {
-                itemName.contains("Bonzo") -> return "1"
-                itemName.contains("Scarf") -> return "2"
-                itemName.contains("Professor") -> return "3"
-                itemName.contains("Thorn") -> return "4"
-                itemName.contains("Livid") -> return "5"
-                itemName.contains("Sadan") -> return "6"
-                itemName.contains("Necron") -> return "7"
+                internalName.contains("BONZO") -> return "1"
+                internalName.contains("SCARF") -> return "2"
+                internalName.contains("PROFESSOR") -> return "3"
+                internalName.contains("THORN") -> return "4"
+                internalName.contains("LIVID") -> return "5"
+                internalName.contains("SADAN") -> return "6"
+                internalName.contains("NECRON") -> return "7"
             }
         }
 
