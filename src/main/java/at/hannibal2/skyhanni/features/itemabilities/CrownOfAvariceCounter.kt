a7efbc38f36a8b68b828bc42587794eaaf379172
Tryp0xd <54819490+Tryp0xd@users.noreply.github.com>
2024-09-29 19:16:41 +0100

    Improvements to the Crown of Avarice Counter + Added getCrownOfAvarice to SkyBlockItemModifierUtils

--- src/main/java/at/hannibal2/skyhanni/features/itemabilities/CrownOfAvariceCounter.kt
+++ src/main/java/at/hannibal2/skyhanni/features/itemabilities/CrownOfAvariceCounter.kt
@@ -2,7 +2,8 @@ package at.hannibal2.skyhanni.features.itemabilities
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.GuiRenderEvent
-import at.hannibal2.skyhanni.events.LorenzTickEvent
+import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
+import at.hannibal2.skyhanni.events.OwnInventoryItemUpdateEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.extraAttributes
@@ -13,6 +14,7 @@ import at.hannibal2.skyhanni.utils.NEUItems.getItemStack
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.NumberUtil.shortFormat
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderable
+import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getCoinsOfAvarice
 import at.hannibal2.skyhanni.utils.renderables.Renderable
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -27,25 +29,23 @@ object CrownOfAvariceCounter {
 
     @SubscribeEvent
     fun onOverlay(event: GuiRenderEvent.GuiOverlayRenderEvent) {
+        if(!isEnabled()) return
         render?.let { config.position.renderRenderable(it, posLabel = "Crown of Avarice Counter") }
     }
 
     @SubscribeEvent
-    fun onTick(event: LorenzTickEvent) {
-        render = check()
-    }
-
-    fun check(): Renderable? {
-        if (!LorenzUtils.inSkyBlock) return null
-        if (!config.enable) return null
-        val item = InventoryUtils.getHelmet()
-        if (item?.getInternalNameOrNull() != internalName) return null
-        val count = item.extraAttributes.getLong("collected_coins");
-        return Renderable.horizontalContainer(
+    fun onInventoryUpdated(event: OwnInventoryItemUpdateEvent) {
+        if (!isEnabled() || event.slot != 5) return
+        val item = InventoryUtils.getHelmet() ?: return
+        if (item.getInternalNameOrNull() != internalName) return
+        val count = item.getCoinsOfAvarice() ?: return
+        render = Renderable.horizontalContainer(
             listOf(
                 Renderable.itemStack(internalName.getItemStack()),
                 Renderable.string("§6" + if (config.shortFormat) count.shortFormat() else count.addSeparators()),
             ),
         )
     }
+
+    private fun isEnabled() = LorenzUtils.inSkyBlock && config.enable
 }
