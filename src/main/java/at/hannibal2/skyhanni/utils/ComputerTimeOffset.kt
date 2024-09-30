6ff17891ad622eb113ef5d5e810eb16f4d8caf03
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-01 00:37:24 +0200

    resolve reviews
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/ComputerTimeOffset.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/ComputerTimeOffset.kt
@@ -10,30 +10,34 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import org.apache.commons.net.ntp.NTPUDPClient
 import java.net.InetAddress
 import kotlin.math.abs
-import kotlin.math.absoluteValue
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
 @SkyHanniModule
-object TimeOffset {
-    var offsetMillis = 0L
+object ComputerTimeOffset {
+    var offsetMillis: Duration? = null
         private set
 
     init {
         SkyHanniMod.coroutineScope.launch {
-            offsetMillis = getNtpOffset("time.google.com") ?: 0
-            print("Time offset: $offsetMillis")
+            offsetMillis = getNtpOffset("time.google.com")
+            offsetMillis?.let {
+                print("SkyHanni detected a time offset of ${it.format()}.")
+            } ?: print("SkyHanni failed to detect a time offset.")
         }
     }
 
-    private fun getNtpOffset(ntpServer: String): Long? {
+    private fun getNtpOffset(ntpServer: String): Duration? {
         return try {
             val client = NTPUDPClient()
             val address = InetAddress.getByName(ntpServer)
             val timeInfo = client.getTime(address)
 
             timeInfo.computeDetails()
-            timeInfo.offset
+            timeInfo.offset.toDuration(DurationUnit.MILLISECONDS)
         } catch (e: Exception) {
             e.printStackTrace()
             null
@@ -42,14 +46,17 @@ object TimeOffset {
 
     @SubscribeEvent
     fun onProfileJoin(event: ProfileJoinEvent) {
-        if (abs(offsetMillis) > 10 * 1000) {
-            val formatted = offsetMillis.toDuration(DurationUnit.MILLISECONDS).format()
-            ChatUtils.userError("Your computer's clock is off by $formatted. Please update your time settings.")
+        if (!SkyHanniMod.feature.misc.warnAboutPcTimeOffset) return
+        offsetMillis?.let {
+            if (it.absoluteValue > 5.seconds) {
+                ChatUtils.userError("Your computer's clock is off by ${it.format()}. Please update your time settings.")
+            }
         }
     }
 
     @SubscribeEvent
     fun onDebugCollect(event: DebugDataCollectEvent) {
+        offsetMillis?.absoluteValue?.let { if (it < 100.milliseconds) return }
         event.title("Time Offset")
         event.addData("$offsetMillis ms")
     }
