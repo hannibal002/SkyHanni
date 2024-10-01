06678b80ea653e57301b3454bfdd43255dce664a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-01 11:27:11 +0200

    add os based instructions
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/ComputerTimeOffset.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/ComputerTimeOffset.kt
@@ -20,6 +20,15 @@ object ComputerTimeOffset {
     var offsetMillis: Duration? = null
         private set
 
+    private val offsetFixLinks by lazy {
+        when {
+            OSUtils.isWindows -> "https://support.microsoft.com/en-us/windows/how-to-set-your-time-and-time-zone-dfaa7122-479f-5b98-2a7b-fa0b6e01b261"
+            OSUtils.isLinux -> "https://unix.stackexchange.com/a/79116"
+            OSUtils.isMac -> "https://support.apple.com/guide/mac-help/set-the-date-and-time-automatically-mchlp2996/mac"
+            else -> null
+        }
+    }
+
     init {
         SkyHanniMod.coroutineScope.launch {
             offsetMillis = getNtpOffset("time.google.com")
@@ -48,7 +57,11 @@ object ComputerTimeOffset {
         if (!SkyHanniMod.feature.misc.warnAboutPcTimeOffset) return
         offsetMillis?.let {
             if (it.absoluteValue > 5.seconds) {
-                ChatUtils.userError("Your computer's clock is off by ${it.format()}. Please update your time settings.")
+                ChatUtils.clickableLinkChat(
+                    "Your computer's clock is off by ${it.format()}. Please update your time settings. Click here for instructions.",
+                    offsetFixLinks ?: return,
+                    prefixColor = "§c",
+                )
             }
         }
     }
