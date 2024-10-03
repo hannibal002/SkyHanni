e7b24589b9f73daddb8a901a11a9f38e0652d2fd
martimavocado <martim.cavaco@tutanota.com>
2024-10-03 18:55:05 +0100

    misc fixes

--- src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/HighHeatSound.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/HighHeatSound.kt
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.MiningAPI
 import at.hannibal2.skyhanni.events.PlaySoundEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.LocationUtils
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @SkyHanniModule
@@ -12,10 +13,11 @@ object HighHeatSound {
 
     @SubscribeEvent
     fun onSound(event: PlaySoundEvent) {
-        if (isEnabled()) return
+        if (!isEnabled()) return
+        if (!(LocationUtils.playerLocation().y <= 65.0 && MiningAPI.heat >= 90)) return
 
         if (event.soundName == "mob.wolf.panting" && event.pitch == 0.0f && event.volume == 1.0f) event.cancel()
     }
 
-    private fun isEnabled() = config.muteHighHeat && MiningAPI.inCrystalHollows() && MiningAPI.heat >= 90
+    private fun isEnabled() = config.muteHighHeat && MiningAPI.inCrystalHollows()
 }
