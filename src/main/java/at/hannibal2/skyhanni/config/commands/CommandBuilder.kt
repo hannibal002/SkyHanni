afcb5d7d0f642c08a9582cf96f6830c5b3047a39
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-03 11:07:20 +0200

    gravy dm
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/commands/CommandBuilder.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/CommandBuilder.kt
@@ -1,9 +1,5 @@
 package at.hannibal2.skyhanni.config.commands
 
-import net.minecraft.command.ICommandSender
-import net.minecraft.util.BlockPos
-
-// TODO: Gravy's dm
 class CommandBuilder(val name: String) {
     var description: String = ""
     var category: CommandCategory = CommandCategory.MAIN
@@ -11,25 +7,6 @@ class CommandBuilder(val name: String) {
     var autoComplete: ((Array<String>) -> List<String>) = { listOf() }
     var callback: (Array<String>) -> Unit = {}
 
-    fun toSimpleCommand() = SimpleCommand(
-        name.lowercase(),
-        aliases,
-        createCommand(callback),
-        object : SimpleCommand.TabCompleteRunnable {
-            override fun tabComplete(
-                sender: ICommandSender?,
-                args: Array<String>?,
-                pos: BlockPos?,
-            ): List<String> {
-                return autoComplete(args ?: emptyArray())
-            }
-        },
-    )
-
-    private fun createCommand(function: (Array<String>) -> Unit) = object : SimpleCommand.ProcessCommandRunnable() {
-        override fun processCommand(sender: ICommandSender?, args: Array<String>?) {
-            if (args != null) function(args.asList().toTypedArray())
-        }
-    }
+    fun toSimpleCommand() = SimpleCommand(name.lowercase(), aliases, callback, autoComplete)
 }
 
--- src/main/java/at/hannibal2/skyhanni/config/commands/SimpleCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/SimpleCommand.kt
@@ -6,36 +6,25 @@ import net.minecraft.command.ICommandSender
 import net.minecraft.util.BlockPos
 
 class SimpleCommand(
-    private val commandName: String,
-    private var aliases: List<String>,
-    private val runnable: ProcessCommandRunnable,
-    private var tabRunnable: TabCompleteRunnable?,
+    private val name: String,
+    private val aliases: List<String>,
+    private val callback: (Array<String>) -> Unit,
+    private val tabCallback: ((Array<String>) -> List<String>) = { emptyList() },
 ) : CommandBase() {
 
-    abstract class ProcessCommandRunnable {
-        abstract fun processCommand(sender: ICommandSender?, args: Array<String>?)
-    }
-
-    interface TabCompleteRunnable {
-        fun tabComplete(sender: ICommandSender?, args: Array<String>?, pos: BlockPos?): List<String>
-    }
-
     override fun canCommandSenderUseCommand(sender: ICommandSender) = true
-
-    override fun getCommandName() = commandName
-
+    override fun getCommandName() = name
     override fun getCommandAliases() = aliases
-
-    override fun getCommandUsage(sender: ICommandSender) = "/$commandName"
+    override fun getCommandUsage(sender: ICommandSender) = "/$name"
 
     override fun processCommand(sender: ICommandSender, args: Array<String>) {
         try {
-            runnable.processCommand(sender, args)
+            callback(args)
         } catch (e: Throwable) {
-            ErrorManager.logErrorWithData(e, "Error while running command /$commandName")
+            ErrorManager.logErrorWithData(e, "Error while running command /$name")
         }
     }
 
     override fun addTabCompletionOptions(sender: ICommandSender, args: Array<String>, pos: BlockPos) =
-        if (tabRunnable != null) tabRunnable!!.tabComplete(sender, args, pos) else null
+        tabCallback(args).takeIf { it.isNotEmpty() }
 }
