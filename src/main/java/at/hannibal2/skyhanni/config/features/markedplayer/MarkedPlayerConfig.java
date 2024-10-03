88cc2937c9f8fb93e83f806b331fd0aa2e6b55dc
HiZe <super@hize.be>
2024-10-03 16:30:01 +0200

    message

--- src/main/java/at/hannibal2/skyhanni/config/features/markedplayer/MarkedPlayerConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/markedplayer/MarkedPlayerConfig.java
@@ -2,8 +2,10 @@ package at.hannibal2.skyhanni.config.features.markedplayer;
 
 import at.hannibal2.skyhanni.utils.LorenzColor;
 import com.google.gson.annotations.Expose;
+import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 import io.github.notenoughupdates.moulconfig.observer.Property;
 
@@ -33,5 +35,37 @@ public class MarkedPlayerConfig {
     @Expose
     @ConfigEditorDropdown
     public Property<LorenzColor> entityColor = Property.of(LorenzColor.YELLOW);
+
+    @Expose
+    @ConfigOption(name = "Join/Leave Message", desc = "")
+    @Accordion
+    public JoinLeaveMessage joinLeaveMessage = new JoinLeaveMessage();
+
+    public static class JoinLeaveMessage {
+        @Expose
+        @ConfigOption(name = "Enabled", desc = "Enable the Join/Leave message for marked players.")
+        @ConfigEditorBoolean
+        public boolean enabled = false;
+
+        @Expose
+        @ConfigOption(name = "Players List", desc = "Players list you want to be notified for.\n§cCase sensitive, separated by comma.")
+        @ConfigEditorText
+        public Property<String> playersList = Property.of("hypixel,Minikloon");
+
+        @Expose
+        @ConfigOption(name = "Use Prefix", desc = "Should the [SkyHanni] prefix should be in the join/leave message ?")
+        @ConfigEditorBoolean
+        public boolean usePrefix = true;
+
+        @Expose
+        @ConfigOption(name = "Join Message", desc = "Configure the message when someone join.\n&& is replaced with the minecraft color code §.\n%s is replaced with the player name.")
+        @ConfigEditorText
+        public String joinMessage = "&&b%s &&ajoined your lobby.";
+
+        @Expose
+        @ConfigOption(name = "Left Message", desc = "Configure the message when someone leave.\n&& is replaced with the minecraft color code §.\n%s is replaced with the player name.")
+        @ConfigEditorText
+        public String leftMessage = "&&b%s &&cleft your lobby.";
+    }
 
 }
