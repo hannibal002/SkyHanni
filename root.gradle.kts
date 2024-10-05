86c3700cfd11ba53da18bff1987c69d19de17b81
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-10-05 17:45:37 +0200

    Version 0.27 Beta 15

--- root.gradle.kts
+++ root.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 allprojects {
     group = "at.hannibal2.skyhanni"
-    version = "0.27.Beta.14"
+    version = "0.27.Beta.15"
     repositories {
         mavenCentral()
         mavenLocal()
