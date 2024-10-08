14ca66bdac411c46cc021c0425849c3584a4f5a9
Cal <cwolfson58@gmail.com>
2024-10-08 13:37:42 +1100

    put the bounding boxes into the enum

--- src/main/java/at/hannibal2/skyhanni/features/event/hoppity/NucleusBarriersBox.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/hoppity/NucleusBarriersBox.kt
@@ -16,46 +16,53 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 object NucleusBarriersBox {
     private val config get() = SkyHanniMod.feature.mining.crystalHighlighter
 
-    private enum class Crystal(val color: LorenzColor) {
-        AMBER(LorenzColor.GOLD),
-        AMETHYST(LorenzColor.DARK_PURPLE),
-        TOPAZ(LorenzColor.YELLOW),
-        JADE(LorenzColor.GREEN),
-        SAPPHIRE(LorenzColor.BLUE),
+    private enum class Crystal(val color: LorenzColor, val boundingBox: AxisAlignedBB) {
+        AMBER(
+            LorenzColor.GOLD,
+            AxisAlignedBB(
+                BlockPos(474.0, 124.0, 524.0),
+                BlockPos(485.0, 111.0, 535.0),
+            ).expandBlock(),
+        ),
+        AMETHYST(
+            LorenzColor.DARK_PURPLE,
+            AxisAlignedBB(
+                BlockPos(474.0, 124.0, 492.0),
+                BlockPos(485.0, 111.0, 503.0),
+            ).expandBlock(),
+        ),
+        TOPAZ(
+            LorenzColor.YELLOW,
+            AxisAlignedBB(
+                BlockPos(508.0, 124.0, 473.0),
+                BlockPos(519.0, 111.0, 484.0),
+            ).expandBlock(),
+        ),
+        JADE(
+            LorenzColor.GREEN,
+            AxisAlignedBB(
+                BlockPos(542.0, 124.0, 492.0),
+                BlockPos(553.0, 111.0, 503.0),
+            ).expandBlock(),
+        ),
+        SAPPHIRE(
+            LorenzColor.BLUE,
+            AxisAlignedBB(
+                BlockPos(542.0, 124.0, 524.0),
+                BlockPos(553.0, 111.0, 535.0),
+            ).expandBlock(),
+        ),
     }
-
-    private val crystalCoordinatePairs: Map<Crystal, AxisAlignedBB> = mapOf(
-        Crystal.AMBER to AxisAlignedBB(
-            BlockPos(474.0, 124.0, 524.0),
-            BlockPos(485.0, 111.0, 535.0),
-        ).expandBlock(),
-        Crystal.AMETHYST to AxisAlignedBB(
-            BlockPos(474.0, 124.0, 492.0),
-            BlockPos(485.0, 111.0, 503.0),
-        ).expandBlock(),
-        Crystal.TOPAZ to AxisAlignedBB(
-            BlockPos(508.0, 124.0, 473.0),
-            BlockPos(519.0, 111.0, 484.0),
-        ).expandBlock(),
-        Crystal.JADE to AxisAlignedBB(
-            BlockPos(542.0, 124.0, 492.0),
-            BlockPos(553.0, 111.0, 503.0),
-        ).expandBlock(),
-        Crystal.SAPPHIRE to AxisAlignedBB(
-            BlockPos(542.0, 124.0, 524.0),
-            BlockPos(553.0, 111.0, 535.0),
-        ).expandBlock(),
-    )
 
     @SubscribeEvent
     fun onRenderWorld(event: LorenzRenderWorldEvent) {
         if (!isEnabled()) return
 
-        crystalCoordinatePairs.forEach { (crystal, axis) ->
+        Crystal.entries.forEach { crystal ->
             event.drawFilledBoundingBox_nea(
-                axis,
+                crystal.boundingBox,
                 crystal.color.addOpacity(config.opacity),
-                renderRelativeToCamera = false
+                renderRelativeToCamera = false,
             )
         }
     }
