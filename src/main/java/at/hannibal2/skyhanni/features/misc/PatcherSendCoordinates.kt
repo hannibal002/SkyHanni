f8533435f8b3cdac685cf35de0d1b2e9ae43ea22
jani270 <69345714+jani270@users.noreply.github.com>
2024-10-09 19:01:42 +0200

    Improvement: Add Distance to Patcher Send Coords Waypoints

--- src/main/java/at/hannibal2/skyhanni/features/misc/PatcherSendCoordinates.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/PatcherSendCoordinates.kt
@@ -12,6 +12,7 @@ import at.hannibal2.skyhanni.utils.LocationUtils
 import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzLogger
 import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.RenderUtils.drawColor
 import at.hannibal2.skyhanni.utils.RenderUtils.drawString
@@ -67,9 +68,12 @@ object PatcherSendCoordinates {
 
         for (beacon in patcherBeacon) {
             val location = beacon.location
+            val distance = location.distance(LocationUtils.playerLocation())
+            val formattedDistance = distance.toInt().addSeparators()
+
             event.drawColor(location, LorenzColor.DARK_GREEN, alpha = 1f)
             event.drawWaypointFilled(location, config.color.toChromaColor(), true, true)
-            event.drawString(location.add(0.5, 0.5, 0.5), beacon.name, true, LorenzColor.DARK_BLUE.toColor())
+            event.drawString(location.add(0.5, 0.5, 0.5), beacon.name + " §e[${formattedDistance}m]", true, LorenzColor.DARK_BLUE.toColor())
         }
     }
 
