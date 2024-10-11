a0559db957420684950b00a52e1134dbdd0f08fb
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-10-11 22:24:34 +0200

    Fixed griffin pet warning not supporting diana autopet rules.

--- src/main/java/at/hannibal2/skyhanni/features/event/diana/GriffinPetWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/diana/GriffinPetWarning.kt
@@ -1,9 +1,12 @@
 package at.hannibal2.skyhanni.features.event.diana
 
 import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.events.BurrowDugEvent
+import at.hannibal2.skyhanni.events.IslandChangeEvent
 import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -11,7 +14,7 @@ import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object GriffinPetWarning {
-
+    private var wasCorrectPetAlready = false
     private var lastWarnTime = SimpleTimeMark.farPast()
 
     @SubscribeEvent
@@ -21,10 +24,36 @@ object GriffinPetWarning {
         if (!DianaAPI.isDoingDiana()) return
         if (!DianaAPI.hasSpadeInHand()) return
 
-        if (!DianaAPI.hasGriffinPet() && lastWarnTime.passedSince() > 30.seconds) {
+        val hasGriffinPet = DianaAPI.hasGriffinPet()
+        if (hasGriffinPet) {
+            wasCorrectPetAlready = true
+            return
+        }
+
+        if (wasCorrectPetAlready) return
+
+        warn()
+    }
+
+    @SubscribeEvent
+    fun onBurrowDug(event: BurrowDugEvent) {
+        DelayedRun.runDelayed(2.seconds) {
+            wasCorrectPetAlready = false
+        }
+    }
+
+    @SubscribeEvent
+    fun onIslandChange(event: IslandChangeEvent) {
+        DelayedRun.runDelayed(5.seconds) {
+            wasCorrectPetAlready = false
+        }
+    }
+
+    private fun warn() {
+        ChatUtils.chat("Reminder to use a Griffin pet for Mythological Ritual!")
+        if (lastWarnTime.passedSince() > 30.seconds) {
             lastWarnTime = SimpleTimeMark.now()
             LorenzUtils.sendTitle("§cGriffin Pet!", 3.seconds)
-            ChatUtils.chat("Reminder to use a Griffin pet for Mythological Ritual!")
         }
     }
 }
