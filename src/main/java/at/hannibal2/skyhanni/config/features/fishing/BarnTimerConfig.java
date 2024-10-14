d8d83828293dc8e5b87d0bc2d2ef59d0165220ca
NeoNyaa <56982408+NeoNyaa@users.noreply.github.com>
2024-10-14 05:56:18 +0100

    Changed option to false by default

--- src/main/java/at/hannibal2/skyhanni/config/features/fishing/BarnTimerConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/fishing/BarnTimerConfig.java
@@ -27,7 +27,7 @@ public class BarnTimerConfig {
         desc = "Show the Barn Fishing Timer whenever you fish up a sea creature, regardless of location."
     )
     @ConfigEditorBoolean
-    public Property<Boolean> showAnywhere = Property.of(true);
+    public Property<Boolean> showAnywhere = Property.of(false);
 
     @Expose
     @ConfigOption(
