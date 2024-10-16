414d66eb725a8e8e374a9380169c9f95569f5da3
Mikecraft1224 <Mikecraft1224@gmail.com>
2024-10-16 17:03:27 +0200

    Added 1s delay to joining Skyblock

--- src/main/java/at/hannibal2/skyhanni/features/misc/AutoJoinSkyblock.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/AutoJoinSkyblock.kt
@@ -3,8 +3,10 @@ package at.hannibal2.skyhanni.features.misc
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.events.HypixelJoinEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object AutoJoinSkyblock {
@@ -12,6 +14,8 @@ object AutoJoinSkyblock {
     fun onHypixelJoin(event: HypixelJoinEvent) {
         if (!SkyHanniMod.feature.misc.autoJoinSkyblock) return
 
-        HypixelCommands.skyblock()
+        DelayedRun.runDelayed(1.seconds) {
+            HypixelCommands.skyblock()
+        }
     }
 }
