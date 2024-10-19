60c420a4fffb660f684a6e7fee59030f0cb287aa
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-10-20 00:47:56 +0200

    Version 0.28 Beta 5

--- root.gradle.kts
+++ root.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 allprojects {
     group = "at.hannibal2.skyhanni"
-    version = "0.28.Beta.4"
+    version = "0.28.Beta.5"
     repositories {
         mavenCentral()
         mavenLocal()
