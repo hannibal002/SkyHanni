8ede66854b6414fbf71fa8c038d5685cad28a688
Cal <cwolfson58@gmail.com>
2024-10-20 20:51:50 +1100

    some more 1.8.9 mappings

--- src/main/java/at/hannibal2/skyhanni/data/ChatManager.kt
+++ src/main/java/at/hannibal2/skyhanni/data/ChatManager.kt
@@ -118,7 +118,11 @@ object ChatManager {
 
     @SubscribeEvent(receiveCanceled = true)
     fun onChatReceive(event: ClientChatReceivedEvent) {
+        //#if MC<1.12
         if (event.type.toInt() == 2) return
+        //#else
+        //$$ if (event.type.id.toInt() == 2) return
+        //#endif
 
         val original = event.message
         val message = LorenzUtils.stripVanillaMessage(original.formattedText)
