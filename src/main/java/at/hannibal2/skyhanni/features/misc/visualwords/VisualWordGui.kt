b252b1f68783ce9f8f698a2759bfb37e80ec6582
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-10-19 21:42:38 -0400

    Remove hardcoded textures, add detekt rule

--- src/main/java/at/hannibal2/skyhanni/features/misc/visualwords/VisualWordGui.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/visualwords/VisualWordGui.kt
@@ -12,6 +12,7 @@ import at.hannibal2.skyhanni.utils.KeyboardManager
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.OSUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
+import at.hannibal2.skyhanni.utils.SkullTextureHolder
 import at.hannibal2.skyhanni.utils.SoundUtils
 import at.hannibal2.skyhanni.utils.StringUtils.convertToFormatted
 import com.google.gson.JsonObject
@@ -83,8 +84,7 @@ open class VisualWordGui : GuiScreen() {
             ItemUtils.createSkull(
                 displayName = "§§Up",
                 uuid = "7f68dd73-1ff6-4193-b246-820975d6fab1",
-                value = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1" +
-                    "cmUvNzczMzRjZGRmYWI0NWQ3NWFkMjhlMWE0N2JmOGNmNTAxN2QyZjA5ODJmNjczN2RhMjJkNDk3Mjk1MjUxMDY2MSJ9fX0=",
+                value = SkullTextureHolder.getTexture("UP_ARROW"),
             )
         }
 
@@ -92,9 +92,7 @@ open class VisualWordGui : GuiScreen() {
             ItemUtils.createSkull(
                 displayName = "§§Down",
                 uuid = "e4ace6de-0629-4719-aea3-3e113314dd3f",
-                value =
-                "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTc3NDIwMz" +
-                    "RmNTlkYjg5MGM4MDA0MTU2YjcyN2M3N2NhNjk1YzQzOTlkOGUwZGE1Y2U5MjI3Y2Y4MzZiYjhlMiJ9fX0=",
+                value = SkullTextureHolder.getTexture("DOWN_ARROW"),
             )
         }
     }
