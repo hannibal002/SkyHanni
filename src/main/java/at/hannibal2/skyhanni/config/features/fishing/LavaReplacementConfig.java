fc5a658cc02955401ed6500a470039cde827547d
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2024-10-22 15:45:25 +0100

    Update Kuudra Lava Options to add Enum and BAD IF STATEMENTS.

--- src/main/java/at/hannibal2/skyhanni/config/features/fishing/LavaReplacementConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/fishing/LavaReplacementConfig.java
@@ -1,12 +1,15 @@
 package at.hannibal2.skyhanni.config.features.fishing;
 
 import at.hannibal2.skyhanni.config.FeatureToggle;
+import at.hannibal2.skyhanni.features.fishing.LavaReplacement.IslandsToReplace;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorButton;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableList;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 import io.github.notenoughupdates.moulconfig.observer.Property;
-import net.minecraft.client.Minecraft;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LavaReplacementConfig {
 
@@ -17,12 +20,13 @@ public class LavaReplacementConfig {
     public Property<Boolean> enabled = Property.of(false);
 
     @Expose
-    @ConfigOption(name = "Only In Crimson Isle", desc = "Enable the water texture only in Crimson Isle.")
+    @ConfigOption(name = "Replace Everywhere", desc = "Replace the lava texture In All Islands regardless of List Below.")
     @ConfigEditorBoolean
-    public Property<Boolean> onlyInCrimsonIsle = Property.of(true);
+    @FeatureToggle
+    public Property<Boolean> enabledeverywehere = Property.of(false);
 
     @Expose
-    @ConfigOption(name = "Only In Kuudra", desc = "Enable the water texture only in Kuudra.")
-    @ConfigEditorBoolean
-    public Property<Boolean> onlyInKuudra = Property.of(true);
-}
+    @ConfigOption(name = "Islands", desc = "Islands to Replace Lava In.")
+    @ConfigEditorDraggableList
+    public Property<List<IslandsToReplace>> IslandsToReplace = Property.of(new ArrayList<>());
+    }
--- src/main/java/at/hannibal2/skyhanni/features/fishing/LavaReplacement.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/LavaReplacement.kt
@@ -3,9 +3,11 @@ package at.hannibal2.skyhanni.features.fishing
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.ConfigLoadEvent
+import at.hannibal2.skyhanni.events.IslandChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ConditionalUtils
 import at.hannibal2.skyhanni.utils.LorenzUtils
+import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
 import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
@@ -15,19 +17,37 @@ object LavaReplacement {
     private val config get() = SkyHanniMod.feature.fishing.lavaReplacement
 
     @SubscribeEvent
-    fun onConfigLoad(event: ConfigLoadEvent) {
-        ConditionalUtils.onToggle(config.enabled, config.onlyInCrimsonIsle) {
+    fun onIslandChange(event: IslandChangeEvent) {
+        if (replaceLava()) {
             Minecraft.getMinecraft().renderGlobal.loadRenderers()
         }
     }
+    fun onConfigLoad(event: ConfigLoadEvent) {
 
+        ConditionalUtils.onToggle(
+            config.enabled,
+            config.IslandsToReplace,
+            ) {
+            Minecraft.getMinecraft().renderGlobal.loadRenderers()
+        }
+
+    }
+    enum class IslandsToReplace(private val displayName: String)
+    {
+        KUUDRA("Kuudra"),
+        CATACOMBS("Dungeons"),
+        CRIMSON_ISLE("Crimson Isle"),
+        ;
+        override fun toString() = displayName
+
+        fun isSelected() = SkyHanniMod.feature.fishing.lavaReplacement.IslandsToReplace.get().contains(this)
+    }
     @JvmStatic
     fun replaceLava(): Boolean {
         if (!LorenzUtils.inSkyBlock || !config.enabled.get()) return false
-        return when (LorenzUtils.skyBlockIsland) {
-            IslandType.CRIMSON_ISLE -> config.onlyInCrimsonIsle.get()
-            IslandType.KUUDRA_ARENA -> config.onlyInKuudra.get()
-            else -> false
-        }
-    }
-}
+        if (config.enabledeverywehere.get()) return true
+        if (IslandsToReplace.CATACOMBS.isSelected() && IslandType.CATACOMBS.isInIsland()) return true
+        if (IslandsToReplace.KUUDRA.isSelected() && IslandType.KUUDRA_ARENA.isInIsland()) return true
+        if (IslandsToReplace.CRIMSON_ISLE.isSelected() && IslandType.CRIMSON_ISLE.isInIsland()) return true
+        return false
+}}
