bd713a0072e11f908550158f75af6dd464a4c39f
Empa <itsempa@users.noreply.github.com>
2024-10-22 14:46:47 +0200

    shut up intellij too

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/ChatLineData.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/ChatLineData.kt
@@ -3,6 +3,6 @@ package at.hannibal2.skyhanni.mixins.hooks
 import net.minecraft.util.IChatComponent
 
 interface ChatLineData {
-    @Suppress("ktlint:standard:property-naming", "VariableNaming")
+    @Suppress("ktlint:standard:property-naming", "VariableNaming", "PropertyName")
     var skyHanni_fullComponent: IChatComponent
 }
