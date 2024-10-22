e5b2015a113b725f0f0cb2fbb03493d27ba5232f
nopo <nopotheemail@gmail.com>
2024-10-23 00:01:19 +1100

    fix merge 3

--- src/main/java/at/hannibal2/skyhanni/features/commands/HelpCommand.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/HelpCommand.kt
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.features.commands
 
 import at.hannibal2.skyhanni.config.commands.CommandBuilder
 import at.hannibal2.skyhanni.config.commands.Commands.commandList
-import at.hannibal2.skyhanni.config.commands.Commands.commands
 import at.hannibal2.skyhanni.utils.StringUtils.splitLines
 import at.hannibal2.skyhanni.utils.chat.Text
 import at.hannibal2.skyhanni.utils.chat.Text.hover
