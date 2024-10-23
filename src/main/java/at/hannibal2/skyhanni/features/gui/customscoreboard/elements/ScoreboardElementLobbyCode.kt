07974eeca7c667b0a12aa1cb351da63c60f6ae03
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-23 18:47:36 +0200

    add date!
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementLobbyCode.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementLobbyCode.kt
@@ -2,15 +2,22 @@ package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 
 import at.hannibal2.skyhanni.data.HypixelData
 import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 // internal
 // update on island change and every second while in dungeons
 object ScoreboardElementLobbyCode : ScoreboardElement() {
+    private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+
     override fun getDisplay(): String? {
-        val lobbyCode = HypixelData.serverId ?: return null
+        val lobbyCode = HypixelData.serverId
         val roomId = DungeonAPI.getRoomID()?.let { " §8$it" }.orEmpty()
-        return "§8$lobbyCode$roomId"
+        val localDate = LocalDate.now().format(formatter)
+
+        return (if (CustomScoreboard.displayConfig.dateInLobbyCode) "§7$localDate " else "") + lobbyCode?.let { "§8$it" } + roomId
     }
 
-    override val configLine = "§8mega77CK"
+    override val configLine = "§710/23/2024 §8mega77CK"
 }
