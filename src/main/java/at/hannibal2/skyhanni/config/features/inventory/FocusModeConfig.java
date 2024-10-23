faf8d31a867b314a144bf0f1b9255438675eb961
calwolfson <cwolfson58@gmail.com>
2024-10-23 15:33:27 +1100

    search tagging

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/FocusModeConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/FocusModeConfig.java
@@ -4,12 +4,14 @@ import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorKeybind;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
+import io.github.notenoughupdates.moulconfig.annotations.SearchTag;
 import org.lwjgl.input.Keyboard;
 
 public class FocusModeConfig {
 
     @Expose
     @ConfigOption(name = "Enabled", desc = "In focus mode you only see the name of the item instead of the whole description.")
+    @SearchTag("compact hide")
     @ConfigEditorBoolean
     public boolean enabled = false;
 
