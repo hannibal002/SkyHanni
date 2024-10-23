71faa07b0392bab1bc9382a5a6c7a4ac7a50a22f
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-23 18:59:11 +0200

    fuck you detekt
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementLobbyCode.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementLobbyCode.kt
@@ -12,11 +12,15 @@ object ScoreboardElementLobbyCode : ScoreboardElement() {
     private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
 
     override fun getDisplay(): String? {
-        val lobbyCode = HypixelData.serverId
+        val lobbyCode = HypixelData.serverId?.let { "§8$it" }.orEmpty()
         val roomId = DungeonAPI.getRoomID()?.let { " §8$it" }.orEmpty()
         val localDate = LocalDate.now().format(formatter)
 
-        return (if (CustomScoreboard.displayConfig.dateInLobbyCode) "§7$localDate " else "") + lobbyCode?.let { "§8$it" } + roomId
+        return buildString {
+            if (CustomScoreboard.displayConfig.dateInLobbyCode) append("§7$localDate ")
+            append(lobbyCode)
+            append(roomId)
+        }
     }
 
     override val configLine = "§710/23/2024 §8mega77CK"
