8fe2917495a4a79617f8f589504342f3eb73fef9
syeyoung <cyoung06@naver.com>
2025-02-04 03:17:39 +0900

    Kotlin Coroutines Dispatcher.Main support
    Create Dispatcher.Minecraft with .delay support, and register it in service provider so that it gets picked up by kotlinx.coroutine.

--- src/main/java/at/hannibal2/skyhanni/utils/MinecraftDispatcher.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/MinecraftDispatcher.kt
@@ -4,16 +4,76 @@ import kotlinx.coroutines.MainCoroutineDispatcher
 import net.minecraft.client.Minecraft
 import kotlin.coroutines.CoroutineContext
 
-object MinecraftDispatcher : MainCoroutineDispatcher() {
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.internal.*
+import kotlin.time.Duration.Companion.milliseconds
 
-    override val immediate: MainCoroutineDispatcher
-        get() = this
+@Suppress("unused")
+public val Dispatchers.Minecraft : MinecraftDispatcher
+    get() = at.hannibal2.skyhanni.utils.Minecraft
 
-    override fun isDispatchNeeded(context: CoroutineContext): Boolean {
-        return !Minecraft.getMinecraft().isCallingFromMinecraftThread
-    }
-
+@OptIn(InternalCoroutinesApi::class)
+public sealed class MinecraftDispatcher : MainCoroutineDispatcher(), Delay {
     override fun dispatch(context: CoroutineContext, block: Runnable) {
         Minecraft.getMinecraft().addScheduledTask(block)
     }
+
+    override fun scheduleResumeAfterDelay(timeMillis: Long, continuation: CancellableContinuation<Unit>) {
+        val cancellableRun = schedule(timeMillis) {
+            with(continuation) { resumeUndispatched(Unit) }
+        }
+        continuation.invokeOnCancellation { cancellableRun.cancel() }
+    }
+
+    override fun invokeOnTimeout(timeMillis: Long, block: Runnable, context: CoroutineContext): DisposableHandle {
+        val cancellableRun = schedule(timeMillis) {
+            block.run()
+        }
+        return DisposableHandle { cancellableRun.cancel() }
+    }
+
+    private fun schedule(timeMillis: Long, action: () -> Unit): CancellableRun {
+        val run = CancellableRun(action);
+        DelayedRun.runDelayed(timeMillis.milliseconds, run);
+        return run;
+    }
+
+    class CancellableRun(val run: () -> Unit, @Transient var isCancelled: Boolean = false): () -> Unit {
+        override fun invoke() {
+            if (isCancelled) return
+            this.run();
+        }
+
+        fun cancel() {
+            isCancelled = true
+        }
+    }
 }
+
+@OptIn(InternalCoroutinesApi::class)
+internal class MinecraftDispatcherFactory : MainDispatcherFactory {
+    override val loadPriority: Int
+        get() = 0
+
+    override fun createDispatcher(allFactories: List<MainDispatcherFactory>): MainCoroutineDispatcher = Minecraft
+}
+
+private object ImmediateMinecraftDispatcher : MinecraftDispatcher() {
+    override val immediate: MainCoroutineDispatcher
+        get() = this
+
+    override fun isDispatchNeeded(context: CoroutineContext): Boolean = !Minecraft.getMinecraft().isCallingFromMinecraftThread
+
+    @OptIn(InternalCoroutinesApi::class)
+    override fun toString() = toStringInternalImpl() ?: "Minecraft.immediate"
+}
+
+internal object Minecraft : MinecraftDispatcher() {
+    override val immediate: MainCoroutineDispatcher
+        get() = ImmediateMinecraftDispatcher
+
+    @OptIn(InternalCoroutinesApi::class)
+    override fun toString() = toStringInternalImpl() ?: "Minecraft"
+}
+
