2ef80e95ce0f31f7f68d29e72aec28d1a1001fed
syeyoung <cyoung06@naver.com>
2025-02-04 16:54:54 +0900

    - Fix wildcard imports, import order, and few formatting issues.

--- src/main/java/at/hannibal2/skyhanni/utils/MinecraftDispatcher.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/MinecraftDispatcher.kt
@@ -1,16 +1,19 @@
 package at.hannibal2.skyhanni.utils
 
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DisposableHandle
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.MainCoroutineDispatcher
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.internal.MainDispatcherFactory
 import net.minecraft.client.Minecraft
 import kotlin.coroutines.CoroutineContext
-
-import kotlinx.coroutines.*
-import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.internal.*
 import kotlin.time.Duration.Companion.milliseconds
 
 @Suppress("unused")
-public val Dispatchers.Minecraft : MinecraftDispatcher
+public val Dispatchers.Minecraft: MinecraftDispatcher
     get() = at.hannibal2.skyhanni.utils.Minecraft
 
 @OptIn(InternalCoroutinesApi::class)
@@ -34,15 +37,15 @@ public sealed class MinecraftDispatcher : MainCoroutineDispatcher(), Delay {
     }
 
     private fun schedule(timeMillis: Long, action: () -> Unit): CancellableRun {
-        val run = CancellableRun(action);
-        DelayedRun.runDelayed(timeMillis.milliseconds, run);
-        return run;
+        val run = CancellableRun(action)
+        DelayedRun.runDelayed(timeMillis.milliseconds, run)
+        return run
     }
 
-    class CancellableRun(val run: () -> Unit, @Transient var isCancelled: Boolean = false): () -> Unit {
+    class CancellableRun(val run: () -> Unit, @Transient var isCancelled: Boolean = false) : () -> Unit {
         override fun invoke() {
             if (isCancelled) return
-            this.run();
+            this.run()
         }
 
         fun cancel() {
