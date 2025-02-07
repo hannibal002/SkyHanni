4f253830c6d0d7f2159bbaee940f8a2d05c06b18
NopoTheGamer <nopotheemail@gmail.com>
2025-02-08 10:06:46 +1100

    oops

--- src/main/java/at/hannibal2/skyhanni/config/features/rift/area/mountaintop/MountaintopConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/rift/area/mountaintop/MountaintopConfig.kt
@@ -5,7 +5,8 @@ import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
 class MountaintopConfig {
-    
+
+    @Expose
     @ConfigOption(name = "Sun Gecko", desc = "")
     @Accordion
     var sunGecko: SunGeckoConfig = SunGeckoConfig()
