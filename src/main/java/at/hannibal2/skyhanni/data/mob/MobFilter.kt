5a38cf39fbe7ebd3d474e379453f5febc5a2bf5a
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2025-02-08 22:24:34 +0100

    pattern should work with runic now

--- src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
+++ src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
@@ -87,9 +87,14 @@ object MobFilter {
         "filter.dojo",
         "^(?:(?<points>\\d+) pts|(?<empty>\\w+))$",
     )
+
+    /**REGEX-TEST: [Lv3] TheNewArrow's Purple Jerry 5 Hits
+     * REGEX-TEST: [Lv3] aheNewarrow's Purple Jerry 5 Hits
+     * REGEX-TEST: [Lv1] aThunderblade73's Green Jerrya 7 Hits
+     */
     val jerryPattern by patternGroup.pattern(
         "jerry",
-        "(?:\\[\\w+(?<level>\\d+)\\] )?(?<owner>\\w+)'s (?<name>\\w+ Jerry) \\d+ Hits",
+        "(?:\\[\\w+(?<level>\\d+)\\] )?(?:(?:a(?=a ))?(?<owner>\\w+)'s (?<name>\\w+ Jerrya?)) \\d+ Hits",
     )
     val petCareNamePattern by patternGroup.pattern(
         "pattern.petcare",
