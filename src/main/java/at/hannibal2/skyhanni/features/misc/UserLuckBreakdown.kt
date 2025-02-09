8da7f80a42ea7fbac13afc262d0fbb7804f89031
nopo <nopotheemail@gmail.com>
2025-02-09 19:09:38 +1100

    used but not import

--- src/main/java/at/hannibal2/skyhanni/features/misc/UserLuckBreakdown.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/UserLuckBreakdown.kt
@@ -15,8 +15,6 @@ import at.hannibal2.skyhanni.utils.CollectionUtils.addOrPut
 import at.hannibal2.skyhanni.utils.ItemUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.ItemUtils.name
-import at.hannibal2.skyhanni.utils.NeuInternalName.Companion.toInternalName
-import at.hannibal2.skyhanni.utils.NeuItems.getItemStack
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.NumberUtil.roundTo
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
