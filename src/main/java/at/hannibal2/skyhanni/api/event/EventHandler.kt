ecf46a37cc958786d310b43899632bb52298fbea
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-02-10 18:59:23 +0100

    more data for /shdebug event

--- src/main/java/at/hannibal2/skyhanni/api/event/EventHandler.kt
+++ src/main/java/at/hannibal2/skyhanni/api/event/EventHandler.kt
@@ -18,8 +18,7 @@ class EventHandler<T : SkyHanniEvent> private constructor(
     private val canReceiveCancelled: Boolean,
 ) {
 
-    var invokeCount: Long = 0L
-        private set
+    val invokeLog = SkyHanniEvents.EventInvokeLog()
 
     constructor(event: Class<T>, listeners: List<EventListeners.Listener>) : this(
         (event.name.split(".").lastOrNull() ?: event.name).replace("$", "."),
@@ -43,7 +42,7 @@ class EventHandler<T : SkyHanniEvent> private constructor(
     }
 
     fun post(event: T, onError: ((Throwable) -> Unit)? = null): Boolean {
-        invokeCount++
+        invokeLog.invokeCount++
         if (this.listeners.isEmpty()) return false
 
         if (SkyHanniEvents.isDisabledHandler(name)) return false
