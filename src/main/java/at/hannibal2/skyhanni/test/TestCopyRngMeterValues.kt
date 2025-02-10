bc114444c1759fe3a9cf2625f54017303d0a09f0
jani270 <69345714+jani270@users.noreply.github.com>
2025-02-10 17:23:39 +0100

    feat: Regex test

--- src/main/java/at/hannibal2/skyhanni/test/TestCopyRngMeterValues.kt
+++ src/main/java/at/hannibal2/skyhanni/test/TestCopyRngMeterValues.kt
@@ -19,10 +19,18 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 object TestCopyRngMeterValues {
 
     private val patternGroup = RepoPattern.group("test.dev.copyrng")
+
+    /**
+     * REGEX-TEST: §7§7Slayer XP: §d20,625§5/§d7,917
+     */
     private val slayerPattern by patternGroup.pattern(
         "slayer",
         "§7§7Slayer XP: §d.*§5/§d(?<xp>.*)"
     )
+
+    /**
+     * REGEX-TEST: §7§7Dungeon Score: §d1,237§5/§d40,620
+     */
     private val dungeonPattern by patternGroup.pattern(
         "dungeon",
         "§7§7Dungeon Score: §d.*§5/§d(?<xp>.*)"
