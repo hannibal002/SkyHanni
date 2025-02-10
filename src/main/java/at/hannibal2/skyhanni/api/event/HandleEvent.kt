c9ca4f0cb53510a5c822976f30f91f743e12f166
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-02-10 17:22:23 -0500

    Add multi

--- src/main/java/at/hannibal2/skyhanni/api/event/HandleEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/api/event/HandleEvent.kt
@@ -8,8 +8,15 @@ import kotlin.reflect.KClass
 annotation class HandleEvent(
     /**
      * For cases where the event properties are themselves not needed, and solely a listener for an event fire suffices.
+     * To specify multiple events, use [eventTypes] instead.
      */
     val eventType: KClass<out SkyHanniEvent> = SkyHanniEvent::class,
+
+    /**
+     * For cases where multiple events are listened to, and properties are unnecessary.
+     * To specify only one event, use [eventType] instead.
+     */
+    val eventTypes: Array<KClass<out SkyHanniEvent>> = [],
 
     /**
      * If the event should only be received while on SkyBlock.
--- src/main/java/at/hannibal2/skyhanni/api/event/SkyHanniEvents.kt
+++ src/main/java/at/hannibal2/skyhanni/api/event/SkyHanniEvents.kt
@@ -35,14 +35,29 @@ object SkyHanniEvents {
     fun isDisabledHandler(handler: String): Boolean = handler in disabledHandlers
     fun isDisabledInvoker(invoker: String): Boolean = invoker in disabledHandlerInvokers
 
-    @Suppress("UNCHECKED_CAST")
     private fun registerMethod(method: Method, instance: Any) {
         val options = method.getAnnotation(HandleEvent::class.java) ?: return
+        registerSingleEventType(options, method, instance)
+        registerMultipleEventTypes(options, method, instance)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun registerSingleEventType(options: HandleEvent, method: Method, instance: Any) {
         val eventType = method.parameterTypes.getOrNull(0) ?: options.eventType.java
         if (!SkyHanniEvent::class.java.isAssignableFrom(eventType)) return
         listeners.getOrPut(eventType as Class<SkyHanniEvent>) { EventListeners(eventType) }
             .addListener(method, instance, options)
     }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun registerMultipleEventTypes(options: HandleEvent, method: Method, instance: Any) {
+        options.eventTypes.map { it.java }.forEach { eventType ->
+            if (!SkyHanniEvent::class.java.isAssignableFrom(eventType)) return
+            listeners.getOrPut(eventType as Class<SkyHanniEvent>) { EventListeners(eventType) }
+                .addListener(method, instance, options)
+        }
+    }
+
 
     @HandleEvent
     fun onRepoReload(event: RepositoryReloadEvent) {
