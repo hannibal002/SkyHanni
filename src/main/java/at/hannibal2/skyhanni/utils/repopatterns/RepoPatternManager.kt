b4775c9a7d440b989789b88cfc8daf855f6879f6
Empa <itsempa@users.noreply.github.com>
2025-02-11 14:55:18 +0100

    this should hopefully fix the tests failing
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/repopatterns/RepoPatternManager.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/repopatterns/RepoPatternManager.kt
@@ -80,7 +80,9 @@ object RepoPatternManager {
     /**
      * Crash if in a development environment.
      */
-    private fun crash(reason: String) = ErrorManager.crashInDevEnv(reason) { RuntimeException(reason) }
+    private fun crash(reason: String) {
+        if (PlatformUtils.isDevEnvironment) throw RuntimeException(reason)
+    }
 
     /**
      * Check that the [owner] has exclusive right to the specified [key], and locks out other code parts from ever
