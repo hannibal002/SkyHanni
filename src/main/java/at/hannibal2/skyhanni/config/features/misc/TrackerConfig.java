b9594ab01ab46bfd07415cffb3d24958a214f09a
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-02-14 23:48:36 +0100

    replaced hide cheap items with scrollable

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/TrackerConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/TrackerConfig.java
@@ -75,29 +75,6 @@ public class TrackerConfig {
     }
 
     @Expose
-    @ConfigOption(name = "Hide Cheap Items", desc = "Hide cheap items.")
-    @Accordion
-    public HideCheapItemsConfig hideCheapItems = new HideCheapItemsConfig();
-
-    public static class HideCheapItemsConfig {
-
-        @Expose
-        @ConfigOption(name = "Enabled", desc = "Limit how many items should be shown.")
-        @ConfigEditorBoolean
-        public Property<Boolean> enabled = Property.of(true);
-
-        @Expose
-        @ConfigOption(name = "Show Expensive #", desc = "Always show the # most expensive items.")
-        @ConfigEditorSlider(minValue = 1, maxValue = 40, minStep = 1)
-        public Property<Integer> alwaysShowBest = Property.of(8);
-
-        @Expose
-        @ConfigOption(name = "Still Show Above", desc = "Always show items above this §6price in 1k §7even when not in the top # of items.")
-        @ConfigEditorSlider(minValue = 5, maxValue = 500, minStep = 5)
-        public Property<Integer> minPrice = Property.of(100);
-    }
-
-    @Expose
     @ConfigOption(name = "Hide outside Inventory", desc = "Hide Profit Trackers while not inside an inventory.")
     @ConfigEditorBoolean
     public boolean hideItemTrackersOutsideInventory = false;
