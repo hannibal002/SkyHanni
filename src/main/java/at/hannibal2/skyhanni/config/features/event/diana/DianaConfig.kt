c1e6bb676bb068779d9443b33d3896c9c12a3617
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-02-18 16:11:28 +0100

    change config to kotlin

--- src/main/java/at/hannibal2/skyhanni/config/features/event/diana/DianaConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/event/diana/DianaConfig.kt
@@ -1,110 +1,121 @@
-package at.hannibal2.skyhanni.config.features.event.diana;
+package at.hannibal2.skyhanni.config.features.event.diana
 
-import at.hannibal2.skyhanni.config.FeatureToggle;
-import com.google.gson.annotations.Expose;
-import io.github.notenoughupdates.moulconfig.annotations.Accordion;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorColour;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorKeybind;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
-import org.lwjgl.input.Keyboard;
+import at.hannibal2.skyhanni.config.FeatureToggle
+import com.google.gson.annotations.Expose
+import io.github.notenoughupdates.moulconfig.annotations.Accordion
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorColour
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorKeybind
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+import org.lwjgl.input.Keyboard
 
-public class DianaConfig {
-
+class DianaConfig {
     @Expose
-    @ConfigOption(name = "Highlight Inquisitors", desc = "Highlight Inquisitors found from the Mythological Event perk.")
+    @ConfigOption(
+        name = "Highlight Inquisitors",
+        desc = "Highlight Inquisitors found from the Mythological Event perk."
+    )
     @ConfigEditorBoolean
     @FeatureToggle
-    public boolean highlightInquisitors = true;
+    var highlightInquisitors: Boolean = true
 
     @Expose
     @ConfigOption(name = "Inquisitor Highlight", desc = "Color in which Inquisitors will be highlighted.")
     @ConfigEditorColour
-    public String color = "0:127:85:255:255";
+    var color: String = "0:127:85:255:255"
 
     @Expose
-    @ConfigOption(name = "Guess Next Burrow", desc = "Guess the next burrow location when using the Ancestral Spade. §eDoes not show the type of burrow.")
+    @ConfigOption(
+        name = "Guess Next Burrow",
+        desc = "Guess the next burrow location when using the Ancestral Spade. §eDoes not show the type of burrow."
+    )
     @ConfigEditorBoolean
     @FeatureToggle
-    public boolean guess = false;
+    var guess: Boolean = false
 
-    public enum GuessLogic {
-        SOOPY_GUESS("Soopy"),
-        PRECISE_GUESS("Precise");
+    enum class GuessLogic(private val display: String) {
+        SOOPY_GUESS("Soopy"), PRECISE_GUESS("Precise");
 
-        private final String display;
-
-        GuessLogic(String string) {
-            display = string;
-        }
-
-        @Override
-        public String toString() {
-            return display;
+        override fun toString(): String {
+            return display
         }
     }
 
     @Expose
     @ConfigOption(name = "Guessing Logic", desc = "Change which guess strategy to use.")
     @ConfigEditorDropdown
-    public GuessLogic guessLogic = GuessLogic.PRECISE_GUESS;
+    var guessLogic: GuessLogic = GuessLogic.PRECISE_GUESS
 
     @Expose
-    @ConfigOption(name = "Multi Guesses", desc = "Remember previous guess locations when guessing to a new location. §eDes only work with precise guess logic.")
+    @ConfigOption(
+        name = "Multi Guesses",
+        desc = "Remember previous guess locations when guessing to a new location. §eDes only work with precise guess logic."
+    )
     @ConfigEditorBoolean
-    public boolean multiGuesses = true;
+    var multiGuesses: Boolean = true
 
     @Expose
-    @ConfigOption(name = "Nearby Detection", desc = "Show burrow locations near you, include type of burrow. §eThis is unrelated to guess logic.")
+    @ConfigOption(
+        name = "Nearby Detection",
+        desc = "Show burrow locations near you, include type of burrow. §eThis is unrelated to guess logic."
+    )
     @ConfigEditorBoolean
     @FeatureToggle
-    public boolean burrowsNearbyDetection = false;
+    var burrowsNearbyDetection: Boolean = false
 
     @Expose
-    @ConfigOption(name = "Line To Next", desc = "Show a line to the closest burrow or guess location.\n" + "§eRequires Burrow particle detection.")
+    @ConfigOption(
+        name = "Line To Next", desc = """Show a line to the closest burrow or guess location.
+§eRequires Burrow particle detection."""
+    )
     @ConfigEditorBoolean
-    public boolean lineToNext = true;
+    var lineToNext: Boolean = true
 
     @Expose
-    @ConfigOption(name = "Nearest Warp", desc = "Warp to the nearest warp point on the hub, if closer to the next burrow.")
+    @ConfigOption(
+        name = "Nearest Warp",
+        desc = "Warp to the nearest warp point on the hub, if closer to the next burrow."
+    )
     @ConfigEditorBoolean
-    public boolean burrowNearestWarp = false;
+    var burrowNearestWarp: Boolean = false
 
     @Expose
     @ConfigOption(name = "Warp Key", desc = "Press this key to warp to nearest burrow waypoint.")
     @ConfigEditorKeybind(defaultKey = Keyboard.KEY_NONE)
-    public int keyBindWarp = Keyboard.KEY_NONE;
+    var keyBindWarp: Int = Keyboard.KEY_NONE
 
     @Expose
     @ConfigOption(name = "Ignored Warps", desc = "")
     @Accordion
-    public IgnoredWarpsConfig ignoredWarps = new IgnoredWarpsConfig();
+    var ignoredWarps: IgnoredWarpsConfig = IgnoredWarpsConfig()
 
     @Expose
     @ConfigOption(name = "Inquisitor Waypoint Sharing", desc = "")
     @Accordion
-    public InquisitorSharingConfig inquisitorSharing = new InquisitorSharingConfig();
+    var inquisitorSharing: InquisitorSharingConfig = InquisitorSharingConfig()
 
     @Expose
-    @ConfigOption(name = "Griffin Pet Warning", desc = "Warn when holding an Ancestral Spade if a Griffin Pet is not equipped.")
+    @ConfigOption(
+        name = "Griffin Pet Warning",
+        desc = "Warn when holding an Ancestral Spade if a Griffin Pet is not equipped."
+    )
     @ConfigEditorBoolean
     @FeatureToggle
-    public boolean petWarning = true;
+    var petWarning: Boolean = true
 
     @Expose
     @ConfigOption(name = "Diana Profit Tracker", desc = "")
     @Accordion
-    public DianaProfitTrackerConfig dianaProfitTracker = new DianaProfitTrackerConfig();
+    var dianaProfitTracker: DianaProfitTrackerConfig = DianaProfitTrackerConfig()
 
     @Expose
     @ConfigOption(name = "Mythological Creature Tracker", desc = "")
-    @Accordion
-    // TODO rename mythologicalMobTracker
-    public MythologicalMobTrackerConfig mythologicalMobtracker = new MythologicalMobTrackerConfig();
+    @Accordion // TODO rename mythologicalMobTracker
+    var mythologicalMobtracker: MythologicalMobTrackerConfig = MythologicalMobTrackerConfig()
 
     @Expose
     @ConfigOption(name = "All Burrows List", desc = "")
     @Accordion
-    public AllBurrowsListConfig allBurrowsList = new AllBurrowsListConfig();
+    var allBurrowsList: AllBurrowsListConfig = AllBurrowsListConfig()
 }
