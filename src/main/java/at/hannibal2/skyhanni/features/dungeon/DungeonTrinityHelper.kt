b0c329ec0dff0fffe454c629ab0455a109f7b7b6
Bugzilla <171168915+Bugzilla123@users.noreply.github.com>
2025-02-19 17:54:18 +0100

    Fix: get data from tab correctly for trinity

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonTrinityHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonTrinityHelper.kt
@@ -3,7 +3,7 @@ package at.hannibal2.skyhanni.features.dungeon
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.model.TabWidget
-import at.hannibal2.skyhanni.events.dungeon.DungeonEnterEvent
+import at.hannibal2.skyhanni.events.dungeon.DungeonStartEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.LorenzUtils
@@ -14,19 +14,20 @@ import kotlin.time.Duration.Companion.seconds
 @SkyHanniModule
 object DungeonTrinityHelper {
     private val config get() = SkyHanniMod.feature.dungeon.trinityHelper
-    private val dragonSound by lazy { createSound("mob.enderdragon.growl", 1f) }
 
     @HandleEvent
-    fun onDungeonEnter(event: DungeonEnterEvent) {
+    fun onDungeonStart(event: DungeonStartEvent) {
         if (!config.enabled) return
 
-        val puzzleCount = TabWidget.DUNGEON_PUZZLE.matchMatcherFirstLine { group("amount") }?.toIntOrNull() ?: 0
+        TabWidget.DUNGEON_PUZZLE.matchMatcherFirstLine {
+            val puzzleCount = group("amount")?.toIntOrNull() ?: 0
 
-        // https://hypixel.net/threads/best-way-to-get-trinitys-number-instead-of-acquiring-actual-friends.5489159/
-        if (puzzleCount == 5) {
-            dragonSound.playSound()
-            LorenzUtils.sendTitle("§dTrinity ?!?!", 5.seconds)
-            if (config.sendPartyChat) HypixelCommands.partyChat("5 Puzzle dungeon, watch out for Trinity room")
+            // https://hypixel.net/threads/best-way-to-get-trinitys-number-instead-of-acquiring-actual-friends.5489159/
+            if (puzzleCount == 5) {
+                LorenzUtils.sendTitle("§dTrinity ?!?!", 5.seconds)
+                if (config.sendPartyChat) HypixelCommands.partyChat("5 Puzzle dungeon, watch out for Trinity room")
+            }
         }
     }
+
 }
