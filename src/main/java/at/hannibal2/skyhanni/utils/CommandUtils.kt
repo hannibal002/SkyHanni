58fe796f1a90f7c27a4f0ed129339faba64d28d1
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2025-02-28 20:05:07 +0100

    improvement for usage paradigm

--- src/main/java/at/hannibal2/skyhanni/utils/CommandUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/CommandUtils.kt
@@ -3,6 +3,7 @@ package at.hannibal2.skyhanni.utils
 import at.hannibal2.skyhanni.config.commands.ComplexCommand
 import at.hannibal2.skyhanni.utils.NeuInternalName.Companion.toInternalName
 import at.hannibal2.skyhanni.utils.NeuItems.getItemStackOrNull
+import at.hannibal2.skyhanni.utils.NeuItems.isGenerator
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import java.util.TreeMap
 
@@ -52,6 +53,7 @@ object CommandUtils {
         context: CommandContextAwareObject,
         validItems: (NeuInternalName) -> Boolean = { true },
         aliases: Map<String, NeuInternalName> = NeuItems.commonItemAliases.global,
+        notFoundResponse: (String) -> String = { "Could not find a valid item for: '$it'" },
     ): Pair<Int, Any?> {
         // This replacement does not work for iterable interface. Therefore, the suppression.
         @Suppress("ReplaceSizeZeroCheckWithIsEmpty") if (args.count() == 0) {
@@ -96,7 +98,7 @@ object CommandUtils {
         }
 
         if ((item as? NeuInternalName)?.getItemStackOrNull() == null && (item as? ItemGroup) == null) {
-            context.errorMessage = "Could not find a valid item for: '$collected'"
+            context.errorMessage = notFoundResponse(collected)
         }
 
         return grabbed.size to item
@@ -104,10 +106,11 @@ object CommandUtils {
 
     fun itemTabComplete(
         start: String,
-        validItems: (NeuInternalName) -> Boolean = { true },
+        validItems: (NeuInternalName) -> Boolean = { !it.isGenerator() },
         aliases: Map<String, NeuInternalName> = NeuItems.commonItemAliases.global,
+        suggestAtEmpty: Boolean = false,
     ): List<String> = buildList {
-        if (start.isEmpty()) return@buildList
+        if (!suggestAtEmpty && start.isEmpty()) return@buildList
         val expected = when {
             namePattern.matches(start) -> NameSource.ITEM_NAME
             internalPattern.matches(start) -> NameSource.INTERNAL_NAME
@@ -229,22 +232,9 @@ data class CommandArgument<T : CommandContextAwareObject>(
     val prefix: String = "",
     val defaultPosition: Int = -1,
     val validity: (T) -> Boolean = { true },
-    val tabComplete: (String) -> Collection<String> = { emptyList() },
+    val tabComplete: (String, T) -> Collection<String> = { _, _ -> emptyList() },
     val handler: (Iterable<String>, T) -> Int,
 ) {
-
-    constructor(
-        documentation: String,
-        prefix: String = "",
-        defaultPosition: Int = -1,
-        validity: (T) -> Boolean = { true },
-        tabComplete: (String) -> Collection<String> = { emptyList() },
-        /** Used to declare that this argument shouldn't be used for creating a certain help call. Used for polymorphic commands*/
-        noDocumentationFor: List<MutableSet<CommandArgument<T>>> = emptyList(),
-        handler: (Iterable<String>, T) -> Int,
-    ) : this(documentation, prefix, defaultPosition, validity, tabComplete, handler) {
-        noDocumentationFor.forEach { it.add(this) }
-    }
 
     override fun toString(): String = documentation
 
