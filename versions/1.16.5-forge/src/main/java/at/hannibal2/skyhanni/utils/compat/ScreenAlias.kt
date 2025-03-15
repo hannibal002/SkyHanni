3b085ab59f31834d172be4c4d3a133b9db00a4b0
Cal <cwolfson58@gmail.com>
2025-03-15 21:31:52 +1100

    Backend: Start making inventories work

---
+++ versions/1.16.5-forge/src/main/java/at/hannibal2/skyhanni/utils/compat/ScreenAlias.kt
@@ -0,0 +1,8 @@
+package at.hannibal2.skyhanni.utils.compat
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.world.inventory.AbstractContainerMenu
+
+typealias SkyHanniGuiContainer = AbstractContainerScreen<AbstractContainerMenu>
+
+// typealias SkyHanniContainer = AbstractContainerMenu
