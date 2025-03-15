cf272b0e966fb5da3becf9aa087398cf24657812
Seyon Sivatharan <sivatharanseyon@gmail.com>
2025-03-15 00:26:41 -0400

    Fix thunder in a bottle and variants

--- src/main/java/at/hannibal2/skyhanni/features/misc/ThunderBottleNotification.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/ThunderBottleNotification.kt
@@ -2,11 +2,18 @@ package at.hannibal2.skyhanni.features.misc
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
-import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
+import at.hannibal2.skyhanni.events.SecondPassedEvent
+import at.hannibal2.skyhanni.features.fishing.IsFishingDetection.isFishing
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.ConfigUtils.jumpToEditor
+import at.hannibal2.skyhanni.utils.InventoryUtils
+import at.hannibal2.skyhanni.utils.ItemUtils.itemNameWithoutColor
 import at.hannibal2.skyhanni.utils.LorenzUtils
-import at.hannibal2.skyhanni.utils.RegexUtils.matches
-import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import at.hannibal2.skyhanni.utils.NeuInternalName.Companion.toInternalName
+import at.hannibal2.skyhanni.utils.SimpleTimeMark
+import at.hannibal2.skyhanni.utils.StringUtils
+import at.hannibal2.skyhanni.utils.StringUtils.createCommaSeparatedList
 import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
@@ -14,17 +21,32 @@ object ThunderBottleNotification {
 
     private val config get() = SkyHanniMod.feature.misc
 
-    private val thunderBottleChargedPattern by RepoPattern.pattern(
-        "thunderbottle.charged",
-        "§e> Your bottle of thunder has fully charged!",
-    )
+    private val THUNDER_IN_A_BOTTLE = "THUNDER_IN_A_BOTTLE".toInternalName()
+    private val STORM_IN_A_BOTTLE = "STORM_IN_A_BOTTLE".toInternalName()
+    private val HURRICANE_IN_A_BOTTLE = "HURRICANE_IN_A_BOTTLE".toInternalName()
+
+    private val bottles = listOf(THUNDER_IN_A_BOTTLE, STORM_IN_A_BOTTLE, HURRICANE_IN_A_BOTTLE)
+
+    private var lastChecked = SimpleTimeMark.farPast()
 
     @HandleEvent
-    fun onChat(event: SkyHanniChatEvent) {
+    fun onSecondPassed(event: SecondPassedEvent) {
         if (!isEnabled()) return
-
-        if (thunderBottleChargedPattern.matches(event.message)) {
-            LorenzUtils.sendTitle("§eThunder Bottle Charged!", 3.seconds)
+        if (lastChecked.passedSince() < 10.seconds) return
+        if (isFishing) {
+            lastChecked = SimpleTimeMark.now()
+            val bottlesInInventory = bottles.filter { InventoryUtils.isItemInInventory(it) }
+                .map { it.itemNameWithoutColor }
+            if (bottlesInInventory.isNotEmpty()) {
+                val length = bottlesInInventory.size
+                ChatUtils.clickableChat(
+                    "You are currently fishing, but " +
+                        "${bottlesInInventory.createCommaSeparatedList()} ${StringUtils.pluralize(length, "is", "are")} full. " +
+                        "Click here to disable this notification.",
+                    { config::thunderBottleNotification.jumpToEditor() },
+                    replaceSameMessage = true,
+                )
+            }
         }
     }
 
