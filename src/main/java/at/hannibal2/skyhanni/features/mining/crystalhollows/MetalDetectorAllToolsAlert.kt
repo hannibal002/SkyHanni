08d39789203d67b7b02f9d1d837fff43d9303672
Seyon Sivatharan <sivatharanseyon@gmail.com>
2025-03-16 23:53:43 -0400

    Fix detekt

--- src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/MetalDetectorAllToolsAlert.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/MetalDetectorAllToolsAlert.kt
@@ -8,7 +8,6 @@ import at.hannibal2.skyhanni.events.ActionBarUpdateEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
-import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NeuInternalName.Companion.toInternalName
 import kotlin.time.Duration.Companion.seconds
 
