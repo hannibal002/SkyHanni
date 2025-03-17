68159ef547ff0f55edcecccd9b137477124f1665
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-03-17 21:16:50 +0100

    strictest detekt

--- src/main/java/at/hannibal2/skyhanni/features/garden/inventory/plots/GardenPlotIcon.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/inventory/plots/GardenPlotIcon.kt
@@ -101,27 +101,29 @@ object GardenPlotIcon {
                 else
                     editMode--
             }
-        } else if (editMode != 0) {
-            if (event.slotId in 54..89) {
-                event.cancel()
-                copyStack = event.slot?.stack?.copy()?.also {
-                    it.stackSize = 1
-                } ?: return
-                // TODO different format, not bold or show not in chat at all.
-                ChatUtils.chat("§6§lClick an item in the desk menu to replace it with that item!")
-                return
-            }
-            val plotList = plotList ?: return
-            if (!whitelistedSlot.contains(event.slotId)) return
-            event.cancel()
-            if (editMode == 2) {
-                plotList.remove(event.slotId)
-                return
-            }
-            val copyStack = copyStack ?: return
-            plotList[event.slotId] = copyStack.getInternalName()
-            cachedStack[event.slotId] = copyStack
         }
+        if (editMode == 0) return
+
+        if (event.slotId in 54..89) {
+            event.cancel()
+            copyStack = event.slot?.stack?.copy()?.also {
+                it.stackSize = 1
+            } ?: return
+            // TODO different format, not bold or show not in chat at all.
+            ChatUtils.chat("§6§lClick an item in the desk menu to replace it with that item!")
+            return
+        }
+
+        val plotList = plotList ?: return
+        if (!whitelistedSlot.contains(event.slotId)) return
+        event.cancel()
+        if (editMode == 2) {
+            plotList.remove(event.slotId)
+            return
+        }
+        val copyStack = copyStack ?: return
+        plotList[event.slotId] = copyStack.getInternalName()
+        cachedStack[event.slotId] = copyStack
     }
 
     @HandleEvent
