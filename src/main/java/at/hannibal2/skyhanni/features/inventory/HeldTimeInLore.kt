bcdb30d5a21daebcd3f1d4237d73c96a8ed6b1ff
Obsidian <108832807+Obsidianninja11@users.noreply.github.com>
2025-03-18 05:32:10 -0800

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/inventory/HeldTimeInLore.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/HeldTimeInLore.kt
@@ -19,7 +19,7 @@ object HeldTimeInLore {
 
         val secondsHeld = event.itemStack.getSecondsHeld() ?: return
         val timeHeldFormatted = secondsHeld.seconds.format(maxUnits = 3)
-        val timeLeftFormatted = (300*60*60 - secondsHeld).seconds.format(maxUnits = 3)
+        val timeLeftFormatted = (300 * 60 * 60 - secondsHeld).seconds.format(maxUnits = 3)
         // All the current ones take 300 hours. If any in the future need a different amount, this will need to be changed.
 
         if (config.timeHeldInLore) event.toolTip.addOrInsert(10, "§7Time Held: §b$timeHeldFormatted")
