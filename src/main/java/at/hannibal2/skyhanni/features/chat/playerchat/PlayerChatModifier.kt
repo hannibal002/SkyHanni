4b06c71d2bb547710cebaf5e805f21ea3d7ee878
nopo <nopotheemail@gmail.com>
2025-03-23 21:53:31 +1100

    fix this not workign?

--- src/main/java/at/hannibal2/skyhanni/features/chat/playerchat/PlayerChatModifier.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/playerchat/PlayerChatModifier.kt
@@ -43,15 +43,11 @@ object PlayerChatModifier {
         }
         val hoverEvent = chatComponent.chatStyle.chatHoverEvent ?: return
         hoverEvent.action ?: return
-        if (
-            hoverEvents.any {
-                //#if MC < 1.21
-            it.value == hoverEvent.value
-                //#else
-                //$$ it.getValue(HoverEvent.Action.SHOW_TEXT) == hoverEvent.getValue(HoverEvent.Action.SHOW_TEXT)
-                //#endif
-        }
-            ) return
+        //#if MC < 1.21
+        if (hoverEvents.any { it.value == hoverEvent.value }) return
+        //#else
+        //$$ if (hoverEvents.any { it.getValue(HoverEvent.Action.SHOW_TEXT) == hoverEvent.getValue(HoverEvent.Action.SHOW_TEXT) }) return
+        //#endif
         hoverEvents.add(hoverEvent)
     }
 
