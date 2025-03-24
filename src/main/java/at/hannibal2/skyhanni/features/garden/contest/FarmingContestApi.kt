33c9b07c70aa342800a42104e2b4e5c6ac40e133
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-24 13:22:33 +0100

    style: imports
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/garden/contest/FarmingContestApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/contest/FarmingContestApi.kt
@@ -11,9 +11,7 @@ import at.hannibal2.skyhanni.features.garden.CropType
 import at.hannibal2.skyhanni.features.garden.GardenApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.test.command.ErrorManager
-import at.hannibal2.skyhanni.utils.EnumUtils.isAnyOf
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
-import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NumberUtil.formatInt
 import at.hannibal2.skyhanni.utils.RegexUtils.firstMatcher
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
