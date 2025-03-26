794bd9e26547c143a1622972bf3f75e211b27130
DavidArthurCole <davidarthurcole23@gmail.com>
2025-03-26 19:31:35 -0400

    ???

--- src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/LorenzUtils.kt
@@ -90,7 +90,4 @@ object LorenzUtils {
 
     fun inAnyIsland(vararg islandTypes: IslandType) = inSkyBlock && HypixelData.skyBlockIsland in islandTypes
     fun inAnyIsland(islandTypes: Collection<IslandType>) = inSkyBlock && HypixelData.skyBlockIsland in islandTypes
-    fun formatPercentage(coerceAtMost: Double): Any {
-
-    }
 }
