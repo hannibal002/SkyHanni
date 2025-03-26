f4d77e2d6f18611a5c4ec9a1f5013456f35cde3a
DavidArthurCole <davidarthurcole23@gmail.com>
2025-03-25 20:25:34 -0400

    Fixes

--- src/main/java/at/hannibal2/skyhanni/events/experiments/TableTaskStartedEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/experiments/TableTaskStartedEvent.kt
@@ -1,8 +1,8 @@
 package at.hannibal2.skyhanni.events.experiments
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-import at.hannibal2.skyhanni.features.inventory.experimentationtable.ExperimentationTaskType
-import at.hannibal2.skyhanni.features.inventory.experimentationtable.ExperimentationTier
+import at.hannibal2.skyhanni.features.inventory.experimentationtable.ExperimentationTableApi.ExperimentationTaskType
+import at.hannibal2.skyhanni.features.inventory.experimentationtable.ExperimentationTableApi.ExperimentationTier
 
 class TableTaskStartedEvent(
     val type: ExperimentationTaskType,
