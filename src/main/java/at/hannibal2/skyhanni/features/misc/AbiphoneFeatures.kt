c7c1aa061952a2526ab512dbf724ea359b797aff
DavidArthurCole <davidarthurcole23@gmail.com>
2025-03-26 17:27:57 -0400

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/misc/AbiphoneFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/AbiphoneFeatures.kt
@@ -14,7 +14,6 @@ import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.StringUtils.isValidUuid
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
-import net.minecraft.client.Minecraft
 import org.lwjgl.input.Keyboard
 import kotlin.time.Duration.Companion.seconds
 
