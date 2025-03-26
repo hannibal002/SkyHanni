c628450d3577295583eb675a3941f2a9214757bc
DavidArthurCole <davidarthurcole23@gmail.com>
2025-03-26 17:32:35 -0400

    More work on storage set

--- src/main/java/at/hannibal2/skyhanni/config/storage/ResettableStorageSet.kt
+++ src/main/java/at/hannibal2/skyhanni/config/storage/ResettableStorageSet.kt
@@ -1,15 +1,15 @@
 package at.hannibal2.skyhanni.config.storage
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 
 open class ResettableStorageSet {
     @Suppress("UNCHECKED_CAST")
-    private val mutableMemberProperties: List<KMutableProperty1<Any?, Any?>> =
-        this::class.memberProperties
-            .filterIsInstance<KMutableProperty1<Any, Any?>>()
-            .map { it as KMutableProperty1<Any?, Any?> }
+    private val mutableMemberProperties: List<KMutableProperty1<Any, Any?>> =
+        this::class.memberProperties.filterIsInstance<KMutableProperty1<Any, Any?>>()
 
     open fun reset() = applyFromOther(this::class.createInstance())
 
@@ -17,7 +17,7 @@ open class ResettableStorageSet {
         if (this::class != other::class) return
         mutableMemberProperties.forEach { prop ->
             try {
-                prop.set(this, prop.get(other))
+                prop.forceSet(prop.get(other))
             } catch (e: Exception) {
                 e.printStackTrace()
                 ErrorManager.skyHanniError(
@@ -25,5 +25,19 @@ open class ResettableStorageSet {
                 )
             }
         }
+    }
+
+    private fun KMutableProperty1<Any, Any?>.forceSet(value: Any?) {
+        val wasAccessible = this.isAccessible
+        this.isAccessible = true
+        this.set(this@ResettableStorageSet, value)
+        this.isAccessible = wasAccessible
+    }
+
+    override fun toString(): String = mutableMemberProperties.joinToString("\n") { prop ->
+        val wasAccessible = prop.isAccessible
+        prop.isAccessible = true
+        "${prop.name} = ${(prop as KProperty1<Any, Any?>).get(this)}"
+            .also { prop.isAccessible = wasAccessible }
     }
 }
