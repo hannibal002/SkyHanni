eefef4a7d89b6d514e23f228622bb1f9c1d1c888
DavidArthurCole <davidarthurcole23@gmail.com>
2025-03-26 23:23:04 -0400

    Merge fix

--- src/main/java/at/hannibal2/skyhanni/features/fishing/FishingTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/FishingTimer.kt
@@ -12,7 +12,6 @@ import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.events.fishing.SeaCreatureFishEvent
 import at.hannibal2.skyhanni.events.minecraft.KeyDownEvent
-import at.hannibal2.skyhanni.events.minecraft.KeyPressEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.LocationUtils.distanceTo
