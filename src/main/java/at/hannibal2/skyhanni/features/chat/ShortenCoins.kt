0c1d03dc98790a233288f4453da40443fde1de5d
ksndq <ksandwojtek@gmail.com>
2025-03-29 22:41:11 +0100

    whoops v2

--- src/main/java/at/hannibal2/skyhanni/features/chat/ShortenCoins.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/ShortenCoins.kt
@@ -9,7 +9,6 @@ import at.hannibal2.skyhanni.utils.NumberUtil.formatDouble
 import at.hannibal2.skyhanni.utils.NumberUtil.shortFormat
 import at.hannibal2.skyhanni.utils.RegexUtils.replace
 import at.hannibal2.skyhanni.utils.chat.TextHelper.asComponent
-import at.hannibal2.skyhanni.utils.chat.TextHelper.style
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 @SkyHanniModule
