8aeea0bbb0f7870f93cb9b9936ef154db8a81a07
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2025-03-29 20:24:22 +0100

    Fuck you Intellij

--- src/main/java/at/hannibal2/skyhanni/config/features/combat/end/EndIslandConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/combat/end/EndIslandConfig.kt
@@ -1,7 +1,6 @@
 package at.hannibal2.skyhanni.config.features.combat.end
 
 import at.hannibal2.skyhanni.config.FeatureToggle
-import at.hannibal2.skyhanni.config.features.misc.DraconicSacrificeTrackerConfig
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
