4fcbd978613adc0304930942aedcdead43715076
DavidArthurCole <davidarthurcole23@gmail.com>
2025-04-05 12:07:25 -0400

    Fix

--- src/main/java/at/hannibal2/skyhanni/config/storage/ProfileSpecificStorage.kt
+++ src/main/java/at/hannibal2/skyhanni/config/storage/ProfileSpecificStorage.kt
@@ -352,21 +352,27 @@ class ProfileSpecificStorage {
 
         @Expose
         var typeCountsSince: RabbitData? = RabbitData(),
-    )
-
-    data class RabbitData(
-        @Expose var uniques: Int = 0,
-        @Expose var dupes: Int = 0,
-        @Expose var strays: Int = 0,
     ) {
-        fun getByIndex(index: Int): Int = when (index) {
-            0 -> uniques
-            1 -> dupes
-            2 -> strays
-            else -> throw IllegalArgumentException("Invalid index: $index")
+        companion object {
+            data class RabbitData(
+                @Expose var uniques: Int = 0,
+                @Expose var dupes: Int = 0,
+                @Expose var strays: Int = 0,
+            ) {
+                fun getByIndex(index: Int): Int = when (index) {
+                    0 -> uniques
+                    1 -> dupes
+                    2 -> strays
+                    else -> throw IllegalArgumentException("Invalid index: $index")
+                }
+
+                companion object {
+                    val EMPTY = RabbitData(0, 0, 0)
+                }
+            }
+            data class LeaderboardPosition(@Expose var position: Int, @Expose var percentile: Double)
         }
     }
-    data class LeaderboardPosition(@Expose var position: Int, @Expose var percentile: Double)
 
     // - fame
     @Expose
