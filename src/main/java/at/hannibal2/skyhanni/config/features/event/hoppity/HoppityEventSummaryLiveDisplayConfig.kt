07b8e688f8ac695436eca1369c8f93f8c2216f0c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-04-06 08:56:11 +0200

    fix formatting again (ty intellij again)

--- src/main/java/at/hannibal2/skyhanni/config/features/event/hoppity/HoppityEventSummaryLiveDisplayConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/event/hoppity/HoppityEventSummaryLiveDisplayConfig.kt
@@ -37,7 +37,7 @@ class HoppityEventSummaryLiveDisplayConfig {
     @ConfigOption(
         name = "Date/Time Display",
         desc = "Display the date and time of the event in the header, for the current event, past events, or the next event.\n" +
-                "§cNote§7: The Next Event option will only appear if Next Event is added here.",
+            "§cNote§7: The Next Event option will only appear if Next Event is added here.",
     )
     @ConfigEditorDraggableList
     var dateTimeDisplay: MutableList<HoppityDateTimeDisplayType> = mutableListOf(
@@ -74,7 +74,7 @@ class HoppityEventSummaryLiveDisplayConfig {
     @ConfigOption(
         name = "Specific Inventories",
         desc = "§cOnly§r show the card while in certain inventories.\n" +
-                "§eIf the list is empty, the card will show in all inventories.",
+            "§eIf the list is empty, the card will show in all inventories.",
     )
     @ConfigEditorDraggableList
     var specificInventories: MutableList<HoppityLiveDisplayInventoryType> = mutableListOf(
