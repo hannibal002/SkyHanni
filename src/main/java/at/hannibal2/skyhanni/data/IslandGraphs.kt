1de1dda83c993476303d01f5ab253ab94edaf834
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-04-06 09:34:40 +0200

    this looks uglier but if you insist detekt, ur the boss (im too lazy to find and disable or tweak the rule)

--- src/main/java/at/hannibal2/skyhanni/data/IslandGraphs.kt
+++ src/main/java/at/hannibal2/skyhanni/data/IslandGraphs.kt
@@ -186,8 +186,8 @@ object IslandGraphs {
             return
         }
 
-        @Suppress("DEPRECATION") // can not use IslandAreas for area detection here. It HAS TO be the scoreboard
-        val now = isGlaciteTunnelsArea(LorenzUtils.skyBlockArea)
+        // can not use IslandAreas for area detection here. It HAS TO be the scoreboard
+        @Suppress("DEPRECATION") val now = isGlaciteTunnelsArea(LorenzUtils.skyBlockArea)
         if (inGlaciteTunnels != now) {
             inGlaciteTunnels = now
             loadDwarvenMines()
@@ -199,8 +199,8 @@ object IslandGraphs {
     }
 
     private fun loadDwarvenMines() {
-        @Suppress("DEPRECATION") // can not use IslandAreas for area detection here. It HAS TO be the scoreboard
-        if (isGlaciteTunnelsArea(LorenzUtils.skyBlockArea)) {
+        // can not use IslandAreas for area detection here. It HAS TO be the scoreboard
+        @Suppress("DEPRECATION") if (isGlaciteTunnelsArea(LorenzUtils.skyBlockArea)) {
             reloadFromJson("GLACITE_TUNNELS")
         } else {
             reloadFromJson("DWARVEN_MINES")
