3a63eecff688d53a25707946536b2e00d7a3a30a
Thunderblade73 <85900443+Thunderblade73@users.noreply.github.com>
2025-04-10 01:58:56 +0200

    Update src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
    Co-authored-by: David Cole <40234707+DavidArthurCole@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
+++ src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
@@ -121,7 +121,8 @@ object MobFilter {
         "§.§lCOLLECT!",
     )
 
-    /** REGEX-TEST: SHINY PIG
+    /**
+     * REGEX-TEST: SHINY PIG
      * */
     val shinyPig by patternGroup.pattern(
         "pattern.shiny",
