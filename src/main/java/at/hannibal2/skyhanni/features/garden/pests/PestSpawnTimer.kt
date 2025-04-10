53f18977562a6600b31e1a89c152dacf48d675aa
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-04-10 18:57:33 -0500

    detekt my hated

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawnTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawnTimer.kt
@@ -17,8 +17,8 @@ import at.hannibal2.skyhanni.features.garden.GardenApi
 import at.hannibal2.skyhanni.features.garden.GardenApi.hasFarmingToolInHand
 import at.hannibal2.skyhanni.features.garden.GardenApi.lastCropBrokenTime
 import at.hannibal2.skyhanni.features.garden.GardenApi.pestCooldownEndTime
-import at.hannibal2.skyhanni.features.garden.pests.PestApi.lastPestSpawnTime
 import at.hannibal2.skyhanni.features.garden.pests.PestApi.hasVacuumInHand
+import at.hannibal2.skyhanni.features.garden.pests.PestApi.lastPestSpawnTime
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.NumberUtil.formatInt
