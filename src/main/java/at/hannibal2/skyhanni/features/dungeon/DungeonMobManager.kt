5555c50996c9ebbf3a816baa63b0c741ad564b4c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-04-10 19:18:48 +0200

    fixed fel

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonMobManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonMobManager.kt
@@ -2,19 +2,23 @@ package at.hannibal2.skyhanni.features.dungeon
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.EntityMovementData
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.mob.Mob
 import at.hannibal2.skyhanni.data.mob.MobData
 import at.hannibal2.skyhanni.events.ConfigLoadEvent
 import at.hannibal2.skyhanni.events.MobEvent
+import at.hannibal2.skyhanni.events.entity.EntityMoveEvent
 import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ConditionalUtils.onToggle
+import at.hannibal2.skyhanni.utils.MobUtils.mob
 import at.hannibal2.skyhanni.utils.RenderUtils.draw3DLine
 import at.hannibal2.skyhanni.utils.RenderUtils.drawWaypointFilled
 import at.hannibal2.skyhanni.utils.RenderUtils.exactPlayerEyeLocation
 import at.hannibal2.skyhanni.utils.SpecialColor.toSpecialColor
 import at.hannibal2.skyhanni.utils.getLorenzVec
+import net.minecraft.entity.EntityLivingBase
 import java.awt.Color
 
 @SkyHanniModule
@@ -72,6 +76,11 @@ object DungeonMobManager {
     }
 
     @HandleEvent(onlyOnIsland = IslandType.CATACOMBS)
+    fun onPlayerMove(event: EntityMoveEvent<EntityLivingBase>) {
+        felOnTheGround.remove(event.entity.mob)
+    }
+
+    @HandleEvent(onlyOnIsland = IslandType.CATACOMBS)
     fun onTick() {
         handleInvisibleStar()
     }
@@ -91,7 +100,7 @@ object DungeonMobManager {
             }
         }
 
-        felOnTheGround.removeIf { mob ->
+        felOnTheGround.forEach { mob ->
             event.drawWaypointFilled(
                 mob.baseEntity.getLorenzVec().add(-0.5, -0.23, -0.5),
                 fel.colour.get().toSpecialColor(),
@@ -101,7 +110,6 @@ object DungeonMobManager {
                 minimumAlpha = 0.8f,
                 inverseAlphaScale = true,
             )
-            !mob.isInvisible()
         }
     }
 
@@ -135,8 +143,8 @@ object DungeonMobManager {
     private fun handleFel(mob: Mob) {
         if (!fel.highlight.get()) return
         if (mob.name != "Fels") return
-        if (!mob.isInvisible()) return
         felOnTheGround.add(mob)
+        EntityMovementData.addToTrack(mob)
     }
 
     private fun handleFelDespawn(mob: Mob) {
