564fface3bfa55a3ba0f61035d488fe03814dc1e
Cal <cwolfson58@gmail.com>
2025-04-12 15:00:49 +1000

    Backend: Create global-ish DrawContext

--- src/main/java/at/hannibal2/skyhanni/utils/compat/DrawContext.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/compat/DrawContext.kt
@@ -1,7 +1,19 @@
 package at.hannibal2.skyhanni.utils.compat
 
+import net.minecraft.client.gui.FontRenderer
+import net.minecraft.client.gui.GuiScreen
+
 class DrawContext {
     private val _matrices = MatrixStack()
     val matrices: MatrixStack
         get() = _matrices
+
+    fun drawString(fr: FontRenderer, text: String, x: Int, y: Int, color: Int, shadow: Boolean) {
+        fr.drawString(text, x.toFloat(), y.toFloat(), color, shadow)
+    }
+
+    fun fill(left: Int, top: Int, right: Int, bottom: Int, color: Int) {
+        GuiScreen.drawRect(left, top, right, bottom, color)
+    }
+
 }
