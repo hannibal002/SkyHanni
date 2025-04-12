524e438ad8e58dcf4623a28597d099ebaa9684ef
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-04-12 02:01:27 +0200

    hide error when faction type is not yet loaded

---
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/sacks/ProfitOutsideSacks.kt
@@ -0,0 +1,8 @@
+package at.hannibal2.skyhanni.features.inventory.sacks
+
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+
+// shows the price of items in sacks while not in the sacks
+@SkyHanniModule
+object ProfitOutsideSacks {
+}
