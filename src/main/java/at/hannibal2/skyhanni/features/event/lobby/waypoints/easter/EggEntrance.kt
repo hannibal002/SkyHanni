2538b85afdcc02148433d79c469ade2146aaa7bc
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-04-12 21:01:21 +0200

    lobby

--- src/main/java/at/hannibal2/skyhanni/features/event/lobby/waypoints/easter/EggEntrance.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/lobby/waypoints/easter/EggEntrance.kt
@@ -11,17 +11,17 @@ enum class EggEntrance(
     EASTER_EGG_ENTER_8(
         "#8 (go down here)",
         LorenzVec(94, 78, 44),
-        EasterEgg.EASTER_EGG_8
+        EasterEgg.EASTER_EGG_8,
     ),
     EASTER_EGG_ENTER_21(
         "#21 (enter cave)",
         LorenzVec(-55, 86, -40),
-        EasterEgg.EASTER_EGG_21
+        EasterEgg.EASTER_EGG_21,
     ),
     EASTER_EGG_ENTER_22(
         "#22 (enter here)",
         LorenzVec(-97, 111, 22),
-        EasterEgg.EASTER_EGG_22
+        EasterEgg.EASTER_EGG_22,
     )
 }
 
