33550f1769e22c55bd34265305b4f625b4a0e561
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-04-12 21:30:00 +0200

    JoinCrystalHollows.kt

--- src/main/java/at/hannibal2/skyhanni/features/misc/JoinCrystalHollows.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/JoinCrystalHollows.kt
@@ -32,16 +32,17 @@ object JoinCrystalHollows {
                     "Click here to warp to Dwarven Mines!",
                     onClick = {
                         HypixelCommands.warp("mines")
-                    }, "§eClick to run /warp mines!"
+                    },
+                    "§eClick to run /warp mines!",
                 )
             } else {
                 ChatUtils.chat("Buy a §2Crystal Hollows Pass §efrom §5Gwendolyn")
             }
         }
         if (message == "§e[NPC] §5Gwendolyn§f: §rGreat! Now hop on into the Minecart and I'll get you on your way!" && inTime()) {
-            ChatUtils.clickableChat("Click here to warp to Crystal Hollows!", onClick = {
-                HypixelCommands.warp("ch")
-            }, "§eClick to run /warp ch!")
+            ChatUtils.clickableChat(
+                "Click here to warp to Crystal Hollows!", onClick = { HypixelCommands.warp("ch") }, "§eClick to run /warp ch!",
+            )
         }
     }
 
