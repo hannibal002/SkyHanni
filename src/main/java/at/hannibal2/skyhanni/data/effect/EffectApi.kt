a1a9bbba7eb3b64339e46706dfa355b1450777e0
DavidArthurCole <davidarthurcole23@gmail.com>
2025-04-13 03:10:15 -0400

    Fixes

--- src/main/java/at/hannibal2/skyhanni/data/effect/EffectApi.kt
+++ src/main/java/at/hannibal2/skyhanni/data/effect/EffectApi.kt
@@ -3,6 +3,7 @@ package at.hannibal2.skyhanni.data.effect
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.ProfileStorageData
+import at.hannibal2.skyhanni.data.effect.EffectApi.isGodPotEffectsFilterSelect
 import at.hannibal2.skyhanni.data.model.TabWidget
 import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
 import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
