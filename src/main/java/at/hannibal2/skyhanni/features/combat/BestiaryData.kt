64a889a6fdcdff066ee13ae9dafc03d1bd4f17c7
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-04-13 17:00:28 +0200

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/combat/BestiaryData.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/BestiaryData.kt
@@ -3,7 +3,6 @@ package at.hannibal2.skyhanni.features.combat
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
-import at.hannibal2.skyhanni.config.features.combat.BestiaryConfig
 import at.hannibal2.skyhanni.config.features.combat.BestiaryConfig.DisplayTypeEntry
 import at.hannibal2.skyhanni.config.features.combat.BestiaryConfig.NumberFormatEntry
 import at.hannibal2.skyhanni.events.GuiContainerEvent
