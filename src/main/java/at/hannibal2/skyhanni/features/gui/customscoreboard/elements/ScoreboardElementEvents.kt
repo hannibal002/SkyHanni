c54f99b4332e5a4970225c75ac680fec3adacbeb
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-04-14 15:23:11 +0200

    cleanup

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementEvents.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/elements/ScoreboardElementEvents.kt
@@ -1,7 +1,6 @@
 package at.hannibal2.skyhanni.features.gui.customscoreboard.elements
 
-import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.currentIslandEvents
-import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard.displayConfig
+import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboard
 import at.hannibal2.skyhanni.features.gui.customscoreboard.CustomScoreboardUtils.getElementsFromAny
 import at.hannibal2.skyhanni.features.gui.customscoreboard.ScoreboardConfigEventElement
 import at.hannibal2.skyhanni.features.gui.customscoreboard.ScoreboardLine
@@ -10,9 +9,13 @@ import at.hannibal2.skyhanni.utils.collection.CollectionUtils.takeIfNotEmpty
 // everything
 // whenever an event gets updated
 object ScoreboardElementEvents : ScoreboardElement() {
-    override fun getDisplay() =
-        if (displayConfig.events.showAllActiveEvents) currentIslandEvents.mapNotNull { it.getLines().takeIfNotEmpty() }.flatten()
-        else currentIslandEvents.firstNotNullOfOrNull { it.getLines().takeIfNotEmpty() }
+    override fun getDisplay(): List<ScoreboardLine>? = with(CustomScoreboard.currentIslandEvents) {
+        if (CustomScoreboard.displayConfig.events.showAllActiveEvents) {
+            mapNotNull { it.getLines().takeIfNotEmpty() }.flatten()
+        } else {
+            firstNotNullOfOrNull { it.getLines().takeIfNotEmpty() }
+        }
+    }
 
     override val configLine = "§7Wide Range of Events\n§7(too much to show all)"
 
