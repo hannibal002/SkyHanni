a2db1edc48fb919fcd40585c8963c5ee10503dbf
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-15 08:38:17 +0200

    Space

--- src/main/java/at/hannibal2/skyhanni/data/SlayerApi.kt
+++ src/main/java/at/hannibal2/skyhanni/data/SlayerApi.kt
@@ -81,7 +81,7 @@ object SlayerApi {
             if (!isInCorrectArea) {
                 add("currentAreaType: $currentAreaType")
                 add(" hypixel area: ${HypixelData.skyBlockArea}")
-                add("graph area: ${IslandAreas.currentAreaName}")
+                add(" graph area: ${IslandAreas.currentAreaName}")
             }
             add("isInAnyArea: $isInAnyArea")
             add("latestSlayerProgress: ${latestSlayerProgress.removeColor()}")
