db708035614e70ebaeed2b750f55cee1db00edec
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-04-16 21:51:39 +0200

    naughty @hannibal002

--- src/main/java/at/hannibal2/skyhanni/utils/KeyboardManager.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/KeyboardManager.kt
@@ -1,6 +1,5 @@
 package at.hannibal2.skyhanni.utils
 
-//#if MC < 1.21
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.model.TextInput
 import at.hannibal2.skyhanni.events.minecraft.KeyDownEvent
@@ -9,8 +8,6 @@ import at.hannibal2.skyhanni.events.minecraft.KeyUpEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.compat.MouseCompat
-import io.github.notenoughupdates.moulconfig.gui.GuiScreenElementWrapper
-import io.github.notenoughupdates.moulconfig.internal.KeybindHelper
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiChat
 import net.minecraft.client.settings.KeyBinding
@@ -18,7 +15,9 @@ import org.apache.commons.lang3.SystemUtils
 import org.lwjgl.input.Keyboard
 import org.lwjgl.input.Mouse
 import kotlin.time.Duration.Companion.milliseconds
-
+//#if MC < 1.21
+import io.github.notenoughupdates.moulconfig.gui.GuiScreenElementWrapper
+import io.github.notenoughupdates.moulconfig.internal.KeybindHelper
 //#else
 //$$ import io.github.moulberry.notenoughupdates.core.config.KeybindHelper
 //$$ import net.minecraft.client.util.InputUtil
