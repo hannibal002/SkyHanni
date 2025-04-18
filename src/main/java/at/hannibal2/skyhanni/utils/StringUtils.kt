b5a01c3f2751f2ddc6708d22d8abb091f1be80b3
DavidArthurCole <davidarthurcole23@gmail.com>
2025-04-18 11:41:05 -0400

    Update

--- src/main/java/at/hannibal2/skyhanni/utils/StringUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/StringUtils.kt
@@ -201,8 +201,7 @@ object StringUtils {
         return "$allButLast$delimiterColor, and ${this[lastIndex]}"
     }
 
-    fun String.pluralize(number: Int, plural: String? = null, withNumber: Boolean = false) =
-        pluralize(number, this, plural, withNumber)
+    fun String.pluralize(number: Int) = pluralize(number, this)
 
     fun pluralize(number: Int, singular: String, plural: String? = null, withNumber: Boolean = false): String {
         val pluralForm = plural ?: "${singular}s"
