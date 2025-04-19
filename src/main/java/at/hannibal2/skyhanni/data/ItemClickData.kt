1120710ddf0726256ced11e8446d7f7aead622af
ksndq <ksandwojtek@gmail.com>
2025-04-19 20:41:50 +0200

    detekt

--- src/main/java/at/hannibal2/skyhanni/data/ItemClickData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/ItemClickData.kt
@@ -6,7 +6,6 @@ import at.hannibal2.skyhanni.events.ItemClickEvent
 import at.hannibal2.skyhanni.events.entity.EntityClickEvent
 import at.hannibal2.skyhanni.events.minecraft.packet.PacketSentEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
 import at.hannibal2.skyhanni.utils.compat.getUsedItem
