604898d8b737e2f9877d693731464b57c8ef4e72
DavidArthurCole <davidarthurcole23@gmail.com>
2025-04-26 03:59:20 -0400

    Detekt

--- src/main/java/at/hannibal2/skyhanni/features/event/hoppity/summary/HoppityLiveDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/hoppity/summary/HoppityLiveDisplay.kt
@@ -30,7 +30,6 @@ import at.hannibal2.skyhanni.features.inventory.chocolatefactory.CFApi
 import at.hannibal2.skyhanni.features.inventory.chocolatefactory.CFApi.partyModeReplace
 import at.hannibal2.skyhanni.features.inventory.chocolatefactory.CFShopPrice.menuNamePattern
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.ConditionalUtils.afterChange
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.LorenzColor
