2199cd61aebe3782801decdb1cd52efd3a9790cf
minhperry <minhpt2002@gmail.com>
2025-05-03 20:43:13 +0200

    add an explanation for why not bits/xp

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/FannCostConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/FannCostConfig.java
@@ -3,6 +3,7 @@ package at.hannibal2.skyhanni.config.features.inventory;
 import at.hannibal2.skyhanni.config.FeatureToggle;
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorInfoText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class FannCostConfig {
@@ -18,4 +19,8 @@ public class FannCostConfig {
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean xpPerBit = false;
+
+    @ConfigEditorInfoText()
+    @ConfigOption(name = "Why not Bits/XP?", desc = "One, it's fixed 500 Bits a day, and two, the result would have lots of zeros after decimal.")
+    public String whyNot = "";
 }
