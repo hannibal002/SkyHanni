7bacccdac67a43ea09c742f5f2323311d5b227d0
Cal <cwolfson58@gmail.com>
2025-05-07 21:11:15 +1000

    detekt im faster than you

--- src/main/java/at/hannibal2/skyhanni/features/misc/items/EstimatedItemValue.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/items/EstimatedItemValue.kt
@@ -34,7 +34,6 @@ import at.hannibal2.skyhanni.utils.system.PlatformUtils
 import io.github.moulberry.notenoughupdates.NotEnoughUpdates
 import io.github.moulberry.notenoughupdates.profileviewer.GuiProfileViewer
 import net.minecraft.client.Minecraft
-import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 import org.lwjgl.input.Keyboard
