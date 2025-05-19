42571297aace592db38101b5dab1498454ea2b75
Cédric <75255906+rueblimaster@users.noreply.github.com>
2025-05-19 10:35:49 +0200

    fix bad merge

--- src/main/java/at/hannibal2/skyhanni/events/minecraft/KeyHeldEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/minecraft/KeyHeldEvent.kt
@@ -1,6 +1,4 @@
 package at.hannibal2.skyhanni.events.minecraft
 
-import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-
 /** Gets posted repeatedly while a key is held down, also on initial key press, use this for holding keys*/
-class KeyHeldEvent(val keyCode: Int) : SkyHanniEvent()
+class KeyHeldEvent(override val keyCode: Int) : KeyEvent(keyCode)
--- src/main/java/at/hannibal2/skyhanni/events/minecraft/KeyPressEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/minecraft/KeyPressEvent.kt
@@ -1,6 +1,4 @@
 package at.hannibal2.skyhanni.events.minecraft
 
-import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-
 /** Gets posted when a key is first pressed, use this for taps*/
-class KeyPressEvent(val keyCode: Int) : SkyHanniEvent()
+class KeyPressEvent(override val keyCode: Int) : KeyEvent(keyCode)
