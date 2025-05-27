f961f2ae20751c853fdd916500b49652646bc024
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2025-05-27 12:18:32 +0100

    Allow Self Run of AllInvite, Warp, Ping and TPS

--- src/main/java/at/hannibal2/skyhanni/features/commands/PartyChatCommands.kt
+++ src/main/java/at/hannibal2/skyhanni/features/commands/PartyChatCommands.kt
@@ -12,6 +12,7 @@ import at.hannibal2.skyhanni.features.misc.TpsCounter
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.ConfigUtils.jumpToEditor
+import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
@@ -27,6 +28,7 @@ object PartyChatCommands {
         val names: List<String>,
         val isEnabled: () -> Boolean,
         val requiresPartyLead: Boolean,
+        val worksOnSelf: Boolean,
         val executable: (PartyChatEvent) -> Unit,
     )
 
@@ -38,6 +40,7 @@ object PartyChatCommands {
             listOf("pt", "ptme", "transfer"),
             { config.transferCommand },
             requiresPartyLead = true,
+            worksOnSelf = false,
             executable = {
                 HypixelCommands.partyTransfer(it.cleanedAuthor)
             },
@@ -46,24 +49,31 @@ object PartyChatCommands {
             listOf("pw", "warp", "warpus"),
             { config.warpCommand && lastWarp.passedSince() > 5.seconds },
             requiresPartyLead = true,
+            worksOnSelf = true,
             executable = {
                 lastWarp = SimpleTimeMark.now()
-                HypixelCommands.partyWarp()
+                DelayedRun.runNextTick {
+                    HypixelCommands.partyWarp()
+                }
             },
         ),
         PartyChatCommand(
             listOf("allinv", "allinvite"),
             { config.allInviteCommand && lastAllInvite.passedSince() > 2.seconds },
             requiresPartyLead = true,
+            worksOnSelf = true,
             executable = {
                 lastAllInvite = SimpleTimeMark.now()
-                HypixelCommands.partyAllInvite()
+                DelayedRun.runNextTick {
+                    HypixelCommands.partyAllInvite()
+                }
             },
         ),
         PartyChatCommand(
             listOf("ping"),
             { config.pingCommand },
             requiresPartyLead = false,
+            worksOnSelf = true,
             executable = {
                 if (!CurrentPing.isEnabled()) {
                     ChatUtils.clickableChat(
@@ -72,19 +82,25 @@ object PartyChatCommands {
                         onClick = {
                             CurrentPing.config::hypixelPingApi.jumpToEditor()
                         },
-                        hover = "§eClick to find setting in the config!"
+                        hover = "§eClick to find setting in the config!",
                     )
                     return@PartyChatCommand
                 }
-                HypixelCommands.partyChat("Current Ping: ${CurrentPing.averagePing.inWholeMilliseconds.addSeparators()}ms")
+                DelayedRun.runNextTick {
+                    HypixelCommands.partyChat("Current Ping: ${CurrentPing.averagePing.inWholeMilliseconds.addSeparators()}ms")
+                }
+
             },
         ),
         PartyChatCommand(
             listOf("tps"),
             { config.tpsCommand },
             requiresPartyLead = false,
+            worksOnSelf = true,
             executable = {
-                HypixelCommands.partyChat("Current TPS: ${TpsCounter.tps}")
+                DelayedRun.runNextTick {
+                    HypixelCommands.partyChat("Current TPS: ${TpsCounter.tps}")
+                }
             },
         ),
     )
@@ -119,8 +135,7 @@ object PartyChatCommands {
         val commandLabel = event.message.substring(1).substringBefore(' ')
         val command = indexedPartyChatCommands[commandLabel.lowercase()] ?: return
         val name = event.cleanedAuthor
-
-        if (name == LorenzUtils.getPlayerName()) return
+        if (name == LorenzUtils.getPlayerName() && !command.worksOnSelf) return
         if (!command.isEnabled()) return
         if (command.requiresPartyLead && PartyApi.partyLeader != LorenzUtils.getPlayerName()) return
         if (isBlockedUser(name)) {
