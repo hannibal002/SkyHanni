dc2979c05b998c539276baf8feebf468b3d670d2
ILike2WatchMemes <ilike2watchmemes@gmail.com>
2025-05-28 01:47:39 +0200

    config

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/InventoryConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/InventoryConfig.java
@@ -2,6 +2,7 @@ package at.hannibal2.skyhanni.config.features.inventory;
 
 import at.hannibal2.skyhanni.config.FeatureToggle;
 import at.hannibal2.skyhanni.config.HasLegacyId;
+import at.hannibal2.skyhanni.config.core.config.Position;
 import at.hannibal2.skyhanni.config.features.inventory.chocolatefactory.CFConfig;
 import at.hannibal2.skyhanni.config.features.inventory.customwardrobe.CustomWardrobeConfig;
 import at.hannibal2.skyhanni.config.features.inventory.experimentationtable.ExperimentationTableConfig;
@@ -15,6 +16,7 @@ import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.Category;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableList;
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorInfoText;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 import io.github.notenoughupdates.moulconfig.annotations.SearchTag;
 
@@ -167,10 +169,15 @@ public class InventoryConfig {
     @Accordion
     public TradeConfig trade = new TradeConfig();
 
+    @ConfigOption(
+        name = "Searchable Storage",
+        desc = "For this feature to work properly you need to at least have opened each storage page once."
+    )
+    @ConfigEditorInfoText
+    public String notice = "";
+
     @Expose
-    @ConfigOption(name = "Searchable Storage", desc = "test")
-    @Accordion
-    public SearchableStorageConfig searchableStorage = new SearchableStorageConfig();
+    public Position displayPosition = new Position(-300, 140);
 
     @Expose
     @ConfigOption(name = "Item Number", desc = "Showing the item number as a stack size for these items.")
--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/SearchableStorageConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/SearchableStorageConfig.kt
@@ -1,21 +1,19 @@
 package at.hannibal2.skyhanni.config.features.inventory
 
-import at.hannibal2.skyhanni.config.FeatureToggle
 import at.hannibal2.skyhanni.config.core.config.Position
 import com.google.gson.annotations.Expose
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
-import io.github.notenoughupdates.moulconfig.annotations.ConfigLink
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorInfoText
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
 class SearchableStorageConfig {
 
-    @Expose
-    @ConfigOption(name = "Enabled", desc = "test")
-    @ConfigEditorBoolean
-    @FeatureToggle
-    var enabled: Boolean = false
+    @ConfigOption(
+        name = "Searchable Storage",
+        desc = "For this feature to work properly you need to at least have opened each storage page once.",
+    )
+    @ConfigEditorInfoText
+    var notice: String = ""
 
     @Expose
-    @ConfigLink(owner = SearchableStorageConfig::class, field = "enabled")
     var displayPosition: Position = Position(-300, 140)
 }
