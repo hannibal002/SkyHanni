6c22307446a48a4e6390a9513756976d98ca0670
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-05 23:51:39 -0400

    Fix for 1.21

--- src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/event/SystemMessageEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/event/SystemMessageEvent.kt
@@ -1,9 +1,12 @@
 package at.hannibal2.skyhanni.data.hypixel.chat.event
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
+//#if TODO
 import at.hannibal2.skyhanni.data.ChatManager
+//#endif
 import net.minecraft.util.IChatComponent
 
+// todo needs 1.21 impl
 // A SkyHanniChatEvent after filtering all player send events, leaving messages from the game/system.
 open class SystemMessageEvent(
     open val message: String,
@@ -11,7 +14,9 @@ open class SystemMessageEvent(
     open var blockedReason: String? = null,
 ) : SkyHanniEvent() {
     fun replaceComponent(newComponent: IChatComponent, reason: String) {
+        //#if TODO
         ChatManager.addReplacementContext(chatComponent, reason)
+        //#endif
         chatComponent = newComponent
     }
 }
