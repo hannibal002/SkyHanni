6323a2d7b00aa1469d9fe4dce607db9d6b2487ae
Cal <cwolfson58@gmail.com>
2025-06-07 01:36:31 +1000

    works on 1.21

--- src/main/java/at/hannibal2/skyhanni/features/mining/HotxFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/HotxFeatures.kt
@@ -9,7 +9,6 @@ import at.hannibal2.skyhanni.events.RenderItemTipEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.RenderUtils.highlight
-import at.hannibal2.skyhanni.utils.SkyBlockUtils
 
 @SkyHanniModule
 object HotxFeatures {
