c8b5a6274412a23d8cbe6ffb54a7c8a69f411d4b
martimavocado <39881008+martimavocado@users.noreply.github.com>
2025-06-10 00:26:45 +0100

    if something breaks it's empa's fault
    Co-authored-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/HighHeatSound.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/crystalhollows/HighHeatSound.kt
@@ -14,7 +14,7 @@ object HighHeatSound {
     @SubscribeEvent
     fun onSound(event: PlaySoundEvent) {
         if (!isEnabled()) return
-        if (!(LocationUtils.playerLocation().y <= 65.0 && MiningAPI.heat >= 90)) return
+        if (LocationUtils.playerLocation().y > 65.0 || MiningAPI.heat < 90) return
 
         if (event.soundName == "mob.wolf.panting" && event.pitch == 0.0f && event.volume == 1.0f) event.cancel()
     }
