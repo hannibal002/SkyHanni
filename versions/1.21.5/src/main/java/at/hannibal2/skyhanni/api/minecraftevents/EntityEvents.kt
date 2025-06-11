82dfc2edc0ef81c0c233124e3d021e3182af86c8
Cal <cwolfson58@gmail.com>
2025-06-11 15:16:39 +1000

    EntityHurtEvent

--- versions/1.21.5/src/main/java/at/hannibal2/skyhanni/api/minecraftevents/EntityEvents.kt
+++ versions/1.21.5/src/main/java/at/hannibal2/skyhanni/api/minecraftevents/EntityEvents.kt
@@ -1,28 +1,19 @@
 package at.hannibal2.skyhanni.api.minecraftevents
 
+import at.hannibal2.skyhanni.data.mob.MobData
+import at.hannibal2.skyhanni.data.mob.MobDetection
+import at.hannibal2.skyhanni.events.entity.EntityHurtEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import net.minecraft.entity.Entity
+import net.minecraft.entity.damage.DamageSource
 
 @SkyHanniModule
 object EntityEvents {
 
-    init {
-
-        // EntityEnterWorldEvent
-
-        // EntityHurtEvent
-
-        // EndermanTeleportEvent
-
-        // EntityDeathEvent
-
-        // SkyHanniRenderEntityEvent.Pre
-
-        // SkyHanniRenderEntityEvent.Post
-
-        // SkyHanniRenderEntityEvent.Specials.Pre
-
-        // SkyHanniRenderEntityEvent.Specials.Post
-
+    @JvmStatic
+    fun postEntityHurt(entity: Entity, source: DamageSource, amount: Float) {
+        EntityHurtEvent(entity, source, amount).post()
+        val skyblockMob = MobData.entityToMob[entity] ?: return
+        MobDetection.postMobHurtEvent(skyblockMob, source, amount)
     }
-
 }
--- versions/1.21.5/src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinLivingEntity.java
+++ versions/1.21.5/src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinLivingEntity.java
@@ -1,8 +1,10 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
+import at.hannibal2.skyhanni.api.minecraftevents.EntityEvents;
 import at.hannibal2.skyhanni.events.entity.EntityDeathEvent;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,4 +18,8 @@ public class MixinLivingEntity {
         new EntityDeathEvent((LivingEntity) (Object) this).post();
     }
 
+    @Inject(method = "applyDamage", at = @At("HEAD"))
+    public void applyDamage(ServerWorld world, DamageSource source, float amount, CallbackInfo ci) {
+        EntityEvents.postEntityHurt((LivingEntity) (Object) this, source, amount);
+    }
 }
