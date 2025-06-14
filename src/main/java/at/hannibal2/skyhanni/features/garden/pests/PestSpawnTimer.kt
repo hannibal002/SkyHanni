3e6f5a5822c06a78fd75dcb11b9019e1d50ff5ff
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-06-13 21:03:53 -0500

    fix display showing when disabled

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawnTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawnTimer.kt
@@ -211,6 +211,7 @@ object PestSpawnTimer {
     }
 
     private fun shouldRender(): Boolean = when {
+        !config.enabled -> false
         config.onlyWithFarmingTool && config.onlyWithVacuum -> hasFarmingToolInHand() || hasVacuumInHand()
         config.onlyWithFarmingTool -> hasFarmingToolInHand()
         config.onlyWithVacuum -> hasVacuumInHand()
