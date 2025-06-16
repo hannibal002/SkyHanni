b8a99ded12a33c67c8f84d0f7575e7b7576edfb5
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-15 20:11:10 -0400

    Fix usage of TimeLimitedCache

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestTrapApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestTrapApi.kt
@@ -12,10 +12,10 @@ import at.hannibal2.skyhanni.utils.RegexUtils.groupOrNull
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
-import at.hannibal2.skyhanni.utils.TimeLimitedCache
 import at.hannibal2.skyhanni.utils.collection.CollectionUtils.addOrPut
 import at.hannibal2.skyhanni.utils.collection.CollectionUtils.enumMapOf
 import at.hannibal2.skyhanni.utils.collection.CollectionUtils.takeIfNotEmpty
+import at.hannibal2.skyhanni.utils.collection.TimeLimitedCache
 import com.google.common.cache.RemovalCause.EXPIRED
 import java.util.regex.Matcher
 import kotlin.time.Duration.Companion.seconds
