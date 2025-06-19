f437876d0658b65d2d58ca5005dfff841d2f99e1
nopo <nopotheemail@gmail.com>
2025-06-19 23:10:15 +1000

    fix build

--- src/main/java/at/hannibal2/skyhanni/config/storage/ProfileSpecificStorage.kt
+++ src/main/java/at/hannibal2/skyhanni/config/storage/ProfileSpecificStorage.kt
@@ -33,10 +33,8 @@ import at.hannibal2.skyhanni.features.fishing.trophy.TrophyRarity
 import at.hannibal2.skyhanni.features.garden.CropAccessory
 import at.hannibal2.skyhanni.features.garden.CropType
 import at.hannibal2.skyhanni.features.garden.GardenPlotApi.PlotData
-//#if TODO
 import at.hannibal2.skyhanni.features.garden.farming.ArmorDropTracker
 import at.hannibal2.skyhanni.features.garden.farming.DicerRngDropTracker
-//#endif
 import at.hannibal2.skyhanni.features.garden.farming.lane.FarmingLane
 import at.hannibal2.skyhanni.features.garden.fortuneguide.FarmingItemType
 import at.hannibal2.skyhanni.features.garden.pests.PestProfitTracker
@@ -464,7 +462,6 @@ class ProfileSpecificStorage {
         @Expose
         var savedCropAccessory: CropAccessory? = CropAccessory.NONE
 
-        //#if TODO
         @Expose
         var dicerDropTracker: DicerRngDropTracker.Data = DicerRngDropTracker.Data()
 
@@ -482,7 +479,6 @@ class ProfileSpecificStorage {
 
         @Expose
         var armorDropTracker: ArmorDropTracker.Data = ArmorDropTracker.Data()
-        //#endif
 
         @Expose
         var composterUpgrades: MutableMap<ComposterUpgrade, Int> = enumMapOf()
