f5289b5a6e4b8101057f7819c6aeeba0ebe5b935
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-06-19 15:41:12 +0200

    adding orNull

--- src/main/java/at/hannibal2/skyhanni/features/slayer/BlockNotSpawnable.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/BlockNotSpawnable.kt
@@ -8,6 +8,7 @@ import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.anyMatches
+import at.hannibal2.skyhanni.utils.compat.InventoryCompat.orNull
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 @SkyHanniModule
@@ -30,7 +31,7 @@ object BlockNotSpawnable {
         val slot = event.slot ?: return
         if (InventoryUtils.openInventoryName() != "Slayer") return
 
-        val stack = slot.stack ?: return
+        val stack = slot.stack?.orNull() ?: return
         if (notSpawnablePattern.anyMatches(stack.getLore())) {
             event.cancel()
         }
