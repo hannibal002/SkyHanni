bcee491f1323cc165ff2c0b0d0e04a4585b04fb9
Empa <itsempa@users.noreply.github.com>
2025-06-19 16:21:29 +0200

    fix: didnt delete ff command oopsie
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/Commands.kt
@@ -24,7 +24,6 @@ import at.hannibal2.skyhanni.features.garden.composter.ComposterOverlay
 import at.hannibal2.skyhanni.features.garden.farming.CropSpeedMeter
 import at.hannibal2.skyhanni.features.garden.farming.lane.FarmingLaneCreator
 import at.hannibal2.skyhanni.features.garden.fortuneguide.CaptureFarmingGear
-import at.hannibal2.skyhanni.features.garden.fortuneguide.FFGuideGui
 import at.hannibal2.skyhanni.features.garden.pests.PestFinder
 import at.hannibal2.skyhanni.features.mining.MineshaftPityDisplay
 import at.hannibal2.skyhanni.features.minion.MinionFeatures
@@ -62,10 +61,6 @@ object Commands {
     }
 
     private fun usersMain(event: CommandRegistrationEvent) {
-        event.register("ff") {
-            description = "Opens the Farming Fortune Guide"
-            callback { FFGuideGui.onCommand() }
-        }
         event.register("shwords") {
             description = "Opens the config list for modifying visual words"
             callback { VisualWordGui.onCommand() }
