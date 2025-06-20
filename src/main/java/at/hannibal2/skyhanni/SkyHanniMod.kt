1b6561cfe3cc58b3cc46b16bd929b2c8c594100b
nopo <nopotheemail@gmail.com>
2025-06-20 12:54:21 +1000

    load repo after config

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -50,7 +50,6 @@ object SkyHanniMod {
         LoadedModules.modules.forEach { SkyHanniModLoader.loadModule(it) }
 
         SkyHanniEvents.init(modules)
-        if (!PlatformUtils.isNeuLoaded()) EnoughUpdatesManager.downloadRepo()
 
         PreInitFinishedEvent.post()
     }
@@ -58,6 +57,7 @@ object SkyHanniMod {
     fun init() {
         configManager = ConfigManager()
         configManager.firstLoad()
+        if (!PlatformUtils.isNeuLoaded()) EnoughUpdatesManager.downloadRepo()
         MinecraftConsoleFilter.initLogging()
         Runtime.getRuntime().addShutdownHook(
             Thread { configManager.saveConfig(ConfigFileType.FEATURES, "shutdown-hook") },
