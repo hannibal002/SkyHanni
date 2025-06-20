7acc7f6bed67cfc523c06db61f1241a22f3566a0
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-20 13:54:26 -0400

    Add back drawCircle that got eaten by merge :(

--- src/main/java/at/hannibal2/skyhanni/utils/RenderUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/RenderUtils.kt
@@ -829,6 +829,51 @@ object RenderUtils {
         DrawContextUtils.popMatrix()
     }
 
+    /**
+     * Method to draw a circle.
+     *
+     * **NOTE:** If you are using [GlStateManager.translate] or [GlStateManager.scale]
+     * with this method, ensure they are invoked in the correct order if you use both. That is, [GlStateManager.translate]
+     * is called **BEFORE** [GlStateManager.scale], otherwise the rectangle will not be rendered correctly
+     *
+     * @param x The x-coordinate of the circle's center.
+     * @param y The y-coordinate of the circle's center.
+     * @param radius The circle's radius.
+     * @param color The fill color.
+     * @param angle1 defines the start of the semicircle (Default value makes it a full circle). Must be in range [0,2*pi] (0 is on the left and increases counterclockwise)
+     * @param angle2 defines the end of the semicircle (Default value makes it a full circle). Must be in range [0,2*pi] (0 is on the left and increases counterclockwise)
+     * @param smoothness smooths out the edge. (In amount of blurred pixels)
+     */
+    fun drawFilledCircle(x: Int, y: Int, radius: Int, color: Color, smoothness: Float = 2.5f, angle1: Float = 7.0f, angle2: Float = 7.0f) {
+        //#if MC < 1.21
+        val scaleFactor = ScaledResolution(Minecraft.getMinecraft()).scaleFactor
+        //#else
+        //$$ val scaleFactor = MinecraftClient.getInstance().window.scaleFactor
+        //#endif
+        //#endif
+
+        DrawContextUtils.popMatrix()
+        val radiusIn = radius * scaleFactor
+        val xIn = x * scaleFactor
+        val yIn = y * scaleFactor
+
+        CircleShader.scaleFactor = scaleFactor.toFloat()
+        CircleShader.radius = radiusIn.toFloat()
+        CircleShader.smoothness = smoothness
+        CircleShader.centerPos = floatArrayOf((xIn + radiusIn).toFloat(), (yIn + radiusIn).toFloat())
+        CircleShader.angle1 = angle1 - Math.PI.toFloat()
+        CircleShader.angle2 = angle2 - Math.PI.toFloat()
+
+        // TODO: Once ChromaColour no longer drops alpha sometimes, remove this 255 hardcode
+        val circleColor = color.addAlpha(255).rgb
+
+        DrawContextUtils.pushPop {
+            ShaderManager.enableShader(ShaderManager.Shaders.CIRCLE)
+            GuiRenderUtils.drawRect(x - 5, y - 5, x + radius * 2 + 5, y + radius * 2 + 5, circleColor)
+            ShaderManager.disableShader()
+        }
+    }
+
     //#if TODO
     fun getAlpha(): Float {
         colorBuffer.clear()
