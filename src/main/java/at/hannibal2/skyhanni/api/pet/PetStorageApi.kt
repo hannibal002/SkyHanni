49d05ce36c073017c9ded42335872674fb0244fa
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-21 12:41:52 -0400

    Detekt

--- src/main/java/at/hannibal2/skyhanni/api/pet/PetStorageApi.kt
+++ src/main/java/at/hannibal2/skyhanni/api/pet/PetStorageApi.kt
@@ -435,7 +435,7 @@ object PetStorageApi {
 
     @HandleEvent
     fun onDebug(event: DebugDataCollectEvent) {
-        fun PetData.formatForDebug() = fauxInternalName.asString() + ":<lvl${level}>:" + uuid.toString()
+        fun PetData.formatForDebug() = fauxInternalName.asString() + ":<lvl$level>:" + uuid.toString()
         event.title("PetStorageApi")
         event.addIrrelevant {
             val petStorage = petStorage ?: return@addIrrelevant
