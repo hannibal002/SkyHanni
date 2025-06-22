4023ccdf2b780c659f2f6b107df22bd7083962a8
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-22 19:32:47 -0400

    Merge

--- src/main/java/at/hannibal2/skyhanni/test/DVDTest.kt
+++ src/main/java/at/hannibal2/skyhanni/test/DVDTest.kt
@@ -2,25 +2,28 @@ package at.hannibal2.skyhanni.test
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.config.core.config.Position
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderable
 import at.hannibal2.skyhanni.utils.renderables.DVDLogoRenderable
-import at.hannibal2.skyhanni.utils.renderables.Renderable
+import at.hannibal2.skyhanni.utils.renderables.RenderableString
 
 @SkyHanniModule
 object DVDTest {
 
-    private val config get() = SkyHanniMod.feature.dev.debug
     private val dvdRenderable by lazy {
         DVDLogoRenderable(
-            RenderableString("§zDVD Logo Test", scale = 3.0),
+            renderable = RenderableString("§zDVD Logo Test", scale = 3.0),
+            movementSpeed = 80f,
         )
     }
 
+    private val position = Position(200, 200)
+
     @HandleEvent
     fun onRenderOverlay(event: GuiRenderEvent.GuiOverlayRenderEvent) {
-        if (!config.dvdLogo) return
-        config.dvdLogoPosition.renderRenderable(dvdRenderable, posLabel = "DVDTest")
+        if (!SkyHanniMod.feature.dev.debug.dvdLogo) return
+        position.renderRenderable(dvdRenderable, posLabel = "DVDTest")
     }
 }
