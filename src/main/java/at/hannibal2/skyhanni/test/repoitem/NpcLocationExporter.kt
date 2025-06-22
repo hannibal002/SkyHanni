83f5abc58ece4d9ca32b378735b7a02c5a4c873c
Cal <cwolfson58@gmail.com>
2025-06-22 22:04:46 +1000

    detektttttttttttt

--- src/main/java/at/hannibal2/skyhanni/test/repoitem/NpcLocationExporter.kt
+++ src/main/java/at/hannibal2/skyhanni/test/repoitem/NpcLocationExporter.kt
@@ -10,7 +10,6 @@ import at.hannibal2.skyhanni.events.minecraft.KeyDownEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getSkullTexture
 import at.hannibal2.skyhanni.utils.KeyboardManager
