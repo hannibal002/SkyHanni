5c9485b593a78ff696bebc20e71cb6bc8bba1c9b
OSzwed <olis.sz1234@gmail.com>
2025-06-25 21:06:56 +0200

    Import order fix

--- src/main/java/at/hannibal2/skyhanni/features/misc/HideArmor.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/HideArmor.kt
@@ -1,11 +1,11 @@
 package at.hannibal2.skyhanni.features.misc
 
 
+import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
 import at.hannibal2.skyhanni.config.features.misc.HideArmorConfig.ModeEntry
 import at.hannibal2.skyhanni.events.SkyHanniRenderEntityEvent
-import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ConfigUtils
 import at.hannibal2.skyhanni.utils.EntityUtils.getArmorInventory
