624d3b318e656aa2f5191f0cd54a33e90c2742ae
ILike2WatchMemes <ilike2watchmemes@gmail.com>
2025-06-26 19:11:13 +0200

    Update CarnivalZombieShootout.kt

--- src/main/java/at/hannibal2/skyhanni/features/event/carnival/CarnivalZombieShootout.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/carnival/CarnivalZombieShootout.kt
@@ -24,11 +24,7 @@ import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawDynamicText
 import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawHitbox
 import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawWaypointFilled
 import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.exactPlayerEyeLocation
-<<<<<<< zombie-shootout
-import at.hannibal2.skyhanni.utils.renderables.RenderableString
-=======
 import at.hannibal2.skyhanni.utils.renderables.StringRenderable
->>>>>>> beta
 import at.hannibal2.skyhanni.utils.renderables.container.HorizontalContainerRenderable
 import at.hannibal2.skyhanni.utils.renderables.item.ItemStackRenderable
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
@@ -92,10 +88,7 @@ object CarnivalZombieShootout {
 
         if (config.zombieTimer) event.renderZombieTimer()
         if (config.coloredHitboxes) event.renderHitBoxes()
-<<<<<<< zombie-shootout
         if (config.coloredLines) event.renderLines()
-=======
->>>>>>> beta
     }
 
     private fun SkyHanniRenderWorldEvent.renderZombieTimer() {
@@ -126,28 +119,12 @@ object CarnivalZombieShootout {
                 }
             }
         }
-<<<<<<< zombie-shootout
 
         zombiesToRemove.forEach { zombieTimes.remove(it) }
     }
 
     private fun SkyHanniRenderWorldEvent.renderHitBoxes() {
         lamp?.let {
-=======
-
-        zombiesToRemove.forEach { zombieTimes.remove(it) }
-    }
-
-    private fun SkyHanniRenderWorldEvent.renderHitBoxes() {
-        lamp?.let {
-            if (config.coloredLines) draw3DLine(
-                exactPlayerEyeLocation(),
-                it.pos.add(0.0, 0.5, 0.0),
-                Color.RED,
-                3,
-                false,
-            )
->>>>>>> beta
             drawWaypointFilled(it.pos, Color.RED, minimumAlpha = 1.0f)
         }
 
@@ -266,11 +243,7 @@ object CarnivalZombieShootout {
         return HorizontalContainerRenderable(
             listOf(
                 ItemStackRenderable(lamp),
-<<<<<<< zombie-shootout
-                RenderableString("§6Disappears in $prefix$timer"),
-=======
                 StringRenderable("§6Disappears in $prefix$timer"),
->>>>>>> beta
             ),
             spacing = 1,
             verticalAlign = RenderUtils.VerticalAlignment.CENTER,
