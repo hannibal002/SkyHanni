5c2cfdc6db87e3c81824da596bab7813f3b4dd74
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-27 18:53:45 +0200

    what the fuck
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/CustomScoreboardConfigFix.kt
+++ src/main/java/at/hannibal2/skyhanni/features/gui/customscoreboard/CustomScoreboardConfigFix.kt
@@ -8,7 +8,6 @@ import at.hannibal2.skyhanni.utils.RenderUtils.VerticalAlignment
 import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonPrimitive
-import scala.xml.parsing.`MarkupParser$class`.element
 
 @SkyHanniModule
 object CustomScoreboardConfigFix {
