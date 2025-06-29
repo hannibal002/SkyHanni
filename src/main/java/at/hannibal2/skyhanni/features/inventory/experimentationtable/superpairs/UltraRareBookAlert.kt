2e19298643d5742c180af80d1f0de5a20169031e
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-29 13:03:05 -0400

    Detekt

--- src/main/java/at/hannibal2/skyhanni/features/inventory/experimentationtable/superpairs/UltraRareBookAlert.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/experimentationtable/superpairs/UltraRareBookAlert.kt
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.features.inventory.experimentationtable.superpairs
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.ExperimentationTableApi
-import at.hannibal2.skyhanni.api.TaskType
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
 import at.hannibal2.skyhanni.data.IslandType
