52177a5e111d49f666b640c3d40df9e0670e5cfe
DavidArthurCole <davidarthurcole23@gmail.com>
2025-06-29 12:50:39 -0400

    Code cleanup

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/experimentationtable/ExperimentsProfitTrackerConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/experimentationtable/ExperimentsProfitTrackerConfig.java
@@ -2,20 +2,16 @@ package at.hannibal2.skyhanni.config.features.inventory.experimentationtable;
 
 import at.hannibal2.skyhanni.config.FeatureToggle;
 import at.hannibal2.skyhanni.config.core.config.Position;
-//#if TODO
 import at.hannibal2.skyhanni.features.inventory.experimentationtable.ExperimentationTableApi.ExperimentationMessages;
-//#endif
 import com.google.gson.annotations.Expose;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableList;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigLink;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// todo 1.21 impl needed
 public class ExperimentsProfitTrackerConfig {
 
     @Expose
@@ -24,12 +20,10 @@ public class ExperimentsProfitTrackerConfig {
     @FeatureToggle
     public boolean enabled = false;
 
-    //#if TODO
     @Expose
     @ConfigOption(name = "Hide Messages", desc = "Change the messages to be hidden after completing Add-on/Main experiments.")
     @ConfigEditorDraggableList
     public List<ExperimentationMessages> hideMessages = new ArrayList<>();
-    //#endif
 
     @Expose
     @ConfigOption(name = "Track Used Bottles", desc = "Track thrown XP bottles while near the experimentation table.")
