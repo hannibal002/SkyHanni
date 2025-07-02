8ad9341c06d0a0a550a4fa3a25ffe4de50ba1d50
Cal <cwolfson58@gmail.com>
2025-07-02 22:30:02 +1000

    modrinth stuff mostly done

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.code.gson:gson:2.13.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.github.SkyHanniStudios:SkyHanniChangelogBuilder:1.1.2")
+    implementation(files("../sharedVariables/build/libs/sharedVariables.jar"))
+    implementation("com.github.mizosoft.methanol:methanol:1.8.3")
 }
---
+++ sharedVariables/src/ModrinthInfo.kt
@@ -0,0 +1,42 @@
+package at.skyhanni.sharedvariables
+
+enum class ModrinthInfo(
+    val minecraftVersions: List<String>,
+    val loader: String,
+    val dependencies: Map<ModrinthDependency, DependencyType>,
+) {
+    SKYHANNI_189(
+        minecraftVersions = listOf("1.8.9"),
+        loader = "forge",
+        dependencies = mapOf(
+            ModrinthDependency.NOT_ENOUGH_UPDATES to DependencyType.OPTIONAL,
+        ),
+    ),
+    SKYHANNI_12105(
+        minecraftVersions = listOf("1.21.5"),
+        loader = "fabric",
+        dependencies = mapOf(
+            ModrinthDependency.FABRIC_API to DependencyType.REQUIRED,
+            ModrinthDependency.FABRIC_LANGUAGE_KOTLIN to DependencyType.REQUIRED,
+        ),
+    ),
+}
+
+enum class ModrinthDependency(
+    val projectId: String,
+) {
+    SKYHANNI("byNkmv5G"),
+    NOT_ENOUGH_UPDATES("GGamhqbw"),
+    FABRIC_API("P7dR8mSH"),
+    FABRIC_LANGUAGE_KOTLIN("Ha28R6CL"),
+}
+
+enum class DependencyType {
+    REQUIRED,
+    OPTIONAL,
+    EMBEDDED,
+    INCOMPATIBLE,
+    ;
+
+    val apiName = name.lowercase()
+}
