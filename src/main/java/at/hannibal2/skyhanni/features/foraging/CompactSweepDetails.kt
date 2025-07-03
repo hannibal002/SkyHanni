8183bd95024b502260be7c6ad9d13feaee0216d2
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-07-03 08:17:05 -0400

    I love accidentally removing import statements.

--- src/main/java/at/hannibal2/skyhanni/features/foraging/CompactSweepDetails.kt
+++ src/main/java/at/hannibal2/skyhanni/features/foraging/CompactSweepDetails.kt
@@ -6,6 +6,7 @@ import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.IslandChangeEvent
 import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.NumberUtil.formatDouble
 import at.hannibal2.skyhanni.utils.RegexUtils.groupOrEmpty
