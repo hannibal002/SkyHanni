21b71d3579eb464fdda8fd0ce83edef665e72ee5
Cal <cwolfson58@gmail.com>
2025-07-06 00:29:50 +1000

    Fix: Color Preview in the config editor

--- src/main/java/at/hannibal2/skyhanni/utils/compat/DrawContext.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/compat/DrawContext.kt
@@ -25,7 +25,7 @@ class DrawContext {
     }
 
     fun enableScissor(left: Int, top: Int, right: Int, bottom: Int) {
-        GlScissorStack.push(left, top, right, bottom, ScaledResolution(Minecraft.getMinecraft()))
+        GlScissorStack.push(left, top, right, bottom, ScaledResolution(Minecraft.getMinecraft()), false)
     }
 
     fun disableScissor() {
