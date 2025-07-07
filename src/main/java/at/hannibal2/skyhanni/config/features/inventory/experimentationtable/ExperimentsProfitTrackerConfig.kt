baa8ddf145e6116fbcf1e1481ae25fabb62991dd
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-07 10:21:18 -0400

    Merge cleanup

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/experimentationtable/ExperimentsProfitTrackerConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/experimentationtable/ExperimentsProfitTrackerConfig.kt
@@ -24,7 +24,7 @@ class ExperimentsProfitTrackerConfig {
     @Expose
     @ConfigOption(name = "Track Time Spent", desc = "Track time spent doing addons and experiments.")
     @ConfigEditorBoolean
-    public boolean trackTimeSpent = false;
+    var trackTimeSpent: Boolean = false
 
     @Expose
     @ConfigOption(name = "Track Used Bottles", desc = "Track thrown XP bottles while near the experimentation table.")
