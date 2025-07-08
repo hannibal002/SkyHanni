32cd24b3fd1c75648222027449eea6d21f242e66
Cal <cwolfson58@gmail.com>
2025-07-08 20:50:15 +1000

    further forcing of java 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,11 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
@@ -23,4 +24,8 @@ dependencies {
     implementation("com.github.SkyHanniStudios:SkyHanniChangelogBuilder:1.1.2")
     implementation(files("../sharedVariables/build/libs/sharedVariables.jar"))
     implementation("com.github.mizosoft.methanol:methanol:1.8.3")
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions.jvmTarget.set(JvmTarget.fromTarget("21"))
 }
