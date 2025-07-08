a845d21f451c4530bd3aa3b2ae8183a2963bc24c
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-08 19:05:42 -0400

    Retention

--- src/main/java/at/hannibal2/skyhanni/config/ConfigManager.kt
+++ src/main/java/at/hannibal2/skyhanni/config/ConfigManager.kt
@@ -107,6 +107,8 @@ class ConfigManager {
 
     // Some position elements don't need config links as they don't have a config option.
     // Use this annotation to mark config elements that don't need a config link.
+    @Retention(AnnotationRetention.RUNTIME)
+    @Target(AnnotationTarget.FIELD)
     annotation class NoConfigLink
 
     private fun findPositionLinks(obj: Any?, slog: MutableSet<IdentityCharacteristics<Any>>) {
