32fd1fc2df119b09bc95b67d748c9bebb2326289
nopo <nopotheemail@gmail.com>
2025-07-10 16:20:40 +1000

    four matt

--- src/main/java/at/hannibal2/skyhanni/config/features/mining/dwarves/RaffleTicketHighlighterConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/dwarves/RaffleTicketHighlighterConfig.kt
@@ -17,21 +17,21 @@ class RaffleTicketHighlighterConfig {
     @ConfigOption(
         name = "§cNotice",
         desc = "If you want to highlight raffle tickets turn on\n" +
-            "/sh Glowing Dropped Items"
+            "§e/sh Glowing Dropped Items"
     )
     @ConfigEditorInfoText
     @OnlyLegacy
     var notice: String = ""
 
     @Expose
-    @ConfigOption(name = "Enabled", desc = "Makes raffle tickets glow during the raffle event")
+    @ConfigOption(name = "Enabled", desc = "Makes raffle tickets glow during the raffle event.")
     @ConfigEditorBoolean
     @FeatureToggle
     @OnlyModern
     var enabled: Boolean = true
 
     @Expose
-    @ConfigOption(name = "Color", desc = "Color that the raffle tickets will be highlighted in")
+    @ConfigOption(name = "Color", desc = "Color that the raffle tickets will be highlighted.")
     @ConfigEditorColour
     @OnlyModern
     var ticketColor: ChromaColour = Color.GREEN.toChromaColor()
