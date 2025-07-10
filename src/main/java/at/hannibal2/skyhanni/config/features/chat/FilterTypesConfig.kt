6266561354a5550929bf17787d2111b06d6630d1
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-07-10 08:27:12 -0400

    Move some formatting codes around.

--- src/main/java/at/hannibal2/skyhanni/config/features/chat/FilterTypesConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/chat/FilterTypesConfig.kt
@@ -159,6 +159,12 @@ class FilterTypesConfig {
     @FeatureToggle
     var teleportPads: Boolean = false
 
+    @Expose
+    @ConfigOption(name = "Reward Bundles", desc = "Hide annoying reminders to claim seasonal Reward Bundles.")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    var rewardBundles: Boolean = false
+
     // TODO remove
     @Expose
     @ConfigOption(name = "Others", desc = "Hide other annoying messages.")
