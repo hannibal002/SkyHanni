cd8b6bc67bcebacdb3507478a063f6a95e43d9b7
Cal <cwolfson58@gmail.com>
2025-07-11 17:59:09 +1000

    cleanup mixin, fabric mod json note

--- versions/1.21.5/src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinDrawContext.java
+++ versions/1.21.5/src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinDrawContext.java
@@ -17,13 +17,11 @@ public class MixinDrawContext {
     //#if MC < 1.21.6
     @Inject(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At("RETURN"))
     private void drawItemPost(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, int z, CallbackInfo ci) {
-        RenderItemHookKt.renderItemReturn((DrawContext) (Object) this, stack, x, y);
-    }
     //#else
     //$$ @Inject(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;III)V", at = @At("RETURN"))
     //$$ private void drawItemPost(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, CallbackInfo ci) {
-    //$$     RenderItemHookKt.renderItemReturn((DrawContext) (Object) this, stack, x, y);
-    //$$ }
+        RenderItemHookKt.renderItemReturn((DrawContext) (Object) this, stack, x, y);
+    }
     //#endif
 
     @Inject(method = "drawStackOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At("RETURN"))
