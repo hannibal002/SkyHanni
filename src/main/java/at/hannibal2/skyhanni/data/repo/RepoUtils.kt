6863ccb5b4383853a0ee669ca708b62fcc852023
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-12 19:36:18 -0400

    Detekt

--- src/main/java/at/hannibal2/skyhanni/data/repo/RepoUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/data/repo/RepoUtils.kt
@@ -3,7 +3,6 @@ package at.hannibal2.skyhanni.data.repo
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
-import java.nio.file.StandardCopyOption
 import java.util.zip.ZipFile
 
 object RepoUtils {
