804729e4586f29cf6f92dffb9fa079d8dc1eb590
ninjaguardian <cjfairchild51@gmail.com>
2025-07-14 23:46:25 -0500

    Fix bait detection accuracy

--- src/main/java/at/hannibal2/skyhanni/features/fishing/FishingBaitWarnings.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/FishingBaitWarnings.kt
@@ -10,13 +10,9 @@ import at.hannibal2.skyhanni.features.nether.kuudra.KuudraApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.DelayedRun
-import at.hannibal2.skyhanni.utils.LocationUtils.distanceToPlayer
-import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.SoundUtils
-import at.hannibal2.skyhanni.utils.TimeLimitedSet
-import at.hannibal2.skyhanni.utils.getLorenzVec
+import at.hannibal2.skyhanni.utils.collection.TimeLimitedSet
 import net.minecraft.entity.item.EntityItem
-import net.minecraft.entity.projectile.EntityFishHook
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 
@@ -27,16 +23,14 @@ object FishingBaitWarnings {
 
     private data class Bait(
         private val entity: EntityItem,
+        val bobberDistance: Float,
         val name: String = entity.entityItem.displayName,
-        val location: LorenzVec = entity.getLorenzVec(),
-    ) {
-        fun distanceTo(bobber: EntityFishHook) = location.distance(bobber.getLorenzVec())
-    }
+    )
 
     private var lastBait: String? = null
     private var wasUsingBait = true
 
-    private val baitEntities = TimeLimitedSet<Bait>(750.seconds)
+    private val baitEntities = TimeLimitedSet<Bait>(4.seconds)
 
     @HandleEvent
     fun onWorldChange() {
@@ -46,27 +40,31 @@ object FishingBaitWarnings {
 
     @HandleEvent(onlyOnSkyblock = true)
     fun onBobber(event: FishingBobberInLiquidEvent) {
-        if (KuudraApi.inKuudra()) return
-        DelayedRun.runDelayed(500.milliseconds) {
+        if (KuudraApi.inKuudra) return
+        DelayedRun.runDelayed(300.milliseconds) {
             checkBait()
         }
     }
 
     @HandleEvent(onlyOnSkyblock = true)
     fun onEntityEnterWorld(event: EntityEnterWorldEvent<EntityItem>) {
-        if (KuudraApi.inKuudra() || !FishingApi.isFishing()) return
-        if (event.entity.distanceToPlayer() > 10) return
+        if (KuudraApi.inKuudra || !FishingApi.isFishing()) return
+        //#if MC < 1.16
+        val bobberDistance = event.entity.getDistanceToEntity(FishingApi.bobber ?: return)
+        //#else
+        //$$ val bobberDistance = event.entity.distanceTo(FishingApi.bobber ?: return)
+        //#endif
+        if (bobberDistance > 2) return
         DelayedRun.runNextTick {
-            val isBait = event.entity.entityItem.isBait()
-            if (isBait) {
-                baitEntities += Bait(event.entity)
+            if (event.entity.entityItem.isBait()) {
+                baitEntities += Bait(event.entity, bobberDistance)
             }
         }
     }
 
     private fun checkBait() {
-        val bobber = FishingApi.bobber ?: return
-        val bait = baitEntities.filter { it.distanceTo(bobber) < 8 }.minByOrNull { it.distanceTo(bobber) }?.name
+        FishingApi.bobber ?: return
+        val bait = baitEntities.filter { it.bobberDistance < 2 }.minByOrNull { it.bobberDistance }?.name // If user has no bait, but another player's bait spawns really close, it will be wrong.
         baitEntities.clear()
 
         if (bait == null) {
