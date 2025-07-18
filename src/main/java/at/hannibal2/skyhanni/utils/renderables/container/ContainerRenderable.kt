e7bc7cb08be1c8bd1f024a6e1b55afebed712bdc
Thunderblade73 <gaidermarkus@gmail.com>
2025-07-18 17:23:16 +0200

    the renderable move and changes

--- src/main/java/at/hannibal2/skyhanni/utils/renderables/container/ContainerRenderable.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/renderables/container/ContainerRenderable.kt
@@ -4,14 +4,8 @@ import at.hannibal2.skyhanni.utils.RenderUtils.HorizontalAlignment
 import at.hannibal2.skyhanni.utils.RenderUtils.VerticalAlignment
 import at.hannibal2.skyhanni.utils.renderables.Renderable
 
-abstract class ContainerRenderable(
-    var renderables: Collection<Renderable>,
-    open val spacing: Int = 0,
-    override val horizontalAlign: HorizontalAlignment,
-    override val verticalAlign: VerticalAlignment
-) : Renderable {
-    abstract override val width: Int
-    abstract override val height: Int
-    abstract override fun render(mouseOffsetX: Int, mouseOffsetY: Int)
+abstract class ContainerRenderable : Renderable {
+    abstract val renderables: Collection<Renderable>
+    open val spacing: Int = 0
 }
 
---
+++ src/main/java/at/hannibal2/skyhanni/utils/renderables/container/table/TabularRenderable.kt
@@ -0,0 +1,31 @@
+package at.hannibal2.skyhanni.utils.renderables.container.table
+
+import at.hannibal2.skyhanni.utils.renderables.Renderable
+
+/**
+ * @property content Collection of rows of Renderables
+ * @property xSpacing Space between rows
+ * @property ySpacing Space between columns
+ */
+interface TabularRenderable<C : Collection<Renderable>, R : Collection<C>> : Renderable {
+    val content: R
+    val xSpacing: Int
+    val ySpacing: Int
+}
+
+interface TabularRenderableWithRowRender<C : Collection<Renderable>, R : Collection<C>> : TabularRenderable<C, R> {
+    fun renderRow(mouseOffsetX: Int, mouseOffsetY: Int, rowIndex: Int, row: C)
+
+    override fun render(mouseOffsetX: Int, mouseOffsetY: Int) {
+        content.forEachIndexed { index, it -> renderRow(mouseOffsetX, mouseOffsetY, index, it) }
+    }
+}
+
+interface TabularRenderableWithCellRender<C : Collection<Renderable>, R : Collection<C>> : TabularRenderableWithRowRender<C, R> {
+    fun renderCell(mouseOffsetX: Int, mouseOffsetY: Int, rowIndex: Int, columnIndex: Int, renderable: Renderable)
+
+    override fun renderRow(mouseOffsetX: Int, mouseOffsetY: Int, rowIndex: Int, row: C) {
+        row.forEachIndexed { index, it -> renderCell(mouseOffsetX, mouseOffsetY, rowIndex, index, it) }
+    }
+}
+
