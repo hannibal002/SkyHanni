04c56160240b89c2b11076f66f1dcc0daf204c2d
Cal <cwolfson58@gmail.com>
2025-07-18 17:25:08 +1000

    cleanup code

--- src/main/java/at/hannibal2/skyhanni/utils/compat/DamageSourceCompat.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/compat/DamageSourceCompat.kt
@@ -1,41 +1,36 @@
 package at.hannibal2.skyhanni.utils.compat
 
-//#if MC < 1.21
 import net.minecraft.util.DamageSource
-//#else
-//$$ import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
-//#endif
 
 object DamageSourceCompat {
     //#if MC < 1.21
-    val cactus get() = DamageSource.cactus
-    val drown get() = DamageSource.drown
-    val fall get() = DamageSource.fall
-    val generic get() = DamageSource.generic
-    val inFire get() = DamageSource.inFire
-    val inWall get() = DamageSource.inWall
-    val lava get() = DamageSource.lava
-    val lightningBolt get() = DamageSource.lightningBolt
-    val magic get() = DamageSource.magic
-    val onFire get() = DamageSource.onFire
-    val outOfWorld get() = DamageSource.outOfWorld
-    val starve get() = DamageSource.starve
-    val wither get() = DamageSource.wither
+    val cactus get(): DamageSource = DamageSource.cactus
+    val drown get(): DamageSource = DamageSource.drown
+    val fall get(): DamageSource = DamageSource.fall
+    val generic get(): DamageSource = DamageSource.generic
+    val inFire get(): DamageSource = DamageSource.inFire
+    val inWall get(): DamageSource = DamageSource.inWall
+    val lava get(): DamageSource = DamageSource.lava
+    val lightningBolt get(): DamageSource = DamageSource.lightningBolt
+    val magic get(): DamageSource = DamageSource.magic
+    val onFire get(): DamageSource = DamageSource.onFire
+    val outOfWorld get(): DamageSource = DamageSource.outOfWorld
+    val starve get(): DamageSource = DamageSource.starve
+    val wither get(): DamageSource = DamageSource.wither
     //#else
-    //$$ private val damageSources = MinecraftCompat.localPlayer.world.getDamageSources()
-    //$$ val cactus get() = damageSources.cactus()
-    //$$ val drown get() = damageSources.drown()
-    //$$ val fall get() = damageSources.fall()
-    //$$ val generic get() = damageSources.generic()
-    //$$ val inFire get() = damageSources.inFire()
-    //$$ val inWall get() = damageSources.inWall()
-    //$$ val lava get() = damageSources.lava()
-    //$$ val lightningBolt get() = damageSources.lightningBolt()
-    //$$ val magic get() = damageSources.magic()
-    //$$ val onFire get() = damageSources.onFire()
-    //$$ val outOfWorld get() = damageSources.outOfWorld()
-    //$$ val starve get() = damageSources.starve()
-    //$$ val wither get() = damageSources.wither()
+    //$$ private val damageSources = MinecraftCompat.localPlayer.world.damageSources
+    //$$ val cactus get(): DamageSource = damageSources.cactus()
+    //$$ val drown get(): DamageSource = damageSources.drown()
+    //$$ val fall get(): DamageSource = damageSources.fall()
+    //$$ val generic get(): DamageSource = damageSources.generic()
+    //$$ val inFire get(): DamageSource = damageSources.inFire()
+    //$$ val inWall get(): DamageSource = damageSources.inWall()
+    //$$ val lava get(): DamageSource = damageSources.lava()
+    //$$ val lightningBolt get(): DamageSource = damageSources.lightningBolt()
+    //$$ val magic get(): DamageSource = damageSources.magic()
+    //$$ val onFire get(): DamageSource = damageSources.onFire()
+    //$$ val outOfWorld get(): DamageSource = damageSources.outOfWorld()
+    //$$ val starve get(): DamageSource = damageSources.starve()
+    //$$ val wither get(): DamageSource = damageSources.wither()
     //#endif
-
 }
