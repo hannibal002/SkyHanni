9d89c0a7da5cf96dbaeb299383a998b4bef4a033
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-20 15:39:06 -0400

    Revert

--- src/main/java/at/hannibal2/skyhanni/config/features/pets/PetConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/pets/PetConfig.kt
@@ -8,6 +8,8 @@ import io.github.notenoughupdates.moulconfig.ChromaColour
 import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorColour
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableList
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 import io.github.notenoughupdates.moulconfig.annotations.SearchTag
 
@@ -48,6 +50,35 @@ class PetConfig {
         @ConfigOption(name = "Highlight Color", desc = "What color the slot should be highlighted.")
         @ConfigEditorColour
         var color: ChromaColour = LorenzColor.GREEN.toChromaColor(alpha = 128)
+    }
+
+    @Expose
+    @ConfigOption(
+        name = "Show Pet Item",
+        desc = "Specify the pet items for which icons should be displayed next to pets."
+    )
+    @ConfigEditorDraggableList
+    val petItemDisplay: MutableList<PetItemsDisplay> = mutableListOf(
+        PetItemsDisplay.XP_SHARE,
+        PetItemsDisplay.TIER_BOOST
+    )
+
+    @Expose
+    @ConfigOption(name = "Pet Item Scale", desc = "The scale at which the Pet Item will be displayed.")
+    @ConfigEditorSlider(minValue = 0.7f, maxValue = 1.5f, minStep = 0.05f)
+    var petItemDisplayScale: Float = 0.9f
+
+    enum class PetItemsDisplay(
+        val icon: String,
+        itemDisplayName: String,
+        val item: String
+    ) {
+        XP_SHARE("§5⚘", "Exp Share", "PET_ITEM_EXP_SHARE"),
+        TIER_BOOST("§c●", "Tier Boost", "PET_ITEM_TIER_BOOST"),
+        ;
+
+        private val displayName: String = "$icon §ffor $itemDisplayName"
+        override fun toString() = "$icon §ffor $displayName"
     }
 
     @Expose
