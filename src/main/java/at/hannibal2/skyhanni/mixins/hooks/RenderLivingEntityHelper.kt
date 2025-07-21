f2221ccd37934d3d6db6b7ec600e9507c9f1c0a7
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-07-20 23:13:52 -0400

    Update RenderLivingEntityHelper.kt

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/RenderLivingEntityHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/RenderLivingEntityHelper.kt
@@ -59,7 +59,7 @@ object RenderLivingEntityHelper {
     }
 
     fun <T : EntityLivingBase> setEntityColor(entity: T, color: Color, condition: () -> Boolean) {
-        if (color == Color.BLACK) return
+        if (color.rgb == 0) return
         entityColorMap[entity] = color
         entityColorCondition[entity] = condition
     }
