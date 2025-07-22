5fd67686b38717d57860827c0f7150d577ed1aee
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2025-07-22 19:37:56 +0200

    init

--- src/main/java/at/hannibal2/skyhanni/config/features/foraging/ForagingConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/foraging/ForagingConfig.kt
@@ -64,4 +64,18 @@ class ForagingConfig {
     @FeatureToggle
     @OnlyModern
     var mutePhantoms = true
+
+    @Expose
+    @ConfigOption(name = "Mute Tree Breaking", desc = "Mutes the sound of the tree fully breaking.")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    @OnlyModern
+    var muteTreeBreaking = true
+
+    @Expose
+    @ConfigOption(name = "Also on Galatea", desc = "Also mutes tree breaking sounds on Galatea.")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    @OnlyModern
+    var muteTreeBreakingOnGalatea = true
 }
