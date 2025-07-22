3b814f88bc1f81a558b923d40eb05f00f55eba4c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-07-22 10:07:21 +0200

    cleanup pr

--- src/main/java/at/hannibal2/skyhanni/config/features/dungeon/DungeonConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/dungeon/DungeonConfig.kt
@@ -191,4 +191,14 @@ class DungeonConfig {
     @Accordion
     val spiritLeapOverlay: SpiritLeapConfig = SpiritLeapConfig()
 
+    @Expose
+    @ConfigOption(
+        name = "Spring Boots Notification",
+        desc = "Plays a sound and displays a title when your Spring Boots are charged up enough to reach the Crystals on Floor 7 of The Catacombs.",
+    )
+    @ConfigEditorBoolean
+    @FeatureToggle
+    var springBootsNotification: Boolean = false
+
+
 }
--- src/main/java/at/hannibal2/skyhanni/features/dungeon/SpringBootsHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/SpringBootsHelper.kt
@@ -1,14 +1,14 @@
 package at.hannibal2.skyhanni.features.dungeon
 
 import at.hannibal2.skyhanni.SkyHanniMod
-import at.hannibal2.skyhanni.data.TitleManager
+import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.title.TitleManager
 import at.hannibal2.skyhanni.events.PlaySoundEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.SoundUtils
 import net.minecraft.client.Minecraft
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
@@ -21,7 +21,7 @@ object SpringBootsHelper {
      */
     private var soundStreak = 0
 
-    @SubscribeEvent
+    @HandleEvent
     fun onSound(event: PlaySoundEvent) {
         if (!isEnabled()) return
         if (!(event.soundName == "note.pling" || event.soundName == "fireworks.launch" || event.soundName == "random.eat")) return
@@ -33,19 +33,19 @@ object SpringBootsHelper {
         if (!Minecraft.getMinecraft().thePlayer.isSneaking) return
         soundStreak += 1
 
-        if (soundStreak == 5) { // 7-8 seems to be perfect but subtract a bit of reaction speed
-            TitleManager.sendTitle("§cSpring Boots ready!", 2.seconds, 3.6, 7.0f)
+        if (soundStreak == 5) {
+            TitleManager.sendTitle("§cSpring Boots ready!", duration = 2.seconds)
             SoundUtils.playBeepSound()
         }
     }
 
     private fun inDungeon(): Boolean {
-        if (!DungeonAPI.inDungeon()) return false
-        if (!DungeonAPI.inBossRoom) return false
-        if (!DungeonAPI.isOneOf("F7", "M7")) return false
+        if (!DungeonApi.inDungeon()) return false
+        if (!DungeonApi.inBossRoom) return false
+        if (!DungeonApi.isOneOf("F7", "M7")) return false
 
         return true
     }
 
-    fun isEnabled() = inDungeon() && config.springBootsNotification
+    private fun isEnabled() = inDungeon() && config.springBootsNotification
 }
