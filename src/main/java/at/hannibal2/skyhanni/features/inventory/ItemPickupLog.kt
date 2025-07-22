4714ecec1903ecdc09a891aad4bacb54fb056a9b
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-07-22 11:52:58 +0200

    removed import

--- src/main/java/at/hannibal2/skyhanni/features/inventory/ItemPickupLog.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/ItemPickupLog.kt
@@ -29,7 +29,6 @@ import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderable
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SkyBlockUtils
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
-import at.hannibal2.skyhanni.utils.collection.RenderableCollectionUtils.addString
 import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
 import at.hannibal2.skyhanni.utils.compat.getItemOnCursor
 import at.hannibal2.skyhanni.utils.renderables.Renderable
