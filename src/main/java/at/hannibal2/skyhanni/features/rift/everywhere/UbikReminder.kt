57bd8ef168ed37f718f62b76fb73257af814fcfc
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-07-22 10:02:02 +0200

    using SimpleTimeMark

--- src/main/java/at/hannibal2/skyhanni/features/rift/everywhere/UbikReminder.kt
+++ src/main/java/at/hannibal2/skyhanni/features/rift/everywhere/UbikReminder.kt
@@ -2,12 +2,15 @@ package at.hannibal2.skyhanni.features.rift.everywhere
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.IslandType
+import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
+import at.hannibal2.skyhanni.events.minecraft.SkyHanniTickEvent
 import at.hannibal2.skyhanni.features.rift.RiftApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
+import at.hannibal2.skyhanni.utils.SimpleTimeMark
+import at.hannibal2.skyhanni.utils.SimpleTimeMark.Companion.fromNow
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import kotlin.time.Duration.Companion.hours
 
@@ -16,7 +19,7 @@ object UbikReminder {
 
     private val config get() = RiftApi.config.area.mountaintop
 
-    private var isTimerRunning = false
+    private var nextRemindTime = SimpleTimeMark.farPast()
     private val patternGroup = RepoPattern.group("rift.ubik")
 
     /**
@@ -30,20 +33,17 @@ object UbikReminder {
     @HandleEvent(onlyOnIsland = IslandType.THE_RIFT)
     fun onChat(event: SkyHanniChatEvent) {
         if (!config.ubikReminder) return
-        val message = event.message
-        if (ubikRoundPattern.matches(message) && !isTimerRunning) {
-            startTimer()
+        if (ubikRoundPattern.matches(event.message)) {
+            nextRemindTime = 2.hours.fromNow()
         }
     }
 
-    private fun startTimer() {
-        isTimerRunning = true
-
-        DelayedRun.runDelayed(2.hours) { // 2 hours as a Duration
-            if (config.ubikReminder) {
-                ChatUtils.chat("§aUbik's cube is ready in the rift!")
-            }
-            isTimerRunning = false
+    @HandleEvent
+    fun onSecondPassed(event: SecondPassedEvent) {
+        if (!nextRemindTime.isInPast()) return
+        if (config.ubikReminder) {
+            ChatUtils.chat("§aUbik's cube is ready in the rift!")
         }
+        nextRemindTime = SimpleTimeMark.farPast()
     }
 }
