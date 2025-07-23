a65d640077eef201146882741187d7093b3b090f
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-23 16:05:33 -0400

    Fix detekt

--- src/main/java/at/hannibal2/skyhanni/features/slayer/HideIrrelevantMobsInSlayer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/HideIrrelevantMobsInSlayer.kt
@@ -40,7 +40,6 @@ object HideIrrelevantMobsInSlayer {
             mobNames = setOf("Magma Cube", "Magma Cube Rider"),
             isInArea = { IslandType.CRIMSON_ISLE.isCurrent() && SkyBlockUtils.graphArea == "Burning Desert" },
         )
-        ;
     }
 
     private fun isActive() = SlayerApi.isInCorrectArea && config.hideIrrelevantMobs
