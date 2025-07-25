9594b51aa82bf5c428d6be48ea9cba8e75860996
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-25 19:43:21 -0400

    fix: chat manager double override

--- src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/event/AbstractSourcedChatEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/event/AbstractSourcedChatEvent.kt
@@ -1,11 +1,14 @@
 package at.hannibal2.skyhanni.data.hypixel.chat.event
 
+import at.hannibal2.skyhanni.events.chat.AbstractChatEvent
 import at.hannibal2.skyhanni.utils.ComponentSpan
 import net.minecraft.util.IChatComponent
 
-class NpcChatEvent(
-    authorComponent: ComponentSpan,
+open class AbstractSourcedChatEvent(
+    val authorComponent: ComponentSpan,
     messageComponent: ComponentSpan,
     chatComponent: IChatComponent,
     blockedReason: String? = null,
-) : AbstractSourcedChatEvent(authorComponent, messageComponent, chatComponent, blockedReason)
+) : AbstractChatEvent(messageComponent, chatComponent, blockedReason) {
+    val author = authorComponent.getText()
+}
