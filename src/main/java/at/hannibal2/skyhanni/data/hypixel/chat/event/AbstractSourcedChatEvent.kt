194521c7bfbe53c52e73c3663a3ecdc421955271
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-25 22:06:55 -0400

    Make Abstract, abstract

--- src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/event/AbstractSourcedChatEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/data/hypixel/chat/event/AbstractSourcedChatEvent.kt
@@ -4,7 +4,7 @@ import at.hannibal2.skyhanni.events.chat.AbstractChatEvent
 import at.hannibal2.skyhanni.utils.ComponentSpan
 import net.minecraft.util.IChatComponent
 
-open class AbstractSourcedChatEvent(
+abstract class AbstractSourcedChatEvent(
     val authorComponent: ComponentSpan,
     messageComponent: ComponentSpan,
     chatComponent: IChatComponent,
