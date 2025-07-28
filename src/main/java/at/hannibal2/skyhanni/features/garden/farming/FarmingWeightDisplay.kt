28ec3927631aee5d0bde0981de3aab910c8b0f58
DavidArthurCole <davidarthurcole23@gmail.com>
2025-07-27 21:48:22 -0400

    fix: formatting

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/FarmingWeightDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/FarmingWeightDisplay.kt
@@ -124,10 +124,9 @@ object FarmingWeightDisplay {
 
     private val config get() = GardenApi.config.eliteFarmingWeights
     private val storage get() = GardenApi.storage?.farmingWeight
-    private val lbName get() = when (config.eliteLBType.get().leaderboardName) {
-        "" -> "Farming Weight"
-        else -> "$config.eliteLBType.get().leaderboardName Farming Weight"
-    }
+    private val lbName get() = config.eliteLBType.get().leaderboardName.let {
+        if (it.isEmpty()) "" else "$it "
+    } + "Farming Weight"
     private val localCounter = mutableMapOf<CropType, Long>()
 
     private var display = emptyList<Renderable>()
