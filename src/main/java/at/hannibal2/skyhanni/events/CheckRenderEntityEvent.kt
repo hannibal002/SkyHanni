c7e4067537c9b4a17f9efcc2636946d3b46df545
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-07-29 12:58:58 +0200

    added docs

--- src/main/java/at/hannibal2/skyhanni/events/CheckRenderEntityEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/CheckRenderEntityEvent.kt
@@ -4,6 +4,13 @@ import at.hannibal2.skyhanni.api.event.GenericSkyHanniEvent
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
 
+/**
+ * THis event is already cached and only fires 5 times per second per entity.
+ * This means the changed visibility state can take up to 200ms until it updates.
+ * This is a cached version similarly to [SkyHanniRenderEntityEvent].
+ * Do not use this event when you want to do further render calls!
+ * Internally we directly mixin to shouldRender.
+ */
 data class CheckRenderEntityEvent<T : EntityLivingBase>(
     val entity: T,
     val camX: Double,
