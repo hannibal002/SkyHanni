8aa105c537fd9d3890d12316604dfe24b062661c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-07-29 14:05:59 +0200

    formatting

--- src/main/java/at/hannibal2/skyhanni/events/BossHealthChangeEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/BossHealthChangeEvent.kt
@@ -3,4 +3,9 @@ package at.hannibal2.skyhanni.events
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 import at.hannibal2.skyhanni.features.combat.damageindicator.DamageIndicatorEntityData
 
-class BossHealthChangeEvent(val entityData: DamageIndicatorEntityData, val lastHealth: Long, val health: Long, val maxHealth: Long) : SkyHanniEvent()
+class BossHealthChangeEvent(
+    val entityData: DamageIndicatorEntityData,
+    val lastHealth: Long,
+    val health: Long,
+    val maxHealth: Long,
+) : SkyHanniEvent()
