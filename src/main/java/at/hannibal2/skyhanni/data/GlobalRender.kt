3a583868f19fbfc05b2abe92a5e7d0020fcb5507
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-08-05 19:53:35 +0200

    fix import

--- src/main/java/at/hannibal2/skyhanni/data/GlobalRender.kt
+++ src/main/java/at/hannibal2/skyhanni/data/GlobalRender.kt
@@ -1,6 +1,5 @@
 package at.hannibal2.skyhanni.data
 
-import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.commands.CommandCategory
 import at.hannibal2.skyhanni.config.commands.CommandRegistrationEvent
