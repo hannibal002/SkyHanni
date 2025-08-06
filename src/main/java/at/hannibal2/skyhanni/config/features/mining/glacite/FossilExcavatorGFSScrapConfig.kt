083c4bacf71a2d2d1eda4853710dca8e78374417
DavidArthurCole <davidarthurcole23@gmail.com>
2025-08-06 11:45:09 -0400

    feat: gfs scrap excavator start

---
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/glacite/FossilExcavatorGFSScrapConfig.kt
@@ -0,0 +1,5 @@
+package at.hannibal2.skyhanni.config.features.mining.glacite
+
+class FossilExcavatorGFSScrapConfig {
+
+}
---
+++ src/main/java/at/hannibal2/skyhanni/features/mining/fossilexcavator/ScrapGFS.kt
@@ -0,0 +1,8 @@
+package at.hannibal2.skyhanni.features.mining.fossilexcavator
+
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+
+@SkyHanniModule
+object ScrapGFS {
+
+}
