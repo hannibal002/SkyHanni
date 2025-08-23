ac4b5786a87abb193ddd565530d63ace0d1b01c1
Luna <lunaynx@gmail.com>
2025-08-23 17:06:42 +0200

    Make negatives more visually distinct

--- versions/1.21.5/src/main/java/at/hannibal2/skyhanni/features/foraging/MoongladeBeacon.kt
+++ versions/1.21.5/src/main/java/at/hannibal2/skyhanni/features/foraging/MoongladeBeacon.kt
@@ -525,7 +525,7 @@ object MoongladeBeacon {
         fun RenderInventoryItemTipEvent.tryLabelIfAble() {
             if (isEnchanted && !upgradingStrength) return
             val offset = getOffsetBySlot(slot.index)?.takeIf { it != 0 } ?: return
-            stackTip = "§a$offset"
+            stackTip = if (offset > 0) "§a$offset" else "§c$offset"
         }
 
         fun getOffsetBySlot(slot: Int): Int? = when (slot) {
