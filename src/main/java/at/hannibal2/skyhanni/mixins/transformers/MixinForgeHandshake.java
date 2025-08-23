82ee3b56a511bb6df5298d59123c4c214e62f2b5
Bloxigus <72960305+Bloxigus@users.noreply.github.com>
2025-08-24 04:29:04 +0930

    merge with beta

---
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinForgeHandshake.java
@@ -0,0 +1,30 @@
+package at.hannibal2.skyhanni.mixins.transformers;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.network.handshake.FMLHandshakeMessage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(FMLHandshakeMessage.ModList.class)
+public class MixinForgeHandshake {
+
+    @Shadow
+    private Map<String, String> modTags;
+
+    @Inject(
+        at = @At("HEAD"),
+        method = "toBytes"
+    )
+    public void onToBytes(ByteBuf buffer, CallbackInfo ci) {
+        for (ModContainer mod : Loader.instance().getActiveModList()) {
+            this.modTags.put(mod.getModId(), mod.getVersion());
+        }
+    }
+}
