39b247eec55ab2df91eb8cafee115d0fca604fde
rueblimaster <75255906+rueblimaster@users.noreply.github.com>
2025-08-25 17:42:40 +0200

    Remove KeyUpEvent and update key event references
    Deleted the KeyUpEvent class and replaced references to KeyDownEvent with KeyPressEvent in DungeonSpiritLeapOverlay and KeyboardManager. This streamlines key event handling and removes unused code.

--- src/main/java/at/hannibal2/skyhanni/utils/KeyboardManager.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/KeyboardManager.kt
@@ -109,7 +109,7 @@ object KeyboardManager {
 
     /**
      * Can only be used once per click, since the function locks itself until the key is no longer held.
-     * Do not use in KeyPressEvent, since it won't be unlocked again, use KeyDownEvent instead.
+     * Do not use in KeyPressEvent, since it won't be unlocked again, use KeyPressEvent instead.
      * */
     fun Int.isKeyClicked(): Boolean = if (this.isKeyHeld()) {
         if (lockedKeys[this] != true) {
