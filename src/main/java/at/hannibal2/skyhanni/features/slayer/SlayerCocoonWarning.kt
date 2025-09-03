e5df12da2b86a9ee2709f7dadfc604d8c7a31f01
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2025-09-03 05:22:03 +0100

    I forgot detekt hates this

--- src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerCocoonWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerCocoonWarning.kt
@@ -11,6 +11,9 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 @SkyHanniModule
 object SlayerCocoonWarning {
+    /**
+     * REGEX-TEST:   §r§c§lYOU COCOONED YOUR SLAYER BOSS
+     */
     private val slayerCocoonPattern by RepoPattern.pattern(
         "slayer.cocooned",
         "\\s+§r§c§lYOU COCOONED YOUR SLAYER BOSS",
