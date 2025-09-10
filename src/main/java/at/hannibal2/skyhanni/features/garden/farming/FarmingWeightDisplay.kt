90b62ceb4592b94fa4a1d8d66094ca14bac1871f
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-09-10 09:58:17 -0500

    fix merge

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/FarmingWeightDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/FarmingWeightDisplay.kt
@@ -328,13 +328,13 @@ object FarmingWeightDisplay {
 
         val displayList: List<FarmingWeightTextEntry> = buildList {
             add(FarmingWeightTextEntry.WEIGHT_POSITION)
-            event.transform(106, "garden.eliteFarmingWeights.overtakeETA") { entry ->
+            event.transform(107, "garden.eliteFarmingWeights.overtakeETA") { entry ->
                 if (entry.asBoolean) add(FarmingWeightTextEntry.OVERTAKE)
                 entry
             }
         }
 
-        event.add(106, "garden.eliteFarmingWeights.text") {
+        event.add(107, "garden.eliteFarmingWeights.text") {
             ConfigManager.gson.toJsonTree(displayList)
         }
 
