229343ee83659dfe09d16977dd229146e3b5aa6e
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-09-10 14:39:25 +0200

    removed unused imports v2

--- src/main/java/at/hannibal2/skyhanni/config/Features.kt
+++ src/main/java/at/hannibal2/skyhanni/config/Features.kt
@@ -20,7 +20,6 @@ import at.hannibal2.skyhanni.config.features.rift.RiftConfig
 import at.hannibal2.skyhanni.config.features.skillprogress.SkillProgressConfig
 import at.hannibal2.skyhanni.config.features.slayer.SlayerConfig
 import at.hannibal2.skyhanni.config.storage.Storage
-import at.hannibal2.skyhanni.test.SkyHanniDebugsAndTests
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.Config
 import io.github.notenoughupdates.moulconfig.Social
