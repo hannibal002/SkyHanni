f92a145830903529d5ac9c1602af13eee059d3dd
Luna <lunaynx@gmail.com>
2025-09-17 22:41:50 +0200

    Backend: Optimize SimpleTimeMark & bring ServerTimeMark in line

--- src/main/java/at/hannibal2/skyhanni/utils/SimpleTimeMark.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SimpleTimeMark.kt
@@ -71,10 +71,16 @@ value class SimpleTimeMark(private val millis: Long) : Comparable<SimpleTimeMark
 
         fun now() = SimpleTimeMark(System.currentTimeMillis())
 
+        private const val FAR_PAST_MS = 0L
+        private const val FAR_FUTURE_MS = Long.MAX_VALUE
+
+        private val FAR_PAST = SimpleTimeMark(FAR_PAST_MS)
+        private val FAR_FUTURE = SimpleTimeMark(FAR_FUTURE_MS)
+
         @JvmStatic
         @JvmName("farPast")
-        fun farPast() = SimpleTimeMark(0)
-        fun farFuture() = SimpleTimeMark(Long.MAX_VALUE)
+        fun farPast() = FAR_PAST
+        fun farFuture() = FAR_FUTURE
 
         fun Duration.fromNow() = now() + this
 
