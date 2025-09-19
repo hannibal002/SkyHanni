ccc937344fb68ee68235f385b550835c9f9f89e8
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-09-19 08:17:46 -0500

    oop

--- src/main/java/at/hannibal2/skyhanni/utils/tracker/SessionUptime.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/tracker/SessionUptime.kt
@@ -3,16 +3,14 @@ package at.hannibal2.skyhanni.utils.tracker
 sealed class SessionUptime {
     data class Normal(val sessionType: NormalSession) : SessionUptime()
     data class Garden(val sessionType: GardenSession) : SessionUptime()
-
-    enum class NormalSession {
-        NORMAL,
-    }
-
-    enum class GardenSession {
-        PEST,
-        VISITOR,
-        CROP,
-    }
 }
 
+enum class NormalSession {
+    NORMAL,
+}
 
+enum class GardenSession {
+    PEST,
+    VISITOR,
+    CROP,
+}
