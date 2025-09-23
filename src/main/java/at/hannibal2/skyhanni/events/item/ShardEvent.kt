c454fa32a82b05dcaeae3d8709de8ee987433ad2
calwolfson <cwolfson58@gmail.com>
2025-09-23 20:06:38 +1000

    javadoc

--- src/main/java/at/hannibal2/skyhanni/events/item/ShardEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/item/ShardEvent.kt
@@ -3,7 +3,13 @@ package at.hannibal2.skyhanni.events.item
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 import at.hannibal2.skyhanni.utils.NeuInternalName
 
+/**
+ * All shard events
+ */
 open class ShardEvent(val shardInternalName: NeuInternalName, val amount: Int) : SkyHanniEvent()
 
+/**
+ * Shard events that are explicitly the player gaining shards. For use in stuff like profit trackers
+ */
 class ShardGainEvent(shardInternalName: NeuInternalName, amount: Int) : ShardEvent(shardInternalName, amount)
 
