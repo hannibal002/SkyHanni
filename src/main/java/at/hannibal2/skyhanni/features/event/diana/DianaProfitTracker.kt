31e0faa75d6227c48bf3c93af183535e84803bd0
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-09-27 16:27:20 -0500

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/event/diana/DianaProfitTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/diana/DianaProfitTracker.kt
@@ -31,7 +31,6 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import at.hannibal2.skyhanni.utils.tracker.ItemTrackerData
 import at.hannibal2.skyhanni.utils.tracker.SessionUptime
 import at.hannibal2.skyhanni.utils.tracker.SkyHanniTimedItemTracker
-import at.hannibal2.skyhanni.utils.tracker.SkyHanniTracker
 import at.hannibal2.skyhanni.utils.tracker.SkyHanniTracker.DisplayMode
 import at.hannibal2.skyhanni.utils.tracker.TimedTrackerData
 import com.google.gson.annotations.Expose
