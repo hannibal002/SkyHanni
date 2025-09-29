0d8e10f88f32c6e40d53224ffbc2494cf3b03a42
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-09-30 01:10:26 +0200

    the actual fix

--- src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
+++ src/main/java/at/hannibal2/skyhanni/SkyHanniMod.kt
@@ -39,11 +39,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiScreen
 import org.apache.logging.log4j.Level
@@ -202,38 +204,33 @@ object SkyHanniMod {
         name: String,
         timeout: Duration = 10.seconds,
         function: suspend CoroutineScope.() -> Unit,
-    ): Job = coroutineScope.launch {
-        val mainJob = launch {
-            try {
+    ): Job = coroutineScope.launch(CoroutineName("SkyHanni $name")) {
+        try {
+            if (timeout != Duration.INFINITE && timeout > Duration.ZERO) {
+                withTimeout(timeout) { function() }
+            } else {
                 function()
-            } catch (e: CancellationException) {
-                // Don't notify the user about cancellation exceptions - these are to be expected at times
-                val jobState = coroutineContext[Job]?.toString() ?: "unknown job"
-                val cancellationCause = coroutineContext[Job]?.getCancellationException()
-                logger.debug("Job $jobState/$name was cancelled with cause: $cancellationCause", e)
-            } catch (e: Throwable) {
-                ErrorManager.logErrorWithData(
-                    e,
-                    e.message ?: "Asynchronous exception caught in $name",
-                    "coroutine name" to name,
-                    "coroutine timeout" to timeout,
-                )
             }
-        }
-
-        if (timeout != Duration.INFINITE && timeout != Duration.ZERO) {
-            launch {
-                delay(timeout)
-                if (mainJob.isActive) {
-                    ErrorManager.logErrorStateWithData(
-                        "Coroutine $name timed out",
-                        "The coroutine '$name' took longer than the specified timeout of $timeout",
-                        "timeout" to timeout,
-                        "coroutine name" to name,
-                    )
-                    mainJob.cancel(CancellationException("Coroutine $name timed out after $timeout"))
-                }
-            }
+        } catch (e: TimeoutCancellationException) {
+            ErrorManager.logErrorWithData(
+                e,
+                "Coroutine $name timed out after $timeout",
+                "coroutine name" to name,
+                "timeout" to timeout,
+            )
+            throw e
+        } catch (e: CancellationException) {
+            // Don't notify the user about cancellation exceptions - these are to be expected at times
+            val jobState = coroutineContext[Job]?.toString() ?: "unknown job"
+            val cancellationCause = coroutineContext[Job]?.getCancellationException()
+            logger.debug("Job $jobState/$name was cancelled with cause: $cancellationCause", e)
+        } catch (e: Throwable) {
+            ErrorManager.logErrorWithData(
+                e,
+                "Asynchronous exception caught in $name",
+                "coroutine name" to name,
+                "coroutine timeout" to timeout,
+            )
         }
     }
 
