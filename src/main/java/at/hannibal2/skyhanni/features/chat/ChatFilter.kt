5bb5ec5240442f811043eb844448d72de76e41ab
AfkUser <95704481+AfkUserMC@users.noreply.github.com>
2025-10-01 17:18:25 -0400

    Fixed commas not being included in the filter

--- src/main/java/at/hannibal2/skyhanni/features/chat/ChatFilter.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/ChatFilter.kt
@@ -105,7 +105,7 @@ object ChatFilter {
      * REGEX-TEST: §aYou earned §r§510 Event EXP §r§afrom playing SkyBlock!
      */
     private val guildExpPatterns = listOf(
-        "§aYou earned §r§[0-9a-f]\\d+ (?:GEXP|Event EXP) (?:§r§a\\+ §r§[0-9a-f]\\d+ Event EXP )?§r§afrom playing SkyBlock!".toPattern(),
+        "§aYou earned §r§[0-9a-f][\\d,]+ (?:GEXP|Event EXP) (?:§r§a\\+ §r§[0-9a-f][\\d,]+ Event EXP )?§r§afrom playing SkyBlock!".toPattern(),
     )
 
     // Kill Combo
