c04e93de7abb2aab556935f1329ba25935eb1b05
nopo <nopotheemail@gmail.com>
2025-10-11 10:21:16 +1100

    first part

--- src/main/java/at/hannibal2/skyhanni/utils/compat/ScoreboardCompat.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/compat/ScoreboardCompat.kt
@@ -30,11 +30,11 @@ fun Collection<Score>.getPlayerNames(scoreboard: Scoreboard): List<Score> {
 //$$         .map {
 //$$             val team = scoreboard.getScoreHolderTeam(it.owner)
 //$$             Text.empty().also { main ->
-//$$                 team?.prefix?.apply { siblings.forEach { sibling -> main.append(sibling) } }
+//$$                 team?.prefix?.apply { main.append(this) }
 //$$                 // the soccer ball is because it is like that on 1.8
 //$$                 // this could be changed later but for now i think this is fine
 //$$                 main.append("⚽")
-//$$                 team?.suffix?.apply { siblings.forEach { sibling -> main.append(sibling) } }
+//$$                 team?.suffix?.apply { main.append(this) }
 //$$             }
 //$$         }
 //$$ }
