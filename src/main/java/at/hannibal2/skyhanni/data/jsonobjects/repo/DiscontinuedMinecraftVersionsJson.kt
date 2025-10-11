79b17a344cb3480606ce8eb8027d461a08e85439
Cal <cwolfson58@gmail.com>
2025-10-11 17:53:19 +1100

    Backend: Support dropping mc versions

---
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/DiscontinuedMinecraftVersionsJson.kt
@@ -0,0 +1,12 @@
+package at.hannibal2.skyhanni.data.jsonobjects.repo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class DiscontinuedMinecraftVersionsJson(
+    @Expose val versions: Map<String, DiscontinuedMinecraftVersion>? = mapOf(),
+)
+
+data class DiscontinuedMinecraftVersion(
+    @Expose @SerializedName("extra_info") val extraInfo: List<String>? = null,
+)
