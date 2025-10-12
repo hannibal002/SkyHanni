4ddb99955f4a2fbf593d57fa2ead9a00721e5e69
Cal <cwolfson58@gmail.com>
2025-10-12 23:35:09 +1100

    it works :fire:

--- src/main/java/at/hannibal2/skyhanni/utils/renderables/FakePlayerRenderable.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/renderables/FakePlayerRenderable.kt
@@ -9,8 +9,15 @@ import net.minecraft.entity.player.EntityPlayer
 import java.awt.Color
 //#if MC < 1.21
 import net.minecraft.client.gui.inventory.GuiInventory.drawEntityOnScreen
-//#else
-//$$ import net.minecraft.client.gui.screen.ingame.InventoryScreen.drawEntity
+//#endif
+//#if MC > 1.21.5
+//$$ import org.joml.Matrix3x2f
+//$$ import kotlin.math.atan
+//$$ import net.minecraft.client.gui.DrawContext
+//$$ import net.minecraft.entity.LivingEntity
+//$$ import net.minecraft.client.gui.screen.ingame.InventoryScreen
+//$$ import org.joml.Quaternionf
+//$$ import org.joml.Vector3f
 //#endif
 
 fun Renderable.Companion.fakePlayer(
@@ -53,7 +60,7 @@ fun Renderable.Companion.fakePlayer(
             mouseYRelativeToPlayer,
             player,
         )
-        //#else
+        //#elseif MC < 1.21.7
         //$$ drawEntity(
         //$$     DrawContextUtils.drawContext,
         //$$     padding,
@@ -64,11 +71,68 @@ fun Renderable.Companion.fakePlayer(
         //$$     0.0625f,
         //$$     if (followMouse) mouse.first - mouseOffsetX.toFloat() else eyesX,
         //$$     if (followMouse) mouse.second - mouseOffsetY.toFloat() else eyesY,
-        //$$     player
+        //$$     player,
+        //$$ )
+        //#else
+        //$$ val peeked = DrawContextUtils.drawContext.matrices.get(Matrix3x2f())
+        //$$ val translationX = peeked.m20().toInt()
+        //$$ val translationY = peeked.m21().toInt()
+        //$$ val averageScale = (peeked.m00() + peeked.m11()) / 2
+        //$$ drawEntityWithoutScissor(
+        //$$     DrawContextUtils.drawContext,
+        //$$     padding + translationX,
+        //$$     padding + translationY,
+        //$$     padding + width + translationX,
+        //$$     padding + height + translationY,
+        //$$     (entityScale * averageScale).toInt(),
+        //$$     0.0625f * averageScale,
+        //$$     if (followMouse) mouse.first - mouseOffsetX.toFloat() + translationX else eyesX,
+        //$$     if (followMouse) mouse.second - mouseOffsetY.toFloat() + translationY else eyesY,
+        //$$     player,
         //$$ )
         //#endif
         DrawContextUtils.popMatrix()
     }
 }
 
-
+//#if MC > 1.21.5
+//$$ private fun drawEntityWithoutScissor(
+//$$     context: DrawContext,
+//$$     x1: Int,
+//$$     y1: Int,
+//$$     x2: Int,
+//$$     y2: Int,
+//$$     size: Int,
+//$$     scale: Float,
+//$$     mouseX: Float,
+//$$     mouseY: Float,
+//$$     entity: LivingEntity,
+//$$ ) {
+//$$     val f = (x1 + x2) / 2.0f
+//$$     val g = (y1 + y2) / 2.0f
+//$$     val h = atan(((f - mouseX) / 40.0f).toDouble()).toFloat()
+//$$     val i = atan(((g - mouseY) / 40.0f).toDouble()).toFloat()
+//$$     val quaternionf = Quaternionf().rotateZ(Math.PI.toFloat())
+//$$     val quaternionf2 = Quaternionf().rotateX(i * 20.0f * (Math.PI / 180.0).toFloat())
+//$$     quaternionf.mul(quaternionf2)
+//$$     val j = entity.bodyYaw
+//$$     val k = entity.yaw
+//$$     val l = entity.pitch
+//$$     val m = entity.lastHeadYaw
+//$$     val n = entity.headYaw
+//$$     entity.bodyYaw = 180.0f + h * 20.0f
+//$$     entity.yaw = 180.0f + h * 40.0f
+//$$     entity.pitch = -i * 20.0f
+//$$     entity.headYaw = entity.yaw
+//$$     entity.lastHeadYaw = entity.yaw
+//$$     val o = entity.scale
+//$$     val vector3f = Vector3f(0.0f, entity.height / 2.0f + scale * o, 0.0f)
+//$$     val p = size / o
+//$$     InventoryScreen.drawEntity(context, x1, y1, x2, y2, p, vector3f, quaternionf, quaternionf2, entity)
+//$$     entity.bodyYaw = j
+//$$     entity.yaw = k
+//$$     entity.pitch = l
+//$$     entity.lastHeadYaw = m
+//$$     entity.headYaw = n
+//$$ }
+//#endif
