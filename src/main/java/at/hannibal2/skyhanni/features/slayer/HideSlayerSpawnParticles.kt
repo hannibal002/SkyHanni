c401419c175b960409d88431e8805c6c68e54aa4
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-10-13 00:00:43 +0200

    cleanup

--- src/main/java/at/hannibal2/skyhanni/features/slayer/HideSlayerSpawnParticles.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/HideSlayerSpawnParticles.kt
@@ -1,12 +1,12 @@
 package at.hannibal2.skyhanni.features.slayer
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
-import at.hannibal2.skyhanni.config.features.slayer.SlayerConfig.SlayerSpawnParticlesToHide
+import at.hannibal2.skyhanni.config.features.slayer.SlayerConfig.SlayerSpawnParticlesToHide as ToHide
 import at.hannibal2.skyhanni.data.SlayerApi
 import at.hannibal2.skyhanni.events.ReceiveParticleEvent
 import at.hannibal2.skyhanni.events.slayer.SlayerStateChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import net.minecraft.util.EnumParticleTypes
+import net.minecraft.util.EnumParticleTypes as ParticleType
 
 @SkyHanniModule
 object HideSlayerSpawnParticles {
@@ -18,10 +18,12 @@ object HideSlayerSpawnParticles {
     fun onReceiveParticle(event: ReceiveParticleEvent) {
         if (!SlayerApi.hasActiveQuest() && !isGrinding) return
 
-        if (event.type == EnumParticleTypes.ENCHANTMENT_TABLE && config.slayerParticle == SlayerSpawnParticlesToHide.BOTH) {
+        val type = event.type
+        val slayerParticle = config.slayerParticle
+        if (type == ParticleType.ENCHANTMENT_TABLE && slayerParticle == ToHide.BOTH) {
             event.cancel()
         }
-        if ((event.type == EnumParticleTypes.SPELL_WITCH || event.type == EnumParticleTypes.SPELL_MOB) && (config.slayerParticle == SlayerSpawnParticlesToHide.BOTH || config.slayerParticle == SlayerSpawnParticlesToHide.PURPLE)) {
+        if ((type == ParticleType.SPELL_WITCH || type == ParticleType.SPELL_MOB) && (slayerParticle == ToHide.BOTH || slayerParticle == ToHide.PURPLE)) {
             event.cancel()
         }
     }
