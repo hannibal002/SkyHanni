cb53b7871c6da56993de6305580967326b2e6279
Luna <lunaynx@gmail.com>
2025-10-13 17:31:25 +0200

    Update PestSpawnEvent.kt

--- src/main/java/at/hannibal2/skyhanni/events/garden/pests/PestSpawnEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/garden/pests/PestSpawnEvent.kt
@@ -4,8 +4,6 @@ import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 
 class PestSpawnEvent(val amountPests: Int?, val plotNames: List<String>) : SkyHanniEvent() {
     init {
-        if (amountPests != null && amountPests < 1) {
-            throw IllegalArgumentException("amountPests must be a positive integer or null")
-        }
+        require(amountPests == null || amountPests > 0) { "amountPests must be a positive integer or null" }
     }
 }
