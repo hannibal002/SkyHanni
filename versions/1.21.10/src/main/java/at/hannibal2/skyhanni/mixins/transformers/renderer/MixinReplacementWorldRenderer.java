e2eff3f79a5001de1aad0ea38f75a5583ee39209
Cal <cwolfson58@gmail.com>
2025-10-17 23:46:41 +1100

    rendering stuff works

--- versions/1.21.10/src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinReplacementWorldRenderer.java
+++ versions/1.21.10/src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinReplacementWorldRenderer.java
@@ -2,19 +2,17 @@ package at.hannibal2.skyhanni.mixins.transformers.renderer;
 
 import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import net.minecraft.client.render.BlockRenderLayerGroup;
 import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.SectionRenderState;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.state.WorldRenderState;
-import net.minecraft.client.util.Handle;
 import net.minecraft.client.util.ObjectAllocator;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.profiler.Profiler;
 import org.joml.Matrix4f;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Final;
@@ -23,9 +21,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-// Adapted from 1.21.7 fabric api implementation
+// Adapted from 1.21.7 and 1.21.10 fabric api implementation
 @Mixin(WorldRenderer.class)
 public class MixinReplacementWorldRenderer {
 
@@ -48,8 +47,12 @@ public class MixinReplacementWorldRenderer {
         currentTickCounter = tickCounter;
     }
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/GameOptions;getCloudRenderModeValue()Lnet/minecraft/client/option/CloudRenderMode;"))
-    private void afterTranslucent(CallbackInfo ci) {
+    @WrapOperation(method = "method_62214",
+        slice = @Slice(from = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V", args = "ldc=translucent")),
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/SectionRenderState;renderSection(Lnet/minecraft/client/render/BlockRenderLayerGroup;)V", ordinal = 0)
+    )
+    private void onTranslucentRender(SectionRenderState instance, BlockRenderLayerGroup group, Operation<Void> original) {
+        original.call(instance, group);
         if (contextMatrixStack == null) return;
 
         SkyHanniRenderWorldEvent event = new SkyHanniRenderWorldEvent(
@@ -63,8 +66,9 @@ public class MixinReplacementWorldRenderer {
         contextMatrixStack = null;
     }
 
-    @Inject(method = "method_62214", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BufferBuilderStorage;getEntityVertexConsumers()Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;"))
-    private void setMatrixStack(CallbackInfo ci, @Local MatrixStack matrixStack) {
+    @ModifyExpressionValue(method = "method_62214", at = @At(value = "NEW", target = "Lnet/minecraft/client/util/math/MatrixStack;"))
+    private MatrixStack onCreateMatrixStack(MatrixStack matrixStack) {
         contextMatrixStack = matrixStack;
+        return matrixStack;
     }
 }
