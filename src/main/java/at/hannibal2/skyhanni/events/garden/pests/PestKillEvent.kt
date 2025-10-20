3d84423c8fb7e70a7b4f7fded7de7c58ca400754
ILike2WatchMemes <ilike2watchmemes@gmail.com>
2025-10-20 02:44:48 +0200

    update

--- src/main/java/at/hannibal2/skyhanni/events/garden/pests/PestKillEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/garden/pests/PestKillEvent.kt
@@ -4,4 +4,4 @@ import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 import at.hannibal2.skyhanni.features.garden.GardenPlotApi
 import at.hannibal2.skyhanni.features.garden.pests.PestType
 
-class PestKillEvent(val plot: GardenPlotApi.Plot, val type: PestType) : SkyHanniEvent()
+object PestKillEvent : SkyHanniEvent()
