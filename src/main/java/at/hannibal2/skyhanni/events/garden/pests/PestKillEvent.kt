0b006dd89242bb13591d6ac58df0f8c5e00d3116
ILike2WatchMemes <ilike2watchmemes@gmail.com>
2025-10-20 02:45:22 +0200

    oops

--- src/main/java/at/hannibal2/skyhanni/events/garden/pests/PestKillEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/garden/pests/PestKillEvent.kt
@@ -1,7 +1,5 @@
 package at.hannibal2.skyhanni.events.garden.pests
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-import at.hannibal2.skyhanni.features.garden.GardenPlotApi
-import at.hannibal2.skyhanni.features.garden.pests.PestType
 
 object PestKillEvent : SkyHanniEvent()
