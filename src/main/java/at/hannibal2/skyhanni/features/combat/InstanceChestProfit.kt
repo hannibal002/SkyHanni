9ef813b936229e4791cd73c839ce1ac0dfe14da4
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2025-11-14 00:52:47 +0000

    Useless Reset removal.

--- src/main/java/at/hannibal2/skyhanni/features/combat/InstanceChestProfit.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/InstanceChestProfit.kt
@@ -294,7 +294,7 @@ object InstanceChestProfit {
 
     private fun createCroesusDisplay() {
         val newDisplay = buildList {
-            add(listOf(Renderable.text("§6§lCroesus Profit Overlay§r")))
+            add(listOf(Renderable.text("§6§lCroesus Profit Overlay")))
             croesusDisplayList.forEach {
                 add(it)
             }
