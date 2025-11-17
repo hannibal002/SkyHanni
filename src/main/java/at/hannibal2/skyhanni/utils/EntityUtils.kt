fcaf082d658bc0349690b2537c983922ad4fc1a6
Empa <itsempa@users.noreply.github.com>
2025-11-17 07:44:00 +0100

    feat: make entities within distance functions internally use bounding box
    Signed-off-by: Empa <itsempa@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/utils/EntityUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/EntityUtils.kt
@@ -37,10 +37,13 @@ import net.minecraft.util.AxisAlignedBB
 //$$ import net.minecraft.entity.EquipmentSlot
 //#else
 import net.minecraft.entity.SharedMonsterAttributes
+
 //#endif
 
 @SkyHanniModule
 object EntityUtils {
+
+    inline val ALWAYS get(): (Entity) -> Boolean = { true }
 
     // TODO remove this relatively heavy call everywhere
     @Deprecated("Use Mob Detection Instead")
@@ -138,11 +141,13 @@ object EntityUtils {
             .firstOrNull { it.name == "textures" }?.value
     }
 
-    inline fun <reified T : Entity> getEntitiesNextToPlayer(radius: Double): Sequence<T> =
-        getEntitiesNearby<T>(LocationUtils.playerLocation(), radius)
+    inline fun <reified T : Entity> getEntitiesNextToPlayer(radius: Double, noinline predicate: (T) -> Boolean = ALWAYS): List<T> =
+        getEntitiesNearby<T>(LocationUtils.playerLocation(), radius, predicate)
 
-    inline fun <reified T : Entity> getEntitiesNearby(location: LorenzVec, radius: Double): Sequence<T> =
-        getEntities<T>().filter { it.distanceTo(location) < radius }
+    // First filters for a bounding box because it's faster, and then filters based on distance
+    inline fun <reified T : Entity> getEntitiesNearby(location: LorenzVec, radius: Double, noinline predicate: (T) -> Boolean = ALWAYS): List<T> {
+        return getEntitiesInBox<T>(location, radius) { it.distanceTo(location) < radius && predicate(it)}
+    }
 
     inline fun <reified T : Entity> getEntitiesNearbyIgnoreY(location: LorenzVec, radius: Double): Sequence<T> =
         getEntities<T>().filter { it.distanceToIgnoreY(location) < radius }
@@ -179,9 +184,13 @@ object EntityUtils {
 
     inline fun <reified R : Entity> getEntities(): Sequence<R> = getAllEntities().filterIsInstance<R>()
 
+    inline fun <reified E : Entity> getEntitiesInBox(pos: LorenzVec, radius: Double, noinline predicate: (E) -> Boolean = ALWAYS): List<E> {
+        return getEntitiesInBoundingBox(pos.boundingCenter(radius), predicate)
+    }
+
     // More efficient than filtering by type, and then for distance, as Minecraft already first filters the chunks that contain the aabb,
     // and then filters both for entity type and with the predicate for entities inside those chunks.
-    inline fun <reified E : Entity> getEntitiesInBoundingBox(aabb: AxisAlignedBB, noinline predicate: (E) -> Boolean = { true }): List<E> {
+    inline fun <reified E : Entity> getEntitiesInBoundingBox(aabb: AxisAlignedBB, noinline predicate: (E) -> Boolean = ALWAYS): List<E> {
         val world = MinecraftCompat.localWorldOrNull ?: return emptyList()
         //#if MC < 1.21
         return world.getEntitiesWithinAABB(E::class.java, aabb) { it != null && predicate(it) }
