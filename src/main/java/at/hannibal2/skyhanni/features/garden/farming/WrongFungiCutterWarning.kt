02b965e0d332ae6dfdc830758162f3079150b717
Luna <lunaynx@gmail.com>
2025-11-27 02:26:00 +0100

    Fix: Wrong Fungi Cutter Mode on 1.21

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/WrongFungiCutterWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/WrongFungiCutterWarning.kt
@@ -9,9 +9,13 @@ import at.hannibal2.skyhanni.events.garden.farming.CropClickEvent
 import at.hannibal2.skyhanni.features.garden.CropType
 import at.hannibal2.skyhanni.features.garden.GardenApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getFungiCutterMode
 import at.hannibal2.skyhanni.utils.SoundUtils
+import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
+import net.minecraft.block.Block
+import net.minecraft.init.Blocks
 import net.minecraft.item.ItemStack
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
@@ -22,14 +26,21 @@ object WrongFungiCutterWarning {
     private var mode = FungiMode.UNKNOWN
     private var lastPlaySoundTime = SimpleTimeMark.farPast()
 
+    private val patternGroup = RepoPattern.group("garden.fungicutter")
+
+    /**
+     * REGEX-TEST: §eFungi Cutter Mode: §r§cRed Mushrooms
+     * REGEX-TEST: §eFungi Cutter Mode: §r§6Brown Mushrooms
+     */
+    private val modePattern by patternGroup.pattern(
+        "mode",
+        "§eFungi Cutter Mode: (?:§.)*(?<color>\\w+) Mushrooms",
+    )
+
     @HandleEvent
     fun onChat(event: SkyHanniChatEvent) {
-        val message = event.message
-        if (message == "§eFungi Cutter Mode: §r§cRed Mushrooms") {
-            mode = FungiMode.RED
-        }
-        if (message == "§eFungi Cutter Mode: §r§6Brown Mushrooms") {
-            mode = FungiMode.BROWN
+        modePattern.matchMatcher(event.message) {
+            mode = FungiMode.from(group("color"))
         }
     }
 
@@ -37,12 +48,9 @@ object WrongFungiCutterWarning {
     fun onCropClick(event: CropClickEvent) {
         if (event.clickType != ClickType.LEFT_CLICK) return
         if (event.crop != CropType.MUSHROOM) return
+        if (mode == FungiMode.UNKNOWN) return
 
-        val toString = event.blockState.toString()
-        if (toString == "minecraft:red_mushroom" && mode == FungiMode.BROWN) {
-            notifyWrong()
-        }
-        if (toString == "minecraft:brown_mushroom" && mode == FungiMode.RED) {
+        if (event.blockState.block != mode.block) {
             notifyWrong()
         }
     }
@@ -69,19 +77,19 @@ object WrongFungiCutterWarning {
     private fun readItem(item: ItemStack) {
         // The fungi cutter mode is not set into the item nbt data immediately after purchasing it.
         val rawMode = item.getFungiCutterMode() ?: return
-        mode = FungiMode.getOrNull(rawMode)
+        mode = FungiMode.from(rawMode)
     }
 
-    enum class FungiMode {
-        RED,
-        BROWN,
-        UNKNOWN,
+    enum class FungiMode(val block: Block? = null) {
+        RED(Blocks.red_mushroom),
+        BROWN(Blocks.brown_mushroom),
+        UNKNOWN(),
         ;
 
         companion object {
-
-            fun getOrNull(mode: String) =
-                entries.firstOrNull { it.name == mode } ?: error("Unknown fungi mode: '$mode'")
+            fun from(name: String): FungiMode = entries.firstOrNull {
+                it.name.equals(name, ignoreCase = true)
+            } ?: error("Unknown Fungi Cutter mode: '$name'")
         }
     }
 }
