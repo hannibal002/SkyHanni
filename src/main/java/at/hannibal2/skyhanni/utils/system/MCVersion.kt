b267de85169350b4d074b7d748287a7c12923077
nopo <nopotheemail@gmail.com>
2025-11-27 22:49:05 +1100

    code cleanup

--- src/main/java/at/hannibal2/skyhanni/utils/system/MCVersion.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/system/MCVersion.kt
@@ -15,11 +15,8 @@ data class MCVersion(val epoch: Int, val major: Int, val minor: Int) : Comparabl
     }
 
     /** Inclusive for both borders */
-    fun isInBetween(current: MCVersion, target: MCVersion): Boolean {
-        if (this > target) return false
-        if (this < current) return false
-        if (this == current) return true
-        return true
+    fun isInBetween(lower: MCVersion, higher: MCVersion): Boolean {
+        return this in lower..higher
     }
 
     companion object {
