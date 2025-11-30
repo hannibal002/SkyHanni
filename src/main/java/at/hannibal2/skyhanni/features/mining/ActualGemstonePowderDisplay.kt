b561807847376f9fd53d0452e757b8c397d6d0a5
tristen dillon <tristendillon24@outlook.com>
2025-11-30 01:26:44 -0600

    Add echo of echoes, clean up code to make more dry and less flat. Make tooltip ui better.

--- src/main/java/at/hannibal2/skyhanni/features/mining/ActualGemstonePowderDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/ActualGemstonePowderDisplay.kt
@@ -1,6 +1,5 @@
 package at.hannibal2.skyhanni.features.mining
 
-import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.HotmApi
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.api.pet.CurrentPetApi
@@ -9,22 +8,24 @@ import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.hotx.HotmData
 import at.hannibal2.skyhanni.data.model.TabWidget
 import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
+import at.hannibal2.skyhanni.features.inventory.attribute.AttributeShardsData
 import at.hannibal2.skyhanni.features.mining.powdertracker.PowderChestReward
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.InventoryUtils
+import at.hannibal2.skyhanni.utils.ItemCategory
+import at.hannibal2.skyhanni.utils.ItemUtils.getItemCategoryOrNull
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.LorenzRarity
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.NumberUtil.formatInt
 import at.hannibal2.skyhanni.utils.RegexUtils.groupOrNull
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
+import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getReforgeModifier
 import at.hannibal2.skyhanni.utils.chat.TextHelper
 import at.hannibal2.skyhanni.utils.compat.hover
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
-import at.hannibal2.skyhanni.features.inventory.attribute.AttributeShardsData
-import at.hannibal2.skyhanni.utils.ItemCategory
-import at.hannibal2.skyhanni.utils.ItemUtils.getItemCategoryOrNull
 import java.util.Locale
 
 @SkyHanniModule
@@ -32,16 +33,38 @@ object ActualGemstonePowderDisplay {
 
     private const val ATOMIZED_CRYSTALS_SHARD = "SHARD_THYST"
     private const val ECHO_OF_ATOMIZED_SHARD = "SHARD_IGUANA"
+    private const val ECHO_OF_ECHOES_SHARD = "SHARD_TIAMAT"
+
+    private data class AttributeStats(
+        val level: Int,
+        val baseRate: Double,
+        val actualBonus: Double
+    )
+
+    private data class PetStats(
+        val petName: String?,
+        val petLevel: Int,
+        val petMultiplier: Double
+    )
+
+    private data class DrillStats(
+        val name: String,
+        val basePercent: Int,
+        val upgradePercent: Int,
+        val totalBonusFraction: Double
+    )
 
     private data class MultiplierBreakdown(
-        val atomizedCrystals: Double,
-        val echoOfAtomizedRate: Double,
+        val atomized: AttributeStats,
+        val echoAtomized: AttributeStats,
+        val echoEchoes: AttributeStats,
+        val totalAttributeBonusFraction: Double,
+        val hotmBuffPercent: Int,
+        val drill: DrillStats,
+        val hotmDrillFactor: Double,
         val is2xEventActive: Boolean,
         val isSkymallActive: Boolean,
-        val hotmBuffPercent: Int,
-        val drillMultiplier: Double,
-        val petMultiplier: Double,
-        val petName: String?,
+        val pet: PetStats,
         val totalMultiplier: Double
     )
 
@@ -77,17 +100,62 @@ object ActualGemstonePowderDisplay {
     )
 
 
-    private fun getAtomizedCrystalsBonus(): Double {
-        val level = AttributeShardsData.getActiveLevel(ATOMIZED_CRYSTALS_SHARD)
-        return level / 100.0
+    private fun getAttributeStats(shardName: String, divisor: Double): Pair<Int, Double> {
+        val level = AttributeShardsData.getActiveLevel(shardName)
+        val rate = (level * divisor) / 100.0
+        return level to rate
     }
 
-    private fun getEchoOfAtomizedBonusRate(): Double {
-        val level = AttributeShardsData.getActiveLevel(ECHO_OF_ATOMIZED_SHARD)
-        val bonusPercent = level * 2.0
-        return bonusPercent / 100.0
+    private fun getPetStats(): PetStats {
+        val pet = CurrentPetApi.currentPet ?: return PetStats(null, 0, 1.0)
+        var bonusPercent = 0.0
+
+        if (pet.cleanName == "Mole" && pet.rarity == LorenzRarity.LEGENDARY) {
+            bonusPercent = 0.2 * pet.level
+        }
+
+        val multiplier = 1.0 + (bonusPercent / 100.0)
+
+        return PetStats(
+            petName = pet.cleanName,
+            petLevel = pet.level,
+            petMultiplier = multiplier
+        )
     }
 
+    private fun getDrillStats(): DrillStats {
+        var basePercent = 0
+        var upgradePercent = 0
+        var drillName = "None"
+
+        val heldItem = InventoryUtils.getItemInHand()
+        if (heldItem == null || heldItem.getItemCategoryOrNull() != ItemCategory.DRILL) {
+            return DrillStats(drillName, basePercent, upgradePercent, 1.0)
+        }
+
+        drillName = heldItem.displayName
+
+        val lore = heldItem.getLore()
+
+        lore.forEach { line ->
+            drillBasePowderPattern.matchMatcher(line) {
+                basePercent += group("bonus").toInt()
+            }
+            drillUpgradePowderPattern.matchMatcher(line) {
+                upgradePercent += group("bonus").toInt()
+            }
+        }
+
+        val totalBonusPercent = basePercent + upgradePercent
+        val totalBonusFraction = 1.0 + (totalBonusPercent / 100.0)
+
+        return DrillStats(
+            name = drillName,
+            basePercent = basePercent,
+            upgradePercent = upgradePercent,
+            totalBonusFraction = totalBonusFraction
+        )
+    }
 
     @HandleEvent
     fun onSecondPassed() {
@@ -111,65 +179,48 @@ object ActualGemstonePowderDisplay {
 
 
     private fun calculateMultiplierBreakdown(): MultiplierBreakdown {
+        val (acLevel, acRate) = getAttributeStats(ATOMIZED_CRYSTALS_SHARD, 1.0)
+        val acStats = AttributeStats(acLevel, acRate, acRate)
+        val (eaLevel, eaRate) = getAttributeStats(ECHO_OF_ATOMIZED_SHARD, 2.0)
+        val eaContribution = acStats.actualBonus * eaRate
+        val eaStats = AttributeStats(eaLevel, eaRate, eaContribution)
+        val (eeLevel, eeRate) = getAttributeStats(ECHO_OF_ECHOES_SHARD, 5.0)
+        val eeContribution = eaStats.actualBonus * eeRate
+        val eeStats = AttributeStats(eeLevel, eeRate, eeContribution)
 
-        var additiveBonus = 0.0
+        val totalAttributeBonus = acStats.actualBonus + eaStats.actualBonus + eeStats.actualBonus
+        val totalAttributeBonusFraction = 1.0 + totalAttributeBonus
 
-        val atomizedCrystalsMulti = getAtomizedCrystalsBonus()
-        val echoOfAtomizedRate = getEchoOfAtomizedBonusRate()
-
-        additiveBonus += atomizedCrystalsMulti
-
-        val echoContribution = atomizedCrystalsMulti * echoOfAtomizedRate
-        ChatUtils.debug("[PowderDebug] Echo Contribution: ${echoContribution * 100}%")
-        additiveBonus += echoContribution
-
-        var totalMultiplier = 1.0 + additiveBonus
-
-        val is2xActive = is2xPowderActive
-        if (is2xActive) {
-            totalMultiplier *= 2.0
-        }
-
-        val isSkymallActive = HotmData.SKY_MALL.enabled && HotmApi.skymall == HotmApi.SkymallPerk.EXTRA_POWDER
-        if (isSkymallActive) {
-            totalMultiplier *= 1.15
-        }
-
-
-
+        val drillStats = getDrillStats()
         val hotmBuffPercent = HotmData.POWDER_BUFF.activeLevel
 
-        val drillFractional = getDrillMultiplierFraction()
-        val drillAdditiveBonusPercent = ((drillFractional - 1) * 100.0)
-        ChatUtils.debug("[PowderDebug] drillAdditiveBonusPercent: $drillAdditiveBonusPercent")
+        val additivePercentSum = hotmBuffPercent + (drillStats.totalBonusFraction - 1.0) * 100.0
 
-        val combinedAdditivePercent = hotmBuffPercent + drillAdditiveBonusPercent
+        val hotmDrillFactor = 1.0 + additivePercentSum / 100.0 // NEW FIELD
 
-        ChatUtils.debug("[PowderDebug] combinedAdditivePercent: $combinedAdditivePercent")
+        var currentMultiplier = totalAttributeBonusFraction * hotmDrillFactor
 
-        if (combinedAdditivePercent > 0) {
-            totalMultiplier *= (1.0 + combinedAdditivePercent / 100.0)
-        }
+        val is2xActive = is2xPowderActive
+        if (is2xActive) currentMultiplier *= 2.0
 
-        val petFractional = getPetMultiplierFraction()
-        val petName = if (petFractional > 1.0) {
-            CurrentPetApi.currentPet?.cleanName
-        } else null
+        val isSkymallActive = HotmData.SKY_MALL.enabled && HotmApi.skymall == HotmApi.SkymallPerk.EXTRA_POWDER
+        if (isSkymallActive) currentMultiplier *= 1.15
 
-        totalMultiplier *= petFractional
-
-        ChatUtils.debug("[PowderDebug] Total Multiplier: $totalMultiplier")
+        val petStats = getPetStats()
+        currentMultiplier *= petStats.petMultiplier
 
         return MultiplierBreakdown(
-            atomizedCrystals = atomizedCrystalsMulti,
-            echoOfAtomizedRate = echoOfAtomizedRate,
+            atomized = acStats,
+            echoAtomized = eaStats,
+            echoEchoes = eeStats,
+            totalAttributeBonusFraction = totalAttributeBonusFraction,
+            hotmBuffPercent = hotmBuffPercent,
+            drill = drillStats,
+            hotmDrillFactor = hotmDrillFactor,
             is2xEventActive = is2xActive,
             isSkymallActive = isSkymallActive,
-            hotmBuffPercent = hotmBuffPercent,
-            drillMultiplier = drillFractional,
-            petMultiplier = petFractional,
-            petName = petName,
-            totalMultiplier = totalMultiplier
+            pet = petStats,
+            totalMultiplier = currentMultiplier
         )
     }
 
@@ -177,59 +228,44 @@ object ActualGemstonePowderDisplay {
         val lines = mutableListOf<String>()
 
         fun Double.formatPretty(): String {
-            return if (this % 1.0 == 0.0) {
-                this.toInt().toString()
-            } else {
-                String.format(Locale.US, "%.2f", this).trimEnd('0').trimEnd('.')
-            }
+            return if (this % 1.0 == 0.0) this.toInt().toString()
+            else String.format(Locale.US, "%.2f", this).trimEnd('0').trimEnd('.')
         }
+        fun Double.toPercentStr() = (this * 100.0).formatPretty()
 
         lines.add("§7Base Powder: §e${baseAmount.addSeparators()}")
         lines.add("§7Multiplier Breakdown:")
         lines.add("")
 
-        if (atomizedCrystals > 0.0) {
-            val basePercent = atomizedCrystals * 100
-            val echoPercent = echoOfAtomizedRate * 100
+        val attributeBonusPercent = (totalAttributeBonusFraction - 1.0) * 100.0
+        lines.add("§6§lAdditive Multipliers:")
 
-            val totalAddedPercent = basePercent + (basePercent * echoOfAtomizedRate)
+        lines.add("§7 └ Attributes Bonus: §a+${attributeBonusPercent.formatPretty()}%")
+        if (atomized.level > 0) lines.add("§8   └ Atomized [${atomized.level}]: §2+${atomized.actualBonus.toPercentStr()}%")
+        if (echoAtomized.level > 0) lines.add("§8   └ Echo of Atomized [${echoAtomized.level}]: §2+${echoAtomized.actualBonus.toPercentStr()}%")
+        if (echoEchoes.level > 0) lines.add("§8   └ Echo of Echoes [${echoEchoes.level}]: §2+${echoEchoes.actualBonus.toPercentStr()}%")
 
-            val sb = StringBuilder()
-            sb.append("§7 Attributes: §a+${totalAddedPercent.formatPretty()}%")
+        lines.add("")
 
-            if (echoOfAtomizedRate > 0.0) {
-                val baseStr = basePercent.formatPretty()
-                val echoStr = echoPercent.formatPretty()
-                sb.append(" §7($baseStr% + $echoStr% of $baseStr%)")
+        lines.add("§6§lStacking Multipliers:")
+
+
+        if (hotmDrillFactor > 1.0) {
+            lines.add("§7 └ HOTM Powder Buff: §a+${hotmBuffPercent}%")
+            val drillBonusPercent = (drill.totalBonusFraction - 1.0) * 100.0
+            if (drillBonusPercent > 0.0) {
+                lines.add("§7 └ ${drill.name}: §a+${drillBonusPercent.formatPretty()}%")
+                if (drill.basePercent > 0) lines.add("§8   └ Drill Base: §2+${drill.basePercent}%")
+                if (drill.upgradePercent > 0) lines.add("§8   └ Goblin Egg: §2+${drill.upgradePercent}%")
             }
-
-            val atomizedLevel = AttributeShardsData.getActiveLevel(ATOMIZED_CRYSTALS_SHARD)
-            val echoLevel = AttributeShardsData.getActiveLevel(ECHO_OF_ATOMIZED_SHARD)
-            sb.append(" §8[AC:$atomizedLevel EC:$echoLevel]")
-
-            lines.add(sb.toString())
         }
 
-        if (is2xEventActive) {
-            lines.add("§7 2x Powder Event: §a+100%")
-        }
+        if (is2xEventActive) lines.add("§7 └ 2x Powder Event: §a+100%")
+        if (isSkymallActive) lines.add("§7 └ Sky Mall: §a+15%")
 
-        if (isSkymallActive) {
-            lines.add("§7 Sky Mall: §a+15%")
-        }
-
-        if (hotmBuffPercent > 0) {
-            lines.add("§7 HOTM Powder Buff: §a+$hotmBuffPercent%")
-        }
-
-        val drillBonusPercent = (drillMultiplier - 1.0) * 100.0
-        if (drillBonusPercent > 0.0) {
-            lines.add("§7 Drill: §a+${drillBonusPercent.formatPretty()}%")
-        }
-
-        val petBonusPercent = (petMultiplier - 1.0) * 100.0
-        if (petBonusPercent > 0.0 && petName != null) {
-            lines.add("§7 $petName Pet: §a+${petBonusPercent.formatPretty()}%")
+        val petBonusPercent = (pet.petMultiplier - 1) * 100
+        if (petBonusPercent > 0.0 && pet.petName != null) {
+            lines.add("§7 └ [Lvl ${pet.petLevel}] ${LorenzRarity.LEGENDARY.color.getChatColor()}${pet.petName}: §a+${petBonusPercent.formatPretty()}%")
         }
 
         lines.add("")
@@ -266,40 +302,6 @@ object ActualGemstonePowderDisplay {
         }
     }
 
-
-    private fun getDrillMultiplierFraction(): Double {
-        var bonusPercent = 0
-        val heldItem = InventoryUtils.getItemInHand() ?: return 1.0
-        if(heldItem.getItemCategoryOrNull() != ItemCategory.DRILL) return 1.0
-
-        val lore = heldItem.getLore()
-
-        lore.forEach { line ->
-            drillBasePowderPattern.matchMatcher(line) {
-                bonusPercent += group("bonus").toInt()
-            }
-            drillUpgradePowderPattern.matchMatcher(line) {
-                bonusPercent += group("bonus").toInt()
-            }
-        }
-        val multiplier = 1.0 + (bonusPercent / 100.0)
-        ChatUtils.debug("[PowderDebug] Drill multiplier: $multiplier")
-        return multiplier
-    }
-
-
-    private fun getPetMultiplierFraction(): Double {
-        var bonusPercent = 0.0
-        val pet = CurrentPetApi.currentPet ?: return 1.0
-
-        if (pet.cleanName == "Scatha" && pet.rarity == LorenzRarity.LEGENDARY) {
-            bonusPercent = 0.2 * pet.level
-        }
-
-        val multiplier = 1.0 + (bonusPercent / 100.0)
-        ChatUtils.debug("[PowderDebug] Pet multiplier: $multiplier")
-        return multiplier
-    }
 
     private fun isEnabled() = IslandType.CRYSTAL_HOLLOWS.isCurrent() && config.showEffectivePowder
 }
