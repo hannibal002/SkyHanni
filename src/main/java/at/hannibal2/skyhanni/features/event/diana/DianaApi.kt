44ff553d7042d981513afc388a7b166b92a6ded5
SteinGaming <danialdaryab@gmail.com>
2025-12-02 21:17:35 +0100

    Fix: move diana force enabling into isRitualActive

--- src/main/java/at/hannibal2/skyhanni/features/event/diana/DianaApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/diana/DianaApi.kt
@@ -27,12 +27,12 @@ object DianaApi {
 
     fun hasSpadeInHand() = InventoryUtils.itemInHandId in spades
 
-    private fun isRitualActive() = (Perk.MYTHOLOGICAL_RITUAL.isActive || Perk.PERKPOCALYPSE.isActive) ||
-        SkyHanniMod.feature.dev.debug.assumeMayor.get() == ElectionCandidate.DIANA
+    private fun isRitualActive() = SkyHanniMod.feature.event.diana.forceEnabled || ((Perk.MYTHOLOGICAL_RITUAL.isActive || Perk.PERKPOCALYPSE.isActive) ||
+        SkyHanniMod.feature.dev.debug.assumeMayor.get() == ElectionCandidate.DIANA)
 
     fun hasGriffinPet() = CurrentPetApi.isCurrentPet("Griffin")
 
-    fun isDoingDiana() = SkyHanniMod.feature.event.diana.forceEnabled || (IslandType.HUB.isCurrent() && isRitualActive() && hasSpadeInInventory())
+    fun isDoingDiana() = IslandType.HUB.isCurrent() && isRitualActive() && hasSpadeInInventory()
 
     val ItemStack.isDianaSpade get() = getInternalName() in spades
 
