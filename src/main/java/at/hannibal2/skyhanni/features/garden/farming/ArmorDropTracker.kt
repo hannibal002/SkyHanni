ffacb0834c4ed3229f155ebd03fbc6a9586262dd
Cal <cwolfson58@gmail.com>
2025-12-12 15:36:53 +1100

    /shtestmessage &6&lRARE CROP! &r&f&r&5Fermento &r&b(Armor Set Bonus)

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/ArmorDropTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/ArmorDropTracker.kt
@@ -63,10 +63,10 @@ object ArmorDropTracker {
 
     // todo before merge need to change these colors back to what they are on main, then once greenhouse releases fix it
     enum class ArmorDropType(val dropName: String, chatMessage: String) {
-        CROPIE("§aCropie", "§6§lRARE CROP! §r§f§r§aCropie §r§b(Armor Set Bonus)"),
-        SQUASH("§9Squash", "§6§lRARE CROP! §r§f§r§9Squash §r§b(Armor Set Bonus)"),
-        FERMENTO("§5Fermento", "§6§lRARE CROP! §r§f§r§5Fermento §r§b(Armor Set Bonus)"),
-        HELIANTHUS("§6Helianthus", "§6§lRARE CROP! §r§f§r§6Helianthus §r§b(Armor Set Bonus)"),
+        CROPIE("§aCropie", "§6§lRARE CROP! §r§f§r§aCropie §r§b\\(Armor Set Bonus\\)"),
+        SQUASH("§9Squash", "§6§lRARE CROP! §r§f§r§9Squash §r§b\\(Armor Set Bonus\\)"),
+        FERMENTO("§5Fermento", "§6§lRARE CROP! §r§f§r§5Fermento §r§b\\(Armor Set Bonus\\)"),
+        HELIANTHUS("§6Helianthus", "§6§lRARE CROP! §r§f§r§6Helianthus §r§b\\(Armor Set Bonus\\)"),
         ;
 
         val chatPattern by patternGroup.pattern(
