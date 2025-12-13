5558757c737af756d2aa70ffa33ad0d6cf399248
Luna <lunaynx@gmail.com>
2025-12-13 15:50:15 +0100

    Improvement: Block Break Particle Hider Searchability

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/BlockBreakParticleConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/BlockBreakParticleConfig.kt
@@ -4,12 +4,14 @@ import at.hannibal2.skyhanni.config.FeatureToggle
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+import io.github.notenoughupdates.moulconfig.annotations.SearchTag
 
 class BlockBreakParticleConfig {
 
     @JvmField
     @Expose
     @ConfigOption(name = "Block Break Particles", desc = "Hide Block Break particles.")
+    @SearchTag("breaking")
     @ConfigEditorBoolean
     @FeatureToggle
     var hide: Boolean = false
@@ -17,6 +19,7 @@ class BlockBreakParticleConfig {
     @JvmField
     @Expose
     @ConfigOption(name = "Only on Garden", desc = "Hide Block Break particles only on garden.")
+    @SearchTag("breaking")
     @ConfigEditorBoolean
     @FeatureToggle
     var onlyInGarden: Boolean = false
