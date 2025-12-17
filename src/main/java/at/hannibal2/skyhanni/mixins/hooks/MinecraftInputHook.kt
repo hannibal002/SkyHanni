222ca4f449009421f03b8bb3e0b75d699905d704
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2025-12-17 00:28:54 +0000

    oops

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/MinecraftInputHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/MinecraftInputHook.kt
@@ -99,7 +99,7 @@ object MinecraftInputHook {
         //#if MC < 1.21
         this.blockPos
     //#else
-    //$$ (blockHitResult as BlockHitResult).blockPos
+    //$$ (this as BlockHitResult).blockPos
     //#endif
 
     private fun MovingObjectPosition.getEntityCompat() =
