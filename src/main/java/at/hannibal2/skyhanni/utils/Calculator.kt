c39c06aa10573aab16a65af9a4d59cbb6295eb66
nopo <nopotheemail@gmail.com>
2025-12-21 23:29:30 +1100

    add neu constants to calc (short for calculator)

--- src/main/java/at/hannibal2/skyhanni/utils/Calculator.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/Calculator.kt
@@ -5,20 +5,45 @@ import at.hannibal2.skyhanni.config.commands.CommandCategory
 import at.hannibal2.skyhanni.config.commands.CommandRegistrationEvent
 import at.hannibal2.skyhanni.config.commands.brigadier.BrigadierArguments
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import com.notkamui.keval.Keval
+import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
+import com.notkamui.keval.keval
 
 @SkyHanniModule
 object Calculator {
 
     fun calculateOrNull(input: String): Double? {
-        return runCatching { Keval.eval(input) }.getOrNull()
-    }
+        return runCatching {
+            input.keval {
+                // taken so kindly from skyocean
+                includeDefault()
 
-    private fun command(expr: String) = ChatUtils.chat(
-        calculateOrNull(expr)?.let { result ->
-            "$expr = §a$result"
-        } ?: "§cFailed to calculate $expr"
-    )
+                constant {
+                    name = "s"
+                    value = 64.0
+                }
+                constant {
+                    name = "e"
+                    value = 160.0
+                }
+                constant {
+                    name = "k"
+                    value = 1000.0
+                }
+                constant {
+                    name = "m"
+                    value = 1_000_000.0
+                }
+                constant {
+                    name = "b"
+                    value = 1_000_000_000.0
+                }
+                constant {
+                    name = "t"
+                    value = 1_000_000_000_000.0
+                }
+            }
+        }.getOrNull()
+    }
 
     @HandleEvent
     fun onCommandRegistration(event: CommandRegistrationEvent) {
@@ -26,8 +51,11 @@ object Calculator {
             category = CommandCategory.USERS_ACTIVE
             description = "Calculates a math expression"
 
-            arg("expr", BrigadierArguments.greedyString()) { expr ->
-                callback { command(getArg(expr)) }
+            argCallback("expr", BrigadierArguments.greedyString()) { expr ->
+                ChatUtils.chat(calculateOrNull(expr)?.let { result ->
+                    "$expr = §a${result.addSeparators()}"
+                } ?: "§cFailed to calculate $expr")
+
             }
         }
     }
