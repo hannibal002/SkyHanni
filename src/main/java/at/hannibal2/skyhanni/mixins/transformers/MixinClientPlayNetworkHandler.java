5443d490ad9f1436f9b2ec996092f314b4f5f9cf
nopo <nopotheemail@gmail.com>
2025-12-21 22:56:02 +1100

    make into event

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinClientPlayNetworkHandler.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinClientPlayNetworkHandler.java
@@ -1,15 +1,13 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
+import at.hannibal2.skyhanni.events.ParticleChangeEvent;
 import at.hannibal2.skyhanni.events.entity.EntityEquipmentChangeEvent;
-import at.hannibal2.skyhanni.features.misc.ColorParticleFix;
 import at.hannibal2.skyhanni.features.misc.CurrentPing;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.core.particles.ColorParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket;
-import net.minecraft.util.ARGB;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket;
@@ -37,12 +35,9 @@ public class MixinClientPlayNetworkHandler {
     }
 
     @ModifyArg(method = "handleParticleEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;addParticle(Lnet/minecraft/core/particles/ParticleOptions;ZZDDDDDD)V"))
-    public ParticleOptions a(ParticleOptions particleOptions, @Local(argsOnly = true)ClientboundLevelParticlesPacket packet) {
-        if (particleOptions instanceof ColorParticleOption options) {
-            if (!ColorParticleFix.INSTANCE.isEnabled()) return particleOptions;
-            options.color = ARGB.colorFromFloat(options.getAlpha(), (1-packet.getXDist()), (1-packet.getZDist()), (1-packet.getYDist()));
-            return options;
-        }
-        return particleOptions;
+    public ParticleOptions postParticleChangeEvent(ParticleOptions particleOptions, @Local(argsOnly = true) ClientboundLevelParticlesPacket packet) {
+        ParticleChangeEvent particleChangeEvent = new ParticleChangeEvent(particleOptions, packet);
+        particleChangeEvent.post();
+        return particleChangeEvent.getParticleOptions();
     }
 }
