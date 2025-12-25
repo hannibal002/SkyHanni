abdf683fb496b5f09afeff75569f1373e87a9903
Haralds Bukavs <haralds.bukavs@gmail.com>
2025-12-25 10:43:12 +0200

    Move the pitches outside of the function

--- src/main/java/at/hannibal2/skyhanni/features/fishing/MuteBanshee.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/MuteBanshee.kt
@@ -9,31 +9,32 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 @SkyHanniModule
 object MuteBanshee {
 
+    val bansheePitches = listOf(
+        0.1904762f,
+        0.20634921f,
+        0.22222222f,
+        0.23809524f,
+        0.25396827f,
+        0.26984128f,
+        0.2857143f,
+        0.3015873f,
+        0.31746033f,
+        0.33333334f,
+        0.34920636f,
+        0.36507937f,
+        0.3809524f,
+        0.3968254f,
+        0.41269842f,
+        0.42857143f,
+        0.44444445f,
+        0.46031746f,
+        0.47619048f,
+        0.4920635f
+    )
+
     @HandleEvent(onlyOnIsland = IslandType.BACKWATER_BAYOU)
     fun onSound(event: PlaySoundEvent) {
         if (!SkyHanniMod.feature.fishing.muteBanshee) return
-        if (event.pitch in listOf(
-                0.1904762f,
-                0.20634921f,
-                0.22222222f,
-                0.23809524f,
-                0.25396827f,
-                0.26984128f,
-                0.2857143f,
-                0.3015873f,
-                0.31746033f,
-                0.33333334f,
-                0.34920636f,
-                0.36507937f,
-                0.3809524f,
-                0.3968254f,
-                0.41269842f,
-                0.42857143f,
-                0.44444445f,
-                0.46031746f,
-                0.47619048f,
-                0.4920635f
-            ) && event.soundName == "entity.ghast.warn"
-        )event.cancel()
+        if (event.pitch in bansheePitches && event.soundName == "entity.ghast.warn") event.cancel()
     }
 }
