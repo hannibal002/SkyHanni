600b5db1354f214ffb8aae7cfdddeefda7d8b400
nopo <nopotheemail@gmail.com>
2025-12-28 21:23:22 +1100

    fix build again (why does intellij not flag these as errors...?)

--- src/main/java/at/hannibal2/skyhanni/features/minion/MinionUpgradeHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/minion/MinionUpgradeHelper.kt
@@ -15,7 +15,7 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ItemPriceUtils.getPriceOrNull
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.ItemUtils.repoItemName
-import at.hannibal2.skyhanni.utils.ItemUtils.setLore
+import at.hannibal2.skyhanni.utils.ItemUtils.setLoreString
 import at.hannibal2.skyhanni.utils.NeuInternalName
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.NumberUtil.shortFormat
@@ -99,7 +99,7 @@ object MinionUpgradeHelper {
 
     private fun createDisplayItem(internalName: NeuInternalName): ItemStack {
         val lore = createLore(internalName)
-        return ItemStack(Blocks.DIAMOND_BLOCK).setLore(lore).setCustomItemName("§bGet Required Items")
+        return ItemStack(Blocks.DIAMOND_BLOCK).setLoreString(lore).setCustomItemName("§bGet Required Items")
     }
 
     private fun createLore(internalName: NeuInternalName): List<String> {
