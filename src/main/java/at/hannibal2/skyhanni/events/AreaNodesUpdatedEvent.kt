792ba0235e4706a13201f5645507505f138c93c5
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-01-03 03:07:51 +0100

    cleanup

--- src/main/java/at/hannibal2/skyhanni/events/AreaNodesUpdatedEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/AreaNodesUpdatedEvent.kt
@@ -1,10 +1,9 @@
 package at.hannibal2.skyhanni.events
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-import at.hannibal2.skyhanni.features.misc.pathfind.AreaNode
 
 /**
  * Fired when area nodes are recalculated after player movement, graph reload, or config changes.
  * Nodes are sorted by distance, closest first.
  */
-class AreaNodesUpdatedEvent() : SkyHanniEvent()
+class AreaNodesUpdatedEvent : SkyHanniEvent()
