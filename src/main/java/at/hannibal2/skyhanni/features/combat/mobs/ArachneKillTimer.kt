dbabb90565d00b8d6d64b0681a726e242167c062
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2026-01-03 18:21:22 +0000

    a bit safer ig

--- src/main/java/at/hannibal2/skyhanni/features/combat/mobs/ArachneKillTimer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/mobs/ArachneKillTimer.kt
@@ -41,7 +41,7 @@ object ArachneKillTimer {
      */
     private val arachneDeathPattern by patternGroup.pattern(
         "dead",
-        " +§r§6§lARACHNE DOWN!",
+        ".*§r§6§lARACHNE DOWN!",
     )
 
     /**
@@ -49,7 +49,7 @@ object ArachneKillTimer {
      */
     private val arachneDamagePattern by patternGroup.pattern(
         "damage",
-        " +§r§eYour Damage: §r§a[0-9,]+ §r§7\\(Position #[0-9,]+\\)",
+        ".*§r§eYour Damage: §r§a[0-9,]+ §r§7\\(Position #[0-9,]+\\)",
     )
 
     private var arachneSpawnedTime = SimpleTimeMark.farPast()
