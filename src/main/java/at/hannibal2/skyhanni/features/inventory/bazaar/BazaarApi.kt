5c6467a12f7974622544c0ab7abbd0af160e5c8c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-01-07 12:02:49 +0100

    small cleanup v2

--- src/main/java/at/hannibal2/skyhanni/features/inventory/bazaar/BazaarApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/bazaar/BazaarApi.kt
@@ -289,13 +289,12 @@ object BazaarApi {
 
     /**
      * Gets the Sum of Coins you can get when using the specified sell type to sell the item.
-     *
      * For example if you want to instant sell 71680 enchanted coal from sack you cant use top offer only.
-     *
-     * The best offers may be inflated and you are selling all the items so you must use the prices in each order as such.
+     * The best offers may be inflated, and you are selling all the items so you must use the prices in each order as such.
      */
     fun calculatePriceOffAvailableOrders(
-        item: NeuInternalName, count: Long,
+        item: NeuInternalName,
+        count: Long,
         priceSource: SimpleTransactionType,
     ): Double? {
         val bazaarData = item.getBazaarData()?.product ?: return null
