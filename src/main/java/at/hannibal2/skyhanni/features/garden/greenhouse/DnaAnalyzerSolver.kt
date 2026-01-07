d3a6294c4730f0ef9174ac0f3602084d37fe680c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-01-07 11:44:10 +0100

    Revert "small cleanup"
    This reverts commit fc3ecd5979f747a139ca595aef9721932e4128c4.

--- src/main/java/at/hannibal2/skyhanni/features/garden/greenhouse/DnaAnalyzerSolver.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/greenhouse/DnaAnalyzerSolver.kt
@@ -59,7 +59,7 @@ object DnaAnalyzerSolver {
         if (!isEnabled()) return
         val initialBoard = MutableList(9) { MutableList(4) { Colors.GREEN } }
         for ((i, stack) in event.inventoryItems) {
-            if (i !in 9..44) continue
+            if (i < 9 || i > 44) continue
             val row = (i / 9) - 1
             val column = i % 9
             initialBoard[column][row] = stack.toColor()
@@ -67,8 +67,8 @@ object DnaAnalyzerSolver {
         currentBoard = DnaBoard(initialBoard)
     }
 
-    @HandleEvent(GuiContainerEvent.BackgroundDrawnEvent::class, onlyOnSkyblock = true)
-    fun onBackgroundDrawn() {
+    @HandleEvent(onlyOnSkyblock = true)
+    fun onBackgroundDrawn(event: GuiContainerEvent.BackgroundDrawnEvent) {
         if (!isEnabled()) return
         val board = currentBoard ?: return
 
@@ -239,13 +239,16 @@ object DnaAnalyzerSolver {
         companion object {
             fun ItemStack.toColor(): Colors {
                 val name = this.hoverName.formattedTextCompatLeadingWhiteLessResets()
-                return when {
-                    name.startsWith("§cDNA") -> RED
-                    name.startsWith("§eDNA") -> YELLOW
-                    name.startsWith("§9DNA") -> BLUE
-                    name.startsWith("§aDNA") -> GREEN
-                    else -> ErrorManager.skyHanniError("unknown color", "name" to name)
+                if (name.startsWith("§cDNA")) {
+                    return RED
+                } else if (name.startsWith("§eDNA")) {
+                    return YELLOW
+                } else if (name.startsWith("§9DNA")) {
+                    return BLUE
+                } else if (name.startsWith("§aDNA")) {
+                    return GREEN
                 }
+                ErrorManager.skyHanniError("unknown color", "name" to name)
             }
         }
     }
