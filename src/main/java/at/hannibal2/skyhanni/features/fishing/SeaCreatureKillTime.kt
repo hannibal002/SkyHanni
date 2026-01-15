14d1ad60fb28f3a306c76caa7fc8f0c99071b9d0
nopo <nopotheemail@gmail.com>
2026-01-15 21:09:58 +1100

    component Builder

--- src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureKillTime.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureKillTime.kt
@@ -5,15 +5,16 @@ import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.fishing.SeaCreatureEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.ExtendedChatColor
 import at.hannibal2.skyhanni.utils.TimeUtils.format
-import net.minecraft.network.chat.Component
+import at.hannibal2.skyhanni.utils.compat.append
+import at.hannibal2.skyhanni.utils.compat.componentBuilder
+import at.hannibal2.skyhanni.utils.compat.withColor
+import net.minecraft.ChatFormatting
 
 @SkyHanniModule
 object SeaCreatureKillTime {
     private val config get() = SkyHanniMod.feature.fishing.seaCreatureKillTimer
 
-    @Suppress("MaxLineLength")
     @HandleEvent
     fun onSeaCreatureDeath(event: SeaCreatureEvent.Death) {
         if (!config) return
@@ -21,14 +22,32 @@ object SeaCreatureKillTime {
         val seaCreature = event.seaCreature
         val time = seaCreature.spawnTime.passedSince()
         if (event.seenDeath) {
-            ChatUtils.chat(Component.literal("${seaCreature.displayName}§e took ").append(ExtendedChatColor("#FFC600").asText(time.format(showMilliSeconds = true))).append("§e to die."))
-
+            ChatUtils.chat(
+                componentBuilder {
+                    append(seaCreature.displayName)
+                    append(" took ")
+                    append(time.format(showMilliSeconds = true)) {
+                        withColor("#FFC600")
+                    }
+                    append(" to die.")
+                }
+            )
         } else {
             val minTime = seaCreature.lastUpdate.passedSince()
-            val message = "${seaCreature.displayName}§e took between " +
-                "§b${minTime.format(showMilliSeconds = true)} §eand " +
-                "§b${time.format(showMilliSeconds = true)}§e to die."
-            ChatUtils.chat(message)
+            ChatUtils.chat(
+                componentBuilder {
+                    append(seaCreature.displayName)
+                    append(" took between ")
+                    append(minTime.format(showMilliSeconds = true)) {
+                        withColor(ChatFormatting.AQUA)
+                    }
+                    append(" and ")
+                    append(time.format(showMilliSeconds = true)) {
+                        withColor(ChatFormatting.AQUA)
+                    }
+                    append(" to die.")
+                }
+            )
         }
     }
 }
