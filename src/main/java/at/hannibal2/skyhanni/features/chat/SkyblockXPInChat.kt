288433ce5336e4b5268919319e379ad5851ddc40
nopo <nopotheemail@gmail.com>
2026-01-17 16:16:24 +1100

    make every message send as a component

--- src/main/java/at/hannibal2/skyhanni/features/chat/SkyblockXPInChat.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/SkyblockXPInChat.kt
@@ -6,7 +6,6 @@ import at.hannibal2.skyhanni.data.ActionBarStatsData
 import at.hannibal2.skyhanni.events.ActionBarValueUpdateEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.chat.TextHelper.asComponent
 
 @SkyHanniModule
 object SkyblockXPInChat {
