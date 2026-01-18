1415baab9308179288b5b107606f37646c22fc0f
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-01-18 22:46:07 +0000

    Tested Via Timer PR, fixed Inconsistencies in Detection

--- src/main/java/at/hannibal2/skyhanni/features/combat/cocoon/CocoonAPI.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/cocoon/CocoonAPI.kt
@@ -2,19 +2,21 @@ package at.hannibal2.skyhanni.features.combat.cocoon
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.mob.Mob
+import at.hannibal2.skyhanni.data.mob.MobData.skyblockMobs
 import at.hannibal2.skyhanni.events.CheckRenderEntityEvent
-import at.hannibal2.skyhanni.events.MobEvent
 import at.hannibal2.skyhanni.events.combat.CocoonSpawnEvent
+import at.hannibal2.skyhanni.events.entity.EntityLeaveWorldEvent
 import at.hannibal2.skyhanni.events.minecraft.WorldChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.EntityUtils.wearingSkullTexture
+import at.hannibal2.skyhanni.utils.LorenzLogger
 import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.ServerTimeMark
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SkullTextureHolder
-import at.hannibal2.skyhanni.utils.collection.TimeLimitedCache
 import at.hannibal2.skyhanni.utils.collection.TimeLimitedSet
 import at.hannibal2.skyhanni.utils.getLorenzVec
-import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.decoration.ArmorStand
 import kotlin.time.Duration.Companion.seconds
 
@@ -24,44 +26,60 @@ object CocoonAPI {
     private val COCOON_SKULL_TEXTURE by lazy { SkullTextureHolder.getTexture("RIFT_LARVA") }
 
     private val recentCocoonMobs: TimeLimitedSet<CocoonMob> = TimeLimitedSet(8.seconds)
-    private val mobRecentDeaths: TimeLimitedCache<Mob, SimpleTimeMark> = TimeLimitedCache(1.seconds)
+    private val recentMobs: TimeLimitedSet<Mob> = TimeLimitedSet(1.seconds)
+
+    val logger: LorenzLogger = LorenzLogger("Combat/Cocoon")
 
     data class CocoonMob(
         val mob: Mob,
         val coordinates: LorenzVec,
         val spawnTime: SimpleTimeMark,
+        val spawnTimeServer: ServerTimeMark,
         val cocoonID: Int,
     )
 
     @HandleEvent
-    fun onCheckRender(event: CheckRenderEntityEvent<Entity>) {
-        val entity = event.entity as? ArmorStand ?: return
+    fun onCheckRenderEntityEvent(event: CheckRenderEntityEvent<ArmorStand>) {
+        val entity = event.entity
         if (recentCocoonMobs.any { (it.coordinates.distanceSqIgnoreY(entity.getLorenzVec()) < 0.5 || it.cocoonID == event.entity.id) }) return
         if (entity.wearingSkullTexture(COCOON_SKULL_TEXTURE)) {
             val position = entity.getLorenzVec()
             val mob = getCocoonMob(position) ?: return
             val id = entity.id
-            val cocoon = CocoonMob(mob, position, SimpleTimeMark.now(), id)
+            val cocoon = CocoonMob(mob, position, SimpleTimeMark.now(), ServerTimeMark.now(), id)
             recentCocoonMobs.add(cocoon)
+            ChatUtils.debug("${cocoon.mob.name}  Cocoon (${cocoon.cocoonID} Entered List")
+            logger.log("${cocoon.mob.name} Cocoon (${cocoon.cocoonID} Entered List")
             CocoonSpawnEvent(cocoon).post()
         }
     }
 
-    @HandleEvent(onlyOnSkyblock = true)
-    fun onSkyblockMobDespawn(event: MobEvent.DeSpawn.SkyblockMob) {
-        mobRecentDeaths[event.mob] = SimpleTimeMark.now()
+    @HandleEvent
+    fun onEntityLeaveWorld(event: EntityLeaveWorldEvent<ArmorStand>) {
+        val cocoon = recentCocoonMobs.firstOrNull { it.cocoonID == event.entity.id } ?: return
+        ChatUtils.debug("${cocoon.mob.name}  Cocoon (${cocoon.cocoonID}) Left World at ${cocoon.spawnTime.passedSince()} Simple and ${cocoon.spawnTimeServer.passedSince()} Server Time Mark")
+        logger.log("${cocoon.mob.name} (Type: ${cocoon.mob.mobType}) Cocoon (${cocoon.cocoonID}) Left World at ${cocoon.spawnTime.passedSince()} Simple and ${cocoon.spawnTimeServer.passedSince()} Server Time Mark")
+    }
+
+    @HandleEvent
+    fun onTick() {
+        skyblockMobs.forEach {
+            if (!recentMobs.contains(it)) {
+                recentMobs.add(it)
+            }
+        }
     }
 
     @HandleEvent(onlyOnSkyblock = true)
     fun onWorldChange(event: WorldChangeEvent) {
-        mobRecentDeaths.clear()
+        recentMobs.clear()
         recentCocoonMobs.clear()
     }
 
     private fun getCocoonMob(cocoonVector: LorenzVec): Mob? {
-        val mob = mobRecentDeaths.minByOrNull { it.key.baseEntity.getLorenzVec().distanceIgnoreY(cocoonVector) } ?: return null
-        if (mob.key.baseEntity.getLorenzVec().distanceSqOnlyY(cocoonVector) > 4.0) return null
-        return mob.key
+        val mob = recentMobs.minByOrNull { it.baseEntity.getLorenzVec().distanceIgnoreY(cocoonVector) } ?: return null
+        if (mob.baseEntity.getLorenzVec().distanceSqOnlyY(cocoonVector) > 4.0) return null
+        return mob
     }
 
 }
