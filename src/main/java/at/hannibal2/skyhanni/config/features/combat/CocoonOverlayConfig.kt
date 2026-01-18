ac7d1489a66457f77393ae726c38e95175711e52
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-01-18 06:40:39 +0000

    Cocoon Mob Name & Overlay

---
+++ src/main/java/at/hannibal2/skyhanni/config/features/combat/CocoonOverlayConfig.kt
@@ -0,0 +1,22 @@
+package at.hannibal2.skyhanni.config.features.combat
+
+import at.hannibal2.skyhanni.config.FeatureToggle
+import com.google.gson.annotations.Expose
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+
+class CocoonOverlayConfig {
+
+    @Expose
+    @ConfigOption(name = "Show Timer", desc = "Shows Time Left till Cocoon Hatches")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    var showCocoonTimerTillHatch: Boolean = false
+
+    @Expose
+    @ConfigOption(name = "Show Mob Name", desc = "Shows Mob Contained Within Cocoon's Name")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    var showCocoonContainedMobName: Boolean = false
+
+}
---
+++ src/main/java/at/hannibal2/skyhanni/features/combat/cocoon/CocoonOverlay.kt
@@ -0,0 +1,34 @@
+package at.hannibal2.skyhanni.features.combat.cocoon
+
+import at.hannibal2.skyhanni.SkyHanniMod
+import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.events.combat.CocoonSpawnEvent
+import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.TimeUtils.format
+import at.hannibal2.skyhanni.utils.collection.TimeLimitedSet
+import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawDynamicText
+import kotlin.time.Duration.Companion.seconds
+
+@SkyHanniModule
+object CocoonOverlay {
+    private val config get() = SkyHanniMod.feature.combat.cocoonOverlay
+
+    private val currentCocoons: TimeLimitedSet<CocoonAPI.CocoonMob> = TimeLimitedSet(8.seconds)
+
+    private val LIFETIME = 6.seconds
+
+    @HandleEvent
+    fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
+        for (cocoon in currentCocoons) {
+            val timeTillSpawn = (LIFETIME - cocoon.spawnTime.passedSince()).format(showMilliSeconds = true)
+            if (config.showCocoonContainedMobName) event.drawDynamicText(cocoon.coordinates, cocoon.mob.name, 2.0, yOff = 0f)
+            if (config.showCocoonTimerTillHatch) event.drawDynamicText(cocoon.coordinates, timeTillSpawn, 2.0, yOff = -20f)
+        }
+    }
+
+    @HandleEvent
+    fun onCocoon(event: CocoonSpawnEvent) {
+        currentCocoons.add(event.cocoonMob)
+    }
+}
