1fe050eb7242fd603768c6dcfff1d60898930a22
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-01-21 02:35:41 +0000

    Move expectedLifetime & note about where 6.4s comes from

--- src/main/java/at/hannibal2/skyhanni/features/combat/cocoon/CocoonOverlay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/cocoon/CocoonOverlay.kt
@@ -4,22 +4,20 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.minecraft.SkyHanniRenderWorldEvent
 import at.hannibal2.skyhanni.features.combat.cocoon.CocoonAPI.existingCocoons
+import at.hannibal2.skyhanni.features.combat.cocoon.CocoonAPI.expectedLifetime
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.TimeUtils.format
 import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawDynamicText
-import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object CocoonOverlay {
     private val config get() = SkyHanniMod.feature.combat.cocoonOverlay
 
 
-    private val LIFETIME = 6.4.seconds
-
     @HandleEvent
     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
         for (cocoon in existingCocoons) {
-            val timeTillSpawn = (LIFETIME - cocoon.spawnTime.passedSince()).format(showMilliSeconds = true)
+            val timeTillSpawn = (expectedLifetime - cocoon.spawnTime.passedSince()).format(showMilliSeconds = true)
             if (config.showCocoonContainedMobName) event.drawDynamicText(
                 cocoon.coordinates,
                 cocoon.mob.name,
