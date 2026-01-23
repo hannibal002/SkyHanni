632f70bd71d216c83e5fc72a44b5afb336776f93
Luna <lunaynx@gmail.com>
2026-01-23 01:04:52 +0100

    Fix

--- src/main/java/at/hannibal2/skyhanni/utils/FakePlayer.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/FakePlayer.kt
@@ -1,16 +1,19 @@
 package at.hannibal2.skyhanni.utils
 
 import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
-import net.minecraft.client.Minecraft
-import net.minecraft.client.entity.ClientMannequin
-import net.minecraft.client.player.RemotePlayer
 import net.minecraft.client.resources.DefaultPlayerSkin
 import net.minecraft.client.resources.PlayerSkin
 import net.minecraft.world.entity.player.PlayerModelPart
 import net.minecraft.world.scores.PlayerTeam
+//? < 1.21.9 {
+import net.minecraft.client.player.RemotePlayer
+//?} else {
+/*import net.minecraft.client.Minecraft
+import net.minecraft.client.entity.ClientMannequin
+*///?}
 
 //? < 1.21.9 {
-class FakePlayer() : RemotePlayer(MinecraftCompat.localWorld, MinecraftCompat.localPlayer.gameProfile) { //
+class FakePlayer() : RemotePlayer(MinecraftCompat.localWorld, MinecraftCompat.localPlayer.gameProfile) {
 //?} else {
 /*class FakePlayer: ClientMannequin(
     MinecraftCompat.localWorld,
