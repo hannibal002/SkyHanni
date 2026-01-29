ca608a6ab9a7efbc8e3a0c75c8a7a3b1e18847bd
HacktheTime <l4bg0jb7@duck.com>
2026-01-29 15:48:47 +0100

    fix for higher quantity crafts like bait.

--- src/main/java/at/hannibal2/skyhanni/features/inventory/SuperCraftingInventory.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/SuperCraftingInventory.kt
@@ -16,6 +16,7 @@ import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
 import at.hannibal2.skyhanni.utils.ItemUtils.getSingleLineLore
 import at.hannibal2.skyhanni.utils.KeyboardManager
+import at.hannibal2.skyhanni.utils.MobUtils.mob
 import at.hannibal2.skyhanni.utils.NumberUtil.formatLongOrNull
 import at.hannibal2.skyhanni.utils.PrimitiveItemStack
 import at.hannibal2.skyhanni.utils.PrimitiveItemStack.Companion.toPrimitiveStackOrNull
@@ -25,6 +26,7 @@ import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.SoundUtils
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPatternGroup
+import net.minecraft.client.Minecraft
 import net.minecraft.world.inventory.Slot
 import kotlin.time.Duration.Companion.seconds
 
@@ -89,8 +91,9 @@ object SuperCraftingInventory {
         if (event.slotId != PICKAXE_SLOT) return
         val slots = InventoryUtils.getItemsInOpenChestWithNull()
         val craftingAmount = getSuperCraftingCount(slots) ?: return
-        val profit = getProfit(slots, craftingAmount) ?: return
-        val maxCraftingAmount = getSuperCraftingMaxCount(slots, craftingAmount)
+        val craftMultiplier = getResultItem(slots).amount
+        val profit = getProfit(slots, craftingAmount, craftMultiplier) ?: return
+        val maxCraftingAmount = getSuperCraftingMaxCount(slots, craftingAmount, craftMultiplier)
         if (!blockWasteClick(profit, craftingAmount, maxCraftingAmount)) return
         SoundUtils.playErrorSound()
         val diff = (-profit).formatChatCoins()
@@ -108,27 +111,28 @@ object SuperCraftingInventory {
         event.cancel()
     }
 
-    private fun getSuperCraftingMaxCount(slots: List<Slot>, craftingAmount: Long) = slots[PICKAXE_SLOT].item.getLore()
-        .mapNotNull { calculateMaxPossible(it, craftingAmount) }
+    private fun getSuperCraftingMaxCount(slots: List<Slot>, craftingAmount: Long, craftMultiplier: Int) = slots[PICKAXE_SLOT].item.getLore()
+        .mapNotNull { calculateMaxPossible(it, craftingAmount, craftMultiplier) }
         .minOrNull() ?: ErrorManager.skyHanniError(
         "Super Crafting resource line not found",
         "lore" to slots.map { slot -> slot.item.getLore().map { line -> line.removeColor() } },
     )
 
-    private fun calculateMaxPossible(string: String, craftingAmount: Long) = craftingResourcePattern.matchMatcher(string.removeColor()) {
-        val owned = groupOrNull("owned")?.formatLongOrNull() ?: return null
-        val used = groupOrNull("used")?.formatLongOrNull() ?: return null
-        if (used == 0L || owned == 0L) return null
-        val matsPerCraft = used / craftingAmount
-        if (matsPerCraft == 0L) return null
-        owned / matsPerCraft
-    }
+    private fun calculateMaxPossible(string: String, craftingAmount: Long, craftMultiplier: Int) =
+        craftingResourcePattern.matchMatcher(string.removeColor()) {
+            val owned = groupOrNull("owned")?.formatLongOrNull() ?: return null
+            val used = groupOrNull("used")?.formatLongOrNull() ?: return null
+            if (used == 0L || owned == 0L) return null
+            val matsPerCraft = used / (craftingAmount / craftMultiplier)
+            if (matsPerCraft == 0L) return null
+            owned / matsPerCraft
+        }
 
-    private fun getProfit(slots: List<Slot>, craftingAmount: Long): Double? {
+    private fun getProfit(slots: List<Slot>, craftingAmount: Long, craftMultiplier: Int): Double? {
         val materials = getRecipeMaterials(slots)
-        val resultItem = getResultItem(slots)
 
-        val recipeMultiplier = resultItem.amount
+        val recipeMultiplier = craftMultiplier
+        val resultItem = getResultItem(slots)
         if (recipeMultiplier == 0) ErrorManager.skyHanniError(
             "Result item amount is 0",
             "item" to resultItem,
