9a6817d3789078d23a5d8611907e0eeaeb6656a2
nopo <nopotheemail@gmail.com>
2026-01-31 16:21:03 +1100

    Fix: bundle packets being ignored

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinClientConnection.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinClientConnection.java
@@ -2,8 +2,10 @@ package at.hannibal2.skyhanni.mixins.transformers;
 
 import at.hannibal2.skyhanni.events.minecraft.packet.PacketReceivedEvent;
 import at.hannibal2.skyhanni.events.minecraft.packet.PacketSentEvent;
+import at.hannibal2.skyhanni.utils.ChatUtils;
 import net.minecraft.network.Connection;
 import net.minecraft.network.PacketListener;
+import net.minecraft.network.protocol.BundlePacket;
 import net.minecraft.network.protocol.Packet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,8 +18,19 @@ public class MixinClientConnection {
 
     @Inject(method = "genericsFtw", at = @At(value = "HEAD"), cancellable = true)
     private static void handlePacket$Inject$HEAD(Packet<?> packet, PacketListener listener, CallbackInfo ci) {
-        if (new PacketReceivedEvent(packet).post()) {
-            ci.cancel();
+        if (packet instanceof BundlePacket<?> bundle) {
+            for (Packet<?> subPacket : bundle.subPackets()) {
+                if (new PacketReceivedEvent(subPacket).post()) {
+                    // not sure what to do here
+                    // dont want to cancel the whole bundle
+                    ChatUtils.INSTANCE.debug("Bundle Packet attempt cancel", false);
+                    System.out.println(subPacket);
+                }
+            }
+        } else {
+            if (new PacketReceivedEvent(packet).post()) {
+                ci.cancel();
+            }
         }
     }
 
