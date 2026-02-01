ea8f1e7b214fc3d7eb1fca2cf08546d6b38b18ae
nopo <nopotheemail@gmail.com>
2026-02-01 12:26:23 +1100

    detekt

--- src/main/java/at/hannibal2/skyhanni/data/garden/FarmingWeightData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/garden/FarmingWeightData.kt
@@ -24,7 +24,6 @@ import at.hannibal2.skyhanni.events.garden.farming.CropCollectionAddEvent
 import at.hannibal2.skyhanni.events.minecraft.SkyHanniTickEvent
 import at.hannibal2.skyhanni.features.garden.CropCollectionType
 import at.hannibal2.skyhanni.features.garden.CropType
-import at.hannibal2.skyhanni.features.garden.GardenApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.EnumUtils.isAnyOf
