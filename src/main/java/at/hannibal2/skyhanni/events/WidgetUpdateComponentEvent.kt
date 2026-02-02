d7848d5f7e25720f22aa36f4fc07611ae5897b18
nopo <nopotheemail@gmail.com>
2026-02-02 15:39:40 +1100

    add new component versions of tab list events

--- src/main/java/at/hannibal2/skyhanni/events/WidgetUpdateComponentEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/WidgetUpdateComponentEvent.kt
@@ -1,20 +1,20 @@
 package at.hannibal2.skyhanni.events
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
-import at.hannibal2.skyhanni.data.model.TabWidget
+import at.hannibal2.skyhanni.data.model.TabWidgetComponent
 import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 import at.hannibal2.skyhanni.utils.EnumUtils.isAnyOf
+import net.minecraft.network.chat.Component
 
 /** The events get send on change of the widget and on island switch */
 @PrimaryFunction("onWidgetUpdate")
-@Deprecated("use the component version", ReplaceWith("WidgetUpdateComponentEvent"))
-class WidgetUpdateEvent(
-    val widget: TabWidget,
-    val lines: List<String>,
+class WidgetUpdateComponentEvent(
+    val widget: TabWidgetComponent,
+    val lines: List<Component>,
 ) : SkyHanniEvent() {
 
-    fun isWidget(widgetType: TabWidget) = widget == widgetType
-    fun isWidget(vararg widgetType: TabWidget) = widget.isAnyOf(*widgetType)
+    fun isWidget(widgetType: TabWidgetComponent) = widget == widgetType
+    fun isWidget(vararg widgetType: TabWidgetComponent) = widget.isAnyOf(*widgetType)
 
     fun isClear() = lines.isEmpty()
 }
