7007f3d29f9f96b4148658fe17ab4768df5d8f1b
Bloxigus <72960305+Bloxigus@users.noreply.github.com>
2026-02-05 12:32:27 +1030

    oops forgot about this

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinHeadFeatureRenderer.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/renderer/MixinHeadFeatureRenderer.java
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.mixins.hooks.HideArmorHookKt;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.client.model.object.skull.SkullModelBase;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.core.Direction;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,6 +22,14 @@ public class MixinHeadFeatureRenderer {
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/blockentity/SkullBlockRenderer;submitSkull(Lnet/minecraft/core/Direction;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;ILnet/minecraft/client/model/object/skull/SkullModelBase;Lnet/minecraft/client/renderer/rendertype/RenderType;ILnet/minecraft/client/renderer/feature/ModelFeatureRenderer$CrumblingOverlay;)V")
     )
     private boolean onRenderArmor(Direction direction, float f, float g, PoseStack matrices, SubmitNodeCollector submitNodeCollector, int i, SkullModelBase skullModelBase, RenderType renderType, int j, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay) {
+        return !HideArmorHookKt.shouldHideArmor();
+    }
+
+    @WrapWithCondition(
+        method = "submit(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;ILnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;FF)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/item/ItemStackRenderState;submit(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;III)V")
+    )
+    private boolean onRenderItemstackOnHead(ItemStackRenderState instance, PoseStack matrices, SubmitNodeCollector submitNodeCollector, int i, int j, int k) {
         return !HideArmorHookKt.shouldHideArmor();
     }
 
