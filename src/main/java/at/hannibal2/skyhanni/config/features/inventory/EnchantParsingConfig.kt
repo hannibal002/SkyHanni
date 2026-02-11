d7fde7e698c7b6902c1abcb745847923419cce4d
Germanminer12 <germanminer12@gmail.com>
2026-02-11 15:35:33 +1300

    Fix: Line length

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/EnchantParsingConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/EnchantParsingConfig.kt
@@ -48,7 +48,8 @@ class EnchantParsingConfig {
     val ultimateEnchantColor: Property<LorenzColor> = Property.of(LorenzColor.LIGHT_PURPLE)
 
     @Expose
-    @ConfigOption(name = "Max Ultimate Enchantment Color", desc = "The color the Ultimate enchantment will be if max level. (Will always be bold)")
+    @ConfigOption(name = "Max Ultimate Enchantment Color",
+        desc = "The color the Ultimate enchantment will be if max level. (Will always be bold)")
     @ConfigEditorDropdown
     val maxUltimateEnchantColor: Property<LorenzColor> = Property.of(LorenzColor.LIGHT_PURPLE)
 
