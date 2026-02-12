553cfc3ba7da7c664ea5d8f48f821a2aff18de51
nopo <nopotheemail@gmail.com>
2026-02-12 22:02:42 +1100

    fix player death event

--- src/main/java/at/hannibal2/skyhanni/data/PlayerDeathManager.kt
+++ src/main/java/at/hannibal2/skyhanni/data/PlayerDeathManager.kt
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.events.player.PlayerDeathEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.PlayerUtils
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
@@ -13,19 +14,42 @@ object PlayerDeathManager {
 
     /**
      * REGEX-TEST: §c ☠ §r§7§r§bZeroHazel§r§7 was killed by §r§8§lAshfang§r§7§r§7.
+     * REGEX-TEST: §c ☠ §r§7You fell into the void§r§7.
      */
     private val deathMessagePattern by RepoPattern.pattern(
         "chat.player.death",
         "§c ☠ §r§7§r§.(?<name>.+)§r§7 (?<reason>.+)",
     )
 
+    /**
+     * REGEX-TEST:  ☠ You fell into the void.
+     * REGEX-TEST:  ☠ You burned to death.
+     * REGEX-TEST:  ☠ You were killed by Bladesoul.
+     */
+    private val selfDeathMessagePattern by RepoPattern.pattern(
+        "chat.player.selfdeath",
+        " ☠ You (?<reason>.+)",
+    )
+
+
     @HandleEvent
     fun onChat(event: SkyHanniChatEvent.Allow) {
-        val message = event.message
-        deathMessagePattern.matchMatcher(message) {
+        deathMessagePattern.matchMatcher(event.message) {
             val name = group("name")
             val reason = group("reason").removeColor()
             PlayerDeathEvent(name, reason, event).post()
         }
+
+        selfDeathMessagePattern.matchMatcher(event.cleanMessage) {
+            val name = PlayerUtils.getName()
+            val reason = group("reason").removeColor()
+            PlayerDeathEvent(name, reason, event).post()
+        }
+    }
+
+    @HandleEvent
+    fun onDeath(event: PlayerDeathEvent) {
+        println(event.name)
+        println(event.reason)
     }
 }
