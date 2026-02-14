e8023ea6572dd606a3ef73836efad6ebf7be28cc
Helium9 <80259183+sivthepolarfox@users.noreply.github.com>
2026-02-15 00:26:46 +0100

    meow

--- src/main/java/at/hannibal2/skyhanni/features/misc/ContributorManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/ContributorManager.kt
@@ -4,46 +4,45 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.jsonobjects.repo.ContributorJsonEntry
 import at.hannibal2.skyhanni.data.jsonobjects.repo.ContributorsJson
-import at.hannibal2.skyhanni.data.mob.MobFilter.isRealPlayer
 import at.hannibal2.skyhanni.events.RepositoryReloadEvent
 import at.hannibal2.skyhanni.events.entity.EntityDisplayNameEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.compat.append
 import net.minecraft.world.entity.player.Player
+import java.util.UUID
 
 @SkyHanniModule
 object ContributorManager {
     private val config get() = SkyHanniMod.feature.dev
 
-    // Key is the lowercase contributor name
-    private var contributors: Map<String, ContributorJsonEntry> = emptyMap()
-
-    // Just the names of the contributors including their proper case
+    var contributors: Map<UUID, ContributorJsonEntry> = emptyMap()
+        private set
     var contributorNames = emptyList<String>()
         private set
 
     @HandleEvent
     fun onRepoReload(event: RepositoryReloadEvent) {
         val map = event.getConstant<ContributorsJson>("Contributors").contributors
-        contributors = map.mapKeys { it.key.lowercase() }
-        contributorNames = map.map { it.key }
+        contributors = map.mapKeys { UUID.fromString(it.key) }
     }
 
     @HandleEvent
     fun onRenderNametag(event: EntityDisplayNameEvent<Player>) {
         if (!config.contributorNametags) return
-        if (event.entity.isRealPlayer()) getSuffix(event.entity.name.string)?.let {
+
+        val gameProfile = event.entity.gameProfile
+        getSuffix(gameProfile.id)?.let {
             event.chatComponent.append(it)
         }
     }
 
-    fun getSuffix(username: String): String? = getContributor(username)?.suffix
+    fun getSuffix(uuid: UUID): String? = getContributor(uuid)?.suffix
 
-    fun shouldSpin(username: String): Boolean = getContributor(username)?.spinny ?: false
-    fun shouldBeUpsideDown(username: String): Boolean = getContributor(username)?.upsideDown ?: false
+    fun shouldSpin(uuid: UUID): Boolean = getContributor(uuid)?.spinny ?: false
+    fun shouldBeUpsideDown(uuid: UUID): Boolean = getContributor(uuid)?.upsideDown ?: false
 
-    private fun getContributor(username: String) =
-        contributors[username.lowercase()]?.let { it.takeIf { it.isAllowed() } }
+    private fun getContributor(uuid: UUID) =
+        contributors[uuid]?.let { it.takeIf { it.isAllowed() } }
 
     private fun ContributorJsonEntry.isAllowed(): Boolean {
         if (!config.fancyContributors) return false
