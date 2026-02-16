937af6725ac4d51bb3f4f6e808a66a16223018bc
Seth Cruz <sethcruz@Seths-MacBook-Pro.local>
2026-02-16 09:55:08 -0500

    Added periods to config desc

--- src/main/java/at/hannibal2/skyhanni/config/features/crimsonisle/VanquisherWaypointShareConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/crimsonisle/VanquisherWaypointShareConfig.kt
@@ -9,23 +9,23 @@ import org.lwjgl.glfw.GLFW
 
 class VanquisherWaypointShareConfig {
     @Expose
-    @ConfigOption(name = "Enabled", desc = "Share your Vanquisher spawns and receive other Vanquisher spawns via Party Chat")
+    @ConfigOption(name = "Enabled", desc = "Share your Vanquisher spawns and receive other Vanquisher spawns via Party Chat.")
     @ConfigEditorBoolean
     @FeatureToggle
     var enabled: Boolean = true
 
     @Expose
-    @ConfigOption(name = "Instant Share", desc = "Instantly share your Vanquisher spawns via Party Chat")
+    @ConfigOption(name = "Instant Share", desc = "Instantly share your Vanquisher spawns via Party Chat.")
     @ConfigEditorBoolean
     var instantShare: Boolean = true
 
     @Expose
-    @ConfigOption(name = "Keybind Share", desc = "Manually share your Vanquisher spawns with a keybind")
+    @ConfigOption(name = "Keybind Share", desc = "Manually share your Vanquisher spawns with a keybind.")
     @ConfigEditorKeybind(defaultKey = GLFW.GLFW_KEY_Y)
     var keybindSharing: Int = GLFW.GLFW_KEY_Y
 
     @Expose
-    @ConfigOption(name = "Read Global Chat", desc = "Register Vanquisher spawns from All Chat")
+    @ConfigOption(name = "Read Global Chat", desc = "Register Vanquisher spawns from All Chat.")
     @ConfigEditorBoolean
     var readGlobalChat: Boolean = false
 }
