b83a30b1026ac2bd84313916bcd6532aabc21ca8
Mridul Purkayastha <141699102+mrid-12@users.noreply.github.com>
2026-02-19 23:59:14 -0500

    Add 'categories' field to MiningJson data class

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MiningJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MiningJson.kt
@@ -5,4 +5,6 @@ import com.google.gson.annotations.SerializedName
 
 data class MiningJson(
     @Expose @SerializedName("block_strengths") val blockStrengths: Map<String, Int>,
+
+    @Expose @SerializedName("allowed_blocks") val categories: Map<String, List<NeuInternalName>>
 )
