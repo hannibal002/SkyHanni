9e771579d3c7432cfc86a2770ec1665c3e7b5a29
Mridul Purkayastha <141699102+mrid-12@users.noreply.github.com>
2026-02-20 00:06:42 -0500

    Add import for NeuInternalName in MiningJson.kt
    We NEVER saw this

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MiningJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MiningJson.kt
@@ -1,5 +1,6 @@
 package at.hannibal2.skyhanni.data.jsonobjects.repo
 
+import at.hannibal2.skyhanni.utils.NeuInternalName
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
