da244be6e227d5b9bfab8d127c86e0760bd49aa4
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-02-22 21:15:08 +0000

    Old Version Fixed Pattern (I haven't tested it, if this somehow doesn't work idk, not a me problem if full version is today).

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonApi.kt
@@ -38,7 +38,7 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.Blocks
 
-@Suppress("MemberVisibilityCanBePrivate")
+@Suppress("MemberVisibilityCanBePrivate", "UnusedPrivateProperty")
 @SkyHanniModule
 object DungeonApi {
 
@@ -91,6 +91,16 @@ object DungeonApi {
     private val dungeonComplete by patternGroup.pattern(
         "completecolorless",
         "\\s+(?:Master Mode )?The Catacombs - (?:Floor [IV]{1,3}|Entrance)",
+    )
+
+    /**
+     * REGEX-TEST: §f                §r§cMaster Mode The Catacombs §r§8- §r§eFloor VII
+     * REGEX-TEST: §f                         §r§cThe Catacombs §r§8- §r§eFloor V
+     */
+    private val dungeonCompleteOld by patternGroup.pattern(
+        // TODO: If It's April or Later and you're Reading This, remove this.
+        "complete",
+        "\\s+§.§.(?:Master Mode )?The Catacombs §.§.- §.§.(?:Floor )?(?<floor>M?[IV]{1,3}|Entrance)",
     )
 
     /**
