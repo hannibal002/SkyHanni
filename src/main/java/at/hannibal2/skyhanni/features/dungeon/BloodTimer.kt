5d90647b12b7cf7e88a939b0c5446a05b1b9d2ff
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-02-23 12:40:53 +0000

    Blood Timer Port

---
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/BloodTimer.kt
@@ -0,0 +1,73 @@
+package at.hannibal2.skyhanni.features.dungeon
+
+import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.data.title.TitleManager
+import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.DelayedRun.runDelayed
+import at.hannibal2.skyhanni.utils.ServerTimeMark
+import at.hannibal2.skyhanni.utils.SimpleTimeMark
+import at.hannibal2.skyhanni.utils.inPartialSeconds
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+
+@SkyHanniModule
+object BloodTimer {
+    private val bloodOpenMessages = setOf(
+        "[BOSS] The Watcher: Things feel a little more roomy now, eh?",
+        "[BOSS] The Watcher: Oh.. hello?",
+        "[BOSS] The Watcher: I'm starting to get tired of seeing you around here...",
+        "[BOSS] The Watcher: You've managed to scratch and claw your way here, eh?",
+        "[BOSS] The Watcher: So you made it this far... interesting.",
+        "[BOSS] The Watcher: Ah, we meet again...",
+        "[BOSS] The Watcher: Ah, you've finally arrived.",
+    )
+    private var bloodOpenTime = SimpleTimeMark.farPast()
+    private var bloodOpenLength = ServerTimeMark.farPast()
+
+    @HandleEvent
+    fun onChatReceived(event: SkyHanniChatEvent.Allow) {
+        when (event.cleanMessage) {
+            in bloodOpenMessages -> {
+                bloodOpenTime = SimpleTimeMark.now()
+                bloodOpenLength = ServerTimeMark.now()
+            }
+
+            "[BOSS] The Watcher: Let's see how you can handle this." -> {
+                val bloodMove = bloodOpenTime.passedSince() + 0.1.seconds
+                val bloodMoveTime = bloodOpenLength.passedSince() + 0.1.seconds
+
+                val bloodLag = bloodMove - bloodMoveTime
+
+                ChatUtils.debug("Blood Timer: $bloodMoveTime move time.")
+
+                // Selects move prediction for 4th/5th mob based on how long watcher took to say activation line
+                // TODO: Calc prediction from bloodMoveTime (may just need to add to repo if not possible; not sure if its set for each floor or random.
+                val bloodMovePredictionNumber: Duration? = when (bloodMoveTime.inPartialSeconds) {
+                    in 31.0..34.0 -> bloodLag + 36.seconds
+                    in 28.0..31.0 -> bloodLag + 33.seconds
+                    in 25.0..28.0 -> bloodLag + 30.seconds
+                    in 22.0..25.0 -> bloodLag + 27.seconds
+                    in 1.0..22.0 -> bloodLag + 24.seconds
+                    else -> null
+                }
+                val bloodMovePrediction = bloodMovePredictionNumber?.inPartialSeconds?.let { "%.2f".format(it) }
+
+                bloodMovePrediction?.let {
+                    ChatUtils.chat("§7Move Prediction: §f$it Seconds§7.")
+                    TitleManager.sendTitle("", "§7Move Prediction: §f${it}s", 2.5.seconds)
+                    val delay = bloodMovePredictionNumber - bloodMoveTime - 150.milliseconds
+                    ChatUtils.debug("Blood Timer: $delay delay.")
+                    runDelayed(delay) {
+                        TitleManager.sendTitle("", "§cKill Blood", 1.5.seconds)
+                    }
+                } ?: run {
+                    ChatUtils.chat("§cInvalid Prediction")
+                }
+            }
+        }
+    }
+
+}
