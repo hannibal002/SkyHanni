bba2cc82620c00664d12e177018aea12ffdf5fb9
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-03-02 18:38:42 +0000

    Fixed Command Name & Lack of Saving new list in Repo Reload.

--- src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureManager.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureManager.kt
@@ -8,6 +8,7 @@ import at.hannibal2.skyhanni.events.RepositoryReloadEvent
 import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.events.fishing.SeaCreatureFishEvent
 import at.hannibal2.skyhanni.features.fishing.seaCreatureXMLGui.SpecificSeaCreatures
+import at.hannibal2.skyhanni.features.fishing.seaCreatureXMLGui.SpecificSeaCreatures.Companion.saveSeaCreatures
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.StringUtils
@@ -137,7 +138,7 @@ object SeaCreatureManager {
         }
         SeaCreatureManager.allFishingMobs = allFishingMobs
         allVariants = variants
-        SpecificSeaCreatures.updateList()
+        saveSeaCreatures(SpecificSeaCreatures.updateList())
     }
 
     private fun getSeaCreatureFromMessage(message: String): SeaCreature? {
