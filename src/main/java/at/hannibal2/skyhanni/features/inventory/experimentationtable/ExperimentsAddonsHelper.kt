91028660fec8bfb481014dafb4b032a7d76f9ac3
Isaac Kim <snakeisaac@gmail.com>
2026-03-02 08:45:53 -0800

    Initialize chronomatron helper as empty

--- src/main/java/at/hannibal2/skyhanni/features/inventory/experimentationtable/ExperimentsAddonsHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/experimentationtable/ExperimentsAddonsHelper.kt
@@ -51,7 +51,7 @@ object ExperimentsAddonsHelper {
     private val userUltrasequencerProgress: MutableList<Int> = mutableListOf()
     private val ultrasequencerDyeMap: MutableMap<Int, ItemStack> = mutableMapOf()
 
-    private var chronHasBeenEmpty: Boolean = false
+    private var chronHasBeenEmpty: Boolean = true
     private var lastChronomatronSound: SimpleTimeMark = SimpleTimeMark.farPast()
     private var currentAddonPhase: HelperPhase? = null
     private var chronomatronSequenceIndex: Int = 0
@@ -105,7 +105,7 @@ object ExperimentsAddonsHelper {
         chronomatronSequenceIndex = 0
         lastChronomatronSound = SimpleTimeMark.farPast()
         currentAddonPhase = null
-        chronHasBeenEmpty = false
+        chronHasBeenEmpty = true
     }
 
     private fun ItemStack.getLorenzColorOrNull(): LorenzColor? = when (hoverName.string.removeColor()) {
