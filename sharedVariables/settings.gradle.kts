c7d1cb426d39c0de69bd8c92a5d8bfb7611bcef7
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2026-03-12 01:55:47 -0400

    Update settings.gradle.kts

--- sharedVariables/settings.gradle.kts
+++ sharedVariables/settings.gradle.kts
@@ -5,6 +5,3 @@ dependencyResolutionManagement {
         }
     }
 }
-
-// Rest of this space has been intentionally left blank
-// It serves such that this folder is a new gradle workspace rather than being a subproject
