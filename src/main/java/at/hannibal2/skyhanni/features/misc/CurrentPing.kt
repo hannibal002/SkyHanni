4208c9e865e04a8256c0934633d72b244b445eb1
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-03-16 23:26:53 +0100

    imports

--- src/main/java/at/hannibal2/skyhanni/features/misc/CurrentPing.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/CurrentPing.kt
@@ -1,6 +1,5 @@
 package at.hannibal2.skyhanni.features.misc
 
-import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.config.ConfigUpdaterMigrator
 import at.hannibal2.skyhanni.config.commands.CommandCategory
