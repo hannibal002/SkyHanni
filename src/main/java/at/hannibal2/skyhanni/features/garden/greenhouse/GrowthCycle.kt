28597221755f61344511f574c7a923b1536b1dbf
Luna <lunaynx@gmail.com>
2026-03-18 18:39:58 +0100

    Detekt

--- src/main/java/at/hannibal2/skyhanni/features/garden/greenhouse/GrowthCycle.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/greenhouse/GrowthCycle.kt
@@ -6,7 +6,6 @@ import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.ProfileStorageData
 import at.hannibal2.skyhanni.events.GuiRenderEvent
 import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
-import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLoreComponent
