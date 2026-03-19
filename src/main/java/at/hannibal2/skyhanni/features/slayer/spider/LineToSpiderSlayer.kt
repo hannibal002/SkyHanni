3ed5c5db72fa8babc3fd820fb3a3940b4b320265
Marina <nathaniel.grima2005@gmail.com>
2026-03-19 18:57:26 +1100

    added line to tara boss but changed to be better!

--- src/main/java/at/hannibal2/skyhanni/features/slayer/spider/LineToSpiderSlayer.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/spider/LineToSpiderSlayer.kt
@@ -39,14 +39,14 @@ object LineToSpiderSlayer {
     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
         if (!SlayerApi.isInAnyArea || !config.lineToBoss) return
         val seenMobs = bosses.filter { it.baseEntity.canBeSeen(30) && it.isAlive }
-		seenMobs.forEach { mob ->
-			event.drawLineToEye(
+        seenMobs.forEach { mob ->
+            event.drawLineToEye(
                 mob.baseEntity.getLorenzVec().up(),
                 LorenzColor.AQUA.toChromaColor(),
                 config.slayerLineWidth,
                 true,
             )
-		}
+        }
+    }
 }
-
 
