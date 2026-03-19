ef582d2db01ce67a7bfd785eb5eb47e93f93c40a
Luna <lunaynx@gmail.com>
2026-03-19 14:57:19 +0100

    Fix

--- src/main/java/at/hannibal2/skyhanni/utils/render/WorldRenderUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/render/WorldRenderUtils.kt
@@ -14,6 +14,8 @@ import at.hannibal2.skyhanni.utils.LocationUtils.union
 import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzColor.Companion.toLorenzColor
 import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.PlayerUtils.SNEAKING_EYE_HEIGHT
+import at.hannibal2.skyhanni.utils.PlayerUtils.STANDING_EYE_HEIGHT
 import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
 import at.hannibal2.skyhanni.utils.compat.createResourceLocation
 import at.hannibal2.skyhanni.utils.compat.deceased
@@ -999,7 +1001,7 @@ object WorldRenderUtils {
     fun SkyHanniRenderWorldEvent.exactPlayerEyeLocation(player: Entity): LorenzVec =
         exactLocation(player) + LorenzVec(
             0.0,
-            if (player.isShiftKeyDown) SNEAKING_EYE_HEIGHT else STANDING_EYE_HEIGHT,
+            (if (player.isShiftKeyDown) SNEAKING_EYE_HEIGHT else STANDING_EYE_HEIGHT).toDouble(),
             0.0,
         )
 
