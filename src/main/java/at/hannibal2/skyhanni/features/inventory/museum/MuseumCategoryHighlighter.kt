a21e63d764ede1e62e780d367c36d03132891067
nopo <nopotheemail@gmail.com>
2026-03-22 22:24:36 +1100

    i had a dream one time that i went to fix this pr and someone had fixed it for me, freaky

--- src/main/java/at/hannibal2/skyhanni/features/inventory/museum/MuseumCategoryHighlighter.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/museum/MuseumCategoryHighlighter.kt
@@ -4,7 +4,7 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.GuiContainerEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.InventoryUtils
+import at.hannibal2.skyhanni.utils.InventoryDetector
 import at.hannibal2.skyhanni.utils.ItemUtils.getLoreComponent
 import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.RenderUtils.highlight
@@ -12,17 +12,16 @@ import at.hannibal2.skyhanni.utils.RenderUtils.highlight
 @SkyHanniModule
 object MuseumCategoryHighlighter {
 
+    private val museumDetector = InventoryDetector { name -> name == "Your Museum" }
+
     @HandleEvent(onlyOnSkyblock = true)
     fun onBackgroundDrawn(event: GuiContainerEvent.BackgroundDrawnEvent) {
         if (!SkyHanniMod.feature.inventory.museumCategoryHighlight) return
-        if (InventoryUtils.openInventoryName() != "Your Museum") return
+        if (!museumDetector.isInside()) return
         for (slot in event.container.slots) {
             val lore = slot.item.getLoreComponent()
-            for (line in lore) {
-                if (line.string == "Items Donated: 100%") {
-                    slot.highlight(LorenzColor.GREEN)
-                    break
-                }
+            if (lore.any { it.string == "Items Donated: 100%" }) {
+                slot.highlight(LorenzColor.GREEN)
             }
         }
     }
