44af3d89df875b5aaca84f20a684944b1e0aba2c
DavidArthurCole <davidarthurcole23@gmail.com>
2026-03-24 14:52:38 -0400

    Make neu stack provider respect components

--- src/main/java/at/hannibal2/skyhanni/utils/NeuItemStackProvider.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/NeuItemStackProvider.kt
@@ -20,12 +20,23 @@ class NeuItemStackProvider(
     @SkyHanniModule
     companion object {
         private val providerCache = WeakHashMap<NeuItemStackProvider, SafeItemStack>()
+        private var waitingOnComponents = false
 
-        @HandleEvent(NeuRepositoryReloadEvent::class, priority = HandleEvent.LOW)
+        private fun rebuildProviderCache() = providerCache.forEach { (provider, _) ->
+            providerCache[provider] = provider.rebuildFromNeu()
+        }
+
+        @HandleEvent(priority = HandleEvent.LOW)
         fun onNeuRepoReload() {
-            providerCache.forEach { (provider, _) ->
-                providerCache[provider] = provider.rebuildFromNeu()
-            }
+            if (SafeItemStackUtils.componentsLoaded) rebuildProviderCache()
+            else waitingOnComponents = true
+        }
+
+        @HandleEvent
+        fun onComponentsLoaded() {
+            if (!waitingOnComponents) return
+            rebuildProviderCache()
+            waitingOnComponents = false
         }
     }
 }
