d3bc4e5d7cb4fda6269c6f761e46be3fe1c7665f
DavidArthurCole <davidarthurcole23@gmail.com>
2026-03-24 17:28:42 -0400

    More work on deferred item stack

--- versions/26.1/src/main/java/at/hannibal2/skyhanni/utils/DeferredItemStack.kt
+++ versions/26.1/src/main/java/at/hannibal2/skyhanni/utils/DeferredItemStack.kt
@@ -4,31 +4,39 @@ import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.minecraft.ComponentsLoadedEvent
 import at.hannibal2.skyhanni.mixins.transformers.IItemStackAccessor
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import net.minecraft.core.component.DataComponentMap
+import net.minecraft.core.component.PatchedDataComponentMap
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
 import java.util.Collections
 import java.util.WeakHashMap
 
 internal class DeferredItemStack(
     private val factory: () -> ItemStack,
     count: Int,
-) : ItemStack(Items.AIR, count) {
+) : ItemStack(null, count, PatchedDataComponentMap(DataComponentMap.EMPTY)) {
 
-    private val accessor get() = this as IItemStackAccessor
+    @Suppress("UNCHECKED_CAST")
+    private val accessor get() = (this as Any) as IItemStackAccessor
     private val emptyHolder = accessor.`skyhanni$getItemHolder`()
     private val emptyComponents = accessor.`skyhanni$getPatchedComponents`()
+    private var isBuilt = false
+
+    override fun isEmpty() = !isBuilt || super.isEmpty()
 
     internal fun rebuild() {
         if (!SafeItemStackUtils.componentsLoaded) return
         val real = factory()
-        val realAccessor = real as IItemStackAccessor
+        @Suppress("UNCHECKED_CAST")
+        val realAccessor = (real as Any) as IItemStackAccessor
         accessor.`skyhanni$setItemHolder`(realAccessor.`skyhanni$getItemHolder`())
         accessor.`skyhanni$setPatchedComponents`(realAccessor.`skyhanni$getPatchedComponents`())
+        isBuilt = true
     }
 
     internal fun invalidate() {
         accessor.`skyhanni$setItemHolder`(emptyHolder)
         accessor.`skyhanni$setPatchedComponents`(emptyComponents)
+        isBuilt = false
     }
 
     init {
