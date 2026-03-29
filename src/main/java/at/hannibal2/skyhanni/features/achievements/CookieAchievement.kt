4b3b507299714abd4ddc5d29473b7162eba11d88
nopo <nopotheemail@gmail.com>
2026-03-29 11:44:36 +1100

    where do these empty lines come from??

--- src/main/java/at/hannibal2/skyhanni/features/achievements/CookieAchievement.kt
+++ src/main/java/at/hannibal2/skyhanni/features/achievements/CookieAchievement.kt
@@ -9,7 +9,6 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.chat.TextHelper.asComponent
 import kotlin.time.Duration.Companion.days
 
-
 @SkyHanniModule
 object CookieAchievement {
 
