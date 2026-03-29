20f65f84a9cee9fd9cdb4bd2590391738d7022de
DavidArthurCole <davidarthurcole23@gmail.com>
2026-03-29 17:59:54 -0400

    Fix message

--- src/main/java/at/hannibal2/skyhanni/features/misc/MutedNarratorWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/MutedNarratorWarning.kt
@@ -4,6 +4,8 @@ import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.OSUtils
+import at.hannibal2.skyhanni.utils.OSUtils.getOperatingSystem
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import net.minecraft.client.Minecraft
 import net.minecraft.client.NarratorStatus
@@ -18,8 +20,18 @@ object MutedNarratorWarning {
     private val reminderInterval = 1.minutes
     private var lastReminded: SimpleTimeMark = SimpleTimeMark.farPast()
 
-    private const val WARNING_MESSAGE = "You currently have the Minecraft narrator turn on, but it is muted.\n" +
-        "§cThis is likely negatively impacting your game's performance."
+    private val toggleNarratorKeybind = when (getOperatingSystem()) {
+        OSUtils.OperatingSystem.WINDOWS, OSUtils.OperatingSystem.LINUX -> "Ctrl + B"
+        OSUtils.OperatingSystem.MACOS -> "Cmd + B"
+        else -> "Ctrl/Cmd + B"
+    }
+
+    private val warningMessage by lazy {
+        "You currently have the Minecraft narrator turned on, " +
+            "but the Voice or Master Volume sliders are muting it.\n" +
+            "§cThis is likely negatively impacting your game's performance.\n" +
+            "§eYou can use §b${toggleNarratorKeybind} §eto disable it, or:"
+    }
 
     private val narratorActive get(): Boolean = runCatching {
         Minecraft.getInstance().narrator.isActive
@@ -36,7 +48,7 @@ object MutedNarratorWarning {
         if (lastReminded.passedSince() < reminderInterval) return
 
         ChatUtils.clickToActionOrDisable(
-            WARNING_MESSAGE,
+            warningMessage,
             config::mutedNarratorWarning,
             "turn off narrator",
             { Minecraft.getInstance().options.narrator().set(NarratorStatus.OFF) },
