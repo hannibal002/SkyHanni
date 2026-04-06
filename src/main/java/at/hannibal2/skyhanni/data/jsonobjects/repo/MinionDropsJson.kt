9cd5564caee0aab06f8f726b267db89615d39c7a
FabiHBBBT <admin@fabihbbbt.net>
2026-04-07 01:07:09 +0200

    detekt

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MinionDropsJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/MinionDropsJson.kt
@@ -6,5 +6,5 @@ import com.google.gson.annotations.SerializedName
 
 data class MinionDropsJson(
     @Expose @SerializedName("fuel_drops") val fuelDrops: Map<NeuInternalName, Set<NeuInternalName>>,
-    @Expose val minions: Map<String, Set<NeuInternalName>>
+    @Expose val minions: Map<String, Set<NeuInternalName>>,
 )
