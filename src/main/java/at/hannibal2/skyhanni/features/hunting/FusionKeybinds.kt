b48eab62ca42639ebc75bb2beaa39cf898b66488
Luna <lunaynx@gmail.com>
2026-04-06 02:32:41 +0200

    Use AttributeShardsData + simplify

--- src/main/java/at/hannibal2/skyhanni/features/hunting/FusionKeybinds.kt
+++ src/main/java/at/hannibal2/skyhanni/features/hunting/FusionKeybinds.kt
@@ -3,15 +3,13 @@ package at.hannibal2.skyhanni.features.hunting
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.GuiContainerEvent.ClickType
+import at.hannibal2.skyhanni.features.inventory.attribute.AttributeShardsData
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.ConditionalUtils.afterChange
-import at.hannibal2.skyhanni.utils.InventoryDetector
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyHeld
-import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
-import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 import org.lwjgl.glfw.GLFW
 import kotlin.time.Duration.Companion.seconds
 
@@ -20,45 +18,20 @@ object FusionKeybinds {
 
     private val config get() = SkyHanniMod.feature.hunting.fusionKeybinds
 
-    private val patternGroup = RepoPattern.group("hunting.fusion.keybinds")
-
-    // <editor-fold desc="Patterns">
-    private val fusionBoxInventoryPattern by patternGroup.pattern(
-        "inventory.fusion-box",
-        "Fusion Box",
-    )
-
-    private val confirmFusionInventoryPattern by patternGroup.pattern(
-        "inventory.confirm-fusion",
-        "Confirm Fusion",
-    )
-    // </editor-fold>
-
-    private val fusionBoxInventory = InventoryDetector { name ->
-        fusionBoxInventoryPattern.matches(name)
-    }
-    private val confirmFusionInventory = InventoryDetector { name ->
-        confirmFusionInventoryPattern.matches(name)
-    }
-
     private var lastDuplicateKeybindsWarnTime = SimpleTimeMark.farPast()
 
     // No island check because Kysha Abiphone contact exists
     @HandleEvent(onlyOnSkyblock = true)
     fun onGuiKeyPress() {
-        val repeatFusionKeybind = config.repeatFusionKeybind.get()
-        val confirmFusionKeybind = config.confirmFusionKeybind.get()
+        val repeatHeld = config.repeatFusionKeybind.get().isKeyHeld()
+        val confirmHeld = config.confirmFusionKeybind.get().isKeyHeld()
 
         when {
-            fusionBoxInventory.isInside() && repeatFusionKeybind.isKeyHeld() -> {
-                if (confirmFusionKeybind.isKeyHeld()) return
+            AttributeShardsData.fusionBoxInventory.isInside() && repeatHeld && !confirmHeld ->
                 InventoryUtils.clickSlot(47, mouseButton = 2, mode = ClickType.MIDDLE)
-            }
 
-            confirmFusionInventory.isInside() && confirmFusionKeybind.isKeyHeld() -> {
-                if (repeatFusionKeybind.isKeyHeld()) return
+            AttributeShardsData.confirmFusionInventory.isInside() && confirmHeld && !repeatHeld ->
                 InventoryUtils.clickSlot(33, mouseButton = 2, mode = ClickType.MIDDLE)
-            }
         }
     }
 
