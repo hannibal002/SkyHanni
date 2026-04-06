0e460e48cb97677c4bef8263c94e05262b4bc83d
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-04-06 10:58:40 +0100

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureCocoonWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureCocoonWarning.kt
@@ -10,7 +10,6 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.SoundUtils
-import at.hannibal2.skyhanni.utils.StringUtils
 
 @SkyHanniModule
 object SeaCreatureCocoonWarning {
