95977c121f5dfa3632602f9ec750aca7655e9b1b
NotRoboo <145162343+NotRoboo@users.noreply.github.com>
2026-04-06 01:31:01 -0400

    Detekt Fix
    Removed unused imports from ToolLevelDisplay.kt

--- src/main/java/at/hannibal2/skyhanni/features/garden/farming/ToolLevelDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/farming/ToolLevelDisplay.kt
@@ -20,9 +20,7 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.InventoryUtils
-import at.hannibal2.skyhanni.utils.ItemCategory
 import at.hannibal2.skyhanni.utils.ItemUtils
-import at.hannibal2.skyhanni.utils.ItemUtils.getItemCategoryOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil.addSeparators
 import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderables
