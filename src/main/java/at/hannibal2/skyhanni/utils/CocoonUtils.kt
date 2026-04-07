1a0cf7246f015ab1234ebc310e795ac2d7e97917
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-04-07 13:26:04 +0100

    this is probably the wrong way to do this.

--- src/main/java/at/hannibal2/skyhanni/utils/CocoonUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/CocoonUtils.kt
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.utils
 
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.events.hypixel.HypixelJoinEvent
 import at.hannibal2.skyhanni.events.skyblock.SkyblockEquipmentChangeEvent
 import at.hannibal2.skyhanni.features.inventory.EquipmentApi
 import at.hannibal2.skyhanni.features.inventory.EquipmentSlot
@@ -12,12 +13,23 @@ import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getReforgeModifier
 @SkyHanniModule
 object CocoonUtils {
     var canCocoon = false
+        private set
 
-    @HandleEvent()
+    private fun playerCanCocoon() {
+        val belt = EquipmentApi.getEquipment(EquipmentSlot.BELT) ?: return
+        canCocoon = (belt.getInternalName() == "THE_PRIMORDIAL".toInternalName() || belt.getReforgeModifier() == "blood_shot")
+    }
+
+    @HandleEvent
     fun onSkyblockEquipmentChange(event: SkyblockEquipmentChangeEvent) {
         if (!event.isBelt) return
         if (event.newItemStack == null) return
         val belt = event.newItemStack
         canCocoon = (belt.getInternalName() == "THE_PRIMORDIAL".toInternalName() || belt.getReforgeModifier() == "blood_shot")
+    }
+
+    @HandleEvent
+    fun onHypixelJoin(event: HypixelJoinEvent) {
+        playerCanCocoon()
     }
 }
