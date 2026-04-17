b2ca5dd020f23a1e9600b2bd9add92107e6312b5
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-04-17 16:45:30 +0100

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/inventory/auctionhouse/HighlightSkeletonMasterChestplate.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/auctionhouse/HighlightSkeletonMasterChestplate.kt
@@ -7,7 +7,6 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.InventoryDetector
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
-import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.NeuInternalName.Companion.toInternalName
 import at.hannibal2.skyhanni.utils.RenderUtils.highlight
 import at.hannibal2.skyhanni.utils.SkyBlockItemModifierUtils.getDungeonTier
