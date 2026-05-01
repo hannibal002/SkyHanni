670f98e633b14cbf6671ebf6e10ff86b121f94eb
Marina <nathaniel.grima2005@gmail.com>
2026-05-01 13:46:26 +1000

    tarantula slayer but it eman.

--- src/main/java/at/hannibal2/skyhanni/config/features/slayer/endermen/EndermanConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/slayer/endermen/EndermanConfig.kt
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.config.FeatureToggle
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
 class EndermanConfig {
@@ -34,4 +35,18 @@ class EndermanConfig {
     @ConfigEditorBoolean
     @FeatureToggle
     var hideParticles: Boolean = false
+    @Expose
+    @ConfigOption(name = "Line to Voidgloom Boss", desc = "Draws a line to your Voidgloom Seraph Boss.")
+    @ConfigEditorBoolean
+    @FeatureToggle
+    var lineToBoss: Boolean = false
+
+    @Expose
+    @ConfigOption(
+        name = "Line to Voidgloom Width",
+        desc = "The width of the line pointing to your Voidgloom Seraph.",
+    )
+    @ConfigEditorSlider(minStep = 1f, minValue = 1f, maxValue = 10f)
+    var slayerLineWidth: Int = 3
 }
+
