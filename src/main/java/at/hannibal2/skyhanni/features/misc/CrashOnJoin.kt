66bece03a13e06404f32be199a0dcf69e9031d44
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2026-05-03 11:51:43 -0500

    crash on join

--- src/main/java/at/hannibal2/skyhanni/features/misc/CrashOnJoin.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/CrashOnJoin.kt
@@ -8,13 +8,13 @@ import net.minecraft.CrashReport
 import net.minecraft.client.Minecraft
 
 @SkyHanniModule
-object CrashOnDeath {
+object CrashOnJoin {
     @HandleEvent
     fun onProfileJoin(event: ProfileJoinEvent) {
         Minecraft.getInstance().delayCrash(
             CrashReport(
-                "SkyHanni Crash on Death",
-                Throwable("Disable Crash on Death in SkyHanni if you want to stop crashing")
+                "SkyHanni Crash on Join",
+                Throwable("Joining Skyblock may lead to bans!")
             ),
         )
     }
