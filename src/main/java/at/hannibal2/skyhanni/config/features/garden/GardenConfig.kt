f42b258061b13ff29ef52e40ee941df0d4dc3223
CloodDev <112552341+CloodDev@users.noreply.github.com>
2026-05-09 16:33:31 +0200

    Feature: Add Feast Display for Harvest Feast

--- src/main/java/at/hannibal2/skyhanni/config/features/garden/GardenConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/garden/GardenConfig.kt
@@ -163,6 +163,11 @@ class GardenConfig {
     val atmosphericFilterDisplay: AtmosphericFilterDisplayConfig = AtmosphericFilterDisplayConfig()
 
     @Expose
+    @ConfigOption(name = "Feast Crop Display", desc = "")
+    @Accordion
+    val feastDisplay: FeastDisplayConfig = FeastDisplayConfig()
+
+    @Expose
     @ConfigOption(name = "Garden Tracker Uptime Settings", desc = "")
     @Accordion
     val trackerUptimeSettings: GardenTrackerUptimeConfig = GardenTrackerUptimeConfig()
