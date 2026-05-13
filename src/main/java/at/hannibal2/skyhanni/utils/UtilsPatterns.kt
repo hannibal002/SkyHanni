c114db385c7a8b0fa62937e9e12fcc507e91f8de
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-05-13 09:10:01 +0200

    made the regex easier to read

--- src/main/java/at/hannibal2/skyhanni/utils/UtilsPatterns.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/UtilsPatterns.kt
@@ -28,7 +28,7 @@ object UtilsPatterns {
      */
     val rarityLoreLinePattern by patternGroup.pattern(
         "item.lore.rarity.line.colorless",
-        "^(?:Rarity: )?(?:a )?(?:SHINY )?(?<rarity>${rarities})(?: (?!(?:a(?: \\(ID \\w\\d+\\))?)?$)(?:DUNGEON (?:ITEM(?=(?: a)?(?: \\(ID \\w\\d+\\))?$))?)?)?(?<itemCategory>.*?)(?: a)?(?: \\(ID \\w\\d+\\))?$",
+        "^(?:Rarity: )?(?:a )?(?:SHINY )?(?<rarity>${rarities})(?: DUNGEON)?(?: (?<itemCategory>[A-Z].*?))?(?: a)?(?: \\(ID \\w\\d+\\))?$",
     )
 
     /**
