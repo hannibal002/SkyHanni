2b35e2c2fe0395b704a515c7ec0e897a199aa81d
Luna <lunaynx@gmail.com>
2026-05-13 13:03:21 +0200

    Fix: Chocolate Factory Rendersystem Error

--- src/main/java/at/hannibal2/skyhanni/features/inventory/chocolatefactory/data/CFDataLoader.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/chocolatefactory/data/CFDataLoader.kt
@@ -14,6 +14,7 @@ import at.hannibal2.skyhanni.features.inventory.chocolatefactory.CFUpgradeWarnin
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.ConditionalUtils
+import at.hannibal2.skyhanni.utils.DelayedRun
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
@@ -236,9 +237,10 @@ object CFDataLoader {
 
     @HandleEvent
     fun onInventoryUpdated(event: InventoryUpdatedEvent) {
-        if (!CFApi.inChocolateFactory) return
-
-        updateInventoryItems(event.inventoryItems)
+        DelayedRun.runOrNextTick {
+            if (!CFApi.inChocolateFactory) return@runOrNextTick
+            updateInventoryItems(event.inventoryItems)
+        }
     }
 
     @HandleEvent
