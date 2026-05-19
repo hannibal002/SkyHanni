95015d56fc998345b481471ee6fca4eef4aa7303
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-05-19 23:57:32 +0200

    cleanup

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiContainerHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiContainerHook.kt
@@ -72,8 +72,10 @@ class GuiContainerHook(guiAny: Any) {
 
     fun onMouseClick(slot: Slot?, slotId: Int, clickedButton: Int, clickType: Int, ci: CallbackInfo) {
         val item = container.items?.takeIf { it.size > slotId && slotId >= 0 }?.get(slotId)
-        if (SlotClickEvent(gui, container, item, slot, slotId, clickedButton, ClickType.getTypeById(clickType)).post().isCancelled
-        ) ci.cancel()
+        val event = SlotClickEvent(gui, container, item, slot, slotId, clickedButton, ClickType.getTypeById(clickType))
+        if (event.post().isCancelled) {
+            ci.cancel()
+        }
     }
 
     fun onDrawScreenAfter(
