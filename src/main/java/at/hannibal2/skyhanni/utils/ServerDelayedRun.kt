9fdf78dbbc5bc7c6ff73b58144aa0e08e9104500
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-05-23 15:46:12 +0300

    create ServerDelayedRun

---
+++ src/main/java/at/hannibal2/skyhanni/utils/ServerDelayedRun.kt
@@ -0,0 +1,48 @@
+package at.hannibal2.skyhanni.utils
+
+import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.test.command.ErrorManager
+import at.hannibal2.skyhanni.utils.collection.CollectionUtils.drainTo
+import java.util.concurrent.ConcurrentLinkedQueue
+import kotlin.time.Duration
+
+@SkyHanniModule
+object ServerDelayedRun {
+    private val tasks = mutableListOf<Pair<() -> Any, ServerTimeMark>>()
+    private val futureTasks = ConcurrentLinkedQueue<Pair<() -> Any, ServerTimeMark>>()
+
+    fun runDelayed(duration: Duration, run: () -> Unit): ServerTimeMark {
+        val time = ServerTimeMark.now() + duration
+        futureTasks.add(run to time)
+        return time
+    }
+
+    fun <T> runDelayedReturning(duration: Duration, run: () -> T): Pair<ServerTimeMark, () -> T> {
+        val time = ServerTimeMark.now() + duration
+        val runnable = { run() }
+        @Suppress("UNCHECKED_CAST")
+        futureTasks.add((runnable as () -> Any) to time)
+        return time to runnable
+    }
+
+    fun runNextTick(run: () -> Unit) = futureTasks.add(run to ServerTimeMark.farPast())
+
+    fun checkRuns() {
+        tasks.removeIf { (runnable, time) ->
+            val inPast = time.isInPast()
+            if (inPast) {
+                try {
+                    runnable()
+                } catch (e: Exception) {
+                    ErrorManager.logErrorWithData(e, "ServerDelayedRun task crashed while executing: ${e.message}")
+                }
+            }
+            inPast
+        }
+        futureTasks.drainTo(tasks)
+    }
+
+    @HandleEvent
+    fun onServerTick() = checkRuns()
+}
