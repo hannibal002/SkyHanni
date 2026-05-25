8f1a4a2b0b4057e7207e75f904fb23c699879a43
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-05-25 16:50:47 +0300

    add primary function

--- src/main/java/at/hannibal2/skyhanni/events/itemabilities/ItemAbilityActivateEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/itemabilities/ItemAbilityActivateEvent.kt
@@ -2,5 +2,7 @@ package at.hannibal2.skyhanni.events.itemabilities
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 import at.hannibal2.skyhanni.features.itemabilities.abilitycooldown.ItemAbility
+import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 
+@PrimaryFunction("onItemAbilityActivate")
 class ItemAbilityActivateEvent(val ability: ItemAbility) : SkyHanniEvent()
