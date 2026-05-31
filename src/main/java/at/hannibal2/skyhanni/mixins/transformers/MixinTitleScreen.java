8e3426dbfe3944e187371224ca586c17eeaa6891
Luna <lunaynx@gmail.com>
2026-06-01 01:28:40 +0200

    chore: force load mixins in dev env

---
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinTitleScreen.java
@@ -0,0 +1,28 @@
+package at.hannibal2.skyhanni.mixins.transformers;
+
+import at.hannibal2.skyhanni.SkyHanniMod;
+import at.hannibal2.skyhanni.test.SkyHanniDebugsAndTests;
+import at.hannibal2.skyhanni.utils.system.PlatformUtils;
+import kotlin.Unit;
+import net.minecraft.client.gui.components.LogoRenderer;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public abstract class MixinTitleScreen {
+
+    @Unique
+    private static boolean skyhanni$hasInited = false;
+
+    @Inject(method = "<init>(ZLnet/minecraft/client/gui/components/LogoRenderer;)V", at = @At("RETURN"))
+    private void onCreate(boolean doBackgroundFade, LogoRenderer logoRenderer, CallbackInfo ci) {
+        if (!skyhanni$hasInited && PlatformUtils.INSTANCE.isDevEnvironment()) {
+            SkyHanniDebugsAndTests.loadAllMixinClasses();
+        }
+        skyhanni$hasInited = true;
+    }
+}
