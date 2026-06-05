b76cb7b021b09d4e46e3aa04a29fd7178b54d999
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-05 12:42:33 +0300

    okay now legit fix future test

--- src/test/java/at/hannibal2/skyhanni/test/TimeMarkTest.kt
+++ src/test/java/at/hannibal2/skyhanni/test/TimeMarkTest.kt
@@ -9,20 +9,20 @@ import kotlin.time.Duration.Companion.milliseconds
 
 class TimeMarkTest {
     @Test
-    fun `far past is not close to now`() {
+    fun `far past should not be close to now`() {
         Assertions.assertTrue(ServerTimeMark.farPast().passedSince() > 30.days)
         Assertions.assertTrue(SimpleTimeMark.farPast().passedSince() > 30.days)
+    }
+
+    @Test
+    fun `far future should not be close to now`() {
+        Assertions.assertTrue(ServerTimeMark.farFuture().timeUntil() > 30.days)
+        Assertions.assertTrue(ServerTimeMark.farFuture().timeUntil() > 30.days)
     }
 
     @Test
     fun `subtracting duration from far past does not underflow`() {
         Assertions.assertTrue(ServerTimeMark.farPast() - 30.days <= ServerTimeMark.farPast())
         Assertions.assertTrue(SimpleTimeMark.farPast() - 30.days <= SimpleTimeMark.farPast())
-    }
-
-    @Test
-    fun `the future should be in the future`() {
-        Assertions.assertTrue(ServerTimeMark.now() + 30.days < ServerTimeMark.farFuture())
-        Assertions.assertTrue(SimpleTimeMark.now() + 30.days < SimpleTimeMark.farFuture())
     }
 }
