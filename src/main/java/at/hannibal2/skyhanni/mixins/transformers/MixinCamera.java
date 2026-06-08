2a49bc408709f8bbed4a2aae80623e8f1aaab84f
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-08 20:54:26 +0300

    fix 26.1 build

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinCamera.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinCamera.java
@@ -3,6 +3,12 @@ package at.hannibal2.skyhanni.mixins.transformers;
 import net.minecraft.client.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 
+//? if >= 26.1 {
+import at.hannibal2.skyhanni.events.minecraft.FovEvent;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import org.spongepowered.asm.mixin.injection.At;
+//?}
+
 @Mixin(Camera.class)
 public class MixinCamera {
     //? if >= 26.1 {
@@ -10,13 +16,12 @@ public class MixinCamera {
         method = "calculateFov",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/client/OptionInstance;get()Ljava/lang/Object;",
-            ordinal = 0
+            target = "Lnet/minecraft/client/OptionInstance;get()Ljava/lang/Object;"
         )
     )
     private Object onBaseFov(Object original) {
         int baseFov = (Integer) original;
-        BaseFovEvent event = new BaseFovEvent(baseFov);
+        FovEvent event = new FovEvent(baseFov);
         event.post();
         return event.getResult();
     }
