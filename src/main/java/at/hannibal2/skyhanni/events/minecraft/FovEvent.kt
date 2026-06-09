1ff95202d972ce0270a54e7a74ab9254528c8ba1
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-09 17:33:06 +0300

    remove additive FOV

--- src/main/java/at/hannibal2/skyhanni/events/minecraft/FovEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/minecraft/FovEvent.kt
@@ -5,13 +5,8 @@ import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 
 @PrimaryFunction("onFov")
 class FovEvent(private var fov: Int) : SkyHanniEvent() {
-    private var additive = 0.0
     private var multiplier = 1.0
     private var currentPriority = 1000
-
-    fun add(value: Double) {
-        additive += value
-    }
 
     fun mult(factor: Double) {
         multiplier *= factor
@@ -24,6 +19,6 @@ class FovEvent(private var fov: Int) : SkyHanniEvent() {
     }
 
     fun getResult(): Int {
-        return ((fov + additive) * multiplier).toInt()
+        return (fov * multiplier).toInt()
     }
 }
