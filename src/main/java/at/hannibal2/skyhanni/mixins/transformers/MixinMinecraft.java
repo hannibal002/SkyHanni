836a907b4195ee6440bd8f52e3e95fe12950d087
Luna <lunaynx@gmail.com>
2026-06-13 14:17:59 +0200

    chore: add auto quit

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinMinecraft.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinMinecraft.java
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
 import at.hannibal2.skyhanni.mixins.hooks.MinecraftInputHook;
+import at.hannibal2.skyhanni.utils.system.PlatformUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.world.phys.HitResult;
@@ -15,7 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 //? if < 26.2 {
 /*import at.hannibal2.skyhanni.events.render.gui.GuiScreenOpenEvent;
-import at.hannibal2.skyhanni.utils.system.PlatformUtils;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.gui.screens.Screen;
 import org.objectweb.asm.Opcodes;
@@ -34,6 +34,9 @@ public abstract class MixinMinecraft {
     @Nullable
     public MultiPlayerGameMode gameMode;
 
+    @Shadow
+    public abstract void stop();
+
     //? if < 26.2 {
     /*@ModifyExpressionValue(
         method = "addInitialScreens",
@@ -48,6 +51,14 @@ public abstract class MixinMinecraft {
         return original;
     }
     *///?}
+
+    @Inject(
+        method = "onGameLoadFinished",
+        at = @At(value = "INVOKE", target = "Ljava/lang/Runnable;run()V", shift = At.Shift.AFTER)
+    )
+    private void autoQuitAfterInitialScreen(CallbackInfo ci) {
+        if (PlatformUtils.isDevEnvironment() && Boolean.getBoolean("skyhanni.autoQuit")) this.stop();
+    }
 
     @Inject(
         at = @At("HEAD"),
