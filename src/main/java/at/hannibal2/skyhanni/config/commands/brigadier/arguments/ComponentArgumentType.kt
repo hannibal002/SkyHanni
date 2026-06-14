e7c21908f34a81c64ad58d2d085e1961aa14ec02
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-14 18:56:59 +0300

    seperate json & component errors

--- src/main/java/at/hannibal2/skyhanni/config/commands/brigadier/arguments/ComponentArgumentType.kt
+++ src/main/java/at/hannibal2/skyhanni/config/commands/brigadier/arguments/ComponentArgumentType.kt
@@ -1,30 +1,42 @@
 package at.hannibal2.skyhanni.config.commands.brigadier.arguments
 
+import at.hannibal2.skyhanni.config.commands.brigadier.BrigadierUtils.escapeDoubleQuote
 import at.hannibal2.skyhanni.config.commands.brigadier.BrigadierUtils.readGreedyString
 import at.hannibal2.skyhanni.utils.json.SkyHanniTypeAdapters
+import com.google.gson.JsonParseException
+import com.google.gson.JsonParser
 import com.google.gson.TypeAdapter
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.exceptions.CommandSyntaxException
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
 import net.minecraft.network.chat.Component
 
-class ComponentArgumentType(private val allowPlainText: Boolean) : ArgumentType<Component> {
-
+class ComponentArgumentType(
+    private val allowPlainText: Boolean
+) : ArgumentType<Component> {
     override fun parse(reader: StringReader): Component {
-        val input = reader.readGreedyString()
+        val input = reader.readGreedyString().escapeDoubleQuote()
 
-        // the length check is to allow stuff like: "[:<"
-        val looksJson = input.length > 3 && (input.startsWith("{") || input.startsWith("["))
+        val looksJson =
+            input.length > 3 &&
+                (input.startsWith("{") || input.startsWith("["))
 
-        if (looksJson || !allowPlainText) {
-            try {
-                return adapter.fromJson(input)
-            } catch (_: Exception) {
-                throw INVALID_COMPONENT.create()
-            }
+        if (!looksJson && allowPlainText) {
+            return Component.literal(input)
         }
 
-        return Component.literal(input)
+        val jsonElement = try {
+            JsonParser.parseString(input)
+        } catch (_: Exception) {
+            throw INVALID_JSON.create()
+        }
+
+        return try {
+            adapter.fromJsonTree(jsonElement)
+        } catch (_: Exception) {
+            throw INVALID_COMPONENT.create()
+        }
     }
 
     companion object {
@@ -32,8 +44,11 @@ class ComponentArgumentType(private val allowPlainText: Boolean) : ArgumentType<
         fun component(allowPlainText: Boolean = false): ComponentArgumentType =
             ComponentArgumentType(allowPlainText)
 
+        private val INVALID_JSON =
+            SimpleCommandExceptionType(Component.literal("Invalid JSON"))
+
         private val INVALID_COMPONENT =
-            SimpleCommandExceptionType(Component.literal("Invalid component JSON"))
+            SimpleCommandExceptionType(Component.literal("Invalid component"))
 
         @Suppress("UNCHECKED_CAST")
         private val adapter: TypeAdapter<Component> by lazy {
