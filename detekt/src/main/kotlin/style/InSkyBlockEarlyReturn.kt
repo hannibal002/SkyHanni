849de9d511da1fc0e36ab582fe1651304fa4094e
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-17 18:30:52 +0300

    Revert "fix `InSkyBlockEarlyReturn`"
    This reverts commit 8d840bb40005db34f9df475ad5b618d787e95051.

--- detekt/src/main/kotlin/style/InSkyBlockEarlyReturn.kt
+++ detekt/src/main/kotlin/style/InSkyBlockEarlyReturn.kt
@@ -3,32 +3,29 @@ package style
 import SkyHanniRule
 import dev.detekt.api.Config
 import utils.DetektUtils.hasAnnotation
+import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtIfExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtReturnExpression
-import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 
 class InSkyBlockEarlyReturn(config: Config) : SkyHanniRule(
     config,
     ".inSkyBlock checks should be removed and replaced with onlyOnSkyblock = true in @HandleEvent annotation",
 ) {
 
-    private fun KtIfExpression.containsInSkyBlockCheck(): Boolean = text.contains("SkyBlockUtils.inSkyBlock")
-    private fun KtIfExpression.isEarlyReturn(): Boolean = then is KtReturnExpression
+    private fun KtExpression.containsInSkyBlockCheck(): Boolean = text.contains("SkyBlockUtils.inSkyBlock")
+    private fun KtExpression.isEarlyReturn(): Boolean = this is KtIfExpression && then is KtReturnExpression
 
     override fun visitNamedFunction(function: KtNamedFunction) {
         if (function.hasAnnotation("HandleEvent")) {
-            function.bodyExpression?.accept(object : KtTreeVisitorVoid() {
-                override fun visitIfExpression(expression: KtIfExpression) {
-                    if (expression.containsInSkyBlockCheck() && expression.isEarlyReturn()) {
-                        expression.reportIssue(
-                            "This early return should be replaced with onlyOnSkyblock = true in @HandleEvent annotation"
-                        )
-                    }
+            val bodyExpressions = function.bodyExpression?.collectDescendantsOfType<KtIfExpression>() ?: return
 
-                    super.visitIfExpression(expression)
+            for (ifExpression in bodyExpressions) {
+                if (ifExpression.containsInSkyBlockCheck() && ifExpression.isEarlyReturn()) {
+                    ifExpression.reportIssue("This early return should be replaced with onlyOnSkyblock = true in @HandleEvent annotation")
                 }
-            })
+            }
         }
 
         super.visitNamedFunction(function)
