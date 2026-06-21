18a2ed64e09b559579683c70eeea433c2d7d048b
Luna <lunaynx@gmail.com>
2026-06-21 14:21:21 +0200

    style: wrap long line

--- src/main/java/at/hannibal2/skyhanni/config/features/mining/orderedwaypoints/OrderedWaypointsConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/orderedwaypoints/OrderedWaypointsConfig.kt
@@ -137,7 +137,8 @@ class OrderedWaypointsConfig {
     @Expose
     @ConfigOption(
         name = "Auto-Load Shaft Routes",
-        desc = "Automatically loads a matching route if found when entering a Mineshaft. (Format is from the scoreboard, e.g. JASP_1/PERI_C)"
+        desc = "Automatically loads a matching route if found when entering a Mineshaft. " +
+            "(Format is from the scoreboard, e.g. JASP_1/PERI_C)"
     )
     @ConfigEditorBoolean
     var autoLoadMatchingShaftRoute: Boolean = false
