cd84b9d406d7f83a1d6b88150167af09a6e9f1e9
Luna <lunaynx@gmail.com>
2026-06-21 03:53:08 +0200

    fix: render 3D lines through gizmos on 26.2

--- src/main/java/at/hannibal2/skyhanni/utils/render/LineDrawer.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/render/LineDrawer.kt
@@ -7,6 +7,9 @@ import at.hannibal2.skyhanni.utils.collection.CollectionUtils.zipWithNext3
 import net.minecraft.world.phys.AABB
 import java.awt.Color
 
+//? if >= 26.2
+import net.minecraft.client.renderer.gizmos.DrawableGizmoPrimitives
+
 class LineDrawer @PublishedApi internal constructor(val event: SkyHanniRenderWorldEvent, val lineWidth: Int, val depth: Boolean) {
 
     private val queuedLines = mutableListOf<QueuedLine>()
@@ -15,28 +18,31 @@ class LineDrawer @PublishedApi internal constructor(val event: SkyHanniRenderWor
     internal fun drawQueuedLines() {
         if (queuedLines.isEmpty()) return
 
-        val layer = SkyHanniRenderLayers.getLines(!depth)
         //? if >= 26.2 {
-        event.submitNodeStorage.submitCustomGeometry(event.matrices, layer) { matrix, buf ->
-        //?} else {
-        /*val buf = event.bufferSource.getBuffer(layer)
-        val matrix = event.matrices.last()
-        *///?}
-
-            // TODO reshape to avoid code duplication
-            for (line in queuedLines) {
-                buf.addVertex(matrix.pose(), line.p1.x.toFloat(), line.p1.y.toFloat(), line.p1.z.toFloat())
-                    .setNormal(matrix, line.normal.x.toFloat(), line.normal.y.toFloat(), line.normal.z.toFloat())
-                    .setColor(line.color.red, line.color.green, line.color.blue, line.color.alpha)
-                    .setLineWidth(lineWidth.toFloat())
-
-                buf.addVertex(matrix.pose(), line.p2.x.toFloat(), line.p2.y.toFloat(), line.p2.z.toFloat())
-                    .setNormal(matrix, line.normal.x.toFloat(), line.normal.y.toFloat(), line.normal.z.toFloat())
-                    .setColor(line.color.red, line.color.green, line.color.blue, line.color.alpha)
-                    .setLineWidth(lineWidth.toFloat())
-            }
-        //? if >= 26.2
+        val gizmos = DrawableGizmoPrimitives()
+        for (line in queuedLines) {
+            gizmos.addLine(line.p1.toVec3(), line.p2.toVec3(), line.color.rgb, lineWidth.toFloat())
         }
+        gizmos.submit(event.submitNodeStorage, event.cameraState, !depth)
+        //?} else {
+        /*
+        val layer = SkyHanniRenderLayers.getLines(!depth)
+        val buf = event.bufferSource.getBuffer(layer)
+        val matrix = event.matrices.last()
+
+        // TODO reshape to avoid code duplication
+        for (line in queuedLines) {
+            buf.addVertex(matrix.pose(), line.p1.x.toFloat(), line.p1.y.toFloat(), line.p1.z.toFloat())
+                .setNormal(matrix, line.normal.x.toFloat(), line.normal.y.toFloat(), line.normal.z.toFloat())
+                .setColor(line.color.red, line.color.green, line.color.blue, line.color.alpha)
+                .setLineWidth(lineWidth.toFloat())
+
+            buf.addVertex(matrix.pose(), line.p2.x.toFloat(), line.p2.y.toFloat(), line.p2.z.toFloat())
+                .setNormal(matrix, line.normal.x.toFloat(), line.normal.y.toFloat(), line.normal.z.toFloat())
+                .setColor(line.color.red, line.color.green, line.color.blue, line.color.alpha)
+                .setLineWidth(lineWidth.toFloat())
+        }
+        *///?}
 
         queuedLines.clear()
     }
