db04afd3797f39f31c1d28b21361a8b66868ad46
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-22 18:01:15 +0300

    Update DungeonFinderFeatures.kt

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonFinderFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonFinderFeatures.kt
@@ -65,7 +65,7 @@ object DungeonFinderFeatures {
      */
     private val memberPattern by patternGroup.pattern(
         "member",
-        " (?<playerName>.*): (?<className>.*) \\((?<level>.*)\\)",
+        " (?<playerName>.*): (?<className>.*) \\(.*?(?<level>\\d+).*?\\)",
     )
 
     /**
