c2636b9c2dbcd015c238b3fc204bd7385a2875bb
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-22 16:55:33 +0300

    use shared oneSkyblockMonth variable for tests

--- src/test/java/at/hannibal2/skyhanni/test/event/HarvestFeastDataFetchingTest.kt
+++ src/test/java/at/hannibal2/skyhanni/test/event/HarvestFeastDataFetchingTest.kt
@@ -1,6 +1,7 @@
 package at.hannibal2.skyhanni.test.event
 
 import at.hannibal2.skyhanni.data.jsonobjects.elitedev.EliteFeastData
+import at.hannibal2.skyhanni.features.garden.CropType
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SimpleTimeMark.Companion.TimeProvider
 import at.hannibal2.skyhanni.utils.api.ApiUtils
@@ -13,6 +14,7 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 class HarvestFeastDataFetchingTest {
+    private val oneSkyblockMonth = 34_192.seconds
 
     @BeforeEach
     fun setUp() {
@@ -25,7 +27,7 @@ class HarvestFeastDataFetchingTest {
         SimpleTimeMark.resetTimeProvider()
     }
 
-    fun assertRemainingTime(jsonString: String, expectedTime: Duration) {
+    fun assertRemainingTime(jsonString: String, expectedTime: Duration = oneSkyblockMonth) {
         val feastData = ApiUtils.serializeNullsGson.fromJson<EliteFeastData>(jsonString)
         assertEquals(expectedTime, feastData.getActiveDuration())
     }
@@ -33,13 +35,11 @@ class HarvestFeastDataFetchingTest {
     @Test
     fun `current in-season crops have correct remaining time`() = assertRemainingTime(
         """{"complete":true,"current":["Melon","Potato","Sunflower"],"isGrandFeast":false,"month":7,"next":{"Cactus":1779309300,"Carrot":1779309300,"Cocoa Beans":1779272100,"Melon":null,"Moonflower":null,"Mushroom":null,"Nether Wart":1779272100,"Potato":null,"Pumpkin":null,"Sugar Cane":1779309300,"Sunflower":null,"Wheat":null,"Wild Rose":1779272100},"year":491}""",
-        34_192.seconds,
-    )
+        )
 
     @Test
     fun `current in-season crops have correct remaining time for last rotation`() = assertRemainingTime(
         """{"complete":true,"current":["Melon","Potato","Sunflower"],"isGrandFeast":false,"month":7,"next":{"Cactus":null,"Carrot":null,"Cocoa Beans":null,"Melon":null,"Moonflower":null,"Mushroom":null,"Nether Wart":null,"Potato":null,"Pumpkin":null,"Sugar Cane":null,"Sunflower":null,"Wheat":null,"Wild Rose":null},"year":491}""",
-        34_192.seconds,
     )
 
     companion object {
