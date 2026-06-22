65ca89bd90bdd08d6fcca84fc1683dd3c42081b5
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-06-22 09:53:34 +0200

    fix config version

--- src/main/java/at/hannibal2/skyhanni/features/garden/visitor/GardenVisitorShoppingList.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/visitor/GardenVisitorShoppingList.kt
@@ -378,9 +378,9 @@ object GardenVisitorShoppingList {
 
     @HandleEvent
     fun onConfigFix(event: ConfigUpdaterMigrator.ConfigFixEvent) {
-        event.transform(135, "garden.visitors.shoppingList.ignoreSpaceman") { entry ->
+        event.transform(137, "garden.visitors.shoppingList.ignoreSpaceman") { entry ->
             if (entry.asBoolean) {
-                event.add(135, "#profile.garden.ignoredVisitors") {
+                event.add(137, "#profile.garden.ignoredVisitors") {
                     JsonArray().apply { add("Spaceman") }
                 }
             }
