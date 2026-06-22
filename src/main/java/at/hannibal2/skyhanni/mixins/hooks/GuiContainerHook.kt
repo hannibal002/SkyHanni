f6542bac2802858d6edc9e94eebf5f63ad7567ac
Luna <lunaynx@gmail.com>
2026-06-22 15:06:20 +0200

    style: detekt

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiContainerHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiContainerHook.kt
@@ -71,7 +71,7 @@ class GuiContainerHook(guiAny: Any) {
     }
 
     fun onMouseClick(slot: Slot?, slotId: Int, clickedButton: Int, clickType: ContainerInput, ci: CallbackInfo) {
-        val item = container.items?.takeIf { it.size > slotId && slotId >= 0 }?.get(slotId)
+        val item = container.items.takeIf { it.size > slotId && slotId >= 0 }?.get(slotId)
         val event = SlotClickEvent(gui, container, item, slot, slotId, clickedButton, clickType)
         if (event.post().isCancelled) {
             ci.cancel()
