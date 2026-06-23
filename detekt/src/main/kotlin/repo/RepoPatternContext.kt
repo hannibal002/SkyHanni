21669b358e7c557100d246f647d1b731e4ad4e93
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-23 18:08:12 +0300

    cache failing to parse REGEX-TEST too

--- detekt/src/main/kotlin/repo/RepoPatternContext.kt
+++ detekt/src/main/kotlin/repo/RepoPatternContext.kt
@@ -5,12 +5,18 @@ import repo.RepoPatternElement.Companion.asRepoPatternElement
 import java.util.IdentityHashMap
 
 class RepoPatternContext {
-    private val cache = IdentityHashMap<KtPropertyDelegate, RepoPatternElement>()
+    private val cache = IdentityHashMap<KtPropertyDelegate, RepoPatternElement?>()
 
-    fun getRepoPatternElement(property: KtPropertyDelegate): RepoPatternElement? =
-        cache.getOrPut(property) {
-            property.asRepoPatternElement() ?: return null
+    // This intentionally caches null values, so that we don't have to reparse the same property delegate multiple times.
+    fun getRepoPatternElement(property: KtPropertyDelegate): RepoPatternElement? {
+        if (cache.containsKey(property)) {
+            return cache[property]
         }
+
+        val element = property.asRepoPatternElement()
+        cache[property] = element
+        return element
+    }
 
     data class RepoPatternTestContext(
         val element: RepoPatternElement,
