378fff25b204ec8c599338d18f6d7624f6ca9e43
Piggered <19157702+Piggered@users.noreply.github.com>
2026-06-25 15:36:39 -0400

    improve handling of forge recipes & tweak messages

--- src/main/java/at/hannibal2/skyhanni/config/features/inventory/InventoryConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/inventory/InventoryConfig.kt
@@ -80,7 +80,7 @@ class InventoryConfig {
     val craftableItemList: CraftableItemListConfig = CraftableItemListConfig()
 
     @Expose
-    @Category(name = "Compact Item Recipes", desc = "")
+    @Category(name = "Compact Item Recipes", desc = "Quickly open recipes for compacting items.")
     @Accordion
     val compactItemFormRecipe: CompactItemRecipeConfig = CompactItemRecipeConfig()
 
--- src/main/java/at/hannibal2/skyhanni/features/inventory/CompactedItemFormRecipe.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/CompactedItemFormRecipe.kt
@@ -2,21 +2,24 @@ package at.hannibal2.skyhanni.features.inventory
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.config.features.inventory.CompactItemRecipeConfig
 import at.hannibal2.skyhanni.data.jsonobjects.repo.CompactedItemFormRecipeEntry
 import at.hannibal2.skyhanni.data.jsonobjects.repo.CompactedItemFormRecipesJson
 import at.hannibal2.skyhanni.data.jsonobjects.repo.neu.recipe.NeuRecipeType
 import at.hannibal2.skyhanni.events.RepositoryReloadEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.HypixelCommands
+import at.hannibal2.skyhanni.utils.ItemUtils.getInternalName
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalNameOrNull
 import at.hannibal2.skyhanni.utils.ItemUtils.repoItemName
 import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyHeld
-import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.NeuInternalName
-import at.hannibal2.skyhanni.utils.compat.appendWithColor
 import at.hannibal2.skyhanni.utils.compat.componentBuilder
+import at.hannibal2.skyhanni.utils.compat.formattedTextCompat
 import at.hannibal2.skyhanni.utils.compat.stackUnderCursor
+import net.minecraft.network.chat.Component
 
 @SkyHanniModule
 object CompactedItemFormRecipe {
@@ -31,26 +34,48 @@ object CompactedItemFormRecipe {
         val recipe = recipes[item]
 
         if (recipe == null) {
-            ChatUtils.chat(
-                componentBuilder {
-                    append(stack.hoverName)
-                    appendWithColor(" does not have a recipe for a compacted form!", LorenzColor.RED.toChatFormatting())
-                }
-            )
+            val itemName = stack.hoverName.formattedTextCompat()
+            ChatUtils.chat("$itemName§c does not have a recipe for a compacted form.")
             return
         }
-        
+
         when (recipe.type) {
             NeuRecipeType.CRAFTING -> HypixelCommands.viewRecipe(recipe.result)
-            NeuRecipeType.FORGE -> ChatUtils.chat(
-                componentBuilder {
-                    append(stack.hoverName)
-                    append(" can be compacted into ")
-                    append(recipe.result.repoItemName)
-                    append(" at the Forge.")
-                }
+            NeuRecipeType.FORGE -> sendForgeRecipeMessage(stack.hoverName, recipe.result.repoItemName)
+            else -> ErrorManager.logErrorWithData(
+                IllegalStateException("Unsupported NeuRecipeType"),
+                "Unsupported recipe type for compacted form",
+                "type" to recipe.type,
+                "ingredient" to stack.getInternalName(),
+                "result" to recipe.result,
             )
-            else -> {}
+        }
+    }
+
+    private fun sendForgeRecipeMessage(ingredientName: Component, resultName: String) {
+        val message = componentBuilder {
+            append(ingredientName)
+            append(" can be compacted into ")
+            append(resultName)
+            append(" at the Forge.")
+        }.formattedTextCompat()
+
+        when (config.forgeRecipeAction) {
+            CompactItemRecipeConfig.ForgeRecipeAction.NONE -> ChatUtils.chat(message)
+            CompactItemRecipeConfig.ForgeRecipeAction.WARP_FORGE -> {
+                ChatUtils.clickableChat(
+                    "$message Click §lHERE§r§e to warp to the Forge!",
+                    { HypixelCommands.warp("forge") },
+                    "§eClick to warp to the Forge!",
+                )
+            }
+            CompactItemRecipeConfig.ForgeRecipeAction.CALL_FRED -> {
+                ChatUtils.clickableChat(
+                    "$message Click §lHERE§r§e to call Fred!",
+                    { HypixelCommands.call("fred") },
+                    "§eClick to call Fred!",
+                )
+            }
         }
     }
 
