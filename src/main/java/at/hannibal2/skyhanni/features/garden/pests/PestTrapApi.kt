961aa8bdeada666971798d1bdd8b2d87a59d3c05
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-25 19:25:41 +0300

    wait a second, an enum can't get garbage collected

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestTrapApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestTrapApi.kt
@@ -18,7 +18,6 @@ import at.hannibal2.skyhanni.utils.collection.CollectionUtils.enumMapOf
 import at.hannibal2.skyhanni.utils.collection.CollectionUtils.takeIfNotEmpty
 import at.hannibal2.skyhanni.utils.collection.TimeLimitedCache
 import com.google.common.cache.RemovalCause.EXPIRED
-import com.google.common.cache.RemovalCause.COLLECTED
 import net.minecraft.network.chat.Component
 import java.util.regex.Matcher
 import kotlin.time.Duration.Companion.seconds
@@ -132,7 +131,7 @@ object PestTrapApi {
     private fun baseWidgetStatus() = TimeLimitedCache<TabWidget, Boolean>(
         expireAfterWrite = 30.seconds,
         removalListener = { key, _, removalCause ->
-            if (removalCause != EXPIRED && removalCause != COLLECTED) return@TimeLimitedCache
+            if (removalCause != EXPIRED) return@TimeLimitedCache
             widgetErrors.addOrPut(key ?: return@TimeLimitedCache, 1)
         },
     )
