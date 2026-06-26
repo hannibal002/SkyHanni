b85b39aa7a1244673cdd5d26ffd4fad7f5d9f2da
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-26 19:16:56 +0300

    revert build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -355,14 +355,10 @@ tasks.withType<KotlinCompile> {
                            else target.minecraftVersion.formattedJavaLanguageVersion
         jvmTarget.set(JvmTarget.fromTarget(jvmTargetStr))
         optIn.addAll("kotlin.concurrent.atomics.ExperimentalAtomicApi")
-        freeCompilerArgs.addAll(
-            // 0 (all cores) triggers a race condition in JvmIrCodegenFactory's parallel codegen on Kotlin 2.3.x,
-            // leaving corrupt .class files that break subsequent incremental builds.
-            // see: https://youtrack.jetbrains.com/issue/KT-85498/
-            "-Xbackend-threads=1",
-            // This is so that workflows logs look cleaner, IntelliJ shows the warnings in the IDE anyway
-            "-Xsuppress-warnings=DEPRECATION"
-        )
+        // 0 (all cores) triggers a race condition in JvmIrCodegenFactory's parallel codegen on Kotlin 2.3.x,
+        // leaving corrupt .class files that break subsequent incremental builds.
+        // see: https://youtrack.jetbrains.com/issue/KT-85498/
+        freeCompilerArgs.addAll("-Xbackend-threads=1")
     }
 }
 
