ea8a2aecccd4d8f9680ed337327cc376ee3e09a8
fazfoxy <78978526+Fazfoxy@users.noreply.github.com>
2026-06-27 02:08:30 +0100

    Hanni Review comments from 3 days ago

--- src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureLootshareSphere.kt
+++ src/main/java/at/hannibal2/skyhanni/features/fishing/SeaCreatureLootshareSphere.kt
@@ -8,21 +8,15 @@ import at.hannibal2.skyhanni.features.fishing.seaCreatureXMLGui.SeaCreatureSetti
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.LootshareUtils
 import at.hannibal2.skyhanni.utils.LorenzColor
-import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.render.WorldRenderUtils.drawSphereWireframeInWorld
 
 @SkyHanniModule
 object SeaCreatureLootshareSphere {
     private val config get() = SkyHanniMod.feature.fishing
 
-    private val seaCreatures = mutableListOf<LivingSeaCreatureData>()
+    private val seaCreatures = mutableSetOf<LivingSeaCreatureData>()
 
-    private val spherePositions = mutableListOf<LootshareSphere>()
-
-    data class LootshareSphere(
-        val position: LorenzVec? = null,
-        var color: LorenzColor = LorenzColor.WHITE,
-    )
+    private var spherePositions = mutableSetOf<LootshareUtils.Sphere>()
 
     @HandleEvent
     fun onSeaCreatureSpawn(event: SeaCreatureEvent.Spawn) = addMob(event.seaCreature)
@@ -33,25 +27,29 @@ object SeaCreatureLootshareSphere {
     @HandleEvent(onlyOnSkyblock = true)
     fun onRenderWorld(event: SkyHanniRenderWorldEvent) {
         if (!config.lootshareRange) return
-        scLoop@ for (lootshareSphere in spherePositions) {
-            if (lootshareSphere.position == null) return
+        val sphereIterator = spherePositions.iterator()
+        while (sphereIterator.hasNext()) {
+            val lootshareSphere = sphereIterator.next()
             event.drawSphereWireframeInWorld(lootshareSphere.color.toColor(), lootshareSphere.position, LootshareUtils.RANGE)
-            spherePositions.remove(lootshareSphere)
+            sphereIterator.remove()
         }
     }
 
     @HandleEvent(onlyOnSkyblock = true)
     fun onTick() {
         if (!config.lootshareRange) return
-        val seaCreaturesToRender = mutableSetOf<LorenzVec>()
         for (seaCreature in seaCreatures) {
             if (!seaCreature.exists()) continue
             var otherNearbySpheres = 0
             val pos = seaCreature.pos ?: continue
             val color = if (seaCreature.isOwn || LootshareUtils.isInRange(pos)) LorenzColor.GREEN else LorenzColor.WHITE
-            seaCreaturesToRender.forEach {
-                if (it.distance(pos) < 10) otherNearbySpheres++
-                if (otherNearbySpheres < 2) spherePositions.add(LootshareSphere(pos, color))
+
+            for (lootshareSphere in spherePositions) {
+                val position = lootshareSphere.position
+                if (position.distance(pos) < 10) otherNearbySpheres++
+            }
+            if (otherNearbySpheres < 2) {
+                spherePositions.add(LootshareUtils.Sphere(pos, color))
             }
         }
     }
--- src/main/java/at/hannibal2/skyhanni/utils/LootshareUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/LootshareUtils.kt
@@ -6,5 +6,10 @@ object LootshareUtils {
 
     const val RANGE = 30.0f
 
+    data class Sphere(
+        val position: LorenzVec,
+        var color: LorenzColor = LorenzColor.WHITE,
+    )
+
     fun isInRange(pos: LorenzVec): Boolean = pos.distanceToPlayer() < RANGE
 }
