0f488627a54b02e58f3fdd6366f04d28b2e90529
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-28 11:40:08 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/minion/MinionFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/minion/MinionFeatures.kt
@@ -48,7 +48,6 @@ import at.hannibal2.skyhanni.utils.RegexUtils.matchMatcher
 import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderable
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
-import at.hannibal2.skyhanni.utils.SkyBlockUtils
 import at.hannibal2.skyhanni.utils.TimeUtils.format
 import at.hannibal2.skyhanni.utils.chat.TextHelper.asComponent
 import at.hannibal2.skyhanni.utils.compat.EntityCompat.deceased
