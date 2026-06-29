93bcc4090ba7de5ab7fc034addcabd0d0104558c
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-06-29 11:19:31 +0200

    fix merge conflicts

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinMouse.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinMouse.java
@@ -9,7 +9,6 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.MouseHandler;
 import net.minecraft.client.input.MouseButtonInfo;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.client.input.MouseButtonInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
