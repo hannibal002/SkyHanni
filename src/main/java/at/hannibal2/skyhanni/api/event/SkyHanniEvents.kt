21e8dd17b2408e073f74d0ed8674e29ada80d593
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-06-30 20:34:10 +0300

    cache disabled invoker check too

--- src/main/java/at/hannibal2/skyhanni/api/event/SkyHanniEvents.kt
+++ src/main/java/at/hannibal2/skyhanni/api/event/SkyHanniEvents.kt
@@ -95,6 +95,7 @@ object SkyHanniEvents {
         val data = event.getConstant<DisabledEventsJson>("DisabledEvents")
         disabledHandlers = data.disabledHandlers
         disabledHandlerInvokers = data.disabledInvokers
+        EventListeners.markEventCacheDirty()
     }
 
     val seconds = listOf(10, 60, 60 * 5)
