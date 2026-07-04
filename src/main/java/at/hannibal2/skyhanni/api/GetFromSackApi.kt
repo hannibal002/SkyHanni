25d20e369f9ffeb28c98dd53eb405a762f24beab
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-07-04 12:50:51 +0200

    cleanup

--- src/main/java/at/hannibal2/skyhanni/api/GetFromSackApi.kt
+++ src/main/java/at/hannibal2/skyhanni/api/GetFromSackApi.kt
@@ -13,8 +13,6 @@ import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.test.command.ErrorManager
 import at.hannibal2.skyhanni.utils.Calculator
 import at.hannibal2.skyhanni.utils.ChatUtils
-import at.hannibal2.skyhanni.utils.ChatUtils.isCommand
-import at.hannibal2.skyhanni.utils.ChatUtils.senderIsSkyhanni
 import at.hannibal2.skyhanni.utils.ClipboardUtils
 import at.hannibal2.skyhanni.utils.HypixelCommands
 import at.hannibal2.skyhanni.utils.NeuInternalName
