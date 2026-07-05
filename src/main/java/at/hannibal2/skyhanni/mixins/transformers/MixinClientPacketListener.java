9c522c3bd5a33aa9702935779061b2d1b76cf68b
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-05 17:53:08 +0300

    Revert "Fix: Sodium-Extra incompatiblity (#5991)"
    This reverts commit 9985addde84fef235c86eafea2359567c84c1159.

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinClientPacketListener.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinClientPacketListener.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ClientPacketListener.class, priority = 0)
+@Mixin(ClientPacketListener.class)
 public class MixinClientPacketListener {
 
     @ModifyExpressionValue(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/DebugScreenOverlay;showNetworkCharts()Z"))
