3a4e2b78730fe2c5d780d4d94bbace580781407b
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-07 14:06:19 +0300

    simplify event creation

--- src/main/java/at/hannibal2/skyhanni/utils/ParticleUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/ParticleUtils.kt
@@ -2,42 +2,33 @@ package at.hannibal2.skyhanni.utils
 
 import at.hannibal2.skyhanni.events.ParticleDetectedEvent
 import at.hannibal2.skyhanni.events.ParticleReceivedEvent
-import net.minecraft.core.particles.ParticleType
 import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket
 import net.minecraft.resources.Identifier
 
 object ParticleUtils {
-    private data class ParticlePacketData(
-        val type: ParticleType<*>,
-        val location: LorenzVec,
-        val count: Int,
-        val speed: Float,
-        val offset: LorenzVec,
-        val longDistance: Boolean,
-    )
-
-    fun getParticleTypeByName(name: String, shouldError: Boolean = false): Identifier = Identifier.parse(name.lowercase())
+    fun getParticleTypeByName(name: String): Identifier = Identifier.parse(name.lowercase())
 
     @JvmStatic
     fun postReceivedParticleEvent(packet: ClientboundLevelParticlesPacket): Boolean {
-        val data = packet.toParticlePacketData()
-        val event = ParticleReceivedEvent(data.type, data.location, data.count, data.speed, data.offset, data.longDistance)
-        return event.post()
+        return ParticleReceivedEvent(
+            type = packet.particle.type,
+            location = packet.toLorenzVec(),
+            count = packet.count,
+            speed = packet.maxSpeed,
+            offset = LorenzVec(packet.xDist, packet.yDist, packet.zDist),
+            longDistance = packet.isOverrideLimiter,
+        ).post()
     }
 
     @JvmStatic
-    fun postDetectedParticleEvent(packet: ClientboundLevelParticlesPacket): Boolean {
-        val data = packet.toParticlePacketData()
-        val event = ParticleDetectedEvent(data.type, data.location, data.count, data.speed, data.offset, data.longDistance)
-        return event.post()
+    fun postDetectedParticleEvent(packet: ClientboundLevelParticlesPacket) {
+        ParticleDetectedEvent(
+            type = packet.particle.type,
+            location = packet.toLorenzVec(),
+            count = packet.count,
+            speed = packet.maxSpeed,
+            offset = LorenzVec(packet.xDist, packet.yDist, packet.zDist),
+            longDistance = packet.isOverrideLimiter,
+        ).post()
     }
-
-    private fun ClientboundLevelParticlesPacket.toParticlePacketData() = ParticlePacketData(
-        type = particle.type,
-        location = toLorenzVec(),
-        count = count,
-        speed = maxSpeed,
-        offset = LorenzVec(xDist, yDist, zDist),
-        longDistance = isOverrideLimiter,
-    )
 }
