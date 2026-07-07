127c4cc73f053636cfbb59a704109eb56ae5831e
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-07 15:08:43 +0300

    Backend: Fix all current import orders (#5985)

--- src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiRendererHook.kt
+++ src/main/java/at/hannibal2/skyhanni/mixins/hooks/GuiRendererHook.kt
@@ -17,13 +17,13 @@ import com.mojang.blaze3d.systems.RenderPass
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.font.glyphs.BakedSheetGlyph.GlyphInstance
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer
+import net.minecraft.client.renderer.MultiBufferSource
+import net.minecraft.client.renderer.feature.FeatureRenderDispatcher
 import net.minecraft.client.renderer.state.gui.BlitRenderState
 import net.minecraft.client.renderer.state.gui.GlyphRenderState
 import net.minecraft.client.renderer.state.gui.GuiElementRenderState
 import net.minecraft.client.renderer.state.gui.GuiRenderState
 import net.minecraft.client.renderer.state.gui.pip.PictureInPictureRenderState
-import net.minecraft.client.renderer.MultiBufferSource
-import net.minecraft.client.renderer.feature.FeatureRenderDispatcher
 
 object GuiRendererHook {
     var chromaUniform = SkyHanniChromaUniform()
