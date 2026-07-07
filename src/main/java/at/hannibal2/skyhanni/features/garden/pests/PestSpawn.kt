487661a1208f245aca00dbaad0f7003ca21501b9
heyngra <heyngra.wspolpraca@gmail.com>
2026-07-07 23:59:16 +0200

    fix(PestSpawn): pest spawn message appearing twice

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawn.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawn.kt
@@ -27,32 +27,35 @@ object PestSpawn {
     /**
      * REGEX-TEST: GROSS! A ൠ Pest has appeared in Plot - S 4!
      * REGEX-TEST: GROSS! A ൠ Pest has appeared in The Barn!
+     * REGEX-FAIL: [SkyHanni] GROSS! A ൠ Pest has appeared in Plot - S 4!
      * REGEX-FAIL: From [MVP+] ThePleader: GROSS! A ൠ Pest has appeared in Plot - 67!
      */
     private val onePestPattern by patternGroup.list(
         "one.colorless",
-        "^\\w+! A ൠ Pest has appeared in Plot - (?<plot>.*)!",
-        "^\\w+! A ൠ Pest has appeared in (?<plot>The Barn)!",
+        "^(?!\\[SkyHanni])\\w+! A ൠ Pest has appeared in Plot - (?<plot>.*)!",
+        "^(?!\\[SkyHanni])\\w+! A ൠ Pest has appeared in (?<plot>The Barn)!",
     )
 
     /**
      * REGEX-TEST: YUCK! 4 ൠ Pest have spawned in Plot - 14!
      * REGEX-TEST: YUCK! 4 ൠ Pest have spawned in The Barn!
+     * REGEX-TEST: [SkyHanni] YUCK! 4 ൠ Pest have spawned in Plot - 14!
      * REGEX-FAIL: From [MVP+] ThePleader: YUCK! 6 ൠ Pest have spawned in Plot - 7!
      */
     private val multiplePestsPattern by patternGroup.list(
         "multiple.colorless",
-        "^\\w+! (?<amount>\\d) ൠ Pests? have spawned in Plot - (?<plot>.*)!",
-        "^\\w+! (?<amount>\\d) ൠ Pests? have spawned in (?<plot>The Barn)!",
+        "^(?!\\[SkyHanni])\\w+! (?<amount>\\d) ൠ Pests? have spawned in Plot - (?<plot>.*)!",
+        "^(?!\\[SkyHanni])\\w+! (?<amount>\\d) ൠ Pests? have spawned in (?<plot>The Barn)!",
     )
 
     /**
      * REGEX-TEST: GROSS! While you were offline, ൠ Pest spawned in Plots 12, 9, 5, 11 and 3!
+     * REGEX-FAIL: [SkyHanni] GROSS! While you were offline, ൠ Pest spawned in Plots 6 and 7!
      * REGEX-FAIL: From [MVP+] ThePleader: GROSS! While you were offline, ൠ Pest spawned in Plots 6 and 7!
      */
     private val offlinePestsPattern by patternGroup.pattern(
         "offline.colorless",
-        "^\\w+! While you were offline, ൠ Pests? spawned in Plots (?<plots>.*)!",
+        "^(?!\\[SkyHanni])\\w+! While you were offline, ൠ Pests? spawned in Plots (?<plots>.*)!",
     )
 
     /**
