711144d18092a11768c886f6cd531cdfc1b96d57
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-07 14:15:26 +0300

    add KDoc

--- src/main/java/at/hannibal2/skyhanni/events/WorldEvents.kt
+++ src/main/java/at/hannibal2/skyhanni/events/WorldEvents.kt
@@ -4,10 +4,19 @@ import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 import at.hannibal2.skyhanni.utils.LorenzVec
 
 /**
- * This class is a placeholder for world-related events.
+ * Base class for world-related events.
+ *
+ * Used by TrackerCommands and other built-in utilities that need to debug or interact
+ * with world events based on their location, such as particles and sounds.
  */
 abstract class WorldEvent : SkyHanniEvent() {
+    /**
+     * The location of this event.
+     */
     abstract val location: LorenzVec
 }
 
+/**
+ * Base class for cancellable world events.
+ */
 abstract class CancellableWorldEvent : WorldEvent(), SkyHanniEvent.Cancellable
