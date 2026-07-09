6dd6ae6ef89ec87b44b8c822f3c6aaf0909024ad
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-10 00:47:52 +0300

    fix KDoc

--- src/main/java/at/hannibal2/skyhanni/events/skyblock/AreaChangeEvents.kt
+++ src/main/java/at/hannibal2/skyhanni/events/skyblock/AreaChangeEvents.kt
@@ -16,14 +16,15 @@ open class AreaChangeEvent(val area: AreaType, val previousArea: AreaType?) : Sk
 }
 
 /**
- * For detecting area changes in the scoreboard, which is less accurate than the graph.
+ * For detecting area changes in the scoreboard.
  * Only use this if you must specifically use the scoreboard area.
  */
 @PrimaryFunction("onScoreboardAreaChange")
 class ScoreboardAreaChangeEvent(val area: String, val previousArea: String?) : SkyHanniEvent()
 
 /**
- * For detecting area changes in the graph, which is more accurate than the scoreboard.
+ * For detecting area changes via the graph.
+ * Only use this if you must specifically use the graph area.
  */
 @PrimaryFunction("onGraphAreaChange")
 class GraphAreaChangeEvent(val area: String, val previousArea: String?, val onlyInternal: Boolean) : SkyHanniEvent()
