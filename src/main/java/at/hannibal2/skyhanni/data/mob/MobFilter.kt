e8ccab5ca5dbd4b7c28fb4c5213404943cb0ea92
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-12 00:18:53 +0300

    Update MobFilter.kt

--- src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
+++ src/main/java/at/hannibal2/skyhanni/data/mob/MobFilter.kt
@@ -112,9 +112,9 @@ object MobFilter {
     )
 
     /**
-     * REGEX-TEST: [Lv1] ✰ Throwpo's Green Jerry 3 Hits
-     * REGEX-TEST: [Lv1] ✰ RecluseFang's Green Jerry 3 Hits
-     * REGEX-TEST: [Lv1] ✰ aThunderblade73's Green Jerrya 7 Hits
+     * REGEX-TEST: [Lv1]  Throwpo's Green Jerry 3 Hits
+     * REGEX-TEST: [Lv1]  RecluseFang's Green Jerry 3 Hits
+     * REGEX-TEST: [Lv1]  aThunderblade73's Green Jerrya 7 Hits
      */
     val jerryPattern by patternGroup.pattern(
         "jerry",
