d3ae3fb58155f3f428f75dda1603301e1ed31c73
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-13 20:06:05 +0300

    ToolTipTextEvent is special

--- src/main/java/at/hannibal2/skyhanni/events/minecraft/ToolTipTextEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/minecraft/ToolTipTextEvent.kt
@@ -2,11 +2,25 @@ package at.hannibal2.skyhanni.events.minecraft
 
 import at.hannibal2.skyhanni.api.event.CancellableSkyHanniEvent
 import at.hannibal2.skyhanni.api.event.Thread
+import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 import at.hannibal2.skyhanni.utils.SafeItemStack
 import net.minecraft.network.chat.Component
 import net.minecraft.world.inventory.Slot
 
-@Thread(RENDER)
+/**
+ * Event that is called when the tooltip text of an item is being generated.
+ * This event is cancellable, meaning that you can cancel it to prevent the tooltip from being shown.
+ * You can also modify the tooltip by adding or removing lines from the `toolTip` list.
+ *
+ * This event is supposed to only be called from the render thread, but due to other mods like JEI
+ * needing to search using tooltips, it can be called from other threads as well.
+ *
+ * @param slot The slot that the item is in, or null if the item is not in a slot.
+ * @param itemStack The item stack that the tooltip is being generated for.
+ * @param toolTip The list of lines that will be shown in the tooltip. You can modify this list to change the tooltip.
+ */
+@Thread(ANY)
+@PrimaryFunction("onToolTip")
 class ToolTipTextEvent(val slot: Slot?, val itemStack: SafeItemStack, val toolTip: MutableList<Component>) : CancellableSkyHanniEvent()
 
 fun MutableList<Component>.add(index: Int, string: String) {
