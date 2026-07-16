3285be625c409cc050cd04a3749f92f0ae178176
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-16 20:35:30 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestFinder.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestFinder.kt
@@ -9,7 +9,6 @@ import at.hannibal2.skyhanni.config.features.garden.pests.PestFinderConfig.WhenT
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.model.TabWidget
 import at.hannibal2.skyhanni.data.title.TitleManager
-import at.hannibal2.skyhanni.events.IslandJoinEvent
 import at.hannibal2.skyhanni.events.PlaySoundEvent
 import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.events.garden.pests.PestUpdateEvent
