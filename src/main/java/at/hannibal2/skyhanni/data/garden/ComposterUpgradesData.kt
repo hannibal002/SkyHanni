7e6e41b856c852f573239c16ccb31962ab6c92cb
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-17 17:12:45 +0300

    woops

--- src/main/java/at/hannibal2/skyhanni/data/garden/ComposterUpgradesData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/garden/ComposterUpgradesData.kt
@@ -14,14 +14,10 @@ import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 @SkyHanniModule
 object ComposterUpgradesData {
-    val composterUpdateInventoryPattern by RepoPattern.pattern(
-        "composter.upgrade.inventory",
-        "Composter Upgrades",
-    )
 
     @HandleEvent(onlyOnIsland = IslandType.GARDEN)
     fun onInventoryFullyOpened(event: InventoryFullyOpenedEvent) {
-        if (!composterUpdateInventoryPattern.matches(event.inventoryName)) return
+        if (event.inventoryName != "Composter Upgrades") return
         for (item in event.inventoryItems.values) {
             ComposterUpgrade.composterUpgradePattern.matchMatcher(item.cleanName) {
                 val name = group("name")
