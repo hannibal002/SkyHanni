8a9bbadd5acfce9b435198ba2d52fc3165f2d319
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-18 16:03:45 +0300

    no static import

--- src/main/java/at/hannibal2/skyhanni/data/garden/ComposterUpgradesData.kt
+++ src/main/java/at/hannibal2/skyhanni/data/garden/ComposterUpgradesData.kt
@@ -3,7 +3,6 @@ package at.hannibal2.skyhanni.data.garden
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.data.model.ComposterUpgrade
-import at.hannibal2.skyhanni.data.model.ComposterUpgrade.entries
 import at.hannibal2.skyhanni.events.InventoryFullyOpenedEvent
 import at.hannibal2.skyhanni.features.garden.composter.ComposterApi
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
@@ -26,7 +25,7 @@ object ComposterUpgradesData {
         "composter.upgrade.name",
         "(?<name>${regexValues()})(?: (?<level>.*))?",
     )
-    private fun regexValues() = entries.joinToString("|") { it.displayName }
+    private fun regexValues() = ComposterUpgrade.entries.joinToString("|") { it.displayName }
 
     @HandleEvent(onlyOnIsland = IslandType.GARDEN)
     fun onInventoryFullyOpened(event: InventoryFullyOpenedEvent) {
