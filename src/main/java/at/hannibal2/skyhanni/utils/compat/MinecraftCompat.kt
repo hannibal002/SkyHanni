88cbf8d745aa29f92b48ef5802379c9c0251692f
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-07-19 19:49:18 +0200

    formatting

--- src/main/java/at/hannibal2/skyhanni/utils/compat/MinecraftCompat.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/compat/MinecraftCompat.kt
@@ -40,7 +40,6 @@ object MinecraftCompat {
     val localWorldExists get(): Boolean = localWorldOrNull != null
     // </editor-fold>
 
-
     // <editor-fold desc="User">
     /**
      * The local user's information, such as the username and UUID.
