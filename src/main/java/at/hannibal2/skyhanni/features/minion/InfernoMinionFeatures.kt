36fa4ea99504e5086ca4b703b889cad55b8f6349
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-19 15:15:54 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/minion/InfernoMinionFeatures.kt
+++ src/main/java/at/hannibal2/skyhanni/features/minion/InfernoMinionFeatures.kt
@@ -33,7 +33,7 @@ object InfernoMinionFeatures {
      */
     val infernoMinionTitlePattern by RepoPattern.pattern(
         "minion.infernominiontitle",
-        "Inferno Minion ([IVX]{1,4}|\\d+)$",
+        "Inferno Minion (?:[IVX]{1,4}|\\d+)$",
     )
     var fuelItemIds = setOf<NeuInternalName>()
         private set
