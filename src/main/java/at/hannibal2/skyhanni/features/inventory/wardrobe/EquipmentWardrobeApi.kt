32902496cd044303fb316596a484377c823c49d9
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-07-19 17:50:31 +0200

    fix detekt

--- src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/EquipmentWardrobeApi.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/EquipmentWardrobeApi.kt
@@ -3,7 +3,6 @@ package at.hannibal2.skyhanni.features.inventory.wardrobe
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.ProfileStorageData
 import at.hannibal2.skyhanni.events.DebugDataCollectEvent
-import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryOpenEvent
 import at.hannibal2.skyhanni.events.InventoryUpdatedEvent
 import at.hannibal2.skyhanni.features.inventory.EquipmentApi
