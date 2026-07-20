aadd00301b3d4ebf71b4bc9d0c4351cf63862ec6
Luna <lunaynx@gmail.com>
2026-07-21 00:33:47 +0200

    test: add mixin test

---
+++ src/test/java/at/hannibal2/skyhanni/test/MixinTest.kt
@@ -0,0 +1,16 @@
+package at.hannibal2.skyhanni.test
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.spongepowered.asm.mixin.MixinEnvironment
+import org.spongepowered.asm.mixin.transformer.IMixinTransformer
+
+class MixinTest {
+
+    @Test
+    fun `mixins load successfully`() {
+        val environment = MixinEnvironment.getCurrentEnvironment()
+        Assertions.assertInstanceOf(IMixinTransformer::class.java, environment.activeTransformer)
+        environment.audit()
+    }
+}
