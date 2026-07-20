63e2ae3ef9930d188d868434928653b7bf9d05e8
Luna <lunaynx@gmail.com>
2026-07-20 13:35:17 +0200

    build: add Hypixel quick play to prod client

--- build.gradle.kts
+++ build.gradle.kts
@@ -188,6 +188,7 @@ tasks.register<ClientProductionRunTask>("prodClient") {
     notCompatibleWithConfigurationCache("Interactive client launches must start a new process every time.")
     outputs.upToDateWhen { false }
     runDir = file("run")
+    programArgs.addAll("--quickPlayMultiplayer", "hypixel.net")
 }
 
 if (target == primaryTarget) {
