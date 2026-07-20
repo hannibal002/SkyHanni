bea2f7b513b367a20cf109559aac90d9b2735087
Luna <lunaynx@gmail.com>
2026-07-20 13:50:09 +0200

    fix: capture GUI render context across mappings

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinGameRenderer.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinGameRenderer.java
@@ -3,31 +3,17 @@ package at.hannibal2.skyhanni.mixins.transformers;
 import at.hannibal2.skyhanni.data.GuiEditManager;
 import at.hannibal2.skyhanni.events.render.gui.RenderingTickEvent;
 import at.hannibal2.skyhanni.utils.compat.MinecraftCompat;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public abstract class MixinGameRenderer {
-
-    @Unique
-    private GuiGraphicsExtractor skyhanni$guiGraphics;
-
-    //? if >= 26.1 {
-    @ModifyVariable(method = "extractGui(Lnet/minecraft/client/DeltaTracker;ZZ)V", at = @At("STORE"), name = "graphics")
-    //?} else {
-    /*@ModifyVariable(method = "render", at = @At("STORE"), name = "guiGraphics")
-    *///?}
-    private GuiGraphicsExtractor skyhanni$captureGuiGraphicsExtractor(GuiGraphicsExtractor graphics) {
-        skyhanni$guiGraphics = graphics;
-        return graphics;
-    }
 
     @Inject(
         //? if >= 26.1 {
@@ -50,9 +36,10 @@ public abstract class MixinGameRenderer {
         //? if >= 26.1 {
         boolean resourcesLoaded,
         //?}
-        CallbackInfo ci
+        CallbackInfo ci,
+        @Local GuiGraphicsExtractor guiGraphics
     ) {
-        if (MinecraftCompat.getLocalPlayerExists()) new RenderingTickEvent(skyhanni$guiGraphics, true).post();
+        if (MinecraftCompat.getLocalPlayerExists()) new RenderingTickEvent(guiGraphics, true).post();
     }
 
     @Inject(
@@ -73,9 +60,10 @@ public abstract class MixinGameRenderer {
         //? if >= 26.1 {
         boolean resourcesLoaded,
         //?}
-        CallbackInfo ci
+        CallbackInfo ci,
+        @Local GuiGraphicsExtractor guiGraphics
     ) {
-        if (MinecraftCompat.getLocalPlayerExists()) new RenderingTickEvent(skyhanni$guiGraphics, false).post();
+        if (MinecraftCompat.getLocalPlayerExists()) new RenderingTickEvent(guiGraphics, false).post();
     }
 
     @Inject(
@@ -97,8 +85,9 @@ public abstract class MixinGameRenderer {
         //? if >= 26.1 {
         boolean resourcesLoaded,
         //?}
-        CallbackInfo ci
+        CallbackInfo ci,
+        @Local GuiGraphicsExtractor guiGraphics
     ) {
-        GuiEditManager.renderLast(skyhanni$guiGraphics);
+        GuiEditManager.renderLast(guiGraphics);
     }
 }
