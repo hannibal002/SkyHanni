986cb6b6855774480ac696fcc5c7a48990b88eb0
Cal <cwolfson58@gmail.com>
2026-07-21 16:48:17 +1000

    improve it again

--- src/main/java/at/hannibal2/skyhanni/features/combat/ghosttracker/GhostTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/ghosttracker/GhostTracker.kt
@@ -125,7 +125,7 @@ object GhostTracker {
      */
     private val itemDropPattern by patternGroup.pattern(
         "itemdrop.colorless",
-        "RARE DROP! (?<item>.+) \\(\\+\\s*(?<mf>\\d+)%? ${SkyblockStat.MAGIC_FIND.hypixelIcon} Magic Find\\)",
+        "RARE DROP! (?<item>.+) \\(\\+(?<mf>\\d+)%? ${SkyblockStat.MAGIC_FIND.hypixelIcon} Magic Find\\)",
     )
 
     /**
