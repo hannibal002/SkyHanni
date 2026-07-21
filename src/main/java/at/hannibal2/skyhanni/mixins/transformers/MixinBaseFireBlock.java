c84f12758a619202f3318872f4bf158ef1434139
Luna <lunaynx@gmail.com>
2026-07-21 23:10:55 +0200

    fix: mixin class names

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinBaseFireBlock.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinBaseFireBlock.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BaseFireBlock.class)
-public class MixinBlockFire {
+public class MixinBaseFireBlock {
 
     @Inject(
         method = "animateTick",
--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinBlaze.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinBlaze.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Blaze.class)
-public class MixinEntityBlaze {
+public class MixinBlaze {
 
     @Inject(
         method = "aiStep",
