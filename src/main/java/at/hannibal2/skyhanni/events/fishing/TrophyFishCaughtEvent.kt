c47cc8b7279b00599a0e77b20f107297e1512953
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-21 15:20:13 +0300

    add KDoc

--- src/main/java/at/hannibal2/skyhanni/events/fishing/TrophyFishCaughtEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/fishing/TrophyFishCaughtEvent.kt
@@ -4,6 +4,11 @@ import at.hannibal2.skyhanni.api.event.SkyHanniEvent
 import at.hannibal2.skyhanni.features.fishing.trophy.TrophyRarity
 import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 
-// trophyFishName is NO Neu Internal Name
+/**
+ * Event that is fired when a trophy fish is caught.
+ *
+ * the trophyFishName is the name of the trophy fish, and is NOT the [NeuInternalName] of it.
+ * and rarity is the rarity of the caught trophy fish. from bronze to diamond.
+ */
 @PrimaryFunction("onTrophyFishCaught")
 class TrophyFishCaughtEvent(val trophyFishName: String, val rarity: TrophyRarity) : SkyHanniEvent()
