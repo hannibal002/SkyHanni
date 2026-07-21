4aff3d3db2e0489a40103c926a888e7fec8106e0
Luna <lunaynx@gmail.com>
2026-07-22 01:12:04 +0200

    chore: remove stray Redirect import

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinChatComponent.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinChatComponent.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.ListIterator;
