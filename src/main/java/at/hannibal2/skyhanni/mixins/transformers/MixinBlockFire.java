a6ac8a529c1ddaa9d606f7e26169afcb79f98cf1
Luna <lunaynx@gmail.com>
2026-07-25 13:06:41 +0200

    fix: don't cancel the entire methods

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinBlockFire.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinBlockFire.java
@@ -1,26 +1,35 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
 import at.hannibal2.skyhanni.features.misc.ParticleHider;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BaseFireBlock.class)
 public class MixinBlockFire {
 
-    @Inject(
+    @WrapOperation(
         method = "animateTick",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"
-        ),
-        cancellable = true
+        )
     )
-    private void onRandomDisplayTick(CallbackInfo ci) {
-        if (ParticleHider.shouldHideFireParticles()) ci.cancel();
+    private void onRandomDisplayTick(
+        Level level,
+        ParticleOptions particle,
+        double x,
+        double y,
+        double z,
+        double xd,
+        double yd,
+        double zd,
+        Operation<Void> original
+    ) {
+        if (!ParticleHider.shouldHideFireParticles()) original.call(level, particle, x, y, z, xd, yd, zd);
     }
 }
--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinEntityBlaze.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinEntityBlaze.java
@@ -1,26 +1,35 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
 import at.hannibal2.skyhanni.features.misc.ParticleHider;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Blaze.class)
 public class MixinEntityBlaze {
 
-    @Inject(
+    @WrapOperation(
         method = "aiStep",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"
-        ),
-        cancellable = true
+        )
     )
-    private void onLivingUpdate(CallbackInfo ci) {
-        if (ParticleHider.shouldHideBlazeParticles()) ci.cancel();
+    private void onLivingUpdate(
+        Level level,
+        ParticleOptions particle,
+        double x,
+        double y,
+        double z,
+        double xd,
+        double yd,
+        double zd,
+        Operation<Void> original
+    ) {
+        if (!ParticleHider.shouldHideBlazeParticles()) original.call(level, particle, x, y, z, xd, yd, zd);
     }
 }
