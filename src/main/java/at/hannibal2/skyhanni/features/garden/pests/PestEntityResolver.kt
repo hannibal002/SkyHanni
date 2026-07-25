d94461381666304242a0ac7c57616fdbcb3ae771
Edgar <edgarmasstudio@gmail.com>
2026-07-26 01:48:40 +0700

    Exclude pets from pest routes

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestEntityResolver.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestEntityResolver.kt
@@ -3,6 +3,8 @@ package at.hannibal2.skyhanni.features.garden.pests
 import at.hannibal2.skyhanni.utils.AllEntitiesGetter
 import at.hannibal2.skyhanni.utils.EntityUtils
 import at.hannibal2.skyhanni.utils.EntityUtils.canBeSeen
+import at.hannibal2.skyhanni.utils.EntityUtils.cleanName
+import at.hannibal2.skyhanni.utils.EntityUtils.getEntitiesNearby
 import at.hannibal2.skyhanni.utils.ItemUtils.getSkullTexture
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.NeuItems.getItemStackOrNull
@@ -28,18 +30,28 @@ object PestEntityResolver {
         }.toMap()
     }
 
-    fun getPestType(entity: ArmorStand): PestType? =
-        entity.getAllEquipment()
+    fun getPestType(entity: ArmorStand): PestType? {
+        val type = entity.getAllEquipment()
             .firstNotNullOfOrNull { item ->
                 item?.getSkullTexture()?.normalizedBase64()?.let(pestTypeByTexture::get)
             }
+            ?: return null
+        return type.takeUnless { entity.hasPetNameNearby(it) }
+    }
 
     @OptIn(AllEntitiesGetter::class)
     fun getLoadedPests(): List<LoadedPest> = EntityUtils.getEntities<ArmorStand>()
         .mapNotNull { entity -> getPestType(entity)?.let { LoadedPest(entity, it) } }
         .toList()
 
+    private fun ArmorStand.hasPetNameNearby(type: PestType): Boolean =
+        getLorenzVec().getEntitiesNearby<ArmorStand>(PET_NAME_SEARCH_RADIUS).any {
+            PET_NAME_PATTERN.matchEntire(it.cleanName)?.groups?.get("name")?.value == type.displayName
+        }
+
     private fun String.normalizedBase64() = trim().trimEnd('=')
 
+    private val PET_NAME_PATTERN = Regex("^\\[Lvl ?[\\d,]+] ?(?<name>.+?)(?: ✦)?$")
     private const val MODEL_HEIGHT_OFFSET = 1.5
+    private const val PET_NAME_SEARCH_RADIUS = 3.0
 }
