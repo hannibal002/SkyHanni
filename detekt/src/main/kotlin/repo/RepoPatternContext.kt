2938c0241a546d662af4d05340790a5f9e082f16
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-25 12:10:32 +0300

    Update RepoPatternContext.kt

--- detekt/src/main/kotlin/repo/RepoPatternContext.kt
+++ detekt/src/main/kotlin/repo/RepoPatternContext.kt
@@ -19,7 +19,6 @@ class RepoPatternContext {
     }
 
     private class IdentityCharacteristics<T>(val value: T) {
-
         override fun equals(other: Any?): Boolean {
             if (other !is IdentityCharacteristics<*>) return false
             return this.value === other.value
