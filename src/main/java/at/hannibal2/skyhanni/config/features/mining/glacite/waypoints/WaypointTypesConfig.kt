c37f252116fdb13f9862d4fd556ee82b4ba0c4c8
Piggered <19157702+Piggered@users.noreply.github.com>
2026-07-25 00:00:13 -0400

    reorganize mineshaft config options
    # Conflicts:
    #	src/main/java/at/hannibal2/skyhanni/config/ConfigUpdaterMigrator.kt

---
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/glacite/waypoints/WaypointTypesConfig.kt
@@ -0,0 +1,31 @@
+package at.hannibal2.skyhanni.config.features.mining.glacite.waypoints
+
+import com.google.gson.annotations.Expose
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
+
+class WaypointTypesConfig {
+    @Expose
+    @ConfigOption(
+        name = "Entrance",
+        desc = "Mark the location of the entrance with a waypoint."
+    )
+    @ConfigEditorBoolean
+    var entrance: Boolean = false 
+
+    @Expose
+    @ConfigOption(
+        name = "Ladder",
+        desc = "Mark the location of the ladders at the bottom of the entrance with a waypoint."
+    )
+    @ConfigEditorBoolean
+    var ladder: Boolean = false
+
+    @Expose
+    @ConfigOption(
+        name = "Found Corpse",
+        desc = "Mark the location of corpses that have been within line of sight with a waypoint."
+    )
+    @ConfigEditorBoolean
+    var foundCorpse: Boolean = true
+}
