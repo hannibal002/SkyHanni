2b09d7c26effa8e646e49ad4c6725f7ac05db98c
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-25 12:02:14 +0300

    use ConcurrentHasMap

--- detekt/src/main/kotlin/repo/RepoPatternContext.kt
+++ detekt/src/main/kotlin/repo/RepoPatternContext.kt
@@ -3,19 +3,33 @@ package repo
 import org.jetbrains.kotlin.psi.KtPropertyDelegate
 import repo.RepoPatternElement.Companion.asRepoPatternElement
 import java.util.IdentityHashMap
+import java.util.concurrent.ConcurrentHashMap
 
 class RepoPatternContext {
-    private val cache = IdentityHashMap<KtPropertyDelegate, RepoPatternElement>(2048)
+    private val cache = ConcurrentHashMap<IdentityCharacteristics<KtPropertyDelegate>, RepoPatternElement>(2048)
 
     fun getRepoPatternElement(property: KtPropertyDelegate): RepoPatternElement? {
-        val cachedValue = cache[property]
+        val identityKey = IdentityCharacteristics(property)
+        val cachedValue = cache[identityKey]
 
         if (cachedValue != null) {
             return cachedValue.takeUnless { it === RepoPatternElement.SENTINEL_VALUE }
         }
 
         val element = property.asRepoPatternElement()
-        cache[property] = element ?: RepoPatternElement.SENTINEL_VALUE
+        cache[identityKey] = element ?: RepoPatternElement.SENTINEL_VALUE
         return element
+    }
+
+    private class IdentityCharacteristics<T>(val value: T) {
+
+        override fun equals(other: Any?): Boolean {
+            if (other !is IdentityCharacteristics<*>) return false
+            return this.value === other.value
+        }
+
+        override fun hashCode(): Int {
+            return System.identityHashCode(value)
+        }
     }
 }
