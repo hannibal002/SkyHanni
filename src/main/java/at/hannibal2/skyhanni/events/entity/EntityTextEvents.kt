b3419def8686e1ff73003902f90217a15afd7d16
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-07-26 16:37:34 +0300

    Update src/main/java/at/hannibal2/skyhanni/events/entity/EntityTextEvents.kt
    Co-authored-by: Luna <lunaynx@gmail.com>

--- src/main/java/at/hannibal2/skyhanni/events/entity/EntityTextEvents.kt
+++ src/main/java/at/hannibal2/skyhanni/events/entity/EntityTextEvents.kt
@@ -6,8 +6,8 @@ import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.Entity
 
 /**
- * Event that is fired when a text display entity's custom name is updated.
- * This includes both text display and armor stand entities, as they can both have custom names.
+ * Event that is fired when text associated with an entity is updated.
+ * This includes both text display entities and armor stand custom names.
  * Do note that event.entity.customName is NOT equivalent to event.newName for text display entities.
  * Only fires on SkyBlock.
  *
