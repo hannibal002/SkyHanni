b220c30985706d94c47390845249512f1fcf3d6f
Luna <lunaynx@gmail.com>
2026-07-27 23:36:22 +0200

    Backend: Update mixin naming + misc cleanup (#6145)

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinScreen.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinScreen.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Screen.class)
-public class MixinScreen {
+public abstract class MixinScreen {
 
     @WrapOperation(
         //~ if < 26.1 'extractRenderStateWithTooltipAndSubtitles' -> 'renderWithTooltipAndSubtitles'
