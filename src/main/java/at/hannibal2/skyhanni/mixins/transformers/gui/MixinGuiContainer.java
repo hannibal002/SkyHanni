ea12aaabff80d7ab80c1c361b7824556b447665f
Luna <lunaynx@gmail.com>
2026-07-28 15:02:58 +0200

    Delete src/main/java/at/hannibal2/skyhanni/mixins/transformers/gui/MixinGuiContainer.java

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/gui/MixinGuiContainer.java
+++
@@ -1,7 +0,0 @@
-package at.hannibal2.skyhanni.mixins.transformers.gui;
-
-
-@Mixin(AbstractContainerScreen.class)
-public abstract class MixinGuiContainer<T extends AbstractContainerMenu> extends Screen {
-
-}
