b0306497cfa9b5396030be1f409080fe58046da9
Piggered <19157702+Piggered@users.noreply.github.com>
2026-07-28 16:35:08 -0400

    refactor rendering conditions and make config changes retroactive

--- src/main/java/at/hannibal2/skyhanni/features/mining/glacitemineshaft/MineshaftWaypointType.kt
+++ src/main/java/at/hannibal2/skyhanni/features/mining/glacitemineshaft/MineshaftWaypointType.kt
@@ -1,15 +1,20 @@
 package at.hannibal2.skyhanni.features.mining.glacitemineshaft
 
+import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.utils.LorenzColor
+
+private val config get() = SkyHanniMod.feature.mining.glaciteMineshaft.waypointsConfig
 
 enum class MineshaftWaypointType(
     val display: String,
     val color: LorenzColor,
+    val isCorpse: Boolean,
+    val renderCondition: () -> Boolean,
 ) {
-    LAPIS("Lapis Corpse", LorenzColor.DARK_BLUE),
-    UMBER("Umber Corpse", LorenzColor.GOLD),
-    TUNGSTEN("Tungsten Corpse", LorenzColor.GRAY),
-    VANGUARD("Vanguard Corpse", LorenzColor.BLUE),
-    ENTRANCE("Entrance", LorenzColor.YELLOW),
-    LADDER("Ladder", LorenzColor.YELLOW)
+    LAPIS("Lapis Corpse", LorenzColor.DARK_BLUE, true, { config.types.foundCorpse }),
+    UMBER("Umber Corpse", LorenzColor.GOLD, true, { config.types.foundCorpse }),
+    TUNGSTEN("Tungsten Corpse", LorenzColor.GRAY, true, { config.types.foundCorpse }),
+    VANGUARD("Vanguard Corpse", LorenzColor.BLUE, true, { config.types.foundCorpse }),
+    ENTRANCE("Entrance", LorenzColor.YELLOW, false, { config.types.entrance }),
+    LADDER("Ladder", LorenzColor.YELLOW, false, { config.types.ladder }),
 }
