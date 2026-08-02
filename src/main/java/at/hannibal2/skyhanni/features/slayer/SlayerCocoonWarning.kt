87eed05a0feafb11f175f13ee80a42060f98c1a6
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-02 19:58:48 +0300

    Revert "fix"
    This reverts commit f7a5d8e944e507211f61f8327452a3b3377d0468.

--- src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerCocoonWarning.kt
+++ src/main/java/at/hannibal2/skyhanni/features/slayer/SlayerCocoonWarning.kt
@@ -3,28 +3,18 @@ package at.hannibal2.skyhanni.features.slayer
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.data.SlayerApi
 import at.hannibal2.skyhanni.data.title.TitleManager
-import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
 import at.hannibal2.skyhanni.events.slayer.SlayerStateChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.RegexUtils.matches
 import at.hannibal2.skyhanni.utils.SoundUtils
-import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 @SkyHanniModule
 object SlayerCocoonWarning {
 
     private val config get() = SlayerApi.config
-    /**
-     * WRAPPED-REGEX-TEST: "  YOU COCOONED YOUR SLAYER BOSS"
-     */
-    private val slayerCocoonPattern by RepoPattern.pattern(
-        "slayer.cocooned.colorless",
-        "\\s+YOU COCOONED YOUR SLAYER BOSS",
-    )
 
     @HandleEvent
-    private fun onChatMessage(event: SkyHanniChatEvent.Allow) {
-        if (slayerCocoonPattern.matches(event.cleanMessage)) {
+    private fun onSlayerStateChange(event: SlayerStateChangeEvent) {
+        if (event.state == COCOONED) {
             if (config.cocoonTitle) TitleManager.sendTitle("§lSlayer Boss Cocooned!")
             if (config.cocoonDing) SoundUtils.repeatSound(100, 10, SoundUtils.plingSound)
         }
