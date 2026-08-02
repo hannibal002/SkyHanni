cf9f1468bb32b45c9008efec072ba176293e4054
Cal <cwolfson58@gmail.com>
2026-08-02 20:34:08 +1000

    matriarch cap iterations

--- src/main/java/at/hannibal2/skyhanni/features/nether/MatriarchHelper.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/MatriarchHelper.kt
@@ -84,6 +84,7 @@ object MatriarchHelper {
         if (config.useShortestDistance) {
             val path = tspCache ?: NavigationUtils.getRoute(
                 pearlList.map { it.second },
+                maxIterations = 5
             ).also {
                 val pearls = path.size
                 if (pearls != lastTspPearls) {
