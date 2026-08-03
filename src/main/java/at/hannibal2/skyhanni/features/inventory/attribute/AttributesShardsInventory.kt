3286c57d91b82fa77b4f0e0ac413fefae57a205f
Cal <cwolfson58@gmail.com>
2026-08-03 13:58:05 +1000

    suggested change
    Co-authored-by: AverageUser125 <96657216+averageuser125@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/attribute/AttributesShardsInventory.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/attribute/AttributesShardsInventory.kt
@@ -3,6 +3,7 @@ package at.hannibal2.skyhanni.features.inventory.attribute
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.RenderInventoryItemTipEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
+import at.hannibal2.skyhanni.utils.ItemUtils.cleanName
 import at.hannibal2.skyhanni.utils.ItemUtils.getInternalNameOrNull
 import at.hannibal2.skyhanni.utils.NumberUtil.romanToDecimal
 import at.hannibal2.skyhanni.utils.RegexUtils.groupOrNull
@@ -20,7 +21,7 @@ object AttributesShardsInventory {
 
         val internalName = event.stack.getInternalNameOrNull() ?: return
         if (!AttributeShardsData.isAttributeShard(internalName)) return
-        AttributeShardsData.attributeShardNamePattern.matchMatcher(event.stack.hoverName.string) {
+        AttributeShardsData.attributeShardNamePattern.matchMatcher(event.stack.cleanName) {
             val tier = groupOrNull("tier")?.romanToDecimal() ?: 0
             val color = when (tier) {
                 0 -> "§c"
