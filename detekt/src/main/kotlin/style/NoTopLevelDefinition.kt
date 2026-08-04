60db607fb4e89c039c9b2be2d29b7ed698fb1c32
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-04 14:00:19 +0300

    Renderable.Companion is fine

--- detekt/src/main/kotlin/style/NoTopLevelDefinition.kt
+++ detekt/src/main/kotlin/style/NoTopLevelDefinition.kt
@@ -11,9 +11,15 @@ class NoTopLevelDefinition(config: Config) :
 
     override fun visitNamedFunction(function: KtNamedFunction) {
         super.visitNamedFunction(function)
-        if (function.parent is KtFile) {
-            function.reportIssue("Top level function definitions are not allowed. Put them inside a class or object instead.")
+
+        if (function.parent !is KtFile) return
+        val receiver = function.receiverTypeReference?.text
+        // Allow companion object extensions.
+        if (receiver?.endsWith(".Companion") == true) {
+            return
         }
+
+        function.reportIssue("Top level function definitions are not allowed. Put them inside a class or object instead.")
     }
 
     override fun visitProperty(property: KtProperty) {
