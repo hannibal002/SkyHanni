4cd3dc9d2a467603b02b38672d8e1238773cc1fd
Cal Wolfson <94038482+CalMWolfs@users.noreply.github.com>
2026-08-05 00:06:29 +1000

    Feature + Backend: Honeyhive navigator (#6280)

---
+++ src/main/java/at/hannibal2/skyhanni/features/misc/pathfind/NavigationCondition.kt
@@ -0,0 +1,22 @@
+package at.hannibal2.skyhanni.features.misc.pathfind
+
+import at.hannibal2.skyhanni.data.model.graph.GraphNode
+
+sealed interface NavigationCondition {
+
+    /**
+     * Navigate directly to the next node after reaching this one
+     */
+    data object None : NavigationCondition
+
+    /**
+     * A condition that the cleanMessage from the chat event must pass
+     */
+    data class ChatMessage(val condition: (String) -> Boolean) : NavigationCondition
+
+    /**
+     * A condition that must be passed, is checked every second. Is also checked immediately upon reaching
+     */
+    data class SecondPassed(val condition: (GraphNode) -> Boolean) : NavigationCondition
+
+}
