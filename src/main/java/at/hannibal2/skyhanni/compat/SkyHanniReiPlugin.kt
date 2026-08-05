865c3e9f77bc17f329ae1a0ca901c915c8b7cbfd
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-06 00:07:49 +0300

    Update SkyHanniReiPlugin.kt

--- src/main/java/at/hannibal2/skyhanni/compat/SkyHanniReiPlugin.kt
+++ src/main/java/at/hannibal2/skyhanni/compat/SkyHanniReiPlugin.kt
@@ -25,7 +25,7 @@ private class SkyHanniDisplayBoundsProvider : DisplayBoundsProvider<SkyHanniBase
     }
 
     override fun getScreenBounds(screen: SkyHanniBaseScreen): Rectangle? {
-        // If the screen does not want to show the item list, return null to indicate no bounds
+        // If the screen does not want to show the item list, return null to indicate not to show the overlay
         // Should not happen because shouldScreenBeOverlaid should have been called first, but just in case
         if (!screen.shouldShowItemList()) return null
         val rectangle = screen.rectangle
