3507e5c1c282600a62352b152830f33d4c5c47d1
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-05 22:24:13 +0300

    better KDoc

--- src/main/java/at/hannibal2/skyhanni/events/GuiKeyPressEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/GuiKeyPressEvent.kt
@@ -11,12 +11,15 @@ import net.minecraft.client.input.MouseButtonEvent
 /**
  * Event that is fired when a key is pressed while a SkyHanniGuiContainer is open.
  * This event is cancellable, and if canceled, the key press will not be processed by the GUI.
+ * Users of this function should use [at.hannibal2.skyhanni.utils.KeyboardManager.isKeyHeld]
+ * Or [at.hannibal2.skyhanni.utils.KeyboardManager.isKeyClicked]
+ * if they want to see which key was pressed.
  */
 @PrimaryFunction("onGuiKeyPress")
 class GuiKeyPressEvent(
     val guiContainer: SkyHanniGuiContainer,
-    val keyEvent: KeyEvent?,
-    val mouseEvent: MouseButtonEvent?
+    private val keyEvent: KeyEvent?,
+    private val mouseEvent: MouseButtonEvent?
 ) : CancellableSkyHanniEvent() {
     constructor(guiContainer: SkyHanniGuiContainer, keyEvent: KeyEvent) : this(guiContainer, keyEvent = keyEvent, mouseEvent = null)
     constructor(guiContainer: SkyHanniGuiContainer, mouseEvent: MouseButtonEvent) : this(guiContainer, keyEvent = null, mouseEvent = mouseEvent)
