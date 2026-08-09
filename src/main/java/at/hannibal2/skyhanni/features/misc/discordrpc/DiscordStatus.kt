57c59ef545ee70b87328949c489b74d6a82a7315
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-09 11:53:08 +0300

    Update DiscordStatus.kt

--- src/main/java/at/hannibal2/skyhanni/features/misc/discordrpc/DiscordStatus.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/discordrpc/DiscordStatus.kt
@@ -202,7 +202,7 @@ enum class DiscordStatus(private val displayMessageSupplier: DiscordStatus.() ->
                 BOSS_FIGHT -> "Slaying $article ${SlayerApi.activeType?.displayName}"
                 SLAIN -> "Finished slaying $article ${SlayerApi.activeType?.displayName}"
                 FAILED -> "Lost to $article ${SlayerApi.activeType?.displayName}"
-                COCOONED -> "Waiting for $article ${SlayerApi.activeType?.displayName} Cocoon to hatch"
+                COCOONED -> "Waiting for $article ${SlayerApi.activeType?.displayName} cocoon to hatch"
                 else -> AutoStatus.SLAYER.placeholderText
             }
         },
