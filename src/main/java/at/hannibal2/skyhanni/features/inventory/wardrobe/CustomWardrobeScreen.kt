c44dce110998e4700e24933f71470b8015db3901
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-09 12:56:45 +0300

    Backend + Fix: Make CustomWardrobe an actual Screen (#6225)

---
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/wardrobe/CustomWardrobeScreen.kt
@@ -0,0 +1,55 @@
+package at.hannibal2.skyhanni.features.inventory.wardrobe
+
+import at.hannibal2.skyhanni.utils.AbstractCustomMenuScreen
+import at.hannibal2.skyhanni.utils.DelayedRun
+import at.hannibal2.skyhanni.utils.SafeItemStack
+import net.minecraft.client.gui.navigation.ScreenRectangle
+import net.minecraft.network.chat.Component
+import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.inventory.ChestMenu
+
+class CustomWardrobeScreen(
+    menu: ChestMenu,
+    title: Component,
+) : AbstractCustomMenuScreen(menu, title) {
+    private var updateScheduled = false
+
+    override fun getRectangle(): ScreenRectangle = ScreenRectangle(
+        CustomWardrobe.renderableTopCorner.first,
+        CustomWardrobe.renderableTopCorner.second,
+        CustomWardrobe.renderableDimensions.first,
+        CustomWardrobe.renderableDimensions.second,
+    )
+
+    override fun shouldShowItemList(): Boolean = CustomWardrobe.config.showReiItems
+
+    override fun isSwitchingScreens(): Boolean = CustomWardrobe.switchingScreens
+
+    override fun onInitGui() {
+        CustomWardrobe.switchingScreens = false
+        CustomWardrobe.updateScreenSize(width to height)
+        // slotChanged is called when a screen is opened, so no need to call CustomWardrobe.onInventoryUpdate() here
+    }
+
+    override fun onDrawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
+        CustomWardrobe.renderWardrobeOverlay(this.width, this.height)
+    }
+
+    override fun slotChanged(container: AbstractContainerMenu, slotId: Int, stack: SafeItemStack) {
+        if (updateScheduled) return
+        updateScheduled = true
+
+        DelayedRun.runNextTick {
+            updateScheduled = false
+            CustomWardrobe.onInventoryUpdate()
+        }
+    }
+
+    override fun onKeyTyped(typedChar: Char?, keyCode: Int?) {
+        CustomWardrobeKeybinds.handlePress()
+    }
+
+    override fun onMouseClicked(originalMouseX: Int, originalMouseY: Int, mouseButton: Int) {
+        CustomWardrobeKeybinds.handlePress()
+    }
+}
