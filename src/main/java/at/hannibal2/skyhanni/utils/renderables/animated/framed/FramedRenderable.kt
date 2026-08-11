f3e0a39ee9e19d2be6414872769ca70db1645a78
Luna <lunaynx@gmail.com>
2026-08-11 13:47:36 +0000

    refactor: remove dead render code

--- src/main/java/at/hannibal2/skyhanni/utils/renderables/animated/framed/FramedRenderable.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/renderables/animated/framed/FramedRenderable.kt
@@ -1,11 +1,6 @@
 package at.hannibal2.skyhanni.utils.renderables.animated.framed
 
-import at.hannibal2.skyhanni.utils.SimpleTimeMark
-import at.hannibal2.skyhanni.utils.inPartialSeconds
-import at.hannibal2.skyhanni.utils.renderables.Renderable
 import at.hannibal2.skyhanni.utils.renderables.animated.AnimatedItemRenderableConfig
-import at.hannibal2.skyhanni.utils.renderables.animated.TimeDependentRenderable
-import at.hannibal2.skyhanni.utils.renderables.decorators.RenderableDecorator
 
 internal interface FramedBehavior<T : AnimatedFrame> : AnimatedFrameStorage<T> {
     val config: AnimatedItemRenderableConfig<T>
@@ -30,30 +25,5 @@ internal interface FramedBehavior<T : AnimatedFrame> : AnimatedFrameStorage<T> {
 
         currentFrameIndex = (currentFrameIndex + 1) % frames.size
         ticksInFrame = 0.0
-    }
-}
-
-class FramedRenderable<T : AnimatedFrame> private constructor(
-    override val root: Renderable,
-    override val config: AnimatedItemRenderableConfig<T>,
-) : RenderableDecorator, TimeDependentRenderable, FramedBehavior<T> {
-    override val height: Int get() = root.height
-    override val width: Int get() = root.width
-    override val horizontalAlign get() = root.horizontalAlign
-    override val verticalAlign get() = root.verticalAlign
-
-    override var lastRenderTime: SimpleTimeMark = SimpleTimeMark.now()
-    override var ticksInFrame: Double = 0.0
-    override var stableRenderId: Int? = null
-
-    override fun renderWithDelta(mouseOffsetX: Int, mouseOffsetY: Int, deltaTime: kotlin.time.Duration) {
-        tryMoveNextFrame(deltaTime.inPartialSeconds)
-    }
-
-    companion object {
-        fun <T : AnimatedFrame> Renderable.Companion.framed(
-            root: Renderable,
-            config: AnimatedItemRenderableConfig<T>.() -> Unit = { }
-        ) = FramedRenderable(root, AnimatedItemRenderableConfig<T>().apply(config))
     }
 }
