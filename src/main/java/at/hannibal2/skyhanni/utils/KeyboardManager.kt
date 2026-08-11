e05801139db0c1d5174d2a1cee07b532cd42a990
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-11 17:22:13 +0300

    Revert "add TODO"
    This reverts commit f7ad2927ce969b79d0bcd9a2d522a0d3a371ab06.

--- src/main/java/at/hannibal2/skyhanni/utils/KeyboardManager.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/KeyboardManager.kt
@@ -175,7 +175,6 @@ object KeyboardManager {
             val mapping = getOrCreateKeyMapping(
                 option.get() as Int,
                 annotation.defaultKey,
-                // TODO: this should be a translation key
                 displayName,
             )
             GuiOptionEditorKeyMapping(option, mapping)
