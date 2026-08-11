465dfd674296fb10b5c2434caf5c71f1b8e99281
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-11 16:45:24 +0300

    fix

--- src/main/java/at/hannibal2/skyhanni/config/features/chat/ChatConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/chat/ChatConfig.kt
@@ -3,9 +3,7 @@ package at.hannibal2.skyhanni.config.features.chat
 import at.hannibal2.skyhanni.config.FeatureToggle
 import at.hannibal2.skyhanni.config.core.config.Position
 import at.hannibal2.skyhanni.config.core.elements.ConfigEditorKeyMapping
-import at.hannibal2.skyhanni.utils.KeyboardManager.KEY_UNKNOWN
 import com.google.gson.annotations.Expose
-import com.mojang.blaze3d.platform.InputConstants
 import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.Category
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
@@ -13,12 +11,13 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDraggableLi
 import io.github.notenoughupdates.moulconfig.annotations.ConfigLink
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 import io.github.notenoughupdates.moulconfig.annotations.SearchTag
+import org.lwjgl.glfw.GLFW
 
 class ChatConfig {
     @Expose
     @ConfigOption(name = "Peek Chat", desc = "Hold this key to keep the chat open.")
-    @ConfigEditorKeyMapping(defaultKey = InputConstants.KEY_Z)
-    var peekChat: Int = KEY_UNKNOWN
+    @ConfigEditorKeyMapping(defaultKey = GLFW.GLFW_KEY_Z)
+    var peekChat: Int = GLFW.GLFW_KEY_Z
 
     // TODO move into own sub category
     @Expose
