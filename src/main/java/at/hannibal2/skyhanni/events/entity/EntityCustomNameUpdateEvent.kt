6da53dbc73cd4b109d8db43cba86d59ee8116a39
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-14 21:55:10 +0300

    Update EntityCustomNameUpdateEvent.kt

--- src/main/java/at/hannibal2/skyhanni/events/entity/EntityCustomNameUpdateEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/entity/EntityCustomNameUpdateEvent.kt
@@ -1,11 +1,20 @@
 package at.hannibal2.skyhanni.events.entity
 
 import at.hannibal2.skyhanni.api.event.GenericSkyHanniEvent
+import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 import at.hannibal2.skyhanni.utils.StringUtils.removeColor
 import at.hannibal2.skyhanni.utils.compat.formattedTextCompatLessResets
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.Entity
 
+/**
+ * Event that is fired when an entity's custom name is updated.
+ *
+ * @param T The type of the entity.
+ * @property entity The entity whose custom name was updated.
+ * @property newName The new custom name of the entity, or null if the name was removed.
+ */
+@PrimaryFunction("onEntityNameUpdate")
 data class EntityCustomNameUpdateEvent<T : Entity>(
     val entity: T,
     val newName: Component?,
