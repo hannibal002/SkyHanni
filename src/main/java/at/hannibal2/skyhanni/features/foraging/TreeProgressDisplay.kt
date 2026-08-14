cd518e9801f54e984dab130b0730661b236bbdc8
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-14 22:34:13 +0300

    better

--- src/main/java/at/hannibal2/skyhanni/features/foraging/TreeProgressDisplay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/foraging/TreeProgressDisplay.kt
@@ -11,7 +11,9 @@ import at.hannibal2.skyhanni.utils.ComponentMatcherUtils.matchStyledMatcher
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemCategory
 import at.hannibal2.skyhanni.utils.ItemUtils.getItemCategoryOrNull
+import at.hannibal2.skyhanni.utils.LocationUtils.distanceToPlayer
 import at.hannibal2.skyhanni.utils.RenderUtils.renderRenderable
+import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
 import at.hannibal2.skyhanni.utils.compat.append
 import at.hannibal2.skyhanni.utils.compat.componentBuilder
 import at.hannibal2.skyhanni.utils.renderables.Renderable
@@ -24,8 +26,13 @@ import net.minecraft.world.entity.decoration.ArmorStand
 object TreeProgressDisplay {
 
     private val config get() = SkyHanniMod.feature.foraging.trees.progress
-    private var display: Renderable? = null
-    private var displayEntityId: Int? = null
+
+    private var treeProgressDisplay: TreeProgressDisplay? = null
+    private data class TreeProgressDisplay(
+        val id: Int,
+        val renderable: Renderable,
+        val distanceToPlayer: Double,
+    )
 
     /**
      * REGEX-TEST: FIG TREE 88%
@@ -39,42 +46,56 @@ object TreeProgressDisplay {
     @HandleEvent(onlyOnIslandTypeTag = [IslandTypeTag.FORAGING_CUSTOM_TREES])
     private fun onGuiRenderOverlay() {
         if (!isEnabled()) return
-        display?.let {
-            config.position.renderRenderable(it, posLabel = "Tree Progress")
+        treeProgressDisplay?.let {
+            config.position.renderRenderable(it.renderable, posLabel = "Tree Progress")
         }
     }
 
     @HandleEvent(onlyOnIslandTypeTag = [IslandTypeTag.FORAGING_CUSTOM_TREES])
     private fun onIslandJoin() {
-        clearData()
+        treeProgressDisplay = null
     }
 
     @HandleEvent(onlyOnIslandTypeTag = [IslandTypeTag.FORAGING_CUSTOM_TREES])
     private fun onEntityRemoved(event: EntityRemovedEvent<ArmorStand>) {
         if (!isEnabled()) return
-        if (event.entity.id == displayEntityId) {
-            clearData()
+        if (event.entity.id == treeProgressDisplay?.id) {
+            treeProgressDisplay = null
         }
     }
 
     @HandleEvent(onlyOnIslandTypeTag = [IslandTypeTag.FORAGING_CUSTOM_TREES])
     private fun onEntityNameUpdate(event: EntityCustomNameUpdateEvent<ArmorStand>) {
         if (!isEnabled()) return
+        if (!MinecraftCompat.localPlayerExists) return
+
         if (config.onlyHoldingAxe && InventoryUtils.getItemInHand()?.getItemCategoryOrNull() != ItemCategory.AXE) {
-            display = null
+            treeProgressDisplay = null
             return
         }
-        val newName = event.newName ?: return
-        currentTreeProgressPattern.matchStyledMatcher(newName) {
-            val component = if (config.compact) formatCompact() else newName
-            displayEntityId = event.entity.id
-            display = Renderable.text(component)
-        }
-    }
 
-    private fun clearData() {
-        display = null
-        displayEntityId = null
+        val newName = event.newName ?: return
+        val displayText = currentTreeProgressPattern.matchStyledMatcher(newName) {
+            if (config.compact) formatCompact() else newName
+        } ?: return
+
+        val entityDistance = event.entity.distanceToPlayer()
+
+        val current = treeProgressDisplay
+        if (current != null && current.id != event.entity.id) {
+            // Prefer the closest tree if there are multiple trees with progress displayed
+            val currentDistance = current.distanceToPlayer
+            val newDistance = event.entity.distanceToPlayer()
+            if (newDistance >= currentDistance) {
+                return
+            }
+        }
+
+        treeProgressDisplay = TreeProgressDisplay(
+            id = event.entity.id,
+            renderable = Renderable.text(displayText),
+            distanceToPlayer = entityDistance,
+        )
     }
 
     private fun ComponentMatcher.formatCompact(): Component {
