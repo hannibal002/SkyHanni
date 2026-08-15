d0e9bad8f698326b28a940c5e0ac8d83201d6ec7
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-08-15 16:04:51 +0200

    Improvement: Currencies in more inventories (#6367)
    Co-authored-by: AverageUser125 <96657216+AverageUser125@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/storage/ProfileSpecificStorage.kt
+++ src/main/java/at/hannibal2/skyhanni/config/storage/ProfileSpecificStorage.kt
@@ -75,6 +75,7 @@ import at.hannibal2.skyhanni.utils.SafeItemStack
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
 import at.hannibal2.skyhanni.utils.SimpleTimeMark.Companion.farFuture
 import at.hannibal2.skyhanni.utils.SimpleTimeMark.Companion.farPast
+import at.hannibal2.skyhanni.utils.SkyblockCurrency
 import at.hannibal2.skyhanni.utils.collection.CollectionUtils.enumMapOf
 import com.google.gson.annotations.Expose
 import net.minecraft.network.chat.Component
@@ -104,6 +105,14 @@ class ProfileSpecificStorage(
 
     @Expose
     var crimsonIsleFaction: FactionType? = null
+
+    /** Written and read by [at.hannibal2.skyhanni.data.CurrencyApi]. */
+    @Expose
+    var currencies: MutableMap<SkyblockCurrency, Long> = enumMapOf()
+
+    /** Written and read by [at.hannibal2.skyhanni.data.CurrencyApi]. */
+    @Expose
+    var essences: MutableMap<NeuInternalName, Long> = mutableMapOf()
 
     // features
     // - combat
