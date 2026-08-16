88636fcb3c872494d54da67aa708cd39c1963812
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-16 15:47:33 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/config/features/dev/MainTogglesConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/dev/MainTogglesConfig.kt
@@ -1,6 +1,5 @@
 package at.hannibal2.skyhanni.config.features.dev
 
-import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorInfoText
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
