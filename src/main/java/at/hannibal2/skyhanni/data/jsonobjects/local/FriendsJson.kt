28472a3f5dbac2e6c9c6688784a02ed57f0063bc
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-19 00:52:01 +0300

    Sub classes

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/local/FriendsJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/local/FriendsJson.kt
@@ -3,21 +3,20 @@ package at.hannibal2.skyhanni.data.jsonobjects.local
 import com.google.gson.annotations.Expose
 import java.util.UUID
 
-
 data class FriendsJson(
     @Expose
     val players: MutableMap<UUID, PlayerFriends> = mutableMapOf(),
-)
+) {
+    data class PlayerFriends(
+        @Expose
+        val friends: MutableMap<UUID, Friend> = mutableMapOf(),
+    )
 
-data class PlayerFriends(
-    @Expose
-    val friends: MutableMap<UUID, Friend> = mutableMapOf(),
-)
+    data class Friend(
+        @Expose
+        val name: String = "",
 
-data class Friend(
-    @Expose
-    val name: String = "",
-
-    @Expose
-    var bestFriend: Boolean = false,
-)
+        @Expose
+        var bestFriend: Boolean = false,
+    )
+}
