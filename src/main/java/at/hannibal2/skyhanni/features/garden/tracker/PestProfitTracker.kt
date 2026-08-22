56e0d8b3d4a35d93336f528667f9f8b782d5c90f
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-22 18:53:51 +0300

    closer

--- src/main/java/at/hannibal2/skyhanni/features/garden/tracker/PestProfitTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/tracker/PestProfitTracker.kt
@@ -92,7 +92,7 @@ object PestProfitTracker : SkyHanniBucketedItemTracker<PestType, PestProfitTrack
     @Suppress("MaxLineLength")
     private val pestRareDropPattern by patternGroup.pattern(
         "raredrop",
-        "(?:§.)*(?:RARE|PET) DROP! (?:§r)*(?<item>.+?)(?: §8x(?<amount>\\d+))? (?:§.)*\\((?:§.)?(?:\\+[\\d.,]+\uE02B|Cocoaleech)\\)",
+        "(?:§.)*(?:RARE|PET) DROP! (?:§r)?(?<item>.+?)(?: §8x(?<amount>\\d+))? (?:§.)*\\((?:§.)?(?:\\+[\\d.,]+${SkyblockStat.OVERBLOOM.hypixelIcon}|Cocoaleech)\\)",
     )
 
     val DUNG_ITEM = "DUNG".toInternalName()
