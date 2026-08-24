b2c2412c2d886afdc14dd3abc6f2b90740f10a48
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-24 16:23:25 +0300

    Brine.

--- src/main/java/at/hannibal2/skyhanni/data/effect/EffectApi.kt
+++ src/main/java/at/hannibal2/skyhanni/data/effect/EffectApi.kt
@@ -167,6 +167,7 @@ object EffectApi {
         EXALTED_LUSHLILAC_BONBON,
         OCEANDY,
         CANDYCOMB,
+        BRINE,
     )
 
     private val profileStorage get() = ProfileStorageData.profileSpecific
