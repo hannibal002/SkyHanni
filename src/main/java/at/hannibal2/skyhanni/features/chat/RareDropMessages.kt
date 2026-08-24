9bc68d1e6f42369d95a67debda5a9391dae71cb4
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-08-24 17:35:51 +0300

    Update RareDropMessages.kt

--- src/main/java/at/hannibal2/skyhanni/features/chat/RareDropMessages.kt
+++ src/main/java/at/hannibal2/skyhanni/features/chat/RareDropMessages.kt
@@ -54,6 +54,7 @@ object RareDropMessages {
     /**
      * REGEX-TEST: §6 §r§6§lGREAT CATCH! §r§fYou caught a §r§7[Lvl 1] §r§aSquid§r§f!
      */
+    @Suppress("MaxLineLength")
     private val petFishedPattern by petGroup.pattern(
         "fishedmessage",
         "(?<start>(?:§.)*${SkyblockIcon.TREASURE_CHANCE} (?:§.)*(?:GOOD|GREAT|OUTSTANDING) CATCH! (?:§.)*You caught a (?:§.)*\\[Lvl 1] )(?:§.)*§(?<rarityColor>.)(?<petName>[^§(.]+)(?<end>.*)",
