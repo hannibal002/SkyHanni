931f874fcfc69702e905a8867deb46575e287e48
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-08-31 10:53:01 +0200

    docs

--- src/main/java/at/hannibal2/skyhanni/events/entity/EntityLeaveWorldEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/entity/EntityLeaveWorldEvent.kt
@@ -7,5 +7,17 @@ import net.minecraft.world.entity.Entity
 @Deprecated("Use EntityLeaveWorldEvent instead", ReplaceWith("EntityLeaveWorldEvent"))
 typealias EntityRemovedEvent<T> = EntityLeaveWorldEvent<T>
 
+/**
+ * Fired when an entity is removed from the client world.
+ *
+ * Fired on the main client thread via a Mixin into `TransientEntitySectionManager.Callback.onRemove`.
+ *
+ * Covers every removal reason: despawning, dying, leaving the tracking range and chunk unloading.
+ * The reason is not part of the event, so this is not a death detection. The type parameter [T]
+ * allows filtering for a specific entity type.
+ *
+ * @param T the type of entity
+ * @param entity the entity that was removed from the world
+ */
 @PrimaryFunction("onEntityLeaveWorld")
 class EntityLeaveWorldEvent<T : Entity>(val entity: T) : GenericSkyHanniEvent<T>(entity.javaClass)
