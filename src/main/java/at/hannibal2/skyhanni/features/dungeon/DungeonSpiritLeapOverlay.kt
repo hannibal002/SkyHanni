37e17ac79ce8e244181080abcc6c1e990f7150bb
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-09-01 14:40:17 +0200

    Split up DungeonSpiritLeapOverlay.createLeapItem

--- src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonSpiritLeapOverlay.kt
+++ src/main/java/at/hannibal2/skyhanni/features/dungeon/DungeonSpiritLeapOverlay.kt
@@ -23,10 +23,13 @@ import at.hannibal2.skyhanni.utils.StringUtils.cleanPlayerName
 import at.hannibal2.skyhanni.utils.compat.container
 import at.hannibal2.skyhanni.utils.compat.formattedTextCompatLeadingWhiteLessResets
 import at.hannibal2.skyhanni.utils.renderables.Renderable
+import at.hannibal2.skyhanni.utils.renderables.container.HorizontalContainerRenderable
 import at.hannibal2.skyhanni.utils.renderables.container.HorizontalContainerRenderable.Companion.horizontal
+import at.hannibal2.skyhanni.utils.renderables.container.VerticalContainerRenderable
 import at.hannibal2.skyhanni.utils.renderables.container.VerticalContainerRenderable.Companion.vertical
 import at.hannibal2.skyhanni.utils.renderables.container.table.TableRenderable.Companion.table
 import at.hannibal2.skyhanni.utils.renderables.primitives.ItemStackRenderable.Companion.item
+import at.hannibal2.skyhanni.utils.renderables.primitives.StringRenderable
 import at.hannibal2.skyhanni.utils.renderables.primitives.WrappedStringRenderable.Companion.wrappedText
 import at.hannibal2.skyhanni.utils.renderables.primitives.placeholder
 import at.hannibal2.skyhanni.utils.renderables.primitives.text
@@ -62,7 +65,7 @@ object DungeonSpiritLeapOverlay {
     data class PlayerStackInfo(val playerInfo: DungeonApi.TeamMember?, val stack: SafeItemStack, val slotNumber: Int)
 
     @HandleEvent
-    fun onGuiContainerPreDraw(event: GuiContainerEvent.PreDraw) {
+    private fun onGuiContainerPreDraw(event: GuiContainerEvent.PreDraw) {
         if (!isEnabled()) return
 
         val gui = event.gui
@@ -72,34 +75,70 @@ object DungeonSpiritLeapOverlay {
         containerHeight = gui.height
         scaleFactor = min(containerWidth, containerHeight).toDouble() / max(containerWidth, containerHeight).toDouble()
 
-        val chest = gui.container as ChestMenu
-        playerList = buildList {
-            for ((slot, stack) in chest.getUpperItems()) {
-                val lore = stack.getLore()
-                if (lore.isNotEmpty()) {
-                    val playerInfo = DungeonApi.getPlayerInfo(stack.hoverName.formattedTextCompatLeadingWhiteLessResets().cleanPlayerName())
-                    add(PlayerStackInfo(playerInfo, stack, slot.index))
-                }
-            }
-        }.sortedBy { it.playerInfo?.dungeonClass?.ordinal }
+        updatePlayerList(gui.container as ChestMenu)
 
-        val leapRenderItems = playerList.mapIndexedNotNull { index, player ->
-            createLeapItem(player, index)
-        }
-
-        val spiritLeapOverlay = createSpiritLeapOverlay(leapRenderItems)
-
-        overlayPosition = Position(
-            (gui.width - spiritLeapOverlay.width) / 2,
-            (gui.height - spiritLeapOverlay.height) / 2,
-        ).apply {
-            renderRenderable(spiritLeapOverlay, posLabel = "Spirit Leap Overlay", addToGuiManager = false)
-        }
+        renderCenteredOverlay(gui)
         event.cancel()
     }
 
+    private fun renderCenteredOverlay(gui: ContainerScreen) {
+        val layout = createLeapButtons().take(4).chunked(2)
+        val renderable = createOverlayTable(layout)
+        overlayPosition = Position(
+            (gui.width - renderable.width) / 2,
+            (gui.height - renderable.height) / 2,
+        ).apply {
+            renderRenderable(renderable, posLabel = "Spirit Leap Overlay", addToGuiManager = false)
+        }
+    }
+
+    private fun createLeapButtons(): List<Renderable> = playerList.mapIndexedNotNull { index, stackInfo ->
+        val member = stackInfo.playerInfo ?: return@mapIndexedNotNull null
+        val itemRenderable = createPlayerItem(stackInfo.stack)
+        val row = createRowLayout(itemRenderable, member.createPlayerInfo())
+        val line = createFixedWidthLine(row)
+        val keybindHint = createKeybindHint(index)
+        val content = createButtonContent(keybindHint, line)
+        Renderable.clickable(
+            render = createButtonFrame(content, member.getBackgroundColor()),
+            onLeftClick = { leapToPlayer(stackInfo) },
+        )
+    }
+
+    private fun createButtonContent(keybindHint: Renderable, line: Renderable): VerticalContainerRenderable = Renderable.vertical(
+        keybindHint,
+        Renderable.placeholder(0, height = (-15 * scaleFactor).toInt()),
+        Renderable.fixedSizeColumn(
+            line,
+            height = (containerHeight * 0.35).toInt(),
+        ),
+    )
+
+    private fun updatePlayerList(chest: ChestMenu) {
+        val list = collectPlayerStacks(chest)
+        playerList = sortByClass(list)
+    }
+
+    private fun collectPlayerStacks(chest: ChestMenu): List<PlayerStackInfo> = buildList {
+        for ((slot, stack) in chest.getUpperItems()) {
+            val lore = stack.getLore()
+            if (lore.isNotEmpty()) {
+                val playerInfo = DungeonApi.getPlayerInfo(stack.hoverName.formattedTextCompatLeadingWhiteLessResets().cleanPlayerName())
+                add(PlayerStackInfo(playerInfo, stack, slot.index))
+            }
+        }
+    }
+
+    private fun sortByClass(list: List<PlayerStackInfo>): List<PlayerStackInfo> =
+        list.sortedBy { it.playerInfo?.dungeonClass?.ordinal }
+
+    private fun DungeonApi.TeamMember.createPlayerInfo(): VerticalContainerRenderable {
+        val classInfo = getClassInfo()
+        return Renderable.playerInfo(username, classInfo)
+    }
+
     @HandleEvent
-    fun onKeyPress(event: KeyDownEvent) {
+    private fun onKeyPress(event: KeyDownEvent) {
         if (!isEnabled() || !config.spiritLeapKeybindConfig.enableKeybind) return
         if (!inventory.isInside()) return
         val index = getKeybindIndex(event.keyCode)
@@ -115,12 +154,9 @@ object DungeonSpiritLeapOverlay {
             config.spiritLeapKeybindConfig.keybindOption4,
         )
 
-    private fun getKeybindIndex(keyCode: Int): Int {
-        return spiritLeapKeybinds.indexOf(keyCode)
-    }
+    private fun getKeybindIndex(keyCode: Int): Int = spiritLeapKeybinds.indexOf(keyCode)
 
-    private fun createSpiritLeapOverlay(leapRenderItems: List<Renderable>): Renderable {
-        val layout = leapRenderItems.take(4).chunked(2)
+    private fun createOverlayTable(layout: List<List<Renderable>>): Renderable {
         return if (layout.isNotEmpty()) Renderable.table(
             layout,
             xSpacing = 18,
@@ -134,92 +170,96 @@ object DungeonSpiritLeapOverlay {
         )
     }
 
-    private fun createLeapItem(playerStackInfo: PlayerStackInfo, index: Int): Renderable? {
-        val player = playerStackInfo.playerInfo ?: return null
-        val classInfo = buildString {
-            player.dungeonClass?.let {
-                append(it.displayName)
-                if (config.showDungeonClassLevel) append(" ${player.classLevel}")
-                if (player.playerDead) append(" (Dead)")
-            }
-        }
-
-        val backgroundColor = if (player.playerDead) deadTeammateColor else getClassColor(player.dungeonClass)
-        val itemRenderable = Renderable.drawInsideRoundedRect(
-            Renderable.item(playerStackInfo.stack) {
-                scale = scaleFactor * 0.9 + 2.7
-            },
-            color = Color(255, 255, 255, 100),
-            radius = 5,
-        )
-
-        val playerInfoRenderable = with(Renderable) {
-            vertical(
-                wrappedText(
-                    player.username,
-                    setWidth = (containerWidth * 0.25).toInt(),
-                    scale = scaleFactor + 1.5,
-                ),
-                placeholder(0, (containerHeight * 0.03).toInt()),
-                wrappedText(
-                    classInfo,
-                    setWidth = (containerWidth * 0.25).toInt(),
-                    scale = (scaleFactor * 0.9) + 1.1,
-                ),
-                horizontalAlign = HorizontalAlignment.CENTER,
-                verticalAlign = VerticalAlignment.CENTER,
-            )
-        }
-
-        val buttonLayout = Renderable.horizontal(
-            Renderable.placeholder((containerWidth * 0.01).toInt(), 0),
-            itemRenderable,
-            Renderable.placeholder((containerWidth * 0.01).toInt(), 0),
-            playerInfoRenderable,
-            verticalAlign = VerticalAlignment.CENTER,
-        )
-
-        return Renderable.clickable(
-            Renderable.drawInsideRoundedRectWithOutline(
-                Renderable.vertical(
-                    if (config.spiritLeapKeybindConfig.showKeybindHint && index in 0..<spiritLeapKeybinds.count()) {
-                        Renderable.drawInsideRoundedRectOutline(
-                            Renderable.text(
-                                KeyboardManager.getKeyName(spiritLeapKeybinds[index]),
-                                (scaleFactor * 0.9) + 0.7,
-                                verticalAlign = VerticalAlignment.CENTER,
-                            ),
-                            topOutlineColor = 0xFFFFF,
-                            bottomOutlineColor = 0xFFFFF,
-                            borderOutlineThickness = 2,
-                            padding = 4,
-                            horizontalAlign = HorizontalAlignment.RIGHT,
-                        )
-                    } else {
-                        Renderable.placeholder(width = 10, height = (12 * scaleFactor).toInt())
-                    },
-                    Renderable.placeholder(0, height = (-15 * scaleFactor).toInt()),
-                    Renderable.fixedSizeColumn(
-                        Renderable.fixedSizeLine(
-                            buttonLayout,
-                            width = (containerWidth * 0.40).toInt(),
-                            verticalAlign = VerticalAlignment.CENTER,
-                        ),
-                        height = (containerHeight * 0.35).toInt(),
-                    ),
-                ),
-                verticalAlign = VerticalAlignment.CENTER,
-                color = backgroundColor.toColor(),
-                topOutlineColor = 0xFFFFF,
-                bottomOutlineColor = 0xFFFFF,
-                borderOutlineThickness = 2,
-                radius = 7,
-                smoothness = 10,
-                padding = 5,
-            ),
-            onLeftClick = { leapToPlayer(playerStackInfo) },
-        )
+    private fun DungeonApi.TeamMember.getBackgroundColor(): Color {
+        val backgroundColor = if (playerDead) deadTeammateColor else getClassColor(dungeonClass)
+        return backgroundColor.toColor()
     }
+
+    private fun createFixedWidthLine(content: HorizontalContainerRenderable): Renderable = Renderable.fixedSizeLine(
+        content,
+        width = (containerWidth * 0.40).toInt(),
+        verticalAlign = VerticalAlignment.CENTER,
+    )
+
+    private fun createButtonFrame(
+        input: VerticalContainerRenderable,
+        backgroundColor: Color,
+    ): Renderable = Renderable.drawInsideRoundedRectWithOutline(
+        input = input,
+        verticalAlign = VerticalAlignment.CENTER,
+        color = backgroundColor,
+        topOutlineColor = 0xFFFFF,
+        bottomOutlineColor = 0xFFFFF,
+        borderOutlineThickness = 2,
+        radius = 7,
+        smoothness = 10,
+        padding = 5,
+    )
+
+    private fun createPlayerItem(stack: SafeItemStack): Renderable = Renderable.drawInsideRoundedRect(
+        Renderable.item(stack) {
+            scale = scaleFactor * 0.9 + 2.7
+        },
+        color = Color(255, 255, 255, 100),
+        radius = 5,
+    )
+
+    private fun DungeonApi.TeamMember.getClassInfo(): String = buildString {
+        dungeonClass?.let {
+            append(it.displayName)
+            if (config.showDungeonClassLevel) append(" $classLevel")
+            if (playerDead) append(" (Dead)")
+        }
+    }
+
+    private fun createRowLayout(
+        itemRenderable: Renderable,
+        playerInfo: VerticalContainerRenderable,
+    ): HorizontalContainerRenderable = Renderable.horizontal(
+        Renderable.placeholder((containerWidth * 0.01).toInt(), 0),
+        itemRenderable,
+        Renderable.placeholder((containerWidth * 0.01).toInt(), 0),
+        playerInfo,
+        verticalAlign = VerticalAlignment.CENTER,
+    )
+
+    private fun Renderable.Companion.playerInfo(text: String, classInfo: String): VerticalContainerRenderable = vertical(
+        wrappedText(
+            text,
+            setWidth = (containerWidth * 0.25).toInt(),
+            scale = scaleFactor + 1.5,
+        ),
+        placeholder(0, (containerHeight * 0.03).toInt()),
+        wrappedText(
+            classInfo,
+            setWidth = (containerWidth * 0.25).toInt(),
+            scale = (scaleFactor * 0.9) + 1.1,
+        ),
+        horizontalAlign = HorizontalAlignment.CENTER,
+        verticalAlign = VerticalAlignment.CENTER,
+    )
+
+    private fun createKeybindHint(index: Int): Renderable {
+        val hasKeybind = index in 0..<spiritLeapKeybinds.count()
+        val showHint = config.spiritLeapKeybindConfig.showKeybindHint && hasKeybind
+        if (!showHint) return Renderable.placeholder(width = 10, height = (12 * scaleFactor).toInt())
+        return createKeybindBox(createKeybindText(index))
+    }
+
+    private fun createKeybindBox(input: StringRenderable): Renderable = Renderable.drawInsideRoundedRectOutline(
+        input,
+        topOutlineColor = 0xFFFFF,
+        bottomOutlineColor = 0xFFFFF,
+        borderOutlineThickness = 2,
+        padding = 4,
+        horizontalAlign = HorizontalAlignment.RIGHT,
+    )
+
+    private fun createKeybindText(index: Int): StringRenderable = Renderable.text(
+        KeyboardManager.getKeyName(spiritLeapKeybinds[index]),
+        (scaleFactor * 0.9) + 0.7,
+        verticalAlign = VerticalAlignment.CENTER,
+    )
 
     private fun leapToPlayer(player: PlayerStackInfo) {
         val playerInfo = player.playerInfo ?: return
@@ -244,7 +284,7 @@ object DungeonSpiritLeapOverlay {
     private fun isEnabled() = config.enabled && DungeonApi.inDungeon() && DungeonApi.started && !DungeonApi.completed
 
     @HandleEvent
-    fun onConfigFix(event: ConfigUpdaterMigrator.ConfigFixEvent) {
+    private fun onConfigFix(event: ConfigUpdaterMigrator.ConfigFixEvent) {
         event.move(98, "dungeon.spiritLeapOverlay.archerClassColor", "dungeon.spiritLeapOverlay.colorConfig.archerClassColor")
         event.move(98, "dungeon.spiritLeapOverlay.mageClassColor", "dungeon.spiritLeapOverlay.colorConfig.mageClassColor")
         event.move(98, "dungeon.spiritLeapOverlay.berserkClassColor", "dungeon.spiritLeapOverlay.colorConfig.berserkClassColor")
