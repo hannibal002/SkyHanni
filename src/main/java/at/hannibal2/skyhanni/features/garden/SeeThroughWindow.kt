affe8aaf4c828fa4ec80f1cf6868987b1cb28a47
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-04 16:57:00 +0300

    use the event key code

--- src/main/java/at/hannibal2/skyhanni/features/garden/SeeThroughWindow.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/SeeThroughWindow.kt
@@ -2,10 +2,10 @@ package at.hannibal2.skyhanni.features.garden
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
+import at.hannibal2.skyhanni.events.minecraft.KeyDownEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.ChatUtils
 import at.hannibal2.skyhanni.utils.ConditionalUtils.afterChange
-import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyHeld
 import at.hannibal2.skyhanni.utils.collection.CollectionUtils.equalsOneOf
 import at.hannibal2.skyhanni.utils.compat.MinecraftCompat
 import net.minecraft.client.Minecraft
@@ -27,8 +27,8 @@ object SeeThroughWindow {
     }
 
     @HandleEvent
-    private fun onKeyDown() {
-        if (!config.keybind.isKeyHeld()) return
+    private fun onKeyDown(event: KeyDownEvent) {
+        if (event.keyCode != config.keybind) return
         if (MinecraftCompat.screen != null) return
 
         isActive = !isActive
