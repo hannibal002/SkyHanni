f37350241b6bbaddcdcf8137caadf14e26add295
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-10 20:59:48 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/events/GuiKeyPressEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/GuiKeyPressEvent.kt
@@ -1,7 +1,6 @@
 package at.hannibal2.skyhanni.events
 
 import at.hannibal2.skyhanni.api.event.CancellableSkyHanniEvent
-import at.hannibal2.skyhanni.events.render.gui.GuiMouseInputEvent
 import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 import at.hannibal2.skyhanni.utils.SafeItemStack
 import at.hannibal2.skyhanni.utils.compat.InventoryCompat
