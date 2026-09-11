420d43f1ae19596a37e9fee566e2013c93a52558
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-11 16:22:58 +0300

    use `File.seperator`

--- src/main/java/at/hannibal2/skyhanni/utils/SkyHanniLogger.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/SkyHanniLogger.kt
@@ -14,12 +14,12 @@ import kotlin.time.Duration.Companion.days
 open class SkyHanniLogger(private val filePath: String) {
 
     private val format = SimpleDateFormat("HH:mm:ss")
-    private val logFileName by lazy { "$timedFormattedDir/$filePath.log" }
+    private val logFileName by lazy { "$timedFormattedDir${File.separator}$filePath.log" }
 
     companion object {
         private var deletedExpired = false
         private val timedFormattedDir by lazy {
-            "${SkyHanniMod.logsDir}/${SimpleDateFormat("yyyy_MM_dd/HH_mm_ss").formatCurrentTime()}"
+            "${SkyHanniMod.logsDir}${File.separator}${SimpleDateFormat("yyyy_MM_dd/HH_mm_ss").formatCurrentTime()}"
         }
     }
 
