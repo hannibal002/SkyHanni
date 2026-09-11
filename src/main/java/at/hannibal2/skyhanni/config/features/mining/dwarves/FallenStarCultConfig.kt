2301edcef9d7e61a16030930f0d43fc23ade94ed
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-11 13:22:04 +0300

    bye bye

--- src/main/java/at/hannibal2/skyhanni/config/features/mining/dwarves/FallenStarCultConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/dwarves/FallenStarCultConfig.kt
@@ -16,7 +16,7 @@ class FallenStarCultConfig {
     )
     @ConfigEditorBoolean
     @FeatureToggle
-    var enabled: Boolean = true
+    var enabled: Boolean = false
 
     @Expose
     @ConfigOption(
