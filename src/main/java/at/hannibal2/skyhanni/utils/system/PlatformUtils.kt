c66f2e47eb31b11fba1ceb362abb0161f804f0c5
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-12 01:00:34 +0300

    step 1

--- src/main/java/at/hannibal2/skyhanni/utils/system/PlatformUtils.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/system/PlatformUtils.kt
@@ -10,6 +10,7 @@ import at.hannibal2.skyhanni.utils.MarkdownBuilder
 import at.hannibal2.skyhanni.utils.OSUtils
 import at.hannibal2.skyhanni.utils.VersionConstants
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.SharedConstants
 import java.nio.file.Path
 import kotlin.system.exitProcess
 
@@ -20,29 +21,19 @@ import kotlin.system.exitProcess
 @SkyHanniModule
 object PlatformUtils {
 
-    val MC_VERSION: String = net.minecraft.SharedConstants.getCurrentVersion().name()
+    val MC_VERSION: String = SharedConstants.getCurrentVersion().name()
 
     @JvmStatic
     @get:JvmName("isDevEnvironment")
-    val isDevEnvironment: Boolean by lazy {
-        FabricLoader.getInstance().isDevelopmentEnvironment
-    }
+    val isDevEnvironment: Boolean = FabricLoader.getInstance().isDevelopmentEnvironment
 
-    val gameDir: Path by lazy {
-        FabricLoader.getInstance().gameDir
-    }
+    val gameDir: Path = FabricLoader.getInstance().gameDir
 
-    val dataDir: Path by lazy {
-        gameDir.resolve("data")
-    }
+    val dataDir: Path = gameDir.resolve("data")
 
-    val configDir: Path by lazy {
-        FabricLoader.getInstance().configDir
-    }
+    val configDir: Path = FabricLoader.getInstance().configDir
 
-    val logsDir: Path by lazy {
-        dataDir.resolve("logs")
-    }
+    val logsDir: Path = dataDir.resolve("logs")
 
     private val allowedFabricReports = setOf(
         "fabricloader",
