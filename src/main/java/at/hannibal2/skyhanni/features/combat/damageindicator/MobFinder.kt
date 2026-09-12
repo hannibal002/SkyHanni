7d4b35cba22f7d5bf1a81011b7c456adeb43ecf4
AvrgUser <96657216+AverageUser125@users.noreply.github.com>
2026-09-13 00:20:57 +0300

    Fix: Damage Indicator For Dummy (#6534)
    Co-authored-by: hannibal2 <24389977+hannibal002@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/MobFinder.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/MobFinder.kt
@@ -87,35 +87,32 @@ class MobFinder {
         DungeonApi.inDungeon() -> tryAddDungeon(mob)
         RiftApi.inRift() -> tryAddRift(mob)
         GardenApi.inGarden() -> tryAddGarden(mob)
-        else -> {
-            if (mob.name == "Dummy") {
-                EntityResult(bossType = BossType.DUMMY)
-            } else {
-                when (val entity = mob.baseEntity) {
-                    /*
-                     * Note that the order does matter here.
-                     * For example, if you put EntityZombie before EntityPigZombie,
-                     * EntityPigZombie will never be reached because EntityPigZombie extends EntityZombie.
-                     * Please take this into consideration if you are to modify this.
-                     */
-                    is RemotePlayer -> tryAddEntityOtherPlayerMP(mob)
-                    is IronGolem -> tryAddEntityIronGolem(mob)
-                    is ZombifiedPiglin -> tryAddEntityPigZombie(mob)
-                    is MagmaCube -> tryAddEntityMagmaCube(mob)
-                    is EnderMan -> tryAddEntityEnderman(mob)
-                    is AbstractSkeleton -> tryAddEntitySkeleton(mob)
-                    is Guardian -> tryAddEntityGuardian(mob)
-                    is Zombie -> tryAddEntityZombie(mob)
-                    is WitherBoss -> tryAddEntityWither(mob)
-                    is EnderDragon -> tryAddEntityDragon(mob)
-                    is Spider -> tryAddEntitySpider(mob)
-                    is Horse -> tryAddEntityHorse(mob)
-                    is Blaze -> tryAddEntityBlaze(mob)
-                    is Wolf -> tryAddEntityWolf(mob)
-                    else -> null
-                }
-            }
-        }
+        IslandType.PRIVATE_ISLAND.isInIsland() && mob.name == "Dummy" -> EntityResult(bossType = BossType.DUMMY)
+        else -> tryAddByEntityType(mob)
+    }
+
+    /**
+     * Note that the order does matter here.
+     * For example, if you put [Zombie] before [ZombifiedPiglin],
+     * [ZombifiedPiglin] will never be reached because it extends [Zombie].
+     * Please take this into consideration if you are to modify this.
+     */
+    private fun tryAddByEntityType(mob: Mob): EntityResult? = when (mob.baseEntity) {
+        is RemotePlayer -> tryAddEntityOtherPlayerMP(mob)
+        is IronGolem -> tryAddEntityIronGolem(mob)
+        is ZombifiedPiglin -> tryAddEntityPigZombie(mob)
+        is MagmaCube -> tryAddEntityMagmaCube(mob)
+        is EnderMan -> tryAddEntityEnderman(mob)
+        is AbstractSkeleton -> tryAddEntitySkeleton(mob)
+        is Guardian -> tryAddEntityGuardian(mob)
+        is Zombie -> tryAddEntityZombie(mob)
+        is WitherBoss -> tryAddEntityWither(mob)
+        is EnderDragon -> tryAddEntityDragon(mob)
+        is Spider -> tryAddEntitySpider(mob)
+        is Horse -> tryAddEntityHorse(mob)
+        is Blaze -> tryAddEntityBlaze(mob)
+        is Wolf -> tryAddEntityWolf(mob)
+        else -> null
     }
 
     private fun tryAddGarden(mob: Mob): EntityResult? {
@@ -358,7 +355,8 @@ class MobFinder {
             EntityResult(bossType = BossType.GAIA_CONSTRUCT)
         }
         // TODO use Lord Jawbus Name
-        mob.baseEntity.hasMaxHealth(100_000_000) -> {
+        IslandType.CRIMSON_ISLE.isInIsland() &&
+            mob.baseEntity.hasMaxHealth(100_000_000) -> {
             EntityResult(bossType = BossType.LORD_JAWBUS)
         }
 
