33c7542c17faea9e05677d23044995a5e183bf54
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-12 16:40:07 +0300

    add KDOC to attribute event

--- src/main/java/at/hannibal2/skyhanni/events/entity/EntityAttributeUpdateEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/entity/EntityAttributeUpdateEvent.kt
@@ -6,6 +6,14 @@ import net.minecraft.core.Holder
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.Attribute
 
+/**
+ * Event that is called when an entity's attribute is updated.
+ * Like max health, movement speed, absorption etc.
+ *
+ * @param T The type of the entity whose attribute was updated.
+ * @property entity The entity whose attribute was updated.
+ * @property attribute The attribute that was updated.
+ */
 @PrimaryFunction("onEntityAttributeUpdate")
 data class EntityAttributeUpdateEvent<T : LivingEntity>(
     val entity: T,
