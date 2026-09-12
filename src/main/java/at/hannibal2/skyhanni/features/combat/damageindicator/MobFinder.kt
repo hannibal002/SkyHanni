4b1a7c9e5d00b882569a9a90683a43abac3faeb5
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-09-12 22:25:03 +0200

    Extract entity type dispatch into tryAddByEntityType

--- src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/MobFinder.kt
+++ src/main/java/at/hannibal2/skyhanni/features/combat/damageindicator/MobFinder.kt
@@ -88,31 +88,31 @@ class MobFinder {
         RiftApi.inRift() -> tryAddRift(mob)
         GardenApi.inGarden() -> tryAddGarden(mob)
         IslandType.PRIVATE_ISLAND.isInIsland() && mob.name == "Dummy" -> EntityResult(bossType = BossType.DUMMY)
-        else -> {
-            when (mob.baseEntity) {
-                /*
-                 * Note that the order does matter here.
-                 * For example, if you put EntityZombie before EntityPigZombie,
-                 * EntityPigZombie will never be reached because EntityPigZombie extends EntityZombie.
-                 * Please take this into consideration if you are to modify this.
-                 */
-                is RemotePlayer -> tryAddEntityOtherPlayerMP(mob)
-                is IronGolem -> tryAddEntityIronGolem(mob)
-                is ZombifiedPiglin -> tryAddEntityPigZombie(mob)
-                is MagmaCube -> tryAddEntityMagmaCube(mob)
-                is EnderMan -> tryAddEntityEnderman(mob)
-                is AbstractSkeleton -> tryAddEntitySkeleton(mob)
-                is Guardian -> tryAddEntityGuardian(mob)
-                is Zombie -> tryAddEntityZombie(mob)
-                is WitherBoss -> tryAddEntityWither(mob)
-                is EnderDragon -> tryAddEntityDragon(mob)
-                is Spider -> tryAddEntitySpider(mob)
-                is Horse -> tryAddEntityHorse(mob)
-                is Blaze -> tryAddEntityBlaze(mob)
-                is Wolf -> tryAddEntityWolf(mob)
-                else -> null
-            }
-        }
+        else -> tryAddByEntityType(mob)
+    }
+
+    /**
+     * Note that the order does matter here.
+     * For example, if you put [Zombie] before [ZombifiedPiglin],
+     * [ZombifiedPiglin] will never be reached because it extends [Zombie].
+     * Please take this into consideration if you are to modify this.
+     */
+    private fun tryAddByEntityType(mob: Mob): EntityResult? = when (mob.baseEntity) {
+        is RemotePlayer -> tryAddEntityOtherPlayerMP(mob)
+        is IronGolem -> tryAddEntityIronGolem(mob)
+        is ZombifiedPiglin -> tryAddEntityPigZombie(mob)
+        is MagmaCube -> tryAddEntityMagmaCube(mob)
+        is EnderMan -> tryAddEntityEnderman(mob)
+        is AbstractSkeleton -> tryAddEntitySkeleton(mob)
+        is Guardian -> tryAddEntityGuardian(mob)
+        is Zombie -> tryAddEntityZombie(mob)
+        is WitherBoss -> tryAddEntityWither(mob)
+        is EnderDragon -> tryAddEntityDragon(mob)
+        is Spider -> tryAddEntitySpider(mob)
+        is Horse -> tryAddEntityHorse(mob)
+        is Blaze -> tryAddEntityBlaze(mob)
+        is Wolf -> tryAddEntityWolf(mob)
+        else -> null
     }
 
     private fun tryAddGarden(mob: Mob): EntityResult? {
