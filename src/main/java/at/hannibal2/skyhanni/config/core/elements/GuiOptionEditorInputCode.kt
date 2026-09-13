95295307ddf31d509baaa6221f0d44556fce2f79
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-13 16:49:53 +0300

    detekt2

--- src/main/java/at/hannibal2/skyhanni/config/core/elements/GuiOptionEditorInputCode.kt
+++ src/main/java/at/hannibal2/skyhanni/config/core/elements/GuiOptionEditorInputCode.kt
@@ -86,7 +86,8 @@ class GuiOptionEditorInputCode(option: ProcessedOption, defaultInputCode: InputC
                             return true
                         }
                         if (mouseX > width / 6 - 24 + 48 - 3 && mouseX < width / 6 - 24 + 48 + 13 - 5 &&
-                            mouseY > height - 7 - 14 + 3 && mouseY < height - 7 - 14 + 3 + 11) {
+                            mouseY > height - 7 - 14 + 3 && mouseY < height - 7 - 14 + 3 + 11
+                            ) {
                             option.set(defaultInputCode)
                             return true
                         }
