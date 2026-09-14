708b8aa395f1e4cf8603558e3fe15be5c9b35953
AvrgUser <96657216+AverageUser125@users.noreply.github.com>
2026-09-14 22:57:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,7 +80,7 @@ val includeBackupRepo = tasks.register<DownloadBackupRepo>("includeBackupRepo") 
     this.user = "hannibal002"
     this.repo = "SkyHanni-Repo"
     this.branch = "main"
-    this.resourcePath = "assets/skyhanni/repo.tar.gz"
+    this.resourcePath = "assets/skyhanni/shrepo.tar.gz"
     this.outputDirectory.set(layout.buildDirectory.dir("downloadedRepo"))
 }
 
@@ -88,7 +88,7 @@ val includeBackupNeuRepo = tasks.register<DownloadBackupRepo>("includeBackupNeuR
     this.user = "NotEnoughUpdates"
     this.repo = "NotEnoughUpdates-Repo"
     this.branch = "master"
-    this.resourcePath = "assets/skyhanni/neu-repo.tar.gz"
+    this.resourcePath = "assets/skyhanni/neurepo.tar.gz"
     this.outputDirectory.set(layout.buildDirectory.dir("downloadedNeuRepo"))
 }
 
