0fcbca1ac7512cf677fe28d235204cafcb1bbaf0
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2026-09-17 11:54:03 +0200

    Feature: Critter Capsule Hider (#6542)

--- src/main/java/at/hannibal2/skyhanni/config/features/hunting/HuntingConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/hunting/HuntingConfig.kt
@@ -2,7 +2,7 @@ package at.hannibal2.skyhanni.config.features.hunting
 
 import at.hannibal2.skyhanni.config.FeatureToggle
 import at.hannibal2.skyhanni.config.core.config.Position
-import at.hannibal2.skyhanni.config.features.foraging.SafariConfig
+import at.hannibal2.skyhanni.config.features.hunting.safari.SafariConfig
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.Category
