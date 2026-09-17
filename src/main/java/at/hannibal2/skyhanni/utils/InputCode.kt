3c58917076888165922853087da43d778eb03440
AvrgUser <96657216+AverageUser125@users.noreply.github.com>
2026-09-17 13:14:57 +0300

    Add InputCode optimization TODO

--- src/main/java/at/hannibal2/skyhanni/utils/InputCode.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/InputCode.kt
@@ -176,6 +176,7 @@ enum class InputCode(
         fun fromMouseButton(mouseButton: Int): InputCode =
             fromKey(InputConstants.Type.MOUSE.getOrCreate(mouseButton))
 
+        // TODO: Optimize
         fun fromKey(key: InputConstants.Key): InputCode =
             entries.firstOrNull { it.key == key } ?: UNKNOWN
     }
