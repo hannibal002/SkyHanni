51fee80117cbf75759424913643aaf6ce697f25d
AvrgUser <96657216+AverageUser125@users.noreply.github.com>
2026-09-17 13:53:25 +0300

    Detekt

--- src/main/java/at/hannibal2/skyhanni/config/features/hunting/HuntingConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/hunting/HuntingConfig.kt
@@ -3,8 +3,8 @@ package at.hannibal2.skyhanni.config.features.hunting
 import at.hannibal2.skyhanni.config.ConfigEditorKeyMapping
 import at.hannibal2.skyhanni.config.FeatureToggle
 import at.hannibal2.skyhanni.config.core.config.Position
-import at.hannibal2.skyhanni.utils.InputCode
 import at.hannibal2.skyhanni.config.features.hunting.safari.SafariConfig
+import at.hannibal2.skyhanni.utils.InputCode
 import com.google.gson.annotations.Expose
 import io.github.notenoughupdates.moulconfig.annotations.Accordion
 import io.github.notenoughupdates.moulconfig.annotations.Category
