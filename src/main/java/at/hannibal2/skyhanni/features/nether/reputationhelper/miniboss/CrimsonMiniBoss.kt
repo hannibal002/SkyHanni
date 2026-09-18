6c9fc51a38e97f5afa300d6c7960d862b0396a62
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-09-18 14:45:06 +0300

    detekt

--- src/main/java/at/hannibal2/skyhanni/features/nether/reputationhelper/miniboss/CrimsonMiniBoss.kt
+++ src/main/java/at/hannibal2/skyhanni/features/nether/reputationhelper/miniboss/CrimsonMiniBoss.kt
@@ -2,7 +2,6 @@ package at.hannibal2.skyhanni.features.nether.reputationhelper.miniboss
 
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.NeuInternalName
-import at.hannibal2.skyhanni.utils.repopatterns.RepoPattern
 
 class CrimsonMiniBoss(
     val displayName: String,
